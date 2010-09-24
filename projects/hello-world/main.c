02e26536f06a762dad2c9554483706d235dbc258
Kaspar Schleiser <kaspar@schleiser.de>
2010-09-24 16:28:12 +0200

    * restored hello-world project

--- projects/hello-world/main.c
+++ projects/hello-world/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include <math.h>
-#include <board.h>
-#include "swtimer.h"
 
 
 int main(void)
 {
-    double a = 1.2;
-    printf("Hello world! %g\n", a);
+    printf("Hello world!\n");
     return 0;
 }
