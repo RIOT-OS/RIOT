175bc285380459806bf69769370987aba24ddf67
Stephan <stephan@mango.localdomain>
2010-10-05 21:53:00 +0200

    created 6lowpan c-file with todo for output function

---
+++ sys/net/sixlowpan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+static output(void){
+	
+	// TODO
+	// 1. get dest addr from function param
+	// 2. get pointer to packet buffer
+	// 3. if dest is null send broadcast
+	// 4. hc1 or hc1x header compression
+	// 5. check if ip-packet is to large to fit into a single mac-packet
+	// 	if no, fragment the packet
+			// frag1 dispatch + header
+			// fragn dispatch
+			// set fragments into queue and send it 	
+}
