668eb658d05fae2389d9c2cf4a56a7ca33aa9e2f
Stephan <stephan@mango.localdomain>
2010-10-05 22:39:14 +0200

    added packet-fragmentation defines

--- sys/net/sixlowpan.h
+++ sys/net/sixlowpan.h
@@ -1,1 +1,5 @@
-// sixlowpan header file
+/* fragmentation defines */
+#define FRAG1_HDR_LEN		32 	// in bit
+#define FRAGN_HDR_LEN		40 	// in bit
+#define FRAG1_DISPATCH		0xC0	// 11000000
+#define FRAGN_DISPATCH		0xE0	// 11100000  
