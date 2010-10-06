bba315e93ff8fcebf2358d53f6b19d7906412f4a
Stephan Zeisberg <zeisberg@mi.fu-berlin.de>
2010-10-06 17:15:05 +0200

    implemented 48bit mac to eui64 conversion

--- sys/net/sixlowpan.c
+++ sys/net/sixlowpan.c
@@ -1,5 +1,9 @@
+/* 6LoWPAN layer 3 implementation */
+
 #include <stdio.h>
 #include "drivers/cc110x/cc1100.h"
+#include "sixlowmac.h"
+
 
 static void output(void){
 	
@@ -13,4 +17,21 @@ static void output(void){
 			// frag1 dispatch + header
 			// fragn dispatch
 			// set fragments into queue and send it 	
+}
+
+/* convert 48-bit MAC address to IPv6 modified EUI-64 Identifier*/
+static eui64 get_eui64_from_mac(void){
+    mac_addr maddr;
+    eui64 ident;
+    
+    init_mac_address(maddr);
+    
+    /* change bit 7 from oui1 to 1 */
+    ident.oui1 = (maddr.oui_1 >> 8) | 0x2;
+    ident.oui2 =  (maddr.oui_1 << 8) | maddr.oui_2;
+    ident.pattern = MAC_TO_EUI64_PTRN;
+    ident.ext_ident_1 = maddr.ext_ident_1;
+    ident.ext_ident_2 = maddr.ext_ident_1;  
+
+    return ident;
 }
--- sys/net/sixlowpan.h
+++ sys/net/sixlowpan.h
@@ -2,4 +2,15 @@
 #define FRAG1_HDR_LEN		32 	// in bit
 #define FRAGN_HDR_LEN		40 	// in bit
 #define FRAG1_DISPATCH		0xC0	// 11000000
-#define FRAGN_DISPATCH		0xE0	// 11100000  
+#define FRAGN_DISPATCH		0xE0	// 11100000
+
+/* 48-bit MAC to EUI64 pattern */
+#define MAC_TO_EUI64_PTRN   0xFFFE
+
+typedef struct eui64{
+    uint8_t oui_1;
+    uint16_t oui_2;
+    uint16_t pattern;
+    uint8_t ext_ident_1;
+    uint16_t ext_ident_2;
+} eui64;  
