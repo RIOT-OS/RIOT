a5577625b5544e290a091d82765e97dd63e73ef2
Stephan Zeisberg <zeisberg@mi.fu-berlin.de>
2010-10-19 23:07:29 +0200

    reordered functions for better layering

--- sys/net/sixlowpan/sixlowmac.h
+++ sys/net/sixlowpan/sixlowmac.h
@@ -3,29 +3,12 @@
 #ifndef SIXLOWMAC_H
 #define SIXLOWMAC_H
 
-#include "sixlowip.h"
 #include <stdio.h>
 #include <stdint.h>
+#include "sixlowip.h"
 
-#define MSBA2_OUI	            0x005BA2	// 24bit OUI 
-#define R8BIT	                0xA2		// random 8bit
+uint16_t get_radio_address(link_layer_addr *lla);
 
-#ifdef MSBA2_SENSOR_NODE
-#define OUI                     0x005BA2
-#else
-#define OUI                     0x0
-#endif
+void send(void);
 
-#define RADIO_CONF              radio_cc1100
-#define RADIO                   RADIO_CONF
-
-void create_link_local_prefix(ipv6_addr *ipaddr);
-
-void create_all_routers_mcast_addr(ipv6_addr *ipaddr);
-
-void set_eui64(ipv6_addr *ipaddr);
-
-void get_link_layer_addr_from_ipaddr(ipv6_addr *ipaddr, link_layer_addr *lla);
-
-void bootstrapping(void);
-#endif
+#endif /* SIXLOWMAC_H*/
