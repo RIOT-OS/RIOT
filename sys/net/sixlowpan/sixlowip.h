3dd901a5491d62d7ec32b5dc3679a5f075852cc0
Stephan Zeisberg <zeisberg@mi.fu-berlin.de>
2010-10-19 23:06:47 +0200

    reordered functions for better layering

--- sys/net/sixlowpan/sixlowip.h
+++ sys/net/sixlowpan/sixlowip.h
@@ -33,6 +33,19 @@
 /* size of global buffer */
 #define BUFFER_SIZE (LL_HEADER_LENGTH + MTU)
 
+#define MSBA2_OUI                   0x005BA2    // 24bit OUI 
+#define R8BIT                       0xA2        // random 8bit
+
+#ifdef MSBA2_SENSOR_NODE
+#define OUI                         0x005BA2
+#else
+#define OUI                         0x0
+#endif
+
+#define RADIO_CONF                  radio_cc1100
+#define RADIO                       RADIO_CONF
+
+
 extern uint8_t ipv6_ext_hdr_len;
 
 #define LLHDR_IPV6HDR_LENGTH (LL_HEADER_LENGTH + IPV6_HEADER_LENGTH + ipv6_ext_hdr_len)
@@ -73,4 +86,14 @@ typedef struct link_layer_addr{
 
 #define HTONS(a) (uint16_t)((((uint16_t) (a)) << 8) | (((uint16_t) (a)) >> 8))
 
-#endif
+void create_link_local_prefix(ipv6_addr *ipaddr);
+
+void create_all_routers_mcast_addr(ipv6_addr *ipaddr);
+
+void set_eui64(ipv6_addr *ipaddr);
+
+link_layer_addr* get_eui(ipv6_addr *ipaddr);
+
+void bootstrapping(void);
+
+#endif /* SIXLOWIP_H*/
