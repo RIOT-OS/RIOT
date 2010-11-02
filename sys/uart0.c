09a8bdd2695a078b40486bd347ee14d8473552c8
Kaspar Schleiser <kaspar@schleiser.de>
2010-11-02 13:01:50 +0100

    * remove unneccessary debug message

--- sys/uart0.c
+++ sys/uart0.c
@@ -25,7 +25,6 @@ void board_uart0_init() {
     int pid = thread_create(&uart0_thread_tcb, uart0_thread_stack, sizeof(uart0_thread_stack), PRIORITY_MAIN-1, CREATE_STACKTEST, uart0_loop, "uart0");
     uart0_handler_pid = pid;
     puts("uart0_init() [OK]");
-    printf("%i\n", sizeof(uart0_thread_stack));
 }
 
 void uart0_handle_incoming(int c) {
