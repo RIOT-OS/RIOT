7f8baa881853489fdf5ae2c47a0f7f9f45bac73e
Kaspar Schleiser <kaspar@schleiser.de>
2010-11-04 14:21:12 +0100

    * removed unneccessary priority names

--- core/include/kernel.h
+++ core/include/kernel.h
@@ -71,14 +71,6 @@
 
 #define PRIORITY_IDLE           PRIORITY_MIN
 #define PRIORITY_MAIN           PRIORITY_MIN-1
-#define PRIORITY_CMD_THREADS    PRIORITY_MIN-2      ///< all cmd handler threads
-#define PRIORITY_CBD            PRIORITY_MIN-3
-#define PRIORITY_CMDD           PRIORITY_MIN-4      ///< cmdengine demon
-#define PRIORITY_PRINTTHREAD    PRIORITY_MIN-5      ///< mprint worker thread
-#define PRIORITY_HAL            PRIORITY_MIN-6
-#define PRIORITY_UTIMER         PRIORITY_MIN-7
-#define PRIORITY_MMREQ          PRIORITY_MIN-8
-#define PRIORITY_CC1100         PRIORITY_MIN-9
 
 /**
  * @brief   Check whether called from interrupt service routine
