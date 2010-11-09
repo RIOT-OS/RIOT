fb784c6628cf03d5ba37785ba68835773576e2d9
Oliver Hahm <oleg@hobbykeller.org>
2010-11-09 18:51:04 +0100

    * fixed units in ltc command

--- sys/shell/ltc4150.c
+++ sys/shell/ltc4150.c
@@ -2,7 +2,7 @@
 #include <ltc4150.h>
 
 void _get_current_handler(char* unused) {
-	printf("Power usage: %.4f mA (%.4f mA avg/ %.4f mAh total / %i sec)\n", ltc4150_get_current_mA(), ltc4150_get_avg_mA(), ltc4150_get_total_mAh(), ltc4150_get_interval());
+	printf("Power usage: %.4f mA (%.4f mA avg/ %.4f mAh total / %i usec)\n", ltc4150_get_current_mA(), ltc4150_get_avg_mA(), ltc4150_get_total_mAh(), ltc4150_get_interval());
 }
 
 void _reset_current_handler(char* unused) {
