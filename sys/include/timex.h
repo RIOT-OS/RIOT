0de6300af09151ce640d29766c29e01c747511c4
Kaspar Schleiser <kaspar@schleiser.de>
2010-12-01 17:23:28 +0100

    * added some time functions

--- sys/include/timex.h
+++ sys/include/timex.h
@@ -8,6 +8,12 @@ typedef struct timex_t {
     uint32_t nanoseconds;
 } timex_t;
 
+/* a+b */
 timex_t timex_add(const timex_t a, const timex_t b); 
+
+/* a-b*/
+timex_t timex_sub(const timex_t a, const timex_t b);
+
+timex_t timex_set(uint32_t seconds, uint32_t nanoseconds);
 
 #endif /* __TIMEX_H */
--- sys/timex.c
+++ sys/timex.c
@@ -12,3 +12,25 @@ timex_t timex_add(const timex_t a, const timex_t b) {
 
     return result;
 }
+
+timex_t timex_set(uint32_t seconds, uint32_t nanoseconds) {
+    timex_t result;
+    result.seconds = seconds;
+    result.nanoseconds = nanoseconds;
+
+    while (result.nanoseconds > 1000*1000) {
+        result.nanoseconds -= 1000*1000;
+        result.seconds++;
+    }
+
+    return result;
+
+}
+
+timex_t timex_sub(const timex_t a, const timex_t b) {
+    timex_t result;
+    result.seconds = a.seconds - b.seconds;
+    result.nanoseconds = a.nanoseconds - b.nanoseconds;
+
+    return result;
+}
