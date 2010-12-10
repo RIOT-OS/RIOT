d5ff633d07dc9c1bbb39e02403fb2a05af5818ab
Kaspar Schleiser <kaspar@schleiser.de>
2010-12-10 16:49:29 +0100

    * initial checkin of last in first out array queue (lifo)

---
+++ core/include/lifo.h
@@ -0,0 +1,9 @@
+#ifndef __LIFO_H
+#define __LIFO_H 
+
+int lifo_empty(int *array);
+void lifo_init(int *array, int n);
+void lifo_insert(int *array, int i);
+int lifo_get(int *array);
+
+#endif /* __LIFO_H */
