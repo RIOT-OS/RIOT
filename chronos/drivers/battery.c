27afca63aacd80388643364dbfb1b5070b1c0e5e
Oliver Hahm <oleg@hobbykeller.org>
2010-12-17 13:38:03 +0100

    [board/chronos] * introduced battery driver

---
+++ chronos/drivers/battery.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <cc430x613x.h>
+#include <cc430-adc.h>
+
+uint16_t battery_get_voltate(void) {
+    uint16_t voltage;
+    voltage = adc12_single_conversion(REFVSEL_1, ADC12SHT0_10, ADC12INCH_11);
+
+    /* Ideally we have A11=0->AVCC=0V ... A11=4095(2^12-1)->AVCC=4V
+     * --> (A11/4095)*4V=AVCC --> AVCC=(A11*4)/4095 */
+    voltage = (voltage * 2 * 2 * 1000) / 4095;  
+    return voltage;
+}
---
+++ chronos/drivers/include/battery.h
@@ -0,0 +1,6 @@
+#ifndef BATTERY_H
+#define BATTERY_H 
+
+uint16_t battery_get_voltate(void);
+
+#endif /* BATTERY_H */
