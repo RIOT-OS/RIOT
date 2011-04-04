c0af1a81893e363c0094dd2c66f3d887bcc1d649
Oliver Hahm <oleg@hobbykeller.org>
2011-04-04 13:28:33 +0200

    [projects WEAtHeR] * added project

---
+++ projects/WEAtHeR/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sht11.h>
+#include <board.h>
+#include <swtimer.h>
+#include <ltc4150.h>
+
+int main(void)
+{
+    sht11_val_t sht11_val;
+    double mAh = 0;
+
+    uint8_t success = 0;
+    
+    puts("");
+    puts("WEAtHeR: Wireless Energy-Aware mulTi-Hop sEnsor Reading.");
+    puts("Printing \"temperature in °C;relative humidity;temperature compensated relative humidity\".");
+    puts("");
+    
+    ltc4150_start();
+
+    while (1) {
+        success = sht11_read_sensor(&sht11_val, HUMIDITY|TEMPERATURE);
+        mAh = ltc4150_get_total_mAh();
+        if (!success) {
+            printf("error;error;error\n");
+        }
+        else {
+            printf("%.2f;%.2f;%.2f;%.2f\n", sht11_val.temperature, sht11_val.relhum, sht11_val.relhum_temp, mAh);
+        }
+        LED_RED_TOGGLE;
+        swtimer_usleep(60 * 1000*1000);
+    }
+}
