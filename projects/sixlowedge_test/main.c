0c291bd7e2cdea7dd839382957a8d6e55faf79f7
mlenders <mlenders@inf.fu-berlin.de>
2011-07-24 17:26:29 +0200

    added test for edge router

---
+++ projects/sixlowedge_test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <transceiver.h>
+#include <vtimer.h>
+#include "sys/net/sixlowpan/sixlowip.h"
+#include "sys/net/sixlowpan/sixlowedge.h"
+#include "sys/net/sixlowpan/sixlowerror.h"
+
+int main(void)
+{        
+    border_initialize(TRANSCEIVER_CC1100, NULL);
+}
