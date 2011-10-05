3a4c88c63725f609f6afdf783ce4dc10c5206ea1
Oliver Hahm <oleg@hobbykeller.org>
2011-10-05 15:28:25 +0200

    [sys shell shell_commands]
    * added missing function prototype

--- sys/shell/shell_commands.c
+++ sys/shell/shell_commands.c
@@ -1,9 +1,13 @@
 #include <shell.h>
 #include <stdlib.h>
 
+#ifdef MODULE_PS
+   extern void _ps_handler(char* unused);
+#endif
+
 const shell_command_t _shell_command_list[] = {
 #ifdef MODULE_PS
-    {"ps", ps_handler},
+    {"ps", _ps_handler},
 #endif
     {NULL, NULL}
 };
