d9017085432f2aa677ba8d6eb4de45e1d1c7982d
Oliver Hahm <oleg@hobbykeller.org>
2012-11-20 21:30:03 +0100

    [board msba2-common tools]
    * fixed lpc2k_pgm

--- msba2-common/tools/src/control_2xxx.c
+++ msba2-common/tools/src/control_2xxx.c
@@ -8,28 +8,30 @@
 
 void hard_reset_to_bootloader(void)
 {
-/*	Use this lines for flashing a node with interrupted DTR line
- *	printf("Press Reset - confirm with anykey\n");
+/*	Use this lines for flashing a node with interrupted DTR line */
+    /*	printf("Press Reset - confirm with anykey\n");
     getchar();
     */
     printf("Reset CPU (into bootloader)\r\n");
-	set_rts(1);		// RTS (ttl level) connects to P0.14
+	set_rts(0);		// RTS (ttl level) connects to P0.14
     /* the next two lines should be commented for the prepared node */
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);
-    printf("Release Reset - confirm with anykey\n");
+    /*	Use this lines for flashing a node with interrupted DTR line */
+    /* printf("Release Reset - confirm with anykey\n");
     getchar();
+    */
 	set_dtr(0);		// allow the CPU to run:
 	set_baud(baud_rate);
-	set_rts(1);		// set RTS again (as it has been reset by set_baudrate) 
+	set_rts(0);		// set RTS again (as it has been reset by set_baudrate) 
 	usleep(40000);
 }
 
 void hard_reset_to_user_code(void)
 {
 	printf("Reset CPU (into user code)\r\n");
-	set_rts(0);		// RTS (ttl level) connects to P0.14
+	set_rts(1);		// RTS (ttl level) connects to P0.14
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);
