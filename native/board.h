81f2a5bc1223172911ebe179527b7b4336a4dbe3
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2013-03-06 12:19:15 +0100

    native board interface and dummy LED implementation

---
+++ native/board.h
@@ -0,0 +1,22 @@
+/**
+ * Native Board interface
+ *
+ * Copyright (C) 2013 Ludwig Ortmann
+ *
+ * This file subject to the terms and conditions of the GNU General Public
+ * License. See the file LICENSE in the top level directory for more details.
+ *
+ * @ingroup native
+ * @{
+ * @file
+ * @author  Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ * @}
+ */
+
+void LED_GREEN_OFF(void);
+void LED_GREEN_ON(void);
+void LED_GREEN_TOGGLE(void);
+void LED_RED_OFF(void);
+void LED_RED_ON(void);
+void LED_RED_TOGGLE(void);
+
