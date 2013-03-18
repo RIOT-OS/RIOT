RIOT-OS/RIOT

commit d9017085432f2aa677ba8d6eb4de45e1d1c7982d

[board msba2-common tools]
* fixed lpc2k_pgm

msba2-common/tools/src/control_2xxx.c
@@ -8,28 +8,30 @@
 
 void hard_reset_to_bootloader(void)
 {
-/*	Use this lines for flashing a node with interrupted DTR line
- *	printf("Press Reset - confirm with anykey\n");
+/*	Use this lines for flashing a node with interrupted DTR line */
+    /*	printf("Press Reset - confirm with anykey\n");
     getchar();
     */
     printf("Reset CPU (into bootloader)\r\n");
-	set_rts(1);		// RTS (ttl level) connects to P0.14
+	set_rts(0);		// RTS (ttl level) connects to P0.14
     /* the next two lines should be commented for the prepared node */
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);
-    printf("Release Reset - confirm with anykey\n");
+    /*	Use this lines for flashing a node with interrupted DTR line */
+    /* printf("Release Reset - confirm with anykey\n");
     getchar();
+    */
 	set_dtr(0);		// allow the CPU to run:
 	set_baud(baud_rate);
-	set_rts(1);		// set RTS again (as it has been reset by set_baudrate) 
+	set_rts(0);		// set RTS again (as it has been reset by set_baudrate) 
 	usleep(40000);
 }
 
 void hard_reset_to_user_code(void)
 {
 	printf("Reset CPU (into user code)\r\n");
-	set_rts(0);		// RTS (ttl level) connects to P0.14
+	set_rts(1);		// RTS (ttl level) connects to P0.14
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);

commit defd7cef13987a19bd2db1e3fbe56138beadcdd8

* fixed lpc2k_pgm (was broken in d9017085432f2aa677ba8d6eb4de45e1d1c7982d)

msba2-common/tools/src/control_2xxx.c
@@ -13,7 +13,7 @@ void hard_reset_to_bootloader(void)
     getchar();
     */
     printf("Reset CPU (into bootloader)\r\n");
-	set_rts(0);		// RTS (ttl level) connects to P0.14
+	set_rts(1);		// RTS (ttl level) connects to P0.14
     /* the next two lines should be commented for the prepared node */
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
@@ -24,14 +24,14 @@ void hard_reset_to_bootloader(void)
     */
 	set_dtr(0);		// allow the CPU to run:
 	set_baud(baud_rate);
-	set_rts(0);		// set RTS again (as it has been reset by set_baudrate) 
+	set_rts(1);		// set RTS again (as it has been reset by set_baudrate) 
 	usleep(40000);
 }
 
 void hard_reset_to_user_code(void)
 {
 	printf("Reset CPU (into user code)\r\n");
-	set_rts(1);		// RTS (ttl level) connects to P0.14
+	et_rts(0);		// RTS (ttl level) connects to P0.14
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);