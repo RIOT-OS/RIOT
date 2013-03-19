RIOT-OS/RIOT

commit 7cef6c46552a988c1c710a0a2404316a27ef1d43

* adapted thread_stack_init() for ARM and msp430 to the new prototype introduced by bd5b46628f

cpu/arm_common/arm_cpu.c
@@ -33,10 +33,10 @@ void thread_yield() {
 // Processor specific routine - here for ARM7
 // sizeof(void*) = sizeof(int)
 //----------------------------------------------------------------------------
-char * thread_stack_init(void * task_func, void * stack_start)
+char * thread_stack_init(void * task_func, void * stack_start, int stack_size)
 {
    unsigned int * stk;
-   stk = (unsigned int *) stack_start;
+   stk = (unsigned int *) stack_start + stack_size;
     stk--;
 
     *stk = 0x77777777;

Makefile.include
@@ -4,8 +4,8 @@ CFLAGS += -DBOARD=$(BB)
 export CFLAGS
 
 # mandatory include! 
-include $(RIOTBASE)/Makefile.modules 
 include $(RIOTBOARD)/$(BOARD)/Makefile.include
+include $(RIOTBASE)/Makefile.modules 
 
 # your binaries to link
 BASELIBS += $(RIOTBOARD)/$(BOARD)/bin/$(BOARD)_base.a
@@ -36,7 +36,7 @@ clean:
 	rm -f $(PROJBINDIR)/*
 
 flash: all
-	$(FLASH) $(PORT) $(PROJBINDIR)/$(PROJECT).hex
+	$(FLASHER) $(PORT) $(PROJBINDIR)/$(PROJECT).hex
 
 term:
 	$(TERM) $(PORT)

cpu/arm_common/Makefile
@@ -3,5 +3,3 @@ INCLUDES = -Iinclude -I../$(CPU)/include -I../../sys/lib -I../../drivers/include
 
 include $(RIOTBASE)/Makefile.base
 
-
-

cpu/msp430-common/cpu.c
@@ -97,10 +97,10 @@ void cpu_switch_context_exit(void){
 //----------------------------------------------------------------------------
 // Processor specific routine - here for MSP
 //----------------------------------------------------------------------------
-char *thread_stack_init(void *task_func, void *stack_start)
+char *thread_stack_init(void *task_func, void *stack_start, int stack_size)
 {
     unsigned short * stk;
-    stk = (unsigned short *) stack_start;
+    stk = (unsigned short *) stack_start + stack_size;
 
     *stk = (unsigned short) sched_task_exit;
     --stk;

commit 1c710666b976216b4bbec62429aad864ed62dc63

fix bug introduced by 7cef6c46552a988c1c710a0a2404316a27ef1d43 stack_start used to be stack_start + stack_size, so re-setting stk to stack_start would set the pointer to the *end* of the stack instead of the beginning.

cpu/arm_common/arm_cpu.c
@@ -45,7 +45,7 @@ char * thread_stack_init(void * task_func, void * stack_start, int stack_size)
     *stk = (unsigned int)sched_task_exit;       // LR
 
    stk--;
-   *stk = (unsigned int) stack_start - 4;   // SP
+   *stk = (unsigned int) stack_start + stack_size - 4;   // SP
 
    for (int i = 12; i>= 0 ; i--) {          // build base stack
        stk--;