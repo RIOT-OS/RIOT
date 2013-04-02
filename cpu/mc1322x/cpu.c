c8a8069ed5e5d70773fdc8212433d1a73b314e2d
Oliver Hahm <oleg@hobbykeller.org>
2013-04-02 02:07:41 +0200

    * mc1322x specifics syscalls

---
+++ cpu/mc1322x/cpu.c
@@ -0,0 +1,19 @@
+/*
+ * cpu.c - MC1322X architecture common support functions
+ * Copyright (C) 2013 Oliver Hahm <oliver.hahm@inria.fr>
+ *
+ * This source code is licensed under the GNU General Public License,
+ * Version 3.  See the file LICENSE for more details.
+ *
+ * This file is part of RIOT.
+ *
+ */
+
+#include "mc1322x.h"
+
+__attribute__((naked,noreturn)) void arm_reset(void)
+{
+    dINT();
+    CRM->SW_RST = SW_RST_VAL;
+    while(1);
+}
