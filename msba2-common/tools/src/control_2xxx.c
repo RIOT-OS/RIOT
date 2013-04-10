RIOT-OS/RIOT

commit defd7cef13987a19bd2db1e3fbe56138beadcdd8

* fixed lpc2k_pgm (was broken in d9017085432f2aa677ba8d6eb4de45e1d1c7982d)

msba2-common/tools/src/control_2xxx.c
@@ -13,7 +13,7 @@ void hard_reset_to_bootloader(void)
     getchar();
     */
     printf("Reset CPU (into bootloader)\r\n");
-	set_rts(0);		// RTS (ttl level) connects to P0.14
+	set_rts(1);		// RTS (ttl level) connects to P0.14
     /* the next two lines should be commented for the prepared node */
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
@@ -24,14 +24,14 @@ void hard_reset_to_bootloader(void)
     */
 	set_dtr(0);		// allow the CPU to run:
 	set_baud(baud_rate);
-	set_rts(0);		// set RTS again (as it has been reset by set_baudrate) 
+	set_rts(1);		// set RTS again (as it has been reset by set_baudrate) 
 	usleep(40000);
 }
 
 void hard_reset_to_user_code(void)
 {
 	printf("Reset CPU (into user code)\r\n");
-	set_rts(1);		// RTS (ttl level) connects to P0.14
+	et_rts(0);		// RTS (ttl level) connects to P0.14
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);

commit 9f27cf54bc7e53e0849ac25c74420b24063903a6

* fixed the broken bugfix from defd7cef13987a19bd2db1e3fbe56138beadcdd8

msba2-common/tools/src/control_2xxx.c
@@ -31,7 +31,7 @@ void hard_reset_to_bootloader(void)
 void hard_reset_to_user_code(void)
 {
 	printf("Reset CPU (into user code)\r\n");
-	et_rts(0);		// RTS (ttl level) connects to P0.14
+	set_rts(0);		// RTS (ttl level) connects to P0.14
 	set_dtr(1);		// DTR (ttl level) connects to RST
 	send_break_signal();	// or break detect circuit to RST
 	usleep(75000);