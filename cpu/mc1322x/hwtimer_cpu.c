f20673ad9e71e7eff93e4451c57a8308cc21acd3
Thomas Eichinger <thomas.eichinger1@gmail.com>
2013-04-25 13:30:09 +0200

    * changed httimer_arch_init() to init all 4 timers and added implementation for hwtimer_arch_set

--- cpu/mc1322x/hwtimer_cpu.c
+++ cpu/mc1322x/hwtimer_cpu.c
@@ -1,6 +1,7 @@
 /*
  * hwtimer_cpu.c - architecture dependent hardware timer functionality
  * Copyright (C) 2013 Oliver Hahm <oliver.hahm@inria.fr>
+ * Copyright (C) 2013 Thomas Eichinger <thomas.eichinger@fu-berlin.de>
  *
  * This source code is licensed under the GNU General Public License,
  * Version 3.  See the file LICENSE for more details.
@@ -10,42 +11,94 @@
 
 #include <stdint.h>
 #include "mc1322x.h"
+#include "hwtimer_arch.h"
+#include "irq.h"
 
-/* TODO: do scaling voodoo */
-#define COUNT_MODE 1      /* use rising edge of primary source */
-#define PRIME_SRC  0xf    /* Perip. clock with 128 prescale (for 24Mhz = 187500Hz)*/
-#define SEC_SRC    0      /* don't need this */
-#define ONCE       0      /* keep counting */
-#define LEN        0      /* continue counting */
-#define DIR        0      /* count up */
-#define CO_INIT    0      /* other counters cannot force a re-initialization of this counter */
-#define OUT_MODE   0      /* OFLAG is asserted while counter is active */
+#define VULP(x) ((volatile unsigned long*) (x))
 
 /* High level interrupt handler */
 static void (*int_handler)(int);
 
-void hwtimer_arch_init(void (*handler)(int), uint32_t fcpu) {
-    int_handler = handler;
+void timer_x_init(TMR_struct* TMRx) {
     /* Reset the timer */
-    TMR0->ENBL = 0;
+    TMRx->ENBL = 0;
     /* Clear status */
-    TMR0->SCTRL = 0;
+    TMRx->SCTRL = 0;
     /* disable interrupt */
-    TMR0->CSCTRL =0x0000;
+    TMRx->CSCTRL =0x0000;
     /* Reload/initialize to zero */
-    TMR0->LOAD = 0;
+    TMRx->LOAD = 0;
 
     /* disable comparison */
-    TMR0->COMP1 = 0;
-    TMR0->CMPLD1 = 0;
+    TMRx->COMP1 = 0;
+    TMRx->CMPLD1 = 0;
     
     /* set counter to zero */
-    TMR0->CNTR = 0;
+    TMRx->CNTR = 0;
+    
+    /* set timer control bits */
+    TMRx->CTRLbits.COUNT_MODE = 1;              /* use rising edge of primary source */
+    TMRx->CTRLbits.PRIMARY_CNT_SOURCE = 0x0f;   /* Perip. clock with 128 prescale (for 24MHz = 187500Hz) */
+    TMRx->CTRLbits.SECONDARY_CNT_SOURCE = 0x00; /* don't need this */
+    TMRx->CTRLbits.ONCE = 0x00;                 /* keep counting */
+    TMRx->CTRLbits.LENGTH = 0x00;               /* continue counting */
+    TMRx->CTRLbits.DIR = 0x00;                  /* count up */
+    TMRx->CTRLbits.CO_INIT = 0x00;              /* other counters cannot force a reinitialization of this counter*/
+    TMRx->CTRLbits.OUTPUT_MODE = 0x00;          /* OFLAG is asserted while counter is active */
+    
+    TMRx->ENBL = 0xf;                           /* enable all the timers --- why not? */
+};
 
+void hwtimer_arch_init(void (*handler)(int), uint32_t fcpu) {
+    int_handler = handler;
+    
     /* TODO: do scaling voodoo */
     (void) fcpu;
-    /* TODO: use struct */
-    TMR0->CTRL = (COUNT_MODE<<13) | (PRIME_SRC<<9) | (SEC_SRC<<7) | (ONCE<<6) | (LEN<<5) | (DIR<<4) | (CO_INIT<<3) | (OUT_MODE);
-    TMR0->ENBL = 0xf;                   /* enable all the timers --- why not? */
+    
+    timer_x_init(TMR0);
+    timer_x_init(TMR1);
+    timer_x_init(TMR2);
+    timer_x_init(TMR3);
+};
 
+/*---------------------------------------------------------------------------*/
+void hwtimer_arch_enable_interrupt(void) {
+    
+}
+
+/*---------------------------------------------------------------------------*/
+void hwtimer_arch_disable_interrupt(void) {
+    
+}
+
+/*---------------------------------------------------------------------------*/
+void hwtimer_arch_set(unsigned long offset, short timer) {
+    /* get corresponding struct for the given ::timer parameter */
+    TMR_struct* tmr = (void *) TMR_BASE + (timer + TMR_OFFSET);
+    
+    /* disable IRQs and save the status register */
+    unsigned long cpsr = disableIRQ();
+    
+    tmr->COMP1 = tmr->CNTR + offset;            /* load the current value + offset into the compare register */
+    tmr->CSCTRLbits.TCF1 = 0;                   /* reset compare flag */
+    tmr->CSCTRLbits.TCF1EN = 1;                 /* enable intterupts when TCF1 is set \ */
+    tmr->SCTRLbits.TCFIE = 1;                   /* enable interrupts when TCF is one  - do we need both?*/ 
+    
+    /* restor status register */
+    restoreIRQ(cpsr);
+}
+
+/*---------------------------------------------------------------------------*/
+void hwtimer_arch_set_absolute(unsigned long value, short timer) {
+    
+}
+
+/*---------------------------------------------------------------------------*/
+void hwtimer_arch_unset(short timer) {
+    
+}
+
+/*---------------------------------------------------------------------------*/
+unsigned long hwtimer_arch_now(void) {
+    
 }
