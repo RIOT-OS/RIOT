12720200c758d216cec473da7233c164ab90f8e9
Martin <martin.landsmann@haw-hamburg.de>
2013-06-11 11:50:11 +0200

    fix typo transceiver.h

--- cpu/native/include/cpu-conf.h
+++ cpu/native/include/cpu-conf.h
@@ -3,9 +3,8 @@
  *
  * Copyright (C) 2013 Ludwig Ortmann
  *
- * This file subject to the terms and conditions of the GNU Lesser General
- * Public License. See the file LICENSE in the top level directory for more
- * details.
+ * This file subject to the terms and conditions of the GNU General Public
+ * License. See the file LICENSE in the top level directory for more details.
  *
  * @ingroup arch
  * @{
@@ -28,7 +27,7 @@
 #define KERNEL_CONF_STACKSIZE_DEFAULT   (16384)
 #define KERNEL_CONF_STACKSIZE_IDLE      (16384)
 #define NATIVE_ISR_STACKSIZE            (16384)
-/* undefine the TRANSCEIVER_STACK_SIZE (2048 or 512) defined in transceiver.h */
+// undefine the TRANSCEIVER_STACK_SIZE (2048 or 512) defined in transceiver.h
 #ifdef TRANSCEIVER_STACK_SIZE
 #undef TRANSCEIVER_STACK_SIZE
 #endif
