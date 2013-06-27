3ea68ecfc1bb68de17f9dc34099ffbb1791af95c
Zakaria_Kasmi <zkasmi@inf.fu-berlin.de>
2013-06-27 16:16:57 +0200

    add a handler for the heap state

--- sys/shell/commands/shell_commands.c
+++ sys/shell/commands/shell_commands.c
@@ -3,22 +3,25 @@
  *
  * Copyright (C) 2013  INRIA.
  *
- * This file subject to the terms and conditions of the GNU Lesser General
- * Public License. See the file LICENSE in the top level directory for more
- * details.
+ * The source code is licensed under the LGPLv2 license, 
+ * see the file LICENSE for more details.
  *
  * @ingroup shell_commands
  * @{
  * @file    shell_commands.c
  * @brief   sets up the system shell command struct
  * @author  Oliver Hahm <oliver.hahm@inria.fr>
+ * @author  Zakaria Kasmi <zkasmi@inf.fu-berlin.de>
  * @}
  */
 
 #include <shell_commands.h>
 #include <stdlib.h>
 
-extern void _id_handler(char *id);
+
+
+extern void _id_handler(char* id);
+extern void _heap_handler(char* unused);
 
 #ifdef MODULE_PS
 extern void _ps_handler(char *unused);
