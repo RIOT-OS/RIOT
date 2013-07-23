d706317ee2c7c3e33b2327abcd5f800feb092467
Thomas Eichinger <thomas.eichinger1@gmail.com>
2013-07-23 10:36:16 +0200

    * added proper "struct TMR_struct*" in hwtimer_cpu.c * removed dublicate definition of the UART structure in mc1322x.h

--- cpu/mc1322x/include/mc1322x.h
+++ cpu/mc1322x/include/mc1322x.h
@@ -303,87 +303,6 @@ static volatile struct TMR_struct * const TMR3 = (void *) (TMR3_BASE);
 #define TMR_NUM(x) (((uint32_t)(x) - TMR_BASE) / TMR_OFFSET)
 
 /*-----------------------------------------------------------------*/
-/* UART */
-#define UART1_BASE      (0x80005000)
-#define UART2_BASE      (0x8000B000)
-
-struct UART_struct {
-	union {
-		uint32_t CON;
-		struct UART_CON {
-           		uint32_t :16;
-			uint32_t TST:1;
-			uint32_t MRXR:1;
-			uint32_t MTXR:1;
-			uint32_t FCE:1;
-			uint32_t FCP:1;
-			uint32_t XTIM:1;
-			uint32_t :2;
-			uint32_t TXOENB:1;
-			uint32_t CONTX:1;
-			uint32_t SB:1;
-			uint32_t ST2:1;
-			uint32_t EP:1;
-			uint32_t PEN:1;
-			uint32_t RXE:1;
-			uint32_t TXE:1;
-		} CONbits;
-	};
-	union {
-		uint32_t STAT;
-		struct UART_STAT {
-           		uint32_t :24;
-			uint32_t TXRDY:1;
-			uint32_t RXRDY:1;
-			uint32_t RUE:1;
-			uint32_t ROE:1;
-			uint32_t TOE:1;
-			uint32_t FE:1;
-			uint32_t PE:1;
-			uint32_t SE:1;
-		} USTATbits;
-	};
-	union {
-		uint32_t DATA;
-		struct UART_DATA {
-           		uint32_t :24;
-			uint32_t DATA:8;
-		} DATAbits;
-	};
-	union {
-		uint32_t RXCON;
-		struct UART_URXCON {
-                        uint32_t :26;
-                        uint32_t LVL:6;
-		} RXCONbits;
-	};
-	union {
-		uint32_t TXCON;
-		struct UART_TXCON {
-                        uint32_t :26;
-			uint32_t LVL:6;
-		} TXCONbits;
-	};
-	union {
-		uint32_t CTS;
-		struct UART_CTS {
-           		uint32_t :27;
-			uint32_t LVL:5;
-		} CTSbits;
-	};
-	union {
-		uint32_t BR;
-		struct UART_BR {
-           		uint32_t INC:16;
-			uint32_t MOD:16;
-		} BRbits;
-	};
-};
-
-static volatile struct UART_struct * const UART1 = (void *) (UART1_BASE);
-static volatile struct UART_struct * const UART2 = (void *) (UART2_BASE);
-
-/*-----------------------------------------------------------------*/
 /* Interrupts */
 #define INTBASE        (0x80020000)
 
@@ -472,8 +391,8 @@ enum interrupt_nums {
 #define global_irq_disable() (set_bit(*INTCNTL,20))
 #define global_irq_enable()  (clear_bit(*INTCNTL,20))
 
-#define enable_irq(irq)  (*INTENNUM  = INT_NUM_##irq)
-#define disable_irq(irq) (*INTDISNUM = INT_NUM_##irq)
+#define enable_irq(irq)  (*INTENNUM  = irq)
+#define disable_irq(irq) (*INTDISNUM = irq)
 
 #define safe_irq_disable(x)  volatile uint32_t saved_irq; saved_irq = *INTENABLE; disable_irq(x)
 #define irq_restore() *INTENABLE = saved_irq
