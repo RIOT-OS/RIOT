8ed2cda9b119d546adf6248dd434a6063eafce63
Martin <martin.landsmann@haw-hamburg.de>
2013-08-12 17:44:36 +0200

    fix missing typecast at assignment of stack_start

--- cpu/native/native_cpu.c
+++ cpu/native/native_cpu.c
@@ -72,7 +72,7 @@ char *thread_stack_init(void (*task_func)(void), void *stack_start, int stacksiz
 
     DEBUG("thread_stack_init()\n");
 
-    stk = stack_start;
+    stk = (unsigned int *)stack_start;
 
 #ifdef NATIVESPONTOP
     p = (ucontext_t *)stk;
