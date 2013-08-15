f0982dad3eb5b021d939d69d681b768e3a0e0dd4
authmillenon <mlenders@inf.fu-berlin.de>
2013-08-15 09:47:32 +0200

    Document NDP API header

--- sys/net/sixlowpan/include/sixlowpan/ndp.h
+++ sys/net/sixlowpan/include/sixlowpan/ndp.h
@@ -61,39 +61,59 @@ typedef enum __attribute__((packed)) {
     NDP_NCE_TYPE_TENTATIVE      ///< Tentetive.
 } ndp_nce_type_t;
 
+/**
+ * @brief   Prefix list type to store information spread by prefix
+ *          information option.
+ */
 typedef struct __attribute__((packed)) {
-    uint8_t inuse;
+    uint8_t inuse;          ///< Prefix is in in use.
     uint8_t adv;
-    ipv6_addr_t addr;
-    uint8_t length;
-    uint8_t l_a_reserved1;
-    uint32_t val_ltime;
-    uint32_t pref_ltime;
-    uint8_t infinite;
+    ipv6_addr_t addr;       ///< The Prefix.
+    uint8_t length;         ///< Length of the prefix.
+    uint8_t l_a_reserved1;  ///< L and A flag of prefix information option
+    uint32_t val_ltime;     ///< valid lifetime
+    uint32_t pref_ltime;    ///< preferred lifetime
+    uint8_t infinite;       ///< flag to set to infinite lifetime
 } ndp_prefix_list_t;
 
-/* default router list - rfc4861 5.1. */
+/**
+ * @brief   Default router list to store information spread by
+ *          router advertisement.
+ */
 typedef struct __attribute__((packed)) {
-    ipv6_addr_t addr;
-    timex_t inval_time;
+    ipv6_addr_t addr;       ///< Address of router.
+    timex_t inval_time;     ///< remaining time until this entry is
+                            ///< invalid.
 } ndp_default_router_list_t;
 
-/* neighbor cache - rfc4861 5.1. */
+/**
+ * @brief   Neighbor cache as defined in
+ *          <a href="http://tools.ietf.org/html/rfc4861#section-3.5#section-5.1">
+ *              RFC 4861, section 5.1
+ *          </a>.
+ */
 typedef struct __attribute__((packed)) {
-    ndp_nce_type_t type;
-    ndp_nce_state_t state;
-    uint8_t isrouter;
-    ipv6_addr_t addr;
-    ieee_802154_long_t laddr;
-    ieee_802154_short_t saddr;
-    timex_t ltime;
+    ndp_nce_type_t type;        ///< Type of neighbor cache entry.
+    ndp_nce_state_t state;      ///< State of neighbor cache entry.
+    uint8_t isrouter;           ///< Flag to signify that this neighbor
+                                ///< is a router.
+    ipv6_addr_t addr;           ///< IPv6 address of the neighbor.
+    ieee_802154_long_t laddr;   ///< EUI-64 of neighbor
+    ieee_802154_short_t saddr;  ///< IEEE 802.15.4 16-bit short address
+                                ///< of neighbor.
+    timex_t ltime;              ///< lifetime of entry.
 } ndp_neighbor_cache_t;
 
-/* authoritive border router cache - rfc6775 */
+/**
+ * @brief   Authoritive border router cache as defined in
+ *          <a href="http://tools.ietf.org/html/rfc6775">
+ *              RFC 6775
+ *          </a>.
+ */
 typedef struct __attribute__((packed)) {
-    uint16_t version;
-    ipv6_addr_t abr_addr;
-    uint8_t cids[NDP_6LOWPAN_CONTEXT_MAX];
+    uint16_t version;                       ///< version of entry.
+    ipv6_addr_t abr_addr;                   ///< Addres of ABR.
+    uint8_t cids[NDP_6LOWPAN_CONTEXT_MAX];  ///< context IDs.
 } ndp_a6br_cache_t;
 
 ndp_default_router_list_t *ndp_default_router_list_search(ipv6_addr_t *ipaddr);
