5f537c38ac81c54f80f515c9a5c420e4a9d9cfa6
Oleg Hahm <oleg@hobbykeller.org>
2013-09-12 23:13:54 +0200

    changed to _types.h definition for ssize_t

--- cpu/msp430-common/include/msp430_types.h
+++ cpu/msp430-common/include/msp430_types.h
@@ -1,7 +1,9 @@
 #ifndef MSP430_TYPES_H
 #define MSP430_TYPES_H
 
+#include <sys/_types.h>
+
 /** defining signed type for size_t */
-typedef int ssize_t;
+typedef _ssize_t ssize_t;
 
 #endif /* MSP430_TYPES_H */
