b15ab29be92e2c4d0dfd304ef58282d50a0f2c79
Martin <martin.landsmann@haw-hamburg.de>
2013-09-16 08:37:14 +0200

    fix multiple definitions of trnsceiver_pid

--- drivers/cc110x_ng/include/cc110x_ng.h
+++ drivers/cc110x_ng/include/cc110x_ng.h
@@ -110,7 +110,7 @@ extern volatile uint8_t rx_buffer_next;	    ///< Next packet in RX queue
 extern volatile uint8_t radio_state;		///< Radio state
 extern cc110x_statistic_t cc110x_statistic;
 
-int transceiver_pid;                         ///< the transceiver thread pid
+extern int transceiver_pid;                         ///< the transceiver thread pid
 
 void cc110x_init(int transceiver_pid);
 
