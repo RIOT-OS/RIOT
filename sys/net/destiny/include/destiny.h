a6b5e72d547ca5fbf43f3edb28f9f0d375fc3c4a
Martin Lenders <mail@martin-lenders.de>
2013-09-20 14:29:00 +0200

    Refactor destiny.h

---
+++ sys/net/destiny/include/destiny.h
@@ -0,0 +1,13 @@
+/*
+ * destiny.h
+ *
+ *  Created on: 03.09.2011
+ *      Author: Oliver
+ */
+
+#ifndef DESTINY_H
+#define DESTINY_H
+
+int destiny_init_transport_layer(void);
+
+#endif /* DESTINY_H */
