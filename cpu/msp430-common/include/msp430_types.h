7fb1ac44020e373ab72021d30bc61527c71998c7
Christian Mehlis <mehlis@inf.fu-berlin.de>
2013-10-18 13:53:03 +0200

    msp430: add time_t and struct timespec

--- cpu/msp430-common/include/msp430_types.h
+++ cpu/msp430-common/include/msp430_types.h
@@ -6,4 +6,11 @@
 /** defining signed type for size_t */
 typedef _ssize_t ssize_t;
 
+typedef unsigned long time_t;
+
+struct timespec {
+    time_t  tv_sec;   /* Seconds */
+    long  tv_nsec;    /* Nanoseconds */
+};
+
 #endif /* MSP430_TYPES_H */
