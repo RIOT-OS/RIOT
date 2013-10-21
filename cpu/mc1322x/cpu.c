a1cd29d68e3efaab56966bf61e50a4d7a4ba5370
Thomas Eichinger <thomas.eichinger1@gmail.com>
2013-10-21 14:12:29 +0200

    added basic install_irq function

--- cpu/mc1322x/cpu.c
+++ cpu/mc1322x/cpu.c
@@ -22,3 +22,35 @@ __attribute__((naked,noreturn)) void arm_reset(void)
 enum lpm_mode lpm_set(enum lpm_mode target) {
     return LPM_ON;
 }
+
+/******************************************************************************
+** Function name:		cpu_install_irq
+**
+** Descriptions:		Install interrupt handler
+** parameters:			Interrupt number, interrupt handler address,
+**						interrupt priority
+** Returned value:		true or false, return false if IntNum is out of range
+**
+******************************************************************************/
+#define VIC_BASE_ADDR   0xFFFFF000
+
+bool cpu_install_irq(int IntNumber, void *HandlerAddr, int Priority)
+{
+    int *vect_addr;
+    int *vect_cntl;
+
+    VICIntEnClr = 1 << IntNumber;	/* Disable Interrupt */
+
+    if (IntNumber >= VIC_SIZE) {
+        return (false);
+    }
+    else {
+        /* find first un-assigned VIC address for the handler */
+        vect_addr = (int *)(VIC_BASE_ADDR + VECT_ADDR_INDEX + IntNumber * 4);
+        vect_cntl = (int *)(VIC_BASE_ADDR + VECT_CNTL_INDEX + IntNumber * 4);
+        *vect_addr = (int)HandlerAddr;	/* set interrupt vector */
+        *vect_cntl = Priority;
+        VICIntEnable = 1 << IntNumber;	/* Enable Interrupt */
+        return(true);
+    }
+}
