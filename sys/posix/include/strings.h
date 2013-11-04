9f9a48db9f9ccf0a3faf2d881ec18194179054dc
Martin Lenders <mail@martin-lenders.de>
2013-11-04 11:25:27 +0100

    Minor documentation fixes

--- sys/posix/include/strings.h
+++ sys/posix/include/strings.h
@@ -6,10 +6,13 @@
  * details.
  */
 
-/** 
+/**
  * @addtogroup posix
  * @{
- * @file
+ */
+
+/**
+ * @file    strings.h
  * @brief   string operations
  *
  * @see     <a href="http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/strings.h.html">
@@ -26,7 +29,7 @@
 
 /**
  * @brief   Returns the position of the first (least significant) bit set in
- *          integer i, or 0 if no bits are set in i.
+ *          integer *i*, or 0 if no bits are set in i.
  *
  * @param[in] i    an integer number.
  *
@@ -40,15 +43,15 @@
 #define ffs(i) __builtin_ffs(i)
 
 /**
- * @brief   Compares two strings s1 and s2, ignoring the case of the characters
- *          for not more than n bytes from the strings.
+ * @brief   Compares two strings *s1* and *s2*, ignoring the case of the
+ *          characters for not more than n bytes from the strings.
  *
  * @param[in] s1    a string.
  * @param[in] s2    another string.
  * @param[in] n     number of bytes to be compared
  *
- * @return  A value greater 0 if, ignoring the case of the character, s1 is
- *          greater than s2 up to n bytes, less than 0 if smaller, and 0 if
+ * @return  A value greater 0 if, ignoring the case of the character, *s1* is
+ *          greater than *s2* up to n bytes, less than 0 if smaller, and 0 if
  *          equal
  *
  * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/functions/strcasecmp.html">
@@ -58,9 +61,9 @@
 int strncasecmp(const char *s1, const char *s2, size_t n);
 
 /**
- * @brief   Compares two strings s1 and s2, ignoring the case of the characters
- *          for not more than n bytes from the strings, but using the current
- *          locale settings to determine the case.
+ * @brief   Compares two strings *s1* and *s2*, ignoring the case of the
+ *          characters for not more than n bytes from the strings, but using
+ *          the current locale settings to determine the case.
  *
  * @note    Since RIOT does not support locales (yet), this is just an
  *          alias for strncasecmp()
@@ -69,8 +72,8 @@ int strncasecmp(const char *s1, const char *s2, size_t n);
  * @param[in] s2    another string.
  * @param[in] n     number of bytes to be compared
  *
- * @return  A value greater 0 if, ignoring the case of the character, s1 is
- *          greater than s2 up to n bytes, less than 0 if smaller, and 0 if
+ * @return  A value greater 0 if, ignoring the case of the character, *s1* is
+ *          greater than *s2* up to n bytes, less than 0 if smaller, and 0 if
  *          equal
  *
  * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/functions/strcasecmp.html">
@@ -80,13 +83,13 @@ int strncasecmp(const char *s1, const char *s2, size_t n);
 #define strncasecmp_l(s1, s2, n, l) (strncasecmp(s1, s2, n))
 
 /**
- * @brief   Compares two strings s1 and s2, ignoring the case of the characters.
+ * @brief   Compares two strings *s1* and *s2*, ignoring the case of the characters.
  *
  * @param[in] s1    a string.
  * @param[in] s2    another string.
  *
- * @return  A value greater 0 if, ignoring the case of the character, s1 is
- *          greater than s2, less than 0 if smaller, and 0 if equal
+ * @return  A value greater 0 if, ignoring the case of the character, *s1* is
+ *          greater than *s2*, less than 0 if smaller, and 0 if equal
  *
  * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/functions/strcasecmp.html">
  *          The Open Group Base Specification Issue 7, strcasecmp
@@ -95,8 +98,9 @@ int strncasecmp(const char *s1, const char *s2, size_t n);
 #define strcasecmp(s1, s2) strncasecmp(s1, s2, -1)
 
 /**
- * @brief   Compares two strings s1 and s2, ignoring the case of the characters,
- *          but using the current locale settings to determine the case.
+ * @brief   Compares two strings *s1* and *s2*, ignoring the case of the
+ *          characters, but using the current locale settings to determine the
+ *          case.
  *
  * @note    Since RIOT does not support locales (yet), this is just an
  *          alias for strcasecmp()
@@ -104,8 +108,8 @@ int strncasecmp(const char *s1, const char *s2, size_t n);
  * @param[in] s1    a string.
  * @param[in] s2    another string.
  *
- * @return  A value greater 0 if, ignoring the case of the character, s1 is
- *          greater than s2, less than 0 if smaller, and 0 if equal
+ * @return  A value greater 0 if, ignoring the case of the character, *s1* is
+ *          greater than *s2*, less than 0 if smaller, and 0 if equal
  *
  * @see <a href="http://pubs.opengroup.org/onlinepubs/9699919799/functions/strcasecmp.html">
  *          The Open Group Base Specification Issue 7, strcasecmp
