RIOT-OS/RIOT

commit 45ffdeaf52cb5b11524bc15f5f8f264244eaad70

add missing stdlib.h include

cpu/native/native_cpu.c
@@ -16,6 +16,7 @@
  * @author  Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
  */
 #include <stdio.h>
+#include <stdlib.h>
 #ifdef __MACH__
 #define _XOPEN_SOURCE
 #endif

commit 2d7c3288709160e2bd47273766a24e36424424f0

fix native stdlib.h include
Fixes segfault in OSX introduced by 45ffdea

cpu/native/native_cpu.c
@@ -16,7 +16,7 @@
  * @author  Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
  */
 #include <stdio.h>
-#include <stdlib.h>
+
 #ifdef __MACH__
 #define _XOPEN_SOURCE
 #endif
@@ -36,6 +36,8 @@
 #define VALGRIND_STACK_REGISTER(...)
 #define VALGRIND_DEBUG(...)
 #endif
+
+#include <stdlib.h>
 
 #include "kernel_internal.h"
 #include "sched.h"