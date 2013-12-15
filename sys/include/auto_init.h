8d4520242193e1d0d477c547c30c64da8f4f482b
Oleg Hahm <oleg@hobbykeller.org>
2013-12-15 11:48:44 +0100

    add documentation for auto_init (fixes #43)

--- sys/include/auto_init.h
+++ sys/include/auto_init.h
@@ -2,7 +2,18 @@
 /**
  * @defgroup    sys_autoinit Auto-init
  * @ingroup     sys
- * @brief       Autoconfigure libraries
+ * @brief       Auto initialize modules
+ * @note        This feature can be used by any project by adding auto_init to
+ *              USEMODULE in the project's Makefile. auto_init will initialize
+ *              any other included module that does not require a parameter in
+ *              its init function, i.e. if the prototype looks like this: void
+ *              MODULE_init(void). Most timer modules or simple drivers can be
+ *              initialized by auto_init. The modules will be initialized in
+ *              the context of the main thread right before the main function
+ *              gets called. Be aware that most modules expect to be
+ *              initialized only once, so do not call a module's init function
+ *              when using auto_init unless you know what you're doing.
+ *
  * @{
  *
  * @file        auto_init.h
