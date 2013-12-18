3d67ad3a8aae85a41cfb18aadb430b8d088ed19b
Christian Mehlis <mehlis@inf.fu-berlin.de>
2013-12-18 20:47:33 +0100

    add vtimer_get_localtime

--- sys/vtimer/vtimer.c
+++ sys/vtimer/vtimer.c
@@ -259,6 +259,18 @@ void vtimer_now(timex_t *out)
     memcpy(out, &t, sizeof(timex_t));
 }
 
+void vtimer_get_localtime(struct tm *localt)
+{
+    timex_t now;
+    vtimer_now(&now);
+
+    localt->tm_sec = now.seconds % 60;
+    localt->tm_min = (now.seconds / 60) % 60;
+    localt->tm_hour = (now.seconds / 60 / 60) % 24;
+
+    // TODO: fill the other fields
+}
+
 int vtimer_init()
 {
     DEBUG("vtimer_init().\n");
