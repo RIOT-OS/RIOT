8519dcceed01971584a231096445af5b444eda36
Christian Mehlis <mehlis@inf.fu-berlin.de>
2014-01-31 20:18:00 +0100

    added @author fields
    authors found in:
    https://github.com/RIOT-OS/RIOT/commits/master/core/cib.c
    http://ukleos.org/projects/ukleos/repository/revisions/master/changes/core/cib.c

--- core/cib.c
+++ core/cib.c
@@ -13,6 +13,9 @@
  * @file        cib.c
  * @brief       Circular integer buffer implementation
  *
+ * @author      Freie Universität Berlin, Computer Systems & Telematics, FeuerWhere project
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
  * @}
  */
 
