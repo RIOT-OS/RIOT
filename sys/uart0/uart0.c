52bac9f48e493ab87dde622bae62fc037e1de468
Christian Mehlis <mehlis@inf.fu-berlin.de>
2014-02-03 23:19:11 +0100

    added LGPL header and doxygen headers

--- sys/uart0/uart0.c
+++ sys/uart0/uart0.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2013 INRIA
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup sys
+ * @{
+ *
+ * @file
+ * @brief UART implementation
+ *
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * @}
+ */
+
 #include <stdio.h>
 
 #include "cpu-conf.h"
