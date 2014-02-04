e84652ad2307a7c8325244fdced8b4842ac4f7a6
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-02-04 18:52:53 +0100

    Fixed spelling

--- drivers/include/periph/pwm.h
+++ drivers/include/periph/pwm.h
@@ -7,11 +7,11 @@
 
 /**
  * @ingroup     driver_periph
- * @brief       Low-level PWM periphial driver
+ * @brief       Low-level PWM peripheral driver
  * @{
  *
  * @file        pwm.h
- * @brief       Low-level PWM periphial driver interface definitions
+ * @brief       Low-level PWM peripheral driver interface definitions
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  */
@@ -37,7 +37,7 @@ typedef enum {
 #endif
 
 /**
- * @brief Definition of availabe PWM modes
+ * @brief Definition of available PWM modes
  */
 typedef enum {
     PWM_LEFT,           ///< use left aligned PWM
@@ -55,14 +55,14 @@ typedef enum {
  * 
  * The desired frequency and resolution may not be possible on a given device when chosen
  * to large. In this case the PWM driver will always keep the resolution and decrease the
- * frequency if needed. To veryfy the correct settings compare the returned value which
+ * frequency if needed. To verify the correct settings compare the returned value which
  * is the actually set frequency.
  * 
  * @param dev           PWM channel to initialize
  * @param mode          PWM mode, left, right or center aligned
  * @param frequency     the PWM frequency in Hz
  * @param resolution    the PWM resolution
- * @return              the acutally set frequency, 
+ * @return              the actually set frequency, 
  *                      0 if frequency and resolution settings are not applicable,
  *                      -1 on error
  */
@@ -77,7 +77,7 @@ int pwm_init(pwm_t dev, pwm_mode_t mode, unsigned int frequency, unsigned int re
  * @param dev           the PWM device to set
  * @param channel       the channel of the given device to set
  * @param value         the desired duty-cycle to set
- * @return              the acutally set duty-cycle, -1 on invalic device or channel
+ * @return              the actually set duty-cycle, -1 on invalid device or channel
  */
 int pwm_set(pwm_t dev, int channel, unsigned int value);
 
@@ -89,7 +89,7 @@ int pwm_set(pwm_t dev, int channel, unsigned int value);
 void pwm_start(pwm_t dev);
 
 /**
- * @brief Stop PWM generation on the given deivce
+ * @brief Stop PWM generation on the given device
  * 
  * @param dev           device to stop
  */
