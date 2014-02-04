3c2186bb205a4adbd70ebfd545319126da894892
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-02-04 15:40:10 +0100

    Initial import of the stm32f303vc cpu
    - includes Makefiles
    - include cpu config files
    --> Most implementations are still missing for license problems

---
+++ cpu/stm32f303vc/include/cpu.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @defgroup    cpu_stm32f303vg STM32F303VG
+ * @ingroup     cpu
+ * @brief       CPU specific files for the STM32F303VG MCU
+ * @{
+ *
+ * @file        cpu.h
+ * @brief       CPU spedific definitions for the STM32F303VG MCU
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef __CPU_H
+#define __CPU_H
+
+void cpu_init(void);
+
+#endif /* __CPU_H */
+/** @} */
+
