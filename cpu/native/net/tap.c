6a29d18324ac21ea8d5b35821e2d11ba1082e81d
Martin <Martin.Landsmann@HAW-Hamburg.de>
2014-02-10 08:29:22 +0100

    changed assignment to const char*

--- cpu/native/net/tap.c
+++ cpu/native/net/tap.c
@@ -234,7 +234,7 @@ int tap_init(char *name)
     strncpy(clonedev+5, name, 250);
 #else /* Linux */
     struct ifreq ifr;
-    char clonedev[] = "/dev/net/tun";
+    const char *clonedev = "/dev/net/tun";
 #endif
 
     /* implicitly create the tap interface */
