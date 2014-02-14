b69aec47d4fb974ba0162f358825c02cb29c622a
Kévin Roussel <Kevin.Roussel@inria.fr>
2014-02-14 12:13:10 +0100

    Added while(1) to ensure GCC valids NORETURN; fixed indentation

--- cpu/msp430-common/cpu.c
+++ cpu/msp430-common/cpu.c
@@ -92,10 +92,10 @@ int inISR()
 /* System reboot */
 NORETURN void reboot(void)
 {
-  /* force an hardware reboot ("Power-Up Clear"), by writing
-     an illegal value to the watchdog control register */
-  while (1) {
-    WDTCTL = 0x0000;
-  }
+    /* force an hardware reboot ("Power-Up Clear"), by writing
+       an illegal value to the watchdog control register */
+    while (1) {
+        WDTCTL = 0x0000;
+    }
 }
 
