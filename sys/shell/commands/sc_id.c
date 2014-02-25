8e3993a2905fd0e4d43c6fc051aed7e91cedc2d8
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-02-25 10:07:41 +0100

    build id shell command only for config module
    closes #777

--- sys/shell/commands/sc_id.c
+++ sys/shell/commands/sc_id.c
@@ -1,7 +1,7 @@
 /**
  * Shell commands for configuring the node id
  *
- * Copyright (C) 2013  INRIA.
+ * Copyright (C) 2014  INRIA.
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License. See the file LICENSE in the top level directory for more
@@ -27,19 +27,14 @@ void _id_handler(char *id)
     newid = atoi(id + 3);
 
     if (strlen(id) < 3) {
-#ifdef MODULE_CONFIG
         printf("Current id: %u\n", sysconfig.id);
-#endif
     }
     else {
         printf("Setting new id %lu\n", newid);
-#ifdef MODULE_CONFIG
         sysconfig.id = newid;
 
         if (!config_save()) {
             puts("ERROR setting new id");
         }
-
-#endif
     }
 }
