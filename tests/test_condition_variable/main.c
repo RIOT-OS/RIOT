84353cf460484e7ff30a261ab38161c90fbd05b6
Martin <Martin.Landsmann@HAW-Hamburg.de>
2014-02-25 08:34:56 +0100

    fix astyled code

--- tests/test_condition_variable/main.c
+++ tests/test_condition_variable/main.c
@@ -10,37 +10,39 @@ char stack[KERNEL_CONF_STACKSIZE_MAIN];
 
 /**
  * @brief   This thread tries to lock the mutex to enter the critical section.
- *          Then it sets cv true (==1), signals one waiting thread to check cv state and sleep 
+ *          Then it sets cv true (==1), signals one waiting thread to check cv state and sleep
  */
-void second_thread(void) {
-    while(1) {
-        mutex_lock(&mutex);	
+void second_thread(void)
+{
+    while (1) {
+        mutex_lock(&mutex);
         cv.val = 1;
         pthread_cond_signal(&cv);
         thread_sleep();
         mutex_unlock(&mutex);
-	}
+    }
 }
 
-int main(void) {
+int main(void)
+{
     count = 0;
     mutex_init(&mutex);
     pthread_cond_init(&cv, NULL);
 
     int pid = thread_create(stack,
                             KERNEL_CONF_STACKSIZE_MAIN,
-                            PRIORITY_MAIN-1,
+                            PRIORITY_MAIN - 1,
                             CREATE_WOUT_YIELD | CREATE_STACKTEST,
                             second_thread,
                             "second_thread");
 
-    while(1) {
+    while (1) {
         mutex_lock(&mutex);
         thread_wakeup(pid);
         count++;
 
-        if ((count%100000) == 0) {
-            printf("Still alive alternated [count: %dk] times.\n", count/1000);
+        if ((count % 100000) == 0) {
+            printf("Still alive alternated [count: %dk] times.\n", count / 1000);
         }
 
         pthread_cond_wait(&cv, &mutex);
