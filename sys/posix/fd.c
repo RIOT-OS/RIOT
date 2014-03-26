c0bb3c4cc60c467cdbacf5b8e75b7072ac5359c6
René Kijewski <rene.kijewski@fu-berlin.de>
2014-03-26 13:54:36 +0100

    posix: move close into fd.c

--- sys/posix/fd.c
+++ sys/posix/fd.c
@@ -150,6 +150,25 @@ void fd_destroy(int fd)
     memset(cur, 0, sizeof(fd_t));
 }
 
+int close(int fildes)
+{
+    fd_t *fd_obj = fd_get(fildes);
+
+    if (!fd_obj) {
+        errno = EBADF;
+        return -1;
+    }
+
+    if (fd_obj->close(fd_obj->fd) < 0) {
+        errno = EIO;    // EINTR may not occur since RIOT has no signals yet.
+        return -1;
+    }
+
+    fd_destroy(fd_obj->fd);
+
+    return 0;
+}
+
 /**
  * @}
  */
