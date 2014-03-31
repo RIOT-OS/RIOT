d566ae9a28fb4be147b62de5416b20e13dcf1630
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-03-31 13:56:26 +0200

    fix doxygen documentation for clist.h, config.h, tcb.h and cib.h

--- core/include/cib.h
+++ core/include/cib.h
@@ -12,22 +12,61 @@
  *
  * @file        cib.h
  * @brief       Circular integer buffer interface
+ * @details     This structure provides an organizational interface
+ *              and combined with an memory array forms a circular buffer.
  *
+ * @author      unknown, propably Kaspar Schleiser <kaspar@schleiser.de>
  */
 
 #ifndef __CIB_H
 #define __CIB_H
 
+/**
+ * @brief circular integer buffer structure
+ */
 typedef struct cib_t {
-    unsigned int read_count;
-    unsigned int write_count;
-    unsigned int complement;
+    unsigned int read_count;    /**< count read accesses  */
+    unsigned int write_count;   /**< count write accesses */
+    unsigned int complement;    /**< hold complement      */
 } cib_t;
 
+/**
+ * @brief Initialize cib_t to 0 and set size.
+ *
+ * @param[out] cib      Buffer to initialize.
+ *                      Must not be NULL.
+ * @param[in]  size     Size of the buffer.
+ */
 void cib_init(cib_t *cib, unsigned int size);
+
+/**
+ * @brief Get the index of the next item in buffer.
+ *
+ * @param[in,out] cib   corresponding *cib* to buffer.
+ *                      Must not be NULL.
+ * @return index of next item, -1 on error.
+ */
 int cib_get(cib_t *cib);
+
+/**
+ * @brief Get index for item in buffer to put to.
+ *
+ * @param[in,out] cib   corresponding *cib* to buffer.
+ *                      Must not be NULL.
+ * @return index of item to put to, -1 on error.
+ */
 int cib_put(cib_t *cib);
+
+/**
+ * @brief Calculates difference between cib_put() and cib_get() accesses.
+ *
+ * @param[in] cib       the cib_t to check.
+ *                      Must not be NULL.
+ * @return Negative number for #cib_get > #cib_put
+ * @return 0 for #cib_get == #cib_put
+ * @return positive number for #cib_get < #cib_put
+ */
 int cib_avail(cib_t *cib);
 
-/** @} */
 #endif /* __CIB_H */
+/** @} */
