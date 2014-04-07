9ce8ab22751df8b819e0f87ad8d3160d2f458b98
Martin Lenders <mail@martin-lenders.de>
2014-04-07 15:15:25 +0200

    Create unittest application

---
+++ tests/unittests/README.md
@@ -0,0 +1,6 @@
+# Unittests
+
+see [Wiki][Testing-RIOT]
+
+
+  [Testing-RIOT]: https://github.com/RIOT-OS/RIOT/wiki/Testing-RIOT
---
+++ tests/unittests/main.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014 Martin Lenders
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+#include "embUnit/embUnit.h"
+
+#include "lpm.h"
+
+#ifdef OUTPUT
+#define OUTPUT_XML      (1)
+#define OUTPUT_TEXT     (2)
+#define OUTPUT_COMPILER (4)
+
+#if (OUTPUT==OUTPUT_XML)
+#include "textui/XMLOutputter.h"
+
+#define OUTPUTTER   (XMLOutputter_outputter())
+#endif
+
+#if (OUTPUT==OUTPUT_TEXT)
+#include "textui/TextOutputter.h"
+
+#define OUTPUTTER   (TextOutputter_outputter())
+#endif
+
+#if (OUTPUT==OUTPUT_COMPILER)
+#include "textui/CompilerOutputter.h"
+
+#define OUTPUTTER   (CompilerOutputter_outputter())
+#endif
+
+#include "textui/TextUIRunner.h"
+
+#define TESTS_START()   TextUIRunner_start()
+#define TESTS_RUN(t)    TextUIRunner_runTest(t)
+#define TESTS_END()     TextUIRunner_end()
+
+#else
+
+#define TESTS_START()   TestRunner_start()
+#define TESTS_RUN(t)    TestRunner_runTest(t)
+#define TESTS_END()     TestRunner_end()
+
+#endif
+
+
+int main(void)
+{
+#ifdef OUTPUT
+    TextUIRunner_setOutputter(OUTPUTTER);
+#endif
+
+    TESTS_START();
+    /* put test TEST_RUN() calls here */
+    TESTS_END();
+
+    lpm_set(LPM_POWERDOWN);
+
+    return 0;
+}
