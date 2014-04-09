081968bd22ab3df1e2a5417cfb62034ab5beedf4
Martin <Martin.Landsmann@HAW-Hamburg.de>
2014-04-09 08:59:20 +0200

    fixed dumb copy pasted typo

--- tests/test_cpp_mix/cppMix.hpp
+++ tests/test_cpp_mix/cppMix.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Hamburg University of Applied Siences (HAW)
+ * Copyright (C) 2014 Hamburg University of Applied Sciences (HAW)
  *
  * This file subject to the terms and conditions of the GNU Lesser General
  * Public License. See the file LICENSE in the top level directory for more
