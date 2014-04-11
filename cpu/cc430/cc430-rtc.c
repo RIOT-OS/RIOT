2793426c8974feacbbb5d877c71d5bef20d0aedd
René Kijewski <rene.kijewski@fu-berlin.de>
2014-04-11 17:20:23 +0200

    msp430: Fix variable usage in cc430-rtc.c
    Closes #967.

--- cpu/cc430/cc430-rtc.c
+++ cpu/cc430/cc430-rtc.c
@@ -197,7 +197,7 @@ interrupt(RTC_VECTOR) __attribute__((naked)) rtc_isr(void)
         }
 
         if (rtc_second_pid) {
-            msg_t m;
+            static msg_t m;
             m.type = RTC_SECOND;
             msg_send_int(&m, rtc_second_pid);
         }
