ba3088c4bd22ddb8d2a5f93a02c96d612caf0271
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-04-24 16:25:24 +0200

    added iot-lab_M3 and temporarily dropped telosb support; split patch.txt into separate patch files

--- pkg/openwsn/structure_changes.sh
+++ pkg/openwsn/structure_changes.sh
@@ -1,34 +1,69 @@
-#!/usr/bin/zsh
+#!/usr/bin/env sh
 
-printf "Moving openwsn stack ..."
+function _out() {
+    [[ $QUIET != "1" ]] && printf "$@"
+}
+QUIET=0
+
+_out "\n*INFO* restructuring OpenWSN tree\n"
+
+_out "Moving openwsn stack ..."
 # move openwsn stack directory up
 mv openwsn-fw-RB-1.4/firmware/openos/openwsn/ ./
 
 # and all needed hw dependent files too
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/spi.c openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/uart.c openwsn/uart_ow.c
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/leds.c openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/board.c openwsn/board_ow.c
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/board_info.h openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/radiotimer.c openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/eui64.c openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/debugpins.c openwsn
+#
+# telosb disabled for now
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/spi.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/uart.c openwsn/uart_ow.c
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/leds.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/board.c openwsn/board_ow.c
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/board_info.h openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/radiotimer.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/eui64.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/telosb/debugpins.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/chips/cc2420/radio.c openwsn
+# mv openwsn-fw-RB-1.4/firmware/openos/bsp/chips/cc2420/cc2420.h openwsn
+
+#
+# common BSP files
+_out "common..."
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/board.h openwsn/board_ow.h
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/debugpins.h openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/eui64.h openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/leds.h openwsn
+mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/leds.h openwsn/leds_ow.h
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/radio.h openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/radiotimer.h openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/boards/uart.h openwsn/uart_ow.h
 mv openwsn-fw-RB-1.4/firmware/openos/bsp/chips/spi.h openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/chips/cc2420/radio.c openwsn
-mv openwsn-fw-RB-1.4/firmware/openos/bsp/chips/cc2420/cc2420.h openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/drivers/common/openhdlc.* openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/drivers/common/opentimers.* openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/drivers/common/openserial.{c,h} openwsn
 mv openwsn-fw-RB-1.4/firmware/openos/kernel/openos/scheduler.{c,h} openwsn
-printf "[OK]\n"
-printf  "Removing files not needed ... "
+
+#
+# iot-lab_M3 BSPs
+_out "BSPs..."
+touch openwsn/Systick.{c,h}
+touch openwsn/at86rf231.h
+touch openwsn/board_info.h
+touch openwsn/board_ow.c
+touch openwsn/debugpins.c
+touch openwsn/eui64.c
+touch openwsn/exti.c
+touch openwsn/gpio.{c,h}
+touch openwsn/nvic.{c,h}
+touch openwsn/leds_ow.c
+touch openwsn/radio.c
+touch openwsn/radiotimer.c
+touch openwsn/rcc.{c,h}
+touch openwsn/spi.c
+touch openwsn/stm32f10x_lib.h
+touch openwsn/uart_ow.c
+touch openwsn/isr.c
+_out "[OK]\n"
+
+_out  "Removing files not needed ... "
 # remove all *dox files
 for i in `find ./openwsn -name "*.dox"`; do
 rm -f $i
@@ -36,9 +71,9 @@ done
 
 rm -f openwsn/SConscript
 rm -rf openwsn/02.5-MPLS
-printf "[OK]\n"
+_out "[OK]\n"
 
-printf  "Initialize Makefile structure ..."
+_out  "Initialize Makefile structure ..."
 # create empty Makefiles
 touch openwsn/Makefile
 
@@ -61,20 +96,18 @@ rm -f openwsn/07-App/rxl1/Makefile \
         openwsn/07-App/layerdebug/Makefile \
         openwsn/07-App/imu/Makefile \
         openwsn/07-App/heli/Makefile
-printf "[OK]\n"
+_out "[OK]\n"
 
-mkdir openwsn/07-App/r6tus
-touch openwsn/07-App/r6tus/r6tus.c
-touch openwsn/07-App/r6tus/r6tus.h
-
-printf  "Clean up ..."
+_out  "Clean up ..."
 # clean not need files
 rm -rf openwsn-fw-RB-1.4
-printf "[OK]\n"
+_out "[OK]\n"
 
-printf  "Remove CRLF line endings ... "
+_out  "Remove CRLF line endings ... "
 # deal with crlf line endings
 for i in `find ./openwsn -type f`; do
 perl -pi -e 's/\r\n/\n/g' $i
 done
-printf "[OK]\n"
+_out "[OK]\n"
+
+exit 0
