aaa95a281ae8089f1865739474483144d731ff3a
René Kijewski <rene.kijewski@fu-berlin.de>
2014-05-03 18:46:13 +0200

    ringbuffer: remove example

--- sys/lib/ringbuffer.c
+++ sys/lib/ringbuffer.c
@@ -15,19 +15,6 @@
  * @}
  */
 
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-
-#if (defined(__MACH__) || defined(__FreeBSD__))
-#include <stdlib.h>
-#else
-#include "malloc.h"
-#endif
-
-#define ENABLE_DEBUG (0)
-#include "debug.h"
-
 #include "ringbuffer.h"
 
 void ringbuffer_init(ringbuffer_t *rb, char *buffer, unsigned int bufsize)
@@ -88,61 +75,3 @@ int ringbuffer_get(ringbuffer_t *rb, char *buf, int n)
 
     return count;
 }
-
-/*
-int main(int argc, char *argv[] ){
-    ringbuffer r;
-    char buffer[5];
-    ringbuffer_init(&r, buffer, sizeof(buffer));
-
-    ringbuffer_add_one(&r, 1);
-    ringbuffer_add_one(&r, 2);
-    ringbuffer_add_one(&r, 3);
-    ringbuffer_add_one(&r, 4);
-    ringbuffer_add_one(&r, 5);
-    ringbuffer_add_one(&r, 6);
-    ringbuffer_add_one(&r, 7);
-    ringbuffer_add_one(&r, 8);
-    ringbuffer_add_one(&r, 9);
-    ringbuffer_add_one(&r, 10);
-
-    int c;
-    while ( r.avail ) {
-        c = ringbuffer_get_one(&r);
-        if (c == -1) break;
-        printf("c=%i\n", (int)c);
-    }
-
-    ringbuffer_add_one(&r, 1);
-    ringbuffer_add_one(&r, 2);
-    ringbuffer_add_one(&r, 3);
-    ringbuffer_add_one(&r, 4);
-    ringbuffer_add_one(&r, 5);
-
-    char buffer2[10];
-
-    int n = ringbuffer_get(&r, buffer2, sizeof(buffer2));
-
-    for (int i = 0; i < n; i++) {
-        printf("%i\n", buffer2[i]);
-    }
-
-    ringbuffer_add_one(&r, 1);
-    ringbuffer_add_one(&r, 2);
-    ringbuffer_add_one(&r, 3);
-    ringbuffer_add_one(&r, 4);
-    ringbuffer_add_one(&r, 5);
-    ringbuffer_add_one(&r, 6);
-    ringbuffer_add_one(&r, 7);
-    ringbuffer_add_one(&r, 8);
-    ringbuffer_add_one(&r, 9);
-    ringbuffer_add_one(&r, 10);
-
-    while ( r.avail ) {
-        c = ringbuffer_get_one(&r);
-        if (c == -1) break;
-        printf("c=%i\n", (int)c);
-    }
-
-    return 0;
-}*/
