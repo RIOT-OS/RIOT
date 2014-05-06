6f3d0871aaec1d4f324d6d31bc62f00510eca05a
René Kijewski <rene.kijewski@fu-berlin.de>
2014-05-06 17:43:22 +0200

    core/cpu: put __builtin_unreachable() into one place

--- cpu/msp430-common/crash.c
+++ cpu/msp430-common/crash.c
@@ -63,11 +63,5 @@ NORETURN void core_panic(int crash_code, const char *message)
 
     /* tell the compiler that we won't return from this function
        (even if we actually won't even get here...) */
-#if ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 5)) || (__GNUC__ >= 5)
-    __builtin_unreachable();
-#else
-    while (1) {
-        /* do nothing, but do it often */
-    }
-#endif
+    UNREACHABLE();
 }
