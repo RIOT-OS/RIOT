2314915ff2ac498fa4306091580a7677b664d42b
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-05-14 09:49:09 +0200

    license: fix license header grammar
    insert missing "is"

--- examples/default/main.c
+++ examples/default/main.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2013 INRIA
  * Copyright (C) 2013 Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
  *
- * This file subject to the terms and conditions of the GNU Lesser General
+ * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License. See the file LICENSE in the top level directory for more
  * details.
  */
