03cfe05e5f069ac6f98450ec0822528fc148ed4f
Sebastian Sontberg <sebastian@sontberg.de>
2014-05-15 18:53:41 +0200

    Refix dissector.

--- dist/tools/wireshark_dissector/riot.lua
+++ dist/tools/wireshark_dissector/riot.lua
@@ -14,7 +14,7 @@ do
     local f_src = ProtoField.uint16("RIOT.src", "Source", base.DEC, nil)
     local f_pad = ProtoField.bytes("RIOT.pad", "Padding")
     
-    p_riot.fields = { f_length, f_dst, f_src }
+    p_riot.fields = { f_length, f_dst, f_src, f_pad }
 
     local data_dis = Dissector.get("data")
     -- local next_dis = Dissector.get("6lowpan")   -- for 6LoWPAN
@@ -42,7 +42,7 @@ do
         riot_tree:add(f_length, buf(0, 2))
         riot_tree:add(f_dst, buf(2, 2))
         riot_tree:add(f_src, buf(4, 2))
-        --riot_tree:add(f_pad, buf(packet_len + 6))
+        riot_tree:add(f_pad, buf(packet_len + 6))
         
         next_dis:call(buf(6, packet_len):tvb(), pkt, root)
 
