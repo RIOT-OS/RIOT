2c5c177f752eb5df5accd0a9ade52c0cfc9df551
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-05-15 17:43:25 +0200

    native: unify license headers

--- boards/native/drivers/native-uart0.c
+++ boards/native/drivers/native-uart0.c
@@ -1,10 +1,11 @@
 /**
  * Native uart0 implementation
  *
- * Copyright (C) 2013 Ludwig Ortmann
+ * Copyright (C) 2014 Ludwig Ortmann
  *
- * This file is subject to the terms and conditions of the LGPLv2. See
- * the file LICENSE in the top level directory for more details.
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
  *
  * @ingroup native_board
  * @{
