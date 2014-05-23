fa7a902fc2b608fff79789ed4d31e7954d3c452c
Kaspar Schleiser <kaspar@schleiser.de>
2014-05-23 18:26:57 +0200

    tests: test_vtimer_msg: update to new msg_api

--- tests/vtimer_msg/main.c
+++ tests/vtimer_msg/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 INRIA
+ * Copyright (C) 2014 Kaspar Schleiser
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,6 +16,7 @@
  *
  * @author      Oliver Hahm <oliver.hahm@inria.fr>
  * @author      Christian Mehlis <mehlis@inf.fu-berlin.de>
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
  *
  * @}
  */
@@ -25,6 +27,7 @@
 #include "vtimer.h"
 #include "thread.h"
 #include "msg.h"
+#include "msg_queue.h"
 
 char timer_stack[KERNEL_CONF_STACKSIZE_MAIN];
 char timer_stack_local[KERNEL_CONF_STACKSIZE_MAIN];
@@ -49,15 +52,17 @@ void *timer_thread(void *arg)
     /* we need a queue if the second message arrives while the first is still processed */
     /* without a queue, the message would get lost */
     /* because of the way this timer works, there can be max 1 queued message */
-    msg_t msgq[1];
-    msg_init_queue(msgq, sizeof(msgq));
+    msg_queue_t msg_queue;
+    char queue_buf[64];
+    msg_queue_init(&msg_queue, queue_buf, sizeof(queue_buf), 0);
+    thread_set_msg_queue(thread_pid, &msg_queue);
 
     while (1) {
-        msg_t m;
-        msg_receive(&m);
+        msg_pulse_t m;
+        msg_receive_pulse(&m);
         struct timer_msg *tmsg = (struct timer_msg *) m.content.ptr;
         vtimer_now(&now);
-        printf("now=%" PRIu32 ":%" PRIu32 " -> every %" PRIu32 ".%" PRIu32 "s: %s\n",
+        printf("now=%2" PRIu32 ":%" PRIu32 " -> every %" PRIu32 ".%" PRIu32 "s: %s\n",
                now.seconds,
                now.microseconds,
                tmsg->interval.seconds,
@@ -80,18 +85,18 @@ void *timer_thread_local(void *arg)
     printf("This is thread %" PRIkernel_pid "\n", thread_getpid());
 
     while (1) {
-        msg_t m;
-        msg_receive(&m);
+        msg_pulse_t m;
+        msg_receive_pulse(&m);
 
         struct tm t;
         vtimer_get_localtime(&t);
-        printf("sec=%d min=%d hour=%d\n", t.tm_sec, t.tm_min, t.tm_hour);
+        printf("sec=%02d min=%02d hour=%d\n", t.tm_sec, t.tm_min, t.tm_hour);
     }
 }
 
 int main(void)
 {
-    msg_t m;
+    msg_pulse_t m;
     kernel_pid_t pid = thread_create(
                   timer_stack,
                   sizeof(timer_stack),
@@ -103,11 +108,11 @@ int main(void)
 
     puts("sending 1st msg");
     m.content.ptr = (char *) &msg_a;
-    msg_send(&m, pid, false);
+    msg_try_send_pulse(&m, pid);
 
     puts("sending 2nd msg");
     m.content.ptr = (char *) &msg_b;
-    msg_send(&m, pid, false);
+    msg_try_send_pulse(&m, pid);
 
     kernel_pid_t pid2 = thread_create(
                    timer_stack_local,
@@ -122,6 +127,6 @@ int main(void)
 
     while (1) {
         vtimer_sleep(sleep);
-        msg_send(&m, pid2, 0);
+        msg_try_send_pulse(&m, pid2);
     }
 }
