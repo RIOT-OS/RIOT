54069def9df8d2d6b91885133f993260a799c2ca
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2014-06-10 12:08:59 +0200

    WIP: DTLS handshakre and record layer protocols

---
+++ examples/dtls/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello World!");
+    return 0;
+}
