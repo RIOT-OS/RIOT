2ba8f2b23634090eeb1443851f6f1adde6283a9d
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2014-07-01 13:12:55 +0200

    intial added CIPHERS

---
+++ sys/net/transport_layer/dtls/ciphers.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+#include <unistd.h>
+#include "ciphers.h"
+
+int dtls_cipher_aes_128_cbc_sha(dtls_cipher_mode_t mode, uint8_t *input,
+                                size_t size, uint8_t *output)
+{
+  return -1;
+}
+
+int dtls_cipher_aes_128_ccm(dtls_cipher_mode_t mode)
+{
+  return -1;
+}
+
+int dtls_cipher_aes_128_ccm_8(dtls_cipher_mode_t mode)
+{
+  return -1;
+}
---
+++ sys/net/transport_layer/dtls/ciphers.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013  Freie Universität Berlin.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ *
+ * @ingroup dtls
+ * @{
+ * @file
+ * @brief   add foo here
+ * @author  Jonatan Zint <j.zint@fu-berlin.com>
+ * @author  Nico von Geyso <nico.geyso@fu-berlin.com>
+ */
+
+#ifndef DTLS_CIPHERS_H_
+#define DTLS_CIPHERS_H_
+
+typedef enum {
+  DTLS_CIPHER_ENCRYPT,
+  DTLS_CIPHER_DECRYPT
+} dtls_cipher_mode_t;
+
+#endif
+/**
+ * @}
+ */
