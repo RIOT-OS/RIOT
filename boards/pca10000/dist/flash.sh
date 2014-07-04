35fede05b38a64b84a5d55823321024197f0d5d4
taaz <dev@taaz.dialup.fu-berlin.de>
2014-07-04 15:06:42 +0200

    flash fix

--- boards/pca10000/dist/flash.sh
+++ boards/pca10000/dist/flash.sh
@@ -6,7 +6,7 @@ APPLICATION=$3
 BOARD=$4
 
 echo "log /dev/null" > $BINDIR/burn.seg
-echo "device nrf51822" > $BINDIR/burn.seg
+echo "device nrf51822" >> $BINDIR/burn.seg
 echo "speed 1000" >> $BINDIR/burn.seg
 echo "w4 4001e504 1" >> $BINDIR/burn.seg
 echo "loadbin $RIOTBASE/examples/$APPLICATION/bin/$BOARD/$APPLICATION.bin 0" >> $BINDIR/burn.seg
