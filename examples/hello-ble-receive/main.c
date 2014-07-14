07d7559652658dc2aff787c60ffe935f73d564a6
taaz <dev@taaz.dialup.fu-berlin.de>
2014-07-15 01:00:48 +0200

    code refactoring: [examples] hello-ble-receive, hello-ble-send

--- examples/hello-ble-receive/main.c
+++ examples/hello-ble-receive/main.c
@@ -28,79 +28,27 @@
 #include "periph/gpio.h"
 #include "radio.h"
 
+/**
+ * This example assumes that on GPIO pins 1 and 6 are LEDs connected.
+ * This is just necessary to check the current status of a board without using the UART.
+ *
+ * If using the pca10000, gpio_sets are not disturbing, but LED_{COLOR}_{ON/OFF} is executed,
+ * to use the onboard LEDs.
+ **/
 int main(void)
 {
     gpio_init_out(GPIO_6, GPIO_NOPULL);
-    gpio_init_out(GPIO_7, GPIO_NOPULL);
     gpio_init_out(GPIO_1, GPIO_NOPULL);
-    //gpio_set(GPIO_6);
 
-    radioConfig(1);
-    char msg = 'H';
+    gpio_set(GPIO_1);
+    LED_RED_ON;
 
-//    if(msg == 'H')  {
-//    	gpio_set(GPIO_6);
-//    	delay(2*1000*1000);
-//    	gpio_clear(GPIO_6);
-//    }
-
-
-    gpio_set(GPIO_6);
-
-
-    //vtimer_usleep(1000000);
-
-
-
-    //gpio_init_out(GPIO_0, GPIO_NOPULL);
-    //gpio_set(GPIO_0);
-//    GPIO_DEV->OUTSET = (1UL << 1);
-
-    int i = 0;
-
-
-    //timer_init(TIMER_0, 1, printInt0);
-//    timer_init(TIMER_0, 1, printInt0);
-//
-//    timer_set(TIMER_0,0,2*1000*1000);
-//    timer_set(TIMER_0,1,4*1000*1000);
-//    timer_set(TIMER_0,2,6*1000*1000);
-
-
-
-//    timer_set(TIMER_1,0,10*1000*1000);
-//    timer_set(TIMER_1,1,11*1000*1000);
-//    timer_set(TIMER_1,2,12*1000*1000);
-
+    radioConfig();
 
 	while(1) {
-
-		msg = receivePacket();
-
-
-	//    if(msg == 'H')  {
-	//    	gpio_set(GPIO_6);
-	//    	delay(1*1000*1000);
-	//    	gpio_clear(GPIO_6);
-	//    }
-	//    else  {
-	//    	gpio_set(GPIO_7);
-	//    	delay(1*1000*1000);
-	//    	gpio_clear(GPIO_7);
-	//    }
-
+		char msg = receivePacket();
+        printf("message received: %c", msg);
 	}
 
     return 0;
-}
-
-
-
-void delay(uint32_t microseconds){
-    /* perform busy-waiting for specified number of microseconds  */
-    uint32_t cycles = microseconds * 2; // factor has been found by measure
-    for (int i = 0; i < cycles; i++) {
-        asm("nop");
-    }
-
 }
--- examples/hello-ble-send/main.c
+++ examples/hello-ble-send/main.c
@@ -20,8 +20,6 @@
  */
 
 #include <stdio.h>
-
-#include "vtimer.h"
 #include "periph/timer.h"
 #include "board.h"
 #include "cpu.h"
@@ -30,51 +28,37 @@
 #include "periph/gpio.h"
 #include "radio.h"
 
-void cb(int i )  {
-	printf("blaaa");
-
-}
-
+/**
+ * This example assumes that on GPIO pins 1 and 6 are LEDs connected.
+ * This is just necessary to check the current status of a board without using the UART.
+ *
+ * If using the pca10000, gpio_sets are not disturbing, but LED_{COLOR}_{ON/OFF} is executed,
+ * to use the onboard LEDs.
+ **/
 int main(void)
 {
-
-
-    gpio_init_out(GPIO_6, GPIO_NOPULL);
-    gpio_init_out(GPIO_7, GPIO_NOPULL);
+    gpio_init_out(GPIO_6, GPIO_NOPULL); 
     gpio_init_out(GPIO_1, GPIO_NOPULL);
 
-
     gpio_set(GPIO_1);
+    LED_RED_ON;
 
-    LED_RED_OFF;
+    radioConfig();
+    char msg = 'Q';
+    while(1) {
 
-    //vtimer_usleep(2);
+        // sending all the time some message
 
-    LED_RED_OFF;
+        sendPacket(1, msg);
 
-
-
-
-
-
-	radioConfig(0);
-	char msg = 'Q';
-	while(1) {
-
-		// sending all the time some message
-
-		sendPacket(1, msg);
-
-		delay(100*1000);
-	}
+        delay(100*1000);
+    }
     return 0;
 }
 
-
-
 void delay(uint32_t microseconds){
     /* perform busy-waiting for specified number of microseconds  */
-    uint32_t cycles = microseconds * 2; // factor has been found by measure
+    uint32_t cycles = microseconds ;
     for (int i = 0; i < cycles; i++) {
         asm("nop");
     }
