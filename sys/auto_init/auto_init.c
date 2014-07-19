96502e2fd4d97a60dd2e0b7ace4c11940d34ac9f
Martin Lenders <mail@martin-lenders.de>
2014-07-19 18:00:21 +0200

    sys: net: Initial import of a basic MAC protocol layer

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -67,6 +67,10 @@
 #include "tcp.h"
 #endif
 
+#ifdef MODULE_NOMAC
+#include "nomac.h"
+#endif
+
 #ifdef MODULE_NET_IF
 #include "cpu-conf.h"
 #include "cpu.h"
@@ -240,6 +244,10 @@ void auto_init(void)
 #ifdef MODULE_L2_PING
     DEBUG("Auto init net_if module.\n");
     l2_ping_init();
+#endif
+#ifdef MODULE_NOMAC
+    DEBUG("Auto init nomac module.\n");
+    nomac_init_module();
 #endif
 #ifdef MODULE_NET_IF
     DEBUG("Auto init net_if module.\n");
