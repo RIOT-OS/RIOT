cca9a6cb94f8b65275fb1641ee07763488eefdb0
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2014-07-21 17:13:07 +0200

    Added unittests for AES crypto cipher
    As test vector we used input/output of PyCrypto 2.6.1:
    
      from Crypto.Cipher import AES
      binascii.hexlify(AES.new(key, AES.MODE_ECB).encrypt(msg))

---
+++ tests/unittests/tests-crypto/tests-crypto.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2014 Nico von Geyso
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+#include "tests-crypto.h"
+
+void tests_crypto(void)
+{
+	TESTS_RUN(tests_crypto_aes_tests());
+}
+
