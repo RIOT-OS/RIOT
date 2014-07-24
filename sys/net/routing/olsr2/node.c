9a5f00589f409455ff69ed38d0ed9f5eea785eb8
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2014-07-24 16:32:03 +0200

    add @author

--- sys/net/routing/olsr2/node.c
+++ sys/net/routing/olsr2/node.c
@@ -4,6 +4,11 @@
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License. See the file LICENSE in the top level directory for more
  * details.
+ *
+ * @ingroup olsr2
+ * @{
+ * @author  Benjamin Valentin <benpicco@zedat.fu-berlin.de>
+ * @}
  */
 
 #include <stdio.h>
