1154bd5a50a372a97401b364c6c5e0837d97eae2
epiktet <kuehling@zedat.fu-berlin.de>
2014-07-30 14:59:14 +0200

    cpu/nrf51822: squash added support for nrf51822 cpu

---
+++ cpu/nrf51822/include/cpu-conf.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         cpu_nrf51822
+ * @{
+ *
+ * @file            cpu-conf.h
+ * @brief           Implementation specific CPU configuration options
+ *
+ * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
+ */
+
+#ifndef __CPU_CONF_H
+#define __CPU_CONF_H
+
+#include "nrf51.h"
+#include "nrf51_bitfields.h"
+
+
+/**
+ * @name Kernel configuration
+ *
+ * TODO: measure and adjust for the cortex-m0
+ * @{
+ */
+#define KERNEL_CONF_STACKSIZE_PRINTF    (1024)
+
+#ifndef KERNEL_CONF_STACKSIZE_DEFAULT
+#define KERNEL_CONF_STACKSIZE_DEFAULT   (1024)
+#endif
+
+#define KERNEL_CONF_STACKSIZE_IDLE      (256)
+/** @} */
+
+/**
+ * @name UART0 buffer size definition for compatibility reasons
+ *
+ * TODO: remove once the remodeling of the uart0 driver is done
+ * @{
+ */
+#ifndef UART0_BUFSIZE
+#define UART0_BUFSIZE                   (128)
+#endif
+/** @} */
+
+#endif /* __CPU_CONF_H */
+/** @} */
---
+++ cpu/nrf51822/lpm_arch.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     cpu_nrf51822
+ * @{
+ *
+ * @file        lpm_arch.c
+ * @brief       Implementation of the kernels power management interface
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "arch/lpm_arch.h"
+
+void lpm_arch_init(void)
+{
+	/* TODO: needs to be implemented */
+}
+
+enum lpm_mode lpm_arch_set(enum lpm_mode target)
+{
+	/* TODO: needs to be implemented */
+    return 0;
+}
+
+enum lpm_mode lpm_arch_get(void)
+{
+	/* TODO: needs to be implemented */
+    return 0;
+}
+
+void lpm_arch_awake(void)
+{
+	/* TODO: needs to be implemented */
+}
+
+void lpm_arch_begin_awake(void)
+{
+	/* TODO: needs to be implemented */
+}
+
+void lpm_arch_end_awake(void)
+{
+	/* TODO: needs to be implemented */
+}
