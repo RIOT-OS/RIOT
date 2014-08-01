b7b7fd3bc1c1bae668797f986586eb4b634e7c8b
René Kijewski <rene.kijewski@fu-berlin.de>
2014-08-01 02:33:22 +0200

    make: provide BOARD, MCU and CPU name as a string
    This PR adds the defined `RIOT_BOARD`, `RIOT_CPU` and `RIOT_MCU`.
    They each are defined as `BOARD_${BOARD}`, `CPU_${CPU}`, and
    `MCU_${CPU}`, resp.
    
    `BOARD_${BOARD}` is defined as the string `"${BOARD}"`, but not in upper
    case. Same for the other two.

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -24,5 +24,9 @@
 int main(void)
 {
     puts("Hello World!");
+
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
     return 0;
 }
