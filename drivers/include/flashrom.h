c883c33d835352fba3564a0fff0149057bd6c869
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-08-01 10:57:39 +0200

    doc: fix flashrom doxygen

--- drivers/include/flashrom.h
+++ drivers/include/flashrom.h
@@ -1,11 +1,12 @@
-/*
+/**
  * @defgroup    flashrom Flash memory driver
  * @ingroup     drivers
  * @brief       Generic flash memory driver
  * @{
  *
- * @file        flashrom.h
+ * @file
  *
+ * @brief       Generic flash memory driver
  * @author      unknown
  */
 
@@ -15,24 +16,25 @@
 #include <stdint.h>
 #include <stddef.h>
 
-/*
+/**
  * @brief Erase sector
  *
- * @param addr      Address within a flash sector to erase
+ * @param[out] addr Address within a flash sector to erase
  *
  * @return          1 on success, 0 otherwise
  */
 uint8_t             flashrom_erase(uint8_t *addr);
 
-/* @brief Write buffer from ram to flash
+/**
+ * @brief Write buffer from ram to flash
  *
- * @param dst       Address within a flash sector to write, must be a 256 byte boundary
- * @param src       Address within ram, must be a word boundary
- * @param size      Bytes to write
+ * @param[out] dst  Address within a flash sector to write, must be a 256 byte boundary
+ * @param[in] src   Address within ram, must be a word boundary
+ * @param[in] size  Bytes to write
  *
  * @return          1 on success, 0 otherwise
  */
 uint8_t             flashrom_write(uint8_t *dst, const uint8_t *src, size_t size);
 
-/** @} */
 #endif /* FLASHROM_H */
+/** @} */
