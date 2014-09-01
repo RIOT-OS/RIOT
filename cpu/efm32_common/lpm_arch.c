8663db710f317ff9c7395d4534c751bea4e9399b
Ryan Kurte <ryan.kurte@trutest.co.nz>
2014-09-01 15:00:23 +1200

    Fixed serial communication for EFM32GG-ST3700

--- cpu/efm32_common/lpm_arch.c
+++ cpu/efm32_common/lpm_arch.c
@@ -14,6 +14,7 @@
  * @brief       Implementation of the kernels power management interface
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Ryan Kurte <ryankurte@gmail.com>
  *
  * @}
  */
@@ -29,6 +30,27 @@ void lpm_arch_init(void)
 enum lpm_mode lpm_arch_set(enum lpm_mode target)
 {
     /* TODO */
+    switch (target) {
+    case LPM_ON:             /**< MCU is active */
+
+        break;
+    case LPM_IDLE:           /**< MCU is idle */
+
+        break;
+    case LPM_SLEEP:          /**< MCU in sleep mode */
+
+        break;
+    case LPM_POWERDOWN:      /**< MCU is powered down */
+
+        break;
+    case LPM_OFF:            /**< MCU is off */
+
+        break;
+    case LPM_UNKNOWN:         /**< status unknown/unavailable */
+
+        break;
+    }
+
     return 0;
 }
 
