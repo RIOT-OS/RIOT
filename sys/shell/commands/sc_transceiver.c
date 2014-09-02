19f612cf2c3a551c7c4244c979ba95f01069d2fb
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-09-02 11:37:04 +0200

    examples/default: set src addr correctly

--- sys/shell/commands/sc_transceiver.c
+++ sys/shell/commands/sc_transceiver.c
@@ -239,8 +239,9 @@ void _transceiver_send_handler(int argc, char **argv)
     p.frame.payload = (uint8_t*) text_msg;
     p.frame.payload_len = strlen(text_msg) + 1;
     p.frame.fcf.dest_addr_m = IEEE_802154_SHORT_ADDR_M;
+    p.frame.fcf.src_addr_m = IEEE_802154_SHORT_ADDR_M;
     memset(p.frame.dest_addr, 0, sizeof(p.frame.dest_addr));
-    p.frame.dest_addr[7] = atoi(argv[1]);
+    p.frame.dest_addr[0] = atoi(argv[1]);
 #else
     p.data = (uint8_t *) text_msg;
     p.length = strlen(text_msg) + 1;
--- sys/transceiver/transceiver.c
+++ sys/transceiver/transceiver.c
@@ -60,7 +60,7 @@
 #include "ieee802154_frame.h"
 #endif
 
-#define ENABLE_DEBUG (0)
+#define ENABLE_DEBUG (1)
 #if ENABLE_DEBUG
 #define DEBUG_ENABLED
 #undef TRANSCEIVER_STACK_SIZE
