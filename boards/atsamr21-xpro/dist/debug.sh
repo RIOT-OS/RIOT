759d5c3e1b5e0644194f288a5d7816dec939bf3c
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-09-09 14:07:13 +0200

    samr21: another flash fix and added debug target

---
+++ boards/atsamr21-xpro/dist/debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+openocd -f "${RIOTBOARD}/${BOARD}/dist/atmel_samr21_xpro.cfg" \
+    -c "tcl_port 6333" \
+    -c "telnet_port 4444" \
+    -c "init" \
+    -c "targets" \
+    -c "reset halt"
