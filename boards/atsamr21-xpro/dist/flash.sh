2e49558cca309f62b43ce3ca041a942433dcdc58
Thomas Eichinger <thomas.eichinger1@gmail.com>
2014-09-09 13:23:05 +0200

    samr21: fix openocd flashing script

--- boards/atsamr21-xpro/dist/flash.sh
+++ boards/atsamr21-xpro/dist/flash.sh
@@ -1,6 +1,11 @@
-#!/bin/env sh
+#!/bin/sh
 
-openocd -f "${RIOTBOARD}/${BOARD}/dist/atmel_samr21_xplained_pro.cfg" \
+openocd -f "${RIOTBOARD}/${BOARD}/dist/atmel_samr21_xpro.cfg" \
     -c "init" \
     -c "targets" \
+    -c "reset halt" \
+    -c "reset init" \
+    -c "flash write_image erase $1" \
+    -c "verify_image $1" \
+    -c "reset run"\
     -c "shutdown"
