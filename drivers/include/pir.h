e75dd40eea8c045465ec41dae10d91e7842d03c9
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-09-24 19:33:12 +0200

    drivers: import PIR motion sensor driver
    Includes an application for manual testing.

---
+++ drivers/include/pir.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    driver_pir PIR Motion Sensor
+ * @ingroup     drivers
+ * @brief       Device driver interface for the PIR motion sensor
+ * @{
+ *
+ * @file
+ * @brief       Device driver interface for the PIR motion sensor
+ *
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ */
+
+#ifndef __PIR_H
+#define __PIR_H
+
+#include "kernel_types.h"
+#include "periph/gpio.h"
+
+/**
+ * @brief device descriptor for a PIR sensor
+ */
+typedef struct {
+    gpio_t          gpio_dev;       /**< GPIO device which is used */
+    kernel_pid_t    msg_thread_pid; /**< thread to msg on irq */
+} pir_t;
+
+#ifndef PIR_MSG_T_STATUS_START
+#define PIR_MSG_T_STATUS_START 150
+#endif
+
+typedef enum {
+    PIR_STATUS_HI = PIR_MSG_T_STATUS_START,     /**< motion was detected */
+    PIR_STATUS_LO,                              /**< no motion is detected */
+} pir_event_t;
+
+/**
+ * @brief               Initialize a PIR motion sensor
+ *
+ * The PIR motion sensor is interfaced by a single GPIO pin, specified by
+ * `gpio`.
+ *
+ * @note
+ * The sensor needs up to a minute to settle down before meaningful
+ * measurements can be made.
+ *
+ * @param[out] dev      device descriptor of an PIR sensor
+ * @param[in] gpio      the GPIO device the sensor is connected to
+ *
+ * @return              0 on success
+ * @return              -1 on error
+ */
+int pir_init(pir_t *dev, gpio_t gpio);
+
+/**
+ * @brief               Read the current status of the motion sensor
+ *
+ * @param[in] dev       device descriptor of the PIR motion sensor to read from
+ *
+ * @return              1 if motion is detected, 0 otherwise
+ */
+pir_event_t pir_get_status(pir_t *dev);
+
+/**
+ * @brief   Register a thread for notification whan state changes on the
+ *          motion sensor.
+ *
+ * @note
+ * This configures the gpio device for interrupt driven operation.
+ *
+ * @param[in] dev       device descriptor of the PIR motion sensor to
+ *                      register for
+ *
+ * @return              0 on succuess,
+ * @return              -1 on internal errors,
+ * @return              -2 if another thread is registered already
+ */
+int pir_register_thread(pir_t *dev);
+
+#endif /* __PIR_H */
+/** @} */
---
+++ drivers/pir/pir.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     driver_pir
+ * @{
+ *
+ * @file
+ * @brief       Device driver implementation for the PIR motion sensor
+ *
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "pir.h"
+#include "thread.h"
+#include "msg.h"
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+/**********************************************************************
+ * internal API declaration
+ **********************************************************************/
+
+static int pir_activate_int(pir_t *dev);
+static void pir_callback(void *dev);
+static void pir_send_msg(pir_t *dev, pir_event_t event);
+
+/**********************************************************************
+ * public API implementation
+ **********************************************************************/
+
+int pir_init(pir_t *dev, gpio_t gpio)
+{
+    dev->gpio_dev = gpio;
+    dev->msg_thread_pid = KERNEL_PID_UNDEF;
+    return gpio_init_in(dev->gpio_dev, GPIO_NOPULL);
+}
+
+pir_event_t pir_get_status(pir_t *dev)
+{
+    return ((gpio_read(dev->gpio_dev) == 0) ? PIR_STATUS_LO : PIR_STATUS_HI);
+}
+
+int pir_register_thread(pir_t *dev)
+{
+    if (dev->msg_thread_pid != KERNEL_PID_UNDEF) {
+        if (dev->msg_thread_pid != thread_getpid()) {
+            DEBUG("pir_register_thread: already registered to another thread\n");
+            return -2;
+        }
+    }
+    else {
+        DEBUG("pir_register_thread: activating interrupt for %p..\n", dev);
+        if (pir_activate_int(dev) != 0) {
+            DEBUG("\tfailed\n");
+            return -1;
+        }
+        DEBUG("\tsuccess\n");
+    }
+    dev->msg_thread_pid = thread_getpid();
+
+    return 0;
+}
+
+/**********************************************************************
+ * internal API implementation
+ **********************************************************************/
+
+static void pir_send_msg(pir_t *dev, pir_event_t event)
+{
+    DEBUG("pir_send_msg\n");
+    msg_t m = { .type = event, .content.ptr = (char*) dev, };
+
+    int ret = msg_send_int(&m, dev->msg_thread_pid);
+    DEBUG("pir_send_msg: msg_send_int: %i\n", ret);
+    switch (ret) {
+        case 0:
+            DEBUG("pir_send_msg: msg_thread_pid not receptive, event is lost");
+            break;
+        case 1:
+            DEBUG("pir_send_msg: OK");
+            break;
+        case -1:
+            DEBUG("pir_send_msg: msg_thread_pid is gone, clearing it");
+            dev->msg_thread_pid = KERNEL_PID_UNDEF;
+            break;
+    }
+}
+
+static void pir_callback(void *arg)
+{
+    DEBUG("pir_callback: %p\n", arg);
+    pir_t *dev = (pir_t*) arg;
+    if (dev->msg_thread_pid != KERNEL_PID_UNDEF) {
+        pir_send_msg(dev, pir_get_status(dev));
+    }
+}
+
+static int pir_activate_int(pir_t *dev)
+{
+    return gpio_init_int(dev->gpio_dev, GPIO_NOPULL, GPIO_BOTH, pir_callback, dev);
+}
