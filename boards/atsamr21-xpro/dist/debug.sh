28f2b4fa734545eec7421e4342c8f10ba8abfcd1
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2014-10-02 14:10:05 +0200

    removed old board

--- boards/atsamr21-xpro/dist/debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openocd -f "${RIOTBOARD}/${BOARD}/dist/atmel_samr21_xpro.cfg" \
-    -c "tcl_port 6333" \
-    -c "telnet_port 4444" \
-    -c "init" \
-    -c "targets" \
-    -c "reset halt"\
