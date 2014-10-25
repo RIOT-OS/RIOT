759aa5f4945da2957643f585181767723c9046a6
Kaspar Schleiser <kaspar@schleiser.de>
2014-10-25 13:34:25 +0200

    SQUASHME: dropped test_ name, added diff printing

--- tests/vtimer_sleep_until/main.c
+++ tests/vtimer_sleep_until/main.c
@@ -19,17 +19,22 @@
  */
 
 #include <stdio.h>
+#include <hwtimer.h>
 #include <vtimer.h>
 
 int main(void)
 {
     puts("Test Starting.\n");
-    timex_t last_wakeup, interval = { 1, 0 };
+    timex_t last_wakeup, interval = { 0, 2000 };
     vtimer_now(&last_wakeup);
 
+    uint32_t pre, after;
+
     while(1) {
+        pre = hwtimer_now();
         vtimer_sleep_until(&last_wakeup, interval);
-        puts("Woke up.");
+        after = hwtimer_now();
+        printf("Woke up. diff: %i.\n", after-pre);
     }
 
     return 0;
