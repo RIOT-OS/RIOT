38fab9dbb65d2f7129114f9379b14caa491b8876
Martine Lenders <mail@martine-lenders.eu>
2014-10-25 18:34:38 +0200

    rpl_udp: Addresses must only be 8-bit with cc110x

--- examples/rpl_udp/helper.c
+++ examples/rpl_udp/helper.c
@@ -43,7 +43,11 @@ void rpl_udp_set_id(int argc, char **argv)
 {
     if (argc != 2) {
         printf("Usage: %s address\n", argv[0]);
+#if defined(MODULE_CC110X_LEGACY_CSMA) || defined(MODULE_CC110X_LEGACY)
         printf("\taddress must be an 8 bit integer\n");
+#else
+        printf("\taddress must be an 16 bit integer\n");
+#endif
         printf("\n\t(Current address is %u)\n", id);
         return;
     }
