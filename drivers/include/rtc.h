e182ba00435c8b85a890a2183f46f54680150059
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-10-31 12:42:40 +0100

    doc: add @author, @file to drivers/rtc

--- drivers/include/rtc.h
+++ drivers/include/rtc.h
@@ -12,7 +12,11 @@
  * @brief       Generic real time clock driver interface
  * @deprecated  This interface is obsolete. Use the @ref driver_periph_rtc
  *              interface in @ref driver_periph instead.
+ *
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ *
  * @{
+ * @file
  */
 
 #ifndef RTC_H
