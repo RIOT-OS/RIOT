6efff8c1c0c74ace068b8510a084878f9f0295c9
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2014-11-03 10:51:05 +0100

    Crash notification for samd21

--- cpu/samd21/startup.c
+++ cpu/samd21/startup.c
@@ -19,7 +19,7 @@
  */
 
 #include <stdint.h>
-
+#include "crash.h"
 
 /**
  * memory markers as defined in the linker script
@@ -80,9 +80,9 @@ void reset_handler(void)
  */
 void dummy_handler(void)
 {
+    core_panic(DUMMY_HANDLER, "DUMMY HANDLER");
     while (1) {asm ("nop");}
 }
-
 
 void isr_nmi(void)
 {
@@ -101,16 +101,19 @@ void isr_debug_mon(void)
 
 void isr_hard_fault(void)
 {
+    core_panic(HARD_FAULT, "HARD FAULT");
     while (1) {asm ("nop");}
 }
 
 void isr_bus_fault(void)
 {
+    core_panic(BUS_FAULT, "BUS FAULT");
     while (1) {asm ("nop");}
 }
 
 void isr_usage_fault(void)
 {
+    core_panic(USAGE_FAULT, "USAGE FAULT");
     while (1) {asm ("nop");}
 }
 
