ce635bc62054d0cf478a36162164c48863a93b13
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-11-13 18:45:36 +0100

    board/yunjia-nrf51822: enabled NRF51 radio

--- boards/yunjia-nrf51822/include/board.h
+++ boards/yunjia-nrf51822/include/board.h
@@ -64,6 +64,18 @@ extern "C" {
 /* @} */
 
 /**
+ * @name Define the transceiver buffer size
+ */
+#ifdef MODULE_TRANSCEIVER
+#define TRANSCEIVER_BUFFER_SIZE     (3U)
+#endif
+
+/**
+ * Define the type for the radio packet length for the transceiver
+ */
+typedef uint8_t radio_packet_length_t;
+
+/**
  * @brief Initialize board specific hardware, including clock, LEDs and std-IO
  */
 void board_init(void);
