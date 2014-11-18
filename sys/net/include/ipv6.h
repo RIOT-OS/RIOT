375a62a4899e0e765cf949ce9004fb25567c70eb
Martine Lenders <mail@martine-lenders.eu>
2014-11-18 19:55:50 +0100

    [SQUASH ME] ipv6: continue implementation and specification

--- sys/net/include/ipv6.h
+++ sys/net/include/ipv6.h
@@ -17,9 +17,11 @@
  * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
  */
 
-
 #ifndef __IPV6_H_
 #define __IPV6_H_
+
+#include <stdbool.h>
+#include <stdint.h>
 
 #include "byteorder.h"
 #include "ipv6/addr.h"
@@ -56,12 +58,22 @@ extern "C" {
 #define IPV6_PROTO_NUM_IPV6_OPTS    (60)
 
 /**
+ * @brief   L4 protocol number for every data delivered to IPv6.
+ *
+ * @details This value is thought to be used for registration of
+ *          "catch-all" threads with @ref netapi_register(),
+ *          @ref netapi_register_current_thread, or the raw NETAPI_CMD_REG
+ *          command.
+ */
+#define IPV6_PROTO_NUM_IPV6_ANY     (256)
+
+/**
  * @brief   PID of the IPv6 control thread.
  */
 extern kernel_pid_t ipv6_pid;
 
 /**
- * @brief   @ref netapi configuration types for IPv6
+ * @brief   @ref netapi configuration types for IPv6.
  * @extends netapi_conf_type_t
  *
  * @details Since the IPv6 thread is to be supposed the only one of its kind
@@ -76,7 +88,9 @@ typedef enum {
      *          NETDEV_PROTO_IPV6; Only for getting.
      */
     IPV6_CONF_PROTO = NETAPI_CONF_PROTO,
-    IPV6_CONF_REGISTRY = NETAPI_CONF_REGISTRY,  /**< Getter for receiver registry */
+    IPV6_CONF_REGISTRY = NETAPI_CONF_REGISTRY,  /**< Get for receiver registry. */
+    IPV6_CONF_DEFAULT_HOP_LIMIT,    /**< Get or set default hop limit. */
+    IPV6_CONF_ROUTING_PROVIDER,     /**< Set routing provider. */
 } ipv6_conf_t;
 
 /**
@@ -122,9 +136,20 @@ typedef struct __attribute__((packed)) {
 } ipv6_hdr_t;
 
 /**
+ * @brief   Definition of the routing provider callback.
+ *
+ * @param[out] if_id    Interface *dest* is reachable from.
+ * @param[out] dest     IPv6 address to get the next hop for.
+ *
+ * @return  next hop to *dest*.
+ * @return  NULL, if *dest* is unreachable.
+ */
+typedef ipv6_addr_t *(*ipv6_routing_provider_t)(int *if_id, ipv6_addr_t *dest);
+
+/**
  * @brief   Sets the version field of *hdr* to 6
  *
- * @param[in] hdr   Pointer to an IPv6 header.
+ * @param[out] hdr  Pointer to an IPv6 header.
  */
 static inline void ipv6_hdr_set_version(ipv6_hdr_t *hdr)
 {
@@ -139,7 +164,7 @@ static inline void ipv6_hdr_set_version(ipv6_hdr_t *hdr)
  *
  * @return  Value of the version field of *hdr*.
  */
-static inline uint8_t ipv6_hdr_get_version(ipv6_hdr_t *hdr)
+static inline uint8_t ipv6_hdr_get_version(const ipv6_hdr_t *hdr)
 {
     return ((hdr->v_tc_fl.u8[0]) >> 4);
 }
@@ -152,7 +177,7 @@ static inline uint8_t ipv6_hdr_get_version(ipv6_hdr_t *hdr)
  * @return  1, if version field is 6
  * @return  0, otherwise
  */
-static inline uint8_t ipv6_hdr_is_ipv6_hdr(ipv6_hdr_t *hdr)
+static inline uint8_t ipv6_hdr_is_ipv6_hdr(const ipv6_hdr_t *hdr)
 {
     return (((hdr->v_tc_fl.u8[0]) & 0xf0) == 0x60);
 }
@@ -160,7 +185,7 @@ static inline uint8_t ipv6_hdr_is_ipv6_hdr(ipv6_hdr_t *hdr)
 /**
  * @brief   Sets the traffic class field of *hdr*
  *
- * @param[in] hdr           Pointer to an IPv6 header.
+ * @param[out] hdr          Pointer to an IPv6 header.
  * @param[in] trafficclass  The new value for the traffic class field.
  */
 static inline void ipv6_hdr_set_trafficclass(ipv6_hdr_t *hdr, uint8_t trafficclass)
@@ -181,7 +206,7 @@ static inline void ipv6_hdr_set_trafficclass(ipv6_hdr_t *hdr, uint8_t trafficcla
  *          RFC 3168, section 5
  *      </a>
  *
- * @param[in] hdr   Pointer to an IPv6 header.
+ * @param[out] hdr  Pointer to an IPv6 header.
  * @param[in] ecn   The new value for the 2-bit ECN part of the traffic class
  *                  field.
  */
@@ -201,7 +226,7 @@ static inline void ipv6_hdr_set_trafficclass_ecn(ipv6_hdr_t *hdr, uint8_t ecn)
  *          RFC 2474, section 3
  *      </a>
  *
- * @param[in] hdr   Pointer to an IPv6 header.
+ * @param[out] hdr  Pointer to an IPv6 header.
  * @param[in] dscp  The new value for the 6-bit DSCP part of the traffic class
  *                  field.
  */
@@ -220,7 +245,7 @@ static inline void ipv6_hdr_set_trafficclass_dscp(ipv6_hdr_t *hdr, uint8_t dscp)
  *
  * @return  Value of the traffic class field of *hdr*.
  */
-static inline uint8_t ipv6_hdr_get_trafficclass(ipv6_hdr_t *hdr)
+static inline uint8_t ipv6_hdr_get_trafficclass(const ipv6_hdr_t *hdr)
 {
     return ((((hdr->v_tc_fl.u8[0]) & 0x0f) << 4) |
             ((hdr->v_tc_fl.u8[1] & 0xf0) >> 4));
@@ -240,7 +265,7 @@ static inline uint8_t ipv6_hdr_get_trafficclass(ipv6_hdr_t *hdr)
  *
  * @return  Value of the ECN part of the traffic class field of *hdr*.
  */
-static inline uint8_t ipv6_hdr_get_trafficclass_ecn(ipv6_hdr_t *hdr)
+static inline uint8_t ipv6_hdr_get_trafficclass_ecn(const ipv6_hdr_t *hdr)
 {
     return (((hdr->v_tc_fl.u8[0]) & 0x0c) >> 2);
 }
@@ -260,7 +285,7 @@ static inline uint8_t ipv6_hdr_get_trafficclass_ecn(ipv6_hdr_t *hdr)
  *
  * @return  Value of the DSCP part of the traffic class field of *hdr*.
  */
-static inline uint8_t ipv6_hdr_get_trafficclass_dscp(ipv6_hdr_t *hdr)
+static inline uint8_t ipv6_hdr_get_trafficclass_dscp(const ipv6_hdr_t *hdr)
 {
     return ((((hdr->v_tc_fl.u8[0]) & 0x03) << 4) |
             ((hdr->v_tc_fl.u8[1] & 0xf0) >> 4));
@@ -269,7 +294,7 @@ static inline uint8_t ipv6_hdr_get_trafficclass_dscp(ipv6_hdr_t *hdr)
 /**
  * @brief   Sets the flow label field of *hdr*
  *
- * @param[in] hdr       Pointer to an IPv6 header.
+ * @param[out] hdr      Pointer to an IPv6 header.
  * @param[in] flowlabel The new value for the flow label field in host byte
  *                      order.
  */
@@ -287,10 +312,252 @@ static inline void ipv6_hdr_set_flowlabel(ipv6_hdr_t *hdr, uint32_t flowlabel)
  *
  * @return  Value of the flow label field of *hdr*.
  */
-static inline uint32_t ipv6_hdr_get_flowlabel(ipv6_hdr_t *hdr)
+static inline uint32_t ipv6_hdr_get_flowlabel(const ipv6_hdr_t *hdr)
 {
     return byteorder_ntohl(hdr->v_tc_fl) & 0x000fffff;
 }
+
+/**
+ * @brief   Data type to represent an IPv6 source routing header for RPL
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ */
+typedef struct __attribute__((packed)) {
+    uint8_t nextheader;             /**< type of next header in this packet. */
+    uint8_t hdrextlen;              /**< length of header in 8-octet units. */
+    uint8_t routing_type;           /**< identify srh-variant. */
+    uint8_t segments_left;          /**< remaining route segments before reaching
+                                      *  destination. */
+    network_uint32_t cmpr_pad_rsv;  /**< 4+4 bit, expressing prefix octets from each/last segment,
+                                      *  4-bit, expressing the number of octets
+                                      *  used for paddingafter addresses
+                                      *  + 20 bit reseveved */
+} ipv6_srh_t;
+
+/**
+ * @brief   Get CmprI field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[in] ipv6_srh  A source routing header.
+ *
+ * @return  The value of the CmprI field of *ipv6_srh*.
+ */
+static inline uint8_t ipv6_srh_get_cmpri(const ipv6_srh_t *ipv6_srh)
+{
+    return (ipv6_srh->cmpr_pad_rsv.u8[0] >> 4);
+}
+
+/**
+ * @brief   Set CmprI field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[out] ipv6_srh A source routing header.
+ * @param[in] cmpri     New value for the CmprI field.
+ */
+static inline void ipv6_srh_set_cmpri(ipv6_srh_t *ipv6_srh, uint8_t cmpri)
+{
+    ipv6_srh->cmpr_pad_rsv.u8[0] &= 0x0f;
+    ipv6_srh->cmpr_pad_rsv.u8[0] |= (cmpri << 4);
+}
+
+/**
+ * @brief   Get CmprE field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[in] ipv6_srh  A source routing header.
+ *
+ * @return  The value of the CmprE field of *ipv6_srh*.
+ */
+static inline uint8_t ipv6_srh_get_cmpre(const ipv6_srh_t *ipv6_srh)
+{
+    return (ipv6_srh->cmpr_pad_rsv.u8[0] & 0x0f);
+}
+
+/**
+ * @brief   Set CmprE field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[out] ipv6_srh A source routing header.
+ * @param[in] cmpre     New value for the CmprE field.
+ */
+static inline void ipv6_srh_set_cmpre(ipv6_srh_t *ipv6_srh, uint8_t cmpre)
+{
+    ipv6_srh->cmpr_pad_rsv.u8[0] &= 0xf0;
+    ipv6_srh->cmpr_pad_rsv.u8[0] |= (cmpre & 0x0f);
+}
+
+/**
+ * @brief   Get Pad field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[in] ipv6_srh  A source routing header.
+ *
+ * @return  The value of the Pad field of *ipv6_srh*.
+ */
+static inline uint8_t ipv6_srh_get_pad(const ipv6_srh_t *ipv6_srh)
+{
+    return (ipv6_srh->cmpr_pad_rsv.u8[1] >> 4);
+}
+
+/**
+ * @brief   Set Pad field of source routing header
+ *
+ * @see <a href="http://tools.ietf.org/html/rfc6554#section-3">
+ *          RFC 6554, section 3
+ *      </a>
+ *
+ * @param[out] ipv6_srh A source routing header.
+ * @param[in] pad       New value for the Pad field.
+ */
+static inline void ipv6_srh_set_pad(ipv6_srh_t *ipv6_srh, uint8_t pad)
+{
+    ipv6_srh->cmpr_pad_rsv.u8[1] &= 0x0f;
+    ipv6_srh->cmpr_pad_rsv.u8[1] |= (pad << 4);
+}
+
+/**
+ * @brief   Send IPv6 packet to *dest*.
+ *
+ * @param[in] dest              Destination of this packet.
+ * @param[in] next_headers      List of next headers.
+ * @param[in] payload           Payload of the packet.
+ * @param[in] payload_len       Length of *payload*.
+ *
+ * @return  *payload_length*, on success
+ * @return  -EHOSTUNREACH, if no route to the given dest could be obtained
+ * @return  -EINPROGRESS, In case of reactive routing: routing is going to try
+ *          to find a route
+ */
+static inline int ipv6_sendto(const ipv6_addr_t *dest, netdev_hlist_t *next_headers,
+                              void *payload, size_t payload_len)
+{
+    return netapi_send_data2(ipv6_pid, next_headers, (void *)dest,
+                             sizeof(ipv6_addr_t), payload, payload_len);
+}
+
+/**
+ * @brief   Determines if node is a router.
+ *
+ * @return  true, if node is a router
+ * @return  false, otherwise
+ */
+bool ipv6_is_router(void);
+
+/**
+ * @brief   Sets the default hop limit to use with IPv6 packets.
+ *
+ * @param[in] hop_limit The hop limit to set the default hop limit to.
+ *
+ * @return  0 on success
+ * @return  negative value on failure
+ */
+static inline int ipv6_set_default_hop_limit(uint8_t hop_limit)
+{
+    return netapi_set_option(ipv6_pid, IPV6_CONF_DEFAULT_HOP_LIMIT, &hop_limit,
+                             sizeof(hop_limit));
+}
+
+/**
+ * @brief   Gets the default hop limit that is used for IPv6 packets.
+ *
+ * @return  The current default hop limit for IPv6 packets
+ * @return  0, on error.
+ */
+static inline uint8_t ipv6_get_default_hop_limit(void)
+{
+    uint8_t hop_limit;
+
+    if (netapi_get_option(ipv6_pid, IPV6_CONF_DEFAULT_HOP_LIMIT, &hop_limit,
+                          sizeof(hop_limit) < 0) < 0) {
+        return 0;
+    }
+
+    return hop_limit;
+}
+
+/**
+ * @brief   Registers a handler thread for any incoming IPv6 packets.
+ *
+ * @param[in] pid   PID of handler thread.
+ *
+ * @return  0 on success
+ * @return  -ENOBUFS, if maximum number of registrable threads is exceeded.
+ */
+static inline int ipv6_register_packet_handler(kernel_pid_t pid)
+{
+    return netapi_register(ipv6_pid, pid, IPV6_PROTO_NUM_IPV6_ANY);
+}
+
+/**
+ * @brief   Registers a handler thread for L4 protocol.
+ *
+ * @param[in] next_header   Next header ID of the L4 protocol.
+ * @param[in] pid           PID of the handler thread
+ */
+static inline int ipv6_register_next_header_handler(uint8_t next_header,
+        kernel_pid_t pid)
+{
+    return netapi_register(ipv6_pid, pid, (netapi_reg_demux_ctx_t)next_header);
+}
+
+/**
+ * @brief   Registers a function that decides how to route incomming
+ *          IP packets with a destination that is not this interface.
+ *          The default behaviour is to try forwarding such packets to
+ *          the neighborhood.
+ *          Register a function to change the default behaviour.
+ *          Such function shall return the next hop to reach the destination
+ *          of the IP packet, or NULL if no such next hop is known.
+ *          In this case, the packet will be discarded.
+ *
+ * @param   rp  A routing provider.
+ *
+ * @return 0, on success
+ * @return -ENOBUFS, if there is already a routing provider registered.
+ */
+int ipv6_register_set_routing_provider(ipv6_routing_provider_t rp);
+
+/**
+ * @brief   Calculates the Internet Checksum of the IPv6 pseudo header based
+ *          on *ipv6_hdr*, *ul_packet_len*, and *next_header*.
+ *
+ * @see <a href="https://tools.ietf.org/html/rfc2460#section-8.1">
+ *          RFC 2460, section 8.1
+ *      </a>
+ *
+ * @param[in] ipv6_hdr      An IPv6 header to get the pseudo header data from
+ * @param[in] ul_packet_len ipv6_hdr::length in host byte order minus IPv6
+ *                          extension headers.
+ * @param[in] next_header   Next header value for upper layer. Might differ from
+ *                          ipv6_hdr::nextheader, when it is an IPv6 extension
+ *                          header
+ * @param[in] initial       Initial value for the checksum
+ *
+ * @return  The checksum IPv6 pseudo header with values from *ipv6_hdr*,
+ *          *ul_packet_len*, and *next_header* and intial value *initial*.
+ */
+uint16_t ipv6_pseudo_hdr_checksum(const ipv6_hdr_t *ipv6_hdr, uint32_t ul_packet_len,
+                                  uint8_t next_header, uint16_t initial);
+
+
+/* TODO: ICMPv6 option/header handler; IPv6 extension header handler? */
 
 #ifdef __cplusplus
 }
---
+++ sys/net/network_layer/ipv6/ipv6.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014 Martin Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for
+ * more details.
+ */
+
+/**
+ * @addtogroup  net_ipv6
+ * @{
+ *
+ * @file        ipv6.c
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ */
+
+#include "byteorder.h"
+#include "kernel_types.h"
+#include "net_help.h"
+
+#include "ipv6.h"
+
+kernel_pid_t ipv6_pid = KERNEL_PID_UNDEF;
+
+uint16_t ipv6_pseudo_hdr_checksum(const ipv6_hdr_t *ipv6_hdr, uint32_t ul_packet_len,
+                                  uint8_t next_header, uint16_t sum)
+{
+    network_uint32_t length, nh;
+    length = byteorder_htonl(ul_packet_len);
+    nh.u32 = 0;
+    nh.u8[3] = next_header;
+
+    sum = net_help_csum(sum, (uint8_t *)(&ipv6_hdr->srcaddr),
+                        2 * sizeof(ipv6_addr_t));
+    sum = net_help_csum(sum, (uint8_t *)(&length), sizeof(length));
+    sum = net_help_csum(sum, (uint8_t *)(&nh), sizeof(nh));
+
+    return sum;
+}
+
+/**
+ * @}
+ */
