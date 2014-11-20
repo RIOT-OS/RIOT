2e671b69e2a458124ae3d363820cf49ec9d11b44
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-11-20 16:36:31 +0100

    FIXUP: refactor struct tm parser

--- sys/shell/commands/sc_periph.c
+++ sys/shell/commands/sc_periph.c
@@ -35,35 +35,38 @@ static void _rtc_gettime(void)
     printf("%s", asctime(&now));
 }
 
+static int _parse_time(char **argv, struct tm *time)
+{
+    short i1, i2, i3;
+
+    if(sscanf(argv[0], "%6hd:%6hd:%6hd", &i1, &i2, &i3) != 3) {
+        puts("could not parse date");
+        return -1;
+    }
+
+    time->tm_year = i1 - 1900;
+    time->tm_mon = i2 - 1;
+    time->tm_mday = i3;
+
+    if(sscanf(argv[1], "%6hd:%6hd:%6hd", &i1, &i2, &i3) != 3) {
+        puts("could not parse time");
+        return -1;
+    }
+
+    time->tm_hour = i1;
+    time->tm_min = i2;
+    time->tm_sec = i3;
+
+    return 0;
+}
+
 static void _rtc_settime(char **argv)
 {
-    do {
-        short i1, i2, i3;
+    struct tm now;
 
-        int res = sscanf(argv[0], "%6hd-%6hd-%6hd", &i1, &i2, &i3);
-        if (res != 3) {
-            break;
-        }
-
-        struct tm now;
-        now.tm_year = i1 - 1900;
-        now.tm_mon = i2 - 1;
-        now.tm_mday = i3;
-
-        res = sscanf(argv[1], "%6hd:%6hd:%6hd", &i1, &i2, &i3);
-        if (res != 3) {
-            break;
-        }
-        now.tm_hour = i1;
-        now.tm_min = i2;
-        now.tm_sec = i3;
-
+    if (_parse_time(argv, &now) == 0) {
         rtc_set_time(&now);
-        puts("OK");
-        return;
-    } while (0);
-
-    printf("Usage: %s YYYY-MM-DD hh:mm:ss\n", argv[0]);
+    }
 }
 
 static void _rtc_usage(void)
@@ -73,6 +76,9 @@ static void _rtc_usage(void)
     puts("\tinit\t\tinitialize the interface");
     puts("\tpoweron\t\tpower the interface on");
     puts("\tpoweroff\tpower the interface off");
+    puts("\tclearalarm\t\tdeactivate the current alarm");
+    puts("\tgetalarm\t\tprint the currently alarm time");
+    puts("\tsetalarm YYYY-MM-DD HH:MM:SS\n\t\t\tset an alarm for the specified time");
     puts("\tgettime\t\tprint the current time");
     puts("\tsettime YYYY-MM-DD HH:MM:SS\n\t\t\tset the current time");
 }
@@ -90,6 +96,15 @@ void _rtc_handler(int argc, char **argv)
     }
     else if (strncmp(argv[1], "poweroff", 8) == 0) {
         rtc_poweroff();
+    }
+    else if (strncmp(argv[1], "clearalarm", 8) == 0) {
+        rtc_clear_alarm();
+    }
+    else if (strncmp(argv[1], "getalarm", 8) == 0) {
+        _rtc_getalarm();
+    }
+    else if (strncmp(argv[1], "setalarm", 8) == 0) {
+        _rtc_setalarm(argv + 2);
     }
     else if (strncmp(argv[1], "gettime", 7) == 0) {
         _rtc_gettime();
