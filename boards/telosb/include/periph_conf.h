ed54a5765a830dd177f58f3f1307fd797924c4a6
Oleg Hahm <oleg@hobbykeller.org>
2014-11-20 17:54:02 +0100

    boards: created periph-conf.h for remaining boards

---
+++ boards/telosb/include/periph_conf.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014 INRIA
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_telosb
+ * @{
+ *
+ * @file
+ * @brief       TelosB peripheral configuration
+ *
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#endif /* PERIPH_CONF_H */
