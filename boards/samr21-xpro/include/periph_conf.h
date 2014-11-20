d0f402c8b8bb0394075e8ef9e2ae32c1d88d741f
Rane Balslev <ranebalslev@gmail.com>
2014-11-20 20:21:13 +0100

    ADC implementation

--- boards/samr21-xpro/include/periph_conf.h
+++ boards/samr21-xpro/include/periph_conf.h
@@ -120,6 +120,119 @@ extern "C" {
 #define GPIO_3_EXTINT       3
 /** @} */
 
+/**
+ * @ ADC Configuration
+ * @{
+ */
+#define ADC_NUMOF			(1U)
+#define ADC_0_EN			1
+#define ADC_MAX_CHANNELS	8
+
+/* ADC 0 device configuration */
+#define ADC_0_DEV 			ADC
+#define ADC_0_IRQ			ADC_IRQn 	
+#define ADC_0_CHANNELS      8
+
+/* ADC 0 Default values */
+#define ADC_0_CLK_SOURCE	0 //GCLK_GENERATOR_0
+#define ADC_0_PRESCALER		ADC_CTRLB_PRESCALER_DIV4
+#define ADC_0_WINDOW_MODE	ADC_WINCTRL_WINMODE_DISABLE
+#define ADC_0_CORRECTION_EN 0 // disabled
+#define ADC_0_GAIN_CORRECTION	ADC_GAINCORR_RESETVALUE
+#define ADC_0_OFFSET_CORRECTION	ADC_OFFSETCORR_RESETVALUE
+#define ADC_0_SAMPLE_LENGTH	0
+#define ADC_0_PIN_SCAN_OFFSET_START	0 // disabled
+#define	ADC_0_PIN_SCAN_INPUT_TO_SCAN	0 // disabled 	
+#define ADC_0_LEFT_ADJUST	0 // disabled
+#define ADC_0_DIFFERENTIAL_MODE	0 // disabled
+#define ADC_0_FREE_RUNNING	0 // disabled
+#define ADC_0_EVENT_ACTION	0 // disabled 	
+#define ADC_0_RUN_IN_STANDBY	0 // disabled
+
+/* ADC 0 Module Status flags */
+#define ADC_0_STATUS_RESULT_READY	(1UL << 0)
+#define ADC_0_STATUS_WINDOW			(1UL << 1)
+#define ADC_0_STATUS_OVERRUN		(1UL << 2) 	
+
+/* ADC 0 Positive Input Pins */
+#define ADC_0_POS_INPUT		ADC_INPUTCTRL_MUXPOS_PIN6
+
+/* ADC 0 Negative Input Pins */ 	
+#define ADC_0_NEG_INPUT		ADC_INPUTCTRL_MUXNEG_GND
+
+/* ADC 0 Gain Factor */
+#define ADC_0_GAIN_FACTOR_1X	ADC_INPUTCTRL_GAIN_1X
+#define ADC_0_GAIN_FACTOR_2X	ADC_INPUTCTRL_GAIN_2X
+#define ADC_0_GAIN_FACTOR_4X	ADC_INPUTCTRL_GAIN_4X
+#define ADC_0_GAIN_FACTOR_8X	ADC_INPUTCTRL_GAIN_8X
+#define ADC_0_GAIN_FACTOR_16X	ADC_INPUTCTRL_GAIN_16X
+#define ADC_0_GAIN_FACTOR_DEFAULT	ADC_0_GAIN_FACTOR_1X
+
+/* ADC 0 Resolutions */
+//#define ADC_0_RES_6BIT // NOT POSSIBLE! 	
+#define ADC_0_RES_8BIT	ADC_CTRLB_RESSEL_8BIT
+#define ADC_0_RES_10BIT ADC_CTRLB_RESSEL_10BIT
+#define ADC_0_RES_12BIT	ADC_CTRLB_RESSEL_12BIT
+//#define ADC_0_RES_14BIT // NOT POSSIBLE!
+#define ADC_0_RES_16BIT	ADC_CTRLB_RESSEL_16BIT 
+
+/* ADC 0 Voltage reference */
+#define ADC_0_REF_INT_1V	ADC_REFCTRL_REFSEL_INT1V
+#define ADC_0_REF_EXT_B		ADC_REFCTRL_REFSEL_AREFB
+#define ADC_0_REF_COM_EN	0 // default
+#define ADC_0_REF_DEFAULT	ADC_0_REF_INT_1V // Use this to define the value used
+
+/* ADC 0 ACCUMULATE */
+#define ADC_0_ACCUM_DISABLE	ADC_AVGCTRL_SAMPLENUM_1
+#define ADC_0_ACCUM_2		ADC_AVGCTRL_SAMPLENUM_2
+#define ADC_0_ACCUM_4		ADC_AVGCTRL_SAMPLENUM_4
+#define ADC_0_ACCUM_8		ADC_AVGCTRL_SAMPLENUM_8
+#define ADC_0_ACCUM_16		ADC_AVGCTRL_SAMPLENUM_16
+#define ADC_0_ACCUM_32		ADC_AVGCTRL_SAMPLENUM_32
+#define ADC_0_ACCUM_64		ADC_AVGCTRL_SAMPLENUM_64
+#define ADC_0_ACCUM_128		ADC_AVGCTRL_SAMPLENUM_128
+#define ADC_0_ACCUM_256		ADC_AVGCTRL_SAMPLENUM_256
+#define ADC_0_ACCUM_512		ADC_AVGCTRL_SAMPLENUM_512
+#define ADC_0_ACCUM_1024	ADC_AVGCTRL_SAMPLENUM_1024
+#define ADC_0_ACCUM_DEFAULT	ADC_0_ACCUM_1024 // ADC_0_ACCUM_DISABLE // Use this to define the value used
+
+/* ADC 0 DEVIDE RESULT */
+#define ADC_0_DIV_RES_DISABLE	0
+#define ADC_0_DIV_RES_2			1
+#define ADC_0_DIV_RES_4			2
+#define ADC_0_DIV_RES_8			3
+#define ADC_0_DIV_RES_16		4
+#define ADC_0_DIV_RES_32		5
+#define ADC_0_DIV_RES_64		6
+#define ADC_0_DIV_RES_128		7
+#define ADC_0_DIV_RES_DEFAULT	ADC_0_DIV_RES_DISABLE // Use this to define the value used
+	
+/* ADC 0 channel 0 pin config */
+#define ADC_0_CH0           PORT_PB08B_ADC_AIN2//10
+#define ADC_0_CH0_PIN       PIN_PB08B_ADC_AIN2//0
+/* ADC 0 channel 1 pin config */
+#define ADC_0_CH1           PORT_PB09B_ADC_AIN3//11
+#define ADC_0_CH1_PIN       PIN_PB09B_ADC_AIN3//1
+/* ADC 0 channel 2 pin config */
+#define ADC_0_CH2       	PORT_PA04B_ADC_AIN4//12
+#define ADC_0_CH2_PIN      	PIN_PA04B_ADC_AIN4//2
+/* ADC 0 channel 3 pin config */
+#define ADC_0_CH3           PORT_PA05B_ADC_AIN5//13
+#define ADC_0_CH3_PIN       PIN_PA05B_ADC_AIN5//3
+/* ADC 0 channel 4 pin config */
+#define ADC_0_CH4           PORT_PA06B_ADC_AIN6//14
+#define ADC_0_CH4_PIN       PIN_PA06B_ADC_AIN6//4
+/* ADC 0 channel 5 pin config */
+#define ADC_0_CH5           PORT_PA07B_ADC_AIN7//15
+#define ADC_0_CH5_PIN       PIN_PA07B_ADC_AIN7//5
+/* ADC 0 channel 6 pin config */
+#define ADC_0_CH6           PORT_PB00B_ADC_AIN8//14
+#define ADC_0_CH6_PIN       PIN_PB00B_ADC_AIN8//4
+/* ADC 0 channel 7 pin config */
+#define ADC_0_CH7           PORT_PB02B_ADC_AIN10//15
+#define ADC_0_CH7_PIN       PIN_PB02B_ADC_AIN10//5
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
