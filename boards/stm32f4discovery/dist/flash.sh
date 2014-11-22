bea0171754adbd9808b3a93295a5fbbdc679866c
Christian Mehlis <mehlis@inf.fu-berlin.de>
2014-11-23 00:16:52 +0100

    stm32f4discovery: use openocd for flashing

---
+++ boards/stm32f4discovery/dist/flash.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+openocd -f "board/stm32f4discovery.cfg" \
+    -c 'tcl_port 0' \
+    -c 'gdb_port 0' \
+    -c 'telnet_port 0' \
+    -c "init" \
+    -c "targets" \
+    -c "reset" \
+    -c "halt" \
+    -c "flash protect 0 0 11 off" \
+    -c "flash erase_address 0x08000000 0x100000" \
+    -c "flash write_image erase $1 0 ihex" \
+    -c "verify_image $1" \
+    -c "reset run"\
+    -c "shutdown"
