12bb9a1d997303a6b94cc5097bf8eda3c075e2d6
Christian Mehlis <mehlis@inf.fu-berlin.de>
2014-11-25 00:24:57 +0100

    added new board nucleo-f411

---
+++ boards/nucleo-f411/board.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     board_nucleo-f411
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the STM nucleo f411 evaluation board
+ *
+ * @author      Christian Mehlis <mehlis@inf.fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "cpu.h"
+
+static void leds_init(void);
+
+void board_init(void)
+{
+    /* initialize the boards LEDs, this is done first for debugging purposes */
+    leds_init();
+
+    /* initialize the CPU */
+    cpu_init();
+}
+
+/**
+ * @brief Initialize the boards on-board LEDs (LD3 and LD4)
+ *
+ * The LED initialization is hard-coded in this function. As the LEDs are soldered
+ * onto the board they are fixed to their CPU pins.
+ *
+ * The LEDs are connected to the following pins:
+ * - LED: PA05
+ */
+static void leds_init(void)
+{
+    /* enable clock for port GPIOA */
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    /* configure pins as general outputs */
+    LED_PORT->MODER &= ~(0xff000000);
+    LED_PORT->MODER |= 0x55000000;
+    /* set output speed high-speed */
+    LED_PORT->OSPEEDR |= 0xff000000;
+    /* set output type to push-pull */
+    LED_PORT->OTYPER &= ~(0xf000);
+    /* disable pull resistors */
+    LED_PORT->PUPDR &= ~(0xff000000);
+
+    /* turn all LEDs off */
+    LED_PORT->BSRRH = 0xf000;
+}
---
+++ boards/nucleo-f411/dist/debug.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+openocd -f "board/st_nucleo_f411re.cfg" \
+    -c "tcl_port 6333" \
+    -c "telnet_port 4444" \
+    -c "init" \
+    -c "targets" \
+    -c "reset halt" \
+    -l /dev/null &
+
+# save pid to terminate afterwards
+OCD_PID=$?
+
+# needed for openocd to set up
+sleep 2
+
+arm-none-eabi-gdb -tui -command=${1} ${2}
+
+kill ${OCD_PID}
