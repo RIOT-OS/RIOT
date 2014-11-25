d9480cadfcac071f69ac5ee463f778a9f3039ae1
Fabian Nack <fabiannack@web.de>
2014-11-25 12:02:52 +0100

    sys - nhdp: Add nhdp impl

---
+++ sys/net/routing/nhdp/lib_table.c
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     nhdp
+ * @{
+ *
+ * @file
+ * @brief       Local Information Base implementation for NHDP
+ *
+ * @author      Fabian Nack <nack@inf.fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "mutex.h"
+#include "utlist.h"
+#include "kernel_types.h"
+
+#include "rfc5444/rfc5444_iana.h"
+#include "rfc5444/rfc5444_writer.h"
+
+#include "lib_table.h"
+#include "nhdp_address.h"
+#include "nhdp_writer.h"
+
+/* Internal variables */
+static mutex_t mtx_lib_access = MUTEX_INIT;
+static lib_entry_t *lib_entry_head = NULL;
+
+/* Internal function prototypes */
+static int create_if_entry(kernel_pid_t if_pid, nhdp_addr_t *addr);
+static int add_address_to_if(lib_entry_t *if_entry, nhdp_addr_t *addr);
+
+
+/*---------------------------------------------------------------------------*
+ *                       Local Information Base API                          *
+ *---------------------------------------------------------------------------*/
+
+int lib_add_if_addr(kernel_pid_t if_pid, nhdp_addr_t *addr)
+{
+    lib_entry_t *lib_elt;
+    nhdp_addr_entry_t *addr_elt;
+    int result = -1;
+
+    mutex_lock(&mtx_lib_access);
+
+    /* Check whether the given interface is already registered */
+    LL_FOREACH(lib_entry_head, lib_elt) {
+        if (lib_elt->if_pid == if_pid) {
+            LL_FOREACH(lib_entry_head->if_addr_list_head, addr_elt) {
+                if (addr_elt->address == addr) {
+                    /* Address already known for the interface */
+                    result = 0;
+                    break;
+                }
+            }
+
+            if (result) {
+                /* Existing interface entry, but new address */
+                result = add_address_to_if(lib_elt, addr);
+                break;
+            }
+        }
+    }
+
+    if (result) {
+        /* New interface, create a lib entry */
+        result = create_if_entry(if_pid, addr);
+    }
+
+    mutex_unlock(&mtx_lib_access);
+
+    return result;
+}
+
+void lib_rem_if(kernel_pid_t if_pid)
+{
+    lib_entry_t *lib_elt, *lib_tmp;
+
+    mutex_lock(&mtx_lib_access);
+
+    LL_FOREACH_SAFE(lib_entry_head, lib_elt, lib_tmp) {
+        if (lib_elt->if_pid == if_pid) {
+            nhdp_free_addr_list(lib_elt->if_addr_list_head);
+            LL_DELETE(lib_entry_head, lib_elt);
+            free(lib_elt);
+            break;
+        }
+    }
+
+    mutex_unlock(&mtx_lib_access);
+}
+
+void lib_fill_wr_addresses(kernel_pid_t if_pid, struct rfc5444_writer *wr)
+{
+    lib_entry_t *lib_elt;
+    nhdp_addr_entry_t *add_tmp;
+
+    mutex_lock(&mtx_lib_access);
+
+    /* First fill the list for LOCAL_IF = THIS_IF */
+    LL_FOREACH(lib_entry_head, lib_elt) {
+        if (lib_elt->if_pid == if_pid) {
+            LL_FOREACH(lib_elt->if_addr_list_head, add_tmp) {
+                nhdp_writer_add_addr(wr, add_tmp->address,
+                                     RFC5444_ADDRTLV_LOCAL_IF, RFC5444_LOCALIF_THIS_IF);
+                add_tmp->address->in_tmp_table = NHDP_ADDR_TMP_ANY;
+            }
+            break;
+        }
+    }
+
+    /* Second fill the list for LOCAL_IF = OTHER_IF */
+    LL_FOREACH(lib_entry_head, lib_elt) {
+        if (lib_elt->if_pid != if_pid) {
+            LL_FOREACH(lib_elt->if_addr_list_head, add_tmp) {
+                /* Check if this address is not already included in a list */
+                if (!NHDP_ADDR_TMP_IN_ANY(add_tmp->address)) {
+                    /* Address can be added */
+                    nhdp_writer_add_addr(wr, add_tmp->address,
+                                         RFC5444_ADDRTLV_LOCAL_IF, RFC5444_LOCALIF_OTHER_IF);
+                    add_tmp->address->in_tmp_table = NHDP_ADDR_TMP_ANY;
+                }
+            }
+        }
+    }
+
+    mutex_unlock(&mtx_lib_access);
+}
+
+uint8_t lib_is_reg_addr(kernel_pid_t if_pid, nhdp_addr_t *addr)
+{
+    lib_entry_t *lib_elt;
+    nhdp_addr_entry_t *addr_elt;
+
+    LL_FOREACH(lib_entry_head, lib_elt) {
+        LL_FOREACH(lib_elt->if_addr_list_head, addr_elt) {
+            if (addr_elt->address == addr) {
+                if (lib_elt->if_pid == if_pid) {
+                    /* Given address is assigned to the given IF */
+                    return 1;
+                }
+
+                /* Given address is assigned to any other IF */
+                return 2;
+            }
+        }
+    }
+    return 0;
+}
+
+
+/*------------------------------------------------------------------------------------*/
+/*                                Internal functions                                  */
+/*------------------------------------------------------------------------------------*/
+
+/**
+ * Create an entry for a newly registered interface
+ */
+static int create_if_entry(kernel_pid_t if_pid, nhdp_addr_t *addr)
+{
+    lib_entry_t *new_entry;
+
+    new_entry = (lib_entry_t *) malloc(sizeof(lib_entry_t));
+
+    if (!new_entry) {
+        /* Insufficient memory */
+        return -1;
+    }
+
+    new_entry->if_addr_list_head = NULL;
+    new_entry->if_pid = if_pid;
+
+    if (add_address_to_if(new_entry, addr)) {
+        /* Insufficient memory */
+        free(new_entry);
+        return -1;
+    }
+
+    LL_PREPEND(lib_entry_head, new_entry);
+
+    return 0;
+}
+
+/**
+ * Add another address to an interface entry
+ */
+static int add_address_to_if(lib_entry_t *if_entry, nhdp_addr_t *addr)
+{
+    nhdp_addr_entry_t *new_entry = (nhdp_addr_entry_t *) malloc(sizeof(nhdp_addr_entry_t));
+
+    if (!new_entry) {
+        /* Insufficient memory */
+        return -1;
+    }
+
+    /* Increment usage counter of address in central NHDP address storage */
+    addr->usg_count++;
+    new_entry->address = addr;
+    LL_PREPEND(if_entry->if_addr_list_head, new_entry);
+
+    return 0;
+}
---
+++ sys/net/routing/nhdp/nhdp.h
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    nhdp NHDP
+ * @ingroup     net
+ * @brief       The MANET Neighborhood Discovery Protocol (RFC 6130)
+ * @{
+ *
+ * @file
+ * @brief       Interface for core functionality of NHDP
+ *
+ * @author      Fabian Nack <nack@inf.fu-berlin.de>
+ */
+
+#ifndef NHDP_H_
+#define NHDP_H_
+
+#include "timex.h"
+#include "kernel_types.h"
+#include "socket_base/socket.h"
+
+#include "rfc5444/rfc5444_writer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief Definition for RIOTs debugging option */
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+/**
+ * @name    NHDP protocol macros
+ *
+ * @{
+ */
+#ifndef MANET_PORT
+/** @brief Well-known MANET port from RFC 5498 */
+#define MANET_PORT                  (269)
+#endif
+
+/** @brief Stack size for NHDP thread */
+#if ENABLE_DEBUG
+#define NHDP_STACK_SIZE      (KERNEL_CONF_STACKSIZE_DEFAULT + KERNEL_CONF_STACKSIZE_PRINTF)
+#else
+#define NHDP_STACK_SIZE      (KERNEL_CONF_STACKSIZE_DEFAULT)
+#endif
+
+/** @brief Buffer size in bytes for NHDP writer's msg buffer */
+#define NHDP_WR_MSG_BUF_SIZE        (256)
+/** @brief Buffer size in bytes for NHDP writer's tlv buffer */
+#define NHDP_WR_TLV_BUF_SIZE        (512)
+
+/* TODO: Determine a good value */
+/** @brief Queue size for msg queue of NHDP */
+#define NHDP_MSG_QUEUE_SIZE         (16)
+
+/** @brief Maximum size of handled RFC5444 packets */
+#define NHDP_MAX_RFC5444_PACKET_SZ  (128)
+
+/** @brief Default hello interval in milliseconds */
+#define NHDP_DEFAULT_HELLO_INT_MS   (2000)
+/** @brief Default hold time in milliseconds */
+#define NHDP_DEFAULT_HOLD_TIME_MS   (3 * NHDP_DEFAULT_HELLO_INT_MS)
+
+/**
+ * @brief   Initial pending flag value for new link tuples
+ *
+ *          Do not change (link quality currently not considered)
+ */
+#define NHDP_INITIAL_PENDING        (0)
+
+/** @brief Maximum jitter for nhdp messages in milliseconds */
+#define NHDP_HP_MAXJITTER_MS        (200)
+
+#define NHDP_L_HOLD_TIME_MS         (NHDP_DEFAULT_HOLD_TIME_MS)
+#define NHDP_N_HOLD_TIME_MS         (NHDP_DEFAULT_HOLD_TIME_MS)
+#define NHDP_I_HOLD_TIME_MS         (NHDP_DEFAULT_HOLD_TIME_MS)
+/** @} */
+
+/**
+ * @brief   MANET interface representation
+ */
+typedef struct nhdp_if_entry_t {
+    kernel_pid_t if_pid;                        /**< PID of the interface's handling thread */
+    vtimer_t if_timer;                          /**< Vtimer used for the periodic signaling */
+    timex_t hello_interval;                     /**< Interval time for periodic HELLOs */
+    timex_t validity_time;                      /**< Validity time for propagated information */
+    struct rfc5444_writer_target *wr_target;    /**< Interface specific writer target */
+    struct nhdp_if_entry_t *next;               /**< Pointer to next list entry */
+} nhdp_if_entry_t;
+
+/**
+ * @brief   Additional address types for link layer operation
+ */
+enum nhdp_address_type_t {
+    AF_CC110X = AF_MAX + 3,
+};
+
+/**
+ * @brief                   Initialize NHDP for operation
+ *
+ * Sets up NHDP's reader and writer. Call first before starting NHDP's thread
+ * and registering interfaces.
+ */
+void nhdp_init(void);
+
+/**
+ * @brief                   Start NHDP's operational thread
+ *
+ * @return                  PID of NHDP's operational thread
+ * @return                  KERNEL_PID_UNDEF on error
+ */
+kernel_pid_t nhdp_start(void);
+
+/**
+ * @brief                   Register an interface for NHDP operation with default values
+ *
+ * Registers the interface completely for NHDP operation. Registration includes a new
+ * Local Information Base entry, a new Interface Information Base and starting
+ * the periodical HELLO messaging.
+ *
+ * @note
+ * Default values are a hello interval of 2 seconds and a validity time
+ * of 6 seconds for propagated information.
+ *
+ * @param[in] if_pid        PID of the interface
+ * @param[in] addr          A local address of this interface represented in bytes
+ * @param[in] addr_size     Length in bytes of the local address
+ * @param[in] addr_type     AF type of the local address
+ * @param[in] max_pl_size   Maximum payload size for packets send over this interface
+ *
+ * @return                  0 on success
+ * @return                  -1 on error
+ * @return                  -2 on maximum number of interfaces registered
+ */
+int nhdp_register_if_default(kernel_pid_t if_pid, uint8_t *addr, size_t addr_size,
+                             uint8_t addr_type, uint16_t max_pl_size);
+
+/**
+ * @brief                   Register an interface for NHDP operation
+ *
+ * Registers the interface completely for NHDP operation. Registration includes a new
+ * Local Information Base entry, a new Interface Information Base and starting
+ * the periodical HELLO messaging.
+ *
+ * @param[in] if_pid        PID of the interface
+ * @param[in] addr          A local address of this interface represented in bytes
+ * @param[in] addr_size     Length of the local address (number of bytes)
+ * @param[in] addr_type     AF type of the given address
+ * @param[in] max_pl_size   Maximum payload size for packets send over this interface
+ * @param[in] hello_int_ms  Hello interval in ms for periodic message generation
+ * @param[in] val_time_ms   Validity time in ms for propagated information
+ *
+ * @return                  0 on success
+ * @return                  -1 on error
+ * @return                  -2 on maximum number of interfaces registered
+ */
+int nhdp_register_if(kernel_pid_t if_pid, uint8_t *addr, size_t addr_size, uint8_t addr_type,
+                     uint16_t max_pl_size, uint16_t hello_int_ms, uint16_t val_time_ms);
+
+/**
+ * @brief                   Register a non MANET interface in NHDP
+ *
+ * The registered interface does not take part in NHDP operation. Its addresses
+ * will be represented only in other interfaces' messages. No periodical messages
+ * are created for this interface and no message processing is done.
+ *
+ * @param[in] if_pid        PID of the interface
+ * @param[in] addr          A local address of this interface represented in bytes
+ * @param[in] addr_size     Length of the local address (number of bytes)
+ * @param[in] addr_type     AF type of the given address
+ *
+ * @return                  0 on success
+ * @return                  -1 on error
+ */
+int nhdp_register_non_manet_if(kernel_pid_t if_pid, uint8_t *addr, size_t addr_size,
+                               uint8_t addr_type);
+
+/**
+ * @brief                   Register an additional address for an interface in NHDP
+ *
+ * The address is added to the Local Information Base entry of this interface and
+ * will afterwards be propagated as a local address in newly created HELLO messages.
+ *
+ * @param[in] if_pid        PID of the interface
+ * @param[in] addr          Additional local address of this interface represented in bytes
+ * @param[in] addr_size     Length of the local address (number of bytes)
+ * @param[in] addr_type     AF type of the given address
+ *
+ * @return                  0 on success
+ * @return                  -1 on error
+ */
+int nhdp_add_address(kernel_pid_t if_pid, uint8_t *addr, size_t addr_size, uint8_t addr_type);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NHDP_H_ */
+/** @} */
