c17ddbcb9d619c18733bb174dda0815d2112b978
Troels Hoffmeyer <troels.d.hoffmeyer@gmail.com>
2014-11-25 13:26:27 +0100

    Backup of sms

--- examples/beehive/sms.c
+++ examples/beehive/sms.c
@@ -10,8 +10,10 @@
 #include "periph/uart.h"
 #include "ringbuffer.h"
 #include "vtimer.h"
-static char gsm_rx_buf_mem[128];
+#include <stdio.h>
+static char gsm_rx_buf_mem[256];
 static ringbuffer_t gsm_rx_buf;
+static char ctrl_z[] = {0x1a, '\0'};
 void rx_callback(void *arg, char data)
 {
 	ringbuffer_add_one(&gsm_rx_buf, data);
@@ -20,13 +22,24 @@ void sms_init(void)
 {
 	ringbuffer_init(&gsm_rx_buf, gsm_rx_buf_mem, 128);
 	uart_init(UART_1, 115200, rx_callback, 0, 0);
-	send_command_string(CIPSHUT);
-	send_command_string(CSTT);
-	send_command_string(CIICR);
-	send_command_string(CIFSR);
-	//send_command_string(CGDCONT);
-	//send_command_string(CIPSTART);
+	vtimer_usleep(100000);
 
+    // do{
+    // 	send_command(ACTIVATE, strlen(ACTIVATE));
+    //     vtimer_usleep(5000);
+    // } while (wait_for_string(OK) == -1);
+
+	// send_command_string(CIPSHUT);
+	// send_command_string("AT+CGACT=0,1\r\n");
+	// send_command_string(CGATT);
+	// send_command_string(CGDCONT);
+	// send_command(CSTT, strlen(CSTT));
+	// wait_for_string("OK");
+	// send_command_string(CGACT);
+	// wait_for_string("OK");
+	// send_command(CIICR, strlen(CIICR));
+	// wait_for_string("OK");
+	// send_command(CIFSR, strlen(CIFSR));
 }
 void send_to_uart1(int argc, char **argv)
 {
@@ -39,14 +52,42 @@ void send_to_uart1(int argc, char **argv)
 }
 void tcp_send(int argc, char** argv)
 {
-	send_command_string("AT+CIPSEND=20\r\n");
+	// send_command_string(CIPSTART);
+	// wait_for_string("CONNECT OK");
+	// send_command_string("AT+CIPSEND\r\n");
+	// send_command_string("GET ");
+	// send_command_string(URL);
+	// send_command_string(" HTTP:/1.1\r\n");
+	// //send_command_string("Host: beehivemonitor.azurewebsites.net\r\n");
+	// //send_command_string("Connection: keep-alive\r\n");
+	// send_command(ctrl_z,2);
+	// //send_command_string("AT+CIPCLOSE\r\n");
+  	send_command(SETCONTYPE, strlen(SETCONTYPE));
+	wait_for_string(OK);
+    send_command(SETAPN, strlen(SETAPN));
+	 wait_for_string(OK);
+    send_command(SETUSER, strlen(SETUSER));
+	wait_for_string(OK);
+    send_command(SETPWD, strlen(SETPWD));
+	wait_for_string(OK);
+	send_command(ACTIVATE, strlen(ACTIVATE));
+	wait_for_string(OK);
+
+	send_command_string("AT+HTTPINIT\r\n");
+	send_command_string("AT+HTTPPARA=\"CID\",1\r\n");
+	send_command_string("AT+HTTPPARA=\"URL\",\"");
+	send_command_string(URL);
+	send_command_string("\"");
+	send_command_string("\r\n");
+	send_command_string("AT+HTTPACTION=0\r\n");
+	send_command_string("AT+HTTPTERM\r\n");
+
+
 }
 void set_message_format(void)
 {
-	char message[] = "AT+CMGF=1\r";
-	send_command(message, strlen(message));
-	vtimer_usleep(1000);
-	print_buffer();
+	char message[] = "AT+CMGF=1\r\n";
+	send_command_string(message);
 }
 void sms_send(int argc, char **argv)
 {
@@ -55,11 +96,10 @@ void sms_send(int argc, char **argv)
   	end[1]='\0';
   	set_message_format();
 	char start[]   = "AT+CMGS=\"30622610\"\r";
-	send_command(start, strlen(start));
-	vtimer_usleep(1000);
-	send_command(argv[1], strlen(argv[1]));
+	send_command_string(start);
+	send_command_string(argv[1]);
 	send_command(end, 2);
-	send_command("\r\n", 2);
+	//send_command("\r\n", 2);
 	vtimer_usleep(1000);
 	print_buffer();
 
@@ -67,16 +107,21 @@ void sms_send(int argc, char **argv)
 //void init_tcp_connection(ip address, )
 void post_http(int argc, char **argv)
 {
-	send_command_string(CREG);
-	send_command_string(SAPBR);
-	send_command_string(WAIT);
-	send_command_string(HTTPINIT);
-	send_command_string(URL);
-	send_command_string(CID);
-	send_command_string(HTTPACTION);
-	send_command_string(HTTPREAD);
-	send_command_string(WAIT);
-	send_command_string(TERMINATE);
+	send_command_string("AT+CDNSGIP=");
+	send_command_string("\"");
+	send_command_string(argv[1]);
+	send_command_string("\"");
+	send_command_string("\r\n");
+	// send_command_string(CREG);
+	// send_command_string(SAPBR);
+	// send_command_string(WAIT);
+	// send_command_string(HTTPINIT);
+	// send_command_string(URL);
+	// send_command_string(CID);
+	// send_command_string(HTTPACTION);
+	// send_command_string(HTTPREAD);
+	// send_command_string(WAIT);
+	// send_command_string(TERMINATE);
 }
 void get_imei(int argc, char **argv)
 {
@@ -105,12 +150,56 @@ void print_buffer(void)
 		printf("%c", ringbuffer_get_one(&gsm_rx_buf));
 	}
 }
+/* DOES NOT USE TIMEOUT YET */
+void sendATAndWait(char* command, char* waitString, int timeout)
+{
+	send_command(command, strlen(command));
+	wait_for_string(waitString);
+}
+int wait_for_string(char* string, int timeout)
+{
+	int length = strlen(string);
+	int i;
+	char value;
+	int timeout_counter = 0;
+	while(1)
+	{
+		if(ringbuffer_empty(&gsm_rx_buf))
+		{
+			timeout_counter++;
+			vtimer_usleep(10000); //Change?
+			if(timeout_counter > timeout)
+			{
+				DEBUG("NOT FOUND\r\n");
+				return -1;
+			}
+			continue;
+		}
+		value = ringbuffer_get_one(&gsm_rx_buf);
+		DEBUG("%c", value);
+		if(value != string[i])
+		{
+			i = 0;
+		}
+		else
+		{
+			i++;
+		}
+		if(i >= length)
+		{
+			return 1;
+		}
+	}
+	return -1;
+}
 int wait_for_ok(void)
 {
 	while(!ringbuffer_empty(&gsm_rx_buf))
 	{
 		vtimer_usleep(1000);
-		if(ringbuffer_get_one(&gsm_rx_buf) == 'O')
+		char value = ringbuffer_get_one(&gsm_rx_buf);
+		DEBUG("%c", value);
+		if(value == 'O')
 		{
 			if(ringbuffer_get_one(&gsm_rx_buf) == 'K')
 			{
--- examples/beehive/sms.h
+++ examples/beehive/sms.h
@@ -5,12 +5,18 @@ extern "C" {
 #define SMS_H_
 
 #define CIPSHUT "AT+CIPSHUT\r\n"
-#define CGDCONT "AT+CGDCONT=1,\"IP\",internet\r\n"
+#define CGATT   "AT+CGATT=1\r\n"
+#define CGDCONT "AT+CGDCONT=1,\"IP\",\"internet\"\r\n"
+#define CGACT   "AT+CGACT=1,1\r\n"
 #define CSTT "AT+CSTT=\"internet\",\"\",\"\"\r\n"
 #define CIICR "AT+CIICR\r\n"
 #define CIFSR "AT+CIFSR\r\n"
-#define CIPSTART "AT+CIPSTART=\"TCP\",\"128.199.36.229\",3333\r\n"
-
+#define CIPSTART "AT+CIPSTART=\"TCP\",\"beehivemonitor.azurewebsites.net\",80\r\n" //191.236.16.12//128.199.36.229//beehivemonitor.azurewebsites.net
+#define URL2   "/RestApi/PutData%%3fborderrouter%%3d1%%26node%%3d1%%26sensor%%3dtroels%%26value%%3d1%%26timestamp%%3dnow"
+#define URL   "http://beehivemonitor.azurewebsites.net/RestApi/PutData?borderrouter=1&node=1&sensor=troels&value=1&timestamp=now"
+#define URL3   "http://beehivemonitor.azurewebsites.net/RestApi/GetData"
+#define DOMAIN "www.google.com"
+//http://beehivemonitor.azurewebsites.net/
 void rx_cb(void *arg, char data);
 void sms_init(void);
 void set_message_format(void);
@@ -23,15 +29,22 @@ void print_buffer(void);
 void send_to_uart1(int argc, char **argv);
 
 void tcp_send(int argc, char** argv);
-int wait_for_ok(void);
+int wait_for_string(char* string, int timeout);
+void sendATAndWait(char* command, char* waitString, int timeout);
 
-/* Http */
-
+#define OK "OK"
+/* SAPBR */
+#define SETCONTYPE "AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"\r\n"
+#define SETAPN     "AT+SAPBR=3,1,\"APN\",\"internet\"\r\n"
+#define SETUSER    "AT+SAPBR=3,1,\"USER\",\"\"\r\n"
+#define SETPWD     "AT+SAPBR=3,1,\"PWD\",\"\"\r\n"
+#define ACTIVATE     "AT+SAPBR=1,1\r\n"
+/* Http  */
 #define CREG     "AT+CREG?\r\n"
 #define SAPBR    "AT+SAPBR=1,1\r\n"
 #define WAIT     "WAIT=6\r\n"
 #define HTTPINIT "AT+HTTPINIT\r\n"
-#define URL		 "AT+HTTPPARA=\"URL\", \"http://www.m2msupport.net/m2msupport/http_get_test.php\" \r\n"
+//#define URL		 "AT+HTTPPARA=\"URL\", \"http://www.m2msupport.net/m2msupport/http_get_test.php\"\r\n"
 #define CID      "AT+HTTPPARA=\"CID\",1\r\n"
 #define HTTPACTION   "AT+HTTPACTION=0\r\n"
 #define HTTPREAD     "AT+HTTPREAD\r\n"
