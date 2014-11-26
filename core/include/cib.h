8a9220a85c80e25bf86bb0150d545021333f37b4
René Kijewski <rene.kijewski@fu-berlin.de>
2014-11-26 18:04:54 +0100

    core: cib size must not exceed MAXINT/2

--- core/include/cib.h
+++ core/include/cib.h
@@ -41,7 +41,7 @@ typedef struct {
  *
  * @param[out] cib      Buffer to initialize.
  *                      Must not be NULL.
- * @param[in]  size     Size of the buffer.
+ * @param[in]  size     Size of the buffer, must not exceed MAXINT/2.
  */
 static inline void cib_init(cib_t *__restrict cib, unsigned int size)
 {
