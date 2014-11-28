4ebeee758245e62af925fe857f41a177247489f0
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2014-11-28 18:02:49 +0100

    dist/tools/cppcheck: add license

--- dist/tools/cppcheck/check.sh
+++ dist/tools/cppcheck/check.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Copyright 2014 Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+# Copyright 2014 Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
 # Don't show warnings about unusedStructMembers by default
 DEFAULT_SUPPRESSIONS="${1}"
 if echo "${DEFAULT_SUPPRESSIONS}" | grep -q '^--show-unused-struct'; then
