4ae819c011228f1b84bbafef50b0f3f6005dc445
Joakim Gebart <joakim.gebart@eistec.se>
2015-01-12 14:42:48 +0100

    mulle: Initial import of Eistec Mulle board.
    Initially supports only Mulles with serial number > 220 (due to missing
    MK60DN256ZVLL10 support in k60).
    
    See also: https://github.com/RIOT-OS/RIOT/wiki/Board%3A-Mulle
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

---
+++ boards/mulle/board_config.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Eistec AB
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup board_mulle
+ * @{
+ */
+
+/**
+ * @file
+ * @brief       Mulle config module implementation
+ *
+ * @author      Joakim Gebart <joakim.gebart@eistec.se>
+ *
+ * @note Waiting for PR #2353 (NVRAM API) before implementing this.
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "config.h"
+
+void config_load(void)
+{
+    /* TODO: Implement */
+}
+
+uint8_t config_save(void)
+{
+    /* TODO: Implement */
+    return 0;
+}
---
+++ boards/mulle/dist/gdb.conf
@@ -0,0 +1,4 @@
+set architecture arm
+set arm force-mode thumb
+set remote hardware-breakpoint-limit 6
+set remote hardware-watchpoint-limit 4
