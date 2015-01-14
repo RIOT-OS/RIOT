eeb28bd70d26356269d0a9d58bbc960be42a4286
Joakim Gebart <joakim.gebart@eistec.se>
2015-01-14 12:12:03 +0100

    cortex-m3_common: Atomic operations without disabling interrupts.
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- cpu/cortex-m3_common/atomic_arch.c
+++ cpu/cortex-m3_common/atomic_arch.c
@@ -1,32 +1,37 @@
 /*
  * Copyright (C) 2014 Freie Universität Berlin
  *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
  */
 
 /**
  * @ingroup     cpu_cortex-m3
  * @{
  *
- * @file        atomic_arch.c
+ * @file
  * @brief       Implementation of the kernels atomic interface
  *
  * @author      Stefan Pfeiffer <stefan.pfeiffer@fu-berlin.de>
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Joakim Gebart <joakim.gebart@eistec.se>
  *
  * @}
  */
 
 #include "arch/atomic_arch.h"
-#include "irq.h"
+#include "cpu.h"
 
 unsigned int atomic_arch_set_return(unsigned int *to_set, unsigned int value)
 {
-    disableIRQ();
-    unsigned int old = *to_set;
-    *to_set = value;
-    enableIRQ();
+    int status;
+    unsigned int old;
+    do {
+        /* Load exclusive */
+        old = __LDREXW((volatile uint32_t*)to_set);
+        /* Try to write the new value */
+        status = __STREXW(value, (volatile uint32_t*)to_set);
+    } while (status != 0); /* retry until load-store cycle was exclusive. */
     return old;
 }
