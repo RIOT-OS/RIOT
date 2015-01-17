e6c21e2fad21103ca4b3e1cb585892f907575b9d
Frank Holtz <frank-github2013@holtznet.de>
2015-01-17 16:37:09 +0100

    nRF51 support

--- cpu/nrf51822/periph/flash.c
+++ cpu/nrf51822/periph/flash.c
@@ -49,8 +49,6 @@ static uint8_t flash_check_address(void *address)
     return FLASH_ERROR_SUCCESS;
 }
 
-
-
 /************************************************************************/
 /* periph/flash.h *******************************************************/
 /************************************************************************/
@@ -81,54 +79,133 @@ void *flash_get_address(flash_page_number_t page)
 
 uint8_t flash_memcpy(void *dest, const void *src, size_t n)
 {
-    /* Check alignment */
-    flash_page_size_t page_offset;
-    flash_get_page_number(dest, &page_offset);
+    DEBUG("flash_memcpy dest=%d, src=%d, n=%d\n", (size_t)dest, (size_t)src, n);
+    
+    /* Variables */
 
-    if ((n % FLASH_WRITE_ALIGN != 0) || (page_offset % FLASH_WRITE_ALIGN != 0)) {
-        DEBUG("Unaligned access n=%d offset=%d\n", n, page_offset);
-        return FLASH_ERROR_ALIGNMENT;
+    /* temp */
+    uint8_t temp;
+
+    /* Flag is switched on verify error */
+    bool writeverify = true;
+
+    /* Buffer for unaligned access*/
+    flash_data_word_t dstbuffer;
+
+    /* Align dest */
+    flash_data_word_t *mydst_start =
+        (flash_data_word_t *)((size_t)dest & (FLASH_ERASED_WORD_VALUE - FLASH_WRITE_ALIGN + 1));
+    flash_data_word_t *mydst_end =
+        (flash_data_word_t *)((size_t)(dest + n) &
+                              (FLASH_ERASED_WORD_VALUE - FLASH_WRITE_ALIGN +1));
+
+    /* prepare src to read bytewise */
+    uint8_t *mysrc = (uint8_t *)src;
+
+    /* Nothing to do */
+    if (n == 0) {
+        return (FLASH_ERROR_SUCCESS);
     }
 
-    /* write data to flash */
-    uint8_t ret = flash_memcpy_fast(dest, src, n);
+    /* Check destination memory range: start */
+    temp = flash_check_address((uint8_t *) mydst_start);
 
-    /* verify written data if write was successfull */
-    if (ret == FLASH_ERROR_SUCCESS) {
-        while (n > 0) {
-            if (*((flash_data_word_t *) dest) != *((flash_data_word_t *) src)) {
-                return FLASH_ERROR_VERIFY;
+    if (temp > FLASH_ERROR_SUCCESS) {
+        DEBUG("attempted to write below first address\n");
+        return temp;
+    }
+
+    /* Check destination memory range: end */
+    temp = flash_check_address((uint8_t *) mydst_end-1);
+
+    if (temp > FLASH_ERROR_SUCCESS) {
+        DEBUG("attemted to write beyond last address\n");
+        return temp;
+    }
+
+    /* Enable writing to flash */
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+
+    /* move mysrc down to count of unaligned dst bytes */
+    mysrc -= (uint8_t *)dest - (uint8_t *)mydst_start;
+
+    for (flash_data_word_t *i = mydst_start; i <= mydst_end && n > 0; i++) {
+        dstbuffer = FLASH_ERASED_WORD_VALUE;
+
+        /* copy data into dstbuffer */
+        for (temp = 0; temp < FLASH_WRITE_ALIGN; temp++) {
+            /* check address range */
+            if ((mysrc >= (uint8_t *)src) && (n > 0)) {
+                n--;
+                /* for every data word byte (little endian)*/
+                switch (temp) {
+                    case 0:
+                        *((uint8_t *)&dstbuffer) = *mysrc;
+                        break;
+
+                    case 1:
+                        *((uint8_t *)&dstbuffer + 1) = *mysrc;
+                        break;
+
+                    case 2:
+                        *((uint8_t *)&dstbuffer + 2) = *mysrc;
+                        break;
+
+                    default:
+                        *((uint8_t *)&dstbuffer + 3) = *mysrc;
+                        break;
+                }
             }
+            mysrc++;
+        }
 
-            dest += FLASH_WRITE_ALIGN;
-            src  += FLASH_WRITE_ALIGN;
-            n    -= FLASH_WRITE_ALIGN;
+        /* new data -> write
+         * change & to | when erased bit value is 0 */
+        if ( *i != (dstbuffer & *i)) {
+            /* write data */
+            *i = dstbuffer;
+
+            /* wait until write operation is completed */
+            while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+
+            /* verify data */
+            if (*i != dstbuffer) {
+                /* set error flag */
+                writeverify = false;
+            }
         }
     }
 
-    return (ret);
+    /* Disable writing to flash */
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+
+    while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+
+    if (writeverify) {
+        return (FLASH_ERROR_SUCCESS);
+    }
+    else {
+        return (FLASH_ERROR_VERIFY);
+    }
 }
 
-/* TODO: Fix it. Jumps into Hard Fault when src is Pointer to char* */
 uint8_t flash_memcpy_fast(void *dest, const void *src, size_t n)
 {
-    /* Check memory range */
-    uint8_t ret = flash_check_address((uint8_t *) dest);
-
-    if (ret > FLASH_ERROR_SUCCESS) {
-        DEBUG("attempted to write below first address\n");
-        return ret;
-    }
-
-    ret = flash_check_address((((uint8_t *) dest) + n));
-
-    if (ret > FLASH_ERROR_SUCCESS) {
-        DEBUG("attemted to write beyond last address\n");
-        return ret;
-    }
-
     flash_data_word_t *mydst = (flash_data_word_t *)dest;
     flash_data_word_t *mysrc = (flash_data_word_t *)src;
+
+    /* Prevent hard fault: Is src and dst aligned ? */
+    /* Check alignment */
+    if ((n % FLASH_WRITE_ALIGN != 0) ||
+        ((size_t)mysrc % FLASH_WRITE_ALIGN != 0) ||
+        ((size_t)mydst % FLASH_WRITE_ALIGN != 0)) {
+        DEBUG("Unaligned access dest=%d, src=%d, n=%d\n", (size_t)mydst, (size_t)mysrc, n);
+        return FLASH_ERROR_ALIGNMENT;
+    }
+
+    return (0);
 
     /* Enable writing to flash */
     NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
@@ -139,20 +216,21 @@ uint8_t flash_memcpy_fast(void *dest, const void *src, size_t n)
     n = n / FLASH_WRITE_ALIGN;
 
     while (n--) {
+        /* write data */
         *mydst = *mysrc;
-        mydst += FLASH_WRITE_ALIGN;
-        mysrc += FLASH_WRITE_ALIGN;
+
+        /* increment pointer by 4 on Cortex-M0 */
+        mydst++;
+        mysrc++;
 
         /* wait until write operation is completed */
         while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
-
     }
 
     /* Disable writing to flash */
     NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
 
     while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
-
 
     return FLASH_ERROR_SUCCESS;
 }
