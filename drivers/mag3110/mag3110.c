0a9e837dba14e737352750491002c31e073d9ce9
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2015-01-28 14:42:26 +0100

    drivers/mag3110: Acquire exclusive access to I2C bus

--- drivers/mag3110/mag3110.c
+++ drivers/mag3110/mag3110.c
@@ -15,12 +15,14 @@
  * @brief       Driver for the Freescale MAG3110 magnetometer.
  *
  * @author      Johann Fischer <j.fischer@phytec.de>
+ * @author      Peter Kietzmann <peter.kietzmann@haw-hamburg.de>
  *
  * @}
  */
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "mutex.h"
 #include "periph/i2c.h"
 #include "mag3110.h"
 #include "mag3110_reg.h"
@@ -34,9 +36,14 @@ int mag3110_test(mag3110_t *dev)
 {
     char reg;
 
+    /* Acquire exclusive access to the bus. */
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_WHO_AM_I, &reg, 1) != 1) {
+        /* Release the bus for other threads. */
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     if (reg != MAG3110_ID) {
         return -1;
@@ -58,10 +65,13 @@ int mag3110_init(mag3110_t *dev, i2c_t i2c, uint8_t address, uint8_t dros)
         return -1;
     }
 
+    i2c_acquire(dev->i2c);
     /* initialize the I2C bus */
     if (i2c_init_master(i2c, I2C_SPEED) < 0) {
+        i2c_release(dev->i2c);
         return -2;
     }
+    i2c_release(dev->i2c);
 
     if (mag3110_test(dev)) {
         return -3;
@@ -70,15 +80,19 @@ int mag3110_init(mag3110_t *dev, i2c_t i2c, uint8_t address, uint8_t dros)
     /* enable automatic magnetic sensor reset */
     reg = MAG3110_CTRL_REG2_AUTO_MRST_EN;
 
+    i2c_acquire(dev->i2c);
     if (i2c_write_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG2, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -4;
     }
 
     reg = MAG3110_CTRL_REG1_DROS(dros);
 
     if (i2c_write_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG1, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -4;
     }
+    i2c_release(dev->i2c);
 
     dev->initialized = true;
 
@@ -96,9 +110,12 @@ int mag3110_set_user_offset(mag3110_t *dev, int16_t x, int16_t y, int16_t z)
     buf[4] = (char)(z >> 8);
     buf[5] = (char)z;
 
+    i2c_acquire(dev->i2c);
     if (i2c_write_regs(dev->i2c, dev->addr, MAG3110_OFF_X_MSB, buf, 6) != 6) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     return 0;
 }
@@ -111,15 +128,19 @@ int mag3110_set_active(mag3110_t *dev)
         return -1;
     }
 
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG1, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
 
     reg |= MAG3110_CTRL_REG1_AC;
 
     if (i2c_write_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG1, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     return 0;
 }
@@ -128,15 +149,19 @@ int mag3110_set_standby(mag3110_t *dev)
 {
     char reg;
 
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG1, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
 
     reg &= ~MAG3110_CTRL_REG1_AC;
 
     if (i2c_write_regs(dev->i2c, dev->addr, MAG3110_CTRL_REG1, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     return 0;
 }
@@ -149,9 +174,12 @@ int mag3110_is_ready(mag3110_t *dev)
         return -1;
     }
 
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_DR_STATUS, &reg, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     return (int)(reg & MAG3110_DR_STATUS_ZYXDR);
 }
@@ -164,9 +192,12 @@ int mag3110_read(mag3110_t *dev, int16_t *x, int16_t *y, int16_t *z, uint8_t *st
         return -1;
     }
 
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_DR_STATUS, buf, 7) != 7) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     *status = buf[0];
     *x = ((int16_t)buf[1] << 8) | buf[2];
@@ -182,9 +213,12 @@ int mag3110_read_dtemp(mag3110_t *dev, int8_t *dtemp)
         return -1;
     }
 
+    i2c_acquire(dev->i2c);
     if (i2c_read_regs(dev->i2c, dev->addr, MAG3110_DIE_TEMP, (char *)dtemp, 1) != 1) {
+        i2c_release(dev->i2c);
         return -1;
     }
+    i2c_release(dev->i2c);
 
     return 0;
 }
