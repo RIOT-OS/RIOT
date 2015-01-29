dd45333a09e81cee90ff2e5f7916d61a9ef45ea3
Thomas Eichinger <thomas.eichinger1@gmail.com>
2015-01-29 14:48:20 +0100

    SQUASHME: Fixed issues and omitted parameters

--- boards/stm32f3discovery/dist/debug.sh
+++ boards/stm32f3discovery/dist/debug.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-if [ ! -f "$2" ]; then
-    echo "ELF-file $2 does not exist"
-    exit 1
-fi
+echo "##"
+echo "## Debugging ${ELFFILE}"
+echo "##"
 
 # if GDB_PORT does not exist or holds empty string
 if [ -z ${GDB_PORT} ]; then
-    GDB_PORT=0
+    # set to gdb standard port
+    GDB_PORT=3333
+else
+    echo "Listening for GDB connection on port ${GDB_PORT}"
 fi
-
-echo "##"
-echo "## Debugging $2"
-echo "##"
 
 openocd -f "board/stm32f3discovery.cfg" \
     -c "tcl_port 6333" \
     -c "telnet_port 4444" \
-    -c "gdb_port ${GDB_PORT}"
+    -c "gdb_port ${GDB_PORT}" \
     -c "init" \
     -c "targets" \
     -c "reset halt" \
@@ -29,6 +27,6 @@ OCD_PID=$?
 # needed for openocd to set up
 sleep 2
 
-arm-none-eabi-gdb -tui -command=$1 $2
+arm-none-eabi-gdb -tui -command=${RIOTBOARD}/${BOARD}/dist/gdb.conf ${ELFFILE}
 
 kill ${OCD_PID}
--- boards/stm32f3discovery/dist/flash.sh
+++ boards/stm32f3discovery/dist/flash.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "##"
-echo "## Flashing $1"
+echo "## Flashing ${HEXFILE}"
 echo "##"
 
 openocd -f "board/stm32f3discovery.cfg" \
-    -c "init" \
     -c "tcl_port 0" \
     -c "telnet_port 0" \
     -c "gdb_port 0" \
+    -c "init" \
     -c "targets" \
     -c "reset halt" \
-    -c "program $1 0x08000000 verify" \
+    -c "program ${HEXFILE} 0x08000000 verify" \
     -c "reset run"\
     -c "shutdown"
