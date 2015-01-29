ae8c7607ead55a2c93e7d778dea4204d806c9834
Joakim Gebart <joakim.gebart@eistec.se>
2015-01-29 10:14:49 +0100

    cortex-m0: Add extern "C" to core_cmFunc.h
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- cpu/cortex-m0_common/include/core_cmFunc.h
+++ cpu/cortex-m0_common/include/core_cmFunc.h
@@ -38,6 +38,9 @@
 #ifndef __CORE_CMFUNC_H
 #define __CORE_CMFUNC_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 /* ###########################  Core Function Access  ########################### */
 /** \ingroup  CMSIS_Core_FunctionInterface
@@ -633,5 +636,9 @@ __attribute__( ( always_inline ) ) __STATIC_INLINE void __set_FPSCR(uint32_t fps
 #endif
 
 /*@} end of CMSIS_Core_RegAccFunctions */
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __CORE_CMFUNC_H */
