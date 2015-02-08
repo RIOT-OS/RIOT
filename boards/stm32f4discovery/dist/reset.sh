d973729e13851903a46f191c44c7414203846ba7
haukepetersen <hauke.petersen@fu-berlin.de>
2015-02-08 14:46:53 +0100

    board/stm32f4discovery: use unified openocd script

--- boards/stm32f4discovery/dist/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-openocd -f "board/stm32f4discovery.cfg" \
-    -c 'tcl_port 0' \
-    -c 'gdb_port 0' \
-    -c 'telnet_port 0' \
-    -c "init" \
-    -c "reset run" \
-    -c "shutdown"
