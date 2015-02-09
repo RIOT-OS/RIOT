cd711ea9348b2ee4867832c238b45595086ec45a
Martine Lenders <mail@martine-lenders.eu>
2015-02-09 16:13:04 +0100

    unittests: add ng_netreg tests

---
+++ tests/unittests/tests-netreg/tests-netreg.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Martin Lenders
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file        tests-netreg.h
+ * @brief       Unittests for the ``netreg`` module
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ */
+#ifndef __TESTS_NETREG_H_
+#define __TESTS_NETREG_H_
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_netreg(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TESTS_NETREG_H_ */
+/** @} */
