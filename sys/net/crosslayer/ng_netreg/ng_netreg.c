ec5f686cd0c2b23a1236e6a3c12392378cde199e
Martine Lenders <mail@martine-lenders.eu>
2015-02-13 09:58:42 +0100

    netreg: move to pointer-based API
    Moving to a pointer-based API simplifies a lot, since there is no need
    for an internal pool of entries anymore. Therefore, a lot of
    organizational overhead and some restrictions (e.g. an upper limit on
    entries) are dropped.
    
    The entries can be stored in the stack of the respective thread.

--- sys/net/crosslayer/ng_netreg/ng_netreg.c
+++ sys/net/crosslayer/ng_netreg/ng_netreg.c
@@ -25,90 +25,33 @@
 /* The registry as lookup table by ng_nettype_t */
 static ng_netreg_entry_t *netreg[NG_NETTYPE_NUMOF - 1]; /* leave out NG_NETTYPE_UNDEF */
 
-/* unstructured pool of entries, marked as unclaimed by
- * netreg_entries[i].pid == KERNEL_PID_UNDEF */
-static ng_netreg_entry_t netreg_entries[NG_NETREG_SIZE];
-
 void ng_netreg_init(void)
 {
     /* set all pointers in registry to NULL */
     memset(netreg, 0, NG_NETTYPE_NUMOF * sizeof(ng_netreg_entry_t *));
-
-    for (int i = 0; i < NG_NETREG_SIZE; i++) {
-        netreg_entries[i].pid = KERNEL_PID_UNDEF;   /* mark entry pool unclaimed */
-    }
 }
 
-static inline int _netreg_entry_equal(ng_netreg_entry_t *a, ng_netreg_entry_t *b)
+int ng_netreg_register(ng_nettype_t type, ng_netreg_entry_t *entry)
 {
-    /* to be pluggable with utlist, so result must be 0 when match */
-    return ((a->pid != b->pid) || (a->demux_ctx != b->demux_ctx));
-}
-
-int ng_netreg_register(ng_nettype_t type, uint16_t demux_ctx, kernel_pid_t pid)
-{
-    ng_netreg_entry_t *entry = NULL;
-
     if (_INVALID_TYPE(type)) {
         return -EINVAL;
     }
 
-    for (int i = 0; i < NG_NETREG_SIZE; i++) {  /* Search unclaimed entry */
-        if (netreg_entries[i].pid == KERNEL_PID_UNDEF) {
-            entry = &(netreg_entries[i]);
-            break;
-        }
-    }
-
-    if (entry == NULL) {
-        return -ENOMEM;
-    }
-
-    entry->demux_ctx = demux_ctx;
-    entry->pid = pid;
-
-    if (netreg[type - 1] == NULL) {
-        netreg[type - 1] = entry;
-    }
-    else {
-        ng_netreg_entry_t *dup;
-
-        /* search for duplicates to new entry */
-        LL_SEARCH(netreg[type - 1], dup, entry, _netreg_entry_equal);
-
-        if (dup == NULL) {  /* new entry is not a duplicate, add to registry */
-            LL_PREPEND(netreg[type - 1], entry);
-        }
-        else {  /* new entry is a duplicate, mark as unclaimed again */
-            entry->pid = KERNEL_PID_UNDEF;
-        }
-    }
+    LL_PREPEND(netreg[type - 1], entry);
 
     return 0;
 }
 
-void ng_netreg_unregister(ng_nettype_t type, uint16_t demux_ctx, kernel_pid_t pid)
+void ng_netreg_unregister(ng_nettype_t type, ng_netreg_entry_t *entry)
 {
-    ng_netreg_entry_t *entry = netreg[type - 1];
-
     if (_INVALID_TYPE(type)) {
         return;
     }
 
-    while (entry != NULL) {
-        /* Find entry with given parameters in registry */
-        if ((entry->pid == pid) && (entry->demux_ctx == demux_ctx)) {
-            LL_DELETE(netreg[type - 1], entry);
-            entry->pid = KERNEL_PID_UNDEF;
-
-            return;
-        }
-
-        entry = entry->next;
-    }
+    LL_DELETE(netreg[type - 1], entry);
 }
 
-ng_netreg_entry_t *ng_netreg_lookup(ng_nettype_t type, uint16_t demux_ctx)
+ng_netreg_entry_t *ng_netreg_lookup(ng_nettype_t type, uint32_t demux_ctx)
 {
     ng_netreg_entry_t *res;
 
@@ -121,7 +64,7 @@ ng_netreg_entry_t *ng_netreg_lookup(ng_nettype_t type, uint16_t demux_ctx)
     return res;
 }
 
-int ng_netreg_num(ng_nettype_t type, uint16_t demux_ctx)
+int ng_netreg_num(ng_nettype_t type, uint32_t demux_ctx)
 {
     int num = 0;
     ng_netreg_entry_t *entry;
@@ -145,7 +88,7 @@ int ng_netreg_num(ng_nettype_t type, uint16_t demux_ctx)
 
 ng_netreg_entry_t *ng_netreg_getnext(ng_netreg_entry_t *entry)
 {
-    uint16_t demux_ctx;
+    uint32_t demux_ctx;
 
     if (entry == NULL) {
         return NULL;
