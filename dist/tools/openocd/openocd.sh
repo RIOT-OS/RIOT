aedaa10f9f1198a214710b2611f33b47c7627da6
Oleg Hahm <oleg@hobbykeller.org>
2015-02-19 18:10:24 +0100

    SQUASH ME: reset before exit

--- dist/tools/openocd/openocd.sh
+++ dist/tools/openocd/openocd.sh
@@ -124,8 +124,7 @@ do_flash() {
             -c "init" \
             -c "targets" \
             -c "reset halt" \
-            -c "program ${HEXFILE} verify exit" \
-            -c "reset run"
+            -c "program ${HEXFILE} verify reset exit"
 }
 
 do_debug() {
