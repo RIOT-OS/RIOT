e9e9a1c01c55f075ef1fd26ba88fd69e22e64c12
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2015-02-24 00:27:58 +0100

    travis: fixed pep8 code style issues in python support script

--- dist/tools/travis-scripts/get-pkg-list.py
+++ dist/tools/travis-scripts/get-pkg-list.py
@@ -25,35 +25,36 @@ msp_mcu_groups = ["msp430"]
 x86_mcu_groups = ["x86"]
 avr8_mcu_groups = ["avr8"]
 static_tests_groups = ["static-tests"]
-known_mcu_groups = arm_mcu_groups + msp_mcu_groups + x86_mcu_groups + avr8_mcu_groups + \
-                   static_tests_groups
+known_mcu_groups = arm_mcu_groups + msp_mcu_groups + x86_mcu_groups + \
+    avr8_mcu_groups + static_tests_groups
 
 common_pkgs = ["pcregrep", "libpcre3", "python3"]
-arm_pkgs    = ["gcc-arm-none-eabi"]
-msp_pkgs    = ["gcc-msp430"]
-x86_pkgs    = ["qemu-system-x86", "g++-multilib", "gcc-multilib", \
-               "build-essential"]
-avr8_pkgs   = ["gcc-avr", "binutils-avr", "avr-libc"]
+arm_pkgs = ["gcc-arm-none-eabi"]
+msp_pkgs = ["gcc-msp430"]
+x86_pkgs = ["qemu-system-x86", "g++-multilib", "gcc-multilib",
+            "build-essential"]
+avr8_pkgs = ["gcc-avr", "binutils-avr", "avr-libc"]
 static_tests_pkgs = ["doxygen", "cppcheck"]
-all_mcu_pkgs    = arm_pkgs + msp_pkgs + x86_pkgs + avr8_pkgs + static_tests_pkgs
+all_mcu_pkgs = arm_pkgs + msp_pkgs + \
+    x86_pkgs + avr8_pkgs + static_tests_pkgs
 
 
 pkgs_to_install = common_pkgs
 if "BUILDTEST_MCU_GROUP" in os.environ:
-	mcu_group = os.environ["BUILDTEST_MCU_GROUP"]
-	if mcu_group not in known_mcu_groups:
-		pkgs_to_install += all_mcu_pkgs
-	elif mcu_group in arm_mcu_groups:
-		pkgs_to_install += arm_pkgs
-	elif mcu_group in msp_mcu_groups:
-		pkgs_to_install += msp_pkgs
-	elif mcu_group in x86_mcu_groups:
-		pkgs_to_install += x86_pkgs
-	elif mcu_group in avr8_mcu_groups:
-		pkgs_to_install += avr8_pkgs
-	elif mcu_group in static_tests_groups:
-		pkgs_to_install += static_tests_pkgs
+    mcu_group = os.environ["BUILDTEST_MCU_GROUP"]
+    if mcu_group not in known_mcu_groups:
+        pkgs_to_install += all_mcu_pkgs
+    elif mcu_group in arm_mcu_groups:
+        pkgs_to_install += arm_pkgs
+    elif mcu_group in msp_mcu_groups:
+        pkgs_to_install += msp_pkgs
+    elif mcu_group in x86_mcu_groups:
+        pkgs_to_install += x86_pkgs
+    elif mcu_group in avr8_mcu_groups:
+        pkgs_to_install += avr8_pkgs
+    elif mcu_group in static_tests_groups:
+        pkgs_to_install += static_tests_pkgs
 else:
-	pkgs_to_install += all_mcu_pkgs
+    pkgs_to_install += all_mcu_pkgs
 
 print " ".join(pkgs_to_install)
