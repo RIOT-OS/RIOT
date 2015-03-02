40f7227c02ec1f6f3316e1ae01d6eeb2cd06a9d1
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-03-02 18:41:37 +0100

    sys/shell: added (ng_)netif shell commands

--- sys/shell/commands/shell_commands.c
+++ sys/shell/commands/shell_commands.c
@@ -150,6 +150,14 @@ extern void _mersenne_init(int argc, char **argv);
 extern void _mersenne_get(int argc, char **argv);
 #endif
 
+#if defined(MODULE_NG_NETIF) && defined(MODULE_NG_NETAPI) && defined(MODULE_NG_PKTBUF)
+void _netif_list(int argc, char **argv);
+void _netif_send(int argc, char **argv);
+void _netif_addr(int argc, char **argv);
+void _netif_chan(int argc, char **argv);
+void _netif_pan(int argc, char **argv);
+#endif
+
 const shell_command_t _shell_command_list[] = {
     {"reboot", "Reboot the node", _reboot_handler},
 #ifdef MODULE_CONFIG
@@ -242,6 +250,13 @@ const shell_command_t _shell_command_list[] = {
 #endif
 #ifdef CPU_X86
     {"lspci", "Lists PCI devices", _x86_lspci},
+#endif
+#if defined(MODULE_NG_NETIF) && defined(MODULE_NG_NETAPI) && defined(MODULE_NG_PKTBUF)
+    {"netif_list", "list network devices", _netif_list },
+    {"netif_send", "send raw data", _netif_send },
+    {"netif_addr", "get/set link layer address", _netif_addr },
+    {"netif_chan", "get/set channel (for radio devices)", _netif_chan },
+    {"netif_pan", "get/set PAN (for some radios)", _netif_pan },
 #endif
     {NULL, NULL, NULL}
 };
