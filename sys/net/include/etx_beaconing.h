688f721f725c58c3b5ed645703c15979b7b6a2b2
kausthub <kausthubnaarayan@gmail.com>
2015-03-09 23:57:12 +0530

    Update etx_beaconing.h

--- sys/net/include/etx_beaconing.h
+++ sys/net/include/etx_beaconing.h
@@ -21,9 +21,7 @@
 #define ETX_BEACONING_H_
 #include <stdint.h>
 #include "sixlowpan.h"
-/**
-  * makes a function-name in C++ have 'C' linkage so that client C code can link to (i.e use) your function using a 'C' compatible header  file that contains just the declaration of your function
-  */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -43,8 +41,6 @@ extern "C" {
 #else
 #define ETX_MAX_CANDIDATE_NEIGHBORS (40)
 #endif
-/**ETX Interval parameters
-  */
 #define MS  (1000)
 
 /*
@@ -55,15 +51,15 @@ extern "C" {
  * and 5 (For ETX_MAX_JITTER) unless those values are adjusted too.
  */
 #define ETX_INTERVAL        (1000)
-#define ETX_WINDOW          (10)                    /** 10 is the default value */
-#define ETX_BEST_CANDIDATES (15)                    /** Sent only 15 candidates in a beaconing packet */
-#define ETX_TUPLE_SIZE      (2)                     /** 1 Byte for Addr, 1 Byte for packets rec. */
-#define ETX_PKT_REC_OFFSET  (ETX_TUPLE_SIZE - 1)    /** Offset in a tuple of (addr,pkt_rec), will always be the last byte */
-#define ETX_IPV6_LAST_BYTE  (15)                    /** The last byte for an ipv6 address */
-#define ETX_MAX_JITTER      (ETX_INTERVAL / 5)      /** The default value is 20% of ETX_INTERVAL */
-#define ETX_JITTER_MOD      (ETX_MAX_JITTER + 1)    /** The modulo value for jitter computation */
-#define ETX_DEF_JIT_CORRECT (ETX_MAX_JITTER / 2)    /** Default Jitter correction value (normally ETX_MAX_JITTER / 2) */
-#define ETX_CLOCK_ADJUST    (52500)                 /** Adjustment for clockthread computations to stay close/near ETX_INTERVAL */
+#define ETX_WINDOW          (10)                    /**< 10 is the default value */
+#define ETX_BEST_CANDIDATES (15)                    /**< Sent only 15 candidates in a beaconing packet */
+#define ETX_TUPLE_SIZE      (2)                     /**< 1 Byte for Addr, 1 Byte for packets rec. */
+#define ETX_PKT_REC_OFFSET  (ETX_TUPLE_SIZE - 1)    /**< Offset in a tuple of (addr,pkt_rec), will always be the last byte */
+#define ETX_IPV6_LAST_BYTE  (15)                    /**< The last byte for an ipv6 address */
+#define ETX_MAX_JITTER      (ETX_INTERVAL / 5)      /**< The default value is 20% of ETX_INTERVAL */
+#define ETX_JITTER_MOD      (ETX_MAX_JITTER + 1)    /**< The modulo value for jitter computation */
+#define ETX_DEF_JIT_CORRECT (ETX_MAX_JITTER / 2)    /**< Default Jitter correction value (normally ETX_MAX_JITTER / 2) */
+#define ETX_CLOCK_ADJUST    (52500)                 /**< Adjustment for clockthread computations to stay close/near ETX_INTERVAL */
 
 /*
  * The ETX beaconing packet consists of:
@@ -97,52 +93,43 @@ typedef struct __attribute__((packed)) etx_probe_t {
 } etx_probe_t;
 
 typedef struct etx_neighbor_t {
-    ipv6_addr_t addr;           /** The address of this node */
-    uint8_t     tx_cur_round;   /** The indicator for receiving a packet from this candidate this round */
-    uint8_t     packets_tx[ETX_WINDOW]; /** The packets this node has transmitted TO ME */
-    uint8_t     packets_rx;     /** The packets this node has received FROM ME */
-    double      cur_etx;        /** The currently calculated ETX-value */
-    uint8_t     used;           /** The indicator if this node is active or not */
+    ipv6_addr_t addr;                   /**< The address of this node */
+    uint8_t     tx_cur_round;           /**< The indicator for receiving a packet from this candidate this round */
+    uint8_t     packets_tx[ETX_WINDOW]; /**< The packets this node has transmitted TO ME */
+    uint8_t     packets_rx;             /**< The packets this node has received FROM ME */
+    double      cur_etx;                /**< The currently calculated ETX-value */
+    uint8_t     used;                   /**< The indicator if this node is active or not */
 } etx_neighbor_t;
 
-//prototypes
-
  /**
-* This function is used to initialise and accepts one parameter . 
-* @param address a ipv6_addr_t pointer.
-* @see etx_get_metric()
-* @see etx_update()
-* @return It does not return any value.
-*/
+   * Is used to initialise. 
+   * @param[in] address a ipv6_addr_t pointer.
+   */
 void etx_init_beaconing(ipv6_addr_t *address);
 
  /**
-* This function is used to get the calculated ETX-value.
-* @param address a ipv6_addr_t pointer.
-* @see etx_init_beaconing()
-* @see etx_update()
-* @return It returns the calculated ETX-value.
-*/
+   * Is used to get the calculated ETX-value.
+   * @param[in] address a ipv6_addr_t pointer.
+   * @return the calculated ETX-value.
+   */
 double etx_get_metric(ipv6_addr_t *address);
 
  /**
-* This function is used to Update the current ETX value of a candidate.
-* @param neighbour an etx_neighbour_t pointer.
-* @see etx_init_beaconing()
-* @see etx_get_metric()
-* @return It does not return any value.
-*/
+   * Is used to Update the current ETX value of a candidate.
+   * @param[in] neighbour an etx_neighbour_t pointer.
+   */
 void etx_update(etx_neighbor_t *neighbor);
 
-#define ETX_PKT_OPT         (0)     /** Position of Option-Type-Byte */
-#define ETX_PKT_OPTVAL      (0x20)  /** Non-standard way of saying this is an ETX-Packet. */
-#define ETX_PKT_LEN         (1)     /** Position of Length-Byte */
-#define ETX_DATA_MAXLEN     (30)    /** max length of the data */
-#define ETX_PKT_HDR_LEN     (2)     /** Option type + Length (1 Byte each) */
-#define ETX_PKT_DATA        (2)     /** Begin of Data Bytes */
+#define ETX_PKT_OPT         (0)     /**< Position of Option-Type-Byte */
+#define ETX_PKT_OPTVAL      (0x20)  /**< Non-standard way of saying this is an ETX-Packet. */
+#define ETX_PKT_LEN         (1)     /**< Position of Length-Byte */
+#define ETX_DATA_MAXLEN     (30)    /**< max length of the data */
+#define ETX_PKT_HDR_LEN     (2)     /**< Option type + Length (1 Byte each) */
+#define ETX_PKT_DATA        (2)     /**< Begin of Data Bytes */
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* ETX_BEACONING_H_ */
+/** ETX_BEACONING_H_ 
+  */
+#endif 
