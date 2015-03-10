daa9606f71df5853e901dbdc6972916773e43c96
Jan Wagner <mail@jwagner.eu>
2015-03-10 01:29:09 +0100

    Update sosemanuk.c
    no @file

--- sys/crypto/sosemanuk.c
+++ sys/crypto/sosemanuk.c
@@ -10,7 +10,7 @@
  * @ingroup     sys_crypto
  * @{
  *
- * @file        sosemanuk.c
+ * @file
  * @brief       Implementation of the SOSEMANUK stream cipher
  *
  * @author      Jan Wagner <mail@jwagner.eu>
