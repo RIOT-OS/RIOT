7fef7b322d47d7dbe209e2f878c519707d00d976
Jan Wagner <mail@jwagner.eu>
2015-03-10 01:29:09 +0100

    Update dragon.h
    no @file

--- sys/include/crypto/dragon.h
+++ sys/include/crypto/dragon.h
@@ -10,7 +10,7 @@
  * @ingroup     sys_crypto
  * @{
  *
- * @file        dragon.h
+ * @file
  * @brief       Headers for the implementation of the Dragon stream cipher
  *
  * @author      Jan Wagner <mail@jwagner.eu>
