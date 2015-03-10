2405e8cdef5dcd1a239b9776f47333cc1ee9adbb
Jan Wagner <mail@jwagner.eu>
2015-03-10 01:29:09 +0100

    Update nlsv2.c
    no @file

--- sys/crypto/nlsv2.c
+++ sys/crypto/nlsv2.c
@@ -10,7 +10,7 @@
  * @ingroup     sys_crypto
  * @{
  *
- * @file        nlsv2.c
+ * @file
  * @brief       Implementation of the NLSv2 stream cipher
  *
  * @author      Jan Wagner <mail@jwagner.eu>
