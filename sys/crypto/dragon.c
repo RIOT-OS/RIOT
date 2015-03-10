eed75b170b25402991747d984a55f0754abb2faf
Jan Wagner <mail@jwagner.eu>
2015-03-10 01:29:09 +0100

    Update dragon.c
    no @file

--- sys/crypto/dragon.c
+++ sys/crypto/dragon.c
@@ -10,7 +10,7 @@
  * @ingroup     sys_crypto
  * @{
  *
- * @file        dragon.c
+ * @file
  * @brief       Implementation of the Dragon stream cipher
  *
  * @author      Jan Wagner <mail@jwagner.eu>
