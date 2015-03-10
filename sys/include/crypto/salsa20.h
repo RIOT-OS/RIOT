5968d7edb6d038ce5d94329d987e53c402a08a34
Jan Wagner <mail@jwagner.eu>
2015-03-10 01:29:10 +0100

    Update salsa20.h
    no @file

--- sys/include/crypto/salsa20.h
+++ sys/include/crypto/salsa20.h
@@ -10,7 +10,7 @@
  * @ingroup     sys_crypto
  * @{
  *
- * @file        salsa20.h
+ * @file
  * @brief       Headers for the implementation of the Salsa20 stream cipher
  *
  * @author      Jan Wagner <mail@jwagner.eu>
