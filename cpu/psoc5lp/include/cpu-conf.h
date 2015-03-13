d7bea31194fcd1e91c257c45642d36022ad14a6f
Murat Cakmak <mail@muratcakmak.net>
2015-03-13 13:55:07 +0200

    White space character issue is fixed.

--- cpu/psoc5lp/include/cpu-conf.h
+++ cpu/psoc5lp/include/cpu-conf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Freie UniversitÃ¤t Berlin
+ * Copyright (C) 2014 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License v2.1. See the file LICENSE in the top level directory for more
