a537bca04fb736e4262287a66229e24d8c84cfb2
Murat Cakmak <mail@muratcakmak.net>
2015-03-13 13:51:18 +0200

    Revert "Uncorrect umlaut characters are fixed."
    This reverts commit 9cc3779ed4c7df0d10c7c6993551a6c6a4949e82.

--- boards/cy8ckit-050/include/board.h
+++ boards/cy8ckit-050/include/board.h
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * Copyright (C) 2014 INRIA
- * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 Freie UniversitÃ¤t Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License v2.1. See the file LICENSE in the top level directory for more
