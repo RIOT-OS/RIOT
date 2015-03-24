8d831eab14de91e95ba9ad44883384076fe92f75
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-03-24 23:29:48 +0100

    boards/pca10000: enabled nrfmin radio driver

--- boards/pca10000/include/periph_conf.h
+++ boards/pca10000/include/periph_conf.h
@@ -112,6 +112,16 @@ extern "C" {
 /** @} */
 
 /**
+ * @name Radio device configuration
+ *
+ * The radio is not guarded by a NUMOF define, as the radio is selected by its
+ * own module in the build system.
+ * @{
+ */
+#define RADIO_IRQ_PRIO      1
+/** @} */
+
+/**
  * @name GPIO configuration
  * @{
  */
