1992244e411dc8e48fbfcc33267a7784e4f583b6
Martine Lenders <mlenders@inf.fu-berlin.de>
2015-03-24 18:01:40 +0100

    tests: add test application for slip

---
+++ tests/slip/slip_params.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Martine Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief   slip parameters example, used by auto_init_ng_netif
+ *
+ * @author  Martine Lenders <mlenders@inf.fu-berlin.de>
+ */
+
+#ifndef SLIP_PARAMS_H
+#define SLIP_PARAMS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static xbee_params_t xbee_params[] = {
+    {
+        .uart = SLIP_UART,
+        .baudrate = SLIP_BAUDRATE,
+    },
+};
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* SLIP_PARAMS_H */
+/** @} */
