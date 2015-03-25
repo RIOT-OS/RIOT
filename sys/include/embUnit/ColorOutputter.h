3458ce1f4fec2390a6967a48c37b8614214d28f9
Janos Kutscherauer <noshky@gmail.com>
2015-03-25 02:52:42 +0100

    Replaced license information with standard LGPL license header and added doxygen file information.

--- sys/include/embUnit/ColorOutputter.h
+++ sys/include/embUnit/ColorOutputter.h
@@ -1,36 +1,15 @@
 /*
- * COPYRIGHT AND PERMISSION NOTICE
+ * Copyright (C) 2015 Janos Kutscherauer <noshky@gmail.com>
  *
- * Copyright (c) 2003 Embedded Unit Project
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
  *
- * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, and/or sell copies of the Software, and to permit persons
- * to whom the Software is furnished to do so, provided that the above
- * copyright notice(s) and this permission notice appear in all copies
- * of the Software and that both the above copyright notice(s) and this
- * permission notice appear in supporting documentation.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
- * OF THIRD PARTY RIGHTS. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * HOLDERS INCLUDED IN THIS NOTICE BE LIABLE FOR ANY CLAIM, OR ANY
- * SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES WHATSOEVER
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
- * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- * Except as contained in this notice, the name of a copyright holder
- * shall not be used in advertising or otherwise to promote the sale,
- * use or other dealings in this Software without prior written
- * authorization of the copyright holder.
- *
- * $Id: ColorOutputter.h,v 1.2 2003/09/06 13:28:27 arms22 Exp $
+ * @file    ColorOutputter.h
  */
 #ifndef __COLOROUTPUTTER_H__
 #define __COLOROUTPUTTER_H__
@@ -50,3 +29,4 @@ void ColorOutputter_printStatistics(OutputterRef self, TestResultRef result);
 #endif
 
 #endif/*__COLOROUTPUTTER_H__*/
+/** @} */
