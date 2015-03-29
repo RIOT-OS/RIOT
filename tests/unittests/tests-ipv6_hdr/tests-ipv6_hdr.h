7a3c373038d739883a0106656add914b7fdf08da
Martine Lenders <mlenders@inf.fu-berlin.de>
2015-03-29 03:17:31 +0200

    unittests: add tests for ipv6_hdr

---
+++ tests/unittests/tests-ipv6_hdr/tests-ipv6_hdr.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Martin Lenders
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file        tests-ipv6_hdr.h
+ * @brief       Unittests for the ``ipv6_hdr`` module
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ */
+#ifndef TESTS_IPV6_HDR_H_
+#define TESTS_IPV6_HDR_H_
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_ipv6_hdr(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_IPV6_HDR_H_ */
+/** @} */
