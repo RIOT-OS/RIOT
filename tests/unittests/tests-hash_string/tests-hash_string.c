cbacdf234f3fffbf9d1549ea6e4a1b2bf0c9815f
kushalsingh007 <kushal.spiderman.singh@gmail.com>
2015-04-12 12:00:07 +0530

    [squash me]Tests: Modifying unittests for the hash_string module - Modifying the testcase

--- tests/unittests/tests-hash_string/tests-hash_string.c
+++ tests/unittests/tests-hash_string/tests-hash_string.c
@@ -6,14 +6,13 @@
  * directory for more details.
  */
 
-#include <limits.h>
 #include "tests-hash_string.h"
 #include "hash_string.h"
 
 static void test_hash_value(void)
 {
     TEST_ASSERT_EQUAL_INT(177621, hash_string("0");
-    TEST_ASSERT_EQUAL_INT(ULLONG_MAX, hash_string("0123456789abcde-0123456789abcde-0123456789abcde-0123456789abcde-"));
+    TEST_ASSERT_EQUAL_INT(6555485335635392041, hash_string("0123456789abcde-0123456789abcde-0123456789abcde-0123456789abcde-"));
     TEST_ASSERT_EQUAL_INT(210674770391, hash_string("Frank jagt im komplett verwahrlosten Taxi quer durch Bayern"));
     TEST_ASSERT_EQUAL_INT(249811328611268156, hash_string("12A5B6cd_"));
     TEST_ASSERT_EQUAL_INT(5381, hash_string(""));
