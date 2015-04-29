2187b5caa9c21a11823917f871bdff98790f8cee
haukepetersen <hauke.petersen@fu-berlin.de>
2015-04-29 22:12:45 +0200

    tests/driver_at86rf2xx: adjusted netif auto init

--- tests/driver_at86rf2xx/main.c
+++ tests/driver_at86rf2xx/main.c
@@ -20,51 +20,18 @@
 
 #include <stdio.h>
 
-#include "board.h"
-#include "kernel.h"
+
 #include "shell.h"
 #include "shell_commands.h"
 #include "posix_io.h"
 #include "board_uart0.h"
-#include "ng_at86rf2xx.h"
-#include "net/ng_netbase.h"
-#include "net/ng_nomac.h"
 #include "net/ng_pktdump.h"
-
-/* make sure the SPI port and the needed GPIO pins are defined */
-#ifndef ATRF_SPI
-#error "SPI not defined"
-#endif
-#ifndef ATRF_CS
-#error "Chip select pin not defined"
-#endif
-#ifndef ATRF_INT
-#error "Interrupt pin not defined"
-#endif
-#ifndef ATRF_SLEEP
-#error "Sleep pin not defined"
-#endif
-#ifndef ATRF_RESET
-#error "Reset pin not defined"
-#endif
-#ifndef ATRF_SPI_SPEED
-#define ATRF_SPI_SPEED          (SPI_SPEED_5MHZ)
-#endif
+#include "net/ng_netbase.h"
 
 /**
  * @brief   Buffer size used by the shell
  */
 #define SHELL_BUFSIZE           (64U)
-
-/**
- * @brief   Allocate the AT86RF2xx device descriptor
- */
-static ng_at86rf2xx_t dev;
-
-/**
- * @brief   Stack for the nomac thread
- */
-static char nomac_stack[KERNEL_CONF_STACKSIZE_MAIN];
 
 /**
  * @brief   Read chars from STDIO
@@ -89,38 +56,16 @@ void shell_put(int c)
  */
 int main(void)
 {
-    kernel_pid_t iface;
-    int res;
     shell_t shell;
     ng_netreg_entry_t dump;
 
     puts("AT86RF2xx device driver test");
-    printf("Initializing the radio at SPI_%i... \n", ATRF_SPI);
 
     /* register the pktdump thread */
     puts("Register the packet dump thread for NG_NETTYPE_UNDEF packets");
     dump.pid = ng_pktdump_getpid();
     dump.demux_ctx = NG_NETREG_DEMUX_CTX_ALL;
     ng_netreg_register(NG_NETTYPE_UNDEF, &dump);
-
-    /* initialize the AT86RF2xx device */
-    puts("Initialize the AT86RF2xx radio device");
-    res = ng_at86rf2xx_init(&dev, ATRF_SPI, ATRF_SPI_SPEED,
-                            ATRF_CS, ATRF_INT,
-                            ATRF_SLEEP, ATRF_RESET);
-    if (res < 0) {
-        puts("Error initializing AT86RF2xx radio device");
-        return -1;
-    }
-
-    /* start MAC layer */
-    puts("Starting the NOMAC layer on top of the driver");
-    iface = ng_nomac_init(nomac_stack, sizeof(nomac_stack), PRIORITY_MAIN - 3,
-                          "at86rf2xx", (ng_netdev_t *)&dev);
-    if (iface <= KERNEL_PID_UNDEF) {
-        puts("Error initializing MAC layer");
-        return -1;
-    }
 
     /* start the shell */
     puts("Initialization successful - starting the shell now");
