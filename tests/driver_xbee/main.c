8e0d4ab4633e5a7ee2f3190b25d50aba4c9c2ea6
haukepetersen <hauke.petersen@fu-berlin.de>
2015-04-29 22:02:15 +0200

    tests/driver_xbee: adjusted to interface auto_init

--- tests/driver_xbee/main.c
+++ tests/driver_xbee/main.c
@@ -20,40 +20,15 @@
 
 #include <stdio.h>
 
-#include "board.h"
-#include "kernel.h"
 #include "shell.h"
 #include "shell_commands.h"
-#include "xbee.h"
 #include "net/ng_netbase.h"
-#include "net/ng_nomac.h"
 #include "net/ng_pktdump.h"
-
-/* make sure an UART to device is defined in the Makefile */
-#ifndef XBEE_UART
-#error "XBEE_UART not defined"
-#endif
-
-/**
- * @brief   This is the default baudrate the Xbee modules are programmed to
- *          when you buy them
- */
-#define XBEE_BAUDRATE           (9600U)
 
 /**
  * @brief   Buffer size used by the shell
  */
 #define SHELL_BUFSIZE           (64U)
-
-/**
- * @brief   The Xbee device descriptor
- */
-static xbee_t dev;
-
-/**
- * @brief   Stack for the nomac thread
- */
-static char nomac_stack[KERNEL_CONF_STACKSIZE_DEFAULT];
 
 /**
  * @brief   Read chars from STDIO
@@ -76,16 +51,10 @@ void shell_put(int c)
  */
 int main(void)
 {
-    kernel_pid_t iface;
-    int res;
     shell_t shell;
     ng_netreg_entry_t dump;
 
     puts("Xbee S1 device driver test");
-    printf("Initializing the Xbee S1 device UART_%i... \n", XBEE_UART);
-
-    /* initialize network module(s) */
-    ng_netif_init();
 
     /* initialize and register pktdump */
     dump.pid = ng_pktdump_getpid();
@@ -95,20 +64,6 @@ int main(void)
     }
     dump.demux_ctx = NG_NETREG_DEMUX_CTX_ALL;
     ng_netreg_register(NG_NETTYPE_UNDEF, &dump);
-
-    /* setup Xbee device */
-    res = xbee_init(&dev, XBEE_UART, XBEE_BAUDRATE, GPIO_NUMOF, GPIO_NUMOF);
-    if (res < 0) {
-        puts("Error initializing xbee device driver");
-        return -1;
-    }
-    /* start MAC layer */
-    iface = ng_nomac_init(nomac_stack, sizeof(nomac_stack), PRIORITY_MAIN - 3,
-                          "xbee_l2", (ng_netdev_t *)&dev);
-    if (iface <= KERNEL_PID_UNDEF) {
-        puts("Error initializing MAC layer");
-        return -1;
-    }
 
     /* start the shell */
     puts("Initialization OK, starting shell now");
