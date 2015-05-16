8619bdec63162b59a1adac90a3a1eb504593af9c
Joakim Gebart <joakim.gebart@eistec.se>
2015-05-16 10:06:30 +0200

    lpc2387: pwm_init: return actual frequency

--- cpu/lpc2387/periph/pwm.c
+++ cpu/lpc2387/periph/pwm.c
@@ -82,7 +82,7 @@ int pwm_init(pwm_t dev, pwm_mode_t mode, unsigned int frequency, unsigned int re
 #endif
     }
 
-    return 0;
+    return frequency;
 }
 
 int pwm_set(pwm_t dev, int channel, unsigned int value)
