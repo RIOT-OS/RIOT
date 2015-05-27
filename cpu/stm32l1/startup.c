f48ea33805b5762362cc85cd6a2d602a94b47c7a
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-05-27 23:08:41 +0200

    cpu/stm32l1: adapted to centralized cpu conf

--- cpu/stm32l1/startup.c
+++ cpu/stm32l1/startup.c
@@ -82,8 +82,7 @@ void reset_handler(void)
  */
 void dummy_handler(void)
 {
-    core_panic(DUMMY_HANDLER, "DUMMY HANDLER");
-    while (1) {asm ("nop");}
+    core_panic(PANIC_DUMMY_HANDLER, "DUMMY HANDLER");
 }
 
 void isr_nmi(void)
@@ -103,19 +102,19 @@ void isr_debug_mon(void)
 
 void isr_hard_fault(void)
 {
-    core_panic(HARD_FAULT, "HARD FAULT");
+    core_panic(PANIC_HARD_FAULT, "HARD FAULT");
     while (1) {asm ("nop");}
 }
 
 void isr_bus_fault(void)
 {
-    core_panic(BUS_FAULT, "BUS FAULT");
+    core_panic(PANIC_BUS_FAULT, "BUS FAULT");
     while (1) {asm ("nop");}
 }
 
 void isr_usage_fault(void)
 {
-    core_panic(USAGE_FAULT, "USAGE FAULT");
+    core_panic(PANIC_USAGE_FAULT, "USAGE FAULT");
     while (1) {asm ("nop");}
 }
 
