4d3b147122b363c4076bb2a0e3e6a03aec513117
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-05-27 23:06:52 +0200

    cpu/sam3x8e: adapted to centralized cpu conf

--- cpu/sam3x8e/startup.c
+++ cpu/sam3x8e/startup.c
@@ -80,7 +80,7 @@ void reset_handler(void)
  */
 void dummy_handler(void)
 {
-    core_panic(DUMMY_HANDLER, "DUMMY HANDLER");
+    core_panic(PANIC_DUMMY_HANDLER, "DUMMY HANDLER");
 }
 
 void isr_nmi(void)
@@ -100,17 +100,17 @@ void isr_debug_mon(void)
 
 void isr_hard_fault(void)
 {
-    core_panic(HARD_FAULT, "HARD FAULT");
+    core_panic(PANIC_HARD_FAULT, "HARD FAULT");
 }
 
 void isr_bus_fault(void)
 {
-    core_panic(BUS_FAULT, "BUS FAULT");
+    core_panic(PANIC_BUS_FAULT, "BUS FAULT");
 }
 
 void isr_usage_fault(void)
 {
-    core_panic(USAGE_FAULT, "USAGE FAULT");
+    core_panic(PANIC_USAGE_FAULT, "USAGE FAULT");
 }
 
 /* Cortex-M specific interrupt vectors */
