5720fbe5b4415f8d4fe1c5b66d6b508609e808b8
Joakim Gebart <joakim.gebart@eistec.se>
2015-05-28 17:37:12 +0200

    dist/tools/pr_check: refactor, only fetch labels if asked
     - Fetch labels on the first query
     - Handle PR environment variable from other CI systems
     - Return an error if this build is not a PR build

--- dist/tools/pr_check/check_labels.sh
+++ dist/tools/pr_check/check_labels.sh
@@ -11,7 +11,7 @@
 # ./dist/tools/pr_check/pr_check.sh
 
 GITHUB_API_HOST="https://api.github.com"
-GITHUB_REPO="RIOT-OS/RIOT"
+GITHUB_REPO="gebart/RIOT"
 
 if which wget &> /dev/null; then
     GET="wget -O -"
@@ -22,11 +22,35 @@ else
     exit 2
 fi
 
-LABELS_JSON=$(${GET} "${GITHUB_API_HOST}/repos/${GITHUB_REPO}/issues/${TRAVIS_PULL_REQUEST}/labels" 2> /dev/null)
+# initialize label cache variable
+LABELS_JSON=
+
+if [ -z "${CI_PULL_REQUEST}" ]; then
+    # Travis does not set the CI_PULL_REQUEST environment variable
+    # Other PR systems (e.g. Drone, Strider, Codeship) usually set the
+    # variable CI_PULL_REQUEST to the pull request number.
+    if [ -n "${TRAVIS_PULL_REQUEST}" ]; then
+        CI_PULL_REQUEST=${TRAVIS_PULL_REQUEST}
+    fi
+fi
 
 check_gh_label() {
+    if [ -z "${CI_PULL_REQUEST}" ]; then
+        # No pull request number, this happens if we run the CI on a branch
+        # which is not part of a PR, e.g. master, or if we run the CI scripts
+        # from the command line.
+        return 2
+    fi
     LABEL="${1}"
 
-    echo "${LABELS_JSON}" | grep -q "${LABEL}"
+    if [ -z "${LABELS_JSON}" ]; then
+        LABELS_JSON=$(${GET} "${GITHUB_API_HOST}/repos/${GITHUB_REPO}/issues/${CI_PULL_REQUEST}/labels" 2> /dev/null)
+        if [ "$?" -ne 0 ]; then
+            # Reading labels failed
+            return 3
+        fi
+    fi
+
+    printf '%s\n' "${LABELS_JSON}" | grep -q "${LABEL}"
     return $?
 }
