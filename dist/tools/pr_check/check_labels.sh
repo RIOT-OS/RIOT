969b57c52b09638372a6616f5f59aa6b29c41711
Joakim Gebart <joakim.gebart@eistec.se>
2015-05-28 17:37:12 +0200

    dist/tools/pr_check, Travis: refactor label checking
     - pr_check: Fetch labels on the first query
     - pr_check: Handle PR environment variable from other CI systems
     - pr_check: Return an error if querying labels if this is not a PR
     - Travis: Avoid querying labels if there is no PR number
     - Travis: set CI_PULL_REQUEST for compatibility
     - Travis: Always perform compile tests if this is not a PR

--- dist/tools/pr_check/check_labels.sh
+++ dist/tools/pr_check/check_labels.sh
@@ -22,11 +22,26 @@ else
     exit 2
 fi
 
-LABELS_JSON=$(${GET} "${GITHUB_API_HOST}/repos/${GITHUB_REPO}/issues/${TRAVIS_PULL_REQUEST}/labels" 2> /dev/null)
+# initialize label cache variable
+LABELS_JSON=
 
 check_gh_label() {
+    if [ -z "${CI_PULL_REQUEST}" ]; then
+        # No pull request number, this happens if we run the CI on a branch
+        # which is not part of a PR, e.g. master, or if we run the CI scripts
+        # from the command line.
+        return 2
+    fi
     LABEL="${1}"
 
-    echo "${LABELS_JSON}" | grep -q "${LABEL}"
+    if [ -z "${LABELS_JSON}" ]; then
+        LABELS_JSON=$(${GET} "${GITHUB_API_HOST}/repos/${GITHUB_REPO}/issues/${CI_PULL_REQUEST}/labels" 2> /dev/null)
+        if [ "$?" -ne 0 ]; then
+            # Reading labels failed
+            return 3
+        fi
+    fi
+
+    printf '%s\n' "${LABELS_JSON}" | grep -q "${LABEL}"
     return $?
 }
