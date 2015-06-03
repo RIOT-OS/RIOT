96d0f931298dc89223aa91a7abf8ee809f905cbd
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-06-03 18:24:54 +0200

    cpu/lpc1768: added periph_cpu.h

---
+++ cpu/lpc1768/include/periph_cpu.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         cpu_lpc1768
+ * @{
+ *
+ * @file
+ * @brief           CPU specific definitions for internal peripheral handling
+ *
+ * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
+ */
+
+#ifndef PERIPH_CPU_H_
+#define PERIPH_CPU_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* nothing to do here, yet */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CPU_H_ */
+/** @} */
