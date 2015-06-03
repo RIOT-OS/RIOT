c286da8d7faaa405a1b4be932274866366d246b7
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-06-03 12:08:51 +0200

    cpu/cortexm: unified startup code and isr handlers

---
+++ cpu/cortexm_common/vectors_cortexm.c
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_cortexm_common
+ * @{
+ *
+ * @file
+ * @brief       Default implementations for Cortex-M specific interrupt and
+ *              exception handlers
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdint.h>
+
+#include "board.h"
+#include "panic.h"
+#include "kernel_internal.h"
+#include "vectors_cortexm.h"
+
+/**
+ * @brief   Memory markers, defined in the linker script
+ * @{
+ */
+extern uint32_t _sfixed;
+extern uint32_t _efixed;
+extern uint32_t _etext;
+extern uint32_t _srelocate;
+extern uint32_t _erelocate;
+extern uint32_t _szero;
+extern uint32_t _ezero;
+extern uint32_t _sstack;
+extern uint32_t _estack;
+/** @} */
+
+/** @brief Interrupt stack canary value
+ *
+ * @note 0xe7fe is the ARM Thumb machine code equivalent of asm("bl #-2\n") or
+ * 'while (1);', i.e. an infinite loop.
+ */
+#define STACK_CANARY_WORD 0xE7FEE7FEu
+
+/**
+ * @brief   Required by g++ cross compiler
+ */
+void *__dso_handle;
+
+/**
+ * @brief   Pre-start routine for CPU-specific settings
+ */
+__attribute__((weak)) void pre_startup (void)
+{
+}
+
+/**
+ * @brief   Post-start routine for CPU-specific settings
+ */
+__attribute__((weak)) void post_startup (void)
+{
+}
+
+void reset_handler_default(void)
+{
+    uint32_t *dst;
+    uint32_t *src = &_etext;
+
+    pre_startup();
+
+    /* fill stack space with canary values */
+    for (dst = &_sstack; dst < &_estack; ) {
+        *(dst++) = STACK_CANARY_WORD;
+    }
+
+    /* load data section from flash to ram */
+    for (dst = &_srelocate; dst < &_erelocate; ) {
+        *(dst++) = *(src++);
+    }
+    /* default bss section to zero */
+    for (dst = &_szero; dst < &_ezero; ) {
+        *(dst++) = 0;
+    }
+
+    post_startup();
+
+    /* initialize the board (which also initiates CPU initialization) */
+    board_init();
+
+#if MODULE_NEWLIB
+    /* initialize std-c library (this must be done after board_init) */
+    extern void __libc_init_array(void);
+    __libc_init_array();
+#endif
+
+    /* startup the kernel */
+    kernel_init();
+}
+
+void nmi_default(void)
+{
+    core_panic(PANIC_NMI_HANDLER, "NMI HANDLER");
+}
+
+void hard_fault_default(void)
+{
+    core_panic(PANIC_HARD_FAULT, "HARD FAULT HANDLER");
+}
+
+#if defined(CPU_ARCH_CORTEX_M3) || defined(CPU_ARCH_CORTEX_M4) || \
+    defined(CPU_ARCH_CORTEX_M4F)
+void mem_manage_default(void)
+{
+    core_panic(PANIC_MEM_MANAGE, "MEM MANAGE HANDLER");
+}
+
+void bus_fault_default(void)
+{
+    core_panic(PANIC_BUS_FAULT, "BUS FAULT HANDLER");
+}
+
+void usage_fault_default(void)
+{
+    core_panic(PANIC_USAGE_FAULT, "USAGE FAULT HANDLER");
+}
+
+void debug_mon_default(void)
+{
+    core_panic(PANIC_DEBUG_MON, "DEBUG MON HANDLER");
+}
+#endif
+
+void dummy_handler_default(void)
+{
+    core_panic(PANIC_DUMMY_HANDLER, "DUMMY HANDLER");
+}
