70c3d2903632fa9e78463b782d522475cdbf70ba
Martine Lenders <mlenders@inf.fu-berlin.de>
2015-06-10 04:02:10 +0200

    gnrc_icmpv6_error: initial import

--- sys/include/net/gnrc/icmpv6/error.h
+++ sys/include/net/gnrc/icmpv6/error.h
@@ -16,15 +16,65 @@
  * @brief   ICMPv6 error message definitions
  *
  * @author  Martine Lenders <mlenders@inf.fu-berlin.de>
- *
- * @todo implement build and handle functions
  */
 #ifndef GNRC_ICMPV6_ERROR_H_
 #define GNRC_ICMPV6_ERROR_H_
 
+#include <stdint.h>
+
+#include "net/icmpv6.h"
+#include "net/ipv6/hdr.h"
+#include "net/gnrc/pkt.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @brief   Builds an ICMPv6 destination unreachable message for sending.
+ *
+ * @param[in] code      The code for the message @see net/icmpv6.h.
+ * @param[in] orig_pkt  The invoking packet.
+ *
+ * @return  The destination unreachable message on success.
+ * @return  NULL, on failure.
+ */
+gnrc_pktsnip_t *gnrc_icmpv6_error_dst_unr_build(uint8_t code, gnrc_pktsnip_t *orig_pkt);
+
+/**
+ * @brief   Builds an ICMPv6 packet too big message for sending.
+ *
+ * @param[in] mtu       The maximum transission unit of the next-hop link.
+ * @param[in] orig_pkt  The invoking packet.
+ *
+ * @return  The packet too big message on success.
+ * @return  NULL, on failure.
+ */
+gnrc_pktsnip_t *gnrc_icmpv6_error_pkt_too_big_build(uint32_t mtu, gnrc_pktsnip_t *orig_pkt);
+
+/**
+ * @brief   Builds an ICMPv6 time exceeded message for sending.
+ *
+ * @param[in] code      The code for the message @see net/icmpv6.h.
+ * @param[in] orig_pkt  The invoking packet.
+ *
+ * @return  The time exceeded message on success.
+ * @return  NULL, on failure.
+ */
+gnrc_pktsnip_t *gnrc_icmpv6_error_time_exc_build(uint8_t code, gnrc_pktsnip_t *orig_pkt);
+
+/**
+ * @brief   Builds an ICMPv6 parameter problem message for sending.
+ *
+ * @param[in] code      The code for the message @see net/icmpv6.h.
+ * @param[in] ptr       Pointer to the errorneous octet in @p orig_pkt.
+ * @param[in] orig_pkt  The invoking packet.
+ *
+ * @return  The parameter problem message on success.
+ * @return  NULL, on failure.
+ */
+gnrc_pktsnip_t *gnrc_icmpv6_error_param_prob_build(uint8_t code, void *ptr,
+                                                   gnrc_pktsnip_t *orig_pkt);
 
 #ifdef __cplusplus
 }
