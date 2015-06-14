8c690f1d6aa6c85c59988ac02f84ebadb7fffc5c
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-06-14 16:16:28 +0200

    cpu/stm32f3: added custom GPIO_UNDEF define

--- cpu/stm32f3/include/periph_cpu.h
+++ cpu/stm32f3/include/periph_cpu.h
@@ -34,6 +34,11 @@ typedef uint32_t gpio_t;
 /** @} */
 
 /**
+ * @brief   Definition of a fitting UNDEF value
+ */
+#define GPIO_UNDEF          (0xffffffff)
+
+/**
  * @brief   Define a CPU specific GPIO pin generator macro
  */
 #define GPIO(x, y)          ((GPIOA_BASE + (x << 10)) | y)
