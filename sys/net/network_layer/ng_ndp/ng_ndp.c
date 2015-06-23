a1d9fde82a9f471a04302d5316278c24b67b4ee2
haukepetersen <hauke.petersen@fu-berlin.de>
2015-06-23 02:46:28 +0200

    net/ng_ndp: fixed wrong for loop limits

--- sys/net/network_layer/ng_ndp/ng_ndp.c
+++ sys/net/network_layer/ng_ndp/ng_ndp.c
@@ -818,9 +818,10 @@ ng_pktsnip_t *ng_ndp_opt_tl2a_build(const uint8_t *l2addr, uint8_t l2addr_len,
 /* packet queue node allocation */
 static ng_pktqueue_node_t *_alloc_pkt_node(ng_pktsnip_t *pkt)
 {
-    for (size_t i = 0; i < sizeof(_pkt_nodes); i++) {
+    for (size_t i = 0; i < (sizeof(_pkt_nodes) / sizeof(ng_pktqueue_node_t));
+         i++) {
         if (_pkt_nodes[i].data == NULL) {
-            ng_pktqueue_node_init(_pkt_nodes + i);
+            ng_pktqueue_node_init(&(_pkt_nodes[i]));
             _pkt_nodes[i].data = pkt;
 
             return &(_pkt_nodes[i]);
