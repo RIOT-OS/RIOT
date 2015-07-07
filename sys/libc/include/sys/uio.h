7acc4141cebb57cc8d78b5cc8f6d53154ae0d636
Kaspar Schleiser <kaspar@schleiser.de>
2015-07-07 10:31:17 +0200

    sys: add libc includes directory and sys/uio.h header

---
+++ sys/libc/include/sys/uio.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  posix
+ * @{
+ */
+
+/**
+ * @file
+ * @brief   libc header for scatter/gather I/O
+ *
+ * @author  Kaspar Schleiser <kaspar@schleiser.de>
+ */
+#ifndef UIO_H
+#define UIO_H
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Structure for scatter/gather I/O.
+ */
+struct iovec {
+    void *iov_base;     /**< Pointer to data.   */
+    size_t iov_len;     /**< Length of data.    */
+};
+
+#ifdef __cplusplus
+}
+#endif
+/** @} */
+#endif /* UIO_H */
