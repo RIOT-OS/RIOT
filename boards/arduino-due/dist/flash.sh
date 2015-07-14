ae046e42d52c89dfbfb211a0a02ee93f62daadc9
Thomas Eichinger <thomas.eichinger1@gmail.com>
2015-07-14 15:50:06 +0200

    arduino-due: fix variable usage in flash.sh

--- boards/arduino-due/dist/flash.sh
+++ boards/arduino-due/dist/flash.sh
@@ -4,7 +4,7 @@ if [ `uname` = "Linux" ]; then
     stty -F "${PORT}" raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
     "${RIOTBOARD}"/"${BOARD}"/dist/bossac  -R -e -w -v -b "${HEXFILE}"
 elif [ `uname` = "Darwin" ]; then
-    stty -f $(PORT) raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
+    stty -f ${PORT} raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
     "${RIOTBOARD}"/"${BOARD}"/dist/bossac_osx -R -e -w -v -b "${HEXFILE}"
 else
     echo "CAUTION: No flash tool for your host system found!"
