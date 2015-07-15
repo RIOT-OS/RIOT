842694aeee7bee2a13b87f7b956c03c9f9c33d84
Kaspar Schleiser <kaspar@schleiser.de>
2015-07-15 13:53:05 +0200

    sys: auto_init: no need to initialize uart0 when using uart_stdio

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -244,9 +244,11 @@ void auto_init(void)
     DEBUG("Auto init vtimer module.\n");
     vtimer_init();
 #endif
+#ifndef MODULE_UART_STDIO
 #ifdef MODULE_UART0
     DEBUG("Auto init uart0 module.\n");
     board_uart0_init();
+#endif
 #endif
 #ifdef MODULE_RTC
     DEBUG("Auto init rtc module.\n");
