ea697ce65063ecf1709366ca7d3ecc23ba0f7763
Sebastian Sontberg <sebastian@sontberg.de>
2015-08-03 06:50:14 +0200

    cpu/cortexm_common: refactor common isr setup
    - defines the first segment of the NVIC vector tables through
      vector_common[].
    
    - provides weak defined stubs for *_fault-type exceptions.
    
    - provides weak aliases to a dummy handler for isr_*-type exceptions.
    
    - changes names from *_fault_default() to *_fault() (nmi_default became
      isr_nmi)

--- cpu/cortexm_common/vectors_cortexm.c
+++ cpu/cortexm_common/vectors_cortexm.c
@@ -30,15 +30,12 @@
  * @brief   Memory markers, defined in the linker script
  * @{
  */
-extern uint32_t _sfixed;
-extern uint32_t _efixed;
 extern uint32_t _etext;
 extern uint32_t _srelocate;
 extern uint32_t _erelocate;
 extern uint32_t _szero;
 extern uint32_t _ezero;
 extern uint32_t _sstack;
-extern uint32_t _estack;
 /** @} */
 
 /** @brief Interrupt stack canary value
@@ -54,20 +51,25 @@ extern uint32_t _estack;
 void *__dso_handle;
 
 /**
+ * @brief   Used for default ISR stubs
+ */
+#define WEAK    __attribute__((weak))
+
+/**
  * @brief   Pre-start routine for CPU-specific settings
  */
-__attribute__((weak)) void pre_startup (void)
+WEAK void pre_startup (void)
 {
 }
 
 /**
  * @brief   Post-start routine for CPU-specific settings
  */
-__attribute__((weak)) void post_startup (void)
+WEAK void post_startup (void)
 {
 }
 
-void reset_handler_default(void)
+WEAK void reset_handler(void)
 {
     uint32_t *dst;
     uint32_t *src = &_etext;
@@ -107,40 +109,87 @@ void reset_handler_default(void)
     kernel_init();
 }
 
-void nmi_default(void)
+WEAK void hard_fault(void)
 {
-    core_panic(PANIC_NMI_HANDLER, "NMI HANDLER");
-}
-
-void hard_fault_default(void)
-{
-    core_panic(PANIC_HARD_FAULT, "HARD FAULT HANDLER");
+    core_panic(PANIC_HARD_FAULT, "HARD FAULT");
 }
 
 #if defined(CPU_ARCH_CORTEX_M3) || defined(CPU_ARCH_CORTEX_M4) || \
     defined(CPU_ARCH_CORTEX_M4F)
-void mem_manage_default(void)
+WEAK void mem_manage_fault(void)
 {
-    core_panic(PANIC_MEM_MANAGE, "MEM MANAGE HANDLER");
+    core_panic(PANIC_MEM_MANAGE, "MEM MANAGE FAULT");
 }
 
-void bus_fault_default(void)
+WEAK void bus_fault(void)
 {
-    core_panic(PANIC_BUS_FAULT, "BUS FAULT HANDLER");
+    core_panic(PANIC_BUS_FAULT, "BUS FAULT");
 }
 
-void usage_fault_default(void)
+WEAK void usage_fault(void)
 {
-    core_panic(PANIC_USAGE_FAULT, "USAGE FAULT HANDLER");
+    core_panic(PANIC_USAGE_FAULT, "USAGE FAULT");
 }
 
-void debug_mon_default(void)
+WEAK void debug_mon(void)
 {
-    core_panic(PANIC_DEBUG_MON, "DEBUG MON HANDLER");
+    core_panic(PANIC_DEBUG_MON, "NO DEBUG MON");
 }
-#endif
+#endif /* #if defined(CPU_ARCH_CORTEX_M3) || ... */
 
 void dummy_handler_default(void)
 {
     core_panic(PANIC_DUMMY_HANDLER, "DUMMY HANDLER");
 }
+
+WEAK __attribute__((alias("dummy_handler_default"))) void isr_nmi(void);
+
+WEAK __attribute__((alias("dummy_handler_default"))) void isr_pendsv(void);
+
+WEAK __attribute__((alias("dummy_handler_default"))) void isr_svc(void);
+
+WEAK __attribute__((alias("dummy_handler_default"))) void isr_systick(void);
+
+__attribute__((used, section(".vectors.common"))) const void *vectors_common[] = {
+#if defined(CPU_ARCH_CORTEX_M3) || defined(CPU_ARCH_CORTEX_M4) || defined(CPU_ARCH_CORTEX_M4F)
+    /* Cortex-M3/4 handlers */
+    (void*) reset_handler,   /*  1 entry point of the program */
+    (void*) isr_nmi,         /*  2 non maskable interrupt handler */
+
+    (void*) hard_fault,      /*  3 hard fault exception */
+    (void*) mem_manage_fault,/*  4 memory manage exception */
+    (void*) bus_fault,       /*  5 bus fault exception */
+    (void*) usage_fault,     /*  6 usage fault exception */
+    (void*) (0UL),           /*  7 Reserved */
+    (void*) (0UL),           /*  8 Reserved */
+    (void*) (0UL),           /*  9 Reserved */
+    (void*) (0UL),           /* 10 Reserved */
+    (void*) isr_svc,         /* 11 system call interrupt, in RIOT used for
+                              *    switching into thread context on boot */
+    (void*) debug_mon,       /* 12 debug monitor exception */
+    (void*) (0UL),           /* 13 Reserved */
+    (void*) isr_pendsv,      /* 14 pendSV interrupt, in RIOT the actual
+                              * 15 context switching is happening here */
+    (void*) isr_systick      /* 16 SysTick interrupt, not used in RIOT */
+#else
+    /* Cortex-M0 handlers  */
+    (void*) reset_handler,   /*  1 entry point of the program */
+    (void*) isr_nmi,         /*  2 non maskable interrupt handler */
+
+    (void*) hard_fault,      /*  3 hard fault exception */
+    (void*) (0UL),           /*  4 Reserved */
+    (void*) (0UL),           /*  5 Reserved */
+    (void*) (0UL),           /*  6 Reserved */
+    (void*) (0UL),           /*  7 Reserved */
+    (void*) (0UL),           /*  8 Reserved */
+    (void*) (0UL),           /*  9 Reserved */
+    (void*) (0UL),           /* 10 Reserved */
+    (void*) isr_svc,         /* 11 system call interrupt, in RIOT used for
+                              *    switching into thread context on boot */
+    (void*) (0UL),           /* 12 Reserved */
+    (void*) (0UL),           /* 13 Reserved */
+    (void*) isr_pendsv,      /* 14 pendSV interrupt, in RIOT the actual
+                              *    context switching is happening here */
+    (void*) isr_systick      /* 15 SysTick interrupt, not used in RIOT */
+#endif /* #if defined(CPU_ARCH_CORTEX_M3) || ... */
+};
