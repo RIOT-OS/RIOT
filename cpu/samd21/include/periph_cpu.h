0a859b91ee796af3ea0446bddf8ac1e5737e9c8d
Kaspar Schleiser <kaspar@schleiser.de>
2015-08-13 15:20:39 +0200

    cpu: samd21: use periph_common SPI functions

--- cpu/samd21/include/periph_cpu.h
+++ cpu/samd21/include/periph_cpu.h
@@ -88,6 +88,15 @@ typedef enum {
  */
 int gpio_init_mux(gpio_t dev, gpio_mux_t mux);
 
+/**
+ * @brief declare needed generic SPI functions
+ * @{
+ */
+#define PERIPH_SPI_NEEDS_TRANSFER_BYTES
+#define PERIPH_SPI_NEEDS_TRANSFER_REG
+#define PERIPH_SPI_NEEDS_TRANSFER_REGS
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
