a379075ff079e48127ab8e3e4e9c5057c973bf74
DipSwitch <dipswitch@ownage4u.nl>
2015-08-19 18:01:29 +0200

    Re-assign system timer

--- boards/rf-motherboard/include/periph_conf.h
+++ boards/rf-motherboard/include/periph_conf.h
@@ -46,38 +46,42 @@ extern "C" {
  * @brief Timer configuration
  * @{
  */
-#define TIMER_NUMOF         (2U)
+#define TIMER_NUMOF         (1U)
 #define TIMER_0_EN          1
+#if 0
 #define TIMER_1_EN          1
+#endif
 
 /* Timer 0 configuration */
-#define TIMER_0_DEV_0       TIM2
-#define TIMER_0_DEV_1       TIM3
+#define TIMER_0_DEV_0       TIM4
+#define TIMER_0_DEV_1       TIM5
 #define TIMER_0_CHANNELS    4
 #define TIMER_0_PRESCALER   (32U)
 #define TIMER_0_MAX_VALUE   (0xffff)
-#define TIMER_0_CLKEN()     (RCC->APB1ENR |= (RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN))
-#define TIMER_0_ISR_0       isr_tim2
-#define TIMER_0_ISR_1       isr_tim3
-#define TIMER_0_IRQ_CHAN_0  TIM2_IRQn
-#define TIMER_0_IRQ_CHAN_1  TIM3_IRQn
+#define TIMER_0_CLKEN()     (RCC->APB1ENR |= (RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM5EN))
+#define TIMER_0_ISR_0       isr_tim4
+#define TIMER_0_ISR_1       isr_tim5
+#define TIMER_0_IRQ_CHAN_0  TIM4_IRQn
+#define TIMER_0_IRQ_CHAN_1  TIM5_IRQn
 #define TIMER_0_IRQ_PRIO    1
-#define TIMER_0_TRIG_SEL    TIM_SMCR_TS_0
+#define TIMER_0_TRIG_SEL    TIM_SMCR_TS_1
+/** @} */
 
 /* Timer 1 configuration */
-#define TIMER_1_DEV_0       TIM4
-#define TIMER_1_DEV_1       TIM5
+#if 0
+#define TIMER_1_DEV_0       TIM2
+#define TIMER_1_DEV_1       TIM3
 #define TIMER_1_CHANNELS    4
 #define TIMER_1_PRESCALER   (32U)
 #define TIMER_1_MAX_VALUE   (0xffff)
-#define TIMER_1_CLKEN()     (RCC->APB1ENR |= (RCC_APB1ENR_TIM4EN | RCC_APB1ENR_TIM5EN))
-#define TIMER_1_ISR_0       isr_tim4
-#define TIMER_1_ISR_1       isr_tim5
-#define TIMER_1_IRQ_CHAN_0  TIM4_IRQn
-#define TIMER_1_IRQ_CHAN_1  TIM5_IRQn
+#define TIMER_1_CLKEN()     (RCC->APB1ENR |= (RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN))
+#define TIMER_1_ISR_0       isr_tim2
+#define TIMER_1_ISR_1       isr_tim3
+#define TIMER_1_IRQ_CHAN_0  TIM2_IRQn
+#define TIMER_1_IRQ_CHAN_1  TIM3_IRQn
 #define TIMER_1_IRQ_PRIO    1
-#define TIMER_1_TRIG_SEL    TIM_SMCR_TS_1
-/** @} */
+#define TIMER_1_TRIG_SEL    TIM_SMCR_TS_0
+#endif
 
 /**
  * @brief UART configuration
