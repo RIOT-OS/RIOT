a0b3021f60cbe3e49528d618ae89f3efcb808c67
Oleg Hahm <oleg@hobbykeller.org>
2015-08-22 14:32:12 +0200

    icmpv6: remove superfluous header build functions

--- sys/shell/commands/sc_icmpv6_echo.c
+++ sys/shell/commands/sc_icmpv6_echo.c
@@ -215,8 +215,8 @@ int _icmpv6_ping(int argc, char **argv)
         gnrc_pktsnip_t *pkt;
         timex_t start, stop, timeout = { 5, 0 };
 
-        pkt = gnrc_icmpv6_echo_req_build(id, ++max_seq_expected, NULL,
-                                         payload_len);
+        pkt = gnrc_icmpv6_echo_build(ICMPV6_ECHO_REQ, id, ++max_seq_expected,
+                                     NULL, payload_len);
 
         if (pkt == NULL) {
             puts("error: packet buffer full");
