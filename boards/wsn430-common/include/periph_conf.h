949f0cf0d18edb9e008a812e05d0d8194916dd4f
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-08-26 18:46:39 +0200

    board/wsn430-common: added periph timer config

--- boards/wsn430-common/include/periph_conf.h
+++ boards/wsn430-common/include/periph_conf.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 INRIA
+ *               2015 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License v2.1. See the file LICENSE in the top level directory for more
@@ -14,11 +15,28 @@
  * @brief       WSN30 peripheral configuration
  *
  * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ *              Hauke Petersen <hauke.petersen@fu-berlin.de>
  */
 
-#ifndef PERIPH_CONF_H_
-#define PERIPH_CONF_H_
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
 
-/* make Travis happy: #ifdef __cplusplus extern "C" { #endif */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#endif /* PERIPH_CONF_H_ */
+/**
+ * @brief   Timer configuration
+ * @{
+ */
+#define TIMER_DEV           (TIMER_A)
+#define TIMER_CHAN          (3)
+#define TIMER_ISR_CC0       (TIMERA0_VECTOR)
+#define TIMER_ISR_CCX       (TIMERA1_VECTOR)
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
