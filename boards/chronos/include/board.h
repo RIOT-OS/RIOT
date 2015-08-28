5fe6cc7cee4776d910085901a1cc6b673216e66d
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-08-28 19:07:55 +0200

    boards/chronos: adapted to periph UART driver

--- boards/chronos/include/board.h
+++ boards/chronos/include/board.h
@@ -47,6 +47,18 @@ extern "C" {
  */
 #define HW_TIMER                    (0)
 
+/**
+ * @brief   Standard input/output device configuration
+ *
+ * This defines are for compatibility with the CPU implementation but they are
+ * not used for this board (as it has no UART interface accessible...)
+ * @{
+ */
+#define STDIO                       (0)
+#define STDIO_BAUDRATE              (115200U)
+#define STDIO_RX_BUFSIZE            (64U)
+/** @} */
+
 #define MSP430_INITIAL_CPU_SPEED    7372800uL
 #define F_CPU                       MSP430_INITIAL_CPU_SPEED
 #define F_RC_OSCILLATOR             32768
