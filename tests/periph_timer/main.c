8739a2a319cf87487b7cb5631b8575656ffe6920
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-09-07 12:05:10 +0200

    s test: fixed copyright and reformatted license

--- tests/periph_timer/main.c
+++ tests/periph_timer/main.c
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2015 Freie Universität Berlin
  *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
  */
 
 /**
