44cdec0e1e84554b7e5c3927940b3cb330ad41b0
Kaspar Schleiser <kaspar@schleiser.de>
2015-09-14 15:50:00 +0200

    boards: msp430*: added (x)timer XTIMER_SHIFT_ON_COMPARE values

--- boards/chronos/include/board.h
+++ boards/chronos/include/board.h
@@ -39,6 +39,7 @@ extern "C" {
 #define XTIMER                      (0)
 #define XTIMER_CHAN                 (0)
 #define XTIMER_MASK                 (0xffff0000)
+#define XTIMER_SHIFT_ON_COMPARE     (4)
 /** @} */
 
 /**
--- boards/msb-430h/include/board.h
+++ boards/msb-430h/include/board.h
@@ -38,6 +38,8 @@ extern "C" {
 #define XTIMER                      (0)
 #define XTIMER_CHAN                 (0)
 #define XTIMER_MASK                 (0xffff0000)
+#define XTIMER_SHIFT_ON_COMPARE     (4)
+#define XTIMER_BACKOFF              (40)
 /** @} */
 
 /**
