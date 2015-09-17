5a464f103523238fa60061d6d630663d7277d4f7
Marc <dkm@kataplop.net>
2015-09-17 22:28:12 +0200

    Initial gpio support for stellaris/lm4f120

--- cpu/lm4f120/include/periph_cpu.h
+++ cpu/lm4f120/include/periph_cpu.h
@@ -15,17 +15,30 @@
  *
  * @author          Rakendra Thapa <rakendrathapa@gmail.com>
  */
-
 #ifndef PERIPH_CPU_H_
 #define PERIPH_CPU_H_
 
+#include <inttypes.h>
 #include "periph/dev_enums.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define HAVE_GPIO_T
+typedef uint32_t gpio_t;
+
+#define GPIO(x,y) ((gpio_t)(x<<4 | y))
 /* nothing to do here, yet */
+
+  enum {
+    PORT_A = 0,       /**< port A */
+    PORT_B = 1,       /**< port B */
+    PORT_C = 2,       /**< port C */
+    PORT_D = 3,       /**< port D */
+    PORT_E = 4,       /**< port E */
+    PORT_F = 5,       /**< port F */
+  };
 
 #ifdef __cplusplus
 }
