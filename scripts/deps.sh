8c2b00f70ecc33e061e8b6659182234dd56c2277
Kaspar Schleiser <kaspar@schleiser.de>
2015-09-23 17:44:42 +0200

    squash: iff -> if

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -9,17 +9,17 @@ use() {
     DEPS="$DEPS$* "
 }
 
-# true iff the argument is a used module
+# true if the argument is a used module
 used() {
     [ "${DEPS#* $1 }" != "$DEPS" ];
 }
 
-# true iff the argument is the prefix of a used module name
+# true if the argument is the prefix of a used module name
 used_prefix() {
     [ "${DEPS#* $1}" != "$DEPS" ];
 }
 
-# true iff the first argument is a used prefix,
+# true if the first argument is a used prefix,
 # but ignore the following argumens as possible suffices
 used_prefix_but() {
     local prefix=$1
@@ -27,7 +27,7 @@ used_prefix_but() {
     echo "$DEPS" | grep -qP " $prefix(?!$(join "|" $*)) "
 }
 
-# negate a result, i.e. "not used foo" is true iff "foo" is not used
+# negate a result, i.e. "not used foo" is true if "foo" is not used
 not() {
     if "$@"; then
         false
