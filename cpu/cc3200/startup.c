565124ee8284a44f68035d4198095be979be1cbe
Attilio Donà <attilio.dona@gmail.com>
2015-09-24 10:19:15 +0200

    save regs for debug in hardfault handler

--- cpu/cc3200/startup.c
+++ cpu/cc3200/startup.c
@@ -41,6 +41,19 @@
 #include "inc/hw_types.h"
 
 #include "kernel_internal.h"
+#include "periph_conf.h"
+
+#if UART_0_EN
+void UART_0_ISR(void);
+#else
+
+static void IntDefaultHandler10(void)
+{
+    while(1) {}
+}
+
+#endif
+
 
 //*****************************************************************************
 //
@@ -58,7 +71,7 @@ extern unsigned long _eheap;
 //*****************************************************************************
 void ResetISR(void);
 static void NmiSR(void);
-static void FaultISR(void);
+static void FaultISR(void) __attribute__(( naked ));
 static void IntDefaultHandler(void);
 static void BusFaultHandler(void);
 
@@ -116,10 +129,6 @@ static void IntDefaultHandler8(void)
     while(1) {}
 }
 static void IntDefaultHandler9(void)
-{
-    while(1) {}
-}
-static void IntDefaultHandler10(void)
 {
     while(1) {}
 }
@@ -230,12 +239,12 @@ void (* const g_pfnVectors[256])(void) =
     IntDefaultHandler7,                      // GPIO Port A1
     IntDefaultHandler8,                      // GPIO Port A2
     IntDefaultHandler9,                      // GPIO Port A3
-    0,                                      // Reserved
-    IntDefaultHandler10,                      // UART0 Rx and Tx
-    IntDefaultHandler11,                      // UART1 Rx and Tx
-    0,                                      // Reserved
+    0,                                       // Reserved
+	UART_0_ISR,                              // UART0 Rx and Tx
+    IntDefaultHandler11,                     // UART1 Rx and Tx
+    0,                                       // Reserved
     IntDefaultHandler12,                      // I2C0 Master and Slave
-    0,0,0,0,0,                              // Reserved
+    0,0,0,0,0,                                // Reserved
     IntDefaultHandler13,                      // ADC Channel 0
     IntDefaultHandler14,                      // ADC Channel 1
     IntDefaultHandler15,                      // ADC Channel 2
@@ -376,6 +385,7 @@ NmiSR(void)
     }
 }
 
+
 //*****************************************************************************
 //
 // This is the code that gets called when the processor receives a fault
@@ -386,13 +396,56 @@ NmiSR(void)
 static void
 FaultISR(void)
 {
-    //
-    // Enter an infinite loop.
-    //
-    while(1)
-    {
-    }
+	__asm volatile
+	(
+			" tst lr, #4                                                \n"
+			" ite eq                                                    \n"
+			" mrseq r0, msp                                             \n"
+			" mrsne r0, psp                                             \n"
+			" ldr r1, [r0, #24]                                         \n"
+			" ldr r2, handler2_address_const                            \n"
+			" bx r2                                                     \n"
+			" handler2_address_const: .word prvGetRegistersFromStack    \n"
+	);
 }
+
+
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+void prvGetRegistersFromStack( uint32_t *pulFaultStackAddress )
+{
+	/* These are volatile to try and prevent the compiler/linker optimising them
+	away as the variables never actually get used.  If the debugger won't show the
+	values of the variables, make them global my moving their declaration outside
+	of this function. */
+	volatile uint32_t r0;
+	volatile uint32_t r1;
+	volatile uint32_t r2;
+	volatile uint32_t r3;
+	volatile uint32_t r12;
+	volatile uint32_t lr; /* Link register. */
+	volatile uint32_t pc; /* Program counter. */
+	volatile uint32_t psr;/* Program status register. */
+
+	r0 = pulFaultStackAddress[ 0 ];
+	r1 = pulFaultStackAddress[ 1 ];
+	r2 = pulFaultStackAddress[ 2 ];
+	r3 = pulFaultStackAddress[ 3 ];
+
+	r12 = pulFaultStackAddress[ 4 ];
+	lr = pulFaultStackAddress[ 5 ];
+	pc = pulFaultStackAddress[ 6 ];
+	psr = pulFaultStackAddress[ 7 ];
+
+	/* When the following line is hit, the variables contain the register values. */
+	for( ;; );
+}
+#pragma GCC diagnostic pop
+
+
+
+
+
+
 
 //*****************************************************************************
 //
