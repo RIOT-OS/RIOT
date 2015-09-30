94960487c4f4fc9a8044d7bafe775c88856e55c4
Kaspar Schleiser <kaspar@schleiser.de>
2015-09-30 11:28:27 +0200

    fixup! WIP add div.h

--- sys/include/div.h
+++ sys/include/div.h
@@ -5,12 +5,12 @@
 
 static inline uint64_t div_u64_by_1000000(uint64_t val)
 {
-    return (uint64_t)(us * 0x431bde83) >> (0x12 + 32);
+    return (uint64_t)(val * 0x431bde83) >> (0x12 + 32);
 }
 
 static inline uint32_t div_u32_by_512div15625(uint32_t val)
 {
-    return ((uint64_t)(us) * 0x431bde83ul) >> (12 + 32 - 9);
+    return ((uint64_t)(val) * 0x431bde83ul) >> (12 + 32 - 9);
 }
 
 #endif /* DIV_H */
