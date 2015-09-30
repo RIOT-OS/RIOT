b538c742292f0cf0be287e04960d182ec822ba42
Oleg Hahm <oleg@hobbykeller.org>
2015-09-30 17:56:57 +0200

    examples: spending the main thread a message queue
    Since it is likely that the main thread will send netapi IPC calls that expects a reply. These replies may come faster than the thread can handle them, causing the layers below to stuck.

--- examples/gnrc_border_router/main.c
+++ examples/gnrc_border_router/main.c
@@ -21,9 +21,16 @@
 #include <stdio.h>
 
 #include "shell.h"
+#include "msg.h"
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 int main(void)
 {
+    /* we need a message queue for the thread running the shell in order to
+     * receive potentially fast incoming networking packets */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT border router example application");
 
     /* start shell */
