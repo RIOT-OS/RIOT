926381083d96c087a10f507ba6157a1c18de60bf
Kaspar Schleiser <kaspar@schleiser.de>
2015-09-30 12:49:12 +0200

    REMOVE ME: example usage of arbitrary xtimer conversion function

--- cpu/native/include/periph_conf.h
+++ cpu/native/include/periph_conf.h
@@ -16,6 +16,8 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
+#include "div.h"
+
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -55,6 +57,9 @@
 #define XTIMER_OVERHEAD 14
 #define XTIMER_USLEEP_UNTIL_OVERHEAD 1
 /** @} */
+
+#define XTIMER_US_TO_TICKS(val) (div_u32_by_512div15625(val))
+#define XTIMER_TICKS_TO_US(val) (((uint64_t)val * 15625)>>9)
 
 #ifdef __cplusplus
 }
