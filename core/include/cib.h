b92953d32b86b14b2219e8018c48e71ec88479c3
IldarValiev <irvaliev@gmail.com>
2015-10-14 00:07:42 +0900

    added assert

--- core/include/cib.h
+++ core/include/cib.h
@@ -48,6 +48,9 @@ typedef struct {
  */
 static inline void cib_init(cib_t *__restrict cib, unsigned int size)
 {
+    /* check if size is a power of 2 by comparing it to its complement */
+    assert(!(size & (size - 1)));
+    
     cib_t c = CIB_INIT(size);
     *cib = c;
 }
