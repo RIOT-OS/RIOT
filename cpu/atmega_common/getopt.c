634f81fcc5a91f323e3619206c3430fbbc80c2a9
MohmadAyman <no.future2e1@gmail.com>
2015-10-18 19:59:54 +0200

    header
    doxgyen
    
    doxgyen
    
    trying to fix doxgyen warrning
    
    more documentaion
    
    more documentaion

--- cpu/atmega_common/getopt.c
+++ cpu/atmega_common/getopt.c
@@ -1,4 +1,7 @@
-/* This file is subject to the terms and conditions of the GNU Lesser
+/*
+ * Copyright (C) 2015 Mohmmad Ayman <Mohmmad.Salah94@eng-st.cu.edu.eg>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
  */
@@ -17,50 +20,57 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @getopt libirary light implementaion
+ */
+typedef struct {
+    /* Global argument pointer. */
+    char *optarg;
+    /* Global argv index. */
+    int optind;
+} opt_r;
 
-char *optarg;
-int optind = 0;
-char *scanptr = NULL;
-int getopt(int argc, char *argv[], char *optstring)
+int getopt(int argc, char *argv[], char *optstring, opt_r *r)
 {
-char *place;
-char c;
-optarg = NULL;
+    r->optarg = NULL;
+    r->optind = 0;
+    char *scanptr = NULL;
+    char *place = NULL;
+    char c;
         if(!scanptr || *scanptr =='\0')
         {
-            scanptr = argv[optind]+1;
-            optind++;
+            scanptr = argv[r->optind]+1;
+            r->optind++;
         }
-        if (optind == 0 || argv[optind][0] != '-' || !argv[optind][1])
+        if (r->optind == 0 || argv[r->optind][0] != '-' || !argv[r->optind][1])
         {
             return -1;
         }
-        if (argv[optind][1] == '-' && !argv[optind][2])
+        if (argv[r->optind][1] == '-' && !argv[r->optind][2])
         {
-            optind++;
+            r->optind++;
             return -1;
         }
-            c = *scanptr++;
+    c = *scanptr++;
     place = strchr(optstring, c);
 
-    if (!place || c == ':') {
-        fprintf(stderr, "%s: unknown option -%c\n", argv[0], c);
-        return '?';
-    }
+        if (!place || c == ':') {
+            fprintf(stderr, "%s: unknown option -%c\n", argv[0], c);
+            return '?';
+        }
 
     place++;
-
-    if (*place == ':') {
-        if (*scanptr != '\0') {
-            optarg = scanptr;
-            scanptr = NULL;
-        } else if( optind < argc ) {
-            optarg = argv[optind];
-            optind++;
-        } else {
-            fprintf(stderr, "%s: option requires argument -%c\n", argv[0], c);
-            return ':';
+        if (*place == ':') {
+            if (*scanptr != '\0') {
+                r->optarg = scanptr;
+                scanptr = NULL;
+            } else if( r->optind < argc ) {
+                r->optarg = argv[r->optind];
+                r->optind++;
+            } else {
+                fprintf(stderr, "%s: option requires argument -%c\n", argv[0], c);
+                return ':';
+            }
         }
-    }
     return c;
 }
--- cpu/atmega_common/include/getopt.h
+++ cpu/atmega_common/include/getopt.h
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2015 Mohmmad Ayman <Mohmmad.Salah94@eng-st.cu.edu.eg>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_atmega_common
+ * @{
+ *
+ * @file		header
+ * @brief       Implementation of getopt lib.
+ * @author      Mohmmad Ayman <Mohmmad.Salah94@eng-st.cu.edu.eg>
+ *
+ * @}
+ */
+
+typedef struct {
+    char *optarg;
+    int optind;
+    char *scanptr;
+} opt_r;
+
+
 #ifndef _GETOPT_H
 #define _GETOPT_H
 
@@ -5,9 +31,7 @@
 extern "C" {
 #endif
 
-int getopt(int, char * const [], const char *);
-
-
+int getopt(int argc, char *argv[], char *optstring);
 
 #ifdef __cplusplus
 }
