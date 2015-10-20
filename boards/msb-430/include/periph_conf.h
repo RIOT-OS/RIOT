a8502e2991928222744332ba096fc8db69baf22a
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-10-20 16:30:09 +0200

    boards/msb-430: adjusted to UART changes

--- boards/msb-430/include/periph_conf.h
+++ boards/msb-430/include/periph_conf.h
@@ -53,7 +53,7 @@ extern "C" {
 #define UART_NUMOF          (1U)
 #define UART_0_EN           (1U)
 
-#define UART_DEV            (USART_1)
+#define UART_BASE           (USART_1)
 #define UART_IE             (SFR->IE2)
 #define UART_IF             (SFR->IFG2)
 #define UART_IE_RX_BIT      (1 << 4)
