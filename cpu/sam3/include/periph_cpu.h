450562fe46a9431779b53c5b2cfce338949726db
TobiasFredersdorf <Tobias.Fredersdorf@haw-hamburg.de>
2015-10-21 16:13:25 +0200

    Updated the gpio peripheral driver implementation for sam3x CPUs.

--- cpu/sam3/include/periph_cpu.h
+++ cpu/sam3/include/periph_cpu.h
@@ -1,24 +1,6 @@
-/*
- * Copyright (C) 2015 Freie Universität Berlin
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
-
-/**
- * @ingroup         cpu_sam3x8e
- * @{
- *
- * @file
- * @brief           CPU specific definitions for internal peripheral handling
- *
- * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
- */
-
 #ifndef PERIPH_CPU_H_
 #define PERIPH_CPU_H_
-
+#include "cpu.h"
 #include "periph/dev_enums.h"
 
 #ifdef __cplusplus
@@ -26,7 +8,64 @@ extern "C" {
 #endif
 
 /**
- * @brief declare needed generic SPI functions
+ * @brief Define the the base-adress of the gpio
+ */
+#define PIO_BASE_ADRESS 0x400E0E00U
+
+/**
+ * @brief Overwrite the default gpio_t type definition
+ */
+#define HAVE_GPIO_T
+typedef uint32_t gpio_t;
+/** @} */
+
+/**
+ * @brief Definition of a fitting UNDEF value
+ */
+#define GPIO_UNDEF          (0xffffffff)
+
+/**
+ * @brief Define a CPU specific GPIO pin generator macro
+ */
+#define GPIO_PIN(x, y)          ((PIO_BASE_ADRESS + (x << 9)) | y)
+#define GPIO2(i)            ((PIOA + (i << 9)))
+
+/**
+ * @brief Override values for pull register configuration
+ * @{
+ */
+#define HAVE_GPIO_PP_T
+typedef enum {
+    GPIO_NOPULL = 4,        /**< do not use internal pull resistors */
+    GPIO_PULLUP = 9,        /**< enable internal pull-up resistor */
+    GPIO_PULLDOWN = 8       /**< enable internal pull-down resistor */
+} gpio_pp_t;
+/** @} */
+
+/**
+ * @brief Override flank configuration values
+ * @{
+ */
+#define HAVE_GPIO_FLANK_T
+typedef enum {
+    GPIO_RISING = 1,        /**< emit interrupt on rising flank */
+    GPIO_FALLING = 2,       /**< emit interrupt on falling flank */
+    GPIO_BOTH = 3           /**< emit interrupt on both flanks */
+} gpio_flank_t;
+/** @} */
+
+/**
+ * @brief Available ports on the SAM3X8E
+ */
+enum {
+    PORT_A = 0,             /**< port A */
+    PORT_B = 1,             /**< port B */
+    PORT_C = 2,             /**< port C */
+    PORT_D = 3,             /**< port D */
+};
+
+/**
+ * @brief Declare needed generic SPI functions
  * @{
  */
 #define PERIPH_SPI_NEEDS_TRANSFER_BYTES
