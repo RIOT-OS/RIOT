de52225c707b33dd45c00eeba91bcd3605a1ede9
Cenk Gündoğan <cnkgndgn@gmail.com>
2015-10-27 13:47:39 +0100

    sys: fmt: add basic unittests for fmt

---
+++ tests/unittests/tests-fmt/tests-fmt.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Cenk Gündoğan <cnkgndgn@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the ``fmt`` module
+ *
+ * @author      Cenk Gündoğan <cnkgndgn@gmail.com>
+ */
+#ifndef TESTS_FMT_H_
+#define TESTS_FMT_H_
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_fmt(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_FMT_H_ */
+/** @} */
