b6e22e49d61a8413a571cb6b7e522ae07ecae461
Hauke Petersen <hauke.petersen@fu-berlin.de>
2015-10-27 11:20:19 +0100

    cpu/sam3: re-added licence headers

--- cpu/sam3/include/periph_cpu.h
+++ cpu/sam3/include/periph_cpu.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2015 Freie Universität Berlin
+ *               2015 Hamburg University of Applied Sciences
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         cpu_sam3
+ * @{
+ *
+ * @file
+ * @brief           CPU specific definitions for internal peripheral handling
+ *
+ * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
+ *
+ */
+
 #ifndef PERIPH_CPU_H_
 #define PERIPH_CPU_H_
 #include "cpu.h"
--- cpu/sam3/periph/gpio.c
+++ cpu/sam3/periph/gpio.c
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2014 Hauke Petersen <mail@haukepetersen.de>
+ *               2015 Hamburg University of Applied Sciences
+ *               2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_sam3
+ * @{
+ *
+ * @file
+ * @brief       Low-level GPIO driver implementation
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Tobias Fredersdorf <tobias.fredersdorf@haw-hamburg.de>
+ *
+ * @}
+ */
+
 #include "cpu.h"
 #include "sched.h"
 #include "thread.h"
@@ -50,9 +73,9 @@ int gpio_init(gpio_t pin, gpio_dir_t dir, gpio_pp_t pushpull)
     Pio *port = _port(pin);
     int pin_num = _pin_num(pin);
 	int port_num = _port_num(pin);
-	
+
 	PMC->PMC_PCER0 = (1 << (port_num+11));
-	
+
 	 /* give the PIO module the power over the corresponding pin */
     port->PIO_PER = (1<<pin_num);
     /* configure the pin's pull resistor state */
@@ -84,11 +107,11 @@ int gpio_init_int(gpio_t pin, gpio_pp_t pushpull, gpio_flank_t flank, gpio_cb_t 
 	Pio *port = _port(pin);
 	int pin_num = _pin_num(pin);
 	int port_num = _port_num(pin);
-	
+
 	port->PIO_IDR = (1<<pin_num);
-	
+
 	gpio_init(pin, GPIO_DIR_IN, pushpull);
-	
+
 	/* set callback function and parameter */
     exti_ctx[port_num].cb = cb;
     exti_ctx[port_num].arg = arg;
@@ -109,9 +132,9 @@ int gpio_init_int(gpio_t pin, gpio_pp_t pushpull, gpio_flank_t flank, gpio_cb_t 
             port->PIO_AIMDR = (1<<pin_num);
             break;
     }
-	
+
 	NVIC_EnableIRQ((1 << (port_num+11)));
-	
+
     /* clean interrupt status register */
     port->PIO_ISR;
 
@@ -148,7 +171,7 @@ int gpio_read(gpio_t pin)
     if (res < -1) {
         res = 1;
     }
-	
+
 	return res;
 }
 
@@ -194,7 +217,7 @@ void isr_exti(void)
 
 void isr_pioa(void)
 {
-    
+
 	for(int i = 0; i < 32; i++) {
 		if (GPIO2(0)->PIO_ISR & (1 << i)) {
 			exti_ctx[0].cb(exti_ctx[0].arg);
@@ -207,7 +230,7 @@ void isr_pioa(void)
 
 void isr_piob(void)
 {
-    
+
 	for(int i = 0; i < 32; i++) {
 		if (GPIO2(1)->PIO_ISR & (1 << i)) {
 			exti_ctx[1].cb(exti_ctx[1].arg);
@@ -220,7 +243,7 @@ void isr_piob(void)
 
 void isr_pioc(void)
 {
-    
+
 	for(int i = 0; i < 32; i++) {
 		if (GPIO2(2)->PIO_ISR & (1 << i)) {
 			exti_ctx[2].cb(exti_ctx[2].arg);
@@ -233,7 +256,7 @@ void isr_pioc(void)
 
 void isr_piod(void)
 {
-    
+
 	for(int i = 0; i < 32; i++) {
 		if (GPIO2(3)->PIO_ISR & (1 << i)) {
 			exti_ctx[3].cb(exti_ctx[3].arg);
@@ -241,5 +264,5 @@ void isr_piod(void)
 	}
 	if (sched_context_switch_request) {
         thread_yield();
-        } 
+        }
 }
