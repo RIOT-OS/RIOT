3276450dd98b39b06a941ef164ed7aff6c5d93ac
Matt Poppe <matt@poppe.me>
2015-11-17 06:50:26 -0600

    Update test application

--- tests/driver_ksz8851snl/main.c
+++ tests/driver_ksz8851snl/main.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2008, 2009, 2010  Kaspar Schleiser <kaspar@schleiser.de>
- * Copyright (C) 2013 INRIA
- * Copyright (C) 2013 Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ * Copyright (C) 2015 Matt Poppe<matt@poppe.me>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -9,15 +7,13 @@
  */
 
 /**
- * @ingroup     examples
+ * @ingroup     tests
  * @{
  *
  * @file
- * @brief       Default application that shows a lot of functionality of RIOT
+ * @brief       Short test for the KSZ8851SNL Driver on Samr21-Xpro
  *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- * @author      Oliver Hahm <oliver.hahm@inria.fr>
- * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ * @author      Matt Poppe<matt@poppe.me>
  *
  * @}
  */
@@ -29,15 +25,6 @@
 #include "shell.h"
 #include "shell_commands.h"
 
-#if FEATURE_PERIPH_RTC
-#include "periph/rtc.h"
-#endif
-
-#ifdef MODULE_LTC4150
-#include "ltc4150.h"
-#endif
-
-
 #include "net/gnrc/pktdump.h"
 #include "net/gnrc.h"
 
@@ -46,13 +33,6 @@ msg_t msgq[MSG_QUEUE_SIZE];
 
 int main(void)
 {
-#ifdef MODULE_LTC4150
-    ltc4150_start();
-#endif
-
-#ifdef FEATURE_PERIPH_RTC
-    rtc_init();
-#endif
 
 
 //    gnrc_netreg_entry_t dump;
@@ -63,7 +43,7 @@ int main(void)
 
     msg_init_queue(msgq, MSG_QUEUE_SIZE);
 
-    (void) puts("Welcome to RIOT!");
+    (void) puts("RIOT KSZ8851SNL Driver Test!");
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
