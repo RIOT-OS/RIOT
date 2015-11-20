c4e55a2e6de27c4490cf45ae42638720d46d8fde
Kaspar Schleiser <kaspar@schleiser.de>
2015-11-20 01:06:17 +0100

    cpu: msp430-common: add unistd.h

---
+++ cpu/msp430-common/include/unistd.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef UNISTD_H_
+#define UNISTD_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define STDIN_FILENO    0   ///< stdin file descriptor
+#define STDOUT_FILENO   1   ///< stdout file descriptor
+#define STDERR_FILENO   2   ///< stderr file descriptor
+
+int close(int fildes);
+
+typedef uint32_t useconds_t;
+int usleep(useconds_t usec);
+unsigned int sleep(unsigned int seconds);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UNISTD_H_ */
