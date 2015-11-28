20f1750fb620d00a03da19897c94e6a51ecea75e
Oleg Hahm <oleg@hobbykeller.org>
2015-11-28 12:44:54 +0100

    cpu stm32fX: fix pedantic compiler warnings

--- cpu/stm32f1/periph/gpio.c
+++ cpu/stm32f1/periph/gpio.c
@@ -212,7 +212,7 @@ void gpio_write(gpio_t pin, int value)
 
 void isr_exti(void)
 {
-    for (int i = 0; i < GPIO_ISR_CHAN_NUMOF; i++) {
+    for (unsigned i = 0; i < GPIO_ISR_CHAN_NUMOF; i++) {
         if (EXTI->PR & (1 << i)) {
             EXTI->PR = (1 << i);        /* clear by writing a 1 */
             exti_ctx[i].cb(exti_ctx[i].arg);
