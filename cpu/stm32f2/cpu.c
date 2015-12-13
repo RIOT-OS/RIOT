e029fda202ad7046f0b2082edfb7080616e83d76
DipSwitch <dipswitch@ownage4u.nl>
2015-12-13 11:00:06 +0100

    add: STM32F2 CPU family

---
+++ cpu/stm32f2/cpu.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2015 Engineering-Spirit
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     cpu_stm32f2
+ * @{
+ *
+ * @file
+ * @brief       Implementation of the kernel cpu functions
+ *
+ * @author      Nick v. IJzendoorn <nijzendoorn@engineering-spirit.nl>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "periph_conf.h"
+
+#define CLOCK_FLASH_LATENCY         FLASH_ACR_LATENCY_3WS
+#define CLOCK_AHB_DIV               RCC_CFGR_HPRE_DIV1
+#define CLOCK_APB2_DIV              RCC_CFGR_PPRE2_DIV2
+#define CLOCK_APB1_DIV              RCC_CFGR_PPRE1_DIV4
+#define CLOCK_PLL_DIV               25
+#define CLOCK_PLL_MUL               240
+#define CLOCK_PLL_SYSCLK_DIV        2
+#define CLOCK_PLL_USB_SDO_RNG_DIV   5
+
+#ifdef HSI_VALUE
+# define RCC_CR_SOURCE          RCC_CR_HSION
+# define RCC_CR_SOURCE_RDY      RCC_CR_HSIRDY
+# define RCC_PLL_SOURCE         RCC_CFGR_PLLSRC_HSE
+#else
+# define RCC_CR_SOURCE          RCC_CR_HSEON
+# define RCC_CR_SOURCE_RDY      RCC_CR_HSERDY
+# define RCC_PLL_SOURCE         RCC_CFGR_PLLSRC_HSI
+#endif
+
+static void clk_init(void);
+
+void cpu_init(void)
+{
+    /* initialize the Cortex-M core */
+    cortexm_init();
+    /* initialize system clocks */
+    clk_init();
+}
+
+/**
+ * @brief Configure the clock system of the stm32f2
+ *
+ */
+static void clk_init(void)
+{
+    /* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+    /* Set HSION bit */
+    RCC->CR |= 0x00000001U;
+    /* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+    RCC->CFGR = 0x00000000U;
+    /* Reset HSEON, CSSON and PLLON bits */
+    RCC->CR &= 0xFEF6FFFFU;
+    /* Reset PLLCFGR register */
+    RCC->PLLCFGR = 0x24003010U;
+    /* Reset HSEBYP bit */
+    RCC->CR &= 0xFFFBFFFFU;
+    /* Disable all interrupts and clear pending bits  */
+    RCC->CIR = 0x00000000U;
+
+    /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration */
+    /* Enable the high speed clock source */
+    RCC->CR |= RCC_CR_SOURCE;
+    /* Wait till hish speed clock source is ready,
+     * NOTE: the MCU will stay here forever if no HSE clock is connected */
+    while ((RCC->CR & RCC_CR_SOURCE_RDY) == 0);
+    /* Configure Flash prefetch, Instruction cache, Data cache and wait state */
+    FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN;
+    /* Flash 2 wait state */
+    FLASH->ACR &= ~((uint32_t)FLASH_ACR_LATENCY);
+    FLASH->ACR |= (uint32_t)CLOCK_FLASH_LATENCY;
+    /* HCLK = SYSCLK */
+    RCC->CFGR |= (uint32_t)CLOCK_AHB_DIV;
+    /* PCLK2 = HCLK */
+    RCC->CFGR |= (uint32_t)CLOCK_APB2_DIV;
+    /* PCLK1 = HCLK */
+    RCC->CFGR |= (uint32_t)CLOCK_APB1_DIV;
+    /*  PLL configuration: PLLCLK = SOURCE_CLOCK / SOURCE_CLOCK_DIV * SOURCE_CLOCK_MUL */
+    RCC->CFGR &= ~((uint32_t)(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
+    RCC->CFGR |= (uint32_t)(RCC_PLL_SOURCE | CLOCK_PLL_DIV | (CLOCK_PLL_MUL << 6)
+                 | (((CLOCK_PLL_SYSCLK_DIV >> 1) -1) << 16) | (CLOCK_PLL_USB_SDO_RNG_DIV << 24));
+    /* Enable PLL */
+    RCC->CR |= RCC_CR_PLLON;
+    /* Wait till PLL is ready */
+    while ((RCC->CR & RCC_CR_PLLRDY) == 0);
+    /* Select PLL as system clock source */
+    RCC->CFGR &= ~((uint32_t)(RCC_CFGR_SW));
+    RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+    /* Wait till PLL is used as system clock source */
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+}
---
+++ cpu/stm32f2/periph/gpio.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2014-2015 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     cpu_stm32f2
+ * @{
+ *
+ * @file
+ * @brief       Low-level GPIO driver implementation
+ *
+ * @author      Hauke Petersen <mail@haukepetersen.de>
+ * @author      Thomas Eichinger <thomas.eichinger@fu-berlin.de>
+ * @author      Nick v. IJzendoorn <nijzendoorn@engineering-spirit.nl>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "sched.h"
+#include "thread.h"
+#include "periph/gpio.h"
+#include "periph_conf.h"
+
+/**
+ * @brief   Number of available external interrupt lines
+ */
+#define GPIO_ISR_CHAN_NUMOF             (16U)
+
+/**
+ * @brief   Datastructure to hold an interrupt context
+ */
+typedef struct {
+    void (*cb)(void *arg);      /**< interrupt callback routine */
+    void *arg;                  /**< optional argument */
+} exti_ctx_t;
+
+/**
+ * @brief   Hold one callback function pointer for each interrupt line
+ */
+static exti_ctx_t exti_chan[GPIO_ISR_CHAN_NUMOF];
+
+/**
+ * @brief   Extract the port base address from the given pin identifier
+ */
+static inline GPIO_TypeDef *_port(gpio_t pin)
+{
+    return (GPIO_TypeDef *)(pin & ~(0x0f));
+}
+
+/**
+ * @brief   Extract the port number form the given identifier
+ *
+ * The port number is extracted by looking at bits 10, 11, 12, 13 of the base
+ * register addresses.
+ */
+static inline int _port_num(gpio_t pin)
+{
+    return ((pin >> 10) & 0x0f);
+}
+
+/**
+ * @brief   Extract the pin number from the last 4 bit of the pin identifier
+ */
+static inline int _pin_num(gpio_t pin)
+{
+    return (pin & 0x0f);
+}
+
+int gpio_init(gpio_t pin, gpio_dir_t dir, gpio_pp_t pullup)
+{
+    GPIO_TypeDef *port = _port(pin);
+    int pin_num = _pin_num(pin);
+
+    /* enable clock */
+    RCC->AHB1ENR |= (RCC_AHB1ENR_GPIOAEN << _port_num(pin));
+    /* configure pull register */
+    port->PUPDR &= ~(3 << (2 * pin_num));
+    port->PUPDR |= (pullup << (2 * pin_num));
+    /* set direction */
+    if (dir == GPIO_DIR_OUT) {
+        port->MODER &= ~(3 << (2 * pin_num));   /* set pin to output mode */
+        port->MODER |= (1 << (2 * pin_num));
+        port->OTYPER &= ~(1 << pin_num);        /* set to push-pull */
+        port->OSPEEDR |= (3 << (2 * pin_num));  /* set to high speed */
+        port->ODR &= ~(1 << pin_num);           /* set pin to low signal */
+    }
+    else {
+        port->MODER &= ~(3 << (2 * pin_num));   /* configure pin as input */
+    }
+    return 0;
+}
+
+int gpio_init_int(gpio_t pin,
+                   gpio_pp_t pullup, gpio_flank_t flank,
+                   gpio_cb_t cb, void *arg)
+{
+    int pin_num = _pin_num(pin);
+    int port_num = _port_num(pin);
+
+    /* configure and save exti configuration struct */
+    exti_chan[pin_num].cb = cb;
+    exti_chan[pin_num].arg = arg;
+    /* enable the SYSCFG clock */
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    /* initialize pin as input */
+    gpio_init(pin, GPIO_DIR_IN, pullup);
+    /* enable global pin interrupt */
+    if (pin_num < 5) {
+        NVIC_EnableIRQ(EXTI0_IRQn + pin_num);
+    }
+    else if (pin_num < 10) {
+        NVIC_EnableIRQ(EXTI9_5_IRQn);
+    }
+    else {
+        NVIC_EnableIRQ(EXTI15_10_IRQn);
+    }
+    /* configure the active edge(s) */
+    switch (flank) {
+        case GPIO_RISING:
+            EXTI->RTSR |= (1 << pin_num);
+            EXTI->FTSR &= ~(1 << pin_num);
+            break;
+        case GPIO_FALLING:
+            EXTI->RTSR &= ~(1 << pin_num);
+            EXTI->FTSR |= (1 << pin_num);
+            break;
+        case GPIO_BOTH:
+            EXTI->RTSR |= (1 << pin_num);
+            EXTI->FTSR |= (1 << pin_num);
+            break;
+    }
+    /* enable specific pin as exti sources */
+    SYSCFG->EXTICR[pin_num >> 2] &= ~(0xf << ((pin_num & 0x03) * 4));
+    SYSCFG->EXTICR[pin_num >> 2] |= (port_num << ((pin_num & 0x03) * 4));
+    /* clear any pending requests */
+    EXTI->PR = (1 << pin_num);
+    /* enable interrupt for EXTI line */
+    EXTI->IMR |= (1 << pin_num);
+    return 0;
+}
+
+void gpio_init_af(gpio_t pin, gpio_af_t af)
+{
+    GPIO_TypeDef *port = _port(pin);
+    uint32_t pin_num = _pin_num(pin);
+
+    /* set pin to AF mode */
+    port->MODER &= ~(3 << (2 * pin_num));
+    port->MODER |= (2 << (2 * pin_num));
+    /* set selected function */
+    port->AFR[(pin_num > 7) ? 1 : 0] &= ~(0xf << ((pin_num & 0x07) * 4));
+    port->AFR[(pin_num > 7) ? 1 : 0] |= (af << ((pin_num & 0x07) * 4));
+}
+
+void gpio_irq_enable(gpio_t pin)
+{
+    EXTI->IMR |= (1 << _pin_num(pin));
+}
+
+void gpio_irq_disable(gpio_t pin)
+{
+    EXTI->IMR &= ~(1 << _pin_num(pin));
+}
+
+int gpio_read(gpio_t pin)
+{
+    GPIO_TypeDef *port = _port(pin);
+    uint32_t pin_num = _pin_num(pin);
+
+    if (port->MODER & (3 << (pin_num * 2))) {   /* if configured as output */
+        return port->ODR & (1 << pin_num);      /* read output data reg */
+    } else {
+        return port->IDR & (1 << pin_num);      /* else read input data reg */
+    }
+}
+
+void gpio_set(gpio_t pin)
+{
+    _port(pin)->BSRRL = (1 << _pin_num(pin));
+}
+
+void gpio_clear(gpio_t pin)
+{
+    _port(pin)->BSRRH = (1 << _pin_num(pin));
+}
+
+void gpio_toggle(gpio_t pin)
+{
+    if (gpio_read(pin)) {
+        gpio_clear(pin);
+    } else {
+        gpio_set(pin);
+    }
+}
+
+void gpio_write(gpio_t pin, int value)
+{
+    if (value) {
+        gpio_set(pin);
+    } else {
+        gpio_clear(pin);
+    }
+}
+
+void isr_exti(void)
+{
+    for (unsigned i = 0; i < GPIO_ISR_CHAN_NUMOF; i++) {
+        if (EXTI->PR & (1 << i)) {
+            EXTI->PR |= (1 << i);               /* clear by writing a 1 */
+            exti_chan[i].cb(exti_chan[i].arg);
+        }
+    }
+    if (sched_context_switch_request) {
+        thread_yield();
+    }
+}
