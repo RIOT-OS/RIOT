558f211e2cf3fd667550498942744c469015195d
René Kijewski <rene.kijewski@fu-berlin.de>
2015-12-28 15:51:11 +0100

    core: fix mutex debug messages

--- core/mutex.c
+++ core/mutex.c
@@ -38,14 +38,15 @@ static void mutex_wait(struct mutex_t *mutex);
 
 int mutex_trylock(struct mutex_t *mutex)
 {
-    DEBUG("%s: trylocking to get mutex. val: %u\n", sched_active_thread->name, ATOMIC_VALUE(mutex->val));
+    DEBUG("mutex_trylock: pid=%" PRIkernel_pid " val=%u\n",
+          sched_active_pid, ATOMIC_VALUE(mutex->val));
     return atomic_set_to_one(&mutex->val);
 }
 
 void mutex_lock(struct mutex_t *mutex)
 {
-    DEBUG("%s: trying to get mutex. val: %u\n", sched_active_thread->name, ATOMIC_VALUE(mutex->val));
-
+    DEBUG("mutex_lock: pid=%" PRIkernel_pid " val=%u\n",
+          sched_active_pid, ATOMIC_VALUE(mutex->val));
     if (atomic_set_to_one(&mutex->val) == 0) {
         /* mutex was locked. */
         mutex_wait(mutex);
@@ -55,11 +56,11 @@ void mutex_lock(struct mutex_t *mutex)
 static void mutex_wait(struct mutex_t *mutex)
 {
     unsigned irqstate = disableIRQ();
-    DEBUG("%s: Mutex in use. %u\n", sched_active_thread->name, ATOMIC_VALUE(mutex->val));
 
     if (atomic_set_to_one(&mutex->val)) {
         /* somebody released the mutex. return. */
-        DEBUG("%s: mutex_wait early out. %u\n", sched_active_thread->name, ATOMIC_VALUE(mutex->val));
+        DEBUG("mutex_wait: early out, pid=%" PRIkernel_pid " val=%u\n",
+              sched_active_pid, ATOMIC_VALUE(mutex->val));
         restoreIRQ(irqstate);
         return;
     }
@@ -71,7 +72,9 @@ static void mutex_wait(struct mutex_t *mutex)
     n.data = (unsigned int) sched_active_thread;
     n.next = NULL;
 
-    DEBUG("%s: Adding node to mutex queue: prio: %" PRIu32 "\n", sched_active_thread->name, n.priority);
+    DEBUG("mutex_wait: add to wait queue, pid=%" PRIkernel_pid " val=%u "
+          "prio%" PRIu32 "\n", sched_active_pid, ATOMIC_VALUE(mutex->val),
+          n.priority);
 
     priority_queue_add(&(mutex->queue), &n);
 
@@ -85,7 +88,8 @@ static void mutex_wait(struct mutex_t *mutex)
 void mutex_unlock(struct mutex_t *mutex)
 {
     unsigned irqstate = disableIRQ();
-    DEBUG("mutex_unlock(): val: %u pid: %" PRIkernel_pid "\n", ATOMIC_VALUE(mutex->val), sched_active_pid);
+    DEBUG("mutex_unlock: pid=%" PRIkernel_pid " val=%u\n",
+          sched_active_pid, ATOMIC_VALUE(mutex->val));
 
     if (ATOMIC_VALUE(mutex->val) == 0) {
         /* the mutex was not locked */
@@ -102,7 +106,7 @@ void mutex_unlock(struct mutex_t *mutex)
     }
 
     tcb_t *process = (tcb_t *) next->data;
-    DEBUG("mutex_unlock: waking up waiting thread %" PRIkernel_pid "\n", process->pid);
+    DEBUG("mutex_unlock: waking up %" PRIkernel_pid "\n", process->pid);
     sched_set_status(process, STATUS_PENDING);
 
     uint16_t process_priority = process->priority;
@@ -112,21 +116,24 @@ void mutex_unlock(struct mutex_t *mutex)
 
 void mutex_unlock_and_sleep(struct mutex_t *mutex)
 {
-    DEBUG("%s: unlocking mutex. val: %u pid: %" PRIkernel_pid ", and taking a nap\n", sched_active_thread->name, ATOMIC_VALUE(mutex->val), sched_active_pid);
+    DEBUG("mutex_unlock_and_sleep: pid=%" PRIkernel_pid " val=%u\n",
+          sched_active_pid, ATOMIC_VALUE(mutex->val));
     unsigned irqstate = disableIRQ();
 
     if (ATOMIC_VALUE(mutex->val) != 0) {
         priority_queue_node_t *next = priority_queue_remove_head(&(mutex->queue));
         if (next) {
             tcb_t *process = (tcb_t *) next->data;
-            DEBUG("%s: waking up waiter.\n", process->name);
+            DEBUG("mutex_unlock_and_sleep: waking up %" PRIkernel_pid "\n",
+                  process->pid);
             sched_set_status(process, STATUS_PENDING);
         }
         else {
             ATOMIC_VALUE(mutex->val) = 0; /* This is safe, interrupts are disabled */
         }
     }
-    DEBUG("%s: going to sleep.\n", sched_active_thread->name);
+
+    DEBUG("mutex_unlock_and_sleep: going to sleep.\n");
     sched_set_status((tcb_t*) sched_active_thread, STATUS_SLEEPING);
     restoreIRQ(irqstate);
     thread_yield_higher();
