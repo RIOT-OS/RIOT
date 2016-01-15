7e99e5135adf361a3cacaee454a5ae6435aa42c3
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-01-15 17:45:44 +0100

    sys/auto_init: added sequence for hdc1000 sensors

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -298,6 +298,10 @@ void auto_init(void)
     extern void auto_init_jc42(void);
     auto_init_jc42();
 #endif
+#ifdef MODULE_HDC1000
+    extern void auto_init_hdc1000(void);
+    auto_init_hdc1000();
+#endif
 
 #endif /* MODULE_AUTO_INIT_SAUL */
 
