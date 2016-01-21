8048c7d14ea592cb384c00234a9eecec722f99f9
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-01-21 15:31:13 +0100

    tests/driver_dht: cleanup Makefile and use auto_init

--- tests/driver_dht/main.c
+++ tests/driver_dht/main.c
@@ -19,46 +19,34 @@
  * @}
  */
 
-#ifndef DHT_TYPE
-#error "DHT_TYPE not defined"
-#endif
-
-#ifndef DHT_GPIO
-#error "DHT_GPIO not defined"
-#endif
-
 #include <stdio.h>
 
 #include "xtimer.h"
-
 #include "dht.h"
+#include "dht_params.h"
+
+extern dht_t dht_devs[];
 
 int main(void)
 {
-    dht_t dev;
+    dht_data_t data;
+    float temp, hum;
 
     puts("DHT temperature and humidity sensor test application\n");
 
-    printf("Initializing DHT sensor at GPIO_%ld... ", (long)DHT_GPIO);
-    if (dht_init(&dev, DHT_TYPE, DHT_GPIO) == 0) {
-        puts("[OK]\n");
-    }
-    else {
-        puts("[Failed]");
-        return 1;
-    }
-
-    dht_data_t data;
-    float temp, hum;
+    /* periodically read temp and humidity values */
     while (1) {
+        for (unsigned i = 0; i < DHT_NUMOF; i++) {
+            dht_t *dev = &dht_devs[i];
 
-        if (dht_read_raw(&dev, &data) == -1) {
-            puts("error reading data");
+            if (dht_read_raw(dev, &data) == -1) {
+                puts("error reading data");
+            }
+            dht_parse(dev, &data, &hum, &temp);
+            printf("raw relative humidity: %i\nraw temperature: %i C\n", data.humidity, data.temperature);
+            printf("relative humidity: %i\ntemperature: %i C\n", (int) hum, (int) temp);
+            xtimer_usleep(2000 * MS_IN_USEC);
         }
-        dht_parse(&dev, &data, &hum, &temp);
-        printf("raw relative humidity: %i\nraw temperature: %i C\n", data.humidity, data.temperature);
-        printf("relative humidity: %i\ntemperature: %i C\n", (int) hum, (int) temp);
-        xtimer_usleep(2000 * MS_IN_USEC);
     }
 
     return 0;
