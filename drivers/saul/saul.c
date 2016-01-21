d1d7192d93169f06e1dde10be8fb27fa2b8a1d57
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-01-21 22:32:26 +0100

    drivers/saul: added default NOTSUP function

---
+++ drivers/saul/saul.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_saul
+ * @{
+ *
+ * @file
+ * @brief       SAUL fallback functions
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <errno.h>
+
+#include "saul.h"
+
+int saul_notsup(void *dev, phydat_t *dat)
+{
+    (void)dev;
+    (void)dat;
+    return -ENOTSUP;
+}
