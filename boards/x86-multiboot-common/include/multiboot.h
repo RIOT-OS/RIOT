6d7b0a2ea2d6c8f4ebc5576278891cfd0b12b26e
Oleg Hahm <oleg@hobbykeller.org>
2016-01-27 16:10:35 +0100

    license: updated Free Software Foundation address

--- boards/x86-multiboot-common/include/multiboot.h
+++ boards/x86-multiboot-common/include/multiboot.h
@@ -11,8 +11,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 /**
  * @defgroup    boards_x86-multiboot i586 multiboot common
