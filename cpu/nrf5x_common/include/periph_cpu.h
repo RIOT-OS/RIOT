33132c8a607b5a931cd37f25b58c1b06ec69516a
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-02-07 12:52:55 +0100

    cpu: added common cpu for the nRF5x family

--- cpu/nrf5x_common/include/periph_cpu.h
+++ cpu/nrf5x_common/include/periph_cpu.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Freie Universität Berlin
+ * Copyright (C) 2015-2016 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -7,7 +7,7 @@
  */
 
 /**
- * @ingroup         cpu_nrf51822
+ * @ingroup         cpu_nrf5x_common
  * @{
  *
  * @file
@@ -16,26 +16,14 @@
  * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
  */
 
-#ifndef CPU_PERIPH_H_
-#define CPU_PERIPH_H_
+#ifndef CPU_PERIPH_H
+#define CPU_PERIPH_H
 
-#include "periph/dev_enums.h"
+#include "cpu.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * @brief   Mandatory macro for defining GPIO pins
- *
- * The port definition is used (and zeroed) to suppress compiler warnings
- */
-#define GPIO_PIN(x,y)       ((x & 0) | y)
-
-/**
- * @brief   Length of the CPU_ID in octets
- */
-#define CPUID_LEN           (8U)
 
 /**
  * @brief   Override GPIO pull register select values
@@ -44,7 +32,7 @@ extern "C" {
 #define HAVE_GPIO_PP_T
 typedef enum {
     GPIO_NOPULL = 0,        /**< do not use internal pull resistors */
-    GPIO_PULLUP = 2,        /**< enable internal pull-up resistor */
+    GPIO_PULLUP = 3,        /**< enable internal pull-up resistor */
     GPIO_PULLDOWN = 1       /**< enable internal pull-down resistor */
 } gpio_pp_t;
 /** @} */
@@ -61,9 +49,21 @@ typedef enum {
 } gpio_flank_t;
 /** @} */
 
+/**
+ * @brief   Override macro for defining GPIO pins
+ *
+ * The port definition is used (and zeroed) to suppress compiler warnings
+ */
+#define GPIO_PIN(x,y)       ((x & 0) | y)
+
+/**
+ * @brief   Length of the CPU_ID in octets
+ */
+#define CPUID_LEN           (8U)
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* CPU_PERIPH_H_ */
+#endif /* CPU_PERIPH_H */
 /** @} */
