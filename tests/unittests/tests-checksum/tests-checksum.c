889adc18b850f999208f307cf0fff71ffdec8628
Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
2016-02-13 09:52:53 +0100

    unittests: add fletcher* tests

--- tests/unittests/tests-checksum/tests-checksum.c
+++ tests/unittests/tests-checksum/tests-checksum.c
@@ -11,4 +11,6 @@
 void tests_checksum(void)
 {
     TESTS_RUN(tests_checksum_crc16_ccitt_tests());
+    TESTS_RUN(tests_checksum_fletcher16_tests());
+    TESTS_RUN(tests_checksum_fletcher32_tests());
 }
