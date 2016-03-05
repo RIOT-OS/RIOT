a9ef72ff022cdfae9a0679e7457aa2b713a37211
test <test@localhost>
2016-03-05 16:02:35 +0100

    tests: make use of testrunner

--- tests/unittests/tests/01-run.py
+++ tests/unittests/tests/01-run.py
@@ -1,38 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# Copyright (C) 2014 Martine Lenders <mlenders@inf.fu-berlin.de>
+# Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
 #
 # This file is subject to the terms and conditions of the GNU Lesser
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-import os, signal, sys, subprocess
-from pexpect import spawn, TIMEOUT, EOF
+import os
+import sys
 
-DEFAULT_TIMEOUT = 5
+sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
+import testrunner
 
-def main():
-    env = os.environ.copy()
-    child = spawn("make term", env=env, timeout=DEFAULT_TIMEOUT,
-                  encoding="utf-8")
-    child.logfile = sys.stdout
-
-    try:
-        subprocess.check_output(('make', 'reset'), env=env,
-                                stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        # make reset yields error on some boards even if successful
-        pass
-    try:
-        child.expect(u"OK \\([0-9]+ tests\\)")
-    except TIMEOUT:
-        print("There where errors in the unittests")
-        return 1
-    finally:
-        print("")
-        child.close()
-
-    return 0
+def testfunc(child):
+    child.expect(u"OK \\([0-9]+ tests\\)")
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(testrunner.run(testfunc))
