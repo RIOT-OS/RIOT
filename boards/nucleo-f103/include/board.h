0ccadc0f7beb3757ec37fad1c30433c3c2330727
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-11 15:01:58 +0100

    boards/nucleo: added and use common board folder

--- boards/nucleo-f103/include/board.h
+++ boards/nucleo-f103/include/board.h
@@ -21,10 +21,7 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
-#include <stdint.h>
-
-#include "cpu.h"
-#include "periph_conf.h"
+#include "board_common.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,40 +38,6 @@ extern "C" {
 #define XTIMER_MASK         (0xffff0000)
 #define XTIMER_BACKOFF      5
 /** @} */
-
-/**
- * @name LED pin definitions
- * @{
- */
-#define LED_GREEN_GPIO      GPIO_PIN(PORT_A, 5)
-/** @} */
-
-/**
- * @name Macros for controlling the on-board LEDs.
- * @{
- */
-#define LED_RED_ON
-#define LED_RED_OFF
-#define LED_RED_TOGGLE
-
-#define LED_GREEN_ON        (GPIOA->BSRR = (1 << 5))
-#define LED_GREEN_OFF       (GPIOA->BRR = (1 << 5))
-#define LED_GREEN_TOGGLE    (GPIOA->ODR ^= (1 << 5))
-
-#define LED_ORANGE_ON
-#define LED_ORANGE_OFF
-#define LED_ORANGE_TOGGLE
-/** @} */
-
-/**
- * @brief   User button
- */
-#define BTN_B1_PIN          GPIO_PIN(PORT_C, 13)
-
-/**
- * @brief Initialize board specific hardware, including clock, LEDs and std-IO
- */
-void board_init(void);
 
 #ifdef __cplusplus
 }
