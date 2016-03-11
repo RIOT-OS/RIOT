45d059cb24a5776c2aa426238f02b52495e0880d
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-11 18:04:27 +0100

    boards/telosb: unified LED defines

--- boards/telosb/include/board.h
+++ boards/telosb/include/board.h
@@ -28,6 +28,8 @@
 #ifndef TELOSB_BOARD_H_
 #define TELOSB_BOARD_H_
 
+#include "cpu.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -55,24 +57,31 @@ extern "C" {
 #define MSP430_HAS_DCOR             0
 #define MSP430_HAS_EXTERNAL_CRYSTAL 1
 
-/* LEDs ports MSB430 */
-#define LEDS_PxDIR P5DIR
-#define LEDS_PxOUT P5OUT
-#define LEDS_CONF_RED       0x10
-#define LEDS_CONF_GREEN     0x20
-#define LEDS_CONF_BLUE      0x40
+/**
+ * @brief   LED pin definitions and handlers
+ * @{
+ */
+#define LED0_PIN                    GPIO_PIN(4, 0)
+#define LED1_PIN                    GPIO_PIN(4, 1)
+#define LED2_PIN                    GPIO_PIN(4, 2)
 
-#define LED_RED_ON          LEDS_PxOUT &=~LEDS_CONF_RED
-#define LED_RED_OFF         LEDS_PxOUT |= LEDS_CONF_RED
-#define LED_RED_TOGGLE      LEDS_PxOUT ^= LEDS_CONF_RED
+#define LED_OUT_REG                 P5OUT
+#define LED0_MASK                   (0x10)
+#define LED1_MASK                   (0x20)
+#define LED2_MASK                   (0x40)
 
-#define LED_GREEN_ON        LEDS_PxOUT &=~LEDS_CONF_GREEN
-#define LED_GREEN_OFF       LEDS_PxOUT |= LEDS_CONF_GREEN
-#define LED_GREEN_TOGGLE    LEDS_PxOUT ^= LEDS_CONF_GREEN
+#define LED0_ON                     (LED_OUT_REG &=~LED0_MASK)
+#define LED0_OFF                    (LED_OUT_REG |= LED0_MASK)
+#define LED0_TOGGLE                 (LED_OUT_REG ^= LED0_MASK)
 
-#define LED_BLUE_ON         LEDS_PxOUT &=~LEDS_CONF_BLUE
-#define LED_BLUE_OFF        LEDS_PxOUT |= LEDS_CONF_BLUE
-#define LED_BLUE_TOGGLE     LEDS_PxOUT ^= LEDS_CONF_BLUE
+#define LED1_ON                     (LED_OUT_REG &=~LED1_MASK)
+#define LED1_OFF                    (LED_OUT_REG |= LED1_MASK)
+#define LED1_TOGGLE                 (LED_OUT_REG ^= LED1_MASK)
+
+#define LED2_ON                     (LED_OUT_REG &=~LED2_MASK)
+#define LED2_OFF                    (LED_OUT_REG |= LED2_MASK)
+#define LED2_TOGGLE                 (LED_OUT_REG ^= LED2_MASK)
+/** @} */
 
 #ifdef __cplusplus
 }
