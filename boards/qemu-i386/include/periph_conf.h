dfefabbef04b5e91c9544ae49192162d72e3bc1a
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-15 18:37:15 +0100

    boards/qemu-i386: added missing periph_conf.h

---
+++ boards/qemu-i386/include/periph_conf.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_qemu-i386
+ * @{
+ *
+ * @file
+ * @brief       Peripheral configuration for the qemu-i386
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* nothing to define here, yet */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
