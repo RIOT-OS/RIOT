462d1568218f8ef166a9eaaebe7f29f0241d320c
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-03-16 12:13:47 +0100

    cpu/stm32_common: added periph clk en/dis functions

--- cpu/stm32_common/include/periph_cpu_common.h
+++ cpu/stm32_common/include/periph_cpu_common.h
@@ -43,9 +43,25 @@ extern "C" {
  * @brief   Available peripheral buses
  */
 enum {
-    APB1,
-    APB2
+    APB1,           /**< APB1 bus */
+    APB2            /**< APB2 bus */
 };
+
+/**
+ * @brief   Enable the given peripheral clock
+ *
+ * @param[in] bus       bus the peripheral is connected to
+ * @param[in] mask      bit in the RCC enable register
+ */
+void periph_clk_en(uint8_t bus, uint32_t mask);
+
+/**
+ * @brief   Disable the given peripheral clock
+ *
+ * @param[in] bus       bus the peripheral is connected to
+ * @param[in] mask      bit in the RCC enable register
+ */
+void periph_clk_dis(uint8_t bus, uint32_t mask);
 
 #ifdef __cplusplus
 }
