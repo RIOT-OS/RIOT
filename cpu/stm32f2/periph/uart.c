182be862bb0abbd54517fb28b2d8778578a7ff95
Hermann Lelong <hermann@otakeys.com>
2016-04-01 16:38:49 +0200

    stm32f2: Improve UART driver
    Add missing DMA interrupts for UART
    Improve baudrate intialization:
     - Return error if baudrate is theorically unreachable
     - Implement oversampling by 8 method for high baudrates
    Add UART hardware flow control support
    Ensure uart tx thread safety with a mutex
    Allow setting of pins mode per UART

--- cpu/stm32f2/periph/uart.c
+++ cpu/stm32f2/periph/uart.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014-2015 Freie Universität Berlin
+ * Copyright (C) 2016 OTA keys
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -15,6 +16,8 @@
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      Fabian Nack <nack@inf.fu-berlin.de>
+ * @author      Hermann Lelong <hermann@otakeys.com>
+ * @author      Toon Stegen <toon.stegen@altran.com>
  *
  * @}
  */
@@ -25,6 +28,9 @@
 #include "mutex.h"
 #include "periph/uart.h"
 #include "periph/gpio.h"
+
+#define ENABLE_DEBUG    (0)
+#include "debug.h"
 
 /**
  * @brief   Allocate memory to store the callback functions
@@ -44,6 +50,8 @@ static inline USART_TypeDef *_dev(uart_t uart)
  */
 static mutex_t tx_sync[UART_NUMOF];
 
+static mutex_t tx_lock[UART_NUMOF];
+
 /**
  * @brief   Find out which peripheral bus the UART device is connected to
  *
@@ -62,10 +70,25 @@ int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
     float divider;
     uint16_t mantissa;
     uint8_t fraction;
+    uint32_t max_clock;
+    uint8_t over8;
 
     /* check if given UART device does exist */
     if (uart < 0 || uart >= UART_NUMOF) {
         return -1;
+    }
+
+    /* check if baudrate is reachable and choose the right oversampling method*/
+    max_clock = (_bus(uart) == 1) ? CLOCK_APB1 : CLOCK_APB2;
+
+    if (baudrate < (max_clock / 16)) {
+        over8 = 0;
+    }
+    else if (baudrate < (max_clock / 8)) {
+        over8 = 1;
+    }
+    else {
+        return -2;
     }
 
     /* get UART base address */
@@ -76,29 +99,40 @@ int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
     /* init tx lock */
     mutex_init(&tx_sync[uart]);
     mutex_lock(&tx_sync[uart]);
+    mutex_init(&tx_lock[uart]);
 
     /* configure pins */
-    gpio_init(uart_config[uart].rx_pin, GPIO_IN);
-    gpio_init(uart_config[uart].tx_pin, GPIO_OUT);
+    gpio_init(uart_config[uart].rx_pin, uart_config[uart].rx_mode);
+    gpio_init(uart_config[uart].tx_pin, uart_config[uart].tx_mode);
     gpio_init_af(uart_config[uart].rx_pin, uart_config[uart].af);
     gpio_init_af(uart_config[uart].tx_pin, uart_config[uart].af);
     /* enable UART clock */
     uart_poweron(uart);
 
     /* calculate and set baudrate */
-    if (_bus(uart) == 1) {
-        divider = CLOCK_APB1 / (16 * baudrate);
-    }
-    else {
-        divider = CLOCK_APB2 / (16 * baudrate);
-    }
+    divider = max_clock / (8 * (2 - over8) * baudrate);
+
     mantissa = (uint16_t)divider;
-    fraction = (uint8_t)((divider - mantissa) * 16);
-    dev->BRR = ((mantissa & 0x0fff) << 4) | (0x0f & fraction);
-    /* configure UART to 8N1 and enable receive and transmit mode */
+    fraction = (uint8_t)((divider - mantissa) * (8 * (2 - over8)));
+    dev->BRR = ((mantissa & 0x0fff) << 4) | (0x07 & fraction);
+    /* configure UART to 8N1 and enable receive and transmit mode*/
+    dev->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
+    if (over8) {
+        dev->CR1 |= USART_CR1_OVER8;
+    }
     dev->CR3 = USART_CR3_DMAT;
     dev->CR2 = 0;
-    dev->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
+
+    if(uart_config[uart].hw_flow_ctrl) {
+        gpio_init(uart_config[uart].cts_pin, uart_config[uart].cts_mode);
+        gpio_init(uart_config[uart].rts_pin, uart_config[uart].rts_mode);
+        gpio_init_af(uart_config[uart].cts_pin, uart_config[uart].af);
+        gpio_init_af(uart_config[uart].rts_pin, uart_config[uart].af);
+        DEBUG("Init flow control on uart %u\n", uart);
+        /* configure hardware flow control */
+        dev->CR3 |= USART_CR3_RTSE | USART_CR3_CTSE;
+    }
+
     /* configure the DMA stream for transmission */
     dma_poweron(uart_config[uart].dma_stream);
     stream = dma_stream(uart_config[uart].dma_stream);
@@ -128,6 +162,7 @@ void uart_write(uart_t uart, const uint8_t *data, size_t len)
         }
     }
     else {
+        mutex_lock(&tx_lock[uart]);
         DMA_Stream_TypeDef *stream = dma_stream(uart_config[uart].dma_stream);
         /* configure and start DMA transfer */
         stream->M0AR = (uint32_t)data;
@@ -135,6 +170,7 @@ void uart_write(uart_t uart, const uint8_t *data, size_t len)
         stream->CR |= DMA_SxCR_EN;
         /* wait for transfer to complete */
         mutex_lock(&tx_sync[uart]);
+        mutex_unlock(&tx_lock[uart]);
     }
 }
 
@@ -213,12 +249,22 @@ void UART_2_ISR(void)
 {
     irq_handler(2, uart_config[2].dev);
 }
+
+void UART_2_DMA_ISR(void)
+{
+    dma_handler(2, uart_config[2].dma_stream);
+}
 #endif
 
 #ifdef UART_3_ISR
 void UART_3_ISR(void)
 {
     irq_handler(3, uart_config[3].dev);
+}
+
+void UART_3_DMA_ISR(void)
+{
+    dma_handler(3, uart_config[3].dma_stream);
 }
 #endif
 
@@ -227,11 +273,21 @@ void UART_4_ISR(void)
 {
     irq_handler(4, uart_config[4].dev);
 }
+
+void UART_4_DMA_ISR(void)
+{
+    dma_handler(4, uart_config[4].dma_stream);
+}
 #endif
 
 #ifdef UART_5_ISR
 void UART_5_ISR(void)
 {
     irq_handler(5, uart_config[5].dev);
+}
+
+void UART_5_DMA_ISR(void)
+{
+    dma_handler(5, uart_config[5].dma_stream);
 }
 #endif
