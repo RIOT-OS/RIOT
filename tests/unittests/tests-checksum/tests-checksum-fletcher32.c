ef25f32536860021e9ed01a7b92d30417d1d1084
Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
2016-04-08 22:01:49 +0200

    tests/checksum: fix style in fletcher32

--- tests/unittests/tests-checksum/tests-checksum-fletcher32.c
+++ tests/unittests/tests-checksum/tests-checksum-fletcher32.c
@@ -24,48 +24,50 @@ static int calc_and_compare_checksum(const unsigned char *buf, size_t len,
     return result == expected;
 }
 
-static void test_checksum_fletcher32(void)
+static void test_checksum_fletcher32_empty(void)
 {
-    {
-        /* the initial checksum value is 0xFFFFFFFF */
-        unsigned char buf[] = "";
-        uint32_t expect = 0xFFFFFFFF;
+    /* the initial checksum value is 0xFFFFFFFF */
+    unsigned char buf[] = "";
+    uint32_t expect = 0xFFFFFFFF;
 
-        TEST_ASSERT(calc_and_compare_checksum(buf, sizeof(buf) - 1, expect));
-    }
+    TEST_ASSERT(calc_and_compare_checksum(buf, sizeof(buf) - 1, expect));
+}
 
-    {
-        /* fletcher cannot distinguish between all 0 and all 1 segments */
-        unsigned char buf0[16] = {
-            0xA1, 0xA1, 0xA1, 0xA1,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x1A, 0x1A, 0x1A, 0x1A,
-        };
-        uint32_t expect = fletcher32((const uint16_t *) buf0, sizeof(buf0)/2);
-        unsigned char buf1[16] = {
-            0xA1, 0xA1, 0xA1, 0xA1,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0xFF, 0xFF, 0xFF, 0xFF,
-            0x1A, 0x1A, 0x1A, 0x1A,
-        };
+static void test_checksum_fletcher32_0to1_undetected(void)
+{
+    /* fletcher cannot distinguish between all 0 and all 1 segments */
+    unsigned char buf0[16] = {
+        0xA1, 0xA1, 0xA1, 0xA1,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x1A, 0x1A, 0x1A, 0x1A,
+    };
+    uint32_t expect = fletcher32((const uint16_t *) buf0, sizeof(buf0)/2);
+    unsigned char buf1[16] = {
+        0xA1, 0xA1, 0xA1, 0xA1,
+        0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF,
+        0x1A, 0x1A, 0x1A, 0x1A,
+    };
 
-        TEST_ASSERT(calc_and_compare_checksum(buf1, sizeof(buf1), expect));
-    }
+    TEST_ASSERT(calc_and_compare_checksum(buf1, sizeof(buf1), expect));
+}
 
-    {
-        /* XXX: not verified with external implementation yet */
-        unsigned char buf[] = "abcdef";
-        uint32_t expect = 0x56502d2a;
+static void test_checksum_fletcher32_atof(void)
+{
+    /* XXX: not verified with external implementation yet */
+    unsigned char buf[] = "abcdef";
+    uint32_t expect = 0x56502d2a;
 
-        TEST_ASSERT(calc_and_compare_checksum(buf, sizeof(buf) - 1, expect));
-    }
+    TEST_ASSERT(calc_and_compare_checksum(buf, sizeof(buf) - 1, expect));
 }
 
 Test *tests_checksum_fletcher32_tests(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
-        new_TestFixture(test_checksum_fletcher32),
+        new_TestFixture(test_checksum_fletcher32_empty),
+        new_TestFixture(test_checksum_fletcher32_0to1_undetected),
+        new_TestFixture(test_checksum_fletcher32_atof),
     };
 
     EMB_UNIT_TESTCALLER(checksum_fletcher32_tests, NULL, NULL, fixtures);
