25baea1e50ae4e6b629691e9a8c21d9d9dee21bb
Cenk Gündoğan <cnkgndgn@gmail.com>
2016-04-12 11:29:02 +0200

    gnrc_rpl: separate validation from msg handling

--- sys/net/gnrc/routing/rpl/gnrc_rpl_control_messages.c
+++ sys/net/gnrc/routing/rpl/gnrc_rpl_control_messages.c
@@ -246,19 +246,6 @@ void gnrc_rpl_send_DIS(gnrc_rpl_instance_t *inst, ipv6_addr_t *destination)
     gnrc_rpl_send(pkt, KERNEL_PID_UNDEF, NULL, destination, (inst? &(inst->dodag.dodag_id) : NULL));
 }
 
-static bool _gnrc_rpl_check_DIS_validity(gnrc_rpl_dis_t *dis, uint16_t len)
-{
-    uint16_t expected_len = sizeof(*dis) + sizeof(icmpv6_hdr_t);
-
-    if (expected_len <= len) {
-        return true;
-    }
-
-    DEBUG("RPL: wrong DIS len: %d, expected: %d\n", len, expected_len);
-
-    return false;
-}
-
 void gnrc_rpl_recv_DIS(gnrc_rpl_dis_t *dis, kernel_pid_t iface, ipv6_addr_t *src,
                        ipv6_addr_t *dst, uint16_t len)
 {
@@ -292,92 +279,6 @@ void gnrc_rpl_recv_DIS(gnrc_rpl_dis_t *dis, kernel_pid_t iface, ipv6_addr_t *src
             }
         }
     }
-}
-
-static bool _gnrc_rpl_check_options_validity(int msg_type, gnrc_rpl_instance_t *inst,
-                                                gnrc_rpl_opt_t *opt, uint16_t len)
-{
-    uint16_t expected_len = 0;
-
-    while(expected_len < len) {
-        switch(opt->type) {
-            case (GNRC_RPL_OPT_PAD1):
-                expected_len += 1;
-                opt = (gnrc_rpl_opt_t *) (((uint8_t *) opt) + 1);
-                continue;
-
-            case (GNRC_RPL_OPT_DODAG_CONF):
-                if (msg_type != GNRC_RPL_ICMPV6_CODE_DIO) {
-                    DEBUG("RPL: DODAG CONF DIO option not expected\n");
-                    return false;
-                }
-
-                if (opt->length != GNRC_RPL_OPT_DODAG_CONF_LEN) {
-                    DEBUG("RPL: wrong DIO option (DODAG CONF) len: %d, expected: %d\n",
-                            opt->length, GNRC_RPL_OPT_DODAG_CONF_LEN);
-                    return false;
-                }
-                break;
-
-            case (GNRC_RPL_OPT_PREFIX_INFO):
-                if (msg_type != GNRC_RPL_ICMPV6_CODE_DIO) {
-                    DEBUG("RPL: PREFIX INFO DIO option not expected\n");
-                    return false;
-                }
-
-                if (opt->length != GNRC_RPL_OPT_PREFIX_INFO_LEN) {
-                    DEBUG("RPL: wrong DIO option (PREFIX INFO) len: %d, expected: %d\n",
-                            opt->length, GNRC_RPL_OPT_PREFIX_INFO_LEN);
-                    return false;
-                }
-                break;
-
-            case (GNRC_RPL_OPT_TARGET):
-                if (msg_type != GNRC_RPL_ICMPV6_CODE_DAO) {
-                    DEBUG("RPL: RPL TARGET DAO option not expected\n");
-                    return false;
-                }
-
-                if (opt->length > GNRC_RPL_OPT_TARGET_LEN) {
-                    DEBUG("RPL: wrong DAO option (RPL TARGET) len: %d, expected (max): %d\n",
-                            opt->length, GNRC_RPL_OPT_TARGET_LEN);
-                    return false;
-                }
-                break;
-
-            case (GNRC_RPL_OPT_TRANSIT):
-                if (msg_type != GNRC_RPL_ICMPV6_CODE_DAO) {
-                    DEBUG("RPL: RPL TRANSIT INFO DAO option not expected\n");
-                    return false;
-                }
-
-                uint8_t parent_addr = 0;
-                if (inst->mop == GNRC_RPL_MOP_NON_STORING_MODE) {
-                    parent_addr = sizeof(ipv6_addr_t);
-                }
-
-                if (opt->length != (GNRC_RPL_OPT_TRANSIT_INFO_LEN + parent_addr)) {
-                    DEBUG("RPL: wrong DAO option (TRANSIT INFO) len: %d, expected: %d\n",
-                            opt->length, (GNRC_RPL_OPT_TRANSIT_INFO_LEN + parent_addr));
-                    return false;
-                }
-                break;
-
-            default:
-                break;
-
-        }
-        expected_len += opt->length + sizeof(gnrc_rpl_opt_t);
-        opt = (gnrc_rpl_opt_t *) (((uint8_t *) (opt + 1)) + opt->length);
-    }
-
-    if (expected_len == len) {
-        return true;
-    }
-
-    DEBUG("RPL: wrong options len: %d, expected: %d\n", len, expected_len);
-
-    return false;
 }
 
 /** @todo allow target prefixes in target options to be of variable length */
@@ -522,19 +423,6 @@ bool _parse_options(int msg_type, gnrc_rpl_instance_t *inst, gnrc_rpl_opt_t *opt
         opt = (gnrc_rpl_opt_t *) (((uint8_t *) (opt + 1)) + opt->length);
     }
     return true;
-}
-
-static bool _gnrc_rpl_check_DIO_validity(gnrc_rpl_dio_t *dio, uint16_t len)
-{
-    uint16_t expected_len = sizeof(*dio) + sizeof(icmpv6_hdr_t);
-
-    if (expected_len <= len) {
-        return true;
-    }
-
-    DEBUG("RPL: wrong DIO len: %d, expected: %d\n", len, expected_len);
-
-    return false;
 }
 
 void gnrc_rpl_recv_DIO(gnrc_rpl_dio_t *dio, kernel_pid_t iface, ipv6_addr_t *src, uint16_t len)
@@ -970,23 +858,6 @@ void gnrc_rpl_send_DAO_ACK(gnrc_rpl_instance_t *inst, ipv6_addr_t *destination, 
     gnrc_rpl_send(pkt, dodag->iface, NULL, destination, &dodag->dodag_id);
 }
 
-static bool _gnrc_rpl_check_DAO_validity(gnrc_rpl_dao_t *dao, uint16_t len)
-{
-    uint16_t expected_len = sizeof(*dao) + sizeof(icmpv6_hdr_t);
-
-    if ((dao->k_d_flags & GNRC_RPL_DAO_D_BIT)) {
-        expected_len += sizeof(ipv6_addr_t);
-    }
-
-    if (expected_len <= len) {
-        return true;
-    }
-
-    DEBUG("RPL: wrong DAO len: %d, expected: %d\n", len, expected_len);
-
-    return false;
-}
-
 void gnrc_rpl_recv_DAO(gnrc_rpl_dao_t *dao, kernel_pid_t iface, ipv6_addr_t *src, uint16_t len)
 {
     (void)iface;
@@ -1043,23 +914,6 @@ void gnrc_rpl_recv_DAO(gnrc_rpl_dao_t *dao, kernel_pid_t iface, ipv6_addr_t *src
     }
 
     gnrc_rpl_delay_dao(dodag);
-}
-
-static bool _gnrc_rpl_check_DAO_ACK_validity(gnrc_rpl_dao_ack_t *dao_ack, uint16_t len)
-{
-    uint16_t expected_len = sizeof(*dao_ack) + sizeof(icmpv6_hdr_t);
-
-    if ((dao_ack->d_reserved & GNRC_RPL_DAO_ACK_D_BIT)) {
-        expected_len += sizeof(ipv6_addr_t);
-    }
-
-    if (expected_len == len) {
-        return true;
-    }
-
-    DEBUG("RPL: wrong DAO-ACK len: %d, expected: %d\n", len, expected_len);
-
-    return false;
 }
 
 void gnrc_rpl_recv_DAO_ACK(gnrc_rpl_dao_ack_t *dao_ack, kernel_pid_t iface, uint16_t len)
