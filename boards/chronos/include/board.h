963f8a495d6d2af97fbe060e43e1c261a87b2503
Ian Martin <ian@locicontrols.com>
2016-04-18 12:06:20 -0400

    sys/xtimer: eliminate XTIMER_SHIFT_ON_COMPARE

--- boards/chronos/include/board.h
+++ boards/chronos/include/board.h
@@ -41,7 +41,6 @@ extern "C" {
 #define XTIMER                      (0)
 #define XTIMER_CHAN                 (0)
 #define XTIMER_MASK                 (0xffff0000)
-#define XTIMER_SHIFT_ON_COMPARE     (4)
 /** @} */
 
 /**
