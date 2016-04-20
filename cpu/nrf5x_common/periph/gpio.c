e4e5697deb5224c2e84f3403d594f956a4db06a2
Attilio <attilio.dona@gmail.com>
2016-04-20 20:44:03 +0200

    original gpio

--- cpu/nrf5x_common/periph/gpio.c
+++ cpu/nrf5x_common/periph/gpio.c
@@ -32,15 +32,10 @@
 #include "periph_cpu.h"
 #include "periph_conf.h"
 
-#define ENABLE_DEBUG    (0)
-#include "debug.h"
-
-static uint8_t index = 0;
-
 /**
  * @brief   Place to store the interrupt context
  */
-static gpio_isr_ctx_t exti_chan[4];
+static gpio_isr_ctx_t exti_chan;
 
 
 int gpio_init(gpio_t pin, gpio_mode_t mode)
@@ -64,42 +59,33 @@ int gpio_init_int(gpio_t pin, gpio_mode_t mode, gpio_flank_t flank,
                   gpio_cb_t cb, void *arg)
 {
     /* disable external interrupt in case one is active */
-    //NRF_GPIOTE->INTENSET &= ~(GPIOTE_INTENSET_IN0_Msk);
-    NRF_GPIOTE->INTENSET &= ~(0x1UL << index);
-
+    NRF_GPIOTE->INTENSET &= ~(GPIOTE_INTENSET_IN0_Msk);
     /* save callback */
-    exti_chan[index].cb = cb;
-    exti_chan[index].arg = arg;
-
-
+    exti_chan.cb = cb;
+    exti_chan.arg = arg;
     /* configure pin as input */
     gpio_init(pin, mode);
     /* set interrupt priority and enable global GPIOTE interrupt */
     NVIC_EnableIRQ(GPIOTE_IRQn);
     /* configure the GPIOTE channel: set even mode, pin and active flank */
-    NRF_GPIOTE->CONFIG[index] = (GPIOTE_CONFIG_MODE_Event |
+    NRF_GPIOTE->CONFIG[0] = (GPIOTE_CONFIG_MODE_Event |
                              (pin << GPIOTE_CONFIG_PSEL_Pos) |
                              (flank << GPIOTE_CONFIG_POLARITY_Pos));
     /* enable external interrupt */
-    //NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk;
-    NRF_GPIOTE->INTENSET |= (0x1UL << index);
-
-    // point to the next free isr context slot
-    index++;
-
+    NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk;
     return 0;
 }
 
 void gpio_irq_enable(gpio_t pin)
 {
     (void) pin;
-    NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk|GPIOTE_INTENSET_IN1_Msk;
+    NRF_GPIOTE->INTENSET |= GPIOTE_INTENSET_IN0_Msk;
 }
 
 void gpio_irq_disable(gpio_t pin)
 {
     (void) pin;
-    NRF_GPIOTE->INTENCLR |= GPIOTE_INTENSET_IN0_Msk|GPIOTE_INTENSET_IN1_Msk;
+    NRF_GPIOTE->INTENCLR |= GPIOTE_INTENSET_IN0_Msk;
 }
 
 int gpio_read(gpio_t pin)
@@ -138,13 +124,10 @@ void gpio_write(gpio_t pin, int value)
 
 void isr_gpiote(void)
 {
-	for(int i=0; i<4;i++) {
-	    if (NRF_GPIOTE->EVENTS_IN[i] == 1)
-	    {
-	        NRF_GPIOTE->EVENTS_IN[i] = 0;
-	        exti_chan[i].cb(exti_chan[i].arg);
-	    }
-	}
+    if (NRF_GPIOTE->EVENTS_IN[0] == 1) {
+        NRF_GPIOTE->EVENTS_IN[0] = 0;
+        exti_chan.cb(exti_chan.arg);
+    }
     if (sched_context_switch_request) {
         thread_yield();
     }
