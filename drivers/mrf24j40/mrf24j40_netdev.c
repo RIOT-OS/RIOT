cf5b20944fc4e5d0a0b32672cfabe2532a2683f5
TobiasFredersdorf <Tobias.Fredersdorf@haw-hamburg.de>
2016-04-20 12:42:33 +0200

    added getter and setter for the options

--- drivers/mrf24j40/mrf24j40_netdev.c
+++ drivers/mrf24j40/mrf24j40_netdev.c
@@ -172,3 +172,294 @@ static int _set_state(mrf24j40_t *dev, netopt_state_t state)
     }
     return sizeof(netopt_state_t);
 }
+
+static int _get(netdev2_t *netdev, netopt_t opt, void *val, size_t max_len)
+{
+    mrf24j40_t *dev = (mrf24j40_t *) netdev;
+
+    if (netdev == NULL) {
+        return -ENODEV;
+    }
+
+    /* getting these options doesn't require the transceiver to be responsive */
+    switch (opt) {
+        
+
+        case NETOPT_MAX_PACKET_SIZE:
+            if (max_len < sizeof(int16_t)) {
+                return -EOVERFLOW;
+            }
+            *((uint16_t *)val) = MRF24J40_MAX_PKT_LENGTH - _MAX_MHR_OVERHEAD;
+            return sizeof(uint16_t);
+
+        case NETOPT_STATE:
+            if (max_len < sizeof(netopt_state_t)) {
+                return -EOVERFLOW;
+            }
+            *((netopt_state_t *)val) = _get_state(dev);
+            return sizeof(netopt_state_t);
+
+        case NETOPT_PRELOADING:
+            if (dev->netdev.flags & MRF24J40_OPT_PRELOADING) {
+                *((netopt_enable_t *)val) = NETOPT_ENABLE;
+            }
+            else {
+                *((netopt_enable_t *)val) = NETOPT_DISABLE;
+            }
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_PROMISCUOUSMODE:
+            if (dev->netdev.flags & MRF24J40_OPT_PROMISCUOUS) {
+                *((netopt_enable_t *)val) = NETOPT_ENABLE;
+            }
+            else {
+                *((netopt_enable_t *)val) = NETOPT_DISABLE;
+            }
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_RX_START_IRQ:
+            *((netopt_enable_t *)val) =
+                !!(dev->netdev.flags & MRF24J40_OPT_TELL_RX_START);
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_RX_END_IRQ:
+            *((netopt_enable_t *)val) =
+                !!(dev->netdev.flags & MRF24J40_OPT_TELL_RX_END);
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_TX_START_IRQ:
+            *((netopt_enable_t *)val) =
+                !!(dev->netdev.flags & MRF24J40_OPT_TELL_TX_START);
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_TX_END_IRQ:
+            *((netopt_enable_t *)val) =
+                !!(dev->netdev.flags & MRF24J40_OPT_TELL_TX_END);
+            return sizeof(netopt_enable_t);
+
+        case NETOPT_CSMA:
+            *((netopt_enable_t *)val) =
+                !!(dev->netdev.flags & MRF24J40_OPT_CSMA);
+            return sizeof(netopt_enable_t);
+
+        default:
+            /* Can still be handled in second switch */
+            break;
+    }
+
+    int res;
+
+    if (((res = netdev2_ieee802154_get((netdev2_ieee802154_t *)netdev, opt, val,
+                                       max_len)) >= 0) || (res != -ENOTSUP)) {
+        return res;
+    }
+
+    uint8_t old_state = mrf24j40_get_status(dev);
+    res = 0;
+
+    /* temporarily wake up if sleeping */
+    if (old_state == MRF24J40_STATE_SLEEP) {
+        mrf24j40_assert_awake(dev);
+    }
+
+    /* these options require the transceiver to be not sleeping*/
+    switch (opt) {
+        case NETOPT_TX_POWER:
+            if (max_len < sizeof(int16_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                *((uint16_t *)val) = mrf24j40_get_txpower(dev);
+                res = sizeof(uint16_t);
+            }
+            break;
+
+        case NETOPT_IS_CHANNEL_CLR:
+            if (mrf24j40_cca(dev)) {
+                *((netopt_enable_t *)val) = NETOPT_ENABLE;
+            }
+            else {
+                *((netopt_enable_t *)val) = NETOPT_DISABLE;
+            }
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_CSMA_RETRIES:
+            if (max_len < sizeof(uint8_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                *((uint8_t *)val) = mrf24j40_get_csma_max_retries(dev);
+                res = sizeof(uint8_t);
+            }
+            break;
+
+        default:
+            res = -ENOTSUP;
+    }
+
+    /* go back to sleep if were sleeping */
+    if (old_state == MRF24J40_STATE_SLEEP) {
+        mrf24j40_set_state(dev, MRF24J40_STATE_SLEEP);
+    }
+
+    return res;
+}
+
+static int _set(netdev2_t *netdev, netopt_t opt, void *val, size_t len)
+{
+    mrf24j40_t *dev = (mrf24j40_t *) netdev;
+    uint8_t old_state = mrf24j40_get_status(dev);
+    int res = 0;
+
+    if (dev == NULL) {
+        return -ENODEV;
+    }
+
+    /* temporarily wake up if sleeping */
+    if (old_state == MRF24J40_STATE_SLEEP) {
+        mrf24j40_assert_awake(dev);
+    }
+
+    switch (opt) {
+        case NETOPT_ADDRESS:
+            if (len > sizeof(uint16_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                mrf24j40_set_addr_short(dev, *((uint16_t *)val));
+                /* don't set res to set netdev2_ieee802154_t::short_addr */
+            }
+            break;
+
+        case NETOPT_ADDRESS_LONG:
+            if (len > sizeof(uint64_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                mrf24j40_set_addr_long(dev, *((uint64_t *)val));
+                /* don't set res to set netdev2_ieee802154_t::long_addr */
+            }
+            break;
+
+        case NETOPT_NID:
+            if (len > sizeof(uint16_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                mrf24j40_set_pan(dev, *((uint16_t *)val));
+                /* don't set res to set netdev2_ieee802154_t::pan */
+            }
+            break;
+
+        case NETOPT_CHANNEL:
+            if (len != sizeof(uint16_t)) {
+                res = -EINVAL;
+            }
+            else {
+                uint8_t chan = ((uint8_t *)val)[0];
+                if (chan < MRF24J40_MIN_CHANNEL ||
+                    chan > MRF24J40_MAX_CHANNEL) {
+                    res = -ENOTSUP;
+                    break;
+                }
+                mrf24j40_set_chan(dev, chan);
+                /* don't set res to set netdev2_ieee802154_t::chan */
+            }
+            break;
+
+        case NETOPT_TX_POWER:
+            if (len > sizeof(int16_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                mrf24j40_set_txpower(dev, *((int16_t *)val));
+                res = sizeof(uint16_t);
+            }
+            break;
+
+        case NETOPT_STATE:
+            if (len > sizeof(netopt_state_t)) {
+                res = -EOVERFLOW;
+            }
+            else {
+                res = _set_state(dev, *((netopt_state_t *)val));
+            }
+            break;
+
+       
+
+        case NETOPT_PRELOADING:
+            mrf24j40_set_option(dev, MRF24J40_OPT_PRELOADING,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_PROMISCUOUSMODE:
+            mrf24j40_set_option(dev, MRF24J40_OPT_PROMISCUOUS,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_RX_START_IRQ:
+            mrf24j40_set_option(dev, MRF24J40_OPT_TELL_RX_START,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_RX_END_IRQ:
+            mrf24j40_set_option(dev, MRF24J40_OPT_TELL_RX_END,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_TX_START_IRQ:
+            mrf24j40_set_option(dev, MRF24J40_OPT_TELL_TX_START,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_TX_END_IRQ:
+            mrf24j40_set_option(dev, MRF24J40_OPT_TELL_TX_END,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_CSMA:
+            mrf24j40_set_option(dev, MRF24J40_OPT_CSMA,
+                                 ((bool *)val)[0]);
+            res = sizeof(netopt_enable_t);
+            break;
+
+        case NETOPT_CSMA_RETRIES:
+            if ((len > sizeof(uint8_t)) ||
+                (*((uint8_t *)val) > 5)) {
+                res = -EOVERFLOW;
+            }
+            else if (!(dev->netdev.flags & MRF24J40_OPT_CSMA)) {
+                /* If CSMA is disabled, don't allow setting retries */
+                res = -ENOTSUP;
+            }
+            else {
+                mrf24j40_set_csma_max_retries(dev, *((uint8_t *)val));
+                res = sizeof(uint8_t);
+            }
+            break;
+
+
+        default:
+            res = -ENOTSUP;
+    }
+
+    /* go back to sleep if were sleeping and state hasn't been changed */
+    if ((old_state == MRF24J40_STATE_SLEEP) &&
+        (opt != NETOPT_STATE)) {
+        mrf24j40_set_state(dev, MRF24J40_STATE_SLEEP);
+    }
+
+    if (res == -ENOTSUP) {
+        res = netdev2_ieee802154_set((netdev2_ieee802154_t *)netdev, opt,
+                                     val, len);
+    }
+
+    return res;
