1567b019a100686b2ca86bd2e571347a569591a5
zhuoshuguo <shuguo.zhuo@inria.fr>
2016-04-25 13:32:33 +0200

    delete additional spaces

--- boards/wsn430-v1_4/include/board.h
+++ boards/wsn430-v1_4/include/board.h
@@ -37,8 +37,8 @@ extern "C" {
 /**
  * @brief   Define the CPU model for the <msp430.h>
  */
-#ifndef   MSP430F1611
-#define   MSP430F1611
+#ifndef MSP430F1611
+#define MSP430F1611
 #endif
 
 /**
@@ -61,4 +61,4 @@ extern "C" {
 #include <msp430x16x.h>
 
 /** @} */
-#endif /*  WSN_BOARD_H  */
+#endif /* WSN_BOARD_H */
