af69ebdcf600b9507fb97328311e15843f56a243
Oleg Hahm <oleg@hobbykeller.org>
2016-05-12 23:09:57 +0200

    gnrc netdev2: fix debug for pointer printing

--- sys/net/gnrc/link_layer/netdev2/gnrc_netdev2.c
+++ sys/net/gnrc/link_layer/netdev2/gnrc_netdev2.c
@@ -88,7 +88,7 @@ static void _event_cb(netdev2_t *dev, netdev2_event_t event)
                     break;
                 }
                 else if (gnrc_netdev2->retrans_head->cnt-- <= 0) {
-                    DEBUG("giving up sending, removing from buffer and queue: %p\n", gnrc_netdev2->retrans_head->pkt);
+                    DEBUG("giving up sending, removing from buffer and queue: %p\n", (void*)gnrc_netdev2->retrans_head->pkt);
                     gnrc_pktbuf_release(gnrc_netdev2->retrans_head->pkt);
                     gnrc_netdev2->retrans_head->pkt = NULL;
                     gnrc_pktqueue_remove_head((gnrc_pktqueue_t**)&(gnrc_netdev2->retrans_head));
@@ -106,7 +106,7 @@ static void _event_cb(netdev2_t *dev, netdev2_event_t event)
                     break;
                 }
                 else {
-                    DEBUG("packet sent, removing from buffer and queue: %p\n", gnrc_netdev2->retrans_head->pkt);
+                    DEBUG("packet sent, removing from buffer and queue: %p\n", (void*) gnrc_netdev2->retrans_head->pkt);
                     gnrc_pktbuf_release(gnrc_netdev2->retrans_head->pkt);
                     gnrc_netdev2->retrans_head->pkt = NULL;
                     gnrc_pktqueue_remove_head((gnrc_pktqueue_t**)&(gnrc_netdev2->retrans_head));
