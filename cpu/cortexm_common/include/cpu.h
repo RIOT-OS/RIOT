9ad9f400e61c8d15fbb0bfbc43ff1d4cff8488d5
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-06-01 16:12:38 +0200

    cpu/cortexm_common: Remove register keyword from variables
    register is deprecated when building C++11 code and removed in C++17,
    using it will cause build failures with C++ and -Werror. The register
    hint keyword is likely ignored in GCC anyway.

--- cpu/cortexm_common/include/cpu.h
+++ cpu/cortexm_common/include/cpu.h
@@ -63,7 +63,7 @@ void cortexm_init(void);
  */
 static inline void cpu_print_last_instruction(void)
 {
-    register uint32_t *lr_ptr;
+    uint32_t *lr_ptr;
     __asm__ __volatile__("mov %0, lr" : "=r"(lr_ptr));
     printf("%p\n", (void*) lr_ptr);
 }
