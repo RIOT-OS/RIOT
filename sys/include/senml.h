909394d9f6983dbc0b0c8920000f39bce21c4676
Lennart Dührsen <lennart-duehrsen@gmx.net>
2016-06-11 20:02:24 +0200

    reference implementation of the Sensor Markup Language (SenML) with support for JSON

---
+++ sys/include/senml.h
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2016 Freie Universität Berlin
+ * Copyright (C) 2016 Lennart Dührsen <lennart.duehrsen@fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+
+/**
+ * @ingroup     senml
+ * @{
+ *
+ * @file
+ * @brief       Reference implementation of the Sensor Markup Language (SenML)
+ *
+ * The Sensor Markup Language is a specification that defines media types for
+ * representing simple sensor measurements and device parameters.
+ * Representations are defined in JavaScript Object Notation (JSON), Concise
+ * Binary Object Representation (CBOR), eXtensible Markup Language (XML), and
+ * Efficient XML Interchange (EXI), which share the common SenML data model.
+ * This implementation supports only JSON and CBOR. A simple sensor, such as a
+ * temperature sensor, could use this media type in protocols such as HTTP or
+ * CoAP to transport the measurements of the sensor or to be configured.
+ *
+ * @see <a href="https://tools.ietf.org/html/draft-jennings-core-senml-06">
+ *      Media Types for Sensor Markup Language (SenML)
+ *      </a>
+ *
+ * @author      Lennart Dührsen <lennart.duehrsen@fu-berlin.de>
+ */
+
+
+#ifndef SENML_H
+#define SENML_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+
+#define SENML_SUPPORTED_VERSION (5)    //!< The highest SenML version this implementation supports
+
+
+#define SJ_VERSION       "bver" //!< Key for the version attribute in JSON documents
+#define SJ_BASE_NAME     "bn"   //!< Key for the base name attribute in JSON documents
+#define SJ_BASE_TIME     "bt"   //!< Key for the base time attribute in JSON documents
+#define SJ_BASE_UNIT     "bu"   //!< Key for the base unit attribute in JSON documents
+#define SJ_BASE_VALUE    "bv"   //!< Key for the base value attribute in JSON documents
+#define SJ_NAME          "n"    //!< Key for the name attribute in JSON documents
+#define SJ_UNIT          "u"    //!< Key for the unit attribute in JSON documents
+#define SJ_VALUE         "v"    //!< Key for the value attribute in JSON documents
+#define SJ_STRING_VALUE  "vs"   //!< Key for the string value attribute in JSON documents
+#define SJ_BOOL_VALUE    "vb"   //!< Key for the boolean value attribute in JSON documents
+#define SJ_VALUE_SUM     "s"    //!< Key for the value sum attribute in JSON documents
+#define SJ_TIME          "t"    //!< Key for the time attribute in JSON documents
+#define SJ_UPDATE_TIME   "ut"   //!< Key for the update time attribute in JSON documents
+#define SJ_DATA_VALUE    "vd"   //!< Key for the data value attribute in JSON documents
+
+
+#define SC_VERSION       (-1)   //!< Key for the version attribute in CBOR documents
+#define SC_BASE_NAME     (-2)   //!< Key for the base name attribute in CBOR documents
+#define SC_BASE_TIME     (-3)   //!< Key for the base time attribute in CBOR documents
+#define SC_BASE_UNIT     (-4)   //!< Key for the base unit attribute in CBOR documents
+#define SC_BASE_VALUE    (-5)   //!< Key for the base value attribute in CBOR documents
+#define SC_NAME          (0)    //!< Key for the name attribute in CBOR documents
+#define SC_UNIT          (1)    //!< Key for the unit attribute in CBOR documents
+#define SC_VALUE         (2)    //!< Key for the value attribute in CBOR documents
+#define SC_STRING_VALUE  (3)    //!< Key for the string value attribute in CBOR documents
+#define SC_BOOL_VALUE    (4)    //!< Key for the boolean value attribute in CBOR documents
+#define SC_VALUE_SUM     (5)    //!< Key for the value sum attribute in CBOR documents
+#define SC_TIME          (6)    //!< Key for the time attribute in CBOR documents
+#define SC_UPDATE_TIME   (7)    //!< Key for the update time attribute in CBOR documents
+#define SC_DATA_VALUE    (8)    //!< Key for the data value attribute in CBOR documents
+
+
+/*! The different data types SenML supports */
+typedef enum {
+    SENML_TYPE_UNDEF  = 0,  //!< Placeholder for when no value is provided
+    SENML_TYPE_FLOAT  = 1,  //!< Indicates a float value
+    SENML_TYPE_STRING = 2,  //!< Indicates a string value
+    SENML_TYPE_BOOL   = 3,  //!< Indicates a boolean value
+    SENML_TYPE_BINARY = 4   //!< Indicates a binary (data) value
+} senml_value_type_t;
+
+
+/*! Buffer that holds arbitrary binary data */
+typedef struct {
+    uint8_t *p;     //!< Pointer to the actual data
+    size_t   len;   //!< Size of the data in bytes
+} senml_bin_data_t;
+
+
+/*! struct that contains base information which applies to all subsequent entries */
+typedef struct {
+    uint8_t             version;         //!< SenML version of this pack
+    char               *base_name;       //!< Concatenated with name attribute gives sensor's UID
+    double              base_time;       //!< Update time values are relative to this time
+    char               *base_unit;       //!< When all values have the same unit, set this field
+    senml_value_type_t  base_value_type; //!< Indicates which type the base value is of
+    union {
+        double            base_value_f;    //!< A base value of type float
+        char             *base_value_s;    //!< A base value of type string
+        bool              base_value_b;    //!< A boolean base value (FIXME is this sensible?)
+        senml_bin_data_t  base_value_bin;  //!< A binary base value (FIXME is this sensible?)
+    } base_value;
+} senml_base_info_t;
+
+
+/*! struct that contains the values of a SenML record */
+typedef struct {
+    char              *name;        //!< Sensor's name, will be appended to base name if provided
+    char              *unit;        //!< Unit of the measurement
+    double             time;        //!< Time (seconds since unix epoch) of the measurement
+    unsigned int       update_time; //!< Time before sensor provides an updated measurement
+    // [type]          value_sum    // FIXME clear definition missing
+    senml_value_type_t value_type;  //!< Indicates which type the value is of
+    union {
+        double            value_f;   //!< A value of type float
+        char             *value_s;   //!< A value of type string
+        bool              value_b;   //!< A boolean value
+        senml_bin_data_t  value_bin; //!< A binary value (FIXME don't know how to handle this yet)
+    } value;
+} senml_record_t;
+
+
+/*! struct that holds a SenML pack (optional base info and 1..n records) */
+typedef struct {
+    senml_base_info_t  *base_info;  //!< Pointer to the base info, may be NULL
+    senml_record_t     *records;    //!< Pointer to the beginning of an array of records
+    size_t              num;        //!< Number of records in the array
+} senml_pack_t;
+
+
+/**
+ * @brief Decodes a SenML document in JSON format
+ *
+ * @param[in]  input The JSON document containing the SenML pack
+ * @param[out] pack  The <code>senml_pack_t</code> structure in which the
+ *                   records and base info will be stored.
+ *
+ * @return 0 on success, -1 if \p input is not a valid SenML document, or a
+ *         positive number indicating by how much the number of records in
+ *         \p input exceeds <code>pack->num</code>
+ */
+int senml_decode_json_s(char *input, senml_pack_t *pack);
+
+
+/**
+ * @brief Creates a SenML document in JSON format from a SenML pack
+ *
+ * @param[in]  pack   The pack that contains the optional base info and the records
+ * @param[out] output The resulting JSON document
+ * @param[in]  len    The length of \p output in bytes
+ *
+ * @return 0 on success, or -1 when \p output is not big enough
+ */
+int senml_encode_json_s(const senml_pack_t *pack, char *output, size_t len);
+
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif  // SENML_H
+
+/** @} */
---
+++ sys/senml/senml.c
@@ -0,0 +1,342 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#include "senml.h"
+#include "jsmn.h"
+#include "cbor.h"
+
+#define ENABLE_DEBUG  (0)
+#include "debug.h"
+
+
+int senml_decode_json_s(char *input, senml_pack_t *pack)
+{
+    jsmn_parser parser;
+
+    jsmn_init(&parser);
+
+    int num_toks = jsmn_parse(&parser, input, strlen(input), NULL, 65535);
+
+    if (num_toks == JSMN_ERROR_INVAL || num_toks == JSMN_ERROR_PART) {
+        DEBUG("ERROR: unable to parse JSON string #1\n");
+        return -1;
+    }
+
+    jsmntok_t tokens[num_toks];
+
+    jsmn_init(&parser);
+
+    jsmn_parse(&parser, input, strlen(input), tokens, num_toks);
+
+    if (num_toks == JSMN_ERROR_INVAL || num_toks == JSMN_ERROR_PART) {
+        DEBUG("ERROR: unable to parse JSON string #2\n");
+        return -1;
+    }
+
+    if (tokens[0].type != JSMN_ARRAY) {
+        DEBUG("ERROR: not a valid SenML document\n");
+        DEBUG("type is: %d\n", (int)tokens[0].type);
+        return -1;
+    }
+
+    int array_num = -1;
+
+    jsmntype_t last_type = JSMN_UNDEFINED;
+
+    char *tmp_key, *tmp_val;
+
+    for (size_t i = 0, j = num_toks; j > 0; i++, j--) {
+        jsmntok_t *t = &tokens[i];
+
+        if (t->type == JSMN_ARRAY) {
+            last_type = JSMN_ARRAY;
+            continue;
+        }
+
+        if (t->type == JSMN_OBJECT) {
+            array_num++;
+            last_type = JSMN_OBJECT;
+            continue;
+        }
+
+        if (last_type == JSMN_OBJECT && t->type == JSMN_STRING) {
+            tmp_key = &input[t->start];
+            input[t->end] = '\0';
+            i++;
+            j--;
+            t = &tokens[i];
+            tmp_val = &input[t->start];
+            input[t->end] = '\0';
+
+            if (array_num == 0) {
+                if (!strcmp(tmp_key, SJ_VERSION)) {
+                    pack->base_info->version = (uint8_t)atoi(tmp_val);
+                }
+                else if (!strcmp(tmp_key, SJ_BASE_NAME)) {
+                    pack->base_info->base_name = tmp_val;
+                }
+                else if (!strcmp(tmp_key, SJ_BASE_TIME)) {
+                    pack->base_info->base_time = atof(tmp_val);
+                }
+                else if (!strcmp(tmp_key, SJ_BASE_UNIT)) {
+                    pack->base_info->base_unit = tmp_val;
+                }
+                else if (!strcmp(tmp_key, SJ_BASE_VALUE)) {
+                    if (t->type == JSMN_STRING) {
+                        pack->base_info->base_value_type = SENML_TYPE_STRING;
+                        pack->base_info->base_value.base_value_s = tmp_val;
+                    }
+                    else if (tmp_val[0] == 't' || tmp_val[0] == 'f') {
+                        pack->base_info->base_value_type = SENML_TYPE_BOOL;
+                        pack->base_info->base_value.base_value_b = (tmp_val[0] == 't') ? true : false;
+                    }
+                    else if (tmp_val[0] == '-' || isdigit(tmp_val[0])) {
+                        pack->base_info->base_value_type = SENML_TYPE_FLOAT;
+                        pack->base_info->base_value.base_value_f = atof(tmp_val);
+                    }
+                    // FIXME ignoring binary values for now b/c unclear definition
+                }
+            }
+
+            if (!strcmp(tmp_key, SJ_NAME)) {
+                (pack->records)[array_num].name = tmp_val;
+            }
+            else if (!strcmp(tmp_key, SJ_UNIT)) {
+                (pack->records)[array_num].unit = tmp_val;
+            }
+            else if (!strcmp(tmp_key, SJ_VALUE)) {
+                (pack->records)[array_num].value_type = SENML_TYPE_FLOAT;
+                (pack->records)[array_num].value.value_f = atof(tmp_val);
+            }
+            else if (!strcmp(tmp_key, SJ_STRING_VALUE)) {
+                (pack->records)[array_num].value_type = SENML_TYPE_STRING;
+                (pack->records)[array_num].value.value_s = tmp_val;
+            }
+            else if (!strcmp(tmp_key, SJ_BOOL_VALUE)) {
+                (pack->records)[array_num].value_type = SENML_TYPE_BOOL;
+                if (!strcmp(tmp_val, "true")) {
+                    (pack->records)[array_num].value.value_b = true;
+                }
+                else if (!strcmp(tmp_val, "false")) {
+                    (pack->records)[array_num].value.value_b = false;
+                }
+            }
+            else if (!strcmp(tmp_key, SJ_DATA_VALUE)) {
+                (pack->records)[array_num].value_type = SENML_TYPE_BINARY;
+                // FIXME not clear how binary data is represented, so ignoring it for now
+            }
+            else if (!strcmp(tmp_key, SJ_VALUE_SUM)) {
+                // FIXME the draft does not say how to handle the value sum, so ignoring it for now
+            }
+            else if (!strcmp(tmp_key, SJ_TIME)) {
+                (pack->records)[array_num].time = atof(tmp_val);
+            }
+            else if (!strcmp(tmp_key, SJ_UPDATE_TIME)) {
+                (pack->records)[array_num].update_time = (unsigned int)atoi(tmp_val);
+            }
+        }
+    }
+
+    pack->num = (array_num + 1);
+
+    return 0;
+}
+
+
+int senml_encode_json_s(const senml_pack_t *pack, char *output, size_t len)
+{
+    size_t insert_pos = 0;
+
+    output[0] = '[';
+    insert_pos++;
+    len--;
+
+    int chars_written;
+
+    if (!pack->base_info) {
+        goto encode_records;
+    }
+
+    output[insert_pos] = '{';
+    insert_pos++;
+    len--;
+
+    // using len - 2 to leave space for closing } and ]
+
+    if (pack->base_info->version) {
+        chars_written = snprintf(&output[insert_pos], len - 2, "\"bver\":%u,",
+                                 pack->base_info->version);
+
+        insert_pos += chars_written;
+        len -= chars_written;
+
+        if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+            return -1;
+        }
+    }
+
+    if (pack->base_info->base_name) {
+        chars_written = snprintf(&output[insert_pos], len - 2, "\"bn\":\"%s\",",
+                                 pack->base_info->base_name);
+
+        insert_pos += chars_written;
+        len -= chars_written;
+
+        if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+            return -1;
+        }
+    }
+
+    if (pack->base_info->base_time != 0) {
+        chars_written = snprintf(&output[insert_pos], len - 2, "\"bt\":%f,",
+                                 pack->base_info->base_time);
+
+        insert_pos += chars_written;
+        len -= chars_written;
+
+        if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+            return -1;
+        }
+    }
+
+    if (pack->base_info->base_unit) {
+        chars_written = snprintf(&output[insert_pos], len - 2, "\"bu\":\"%s\",",
+                                 pack->base_info->base_unit);
+
+        insert_pos += chars_written;
+        len -= chars_written;
+
+        if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+            return -1;
+        }
+    }
+
+    if (pack->base_info->base_value_type != SENML_TYPE_UNDEF) {
+        if (pack->base_info->base_value_type == SENML_TYPE_FLOAT) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"bv\":%f,",
+                                     pack->base_info->base_value.base_value_f);
+        }
+        else if (pack->base_info->base_value_type == SENML_TYPE_STRING) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"bv\":\"%s\",",
+                                     pack->base_info->base_value.base_value_s);
+        }
+        else if (pack->base_info->base_value_type == SENML_TYPE_BOOL) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"bv\":%s,",
+                                     pack->base_info->base_value.base_value_b ? "true" : "false");
+        }
+        else if (pack->base_info->base_value_type == SENML_TYPE_BINARY) {
+            // FIXME unclear how binary data is represented, ignoring it for now
+        }
+
+        insert_pos += chars_written;
+        len -= chars_written;
+
+        if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+            return -1;
+        }
+    }
+
+    encode_records:
+
+    for (size_t i = 0; i < pack->num; i++) {
+        senml_record_t *curr_record = &(pack->records[i]);
+
+        if (i != 0) {
+            output[insert_pos] = '{';
+            insert_pos++;
+            len--;
+        }
+
+        if (curr_record->name) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"n\":\"%s\",", curr_record->name);
+
+            insert_pos += chars_written;
+            len -= chars_written;
+
+            if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+                return -1;
+            }
+        }
+
+        if (curr_record->unit) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"u\":\"%s\",", curr_record->unit);
+
+            insert_pos += chars_written;
+            len -= chars_written;
+
+            if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+                return -1;
+            }
+        }
+
+        if (curr_record->time != 0) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"t\":%f,", curr_record->time);
+
+            insert_pos += chars_written;
+            len -= chars_written;
+
+            if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+                return -1;
+            }
+        }
+
+        if (curr_record->update_time != 0) {
+            chars_written = snprintf(&output[insert_pos], len - 2, "\"ut\":%u,",
+                                     curr_record->update_time);
+
+            insert_pos += chars_written;
+            len -= chars_written;
+
+            if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+                return -1;
+            }
+        }
+
+        if (curr_record->value_type != SENML_TYPE_UNDEF) {
+            if (curr_record->value_type == SENML_TYPE_FLOAT) {
+                chars_written = snprintf(&output[insert_pos], len - 2, "\"v\":%f",
+                                         curr_record->value.value_f);
+            }
+            else if (curr_record->value_type == SENML_TYPE_STRING) {
+                chars_written = snprintf(&output[insert_pos], len - 2, "\"v\":\"%s\"",
+                                         curr_record->value.value_s);
+            }
+            else if (curr_record->value_type == SENML_TYPE_BOOL) {
+                chars_written = snprintf(&output[insert_pos], len - 2, "\"v\":%s",
+                                         curr_record->value.value_b ? "true" : "false");
+            }
+            else if (curr_record->value_type == SENML_TYPE_BINARY) {
+                // FIXME unclear how binary data is represented, ignoring it for now
+            }
+
+            insert_pos += chars_written;
+            len -= chars_written;
+
+            if (chars_written >= (ssize_t)len - 2 || chars_written < 0) {
+                return -1;
+            }
+        }
+
+        output[insert_pos] = '}';
+        insert_pos++;
+        len--;
+
+        output[insert_pos] = ',';
+        insert_pos++;
+        len--;
+    }
+
+    // remove trailing comma
+    if (insert_pos > 2) {
+        insert_pos--;
+    }
+
+    output[insert_pos] = ']';
+    insert_pos++;
+    output[insert_pos] = '\0';
+
+    return 0;
+}
