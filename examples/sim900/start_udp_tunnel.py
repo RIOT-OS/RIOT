6d72cfb0d0d13e6d4a45c98fb6a6890392f7f875
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-06-13 12:13:33 +0200

    Added try/except to prevent crashes

--- examples/sim900/start_udp_tunnel.py
+++ examples/sim900/start_udp_tunnel.py
@@ -5,17 +5,20 @@ sd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sd.bind(("", 9876))                                                             
                                                                                 
 while True:                                                                     
-    t = select.select([sd.fileno()], [], [], 1)                                 
-    if sd.fileno() in t[0]:                                                     
-        data = sd.recvfrom(1024)                                                
-        recv = IPv6(data[0])                                                    
-        #swap src with dest                                                     
-        print "Received packet is:"                                             
-        recv.show2()                                                            
-                                                                                
-        #Change ICMPv6 to echo reply                                            
-        a = sr1(recv, timeout=3)                                                
-                                                                                
-        print "Reply packet is:"                                              
-        a.show2()                                                               
-        sd.sendto(str(a) ,data[1])   
+    try:
+        t = select.select([sd.fileno()], [], [], 1)                                 
+        if sd.fileno() in t[0]:                                                     
+            data = sd.recvfrom(1024)                                                
+            recv = IPv6(data[0])                                                    
+            #swap src with dest                                                     
+            print "Received packet is:"                                             
+            recv.show2()                                                            
+                                                                                    
+            #Change ICMPv6 to echo reply                                            
+            a = sr1(recv, timeout=3)                                                
+                                                                                    
+            print "Reply packet is:"                                              
+            a.show2()                                                               
+            sd.sendto(str(a) ,data[1])   
+    except:
+        pass
