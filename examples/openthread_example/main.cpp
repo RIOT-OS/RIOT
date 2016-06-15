cb99f6af30f251a955d1a4aab79eb11b3715c4f8
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-06-15 16:30:41 +0200

    openthread: added project files

---
+++ examples/openthread_example/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "openthread.h"
+
+int main(void)
+{
+    puts("Hello World!");
+
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    otGetLeaderWeight();
+
+    return 0;
+}
