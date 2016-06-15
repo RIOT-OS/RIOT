c553d0c519b7bef64afb2d61b43373f0580f386c
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-06-15 18:12:28 +0200

    updated template of alarm hooks

--- pkg/openthread/contrib/platform_alarm.c
+++ pkg/openthread/contrib/platform_alarm.c
@@ -1,11 +1,7 @@
 #include <platform/alarm.h>
 #include <stdint.h>
 
-void otPlatAlarmInit(void)
-{
-}
-
-void PlatAlarmStartAt(uint32_t aT0, uint32_t aDt)
+void otPlatAlarmStartAt(uint32_t aT0, uint32_t aDt)
 {
 }
 
@@ -18,6 +14,6 @@ uint32_t otPlatAlarmGetNow(void)
 	return 0;
 }
 
-void otPlatAlarmSignalFired(void)
+void otPlatAlarmFired(void)
 {
 }
