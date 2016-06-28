f80447cc8db9247a211577770d4fa5156bf3da3e
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-06-28 13:43:11 +0200

    Added seed based on CPUID

--- examples/openthread_example/main.cpp
+++ examples/openthread_example/main.cpp
@@ -11,38 +11,39 @@
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 
+#include "periph/cpuid.h"
 
-static uint8_t buf[100];
+
+static char buf[100];
 char ot_thread_stack[THREAD_STACKSIZE_MAIN];
 
 
 int main(void)
 {
-	xtimer_init();
 	msg_t msg;
 	serial_msg_t sm;
 	sm.len = sizeof(buf)+1;
 	msg.type = OPENTHREAD_SERIAL_MSG_TYPE_EVENT;
 	msg.content.ptr = &sm;
 
-    char buf[30];
     char *p=buf;
 	sm.buf = (uint8_t*) buf;
     sm.len = 0;
-	int now = xtimer_now();
-	DEBUG("Waiting for seed. Please press enter\n");
-	while(0)
-	{
-		if(getchar() == '\r' || getchar() == '\n')
-		{
-			now = xtimer_now()-now;
-			DEBUG("Seed is %i\n", now);
-			break;
-		}
-	}
-	random_init(now);
-    kernel_pid_t pid = thread_create(ot_thread_stack, sizeof(ot_thread_stack),
-                            THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+#ifdef CPUID_LEN
+    char cpu_id[CPUID_LEN];
+    cpuid_get(cpu_id);
+    int seed = 0;
+    for(int i=0;i<(int) CPUID_LEN;i++)
+    {
+        seed += cpu_id[i];
+    }
+    random_init(seed);
+#else
+    random_init(123);
+#endif
+
+   kernel_pid_t pid = thread_create(ot_thread_stack, sizeof(ot_thread_stack),
+                           THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
 			                                ot_thread, NULL, "ot_thread");
     DEBUG("Starting main's loop\n");
 #ifdef CPUID_LEN
