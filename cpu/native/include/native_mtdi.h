1e615caef17c7a626ea6771f0bc117d67e7d6502
Vincent Dupont <vincent@otakeys.com>
2016-07-11 09:53:13 +0200

    native: add pseudo flash

---
+++ cpu/native/include/native_mtdi.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016  OTA keys S.A.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef NATIVE_MTDI_H
+#define NATIVE_MTDI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const mtdi_desc_t native_flash_driver;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NATIVE_MTDI_H */
