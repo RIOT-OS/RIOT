d71f1c5b1836f03f06ad7209056238b817bd00dc
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-07-11 19:09:27 +0200

    openthread: added OT cli as option

--- examples/openthread_example/main.cpp
+++ examples/openthread_example/main.cpp
@@ -1,46 +1,12 @@
-#include "msg.h"
-#include <stdio.h>
-#include <openthread.h>
-#include "ot.h"
-#include "thread.h"
-#include "xtimer.h"
-#include "msg.h"
-#include "random.h"
-#include "string.h"
 
-#define ENABLE_DEBUG (0)
-#include "debug.h"
-
-#include "periph/cpuid.h"
-
-
-static char buf[100];
-
+#ifdef MODULE_OPENTHREAD_CLI
+#include "ot-cli.h"
+#endif
 
 int main(void)
 {
-	msg_t msg;
-	serial_msg_t sm;
-	sm.len = sizeof(buf)+1;
-	msg.type = OPENTHREAD_SERIAL_MSG_TYPE_EVENT;
-	msg.content.ptr = &sm;
-
-    char *p=buf;
-	sm.buf = (uint8_t*) buf;
-    sm.len = 0;
-	while(1)
-	{
-        *p = getchar();
-        if(*p == '\r' || *p == '\n')
-        {
-            sm.len = p-buf+1; 
-            msg_send(&msg, openthread_get_pid());
-            p=buf;
-        }
-        else
-        {
-            p++;
-        }
-	}
+#ifdef MODULE_OPENTHREAD_CLI
+    openthread_cli_run();
+#endif
 	return 0;
 }
