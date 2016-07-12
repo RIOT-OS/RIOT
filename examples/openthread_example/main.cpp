0d4bb42650169f6ee886d409d67f7e6345fa2e78
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-07-12 13:32:13 +0200

    openthread: added state command to RIOT shell

--- examples/openthread_example/main.cpp
+++ examples/openthread_example/main.cpp
@@ -1,18 +1,45 @@
-
 #ifdef MODULE_OPENTHREAD_CLI
 #include "ot-cli.h"
 #else
 #include <openthread.h>
 #endif
 
-
+#include "shell.h"
+#include "shell_commands.h"
+#include <stdio.h>
 
 #ifndef MODULE_OPENTHREAD_CLI
+int state_cmd(int argc, char **argv)
+{
+    switch(otGetDeviceRole())
+    {
+        case kDeviceRoleDisabled:
+            printf("disabled\n");
+            break;
+        case kDeviceRoleDetached:
+            printf("detached\n");
+            break;
+        case kDeviceRoleChild:
+            printf("child\n");
+            break;
+        case kDeviceRoleRouter:
+            printf("router\n");
+            break;
+        case kDeviceRoleLeader:
+            printf("leader\n");
+            break;
+    }
+    return 0;
+}
+static const shell_command_t shell_commands[] = {
+        { "state", "get OpenThread state", state_cmd },
+            { NULL, NULL, NULL }
+};
+
 void cli_run(void)
 {
-    while(1)
-    {
-    }
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 }
 #endif
 
