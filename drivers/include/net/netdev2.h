cc6fc7331446745d07d87e514aedd1ea5f6c1194
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-07-19 10:48:13 +0200

    radio: added TX_COMPLETE_DATA_PENDING in at86rf2xx

--- drivers/include/net/netdev2.h
+++ drivers/include/net/netdev2.h
@@ -74,6 +74,7 @@ typedef enum {
     NETDEV2_EVENT_RX_COMPLETE,      /**< finished receiving a packet */
     NETDEV2_EVENT_TX_STARTED,       /**< started to transfer a packet */
     NETDEV2_EVENT_TX_COMPLETE,      /**< finished transferring packet */
+	NETDEV2_EVENT_TX_COMPLETE_DATA_PENDING, /**< finished transferring packet and has data pending flag **/
     NETDEV2_EVENT_TX_NOACK,         /**< ACK requested but not received */
     NETDEV2_EVENT_TX_MEDIUM_BUSY,   /**< couldn't transfer packet */
     NETDEV2_EVENT_LINK_UP,          /**< link established */
