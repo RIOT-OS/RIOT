3956a26a4d6f956f1c27b3fcca6740133d9f5027
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-07-25 11:31:43 +0200

    added mutex to conn files

--- pkg/openthread/contrib/conn/conn_udp.c
+++ pkg/openthread/contrib/conn/conn_udp.c
@@ -6,6 +6,7 @@
 #include <net/ipv6/addr.h>
 #include "ot.h"
 #include "errno.h"
+#include "platform.h"
 
 static msg_t _msg;
 
@@ -27,8 +28,10 @@ int conn_udp_create(conn_udp_t *conn, const void *addr, size_t addr_len, int fam
 	memcpy(&sockaddr.mAddress.mFields, addr, addr_len);
     sockaddr.mPort = port;
 
+	begin_mutex();
     otOpenUdpSocket(&conn->mSocket, &HandleUdpReceived, conn);
     otBindUdpSocket(&conn->mSocket, &sockaddr);
+	end_mutex();
     return 0;
 }
 
@@ -42,9 +45,11 @@ int conn_udp_sendto(const void *data, size_t len, const void *src, size_t src_le
     otMessage message;
 	otUdpSocket mSocket;
 
+	begin_mutex();
     message = otNewUdpMessage();
     otSetMessageLength(message, len);
     otWriteMessage(message, 0, data, len);
+	end_mutex();
 
 	otMessageInfo mPeer;
 
@@ -61,13 +66,17 @@ int conn_udp_sendto(const void *data, size_t len, const void *src, size_t src_le
     mPeer.mPeerPort = dport;
 
 	//Send UDP packet through OT
+	begin_mutex();
     otSendUdp(&mSocket, message, &mPeer);
+	end_mutex();
 	return 0;
 }
 
 void conn_udp_close(conn_udp_t *conn)
 {
+	begin_mutex();
 	otCloseUdpSocket(&conn->mSocket);
+	end_mutex();
 }
 
 int conn_udp_getlocaladdr(conn_udp_t *conn, void *addr, uint16_t *port)
@@ -89,9 +98,11 @@ int conn_udp_recvfrom(conn_udp_t *conn, void *data, size_t max_len, void *addr, 
 		if(msg.type == OPENTHREAD_MSG_TYPE_RECV)
 		{
 			cudp = (conn_udp_msg_t*) msg.content.ptr;
+			begin_mutex();
 			msg_offset = otGetMessageOffset(cudp->message);
 			msg_length = otGetMessageLength(cudp->message)-msg_offset;
 			otReadMessage(cudp->message, msg_offset, data, msg_length);
+			end_mutex();
 			memcpy(addr, &cudp->message_info->mPeerAddr.mFields, sizeof(ipv6_addr_t));
 			*addr_len = sizeof(ipv6_addr_t);;
 			*port = cudp->message_info->mPeerPort;
