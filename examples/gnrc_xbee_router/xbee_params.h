9328f89fd80d32c92780e9af129bcad1c6aec7bd
Yonezawa-T2 <Yonezawa-T2@mail.dnp.co.jp>
2016-07-26 16:33:29 +0900

    fixup! examples: add example of border router with XBee on native platform.

--- examples/gnrc_xbee_router/xbee_params.h
+++ examples/gnrc_xbee_router/xbee_params.h
@@ -26,9 +26,8 @@ extern "C" {
 static xbee_params_t xbee_params[] = {
     { .uart = XBEE_UART,
       .baudrate = 9600U,
-      // .baudrate = 115200U,
       .sleep_pin = GPIO_UNDEF,
-      .status_pin = GPIO_UNDEF
+      .reset_pin = GPIO_UNDEF
     },
 };
 
