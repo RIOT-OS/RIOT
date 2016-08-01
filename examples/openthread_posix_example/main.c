0c944a26b7d12d4da1f6196e6013f72c4829f990
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-08-01 16:25:27 +0200

    added posix_sockets example main.c file

--- examples/openthread_posix_example/main.c
+++ examples/openthread_posix_example/main.c
@@ -1,17 +1,49 @@
-#include "ot.h"
+/*
+ * Copyright (C) 2015 Martine Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Example application for demonstrating the RIOT's POSIX sockets
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "msg.h"
 #include "shell.h"
-#include "shell_commands.h"
 
+#define MAIN_MSG_QUEUE_SIZE (4)
+static msg_t main_msg_queue[MAIN_MSG_QUEUE_SIZE];
+
+extern int udp_cmd(int argc, char **argv);
+
+static const shell_command_t shell_commands[] = {
+    { "udp", "send data over UDP and listen on UDP ports", udp_cmd },
+    { NULL, NULL, NULL }
+};
 
 int main(void)
 {
-#if defined(MODULE_OPENTHREAD_CLI) || defined(MODULE_OPENTHREAD_NCP)
-    openthread_uart_run();
-#else
-    char line_buf[3*SHELL_DEFAULT_BUFSIZE];
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-#endif
+    /* a sendto() call performs an implicit bind(), hence, a message queue is
+     * required for the thread executing the shell */
+    msg_init_queue(main_msg_queue, MAIN_MSG_QUEUE_SIZE);
+    puts("RIOT socket example application");
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
-	return 0;
+    /* should be never reached */
+    return 0;
 }
