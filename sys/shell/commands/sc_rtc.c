a63238e2a2f02a018d87078323bd2065016bc139
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-08-01 20:01:41 +0200

    sys/shell: Adapt rtc command to new RTC API

--- sys/shell/commands/sc_rtc.c
+++ sys/shell/commands/sc_rtc.c
@@ -1,6 +1,7 @@
 /*
  * Copyright 2013 INRIA.
  * Copyright 2014 Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * Copyright 2016 Eistec AB
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -14,20 +15,19 @@
  * @file
  * @brief       Shell command implementation for the peripheral RTC interface
  *
- * @author  Oliver Hahm <oliver.hahm@inria.fr>
- * @author  Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>#
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * @author      Joakim Nohlgård <joakim.nohlgard@eistec.se>
  *
  * @}
  */
-
-#ifdef FEATURE_PERIPH_RTC
 
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 
-#include "periph/rtc.h"
+#include "rtc.h"
 
 static void _alarm_handler(void *arg)
 {
@@ -83,67 +83,69 @@ static int _print_time(struct tm *time)
     return 0;
 }
 
-static int _rtc_getalarm(void)
+static int _rtc_getalarm(rtc_t *rtc)
 {
-    struct tm t;
-    if (rtc_get_alarm(&t) == 0) {
-        _print_time(&t);
-        return 0;
-    }
-    else {
-        puts("rtc: error getting alarm");
+    struct tm target;
+    int err = rtc_get_alarm(rtc, &target);
+    if (err != 0) {
+        printf("rtc: error %d getting alarm\n", err);
         return 1;
     }
+    _print_time(&target);
+    return 0;
 }
 
-static int _rtc_setalarm(char **argv)
+static int _rtc_setalarm(rtc_t *rtc, char **argv)
+{
+    struct tm target;
+
+    if (_parse_time(argv, &target) != 0) {
+        puts("rtc: error parsing time");
+        return 1;
+    }
+    int err = rtc_set_alarm(rtc, &target, _alarm_handler, NULL);
+    if (err != 0) {
+        printf("rtc: error %d setting alarm\n", err);
+        return 1;
+    }
+    return 0;
+}
+
+static int _rtc_gettime(rtc_t *rtc)
+{
+    struct tm now;
+    int err = rtc_get_time(rtc, &now);
+    if (err != 0) {
+        printf("rtc: error %d getting time\n", err);
+        return 1;
+    }
+    _print_time(&now);
+    return 0;
+}
+
+static int _rtc_settime(rtc_t *rtc, char **argv)
 {
     struct tm now;
 
-    if (_parse_time(argv, &now) == 0) {
-        if (rtc_set_alarm(&now, _alarm_handler, NULL) == -1) {
-            puts("rtc: error setting alarm");
-            return 1;
-        }
-        return 0;
-    }
-    return 1;
-}
-
-static int _rtc_gettime(void)
-{
-    struct tm t;
-    if (rtc_get_time(&t) == 0) {
-        _print_time(&t);
-        return 0;
-    }
-    else {
-        puts("rtc: error getting time");
+    if (_parse_time(argv, &now) != 0) {
+        puts("rtc: error parsing time");
         return 1;
     }
-}
-
-static int _rtc_settime(char **argv)
-{
-    struct tm now;
-
-    if (_parse_time(argv, &now) == 0) {
-        if (rtc_set_time(&now) == -1) {
-            puts("rtc: error setting time");
-            return 1;
-        }
-        return 0;
+    _print_time(&now);
+    int err = rtc_set_time(rtc, &now);
+    if (err != 0) {
+        printf("rtc: error %d setting time\n", err);
+        return 1;
     }
-    return 1;
+    return 0;
 }
 
 static int _rtc_usage(void)
 {
     puts("usage: rtc <command> [arguments]");
     puts("commands:");
+    puts("\tlist\t\tlist all registered RTCs");
     puts("\tinit\t\tinitialize the interface");
-    puts("\tpoweron\t\tpower the interface on");
-    puts("\tpoweroff\tpower the interface off");
     puts("\tclearalarm\tdeactivate the current alarm");
     puts("\tgetalarm\tprint the currently alarm time");
     puts("\tsetalarm YYYY-MM-DD HH:MM:SS\n\t\t\tset an alarm for the specified time");
@@ -152,54 +154,65 @@ static int _rtc_usage(void)
     return 0;
 }
 
+void _rtc_list(void) {
+    rtc_t *rtc = rtc_iterate(NULL); /* Get first device */
+    if (rtc == NULL) {
+        /* No RTCs */
+        puts("No RTCs registered");
+        return;
+    }
+    puts("These are the currently registered RTC devices:");
+    while(rtc != NULL) {
+        printf("%s\n", rtc->name); /* newlib will print (nil) if name is NULL */
+        rtc = rtc_iterate(rtc);
+    }
+}
+
 int _rtc_handler(int argc, char **argv)
 {
     if (argc < 2) {
         _rtc_usage();
         return 1;
     }
-    else if (strncmp(argv[1], "init", 4) == 0) {
-        rtc_init();
+    const char *cmd = argv[1];
+    if (strcmp(cmd, "list") == 0) {
+        _rtc_list();
+        return 0;
     }
-    else if (strncmp(argv[1], "poweron", 7) == 0) {
-        rtc_poweron();
+
+    if (argc < 3) {
+        _rtc_usage();
+        return 1;
     }
-    else if (strncmp(argv[1], "poweroff", 8) == 0) {
-        rtc_poweroff();
+
+    const char *name = argv[2];
+    rtc_t *rtc = rtc_find(name);
+    if (rtc == NULL) {
+        printf("No RTC by that name: %s\n", name);
+        return 1;
     }
-    else if (strncmp(argv[1], "clearalarm", 8) == 0) {
-        rtc_clear_alarm();
+
+    if (strcmp(cmd, "init") == 0) {
+        rtc_init(rtc);
     }
-    else if (strncmp(argv[1], "getalarm", 8) == 0) {
-        _rtc_getalarm();
+    else if (strcmp(cmd, "clearalarm") == 0) {
+        rtc_clear_alarm(rtc);
     }
-    else if (strncmp(argv[1], "setalarm", 8) == 0) {
-        _rtc_setalarm(argv + 2);
+    else if (strcmp(cmd, "getalarm") == 0) {
+        _rtc_getalarm(rtc);
     }
-    else if (strncmp(argv[1], "gettime", 7) == 0) {
-        _rtc_gettime();
+    else if (strcmp(cmd, "setalarm") == 0) {
+        _rtc_setalarm(rtc, argv + 3);
     }
-    else if (strncmp(argv[1], "settime", 7) == 0) {
-        _rtc_settime(argv + 2);
+    else if (strcmp(cmd, "gettime") == 0) {
+        _rtc_gettime(rtc);
+    }
+    else if (strcmp(cmd, "settime") == 0) {
+        _rtc_settime(rtc, argv + 3);
     }
     else {
-        printf("unknown command: %s\n", argv[1]);
+        printf("unknown command: %s\n", cmd);
         return 1;
     }
     return 0;
 }
-
-#else
-
-#include <stdio.h>
-
-int _rtc_handler(int argc, char **argv)
-{
-    (void) argc;
-    (void) argv;
-
-    puts("not implemented");
-    return 1;
-}
-
-#endif /* FEATURE_RTC */
