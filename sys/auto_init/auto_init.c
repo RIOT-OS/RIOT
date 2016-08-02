199b21cc807cd8b457e9ab15a46547afc256f621
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-08-02 14:35:08 +0200

    auto_init: added auto init for OpenThread module

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -84,6 +84,10 @@
 #include "lwip.h"
 #endif
 
+#ifdef MODULE_OPENTHREAD
+#include "ot.h"
+#endif
+
 #ifdef MODULE_FIB
 #include "net/fib.h"
 #endif
@@ -165,6 +169,10 @@ void auto_init(void)
 #ifdef MODULE_LWIP
     DEBUG("Bootstraping lwIP.\n");
     lwip_bootstrap();
+#endif
+#ifdef MODULE_OPENTHREAD
+    extern void openthread_bootstrap(void);
+    openthread_bootstrap();
 #endif
 
 /* initialize network devices */
