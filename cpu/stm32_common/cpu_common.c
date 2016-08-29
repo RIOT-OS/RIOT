2b37d369a06c436af99125e81e2c86afd58f2da9
Pieter Willemsen <pieter.willemsen@altran.com>
2016-08-29 17:21:36 +0200

    stm32_common: add delay when turn on peripheral clock
    Implements workaround 1 (use __DSB()) proposed in the stm32 errata
    It was possible to lose data when you configure a peripheral
    right after enabling its clock

--- cpu/stm32_common/cpu_common.c
+++ cpu/stm32_common/cpu_common.c
@@ -21,6 +21,9 @@
 #include "periph_conf.h"
 #include "periph_cpu_common.h"
 
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
 uint32_t periph_apb_clk(uint8_t bus)
 {
     if (bus == APB1) {
@@ -31,20 +34,66 @@ uint32_t periph_apb_clk(uint8_t bus)
     }
 }
 
-void periph_clk_en(uint8_t bus, uint32_t mask)
+void periph_clk_en(bus_t bus, uint32_t mask)
 {
-    if (bus == APB1) {
-        RCC->APB1ENR |= mask;
-    } else {
-        RCC->APB2ENR |= mask;
+    switch (bus) {
+        case APB1:
+            RCC->APB1ENR |= mask;
+            break;
+        case APB2:
+            RCC->APB2ENR |= mask;
+            break;
+#if defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1) || defined(CPU_FAM_STM32F1) \
+            || defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F3)
+        case AHB:
+            RCC->AHBENR |= mask;
+            break;
+#elif defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4)
+        case AHB1:
+            RCC->AHB1ENR |= mask;
+            break;
+        case AHB2:
+            RCC->AHB2ENR |= mask;
+            break;
+        case AHB3:
+            RCC->AHB3ENR |= mask;
+            break;
+#endif
+        default:
+            DEBUG("unsupported bus %d\n", (int)bus);
+            break;
     }
+    /* stm32xx-errata: Delay after a RCC peripheral clock enable */
+    __DSB();
 }
 
-void periph_clk_dis(uint8_t bus, uint32_t mask)
+void periph_clk_dis(bus_t bus, uint32_t mask)
 {
-    if (bus == APB1) {
-        RCC->APB1ENR &= ~(mask);
-    } else {
-        RCC->APB2ENR &= ~(mask);
+    switch (bus) {
+        case APB1:
+            RCC->APB1ENR &= ~(mask);
+            break;
+        case APB2:
+            RCC->APB2ENR &= ~(mask);
+            break;
+#if defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1) || defined(CPU_FAM_STM32F1) \
+            || defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32F3)
+        case AHB:
+            RCC->AHBENR &= ~(mask);
+            break;
+#elif defined(CPU_FAM_STM32F2) || defined(CPU_FAM_STM32F4)
+        case AHB1:
+            RCC->AHB1ENR &= ~(mask);
+            break;
+        case AHB2:
+            RCC->AHB2ENR &= ~(mask);
+            break;
+        case AHB3:
+            RCC->AHB3ENR &= ~(mask);
+            break;
+#endif
+        default:
+            DEBUG("unsupported bus %d\n", (int)bus);
+            break;
     }
 }
