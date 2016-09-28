0aecf2352722b2590dee95ab9b65aa88655eab99
Neil Jones <neil.jones@imgtec.com>
2016-09-28 16:20:48 +0100

    boards: mips-malta: Add basic malta FPGA support.
    Add basic support for the MIPS malta FPGA platform.

---
+++ boards/mips-malta/include/board.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016, Imagination Technologies Limited and/or its
+ *                 affiliated group companies.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TICKS_PER_US 15 /* needed by timer code */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _BOARD_H_ */
---
+++ boards/mips-malta/include/periph_conf.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2016, Imagination Technologies Limited and/or its
+ *                 affiliated group companies.
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+#ifndef _PERIPH_CONF_H_
+#define _PERIPH_CONF_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Note this file must exist for xtimer code to build */
+#define TIMER_NUMOF         (1U)
+#define TIMER_0_CHANNELS    3
+
+/* This value must exist even if no uart in use */
+#define UART_NUMOF          (0U)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*_PERIPH_CONF_H_*/
