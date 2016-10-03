1d2fa71a4fd2c7e19a1e0627e893aeaedaff73fc
José Ignacio Alamos Aste <jialamos@uc.cl>
2016-10-03 19:00:04 -0300

    added otPlat diag functions

---
+++ pkg/openthread/contrib/platform_diag.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include <openthread-config.h>
+#include <openthread.h>
+
+#include <platform/alarm.h>
+
+/**
+ * diagnostics mode flag.
+ *
+ */
+static bool sDiagMode = false;
+
+void otPlatDiagProcess(int argc, char *argv[], char *aOutput, size_t aOutputMaxLen)
+{
+    // add more plarform specific diagnostics features here
+
+    (void)argc;
+}
+
+void otPlatDiagModeSet(bool aMode)
+{
+    sDiagMode = aMode;
+}
+
+bool otPlatDiagModeGet(void)
+{
+    return sDiagMode;
+}
+
