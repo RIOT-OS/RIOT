6159a38f8b4c429f6d55a23008d18a1e3795ac0b
Neil Jones <neil.jones@imgtec.com>
2016-10-07 10:12:10 +0100

    boards/mips-malta: Fix style issues.

--- boards/mips-malta/include/periph_conf.h
+++ boards/mips-malta/include/periph_conf.h
@@ -13,12 +13,13 @@ extern "C" {
 #endif
 
 /* Note this file must exist for xtimer code to build */
-#define TIMER_NUMOF         (1U)
-#define TIMER_0_CHANNELS    3
+#define TIMER_NUMOF         (1)
+#define TIMER_0_CHANNELS    (3)
 
 /* This value must exist even if no uart in use */
-#define UART_NUMOF          (0U)
+#define UART_NUMOF          (0)
 
+#define MIPS_DSP            (1)
 #ifdef __cplusplus
 }
 #endif
