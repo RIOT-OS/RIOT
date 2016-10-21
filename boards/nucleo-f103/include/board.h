7ee1b9a6f30fc10e3c472066af08667f672be56d
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-10-21 16:44:26 +0200

    boards/nucleo-f103: set stdio uart to USB port by default

--- boards/nucleo-f103/include/board.h
+++ boards/nucleo-f103/include/board.h
@@ -28,9 +28,9 @@ extern "C" {
 #endif
 
 /**
- * @brief Use the 2nd UART for STDIO on this board
+ * @brief Use the first UART for STDIO on this board
  */
-#define UART_STDIO_DEV      UART_DEV(1)
+#define UART_STDIO_DEV      UART_DEV(0)
 
 /**
  * @name xtimer configuration
