ddda017c96850357db41279ecb10f4ba944a397a
Martine Lenders <m.lenders@fu-berlin.de>
2016-10-30 20:11:11 +0100

    static-tests: report cppcheck errors in modified files as errors
    I'm not sure who thought this was a good idea, but if they are not
    reported as errors no-one will look at this.

--- dist/tools/ci/build_and_test.sh
+++ dist/tools/ci/build_and_test.sh
@@ -88,8 +88,7 @@ then
         #   Remove all but `${CI_BASE_BRANCH}` parameters to cppcheck (and remove second
         #   invocation) once all warnings of cppcheck have been taken care of
         #   in ${CI_BASE_BRANCH}.
-        run ./dist/tools/cppcheck/check.sh ${CI_BASE_BRANCH} --diff-filter=MR --error-exitcode=0
-        run ./dist/tools/cppcheck/check.sh ${CI_BASE_BRANCH} --diff-filter=AC
+        run ./dist/tools/cppcheck/check.sh ${CI_BASE_BRANCH} --diff-filter=ACMR
         run ./dist/tools/pr_check/pr_check.sh ${CI_BASE_BRANCH}
         exit $RESULT
     fi
