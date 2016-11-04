39491177390b3b693fe1796d6889d368317ed891
Neil Jones <neil.jones@imgtec.com>
2016-11-04 10:15:53 +0000

    cpu: pic32: UART clean-ups
    Remove unused defines
    Remove duplicate defines available in cpu part files.
    Make uart data static.

--- cpu/mips_pic32_common/periph/uart.c
+++ cpu/mips_pic32_common/periph/uart.c
@@ -13,21 +13,9 @@
 #define UxMODE(U)    (U.regs[0x00/4])
 #define UxMODECLR(U) (U.regs[0x04/4])
 #define UxMODESET(U) (U.regs[0x08/4])
-#define ON           (0x8000)
-#define STSEL        (1)
-#define PDSEL_9N     (6)
-#define PDSEL_8O     (4)
-#define PDSEL_8E     (2)
-#define PDSEL_8N     (0)
-#define UEN_CTSRTS   (0x200)
 #define UxSTA(U)     (U.regs[0x10/4])
 #define UxSTACLR(U)  (U.regs[0x14/4])
 #define UxSTASET(U)  (U.regs[0x18/4])
-#define URXDA        (0x1)
-#define TRMT         (0x100)
-#define UTXBF        (0x200)
-#define UTXEN        (0x400)
-#define URXEN        (0x1000)
 #define UxTXREG(U)   (U.regs[0x20/4])
 #define UxRXREG(U)   (U.regs[0x30/4])
 #define UxBRG(U)     (U.regs[0x40/4])
@@ -41,7 +29,7 @@ typedef struct PIC32_UART_tag {
 } PIC32_UART_T;
 
 /* pic uarts are numbered 1 to 6 */
-PIC32_UART_T pic_uart[UART_NUMOF + 1];
+static PIC32_UART_T pic_uart[UART_NUMOF + 1];
 
 int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
 {
@@ -55,9 +43,9 @@ int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
 
     UxBRG(pic_uart[uart])= (pic_uart[uart].clock / (16 * baudrate)) - 1;
     UxSTA(pic_uart[uart])= 0;
-    UxMODE(pic_uart[uart])= ON;
-    UxSTASET(pic_uart[uart])= URXEN;
-    UxSTASET(pic_uart[uart])= UTXEN;
+    UxMODE(pic_uart[uart])= _U1MODE_ON_MASK;
+    UxSTASET(pic_uart[uart])= _U1STA_URXEN_MASK;
+    UxSTASET(pic_uart[uart])= _U1STA_UTXEN_MASK;
 
     return 0;
 }
@@ -67,7 +55,7 @@ void uart_write(uart_t uart, const uint8_t *data, size_t len)
     assert(uart <= UART_NUMOF && uart != 0);
 
     while(len--) {
-        while(UxSTA(pic_uart[uart])& UTXBF) {}
+        while(UxSTA(pic_uart[uart])& _U1STA_UTXBF_MASK) {}
         UxTXREG(pic_uart[uart]) = *data++;
     }
 }
@@ -76,7 +64,7 @@ void uart_poweron(uart_t uart)
 {
     assert(uart <= UART_NUMOF && uart != 0);
 
-    UxMODESET(pic_uart[uart])= ON;
+    UxMODESET(pic_uart[uart])= _U1MODE_ON_MASK;
 
 }
 
@@ -84,5 +72,5 @@ void uart_poweroff(uart_t uart)
 {
     assert(uart <= UART_NUMOF && uart != 0);
 
-    UxMODECLR(pic_uart[uart])= ON;
+    UxMODECLR(pic_uart[uart])= _U1MODE_ON_MASK;
 }
