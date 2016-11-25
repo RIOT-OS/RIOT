e483c2307bac2099a7f33c5c3b2c92e2a4c87f38
Frits Kuipers <f.p.kuipers@student.utwente.nl>
2016-11-25 20:35:19 +0100

    boards/maple-mini: initial support

---
+++ boards/maple-mini/board.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Frits Kuipers
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_maple-mini
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the maple-mini board
+ *
+ * @author      Frits Kuipers <frits.kuipers@gmail.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+
+    /* initialize the boards LED */
+    gpio_init(LED0_PIN, GPIO_OUT);
+}
