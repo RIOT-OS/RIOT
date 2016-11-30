529c71cd04bef31f6b8e221a5f2e070f8e173179
kYc0o <fco.ja.ac@gmail.com>
2016-11-30 15:34:54 +0100

    sys: Add fw_slots module

---
+++ sys/fw_slots/fw_slots.c
@@ -0,0 +1,511 @@
+/*
+ * Copyright (c) 2016, Mark Solters <msolters@gmail.com>.
+ *               2016, Francisco Acosta <francisco.acosta@inria.fr>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @file
+ * @author      Mark Solters <msolters@gmail.com>
+ * @author      Francisco Acosta <francisco.acosta@inria.fr>
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <tweetnacl.h>
+#include "fw_slots.h"
+#include "cpu_conf.h"
+#include "irq.h"
+#include "periph/flashpage.h"
+#include "hashes/sha256.h"
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+#define HASH_BUF             (1024)
+
+static uint8_t firmware_buffer[HASH_BUF];
+
+/**
+ * @brief       Read internal flash to a buffer at specific address.
+ *
+ * @param[in]   address - Address to be read.
+ * @param[in]   count - count in bytes.
+ *
+ * @param[out]  data_buffer - The buffer filled with the read information.
+ *
+ */
+static void int_flash_read(uint8_t *data_buffer, uint32_t address, uint32_t count)
+{
+    uint8_t *read_addres = (uint8_t*)address;
+    while (count--) {
+        *data_buffer++ = *read_addres++;
+    }
+}
+
+int validate_int_fw_slot(uint8_t fw_slot)
+{
+    FW_metadata_t metadata;
+    int res;
+    unsigned char n[crypto_box_NONCEBYTES];
+    unsigned char hash[NACL_SIGN];
+
+    if (verify_int_fw_slot(fw_slot) != 0) {
+        printf("[fw_slots] ERROR verification for slot %i failed!\n", fw_slot);
+        return -1;
+    }
+
+    if (get_int_fw_slot_metadata(fw_slot, &metadata) != 0) {
+        printf("[fw_slots] ERROR cannot get metadata from slot %i!\n", fw_slot);
+        return -1;
+    }
+
+    memset(hash, 0, sizeof(hash));
+
+    printf("Decrypting metadata.shash...\n");
+
+    res = crypto_box_open(hash, metadata.shash, NACL_SIGN, n, server_pkey, firmware_skey);
+    if (res) {
+        printf("Decryption failed.\n");
+        return -1;
+    } else {
+        printf("Decryption successful! verifying...");
+        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+            if (metadata.hash[i] != (hash[i + crypto_box_ZEROBYTES])) {
+                printf("[fw_slots] ERROR incorrect decrypted hash!\n");
+                return -1;
+            }
+        }
+    }
+
+    printf("[fw_slots] FW slot %i successfully validated!\n", fw_slot);
+
+    return 0;
+}
+
+uint32_t get_slot_fw_address(uint8_t fw_slot)
+{
+    return get_slot_address(fw_slot);
+}
+
+uint32_t get_slot_fw_page(uint8_t fw_slot)
+{
+    return get_slot_page(fw_slot);
+}
+
+void print_metadata(FW_metadata_t *metadata)
+{
+    printf("Firmware Size: %ld\n", metadata->size);
+    printf("Firmware Version: %#x\n", metadata->version);
+    printf("Firmware UUID: %#lx\n", metadata->uuid);
+    printf("Firmware HASH: ");
+    for (unsigned long i = 0; i < sizeof(metadata->hash); i++) {
+        printf("%02x ", metadata->hash[i]);
+    }
+    printf("\n");
+    printf("Firmware signed HASH: ");
+    for (unsigned long i = 0; i < sizeof(metadata->shash); i++) {
+        printf("%02x ", metadata->shash[i]);
+    }
+    printf("\n");
+}
+
+int get_int_metadata(uint8_t fw_slot_page, FW_metadata_t *fw_metadata)
+{
+    uint32_t fw_address;
+
+    fw_address = fw_slot_page * FLASHPAGE_SIZE + CPU_FLASH_BASE;
+
+    DEBUG("[fw_slots] Getting internal metadata on page %d at address %#lx\n",
+            fw_slot_page, fw_address);
+    int_flash_read((uint8_t*)fw_metadata, fw_address, sizeof(FW_metadata_t));
+
+    return 0;
+}
+
+int get_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata)
+{
+    /*
+     * TODO
+     */
+
+    return 0;
+}
+
+int get_int_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata)
+{
+    uint32_t page;
+
+    DEBUG("[fw_slots] Getting internal FW slot %d metadata\n", fw_slot);
+    if (fw_slot > MAX_FW_SLOTS || fw_slot == 0) {
+        printf("[fw_slots] FW slot not valid, should be <= %d and > 0\n",
+                MAX_FW_SLOTS);
+        return -1;
+    }
+
+    page = get_slot_fw_page(fw_slot);
+
+    return get_int_metadata(page, fw_slot_metadata);
+}
+
+int overwrite_int_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata)
+{
+    /*
+     * TODO
+     */
+    return 0;
+}
+
+
+
+int overwrite_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata)
+{
+    /*
+     * TODO
+     */
+    return 0;
+}
+
+int backup_golden_image(void)
+{
+    /*
+     * TODO
+     */
+    return 0;
+}
+
+int verify_int_fw_slot(uint8_t fw_slot)
+{
+    FW_metadata_t fw_metadata;
+    uint32_t fw_image_address;
+    uint32_t address;
+    uint16_t rest;
+    sha256_context_t sha256_ctx;
+    uint8_t hash[SHA256_DIGEST_LENGTH];
+    int parts = 0, i = 0;
+
+    /* Determine the external flash address corresponding to the FW slot */
+    if (fw_slot > MAX_FW_SLOTS || fw_slot == 0) {
+        printf("[fw_slots] FW slot not valid, should be <= %d and > 0\n",
+                MAX_FW_SLOTS);
+        return -1;
+    }
+
+    /* Read the metadata of the corresponding FW slot */
+    fw_image_address = get_slot_fw_address(fw_slot);
+
+    if (get_int_fw_slot_metadata(fw_slot, &fw_metadata) == 0) {
+        print_metadata(&fw_metadata);
+    } else {
+        printf("[fw_slots] ERROR cannot get slot metadata.\n");
+    }
+
+    printf("Verifying slot %d at 0x%lx \n", fw_slot, fw_image_address);
+
+    address = fw_image_address;
+    address += FW_METADATA_SPACE;
+    sha256_init(&sha256_ctx);
+
+    parts = fw_metadata.size / sizeof(firmware_buffer);
+    rest = fw_metadata.size % sizeof(firmware_buffer);
+
+    while (parts) {
+        int_flash_read(firmware_buffer, address, sizeof(firmware_buffer));
+        sha256_update(&sha256_ctx, firmware_buffer, sizeof(firmware_buffer));
+        address += sizeof(firmware_buffer);
+        parts--;
+    }
+
+    int_flash_read(firmware_buffer, address, rest);
+    sha256_update(&sha256_ctx, firmware_buffer, rest);
+    sha256_final(&sha256_ctx, hash);
+
+    for (i = 0; i < sizeof(hash); i++) {
+        if (hash[i] != fw_metadata.hash[i]) {
+            printf("[fw_slots] hash verification failed!\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int validate_fw_metadata(FW_metadata_t *metadata)
+{
+    /* Is the FW slot erased?
+     * First, we check to see if every byte in the metadata is 0xFF.
+     * If this is the case, this metadata is "erased" and therefore we assume
+     * the FW slot to be empty.
+     */
+    int erased = 1;
+    uint8_t *metadata_ptr = (uint8_t*)metadata;
+    int b = FW_METADATA_LENGTH;
+
+    while (b--) {
+        if (*metadata_ptr++ != 0xff) {
+            /* We encountered a non-erased byte.
+             * There's some non-trivial data here.
+             */
+            erased = 0;
+            break;
+        }
+    }
+
+    /* If the FW slot is erased, it's not valid!  No more work to do here. */
+    if (erased) {
+        return 0;
+    }
+
+    /* If we get this far, all metadata bytes were cleared (0xff) */
+    return 0;
+}
+
+int find_matching_int_fw_slot(uint16_t version)
+{
+    int matching_slot = -1; /* Assume there is no matching FW slot. */
+
+    /* Iterate through each of the FW slots. */
+    for (int slot = 1; slot <= MAX_FW_SLOTS; slot++) {
+
+        /* Get the metadata of the current FW slot. */
+        FW_metadata_t fw_slot_metadata;
+        if(get_int_fw_slot_metadata(slot, &fw_slot_metadata) == 0) {
+            print_metadata(&fw_slot_metadata);
+        } else {
+            printf("[fw_slots] ERROR cannot get slot metadata.\n");
+        }
+
+        /* Is this slot empty? If yes, skip. */
+        if (validate_fw_metadata(&fw_slot_metadata) == false) {
+            continue;
+        }
+
+        /* Does this slot's FW version match our search parameter? */
+        if (fw_slot_metadata.version == version) {
+            matching_slot = slot;
+            break;
+        }
+    }
+
+    if (matching_slot == -1) {
+        printf("[fw_slots] No FW slot matches Firmware v%i\n", version);
+    } else {
+        printf("[fw_slots] FW slot #%i matches Firmware v%i\n", matching_slot,
+                version);
+    }
+
+    return matching_slot;
+}
+
+int find_empty_int_fw_slot(void)
+{
+    /* Iterate through each of the MAX_FW_SLOTS internal slots. */
+    for (int slot = 1; slot <= MAX_FW_SLOTS; slot++) {
+
+        /* Get the metadata of the current FW slot. */
+        FW_metadata_t fw_slot_metadata;
+
+        if(get_int_fw_slot_metadata(slot, &fw_slot_metadata) == 0) {
+            print_metadata(&fw_slot_metadata);
+        } else {
+            printf("[fw_slots] ERROR cannot get slot metadata.\n");
+        }
+
+        /* Is this slot invalid? If yes, let's treat it as empty. */
+        if (validate_fw_metadata(&fw_slot_metadata) == false) {
+            return slot;
+        }
+    }
+
+    printf("[fw_slots] Could not find any empty FW slots!"
+            "\nSearching for oldest FW slot...\n");
+    /*
+     * If execution goes this far, no empty slot was found. Now, we look for
+     * the oldest FW slot instead.
+     */
+    return find_oldest_int_fw_image();
+}
+
+int find_oldest_int_fw_image(void)
+{
+    /* The oldest firmware should be the v0 */
+    int oldest_fw_slot = 1;
+    uint16_t oldest_firmware_version = 0;
+
+    /* Iterate through each of the MAX_FW_SLOTS internal slots. */
+    for (int slot = 1; slot <= MAX_FW_SLOTS; slot++) {
+        /* Get the metadata of the current FW slot. */
+        FW_metadata_t fw_slot_metadata;
+
+        if(get_int_fw_slot_metadata(slot, &fw_slot_metadata) == 0) {
+            print_metadata(&fw_slot_metadata);
+        } else {
+            printf("[fw_slots] ERROR cannot get slot metadata.\n");
+        }
+
+        /* Is this slot populated? If not, skip. */
+        if (validate_fw_metadata(&fw_slot_metadata) == false) {
+            continue;
+        }
+
+        /* Is this the oldest image we've found thus far? */
+        if (oldest_firmware_version) {
+            if (fw_slot_metadata.version < oldest_firmware_version) {
+                oldest_fw_slot = slot;
+                oldest_firmware_version = fw_slot_metadata.version;
+            }
+        } else {
+            oldest_fw_slot = slot;
+            oldest_firmware_version = fw_slot_metadata.version;
+        }
+    }
+
+    printf("[fw_slots] Oldest FW slot: #%u; Firmware v%u\n", oldest_fw_slot,
+            oldest_firmware_version);
+
+    return oldest_fw_slot;
+}
+
+int find_newest_int_fw_image(void)
+{
+    /* At first, we only assume knowledge of version v0 */
+    int newest_fw_slot = 0;
+    uint16_t newest_firmware_version = 0;
+
+    /* Iterate through each of the MAX_FW_SLOTS. */
+    for (int slot = 1; slot <= MAX_FW_SLOTS ; slot++) {
+        /* Get the metadata of the current FW slot. */
+        FW_metadata_t fw_slot_metadata;
+
+        if(get_int_fw_slot_metadata(slot, &fw_slot_metadata) == 0) {
+            print_metadata(&fw_slot_metadata);
+        } else {
+            printf("[fw_slots] ERROR cannot get slot metadata.\n");
+        }
+
+        /* Is this slot populated? If not, skip. */
+        if (validate_fw_metadata( &fw_slot_metadata) == false) {
+            continue;
+        }
+
+        /* Is this the newest non-Golden Image image we've found thus far? */
+        if ( fw_slot_metadata.version > newest_firmware_version ) {
+            newest_fw_slot = slot;
+            newest_firmware_version = fw_slot_metadata.version;
+        }
+    }
+
+    printf("Newest FW slot: #%u; Firmware v%u\n", newest_fw_slot,
+            newest_firmware_version);
+
+    return newest_fw_slot;
+}
+
+int erase_int_fw_image(uint8_t fw_slot)
+{
+    /* Get page address of the fw_slot in internal flash */
+    uint32_t fw_image_base_address;
+    /* Get the page where the fw_slot is located */
+    uint8_t slot_page;
+
+    if (fw_slot > MAX_FW_SLOTS || fw_slot == 0) {
+        printf("[fw_slots] FW slot not valid, should be <= %d and > 0\n",
+                MAX_FW_SLOTS);
+        return -1;
+    }
+
+    fw_image_base_address = get_slot_fw_address(fw_slot);
+
+    printf("[fw_slots] Erasing FW slot %u [%#lx, %#lx]...\n", fw_slot,
+            fw_image_base_address,
+            fw_image_base_address + (FW_SLOT_PAGES * FLASHPAGE_SIZE) - 1);
+
+    slot_page = get_slot_fw_page(fw_slot);
+
+    /* Erase each page in the FW internal slot! */
+    for (int page = slot_page; page < slot_page + FW_SLOT_PAGES; page++) {
+        DEBUG("[fw_slots] Erasing page %d\n", page);
+        flashpage_write(page, NULL);
+    }
+
+    printf("[fw_slots] Erase successful\n");
+
+    return 0;
+}
+
+int update_firmware(uint8_t fw_slot)
+{
+    /*
+     * TODO
+     */
+    return 0;
+}
+
+int store_firmware_data( uint32_t ext_address, uint8_t *data, size_t data_length )
+{
+    /*
+     * TODO
+     */
+    return 0;
+}
+
+/*
+ * _estack pointer needed to reset PSP position
+ */
+extern uint32_t _estack;
+
+void jump_to_image(uint32_t destination_address)
+{
+    if (destination_address) {
+        /*
+         * Only add the metadata length offset if destination_address is NOT 0!
+         * (Jumping to 0x0 is used to reboot the device)
+         */
+        destination_address += FW_METADATA_SPACE;
+    }
+
+    /* Disable IRQ */
+    (void)irq_disable();
+
+    /* Move PSP to the end of the stack */
+    __set_PSP((uint32_t)&_estack);
+
+    /* Move to the second pointer on VTOR (reset_handler_default) */
+    destination_address += VTOR_RESET_HANDLER;
+
+    printf("Addr: 0x%lx\n", destination_address);
+
+    /* Load the destination address */
+    __asm("LDR R0, [%[dest]]"::[dest]"r"(destination_address));
+    /* Make sure the Thumb State bit is set. */
+    __asm("ORR R0, #1");
+    /* Branch execution */
+    __asm("BX R0");
+}
---
+++ sys/include/fw_slots.h
@@ -0,0 +1,278 @@
+/*
+ * Copyright (c) 2016, Mark Solters <msolters@gmail.com>
+ *               2016, Francisco Acosta <francisco.acosta@inria.fr>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @defgroup    sys_fw_slots Firmware Slots
+ * @ingroup     sys
+ *
+ * @file
+ * @brief       FW Image R/W and Verification
+ *
+ * @author      Mark Solters <msolters@gmail.com>
+ * @author      Francisco Acosta <francisco.acosta@inria.fr>
+ */
+
+#ifndef FW_SLOTS_H
+#define FW_SLOTS_H
+
+#include "hashes/sha256.h"
+#include "tweetnacl.h"
+
+/*
+ *  FW_METADATA_LENGTH:
+ *    This is just the size of the FW_metadata_t struct, which is 4-byte
+ *    aligned. We use 104 bytes currently, so this struct will be 104 bytes.
+ */
+#define FW_METADATA_LENGTH  sizeof(FW_metadata_t)
+
+#define NACL_SIGN            (SHA256_DIGEST_LENGTH + crypto_box_ZEROBYTES)
+
+typedef struct FW_metadata {
+    uint8_t hash[SHA256_DIGEST_LENGTH]; /* SHA256 Hash of firmware image */
+    uint8_t shash[NACL_SIGN];           /* Signed SHA256 */
+    uint32_t size;                      /* Size of firmware image */
+    uint32_t uuid;                      /* Integer representing unique firmware ID */
+    uint16_t version;                   /* Integer representing firmware version */
+} FW_metadata_t;
+
+extern const unsigned char server_pkey[];
+extern const unsigned char firmware_skey[];
+
+/**
+ * @brief  Print formatted FW image metadata to STDIO.
+ *
+ * @param[in] metadata          Metadata struct to fill with firmware metadata
+ *
+ */
+void print_metadata(FW_metadata_t *metadata);
+
+/**
+ * @brief   Validate internal FW slot as a secure firmware
+ *
+ * @param[in] fw_slot          The FW slot to be validated.
+ *
+ * @return  0 on success or error code
+ */
+int validate_int_fw_slot(uint8_t fw_slot);
+
+/**
+ * @brief   Get the internal metadata belonging to an FW slot in internal
+ *          flash.
+ *
+ * @param[in] fw_slot             The FW slot to be read for metadata.
+ *
+ * @param[in] *fw_slot_metadata   Pointer to the FW_metadata_t struct where
+ *                                 the metadata is to be written.
+ *
+ * @return  0 on success or error code
+ */
+int get_int_metadata(uint8_t fw_slot_page, FW_metadata_t *fw_metadata);
+
+/**
+ * @brief   Get the metadata belonging to an FW slot in external flash.
+ *
+ * @param[in] fw_slot            The FW slot to be read for metadata.
+ *
+ * @param[in] *fw_slot_metadata  Pointer to the FW_metadata_t struct where
+ *                                the metadata is to be written.
+ *
+ * @return  0 on success or error code
+ */
+int get_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata);
+
+/**
+ * @brief   Get the metadata belonging to an FW slot in internal flash.
+ *
+ * @param[in] fw_slot             The FW slot to be read for metadata.
+ *
+ * @param[in] *fw_slot_metadata   Pointer to the FW_metadata_t struct where
+ *                                 the metadata is to be written.
+ *
+ * @return  0 on success or error code
+ */
+int get_int_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata);
+
+/**
+ * @brief   Get the address corresponding to a given slot
+ *
+ * @param[in] fw_slot             The FW slot to get the address.
+ *
+ *
+ * @return  0 on success or error code
+ */
+uint32_t get_slot_fw_address(uint8_t fw_slot);
+
+/**
+ * @brief   Get the page corresponding to a given slot
+ *
+ * @param[in] fw_slot             The FW slot to get the page.
+ *
+ *
+ * @return  0 on success or error code
+ */
+uint32_t get_slot_fw_page(uint8_t fw_slot);
+
+/**
+ * @brief   Write new metadata to a specific FW slot in internal flash.
+ *
+ * @param   fw_slot             The FW slot to be modified.
+ *
+ * @param   *fw_slot_metadata   Pointer to the new FW_metadata_t data.
+ *
+ * @return  0 on success or error code
+ */
+int overwrite_int_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata);
+
+/**
+ * @brief   Write new metadata to a specific FW slot in external flash.
+ *
+ * @param   fw_slot             The FW slot to be modified.
+ *
+ * @param   *fw_slot_metadata   Pointer to the new FW_metadata_t data.
+ *
+ * @return  0 on success or error code
+ */
+int overwrite_fw_slot_metadata(uint8_t fw_slot, FW_metadata_t *fw_slot_metadata);
+
+/**
+ * @brief   Copy the current firmware into FW slot 0 as the "Golden Image"
+ *
+ * @return  0 for success or error code
+ */
+int backup_golden_image(void);
+
+/**
+ * @brief   Given an FW slot, verify the firmware content against the metadata.
+ *          If everything is fine, update the metadata to indicate this FW slot
+ *          is valid.
+ *
+ * @param[in]  fw_slot  - FW slot index to verify. (1-3)
+ *
+ * @return  0 for success or error code
+ */
+int verify_int_fw_slot(uint8_t fw_slot);
+
+/**
+ * @brief   Returns true only if the metadata provided indicates the FW slot
+ *          is populated and valid.
+ *
+ * @param[in] *metadata          FW metadata to be validated
+ *
+ * @return  True if the FW slot is populated and valid. Otherwise, false.
+ */
+int validate_fw_metadata(FW_metadata_t *metadata);
+
+/**
+ * @brief   Find a FW slot containing firmware matching the supplied
+ *          firmware version number. Will only find the first matching
+ *          slot.
+ *
+ * @param[in] version            FW slot version.
+ *
+ * @return  The FW slot index of the matching FW slot. Return -1 in the event
+ *          of no match.
+ */
+int find_matching_int_fw_slot(uint16_t version);
+
+/**
+ * @brief   Find the first empty FW download slot.  Failing this, find the slot
+ *          with the most out-of-date firmware version.
+ *
+ * @return  The FW slot index of the empty/oldest FW slot.  This will never be
+ *          0 because the Golden Image should never be erased.
+ */
+int find_empty_int_fw_slot(void);
+
+/**
+ * @brief   Find the FW slot containing the most out-of-date firmware version.
+ *          FW slots are in external flash.
+ *
+ * @return  The FW slot index of the oldest firmware version.
+ */
+int find_oldest_int_fw_image(void);
+
+/**
+ * @brief   Find the FW slot containing the most recent firmware version.
+ *          FW slots are in external flash.
+ *
+ * @return  The FW slot index of the newest firmware version.
+ */
+int find_newest_int_fw_image(void);
+
+/**
+ * @brief   Clear an FW slot in external flash.
+ *
+ * @param[in] fw_slot   The FW slot index of the firmware image to be copied.
+ *
+ * @return  0 or error code
+ */
+int erase_int_fw_image(uint8_t fw_slot);
+
+/**
+ * @brief   Overwrite firmware located in internal flash with the firmware
+ *          stored in an external flash FW slot.
+ *
+ * @param[in] fw_slot   The FW slot index of the firmware image to be copied.
+ *                       0 = "Golden Image" backup, aka factory restore
+ *                       1, 2, 3 = FW Download slots
+ *
+ * @return  0 or error code
+ */
+int update_firmware(uint8_t fw_slot);
+
+/**
+ * @brief   Store firmware data in external flash at the specified
+ *          address.
+ *
+ * @param[in] ext_address   External flash address to begin writing data.
+ *
+ * @param[in] data          Pointer to the data buffer to be written.
+ *                          Note: page_data can be larger than 4096 bytes, but
+ *                          only the first 4096 bytes will be written!
+ *
+ * @return  0 or error code
+ */
+int store_firmware_data(uint32_t ext_address, uint8_t *data, size_t data_length);
+
+/**
+ * @brief   Begin executing another firmware binary located in internal flash.
+ *
+ * @param[in] destination_address Internal flash address of the vector table
+ *                                for the firmware binary that is to be booted
+ *                                into. Since this FW lib prepends metadata
+ *                                to each binary, the true VTOR start address
+ *                                will be FW_METADATA_SPACE bytes past this
+ *                                address.
+ *
+ */
+void jump_to_image(uint32_t destination_address);
+
+#endif /* FW_SLOTS_H */
