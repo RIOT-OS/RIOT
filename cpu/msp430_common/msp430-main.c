3485ab4e9651ff2b615a451a052e591761307018
Vincent Dupont <vincent@otakeys.com>
2016-12-06 18:26:31 +0100

    msp430: add libc missing symbols and defines

--- cpu/msp430_common/msp430-main.c
+++ cpu/msp430_common/msp430-main.c
@@ -177,4 +177,15 @@ splx_(int sr)
 }
 /*---------------------------------------------------------------------------*/
 
+size_t strnlen(const char *s, size_t maxlen)
+{
+    size_t len;
+
+    for (len = 0; len < maxlen; len++, s++) {
+        if (!*s)
+            break;
+    }
+    return (len);
+}
+
 extern void board_init(void);
