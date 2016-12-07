ab12d60d05d8f048396d0618b99c92805de80818
Hauke Petersen <hauke.petersen@fu-berlin.de>
2016-12-07 12:56:24 +0100

    boards/stm32-based: adapted timer configurations

--- boards/stm32f4discovery/include/board.h
+++ boards/stm32f4discovery/include/board.h
@@ -32,7 +32,7 @@ extern "C" {
  * @name xtimer configuration
  * @{
  */
-#define XTIMER_DEV          TIMER_0
+#define XTIMER_DEV          TIMER_DEV(0)
 #define XTIMER_CHAN         (0)
 #define XTIMER_OVERHEAD     (6)
 #define XTIMER_BACKOFF      (10)
