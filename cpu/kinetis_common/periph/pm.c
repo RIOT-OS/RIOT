2c5a9a5cbfd26ddd4c3f40dab0cd62d7ebf793c8
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-01-13 12:28:17 +0100

    cpu/kinetis: unified PM implementation

--- cpu/kinetis_common/periph/pm.c
+++ cpu/kinetis_common/periph/pm.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
  *               2014 Eistec AB
+ *               2017 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -24,17 +25,17 @@
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
-
+/**
+ * @note    The current PM implementation is very much simplified down to only
+ *          using the 'WAIT' mode. This implementation must be further expanded
+ *          to make use of the available and more efficient (deep) sleep modes
+ *          of the Kinetis CPUs.
+ */
 void pm_set(unsigned mode)
 {
     switch (mode) {
         case 0:
-            SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
+            cortexm_sleep(0);
             break;
     }
-
-    /* Executes a device DSB (Data Synchronization Barrier) */
-    __DSB();
-    /* Enter standby mode */
-    __WFI();
 }
