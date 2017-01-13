a37ba98bfd4a39b3ae842cf70e3b92ad1bd52372
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-01-13 10:43:13 +0100

    cpu/msp430-common: moved pm_reboot to periph/pm.c

--- cpu/msp430_common/cpu.c
+++ cpu/msp430_common/cpu.c
@@ -107,15 +107,3 @@ char *thread_stack_init(thread_task_func_t task_func, void *arg, void *stack_sta
 
     return (char *) stackptr;
 }
-
-/******************************************************************************/
-
-/* System reboot */
-void pm_reboot(void)
-{
-    /* force an hardware reboot ("Power-Up Clear"), by writing
-       an illegal value to the watchdog control register */
-    while (1) {
-        WDTCTL = 0x0000;
-    }
-}
