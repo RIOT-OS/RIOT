569e494efa371d6d0d398a77cf83339fd8588665
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-01-23 13:54:55 +0100

    boards/nucleo-f072: fix ADC configuration

--- boards/nucleo-f072/include/periph_conf.h
+++ boards/nucleo-f072/include/periph_conf.h
@@ -99,18 +99,16 @@ static const uart_conf_t uart_config[] = {
  * @{
  */
 #define ADC_CONFIG {            \
-    { GPIO_PIN(PORT_A, 0), 0 },\
-    { GPIO_PIN(PORT_A, 1), 1 },\
-    { GPIO_PIN(PORT_A, 4), 4 },\
-    { GPIO_PIN(PORT_B, 0), 8 },\
+    { GPIO_PIN(PORT_A, 0), 0 }, \
+    { GPIO_PIN(PORT_A, 1), 1 }, \
+    { GPIO_PIN(PORT_A, 4), 4 }, \
+    { GPIO_PIN(PORT_B, 0), 8 }, \
     { GPIO_PIN(PORT_C, 1), 11 },\
     { GPIO_PIN(PORT_C, 0), 10 } \
 }
 
 #define ADC_NUMOF           (6)
-
 /** @} */
-
 
 /**
  * @brief   DAC configuration
