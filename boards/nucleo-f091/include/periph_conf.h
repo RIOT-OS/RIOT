d860393076bb08ba734831bbdb78afccf997854c
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-01-23 10:17:39 +0100

    boards/nucleo-f091: add ADC configuration

--- boards/nucleo-f091/include/periph_conf.h
+++ boards/nucleo-f091/include/periph_conf.h
@@ -139,9 +139,17 @@ static const pwm_conf_t pwm_config[] = {
  * @brief   ADC configuration
  * @{
  */
-#define ADC_NUMOF          (0)
-/** @} */
+#define ADC_CONFIG {            \
+    { GPIO_PIN(PORT_A, 0), 0 },\
+    { GPIO_PIN(PORT_A, 1), 1 },\
+    { GPIO_PIN(PORT_A, 4), 4 },\
+    { GPIO_PIN(PORT_B, 0), 8 },\
+    { GPIO_PIN(PORT_C, 1), 11 },\
+    { GPIO_PIN(PORT_C, 0), 10 } \
+}
 
+#define ADC_NUMOF           (6)
+/** @} */
 /**
  * @brief   DAC configuration
  * @{
