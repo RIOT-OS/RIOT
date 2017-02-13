7daa33b55e483c3af5c9b7b9b3ca789106ce6a1d
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-02-13 10:40:01 +0100

    boards/nucleo-f070: use new interrupt function name for 3rd uart

--- boards/nucleo-f070/include/periph_conf.h
+++ boards/nucleo-f070/include/periph_conf.h
@@ -102,7 +102,7 @@ static const uart_conf_t uart_config[] = {
 
 #define UART_0_ISR          (isr_usart2)
 #define UART_1_ISR          (isr_usart1)
-#define UART_2_ISR          (isr_usart3_8)
+#define UART_2_ISR          (isr_usart3_4)
 
 #define UART_NUMOF          (sizeof(uart_config) / sizeof(uart_config[0]))
 /** @} */
