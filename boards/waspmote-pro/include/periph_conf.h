8f7c67da405e49fe85723e35be25ee0cac4b6762
Laurent Navet <laurent.navet@gmail.com>
2017-02-15 22:40:18 +0100

    boards/waspmote-pro: add adc definiton
    Allow to build with atmega_common adc support.

--- boards/waspmote-pro/include/periph_conf.h
+++ boards/waspmote-pro/include/periph_conf.h
@@ -106,6 +106,24 @@ extern "C" {
 #define MEGA_PRR            PRR0        /* Power Reduction Register */
 /** @} */
 
+/**
+* @brief ADC configuration
+*
+* @{
+*/
+#define ADC_NUMOF       (8)
+/* ADC Channels */
+#define ADC_PIN_0           0
+#define ADC_PIN_1           1
+#define ADC_PIN_2           2
+#define ADC_PIN_3           3
+#define ADC_PIN_4           4
+#define ADC_PIN_5           5
+#define ADC_PIN_6           6
+#define ADC_PIN_7           7
+/** @} */
+
+
 #ifdef __cplusplus
 }
 #endif
