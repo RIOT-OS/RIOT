ffff6112659a56ea17f44596cb79f846137010a2
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-02-16 12:46:49 +0100

    tests/saul: added some more descriptive output

--- tests/saul/main.c
+++ tests/saul/main.c
@@ -46,9 +46,12 @@ int main(void)
 
         while (dev) {
             int dim = saul_reg_read(dev, &res);
+            printf("\nDev: %s\tType: %s\n", dev->name,
+                   saul_class_to_str(dev->driver->type));
             phydat_dump(&res, dim);
             dev = dev->next;
         }
+        puts("\n##########################");
 
         xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
     }
