b091ca9f329f5d928af8521c69be1986a1622b3f
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-03-13 13:49:43 +0100

    cpu/stm32f0: fix doxygen warning

--- cpu/stm32f0/include/vectors/stm32f072rb_isr.h
+++ cpu/stm32f0/include/vectors/stm32f072rb_isr.h
@@ -21,37 +21,41 @@
 #ifndef STM32F072RB_ISR_H
 #define STM32F072RB_ISR_H
 
-(void*) isr_wwdg,               /* windowed watchdog */
-(void*) isr_pvd,                /* power control */
-(void*) isr_rtc,                /* real time clock */
-(void*) isr_flash,              /* flash memory controller */
-(void*) isr_rcc,                /* reset and clock control */
-(void*) isr_exti,               /* external interrupt lines 0 and 1 */
-(void*) isr_exti,               /* external interrupt lines 2 and 3 */
-(void*) isr_exti,               /* external interrupt lines 4 to 15 */
-(void*) isr_ts,                 /* touch sensing input*/
-(void*) isr_dma1_ch1,           /* direct memory access controller 1, channel 1*/
-(void*) isr_dma1_ch2_3,         /* direct memory access controller 1, channel 2 and 3*/
-(void*) isr_dma1_ch4_5,         /* direct memory access controller 1, channel 4 and 5*/
-(void*) isr_adc1_comp,          /* analog digital converter */
-(void*) isr_tim1_brk_up_trg_com, /* timer 1 break, update, trigger and communication */
-(void*) isr_tim1_cc,            /* timer 1 capture compare */
-(void*) isr_tim2,               /* timer 2 */
-(void*) isr_tim3,               /* timer 3 */
-(void*) isr_tim6_dac,           /* timer 6 and digital to analog converter */
-(void*) isr_tim7,               /* timer 7 */
-(void*) isr_tim14,              /* timer 14 */
-(void*) isr_tim15,              /* timer 15 */
-(void*) isr_tim16,              /* timer 16 */
-(void*) isr_tim17,              /* timer 17 */
-(void*) isr_i2c1,               /* I2C 1 */
-(void*) isr_i2c2,               /* I2C 2 */
-(void*) isr_spi1,               /* SPI 1 */
-(void*) isr_spi2,               /* SPI 2 */
-(void*) isr_usart1,             /* USART 1 */
-(void*) isr_usart2,             /* USART 2 */
-(void*) isr_usart3_4,           /* USART 3 and 4 */
-(void*) isr_cec,                /* consumer electronics control */
-(void*) isr_usb                 /* USB */
+(void*) isr_wwdg,                /**< windowed watchdog */
+(void*) isr_pvd,                 /**< power control */
+(void*) isr_rtc,                 /**< real time clock */
+(void*) isr_flash,               /**< flash memory controller */
+(void*) isr_rcc,                 /**< reset and clock control */
+(void*) isr_exti,                /**< external interrupt lines 0 and 1 */
+(void*) isr_exti,                /**< external interrupt lines 2 and 3 */
+(void*) isr_exti,                /**< external interrupt lines 4 to 15 */
+(void*) isr_ts,                  /**< touch sensing input*/
+(void*) isr_dma1_ch1,            /**< direct memory access controller 1, channel 1*/
+(void*) isr_dma1_ch2_3,          /**< direct memory access controller 1, channel 2 and 3*/
+(void*) isr_dma1_ch4_5,          /**< direct memory access controller 1, channel 4 and 5*/
+(void*) isr_adc1_comp,           /**< analog digital converter */
+(void*) isr_tim1_brk_up_trg_com, /**< timer 1 break, update, trigger and communication */
+(void*) isr_tim1_cc,             /**< timer 1 capture compare */
+(void*) isr_tim2,                /**< timer 2 */
+(void*) isr_tim3,                /**< timer 3 */
+(void*) isr_tim6_dac,            /**< timer 6 and digital to analog converter */
+(void*) isr_tim7,                /**< timer 7 */
+(void*) isr_tim14,               /**< timer 14 */
+(void*) isr_tim15,               /**< timer 15 */
+(void*) isr_tim16,               /**< timer 16 */
+(void*) isr_tim17,               /**< timer 17 */
+(void*) isr_i2c1,                /**< I2C 1 */
+(void*) isr_i2c2,                /**< I2C 2 */
+(void*) isr_spi1,                /**< SPI 1 */
+(void*) isr_spi2,                /**< SPI 2 */
+(void*) isr_usart1,              /**< USART 1 */
+(void*) isr_usart2,              /**< USART 2 */
+(void*) isr_usart3_4,            /**< USART 3 and 4 */
+(void*) isr_cec,                 /**< consumer electronics control */
+(void*) isr_usb                  /**< USB */
+
+#ifdef __cplusplus
+extern "C" {}
+#endif
 
 #endif /* STM32F072RB_ISR_H */
