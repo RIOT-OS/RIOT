6157f9238c8f27e5f7554ec95acf9bb8ba016fd2
Martine Lenders <m.lenders@fu-berlin.de>
2017-03-20 11:48:30 +0100

    evtimer: add and fix doc

--- sys/include/evtimer.h
+++ sys/include/evtimer.h
@@ -7,31 +7,31 @@
  */
 
 /**
- * @defgroup    sys_evtimer Millisecond interval timers
+ * @defgroup    sys_evtimer Millisecond interval event timers
  * @ingroup     sys
- * @brief       Provides timers for events up to 2**32 milliseconds in the future
+ * @brief       Provides timers for events up to @$2^{32}@$ milliseconds in the
+ *              future
  *
- * RIOT's main timer subsystem is xtimer, but for many applications xtimer's
- * 64bit absolute time values are wasteful or clumsy to use.
+ * RIOT's main timer subsystem is @ref sys_xtimer "xtimer", but for many
+ * applications @ref sys_xtimer "xtimer's" 64-bit absolute time values are
+ * wasteful or clumsy to use.
  *
- * Compared to xtimer, evtimer offers:
+ * Compared to @ref sys_xtimer "xtimer", evtimer offers:
  *
- * - only relative 32bit millisecond timer values
+ * - only relative 32-bit millisecond timer values
  *   Events can be scheduled with a relative offset of up to ~49.7 days in the
  *   future.
- *   For time-critical stuff, use xtimer!
- *
- * - more flexible, "intrusive" timer type
- *   evtimer_event_t only contains the necessary fields, which can be extended
- *   as needed, and handlers define actions taken on timer triggers.
- *   Check out evtimer_msg_event as example.
- *
- * - uses xtimer as backend
+ *   **For time-critical stuff, use @ref sys_xtimer "xtimer"!**
+ * - more flexible, "intrusive" timer type @ref evtimer_event_t only contains
+ *   the necessary fields, which can be extended as needed, and handlers define
+ *   actions taken on timer triggers. Check out @ref evtimer_msg_event_t as
+ *   example.
+ * - uses @ref sys_xtimer "xtimer" as backend
  *
  * @{
  *
  * @file
- * @brief       evtimer API
+ * @brief       evtimer API definitions
  *
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
  */
@@ -48,26 +48,56 @@
 extern "C" {
 #endif
 
-/** forward declaration */
-typedef struct evtimer_event evtimer_event_t;
+/**
+ * @brief   Generic event
+ */
+typedef struct evtimer_event {
+    struct evtimer_event *next; /**< the next event in the queue */
+    uint32_t offset;            /**< offset in milliseconds from previous event */
+} evtimer_event_t;
 
+
+/**
+ * @brief   Event timer
+ */
 typedef struct {
-    xtimer_t timer;
-    evtimer_event_t *events;
+    xtimer_t timer;             /**< Timer */
+    evtimer_event_t *events;    /**< Event queue */
 } evtimer_t;
 
-struct evtimer_event {
-    evtimer_event_t *next;
-    uint32_t offset;
-};
-
+/**
+ * @brief   IPC-message event
+ * @extends evtimer_event_t
+ */
 typedef struct {
-    evtimer_event_t event;
-    msg_t msg;
+    evtimer_event_t event;      /**< base class */
+    msg_t msg;                  /**< the IPC message to generate on event */
 } evtimer_msg_event_t;
 
-void evtimer_init(evtimer_t *evtimer, void(*handler)(void*));
+/**
+ * @brief   Initializes an event timer
+ *
+ * @param[in] evtimer   An event timer
+ * @param[in] handler   An event handler
+ */
+void evtimer_init(evtimer_t *evtimer, void (*handler)(void *));
+
+/**
+ * @brief   Adds event to an event timer
+ *
+ * @param[in] evtimer       An event timer
+ * @param[in] event         An event
+ */
 void evtimer_add(evtimer_t *evtimer, evtimer_event_t *event);
+
+/**
+ * @brief   Adds event to an event timer that handles events via IPC
+ *
+ * @param[in] evtimer       An event timer
+ * @param[in] event         An event
+ * @param[in] target_pid    The PID of the thread that should receive the IPC
+ *                          message
+ */
 static inline void evtimer_add_msg(evtimer_t *evtimer,
                                    evtimer_msg_event_t *event,
                                    kernel_pid_t target_pid)
@@ -76,12 +106,37 @@ static inline void evtimer_add_msg(evtimer_t *evtimer,
     event->msg.sender_pid = target_pid;
     evtimer_add(evtimer, &event->event);
 }
+
+/**
+ * @brief   Removes an event from an event timer
+ *
+ * @param[in] evtimer       An event timer
+ * @param[in] event         An event
+ */
 void evtimer_del(evtimer_t *evtimer, evtimer_event_t *event);
+
+/**
+ * @brief   Event handler for IPC messages
+ *
+ * @param[in] arg   The argument for the event handler
+ */
 void evtimer_msg_handler(void *arg);
+
+/**
+ * @brief   Initializes event timer to handle events via IPC
+ *
+ * @param[in] evtimer   An event timer
+ */
 static inline void evtimer_init_msg(evtimer_t *evtimer)
 {
     evtimer_init(evtimer, evtimer_msg_handler);
 }
+
+/**
+ * @brief   Print overview of current state of an event timer
+ *
+ * @param[in] evtimer   An event timer
+ */
 void evtimer_print(const evtimer_t *evtimer);
 
 #ifdef __cplusplus
