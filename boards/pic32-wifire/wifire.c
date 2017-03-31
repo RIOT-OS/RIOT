12690185cbb19ca0ab39b4b37d944ccf0184835c
Francois Berder <francois.berder@imgtec.com>
2017-03-31 10:46:11 +0100

    boards: pic32-wifire: Remove useless UART pin configuration
    Since the UART module takes control of RX and TX pins, there
    is no need to configure the pins.
    
    Signed-off-by: Francois Berder <francois.berder@imgtec.com>

--- boards/pic32-wifire/wifire.c
+++ boards/pic32-wifire/wifire.c
@@ -25,10 +25,6 @@ void board_init(void)
      */
     U4RXREG = 0xb;            /* connect pin RPF2 to UART 4 RX */
     RPF8R =   0x2;            /* connect pin RPF8 to UART 4 TX */
-    PORTFCLR =  BIT8 | BIT2;  /* clear down port F pins 2 and 8 */
-    TRISFCLR =  BIT2;         /* set portf pin 2 as input */
-    TRISFSET =  BIT8;         /* set portf pin 8 as output */
-    ODCFCLR =   BIT8 | BIT2;  /* set portf pint 2 and 8 as not open-drain */
 
     /* intialise UART used for debug (printf) */
 #ifdef DEBUG_VIA_UART
