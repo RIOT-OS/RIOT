cf10bbfea6aa6fbfbf0b70c45ecc623bc5fc880f
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-04-05 22:09:45 +0200

    boards/arduino-mkrzero: add initial support

---
+++ boards/arduino-mkrzero/board.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C)  2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_arduino-mkrzero
+ *
+ * @file
+ * @brief       Board specific implementations for the Arduino MKRZERO board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+    /* initialize the on-board Amber "L" LED on pin PA17 */
+    gpio_init(LED0_PIN, GPIO_OUT);
+}
---
+++ boards/arduino-mkrzero/include/arduino_pinmap.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C)  2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_arduino-mkrzero
+ * @{
+ *
+ * @file
+ * @brief       Mapping from MCU pins to Arduino pins
+ *
+ * You can use the defines in this file for simplified interaction with the
+ * Arduino specific pin numbers.
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ */
+
+#ifndef ARDUINO_PINMAP_H
+#define ARDUINO_PINMAP_H
+
+#include "periph/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Mapping of MCU pins to Arduino pins
+ * @{
+ */
+#define ARDUINO_PIN_0           GPIO_PIN(PA, 22)
+#define ARDUINO_PIN_1           GPIO_PIN(PA, 23)
+#define ARDUINO_PIN_2           GPIO_PIN(PA, 10)
+#define ARDUINO_PIN_3           GPIO_PIN(PA, 11)
+#define ARDUINO_PIN_4           GPIO_PIN(PB, 10)
+#define ARDUINO_PIN_5           GPIO_PIN(PB, 11)
+#define ARDUINO_PIN_6           GPIO_PIN(PA, 20) /* on-board LED */
+#define ARDUINO_PIN_7           GPIO_PIN(PA, 21)
+
+#define ARDUINO_PIN_A0          GPIO_PIN(PA, 2)
+#define ARDUINO_PIN_A1          GPIO_PIN(PB, 2)
+#define ARDUINO_PIN_A2          GPIO_PIN(PB, 3)
+#define ARDUINO_PIN_A3          GPIO_PIN(PA, 4)
+#define ARDUINO_PIN_A4          GPIO_PIN(PA, 5)
+#define ARDUINO_PIN_A5          GPIO_PIN(PA, 6)
+#define ARDUINO_PIN_A6          GPIO_PIN(PA, 7)
+/** @ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARDUINO_PINMAP_H */
+/** @} */
