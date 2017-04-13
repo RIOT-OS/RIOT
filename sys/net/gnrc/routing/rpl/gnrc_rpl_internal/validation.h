da38b77ba495ad3f2704045b219ae6a6fbeec044
BytesGalore <Martin.Landsmann@HAW-Hamburg.de>
2017-04-13 16:06:37 +0200

    rpl: drop DAO-ACK when received on multicast

--- sys/net/gnrc/routing/rpl/gnrc_rpl_internal/validation.h
+++ sys/net/gnrc/routing/rpl/gnrc_rpl_internal/validation.h
@@ -118,13 +118,21 @@ static inline bool gnrc_rpl_validation_DAO(gnrc_rpl_dao_t *dao, uint16_t len)
  *
  * @param[in]   dao_ack     The DAO-ACK control message
  * @param[in]   len         Length of the DAO-ACK control message
+ * @param[in]   dst         Pointer to the destination address of the IPv6 packet.
  *
  * @return  true, if @p dao_ack is valid
  * @return  false, otherwise
  */
-static inline bool gnrc_rpl_validation_DAO_ACK(gnrc_rpl_dao_ack_t *dao_ack, uint16_t len)
+static inline bool gnrc_rpl_validation_DAO_ACK(gnrc_rpl_dao_ack_t *dao_ack,
+                                               uint16_t len,
+                                               ipv6_addr_t *dst)
 {
     uint16_t expected_len = sizeof(*dao_ack) + sizeof(icmpv6_hdr_t);
+
+    if (ipv6_addr_is_multicast(dst)) {
+        DEBUG("RPL: received DAO-ACK on multicast address\n");
+        return false;
+    }
 
     if ((dao_ack->d_reserved & GNRC_RPL_DAO_ACK_D_BIT)) {
         expected_len += sizeof(ipv6_addr_t);
