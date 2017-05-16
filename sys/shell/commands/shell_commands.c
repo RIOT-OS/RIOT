fa1ccb4792e3ea1b5ebacb99f45cbdff69645a48
José Ignacio Alamos <jialamos@uc.cl>
2017-05-15 22:01:35 -0400

    revert unrelated changes in shell_commands file

--- sys/shell/commands/shell_commands.c
+++ sys/shell/commands/shell_commands.c
@@ -133,35 +133,35 @@ extern int _ls_handler(int argc, char **argv);
 #endif
 
 const shell_command_t _shell_command_list[] = {
-    { "reboot", "Reboot the node", _reboot_handler },
+    {"reboot", "Reboot the node", _reboot_handler},
 #ifdef MODULE_CONFIG
-    { "id", "Gets or sets the node's id.", _id_handler },
+    {"id", "Gets or sets the node's id.", _id_handler},
 #endif
 #ifdef MODULE_LPC_COMMON
-    { "heap", "Shows the heap state for the LPC2387 on the command shell.", _heap_handler },
+    {"heap", "Shows the heap state for the LPC2387 on the command shell.", _heap_handler},
 #endif
 #ifdef MODULE_PS
-    { "ps", "Prints information about running threads.", _ps_handler },
+    {"ps", "Prints information about running threads.", _ps_handler},
 #endif
 #ifdef MODULE_SHT11
-    { "temp", "Prints measured temperature.", _get_temperature_handler },
-    { "hum", "Prints measured humidity.", _get_humidity_handler },
-    { "weather", "Prints measured humidity and temperature.", _get_weather_handler },
-    { "offset", "Set temperature offset.", _set_offset_handler },
+    {"temp", "Prints measured temperature.", _get_temperature_handler},
+    {"hum", "Prints measured humidity.", _get_humidity_handler},
+    {"weather", "Prints measured humidity and temperature.", _get_weather_handler},
+    {"offset", "Set temperature offset.", _set_offset_handler},
 #endif
 #ifdef MODULE_LTC4150
-    { "cur", "Prints current and average power consumption.", _get_current_handler },
-    { "rstcur", "Resets coulomb counter.", _reset_current_handler },
+    {"cur", "Prints current and average power consumption.", _get_current_handler},
+    {"rstcur", "Resets coulomb counter.", _reset_current_handler},
 #endif
 #ifdef MODULE_AT30TSE75X
-    { "at30tse75x", "Test AT30TSE75X temperature sensor", _at30tse75x_handler },
+    {"at30tse75x", "Test AT30TSE75X temperature sensor", _at30tse75x_handler},
 #endif
 #ifdef MODULE_MCI
-    { DISK_READ_SECTOR_CMD, "Reads the specified sector of inserted memory card", _read_sector },
-    { DISK_READ_BYTES_CMD, "Reads the specified bytes from inserted memory card", _read_bytes },
-    { DISK_GET_SECTOR_SIZE, "Get the sector size of inserted memory card", _get_sectorsize },
-    { DISK_GET_SECTOR_COUNT, "Get the sector count of inserted memory card", _get_sectorcount },
-    { DISK_GET_BLOCK_SIZE, "Get the block size of inserted memory card", _get_blocksize },
+    {DISK_READ_SECTOR_CMD, "Reads the specified sector of inserted memory card", _read_sector},
+    {DISK_READ_BYTES_CMD, "Reads the specified bytes from inserted memory card", _read_bytes},
+    {DISK_GET_SECTOR_SIZE, "Get the sector size of inserted memory card", _get_sectorsize},
+    {DISK_GET_SECTOR_COUNT, "Get the sector count of inserted memory card", _get_sectorcount},
+    {DISK_GET_BLOCK_SIZE, "Get the block size of inserted memory card", _get_blocksize},
 #endif
 #ifdef MODULE_GNRC_ICMPV6_ECHO
 #ifdef MODULE_XTIMER
@@ -173,10 +173,10 @@ const shell_command_t _shell_command_list[] = {
     { "random_get", "returns 32 bit of pseudo randomness", _random_get },
 #endif
 #if FEATURE_PERIPH_RTC
-    { "rtc", "control RTC peripheral interface",  _rtc_handler },
+    {"rtc", "control RTC peripheral interface",  _rtc_handler},
 #endif
 #ifdef CPU_X86
-    { "lspci", "Lists PCI devices", _x86_lspci },
+    {"lspci", "Lists PCI devices", _x86_lspci},
 #endif
 #ifdef MODULE_GNRC_NETIF
     {"ifconfig", "Configure network interfaces", _netif_config},
@@ -185,28 +185,28 @@ const shell_command_t _shell_command_list[] = {
 #endif
 #endif
 #ifdef MODULE_FIB
-    { "fibroute", "Manipulate the FIB (info: 'fibroute [add|del]')", _fib_route_handler },
+    {"fibroute", "Manipulate the FIB (info: 'fibroute [add|del]')", _fib_route_handler},
 #endif
 #ifdef MODULE_GNRC_IPV6_NC
-    { "ncache", "manage neighbor cache by hand", _ipv6_nc_manage },
-    { "routers", "IPv6 default router list", _ipv6_nc_routers },
+    {"ncache", "manage neighbor cache by hand", _ipv6_nc_manage },
+    {"routers", "IPv6 default router list", _ipv6_nc_routers },
 #endif
 #ifdef MODULE_GNRC_IPV6_WHITELIST
-    { "whitelist", "whitelists an address for receival ('whitelist [add|del|help]')", _whitelist },
+    {"whitelist", "whitelists an address for receival ('whitelist [add|del|help]')", _whitelist },
 #endif
 #ifdef MODULE_GNRC_IPV6_BLACKLIST
-    { "blacklist", "blacklists an address for receival ('blacklist [add|del|help]')", _blacklist },
+    {"blacklist", "blacklists an address for receival ('blacklist [add|del|help]')", _blacklist },
 #endif
 #ifdef MODULE_GNRC_RPL
-    { "rpl", "rpl configuration tool ('rpl help' for more information)", _gnrc_rpl },
+    {"rpl", "rpl configuration tool ('rpl help' for more information)", _gnrc_rpl },
 #endif
 #ifdef MODULE_GNRC_SIXLOWPAN_CTX
 #ifdef MODULE_GNRC_SIXLOWPAN_ND_BORDER_ROUTER
-    { "6ctx", "6LoWPAN context configuration tool", _gnrc_6ctx },
+    {"6ctx", "6LoWPAN context configuration tool", _gnrc_6ctx },
 #endif
 #endif
 #ifdef MODULE_SAUL_REG
-    { "saul", "interact with sensors and actuators using SAUL", _saul },
+    {"saul", "interact with sensors and actuators using SAUL", _saul },
 #endif
 #ifdef MODULE_CCN_LITE_UTILS
     { "ccnl_open", "opens an interface or socket", _ccnl_open },
