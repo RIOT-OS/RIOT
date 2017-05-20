ac00b81e24847d618f5f24327670011e39c276b4
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-05-20 17:55:49 +0200

    boards/nucleo-l496zg: add initial support

--- boards/common/nucleo144/include/board.h
+++ boards/common/nucleo144/include/board.h
@@ -37,11 +37,18 @@ extern "C" {
  * @name    LED pin definitions and handlers
  * @{
  */
+#if defined(CPU_MODEL_STM32L496ZG)
+#define LED0_PORT           GPIOC
+#define LED0_PIN            GPIO_PIN(PORT_C, 7)
+#define LED0_MASK           (1 << 7)
+#else
+#define LED0_PORT           GPIOB
 #define LED0_PIN            GPIO_PIN(PORT_B, 0)
 #define LED0_MASK           (1 << 0)
-#define LED0_ON             (GPIOB->BSRR = LED0_MASK)
-#define LED0_OFF            (GPIOB->BSRR = (LED0_MASK << 16))
-#define LED0_TOGGLE         (GPIOB->ODR  ^= LED0_MASK)
+#endif
+#define LED0_ON             (LED0_PORT->BSRR = LED0_MASK)
+#define LED0_OFF            (LED0_PORT->BSRR = (LED0_MASK << 16))
+#define LED0_TOGGLE         (LED0_PORT->ODR  ^= LED0_MASK)
 
 #define LED1_PIN            GPIO_PIN(PORT_B, 7)
 #define LED1_MASK           (1 << 7)
