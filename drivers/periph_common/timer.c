a1499f419089d0f7ff7c556ffc20b7a1f57e7bdc
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-06-02 12:21:56 +0200

    cpu: add and use shared code for timer_set()

---
+++ drivers/periph_common/timer.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers
+ * @{
+ *
+ * @file
+ * @brief       Shared peripheral timer code
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "periph/timer.h"
+
+#ifndef PERIPH_TIMER_PROVIDES_SET
+int timer_set(tim_t dev, int channel, unsigned int timeout)
+{
+    return timer_set_absolute(dev, channel, timer_read(dev) + timeout);
+}
+#endif
