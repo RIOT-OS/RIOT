f8fa1969bf009ada0ef8283034d6afa321abfbfe
Kaspar Schleiser <kaspar@schleiser.de>
2017-06-15 15:41:41 +0200

    REWORD ME riotboot: introduce riot-based minimal bootloader
    TODO Rebase message edit at the end.
    
     * updated to edbg/openocd new variables
     * Use RIOTBOOT_FW_SLOT_SIZE: upcomming PRs should remove
       RIOTBOOT_SLOT1_SIZE and RIOTBOOT_SLOT2_SIZE
     * Use ROM_START_ADDR for 'firmware' to handle iotlab-m3
     * Use ROM_OFFSET and FW_ROM_LEN
     * Info on non supported existing bootloader for the moment in dist/riotboot
     * dist/riotboot: allow running different targets and not only flash
     * RIOTBOOT_HDR_LEN as hexadecimal and can be overwritten

---
+++ dist/riotboot/README.md
@@ -0,0 +1,6 @@
+# Overview
+
+This folder contains a simple bootloader that allows ping-pong style
+over-the-air updates.
+
+Please see examples/ota as example.
---
+++ makefiles/riotboot.mk
@@ -0,0 +1,89 @@
+ifneq (,$(filter riotboot,$(FEATURES_USED)))
+
+.PHONY: riotboot/flash riotboot/flash-bootloader riotboot/flash-slot1 riotboot/flash-slot2 \
+        riotboot/verify-image riotboot/bootloader/%
+
+RIOTBOOT ?= $(RIOTBASE)/tools/riotboot/bin/$(BOARD)/riotboot.elf
+CFLAGS += -I$(BINDIR)/riotbuild
+
+RIOTBOOT_SECKEY ?= $(RIOTBASE)/dist/tools/firmware/sec.key
+RIOTBOOT_PUBKEY ?= $(RIOTBASE)/dist/tools/firmware/pub.key
+
+FIRMWARE_TOOL ?= $(RIOTBASE)/dist/tools/firmware/bin/firmware
+RIOTBOOT_HDR_LEN ?= 0x100
+
+APP_ID  ?= 0
+APP_VER ?= 0
+
+$(BINDIR)/$(APPLICATION)-%.elf: link
+	$(Q)$(_LINK) -o $@
+
+# slot 1 targets
+SLOT1_OFFSET := $$(($(RIOTBOOT_SLOT0_SIZE) + $(RIOTBOOT_HDR_LEN)))
+SLOT2_OFFSET := $$(($(RIOTBOOT_SLOT0_SIZE) + $(RIOTBOOT_FW_SLOT_SIZE) + $(RIOTBOOT_HDR_LEN)))
+
+$(BINDIR)/$(APPLICATION)-slot%.elf: FW_ROM_LEN=$(RIOTBOOT_FW_SLOT_SIZE)
+$(BINDIR)/$(APPLICATION)-slot1.elf: ROM_OFFSET=$(SLOT1_OFFSET)
+$(BINDIR)/$(APPLICATION)-slot2.elf: ROM_OFFSET=$(SLOT2_OFFSET)
+
+# signing targets
+$(BINDIR)/$(APPLICATION)-%.bin:
+	@echo "creating $@..."
+	$(Q) $(OBJCOPY) -Obinary $< $@.tmp
+	$(Q) $(FIRMWARE_TOOL) sign $@.tmp $(APP_VER) $(APP_ID) $$(($(ROM_START_ADDR)+$(OFFSET))) $(RIOTBOOT_SECKEY) - > $@
+	$(Q) cat $@.tmp >> $@
+	$(Q) rm $@.tmp
+
+$(BINDIR)/$(APPLICATION)-slot1.bin: OFFSET=$(SLOT1_OFFSET)
+$(BINDIR)/$(APPLICATION)-slot1.bin: $(BINDIR)/$(APPLICATION)-slot1.elf
+
+$(BINDIR)/$(APPLICATION)-slot2.bin: OFFSET=$(SLOT2_OFFSET)
+$(BINDIR)/$(APPLICATION)-slot2.bin: $(BINDIR)/$(APPLICATION)-slot2.elf
+
+# creating pubkey header file
+$(RIOTBUILD_CONFIG_HEADER_C): $(BINDIR)/riotbuild/ota_pubkey.h
+$(BINDIR)/riotbuild/ota_pubkey.h: $(RIOTBOOT_PUBKEY)
+	@mkdir -p $(@D)
+	@{ \
+		echo "static const unsigned char ota_public_key[] = {"; \
+		cat $< | xxd -i ; \
+		echo "};"; \
+		} > $@
+
+riotboot: $(BINDIR)/$(APPLICATION)-slot1.bin $(BINDIR)/$(APPLICATION)-slot2.bin $(BINDIR)/riotbuild/ota_pubkey.h
+
+riotboot/verify-image:
+	$(FIRMWARE_TOOL) verify $(BINDIR)/$(APPLICATION)-slot1.bin $(RIOTBOOT_PUBKEY)
+	$(FIRMWARE_TOOL) verify $(BINDIR)/$(APPLICATION)-slot2.bin $(RIOTBOOT_PUBKEY)
+
+riotboot/flash-bootloader: riotboot/bootloader/flash
+riotboot/bootloader/%:
+	$(Q)/usr/bin/env -i \
+		QUIET=$(QUIET)\
+		PATH=$(PATH) BOARD=$(BOARD) \
+			make --no-print-directory -C $(RIOTBASE)/dist/riotboot $*
+
+riotboot/flash-slot1: IMAGE_OFFSET=$(RIOTBOOT_SLOT0_SIZE)
+# edbg
+riotboot/flash-slot1: HEXFILE=$(BINDIR)/$(APPLICATION)-slot1.bin
+# openocd
+riotboot/flash-slot1: IMAGE_FILE=$(BINDIR)/$(APPLICATION)-slot1.bin
+riotboot/flash-slot1: $(BINDIR)/$(APPLICATION)-slot1.bin riotboot/flash-bootloader
+	$(FLASHER) $(FFLAGS)
+
+riotboot/flash-slot2: IMAGE_OFFSET=$$(($(RIOTBOOT_SLOT0_SIZE) + $(RIOTBOOT_FW_SLOT_SIZE)))
+# edbg
+riotboot/flash-slot2: HEXFILE=$(BINDIR)/$(APPLICATION)-slot2.bin
+# openocd
+riotboot/flash-slot2: IMAGE_FILE=$(BINDIR)/$(APPLICATION)-slot2.bin
+riotboot/flash-slot2: $(BINDIR)/$(APPLICATION)-slot2.bin riotboot/flash-bootloader
+	$(FLASHER) $(FFLAGS)
+
+riotboot/flash: riotboot/flash-slot1
+
+else
+riotboot:
+	$(Q)echo "error: riotboot feature not selected! (try FEATURES_REQUIRED += riotboot)"
+	$(Q)false
+
+endif # (,$(filter riotboot,$(FEATURES_USED)))
