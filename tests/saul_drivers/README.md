571da02bae1581f27448844c2fc6f4d02eb94023
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-06-27 18:06:34 +0200

    tests: add test application for saul drivers adaption

---
+++ tests/saul_drivers/README.md
@@ -0,0 +1,6 @@
+Expected result
+===============
+This application is just a smoke test to verify the saul adaptions of drivers
+can be built without errors.
+
+**Don't flash it on hardware**
---
+++ tests/saul_drivers/main.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ *
+ * @file
+ * @brief       This application is not meant to be run: it's just a 
+ *              smoke test to verify the saul adaption of drivers can be built
+ *              without errors.
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ */
+
+int main(void)
+{
+    return 0;
+}
