72734fa939e8872b722b405e5f326d61ada2cbda
Thomas Perrot <thomas.perrot@tupi.fr>
2017-07-03 19:23:20 +0200

    cpu/atmega32u4: add support for ATmega32U4

--- cpu/atmega_common/periph/spi.c
+++ cpu/atmega_common/periph/spi.c
@@ -2,6 +2,7 @@
  * Copyright (C) 2015 Daniel Amkaer Sorensen
  *               2016 Freie Universität Berlin
  *               2017 Hamburg University of Applied Sciences
+ *               2017 Thomas Perrot <thomas.perrot@tupi.fr>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -19,6 +20,7 @@
  * @author      Daniel Amkaer Sorensen <daniel.amkaer@gmail.com>
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      Dimitri Nahm <dimitri.nahm@haw-hamburg.de>
+ * @author      Thomas Perrot <thomas.perrot@tupi.fr>
  *
  * @}
  */
@@ -70,6 +72,9 @@ void spi_init_pins(spi_t bus)
      * ATmega256RFR2 data sheet p. 365
      * */
     DDRB |= ((1 << DDB2) | (1 << DDB1));
+#endif
+#ifdef CPU_ATMEGA32U4
+    DDRB |= ((1 << DDB0) | (1 << DDB1) | (1 << DDB2));
 #endif
 }
 
--- cpu/atmega_common/thread_arch.c
+++ cpu/atmega_common/thread_arch.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 Freie Universität Berlin, Hinnerk van Bruinehsen
+ *               2017 Thomas Perrot <thomas.perrot@tupi.fr>
  *               2018 RWTH Aachen, Josua Arndt <jarndt@ias.rwth-aachen.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
@@ -15,6 +16,7 @@
  * @brief       Implementation of the kernel's architecture dependent thread interface
  *
  * @author      Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
+ * @author      Thomas Perrot <thomas.perrot@tupi.fr>
  * @author      Josua Arndt <jarndt@ias.rwth-aachen.de>
  *
  * @}
@@ -122,7 +124,7 @@ char *thread_stack_init(thread_task_func_t task_func, void *arg,
     stk--;
     *stk = (uint8_t)0x00;
 #endif
-#if defined(RAMPZ)
+#if defined(RAMPZ) && !defined(__AVR_ATmega32U4__)
     stk--;
     *stk = (uint8_t)0x00;
 #endif
@@ -262,7 +264,7 @@ __attribute__((always_inline)) static inline void __context_save(void)
         "in   __tmp_reg__, __SREG__          \n\t"
         "cli                                 \n\t"
         "push __tmp_reg__                    \n\t"
-#if defined(RAMPZ)
+#if defined(RAMPZ) && !defined(__AVR_ATmega32U4__)
         "in     __tmp_reg__, __RAMPZ__       \n\t"
         "push   __tmp_reg__                  \n\t"
 #endif
@@ -354,7 +356,8 @@ __attribute__((always_inline)) static inline void __context_restore(void)
         "pop    __tmp_reg__                  \n\t"
         "out    0x3c, __tmp_reg__            \n\t"
 #endif
-#if defined(RAMPZ)
+
+#if defined(RAMPZ) && !defined(__AVR_ATmega32U4__)
         "pop    __tmp_reg__                  \n\t"
         "out    __RAMPZ__, __tmp_reg__       \n\t"
 #endif
