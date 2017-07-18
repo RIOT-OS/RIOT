ef4455f8b63a21c4b48047d947b1d06f3af138db
Francois Berder <fberder@outlook.fr>
2017-07-18 23:19:49 +0100

    boards: pic32-clicker: Add SPI peripheral support
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- boards/pic32-clicker/include/periph_conf.h
+++ boards/pic32-clicker/include/periph_conf.h
@@ -1,6 +1,7 @@
 /*
  * Copyright(C) 2016,2017, Imagination Technologies Limited and/or its
  *                 affiliated group companies.
+ *              2017 Francois Berder <fberder@outlook.fr>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -18,12 +19,14 @@
  * @brief       peripheral configuration for the MikroE PIC32 Clicker
  *
  * @author      Neil Jones <Neil.Jones@imgtec.com>
+ * @author      Francois Berder <fberder@outlook.fr>
  */
 
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
 #include "periph_cpu.h"
+#include "vendor/p32mx470f512h.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -57,6 +60,48 @@ extern "C" {
 #define UART_NUMOF          (4)
 #define DEBUG_VIA_UART      (3)
 #define DEBUG_UART_BAUD     (9600)
+/** @} */
+
+
+/**
+ * @name    SPI device configuration
+ *
+ * @{
+ */
+
+static const spi_conf_t spi_config[] = {
+    {}, /* No SPI0 on PIC32 */
+
+    {   /*
+         * SPI 1 (Mikrobus)
+         *      MOSI -> RD4
+         *      MISO -> RD3
+         *      SCK  -> RD2
+         */
+        .mosi_pin = GPIO_PIN(PORT_D, 4),
+        .mosi_reg = (volatile uint32_t*)&RPD4R,
+        .mosi_af  = 0b1000,
+        .miso_pin = GPIO_PIN(PORT_D, 3),
+        .miso_reg = (volatile uint32_t*)&SDI1R,
+        .miso_af  = 0b0000
+    },
+
+    {   /*
+         * SPI 2 (6LoWPAN radio)
+         *      MOSI -> RG8
+         *      MISO -> RG7
+         *      SCK  -> RG6
+         */
+        .mosi_pin = GPIO_PIN(PORT_G, 8),
+        .mosi_reg = (volatile uint32_t*)&RPG8R,
+        .mosi_af  = 0b0110,
+        .miso_pin = GPIO_PIN(PORT_G, 7),
+        .miso_reg = (volatile uint32_t*)&SDI2R,
+        .miso_af  = 0b0001
+    }
+};
+
+#define SPI_NUMOF           (2)
 /** @} */
 
 #ifdef __cplusplus
