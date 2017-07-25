4083640219a35b37006986abb62a76d3b467a14f
Martine Lenders <m.lenders@fu-berlin.de>
2017-07-25 22:48:00 +0200

    auto_init: upgrade bootstrap of Ethernet devices for gnrc_netif2

--- sys/auto_init/netif/auto_init_ethos.c
+++ sys/auto_init/netif/auto_init_ethos.c
@@ -23,8 +23,12 @@
 #include "debug.h"
 #include "ethos.h"
 #include "periph/uart.h"
+#ifdef MODULE_GNRC_NETIF2
+#include "net/gnrc/netif2/ethernet.h"
+#else
 #include "net/gnrc/netdev.h"
 #include "net/gnrc/netdev/eth.h"
+#endif
 
 /**
  * @brief global ethos object, used by uart_stdio
@@ -37,14 +41,20 @@ ethos_t ethos;
  */
 #define ETHOS_MAC_STACKSIZE (THREAD_STACKSIZE_DEFAULT + DEBUG_EXTRA_STACKSIZE)
 #ifndef ETHOS_MAC_PRIO
+#ifdef MODULE_GNRC_NETIF2
+#define ETHOS_MAC_PRIO      (GNRC_NETIF2_PRIO)
+#else
 #define ETHOS_MAC_PRIO      (GNRC_NETDEV_MAC_PRIO)
+#endif
 #endif
 
 /**
  * @brief   Stacks for the MAC layer threads
  */
 static char _netdev_eth_stack[ETHOS_MAC_STACKSIZE];
+#ifndef MODULE_GNRC_NETIF2
 static gnrc_netdev_t _gnrc_ethos;
+#endif
 
 static uint8_t _inbuf[2048];
 
@@ -61,11 +71,16 @@ void auto_init_ethos(void)
     ethos_setup(&ethos, &p);
 
     /* initialize netdev<->gnrc adapter state */
+#ifdef MODULE_GNRC_NETIF2
+    gnrc_netif2_ethernet_create(_netdev_eth_stack, ETHOS_MAC_STACKSIZE,
+                                ETHOS_MAC_PRIO, "ethos", (netdev_t *)&ethos);
+#else
     gnrc_netdev_eth_init(&_gnrc_ethos, (netdev_t*)&ethos);
 
     /* start gnrc netdev thread */
     gnrc_netdev_init(_netdev_eth_stack, ETHOS_MAC_STACKSIZE, ETHOS_MAC_PRIO,
                      "gnrc_ethos", &_gnrc_ethos);
+#endif
 }
 
 #else
