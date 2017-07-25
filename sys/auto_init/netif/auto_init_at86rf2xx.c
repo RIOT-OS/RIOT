217913071bfd6f920f9f6457d02b1eeb6aeca48e
Martine Lenders <m.lenders@fu-berlin.de>
2017-07-25 20:28:13 +0200

    auto_init: upgrade bootstrap of IEEE 802.15.4 devices for gnrc_netif2

--- sys/auto_init/netif/auto_init_at86rf2xx.c
+++ sys/auto_init/netif/auto_init_at86rf2xx.c
@@ -21,8 +21,12 @@
 
 #include "log.h"
 #include "board.h"
+#ifdef MODULE_GNRC_NETIF2
+#include "net/gnrc/netif2/ieee802154.h"
+#else
 #include "net/gnrc/netdev.h"
 #include "net/gnrc/netdev/ieee802154.h"
+#endif
 #include "net/gnrc/lwmac/lwmac.h"
 #include "net/gnrc.h"
 
@@ -35,25 +39,35 @@
  */
 #define AT86RF2XX_MAC_STACKSIZE     (THREAD_STACKSIZE_DEFAULT)
 #ifndef AT86RF2XX_MAC_PRIO
+#ifdef MODULE_GNRC_NETIF2
+#define AT86RF2XX_MAC_PRIO          (GNRC_NETIF2_PRIO)
+#else
 #define AT86RF2XX_MAC_PRIO          (GNRC_NETDEV_MAC_PRIO)
+#endif
 #endif
 
 #define AT86RF2XX_NUM (sizeof(at86rf2xx_params) / sizeof(at86rf2xx_params[0]))
 
 static at86rf2xx_t at86rf2xx_devs[AT86RF2XX_NUM];
+#ifndef MODULE_GNRC_NETIF2
 static gnrc_netdev_t gnrc_adpt[AT86RF2XX_NUM];
+#endif
 static char _at86rf2xx_stacks[AT86RF2XX_NUM][AT86RF2XX_MAC_STACKSIZE];
 
 void auto_init_at86rf2xx(void)
 {
     for (unsigned i = 0; i < AT86RF2XX_NUM; i++) {
-        int res;
-
         LOG_DEBUG("[auto_init_netif] initializing at86rf2xx #%u\n", i);
 
         at86rf2xx_setup(&at86rf2xx_devs[i], &at86rf2xx_params[i]);
-        res = gnrc_netdev_ieee802154_init(&gnrc_adpt[i],
-                                          (netdev_ieee802154_t *)&at86rf2xx_devs[i]);
+#ifdef MODULE_GNRC_NETIF2
+        gnrc_netif2_ieee802154_create(_at86rf2xx_stacks[i],
+                                      AT86RF2XX_MAC_STACKSIZE,
+                                      AT86RF2XX_MAC_PRIO, "at86rf2xx",
+                                      (netdev_t *)&at86rf2xx_devs[i]);
+#else
+        int res = gnrc_netdev_ieee802154_init(&gnrc_adpt[i],
+                                              (netdev_ieee802154_t *)&at86rf2xx_devs[i]);
 
         if (res < 0) {
             LOG_ERROR("[auto_init_netif] error initializing at86rf2xx radio #%u\n", i);
@@ -73,6 +87,7 @@ void auto_init_at86rf2xx(void)
                              &gnrc_adpt[i]);
 #endif
         }
+#endif
     }
 }
 #else
--- sys/auto_init/netif/auto_init_cc2538_rf.c
+++ sys/auto_init/netif/auto_init_cc2538_rf.c
@@ -20,8 +20,12 @@
 #ifdef MODULE_CC2538_RF
 
 #include "log.h"
+#ifdef MODULE_GNRC_NETIF2
+#include "net/gnrc/netif2/ieee802154.h"
+#else
 #include "net/gnrc/netdev.h"
 #include "net/gnrc/netdev/ieee802154.h"
+#endif
 
 #include "cc2538_rf.h"
 
@@ -31,22 +35,34 @@
  */
 #define CC2538_MAC_STACKSIZE       (THREAD_STACKSIZE_DEFAULT)
 #ifndef CC2538_MAC_PRIO
+#ifdef MODULE_GNRC_NETIF2
+#define CC2538_MAC_PRIO            (GNRC_NETIF2_PRIO)
+#else
 #define CC2538_MAC_PRIO            (GNRC_NETDEV_MAC_PRIO)
+#endif
 #endif
 
 static cc2538_rf_t cc2538_rf_dev;
+#ifndef MODULE_GNRC_NETIF2
 static gnrc_netdev_t gnrc_adpt;
+#endif
 static char _cc2538_rf_stack[CC2538_MAC_STACKSIZE];
 
 void auto_init_cc2538_rf(void)
 {
-    int res;
-
     LOG_DEBUG("[auto_init_netif] initializing cc2538 radio\n");
 
     cc2538_setup(&cc2538_rf_dev);
-    res = gnrc_netdev_ieee802154_init(&gnrc_adpt,
-                                      (netdev_ieee802154_t *)&cc2538_rf_dev);
+#ifdef MODULE_GNRC_NETIF2
+    if (!gnrc_netif2_ieee802154_create(_cc2538_rf_stack,
+                                       CC2538_MAC_STACKSIZE,
+                                       CC2538_MAC_PRIO, "cc2538_rf",
+                                       (netdev_t *)&cc2538_rf_dev)) {
+        LOG_ERROR("[auto_init_netif] error initializing cc2538 radio\n");
+    }
+#else
+    int res = gnrc_netdev_ieee802154_init(&gnrc_adpt,
+                                          (netdev_ieee802154_t *)&cc2538_rf_dev);
 
     if (res < 0) {
         LOG_ERROR("[auto_init_netif] error initializing cc2538 radio\n");
@@ -58,6 +74,7 @@ void auto_init_cc2538_rf(void)
                          "cc2538_rf",
                          &gnrc_adpt);
     }
+#endif
 }
 
 #else
