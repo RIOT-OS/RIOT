0e6f0a803272ab8246524c06c3f91059b81f81bb
Koen Zandberg <koen@bergzand.net>
2017-08-03 00:27:39 +0200

    drivers/mrf24j40 add tx_retries_needed netopt

--- drivers/mrf24j40/include/mrf24j40_registers.h
+++ drivers/mrf24j40/include/mrf24j40_registers.h
@@ -268,6 +268,14 @@ extern "C" {
 /** @} */
 
 /**
+ * @brief   Shift offsets for TXSTAT register (0x24)
+ * @{
+ */
+#define MRF24J40_TXSTAT_MAX_FRAME_RETRIES_SHIFT 6
+#define MRF24J40_TXSTAT_CCAFAIL_SHIFT           5
+/** @} */
+
+/**
  * @brief   Bitfield definitions for the SOFTRST register (0x2A)
  * @{
  */
