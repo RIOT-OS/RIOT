2c4b94b2360f5627b4fe770816bc90e83154e5dd
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-08-18 00:32:16 +0200

    cpu/stm32l4: add support for stm32l433rc

--- cpu/stm32l4/include/cpu_conf.h
+++ cpu/stm32l4/include/cpu_conf.h
@@ -31,6 +31,8 @@
 #include "vendor/stm32l475xx.h"
 #elif defined(CPU_MODEL_STM32L432KC)
 #include "vendor/stm32l432xx.h"
+#elif defined(CPU_MODEL_STM32L433RC)
+#include "vendor/stm32l433xx.h"
 #elif defined(CPU_MODEL_STM32L452RE)
 #include "vendor/stm32l452xx.h"
 #endif
@@ -44,7 +46,7 @@ extern "C" {
  * @{
  */
 #define CPU_DEFAULT_IRQ_PRIO            (1U)
-#if defined(CPU_MODEL_STM32L432KC)
+#if defined(CPU_MODEL_STM32L432KC) || defined(CPU_MODEL_STM32L433RC)
 #define CPU_IRQ_NUMOF                   (83U)
 #else
 #define CPU_IRQ_NUMOF                   (82U)
