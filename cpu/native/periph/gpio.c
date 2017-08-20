d0f30280c88241be5d21d8824be7dd0177aa90cc
Koen Zandberg <koen@bergzand.net>
2017-08-20 10:59:24 +0200

    Add support for async_read interrupt handler

--- cpu/native/periph/gpio.c
+++ cpu/native/periph/gpio.c
@@ -15,26 +15,109 @@
  * @brief       empty GPIO implementation
  *
  * @author      Takuo Yonezawa <Yonezawa-T2@mail.dnp.co.jp>
+ * @author      Koen Zandberg <koen@bergzand.net>
  */
 
 #include "periph/gpio.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "async_read.h"
+
+#define NATIVE_GPIO_NUMOF   42
+
+char buf[128];
+
+
+static void _gpio_isr(int fd, void* arg);
+
+typedef struct {
+    int active;
+    gpio_t pin;
+    gpio_cb_t cb;
+    void* arg;
+} native_gpio_cb_t;
+
+native_gpio_cb_t gpio_list[NATIVE_GPIO_NUMOF];
 
 int gpio_init(gpio_t pin, gpio_mode_t mode) {
-  (void) pin;
-  (void) mode;
+    int fd, len;
+    fd = open(GPIO_SYSFS_DIR "/export", O_WRONLY);
+    if (fd < 0) {
+        return fd;
+    }
+    len = snprintf(buf, sizeof(buf), "%d", pin);
+    write(fd, buf, len);
+    close(fd);
 
-  return -1;
+    len = snprintf(buf, sizeof(buf), GPIO_SYSFS_DIR "/gpio%d/direction", pin);
+    fd = open(buf, O_WRONLY);
+    if (fd < 0 ) {
+        return fd;
+    }
+    int ret = 0;
+    switch(mode) {
+        case GPIO_IN:
+        case GPIO_IN_PD:
+        case GPIO_IN_PU:
+            write(fd, "in", 3);
+            break;
+        case GPIO_OUT:
+            write(fd, "out", 4);
+            break;
+        default:
+            ret = -1;
+    }
+    close(fd);
+    return ret;
 }
 
 int gpio_init_int(gpio_t pin, gpio_mode_t mode, gpio_flank_t flank,
                   gpio_cb_t cb, void *arg){
-  (void) pin;
-  (void) mode;
-  (void) flank;
-  (void) cb;
-  (void) arg;
 
-  return -1;
+    int fd;
+
+    native_gpio_cb_t *gpio = &(gpio_list[pin]);
+    gpio->active = 1;
+    gpio->pin = pin;
+    gpio->cb = cb;
+    gpio->arg = arg;
+
+    gpio_init(pin, mode);
+
+    snprintf(buf, sizeof(buf), GPIO_SYSFS_DIR "/gpio%d/edge", pin);
+    fd = open(buf, O_WRONLY);
+    if (fd < 0 ) {
+        return fd;
+    }
+    switch(flank) {
+        case GPIO_FALLING:
+            write(fd, "falling", 8);
+            break;
+        case GPIO_RISING:
+            write(fd, "rising", 7);
+            break;
+        case GPIO_BOTH:
+            write(fd, "both", 5);
+            break;
+        default:
+            break;
+    }
+    close(fd);
+
+    snprintf(buf, sizeof(buf), GPIO_SYSFS_DIR "/gpio%d/value", pin);
+
+    fd = open(buf, O_RDONLY);
+    if (fd < 0) {
+        return fd;
+    }
+
+    native_async_read_setup();
+    native_async_interrupt_add_handler(fd, gpio, _gpio_isr);
+    return -1;
 }
 
 void gpio_irq_enable(gpio_t pin) {
@@ -46,25 +129,67 @@ void gpio_irq_disable(gpio_t pin) {
 }
 
 int gpio_read(gpio_t pin) {
-  (void) pin;
+    int fd, val;
+    char c;
 
-  return 0;
+    snprintf(buf, sizeof(buf), GPIO_SYSFS_DIR "/gpio%d/value", pin);
+
+    fd = open(buf, O_RDONLY);
+    if (fd < 0) {
+        return fd;
+    }
+
+    read(fd, &c, 1);
+    if (c != '0') {
+        val = 1;
+    } else {
+        val = 0;
+    }
+    close(fd);
+
+    return val;
 }
 
 void gpio_set(gpio_t pin) {
-  (void) pin;
+    gpio_write(pin, 1);
 }
 
 void gpio_clear(gpio_t pin) {
-  (void) pin;
+    gpio_write(pin, 0);
 }
 
 void gpio_toggle(gpio_t pin) {
-  (void) pin;
+    if(gpio_read(pin) == 1) {
+        gpio_clear(pin);
+    } else {
+        gpio_set(pin);
+    }
 }
 
 void gpio_write(gpio_t pin, int value) {
-  (void) pin;
-  (void) value;
+    int fd;
+
+    snprintf(buf, sizeof(buf), GPIO_SYSFS_DIR "/gpio%d/value", pin);
+
+    fd = open(buf, O_WRONLY);
+    if (fd < 0) {
+        return;
+    }
+
+    if (value) {
+        write(fd, "1", 2);
+    } else {
+        write(fd, "0", 2);
+    }
+    close(fd);
+}
+
+static void _gpio_isr(int fd, void* arg)
+{
+    (void)arg;
+    lseek(fd, 0, SEEK_SET);  // Read from the start of the file
+    read(fd, buf, 128);
+    printf("\npoll() GPIO interrupt occurred\n");
+    native_async_read_continue(fd);
 }
 /** @} */
