f22172f2a8a9692704116d38e8bb76b597c180da
smlng <s@mlng.net>
2017-08-21 15:38:10 +0200

    boards: adapt gpio config for remote-pa

--- boards/remote-pa/include/board.h
+++ boards/remote-pa/include/board.h
@@ -70,11 +70,10 @@
  * 2.4GHz RF switch controlled by SW
  * @{
  */
-#define RF_SWITCH_PORT      GPIO_D
-#define RF_SWITCH_PIN       (4)
-#define RF_SWITCH_EXTERNAL  (RF_SWITCH_PORT->DATA |= (1 << RF_SWITCH_PIN))
-#define RF_SWITCH_INTERNAL  (RF_SWITCH_PORT->DATA &= ~(1 << RF_SWITCH_PIN))
-#define RF_SWITCH_TOGGLE    (RF_SWITCH_PORT->DATA ^= (1 << RF_SWITCH_PIN))
+ #define RF_SWITCH_GPIO      GPIO_PIN(3, 4)
+ #define RF_SWITCH_SUB_GHZ   gpio_set(RF_SWITCH_GPIO)
+ #define RF_SWITCH_2_4_GHZ   gpio_clear(RF_SWITCH_GPIO)
+ #define RF_SWITCH_TOGGLE    gpio_toggle(RF_SWITCH_GPIO)
 /** @} */
 
 /**
