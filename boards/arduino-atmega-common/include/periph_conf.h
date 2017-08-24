935372a3c3651c9f7df01821b76ba8f3925c9e4f
Andreas "Paul" Pauli <andreas.pauli@haw-hamburg.de>
2017-08-24 20:09:59 +0200

    periph_conf.h: add skeleton for concrete boards

---
+++ boards/arduino-atmega-common/include/periph_conf.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017   Freie Universität Berlin
+ * Copyright (C) 2017   Hamburg University of Applied Sciences
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_arduino-atmega-common
+ * @{
+ *
+ * @file
+ * @brief       Board specific skeleton for atmega based arduino boards
+ *
+ * @author      Martine Lenders <m.lenders@fu-berlin.de>
+ * @author      Andreas "Paul" Pauli <andreas.pauli@haw-hamburg.de>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include "periph_conf_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
