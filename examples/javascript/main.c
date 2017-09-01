4fa2be0fd64c1bff53c9a9b27aaa7caa2c0f1ba1
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-01 08:44:40 +0200

    examples/javascript: make use of js module

--- examples/javascript/main.c
+++ examples/javascript/main.c
@@ -23,53 +23,29 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "jerryscript.h"
-#include "jerryscript-ext/handler.h"
+#include "js.h"
 
 /* include header generated from main.js */
 #include "main.js.h"
 
-int js_run(const jerry_char_t *script, size_t script_size)
-{
-    jerry_value_t ret_value;
-
-    /* Initialize engine */
-    jerry_init(JERRY_INIT_EMPTY);
-
-    /* Register the print function in the global object. */
-    jerryx_handler_register_global((const jerry_char_t *) "print", jerryx_handler_print);
-
-    /* Setup Global scope code */
-    ret_value = jerry_parse(script, script_size, false);
-
-    if (!jerry_value_has_error_flag(ret_value)) {
-        /* Execute the parsed source code in the Global scope */
-        ret_value = jerry_run(ret_value);
-    }
-
-    int res = 0;
-
-    if (jerry_value_has_error_flag(ret_value)) {
-        printf("js_run(): Script Error!");
-        res = -1;
-    }
-
-    jerry_release_value(ret_value);
-
-    /* Cleanup engine */
-    jerry_cleanup();
-
-    return res;
-}
+static event_queue_t event_queue;
 
 int main(void)
 {
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
-    printf("Executing main.js:\n");
+    event_queue_init(&event_queue);
 
+    puts("Initializing jerryscript...");
+    js_event_queue = &event_queue;
+    js_init();
+
+    puts("Executing main.js...");
     js_run(main_js, main_js_len);
+
+    puts("Entering event loop...");
+    event_loop(&event_queue);
 
     return 0;
 }
--- examples/javascript/main.js
+++ examples/javascript/main.js
@@ -1,1 +1,16 @@
-print("Hello from JerryScript!");
+print("Hello from JerryScript!!!!");
+
+timer.setInterval = function (callback, interval) {
+    var next = timer.now() + interval;
+    var interval_handler = function () {
+        callback();
+        next += interval;
+        next_interval = next - timer.now();
+        timer.setCallback(interval_handler, next_interval);
+    }
+
+    return timer.setCallback(interval_handler, interval);
+}
+
+var n = 0;
+timer.setInterval(function () { n += 1; print(n, "triggers, time is", timer.now()); }, 1000000)
