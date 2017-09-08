d69291c3a1eb3f8a3a4762bf17ab9b213fa98372
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 01:10:59 +0200

    make: handle optional features in Makefile.dep

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -1,5 +1,3 @@
-USEMODULE := $(filter-out $(filter-out $(FEATURES_PROVIDED), $(FEATURES_OPTIONAL)), $(sort $(USEMODULE)))
-
 ED = $(addprefix FEATURE_,$(sort $(filter $(FEATURES_PROVIDED), $(FEATURES_REQUIRED))))
 ED += $(addprefix MODULE_,$(sort $(USEMODULE) $(USEPKG)))
 EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
