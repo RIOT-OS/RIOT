8965e9e26c3030ce6d425b6650d63a148a49e2a6
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 15:28:01 +0200

    make: make features available to Makefile.include

--- makefiles/features.inc.mk
+++
@@ -1,7 +0,0 @@
-# import list of provided features
-include $(RIOTBOARD)/$(BOARD)/Makefile.features
-
-DEFAULT_FEATURES += periph_pm
-
-# add available default features to required list
-FEATURES_REQUIRED += $(filter-out $(DISABLE_FEATURES), $(filter $(FEATURES_PROVIDED), $(DEFAULT_FEATURES)))
