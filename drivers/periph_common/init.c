3ebcdfd4001dfdefb58456fbd4e0c08f09ad4848
Kaspar Schleiser <kaspar@schleiser.de>
2017-09-09 09:47:44 +0200

    drivers/periph_common: guard spi_init() by spi module

--- drivers/periph_common/init.c
+++ drivers/periph_common/init.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 Freie Universität Berlin
+ *               2017 Kaspar Schleiser <kaspar@schleiser.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser General
  * Public License v2.1. See the file LICENSE in the top level directory for more
@@ -14,16 +15,19 @@
  * @brief       Common static peripheral driver initialization implementation
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
  *
  * @}
  */
 
+#ifdef MODULE_PERIPH_SPI
 #include "periph/spi.h"
+#endif
 
 void periph_init(void)
 {
     /* initialize configured SPI devices */
-#ifdef SPI_NUMOF
+#ifdef MODULE_PERIPH_SPI
     for (unsigned i = 0; i < SPI_NUMOF; i++) {
         spi_init(SPI_DEV(i));
     }
