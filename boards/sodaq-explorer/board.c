e5db719fad7f28c7dc10157cab66327f43cf3dec
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-09-22 19:09:34 +0200

    boards/sodaq-explorer: initial support

---
+++ boards/sodaq-explorer/board.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C)  2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_sodaq-explorer
+ *
+ * @file
+ * @brief       Board common implementations for the SODAQ ExpLoRer board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+    /* initialize the on-board LED */
+    gpio_init(LED0_PIN, GPIO_OUT);
+}
---
+++ boards/sodaq-explorer/include/board.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_sodaq-explorer
+ * @brief       Board specific definitions for the SODAQ ExpLoRer board
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the SODAQ ExpLoRer board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+#include "periph_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name    xtimer configuration
+ * @{
+ */
+#define XTIMER_WIDTH        (16)
+/** @} */
+
+/**
+ * @name    LED pin definitions and handlers
+ * @{
+ */
+#define LED0_PIN            GPIO_PIN(PA, 21)
+
+#define LED0_PORT           PORT->Group[PA]
+#define LED0_MASK           (1 << 21)
+
+#define LED0_ON             (LED0_PORT.OUTSET.reg = LED0_MASK)
+#define LED0_OFF            (LED0_PORT.OUTCLR.reg = LED0_MASK)
+#define LED0_TOGGLE         (LED0_PORT.OUTTGL.reg = LED0_MASK)
+/** @} */
+
+/**
+ * @name    User button
+ */
+#define BTN0_PIN            GPIO_PIN(PA, 14)
+#define BTN0_MODE           GPIO_IN_PU
+/** @} */
+
+/**
+ * @brief Initialize board specific hardware, including clock, LEDs and std-IO
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
