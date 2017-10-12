544aeb1bbd0a7b968cbecc2b270a9a5089413c78
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2017-10-12 15:05:58 +0200

    makefiles/applications.inc.mk: build 'boards' directory

--- makefiles/application.inc.mk
+++ makefiles/application.inc.mk
@@ -1,6 +1,6 @@
 MODULE = $(APPLICATION)
 
-DIRS += $(RIOTCPU)/$(CPU) $(RIOTBOARD)/$(BOARD)
+DIRS += $(RIOTCPU)/$(CPU) $(RIOTBOARD)
 DIRS += $(RIOTBASE)/core $(RIOTBASE)/drivers $(RIOTBASE)/sys
 
 include $(RIOTBASE)/Makefile.base
