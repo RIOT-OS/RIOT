9a1fcd805fa8f63a3cced21904c508fb7fabdd8d
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-10-18 13:24:58 +0200

    tests/cpp11_confition_var: added pexpect script

---
+++ tests/cpp11_condition_variable/tests/01-run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
+# Copyright (C) 2016 Takuo Yonezawa <Yonezawa-T2@mail.dnp.co.jp>
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
+import testrunner
+
+def testfunc(child):
+
+    child.expect_exact("************ C++ condition_variable test ***********")
+    child.expect_exact("Wait with predicate and notify one ...")
+    child.expect_exact("Done")
+    child.expect_exact("Wait and notify all ...")
+    child.expect_exact("Done")
+    child.expect_exact("Wait for ...")
+    child.expect_exact("Done")
+    child.expect_exact("Wait until ...")
+    child.expect_exact("Done")
+    child.expect_exact("Bye, bye.")
+    child.expect_exact("******************************************************")
+
+if __name__ == "__main__":
+    sys.exit(testrunner.run(testfunc))
