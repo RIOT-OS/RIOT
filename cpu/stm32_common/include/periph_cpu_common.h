4c5937e674763818dd2a37b66e354f7bb18ac969
Vincent Dupont <vincent@otakeys.com>
2017-10-23 11:36:18 +0200

    cpu/stm32_common: add PM modes

--- cpu/stm32_common/include/periph_cpu_common.h
+++ cpu/stm32_common/include/periph_cpu_common.h
@@ -80,6 +80,14 @@ extern "C" {
 #if defined(CPU_FAM_STM32F1) || defined(CPU_FAM_STM32F2) \
     || defined(CPU_FAM_STM32F4) || defined(DOXYGEN)
 #define PM_NUM_MODES    (2U)
+
+/**
+ * @name    Power modes
+ * @{
+ */
+#define STM32_PM_STOP         (1U)
+#define STM32_PM_STANDBY      (0U)
+/** @} */
 #endif
 
 /**
