857b44a9751c51cc23354596eda3abf01f7f8bff
Vincent Dupont <vincent@otakeys.com>
2017-10-23 11:37:19 +0200

    cpu/stm32f4: add pm support in i2c driver

--- cpu/stm32f4/periph/i2c.c
+++ cpu/stm32f4/periph/i2c.c
@@ -32,6 +32,7 @@
 #include "mutex.h"
 #include "periph_conf.h"
 #include "periph/i2c.h"
+#include "pm_layered.h"
 
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
@@ -227,6 +228,8 @@ int i2c_acquire(i2c_t dev)
         return -1;
     }
     mutex_lock(&locks[dev]);
+    /* block STOP mode */
+    pm_block(STM32_PM_STOP);
     return 0;
 }
 
@@ -235,6 +238,8 @@ int i2c_release(i2c_t dev)
     if (dev >= I2C_NUMOF) {
         return -1;
     }
+    /* unblock STOP mode */
+    pm_unblock(STM32_PM_STOP);
     mutex_unlock(&locks[dev]);
     return 0;
 }
