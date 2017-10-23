a12215fe9fc7cb94ab3e5c31433c37bde6d8cebd
Martine Lenders <m.lenders@fu-berlin.de>
2017-10-23 14:42:27 +0200

    tests: ds1307: adapt test result for initialization error

--- tests/driver_ds1307/tests/01-run.py
+++ tests/driver_ds1307/tests/01-run.py
@@ -13,7 +13,8 @@ sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
 import testrunner
 
 def testfunc(child):
-    child.expect(u"OK \\([0-9]+ tests\\)")
+    child.expect([r"OK \([0-9]+ tests\)",
+                  r"error: unable to initialize RTC \[I2C initialization error\]"])
 
 if __name__ == "__main__":
     sys.exit(testrunner.run(testfunc))
