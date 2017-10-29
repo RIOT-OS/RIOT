7ff3a2a535579f6ea0ef2e770b8898fde3dda2c0
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-10-29 11:28:25 +0100

    tests/pthread_cleanup: migrate to testrunner script

---
+++ tests/pthread_cleanup/tests/01-run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
+import testrunner
+
+
+def testfunc(child):
+    child.expect('START')
+    for i in range(5):
+        child.expect_exact('<SCOPE {}{}>'
+                           .format(i + 1, ' /' if i == 4 else ''))
+
+    child.expect_exact('Cleanup: <5>')
+    child.expect_exact('</SCOPE 4>')
+    child.expect_exact('</SCOPE 3>')
+    for i in (3, 2, 1):
+        child.expect_exact('Cleanup: <{}>'.format(i))
+    child.expect_exact('Result: 1234')
+    child.expect('SUCCESS')
+
+
+if __name__ == "__main__":
+    sys.exit(testrunner.run(testfunc))
