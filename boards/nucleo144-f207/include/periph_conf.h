0f1e7d12b72da21cde5ed4e06ca9c0c09281d444
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-11-08 17:35:45 +0100

    fixup! nucleo144-f207: fixed ADC config again

--- boards/nucleo144-f207/include/periph_conf.h
+++ boards/nucleo144-f207/include/periph_conf.h
@@ -264,9 +264,9 @@ static const adc_conf_t adc_config[] = {
     { .pin = GPIO_PIN(PORT_A,  3), .dev = ADC_1, .chan =  3 },
     { .pin = GPIO_PIN(PORT_C,  0), .dev = ADC_1, .chan = 10 },
     { .pin = GPIO_PIN(PORT_C,  3), .dev = ADC_1, .chan = 13 },
-    { .pin = GPIO_PIN(PORT_F,  3), .dev = ADC_1, .chan =  9 },
-    { .pin = GPIO_PIN(PORT_F,  5), .dev = ADC_1, .chan = 15 },
-    { .pin = GPIO_PIN(PORT_F, 10), .dev = ADC_1, .chan =  8 }
+    { .pin = GPIO_PIN(PORT_F,  3), .dev = ADC_3, .chan =  9 },
+    { .pin = GPIO_PIN(PORT_F,  5), .dev = ADC_3, .chan = 15 },
+    { .pin = GPIO_PIN(PORT_F, 10), .dev = ADC_3, .chan =  8 }
 };
 
 #define ADC_NUMOF           (sizeof(adc_config) / sizeof(adc_config[0]))
