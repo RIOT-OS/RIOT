b64b8b04d00446613f6562f723df2cc9a0e41305
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-11-09 10:16:43 +0100

    boards: unified wsn430 implementation

--- boards/wsn430/board_init.c
+++ boards/wsn430/board_init.c
@@ -1,10 +1,21 @@
 /*
- * board_init.c - Implementation of functions to init board.
  * Copyright (C) 2013 Milan Babel <babel@inf.fu-berlin.de>
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_wsn430
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the WSN430 type boards
+ *
+ * @author      Milan Babel <babel@inf.fu-berlin.de>
+ *
+ * @}
  */
 
 #include "cpu.h"
