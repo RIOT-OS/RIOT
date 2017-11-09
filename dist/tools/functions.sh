3a1571154658077651a7eb457ffa468ab9c1764b
Kaspar Schleiser <kaspar@schleiser.de>
2017-11-09 22:07:11 +0100

    fixup! fixup! dist: tools: intruduce functions.sh (generic shell helper functions)

--- dist/tools/functions.sh
+++ dist/tools/functions.sh
@@ -3,14 +3,16 @@
 quote()
 {
     while [ $# -ne 0 ]; do
-        printf '%q\n' "$1"
+        printf "'"
+        printf '%s' "$1" | sed "s/'/'\\\\''/g"
+        printf "'\n"
         shift
     done
 }
 
 # run arguments as command, only print output when command errors
 quiet() {
-    OUT="$($(quote "$@") 2>&1)"
+    OUT="$(eval $(quote "$@") 2>&1)"
     RES=$?
     [ -n "$OUT" ] && [ $RES -ne 0 -o "$QUIET" = "0" ] && echo "$OUT"
     return $RES
