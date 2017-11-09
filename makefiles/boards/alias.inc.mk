5903a0cd068c84b0468d8391d295c427ece48205
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-11-09 14:27:53 +0100

    fixup! now working with org and alias names

--- makefiles/boards/alias.inc.mk
+++ makefiles/boards/alias.inc.mk
@@ -1,9 +1,10 @@
 # list of known alias
 ALIAS.phynode   = pba-d-01-kw2x
+#TODO remove this line (as its just for show)
 ALIAS.linux     = native
 
 # this temporary variable is needed as we can not use BOARD when assigning BOARD
 ALIAS_TARGET := $(ALIAS.$(BOARD))
-ifneq (,($(ALIAS_TARGET)))
+ifdef ALIAS_TARGET
   override BOARD = $(ALIAS_TARGET)
 endif
