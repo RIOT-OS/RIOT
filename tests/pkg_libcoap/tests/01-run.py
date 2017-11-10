c3c0039f24c786a43e19103ab897f9cb85c18245
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-10 09:30:10 +0100

    tests/pkg_libcoap: migrate to testrunner

---
+++ tests/pkg_libcoap/tests/01-run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import math
+
+sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
+import testrunner
+
+
+def testfunc(child):
+    child.expect_exact('SUCCESS: Libcoap compiled!')
+
+if __name__ == "__main__":
+    sys.exit(testrunner.run(testfunc))
