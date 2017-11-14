aa7016f49740f5958286c680e9ae109ece63d3cd
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-14 16:36:45 +0100

    tests/libfixmath/tests: fix python pep8 issues in testrunner script

--- tests/libfixmath/do-test.py
+++ tests/libfixmath/do-test.py
@@ -47,6 +47,7 @@ FUNS = {
 
 ABS_ERROR_LIMIT = 0.011
 
+
 def main():
     total = 0
     errors = 0
@@ -71,14 +72,19 @@ def main():
         total += 1
         try:
             res_locals = {}
-            res_locals['input'], res_locals['expected'] = map(str.strip, line.split('='))
+            res_locals['input'], res_locals['expected'] = map(str.strip,
+                                                              line.split('='))
             exec('result = {}'.format(res_locals['input']), FUNS, res_locals)
 
-            abs_error = abs(res_locals['result'] - float(res_locals['expected']))
+            abs_error = abs(res_locals['result'] -
+                            float(res_locals['expected']))
             res_locals['result'] = '{:.4f}'.format(res_locals['result'])
             if abs_error > ABS_ERROR_LIMIT:
-                print('{}: {} != {}, {:.4f} > {}'.format(res_locals['input'], res_locals['result'], res_locals['expected'],
-                                                         abs_error, ABS_ERROR_LIMIT))
+                print('{}: {} != {}, {:.4f} > {}'
+                      .format(res_locals['input'],
+                              res_locals['result'],
+                              res_locals['expected'],
+                              abs_error, ABS_ERROR_LIMIT))
                 errors += 1
         except:
             errors += 1
