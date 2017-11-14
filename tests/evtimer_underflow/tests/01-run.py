60b2aec0901a48cc09d847e205b8f86ae7c9863b
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-14 16:34:34 +0100

    tests/evtimer_*/tests: fix python pep8 issues in testrunner script

--- tests/evtimer_underflow/tests/01-run.py
+++ tests/evtimer_underflow/tests/01-run.py
@@ -16,13 +16,15 @@ import testrunner
 
 how_many = 100
 
+
 def testfunc(child):
     for i in range(how_many):
         for j in range(8):
-            child.expect(r'received msg "%i"' % (j + 1))
+            child.expect(r'received msg "{}"'.format((j + 1)))
         print(".", end="", flush=True)
     print("")
-    print("Stopped after %i iterations, but should run forever." % how_many)
+    print("Stopped after {} iterations, but should run forever."
+          .format(how_many))
     print("=> All tests successful")
 
 if __name__ == "__main__":
