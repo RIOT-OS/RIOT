2aaadb24980bb6f2cc552b88078822e28c8b1339
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-14 16:35:20 +0100

    tests/posix_*/tests: fix python pep8 issues in testrunner script

--- tests/posix_time/tests/01-run.py
+++ tests/posix_time/tests/01-run.py
@@ -18,8 +18,10 @@ import testrunner
 US_PER_SEC = 1000000
 EXTERNAL_JITTER = 0.15
 
+
 class InvalidTimeout(Exception):
     pass
+
 
 def testfunc(child):
     try:
@@ -35,14 +37,15 @@ def testfunc(child):
         child.expect_exact("DONE")
         testtime = (time.time() - start_test) * US_PER_SEC
         exp = sum(i * 500000 for i in range(5)) + \
-              sum(i * US_PER_SEC for i in range(5))
+            sum(i * US_PER_SEC for i in range(5))
         lower_bound = exp - (exp * EXTERNAL_JITTER)
         upper_bound = exp + (exp * EXTERNAL_JITTER)
         if not (lower_bound < testtime < upper_bound):
-            raise InvalidTimeout("Host timer measured %d us (client measured %d us)" % \
-                                 (testtime, exp));
-    except InvalidTimeout as e:
-        print(e)
+            raise InvalidTimeout("Host timer measured {} us "
+                                 "(client measured {} us)"
+                                 .format(testtime, exp))
+    except InvalidTimeout as exc:
+        print(exc)
         sys.exit(1)
 
 if __name__ == "__main__":
