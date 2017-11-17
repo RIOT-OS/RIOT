71d1c4257bce91023633e7466d5dbb7d50cdc0ec
Kaspar Schleiser <kaspar@schleiser.de>
2017-11-17 15:46:32 +0100

    dist/tools/boards: add board grouping test script, enable for static CI tests

---
+++ dist/tools/boards/check.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 Kaspar Schleiser <kaspar@schleiser.de>
+#
+# This script checks whether the board groups defined in boards/groups.mk
+# contain and only contai boards that actually exist in the tree.
+
+_greplist() {
+    if [ $# -eq 0 ]; then
+        echo cat
+    else
+        echo -n "grep -vE ($1"
+        shift
+        for i in $*; do
+            echo -n "|$i"
+        done
+        echo ")"
+    fi
+}
+
+: ${RIOTBASE:=$(pwd)}
+
+export RIOTBASE
+
+BOARD_LIST="$(make --no-print-directory -C${RIOTBASE}/dist/tools/boards list-boards)"
+BOARD_FOLDERS="$(find ${RIOTBASE}/boards/* -maxdepth 0 -type d \! -name "*-common" -exec basename {} \;)"
+
+if [ "$(echo ${BOARD_LIST} | sort)" != "$(echo ${BOARD_FOLDERS} | sort)" ]; then
+    echo "$0: board list mismatch!"
+
+    LIST_MISSING="$(echo "$BOARD_FOLDERS" | $(_greplist $BOARD_LIST))"
+    FOLDER_MISSING="$(echo "$BOARD_LIST" | $(_greplist $BOARD_FOLDERS))"
+
+    [ -n "$LIST_MISSING" ] && {
+        echo "Boards missing in boards/group.mk: $LIST_MISSING"
+    }
+
+    [ -n "$FOLDER_MISSING" ] && {
+        echo "Boards listed in boards/group.mk without board folder: $FOLDER_MISSING"
+    }
+
+    exit 1
+fi
+true
