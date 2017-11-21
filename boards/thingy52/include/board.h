12d01f97fe586c44f0ee121d376b29949462229e
Hauke Petersen <hauke.petersen@fu-berlin.de>
2017-11-21 17:17:29 +0100

    boards: adds support for Nordic Thingy:52

---
+++ boards/thingy52/include/board.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2017 Feie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    boards_thingy52 Nordic Thingy:52
+ * @ingroup     boards
+ * @brief       Board specific configuration for the Nordic Thingy:52 board
+ * @{
+ *
+ * @file
+ * @brief       Board specific configuration for the Nordic Thingy:52 board
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name    Button pin configuration
+ * @{
+ */
+#define BTN0_PIN            GPIO_PIN(0, 11)
+#define BTN0_MODE           GPIO_IN_PU
+/** @} */
+
+/**
+ * @brief   Initialize board specific hardware
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
