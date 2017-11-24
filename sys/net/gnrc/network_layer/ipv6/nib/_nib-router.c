2f8223106b8f67051ae1560cca025c185d38f5ce
Martine Lenders <m.lenders@fu-berlin.de>
2017-11-24 11:46:56 +0100

    gnrc_ipv6_nib: fix ms to sec conversion for sending NDP PIOs

--- sys/net/gnrc/network_layer/ipv6/nib/_nib-router.c
+++ sys/net/gnrc/network_layer/ipv6/nib/_nib-router.c
@@ -100,9 +100,9 @@ static gnrc_pktsnip_t *_offl_to_pio(_nib_offl_entry_t *offl,
     gnrc_pktsnip_t *pio;
     uint8_t flags = 0;
     uint32_t valid_ltime = (offl->valid_until == UINT32_MAX) ? UINT32_MAX :
-                           (offl->valid_until - now);
+                           ((offl->valid_until - now) / MS_PER_SEC);
     uint32_t pref_ltime = (offl->pref_until == UINT32_MAX) ? UINT32_MAX :
-                          (offl->pref_until - now);
+                          ((offl->pref_until - now) / MS_PER_SEC);
 
     DEBUG("nib: Build PIO for %s/%u\n",
           ipv6_addr_to_str(addr_str, &offl->pfx, sizeof(addr_str)),
