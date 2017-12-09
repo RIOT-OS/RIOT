170c7e8e4d7a49439aecab07a440327a576484e8
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-12-09 17:27:26 +0100

    drivers/sx127x: use NETOPT_DEVICE_TYPE for LoRa modem

--- drivers/sx127x/sx127x_netdev.c
+++ drivers/sx127x/sx127x_netdev.c
@@ -292,10 +292,10 @@ static int _get(netdev_t *netdev, netopt_t opt, void *val, size_t max_len)
             assert(max_len >= sizeof(netopt_state_t));
             return _get_state(dev, val);
 
-        case NETOPT_DEVICE_MODE:
-            assert(max_len >= sizeof(uint8_t));
-            *((uint8_t*) val) = dev->settings.modem;
-            return sizeof(uint8_t);
+        case NETOPT_DEVICE_TYPE:
+            assert(max_len >= sizeof(uint16_t));
+            *((uint16_t*) val) = NETDEV_TYPE_LORA;
+            return sizeof(uint16_t);
 
         case NETOPT_CHANNEL:
             assert(max_len >= sizeof(uint32_t));
@@ -375,10 +375,16 @@ static int _set(netdev_t *netdev, netopt_t opt, const void *val, size_t len)
             assert(len <= sizeof(netopt_state_t));
             return _set_state(dev, *((const netopt_state_t*) val));
 
-        case NETOPT_DEVICE_MODE:
-            assert(len <= sizeof(uint8_t));
-            sx127x_set_modem(dev, *((const uint8_t*) val));
-            return sizeof(netopt_enable_t);
+        case NETOPT_DEVICE_TYPE:
+            assert(len <= sizeof(uint16_t));
+            /* Only LoRa modem is supported for the moment */
+            if (*(const uint16_t*) val == NETDEV_TYPE_LORA) {
+                sx127x_set_modem(dev, SX127X_MODEM_LORA);
+                return sizeof(uint16_t);
+            }
+            else {
+                return -EINVAL;
+            }
 
         case NETOPT_CHANNEL:
             assert(len <= sizeof(uint32_t));
