02696a93e740ad0c14af1a7afb462575b538116e
Vincent Dupont <vincent@otakeys.com>
2018-01-03 14:48:30 +0100

    tests: add a dummy app to build DMA

---
+++ tests/periph_dma/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 OTA keys S.A.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief       Dummy test application for DMA peripheral
+ *
+ * @author      Vincent Dupont <vincent@otakeys.com>
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("DMA dummy test app");
+
+    return 0;
+}
