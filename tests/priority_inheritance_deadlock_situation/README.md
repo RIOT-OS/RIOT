4a0e9845c199c7c0232b45ab391c02b6b3ed439d
mmkonrad <Marcus.Konrad@student.hpi.de>
2018-01-06 05:53:56 -0800

    added little demo to cause deadlocks while using priority inheritance

---
+++ tests/priority_inheritance_deadlock_situation/README.md
@@ -0,0 +1,3 @@
+This constructed example causes the program to get stuck in a deadlock while using the by Hauke Petersen proposed priority inheritance module.
+
+THe sole purpose of this example is to raise awareness for the possible necessity of implementing a priority ceiling module.
---
+++ tests/priority_inheritance_deadlock_situation/main.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "thread.h"
+#include "mutex.h"
+#include "xtimer.h"
+
+mutex_t res_mtx_A;
+mutex_t res_mtx_B;
+
+
+char stack_prio_1[THREAD_STACKSIZE_MAIN];
+char stack_prio_2[THREAD_STACKSIZE_MAIN];
+char stack_prio_3[THREAD_STACKSIZE_MAIN];
+
+
+void *t_prio_1_handler(void *arg)
+{
+  (void) arg;
+  /* starting working loop immediately */
+  while(1){
+    printf("prio_1: L(A)\n");
+    mutex_lock(&res_mtx_A);
+    xtimer_sleep(2);
+
+    printf("prio_1: L(B)\n");
+    mutex_lock(&res_mtx_B);
+    //xtimer_sleep(1);
+
+    printf("prio_1: U(B)\n");
+    mutex_unlock(&res_mtx_B);
+    //xtimer_sleep(1);
+
+    printf("prio_1: U(A)\n");
+    mutex_unlock(&res_mtx_A);
+    //xtimer_sleep(1);
+  }
+  return NULL;
+}
+
+
+void *t_prio_2_handler(void *arg)
+{
+  (void) arg;
+  xtimer_sleep(1);
+  /* starting working loop immediately */
+  while(1){
+    printf("prio_2: L(B)\n");
+    mutex_lock(&res_mtx_B);
+    xtimer_sleep(2);
+
+    printf("prio_2: L(A)\n");
+    mutex_lock(&res_mtx_A);
+    //xtimer_sleep(1);
+
+    printf("prio_2: U(A)\n");
+    mutex_unlock(&res_mtx_A);
+    //xtimer_sleep(1);
+
+    printf("prio_2: U(B)\n");
+    mutex_unlock(&res_mtx_B);
+    //xtimer_sleep(1);
+  }
+  return NULL;
+}
+
+
+
+void *t_prio_3_handler(void *arg)
+{
+  (void) arg;
+  xtimer_sleep(2);
+  /* starting working loop immediately */
+  while(1){
+    printf("prio_3: L(A)\n");
+    mutex_lock(&res_mtx_A);
+    //xtimer_sleep(1);
+
+    printf("prio_3: U(A)\n");
+    mutex_unlock(&res_mtx_A);
+    //xtimer_sleep(1);
+  }
+  return NULL;
+}
+
+
+
+
+
+
+kernel_pid_t pid_prio_1;
+kernel_pid_t pid_prio_2;
+kernel_pid_t pid_prio_3;
+
+int main(void)
+{
+  xtimer_init();
+  mutex_init(&res_mtx_A);
+  mutex_init(&res_mtx_B);
+
+
+  pid_prio_1 = thread_create(stack_prio_1, sizeof(stack_prio_1),
+      THREAD_PRIORITY_MAIN - 1,
+      THREAD_CREATE_STACKTEST,
+      t_prio_1_handler, NULL,
+      "t_low");
+
+  pid_prio_2 = thread_create(stack_prio_2, sizeof(stack_prio_2),
+      THREAD_PRIORITY_MAIN - 2,
+      THREAD_CREATE_STACKTEST,
+      t_prio_2_handler, NULL,
+      "t_low");
+
+  pid_prio_3 = thread_create(stack_prio_3, sizeof(stack_prio_3),
+      THREAD_PRIORITY_MAIN - 3,
+      THREAD_CREATE_STACKTEST,
+      t_prio_3_handler, NULL,
+      "t_low");
+
+  thread_sleep();
+  return 0;
+}
