ec9aee918f1d83d06d07cc42d56d766fbd74e240
Vincent Dupont <vincent@otakeys.com>
2018-01-23 12:29:32 +0100

    pkg/spiffs: update mtd usage

--- pkg/spiffs/fs/spiffs_fs.c
+++ pkg/spiffs/fs/spiffs_fs.c
@@ -134,19 +134,19 @@ static int prepare(spiffs_desc_t *fs_desc)
     fs_desc->config.hal_erase_f = _dev_erase;
 
 #if SPIFFS_SINGLETON == 0
-    DEBUG("spiffs: mount: mtd page_size=%" PRIu32 ", pages_per_sector=%" PRIu32
-          ", sector_count=%" PRIu32 "\n", dev->page_size, dev->pages_per_sector, dev->sector_count);
+    DEBUG("spiffs: mount: mtd page_size=%" PRIu32 ", sector_size=%" PRIu32
+          ", sector_count=%" PRIu32 "\n", dev->page_size, dev->sector_size, dev->sector_count);
     uint32_t sector_count = (fs_desc->block_count == 0) ? dev->sector_count : fs_desc->block_count;
     /* inside memory area */
-    assert(((fs_desc->base_addr / (dev->page_size * dev->pages_per_sector)) + sector_count)
+    assert(((fs_desc->base_addr / dev->min_erase_size) + sector_count)
            <= dev->sector_count);
     /* base addr is aligned on a sector */
-    assert(fs_desc->base_addr % (dev->pages_per_sector * dev->page_size) == 0);
-    fs_desc->config.phys_size = dev->page_size * dev->pages_per_sector * sector_count;
-    fs_desc->config.log_block_size = dev->page_size * dev->pages_per_sector;
+    assert(fs_desc->base_addr % dev->min_erase_size == 0);
+    fs_desc->config.phys_size = dev->min_erase_size * sector_count;
+    fs_desc->config.log_block_size = dev->min_erase_size;
     fs_desc->config.log_page_size = dev->page_size;
     fs_desc->config.phys_addr = fs_desc->base_addr;
-    fs_desc->config.phys_erase_block = dev->page_size * dev->pages_per_sector;
+    fs_desc->config.phys_erase_block = dev->min_erase_size;
 #endif
 
     return mtd_init(dev);
