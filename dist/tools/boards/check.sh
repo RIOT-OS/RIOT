edf5526b3dc6363cba9b8e25105d827738bf8411
Kaspar Schleiser <kaspar@schleiser.de>
2018-01-25 14:14:43 +0100

    fixup! fixup! squash fix dist/tools/boards/check.sh

--- dist/tools/boards/check.sh
+++ dist/tools/boards/check.sh
@@ -10,7 +10,7 @@
 export RIOTBASE
 
 BOARD_LIST="$(make --no-print-directory -C${RIOTBASE}/dist/tools/boards list-boards)"
-BOARD_FOLDERS="$(find ${RIOTBASE}/boards/* -maxdepth 0 -type d \! -name "*-common" -exec basename {} \;)"
+BOARD_FOLDERS="$(find ${RIOTBASE}/boards/* -maxdepth 0 -type d \! -name "common" -exec basename {} \;)"
 
 if [ "$(echo ${BOARD_LIST} | sort)" != "$(echo ${BOARD_FOLDERS} | sort)" ]; then
     echo "$0: board list mismatch!"
@@ -19,11 +19,13 @@ if [ "$(echo ${BOARD_LIST} | sort)" != "$(echo ${BOARD_FOLDERS} | sort)" ]; then
     FOLDER_MISSING="$(echo "$BOARD_LIST" | grep -vwF "${BOARD_FOLDERS}")"
 
     [ -n "$LIST_MISSING" ] && {
-        echo "Boards missing in boards/group.mk: $LIST_MISSING"
+        echo "Boards missing in boards/group.mk:"
+        echo "$LIST_MISSING"
     }
 
     [ -n "$FOLDER_MISSING" ] && {
-        echo "Boards listed in boards/group.mk without board folder: $FOLDER_MISSING"
+        echo "Boards listed in boards/group.mk without board folder:"
+        echo "$FOLDER_MISSING"
     }
 
     exit 1
