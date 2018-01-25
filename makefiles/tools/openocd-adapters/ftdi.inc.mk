614e89448662afa4c1f73307bc7427af5611d37b
Raul Fuentes <raul.fuentes-samaniego@inria.fr>
2018-01-25 11:47:36 +0100

    ftdi.inc.mk: Created generick Makefile for ftdi (openocd)

--- makefiles/tools/openocd-adapters/ftdi.inc.mk
+++ makefiles/tools/openocd-adapters/ftdi.inc.mk
@@ -1,3 +1,4 @@
+# ftdi-serial debug adapter
 # Add serial matching command, only if DEBUG_ADAPTER_ID was specified
 ifneq (,$(DEBUG_ADAPTER_ID))
   OPENOCD_ADAPTER_INIT += -c 'ftdi_serial $(DEBUG_ADAPTER_ID)'
