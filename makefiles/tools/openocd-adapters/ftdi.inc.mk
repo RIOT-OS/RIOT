1f3e8bf9b88bc211701f0d9fce9cf9870eb12079
Raul Fuentes <ra.fuentes.sam@gmail.com>
2018-01-30 20:14:11 +0100

    fixup! fixup! ftdi.inc.mk: Support for ftdi-location

--- makefiles/tools/openocd-adapters/ftdi.inc.mk
+++ makefiles/tools/openocd-adapters/ftdi.inc.mk
@@ -2,7 +2,7 @@
 # Add serial matching command, only if DEBUG_ADAPTER_ID was specified
 ifneq (,$(DEBUG_ADAPTER_ID))
   OPENOCD_ADAPTER_INIT += -c 'ftdi_serial $(DEBUG_ADAPTER_ID)'
-else ifeq (,$(DEBUG_ADAPTER_LOCATION))
+else ifeq (Linux,$(OS))
   # Some boards don't have serial at all (eg. itolab-m3)
   # find-usb.sh will return the USB hardware address for PORT (/dev/ttyUSB*)
   DEBUG_ADAPTER_ID := $(firstword $(shell $(RIOTBASE)/dist/tools/usb-serial/find-usb.sh '$(PORT)'))
