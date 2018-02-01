79fec931b6b1198cc3373f635bb09b76188e9784
JP Bonn <833319+jpbonn@users.noreply.github.com>
2018-02-01 11:50:48 -0700

    build: cppcheck ignore vendor directories.

--- dist/tools/ci/changed_files.sh
+++ dist/tools/ci/changed_files.sh
@@ -8,7 +8,7 @@
 
 changed_files() {
     : ${FILEREGEX:='\.([CcHh]|[ch]pp)$'}
-    : ${EXCLUDE:='^(.+/include/vendor/|dist/tools/coccinelle/include|boards/common/msba2/tools/src)'}
+    : ${EXCLUDE:='^(.+/include/vendor/|.+/vendor/|dist/tools/coccinelle/include|boards/common/msba2/tools/src)'}
     : ${DIFFFILTER:='ACMR'}
 
     DIFFFILTER="--diff-filter=${DIFFFILTER}"
