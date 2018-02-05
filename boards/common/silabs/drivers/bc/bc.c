eada4b71df8466715a1a10a47806ffea7a2db66a
Bas Stottelaar <basstottelaar@gmail.com>
2018-02-05 23:41:56 +0100

    boards/common: silabs: add common board drivers for Silicon Labs devkits.

---
+++ boards/common/silabs/drivers/bc/bc.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 Bas Stottelaar <basstottelaar@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_common_silabs_drivers_bc
+ * @{
+ *
+ * @file
+ * @brief       Implementations of the board controller driver.
+ *
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ *
+ * @}
+ */
+
+#include "bc.h"
+
+#include "uart_stdio.h"
+
+#include "periph/gpio.h"
+#include "periph/uart.h"
+
+/**
+ * @brief   Ensure that the BC_PIN is defined
+ */
+#ifndef BC_PIN
+#error "BC_PIN is not defined by the board."
+#endif
+
+/**
+ * @brief   Ensure that the correct UART is used.
+ */
+#if ((UART_STDIO_DEV) != (UART_DEV(0)))
+#error "The BC requires UART_DEV(0)."
+#endif
+
+/**
+ * @brief   Ensure that the correct baud rate is used.
+ */
+#if ((UART_STDIO_BAUDRATE) != 115200)
+#error "The BC requires a baud rate of 115200."
+#endif
+
+void bc_init(void)
+{
+    gpio_init(BC_PIN, GPIO_OUT);
+    gpio_set(BC_PIN);
+}
---
+++ boards/common/silabs/drivers/include/pic.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2018 Bas Stottelaar <basstottelaar@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    boards_common_silabs_drivers_pic Power-and-interrupt driver
+ * @ingroup     boards_common_silabs_drivers
+ * @{
+ *
+ * @file
+ * @brief       Implementations of the power-and-interrupt controller.
+ *
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ */
+
+#ifndef PIC_H
+#define PIC_H
+
+#include "board.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Initialize the power-and-interrupt controller.
+ *
+ * The following values must be defined:
+ *
+ * - PIC_INT_WAKE_PIN
+ * - PIC_I2C
+ * - PIC_I2C_ADDR
+ */
+void pic_init(void);
+
+/**
+ * @brief   Write data to the power-and-interrupt controller.
+ *
+ * @param[in] addr      Address of the peripheral to write to.
+ * @param[in] value     Value to write.
+ */
+void pic_write(uint8_t addr, uint8_t value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PIC_H */
+/** @} */
