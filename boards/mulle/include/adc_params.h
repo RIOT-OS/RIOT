2b0087167d28170c306280e3a54c6953f7c45893
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-02-17 00:00:14 +0100

    boards/mulle: Add SAUL ADC conversion config

--- boards/mulle/include/adc_params.h
+++ boards/mulle/include/adc_params.h
@@ -21,9 +21,38 @@
 
 #include "board.h"
 #include "saul/periph.h"
+#if MODULE_SAUL_ADC_CONVERSION
+#include "periph/adc_conv.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+#if MODULE_SAUL_ADC_CONVERSION
+#define ADC_CONV(func) .conv = (func)
+#else
+#define ADC_CONV(ignore)
+#endif
+
+#if MODULE_SAUL_ADC_CONVERSION
+/**
+ * @brief Convert Mulle Vbat, Vchr samples into real voltage
+ *
+ * @param[in] params SAUL ADC params pointer
+ * @param[in] res    ADC sample
+ *
+ * @return 1
+ */
+static int mulle_vbat_volts(const saul_adc_params_t *params, phydat_t *res)
+{
+    adc_conv_saul_adc_volts(params, res);
+    /* The Vbat, Vchr measurement point is located on a voltage divider which
+     * shows half of the full voltage, to be able to measure voltages greater
+     * than ADC Vref. */
+    res->val[0] *= 2;
+    return 1;
+}
 #endif
 
 /**
@@ -40,36 +69,43 @@ static const  saul_adc_params_t saul_adc_params[] =
         .name = "k60vrefsh",
         .line = ADC_LINE(2),
         .res  = ADC_RES_16BIT,
+        ADC_CONV(adc_conv_saul_adc_volts),
     },
     {
         .name = "k60vrefsl",
         .line = ADC_LINE(3),
         .res  = ADC_RES_16BIT,
+        ADC_CONV(adc_conv_saul_adc_volts),
     },
     {
         .name = "k60bandgap",
         .line = ADC_LINE(1),
         .res  = ADC_RES_16BIT,
+        ADC_CONV(adc_conv_saul_adc_volts),
     },
     {
         .name = "DAC0feedback",
         .line = ADC_LINE(4),
         .res  = ADC_RES_16BIT,
+        ADC_CONV(adc_conv_saul_adc_volts),
     },
     {
         .name = "VREFfeedback",
         .line = ADC_LINE(5),
         .res  = ADC_RES_16BIT,
+        ADC_CONV(adc_conv_saul_adc_volts),
     },
     {
         .name = "Vbat",
         .line = MULLE_VBAT_ADC_LINE,
         .res  = ADC_RES_16BIT,
+        ADC_CONV(mulle_vbat_volts),
     },
     {
         .name = "Vchr",
         .line = MULLE_VCHR_ADC_LINE,
         .res  = ADC_RES_16BIT,
+        ADC_CONV(mulle_vbat_volts),
     },
     {
         .name = "PGA0_DP",
