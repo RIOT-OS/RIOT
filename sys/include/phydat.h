56f44729d737f6854f007fadcc97ab9997179934
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-02-18 07:38:45 +0100

    sys/phydat: rename phydat_scale_to_str -> phydat_prefix_from_scale
    Correct some outdated documentation and make the name represent what the
    function does today.

--- sys/include/phydat.h
+++ sys/include/phydat.h
@@ -36,7 +36,6 @@
 #define PHYDAT_H
 
 #include <stdint.h>
-#include <errno.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -157,15 +156,17 @@ void phydat_dump(phydat_t *data, uint8_t dim);
 const char *phydat_unit_to_str(uint8_t unit);
 
 /**
- * @brief   Convert the given scale factor to a NULL terminated string
+ * @brief   Convert the given scale factor to an SI prefix
  *
- * The given scaling factor will be given as SI unit (e.g. M for Mega, u for
- * micro, etc) for obvious cases or in scientific notation (e.g. 2E11, 1E-22,
- * etc) otherwise.
+ * The given scaling factor is returned as a SI unit prefix (e.g. M for Mega, u
+ * for micro, etc), or `\0` otherwise.
  *
  * @param[in] scale     scale factor to convert
+ *
+ * @return  SI prefix if applicable
+ * @return  `\0` if no SI prefix was found
  */
-char phydat_scale_to_str(int8_t scale);
+char phydat_prefix_from_scale(int8_t scale);
 
 #ifdef __cplusplus
 }
