63f49f22d87e605712327c0580e858e42f96507c
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-02-19 08:43:39 +0100

    sys/phydat: Add phydat_fit
    Fits larger values into the 16 bit integers used by phydat and update
    scale accordingly.

--- sys/include/phydat.h
+++ sys/include/phydat.h
@@ -178,6 +178,40 @@ const char *phydat_unit_to_str(uint8_t unit);
  */
 char phydat_prefix_from_scale(int8_t scale);
 
+/**
+ * @brief   Scale an integer value to fit into a @ref phydat_t
+ *
+ * Insert @p value at position @p index in the given @p dat while rescaling all
+ * numbers in @p dat->val so that @p value fits inside the limits of the data
+ * type, [@ref PHYDAT_MIN, @ref PHYDAT_MAX], and update the stored scale factor.
+ * The result will be rounded towards zero (the standard C99 integer division
+ * behaviour).
+ * The final parameter @p prescale can be used to chain multiple calls to
+ * this function in order to fit multidimensional values into the same phydat_t.
+ *
+ * The code example below shows how to chain multiple calls via the @p prescale parameter
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ {.c}
+ * long val0 = 100000;
+ * long val1 = 2000000;
+ * long val2 = 30000000;
+ * phydat_t dat;
+ * dat.scale = 0;
+ * phydat_fit(&dat, val0, 0, phydat_fit(&dat, val1, 1, phydat_fit(&dat, val2, 2, 0)));
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * The prescale scaling is only applied to @p value, the existing values in
+ * @p dat are only scaled if the prescaled @p value does not fit in phydat_t::val
+ *
+ * @param[in, out]  dat         the value will be written into this data array
+ * @param[in]       value       value to rescale
+ * @param[in]       index       place the value at this position in the phydat_t::val array
+ * @param[in]       prescale    start by scaling the value by this exponent
+ *
+ * @return  scaling offset that was applied
+ */
+uint8_t phydat_fit(phydat_t *dat, long value, unsigned int index, uint8_t prescale);
+
 #ifdef __cplusplus
 }
 #endif
