cee77220f1dbb97a2bdeed351ddcef0e102f4375
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-02-19 15:36:57 +0100

    fixup! fixup! boards/mulle: Add SAUL ADC conversion config

--- boards/mulle/include/board.h
+++ boards/mulle/include/board.h
@@ -28,9 +28,9 @@
  * @name ADC sample conversion configuration
  * @{
  */
-#define BOARD_ADC_VREFH_DEFAULT          33000
-#define BOARD_ADC_VREFL_DEFAULT          0
-#define BOARD_ADC_VREF_SCALE_DEFAULT    -4
+#define BOARD_ADC_VREFH_DEFAULT         (33000)
+#define BOARD_ADC_VREFL_DEFAULT         (    0)
+#define BOARD_ADC_VREF_SCALE_DEFAULT    (-4)
 /** @} */
 
 /* Use the on board RTC 32kHz clock for LPTMR clocking. */
