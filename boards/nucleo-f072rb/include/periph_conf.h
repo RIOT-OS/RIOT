4b20095dc8c4c1e1f1c7084345028e77909e7706
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-02-27 14:27:53 +0100

    boards/nucleo-f072rb: rename to marketing name

--- boards/nucleo-f072rb/include/periph_conf.h
+++ boards/nucleo-f072rb/include/periph_conf.h
@@ -7,13 +7,13 @@
  */
 
 /**
- * @defgroup    boards_nucleo-f072 STM32 Nucleo-F072
+ * @defgroup    boards_nucleo-f072rb STM32 Nucleo-F072RB
  * @ingroup     boards_common_nucleo64
- * @brief       Support for the STM32 Nucleo-F072
+ * @brief       Support for the STM32 Nucleo-F072RB
  * @{
  *
  * @file
- * @brief       Peripheral MCU configuration for the nucleo-f072 board
+ * @brief       Peripheral MCU configuration for the nucleo-f072rb board
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      José Ignacio Alamos <jialamos@uc.cl>
