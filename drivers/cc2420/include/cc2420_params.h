eb81eae0600815b602d2d56fdcef8f44153ada89
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-02-28 18:24:28 +0100

    drivers/cc2420: use new driver params scheme

--- drivers/cc2420/include/cc2420_params.h
+++ drivers/cc2420/include/cc2420_params.h
@@ -57,15 +57,17 @@ extern "C" {
 #define CC2420_PARAM_RESET      (GPIO_PIN(0, 3))
 #endif
 
-#define CC2420_PARAMS_DEFAULT   {.spi        = CC2420_PARAM_SPI, \
-                                 .spi_clk    = CC2420_PARAM_SPI_CLK, \
-                                 .pin_cs     = CC2420_PARAM_CS, \
-                                 .pin_fifo   = CC2420_PARAM_FIFO, \
-                                 .pin_fifop  = CC2420_PARAM_FIFOP, \
-                                 .pin_cca    = CC2420_PARAM_CCA, \
-                                 .pin_sfd    = CC2420_PARAM_SFD, \
-                                 .pin_vrefen = CC2420_PARAM_VREFEN, \
-                                 .pin_reset  = CC2420_PARAM_RESET}
+#ifndef CC2420_PARAMS
+#define CC2420_PARAMS           { .spi        = CC2420_PARAM_SPI,     \
+                                  .spi_clk    = CC2420_PARAM_SPI_CLK, \
+                                  .pin_cs     = CC2420_PARAM_CS,      \
+                                  .pin_fifo   = CC2420_PARAM_FIFO,    \
+                                  .pin_fifop  = CC2420_PARAM_FIFOP,   \
+                                  .pin_cca    = CC2420_PARAM_CCA,     \
+                                  .pin_sfd    = CC2420_PARAM_SFD,     \
+                                  .pin_vrefen = CC2420_PARAM_VREFEN,  \
+                                  .pin_reset  = CC2420_PARAM_RESET }
+#endif
 /**@}*/
 
 /**
@@ -73,11 +75,7 @@ extern "C" {
  */
 static const cc2420_params_t cc2420_params[] =
 {
-#ifdef CC2420_PARAMS_BOARD
-    CC2420_PARAMS_BOARD,
-#else
-    CC2420_PARAMS_DEFAULT,
-#endif
+    CC2420_PARAMS
 };
 
 #ifdef __cplusplus
