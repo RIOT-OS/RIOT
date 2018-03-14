b3a978681a8402364b0a7f4ad1d6706693c75225
bcartier <baptiste.cartier@polytech-lille.net>
2018-03-14 17:56:57 +0100

    Adding sources for the radio module

---
+++ cpu/cc430/include/cc430_radio.h
@@ -0,0 +1,66 @@
+/*
+* Copyright (C) 2018 Baptiste Cartier
+*
+* This file is subject to the terms and conditions of the GNU Lesser General
+* Public License v2.1. See the file LICENSE in the top level directory for more
+* details.
+*/
+
+#include <stdint.h>
+
+#ifndef cc430_radio_H
+#define cc430_radio_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* -------------------------------------------------------------------------
+ *                                Defines
+ * -------------------------------------------------------------------------
+ */
+
+
+#define CONF_REG_SIZE   47     /* There are 47 8-bit configuration registers */
+#define cc430_radio_POWER_OUTPUT_MINUS_30DBM       0x03
+#define cc430_radio_POWER_OUTPUT_MINUS_12DBM       0x25
+#define cc430_radio_POWER_OUTPUT_MINUS_6DBM        0x2D
+#define cc430_radio_POWER_OUTPUT_0DBM              0x8D
+#define cc430_radio_POWER_OUTPUT_10DBM             0xC3
+#define cc430_radio_POWER_OUTPUT_MAX               0xC0
+#define cc430_radio_POWER_OUTPUT_DEFAULT_8_8DBM    0xC6
+
+#define RADIOINSTRW_MINUS_30DBM              0x7E03
+#define RADIOINSTRW_MINUS_12DBM              0x7E25
+#define RADIOINSTRW_MINUS_6DBM               0x7E2D
+#define RADIOINSTRW_0DBM                     0x7E8D
+#define RADIOINSTRW_10DBM                    0x7EC3
+#define RADIOINSTRW_MAX                      0x7EC0
+#define RADIOINSTRW_DEFAULT_8_8DBM           0x7EC6
+
+#define TX_TO_IDLE_TIME     10    // TX to IDLE, no calibration: ~1us   => 0.3us *10 = 3us
+#define RX_TO_IDLE_TIME     2     // RX to IDLE, no calibration: ~0.1us => 0.3*2 = 0.6us
+#define IDLE_TO_RX_TIME     300   // IDLE to RX, no calibration: 75.1us => 0.3*300 = 90us
+
+
+void cc430_radio_delay_rf(volatile unsigned long p);
+uint8_t cc430_radio_strobe(uint8_t strobe);
+uint8_t cc430_radio_read_single_reg(uint8_t addr);
+void cc430_radio_write_single_reg(uint8_t addr, uint8_t value);
+void cc430_radio_read_burst_reg(uint8_t addr, void *buffer, uint8_t count);
+void cc430_radio_write_burst_reg(uint8_t addr, void *buffer, uint8_t count);
+void cc430_radio_reset_radio_core (void);
+int8_t cc430_radio_write_pa_table(uint8_t value);
+void cc430_radio_transmit(void *buffer, uint8_t length);
+void cc430_radio_receive(void *buffer, uint8_t *length);
+void cc430_radio_receive_on(void);
+void cc430_radio_receive_off(void);
+void cc430_radio_write_rf_reg(const uint8_t SmartRFSetting[][2], uint8_t size);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* cc430_radio_H */
---
+++ cpu/cc430/include/cc430_radio_rfsetting.h
@@ -0,0 +1,116 @@
+/*****************************************************************
+* Configuration file for the cc1101 registers, change the desired value here.
+* Texas Instrument's SmartRF Studio software can be used to generate this software,
+* but it requires a Windows OS to work.
+*****************************************************************/
+
+
+#ifndef CC430_RADIO_RFSETTING_H
+#define CC430_RADIO_RFSETTING_H
+#endif
+
+#ifndef RECEIVE_OR_TRANSMIT
+#define RECEIVE_OR_TRANSMIT 1   //1 for transmission, else for reception
+#endif
+
+/*****************************************************************
+* Example configuration for radio module
+*****************************************************************/
+
+#if RECEIVE_OR_TRANSMIT == 1
+
+#define CC430_RADIO_RFSETTING_IOCFG2     0x29   //RF_RDYn , active high
+#define CC430_RADIO_RFSETTING_IOCFG1     0x2E   //Tri-State, active high
+#define CC430_RADIO_RFSETTING_IOCFG0     0x06
+#define CC430_RADIO_RFSETTING_FIFOTHR    0x40   //61 bytes in TX FIFO
+#define CC430_RADIO_RFSETTING_SYNC1      0xD3   //default
+#define CC430_RADIO_RFSETTING_SYNC0      0x91   //default
+#define CC430_RADIO_RFSETTING_PKTLEN     0x40   //64 maximum packet length
+#define CC430_RADIO_RFSETTING_PKTCTRL1   0x00   //No address check, no CRC flush, no bytes appended
+#define CC430_RADIO_RFSETTING_PKTCTRL0   0x01   //Varaible packet length, no data Whitening, Normal mode, no CRC calculation and check
+#define CC430_RADIO_RFSETTING_ADDR       0x00   //Device address 0x00
+#define CC430_RADIO_RFSETTING_CHANNR     0xC0   //Channel 192
+#define CC430_RADIO_RFSETTING_FSCTRL1    0x06   //Desired frequency
+#define CC430_RADIO_RFSETTING_FSCTRL0    0x00   //Frequency offset
+#define CC430_RADIO_RFSETTING_FREQ2      0x1D   
+#define CC430_RADIO_RFSETTING_FREQ1      0xEE 
+#define CC430_RADIO_RFSETTING_FREQ0      0x3F 
+#define CC430_RADIO_RFSETTING_MDMCFG4    0xF5
+#define CC430_RADIO_RFSETTING_MDMCFG3    0x83
+#define CC430_RADIO_RFSETTING_MDMCFG2    0x13   //2-GFSK, 30/32 sync word bits detected, Manchester disabled
+#define CC430_RADIO_RFSETTING_MDMCFG1    0x22   //4 preamble bytes
+#define CC430_RADIO_RFSETTING_MDMCFG0    0xF8 
+#define CC430_RADIO_RFSETTING_DEVIATN    0x15
+#define CC430_RADIO_RFSETTING_MCSM2      0x07   //Timeout disabled 
+#define CC430_RADIO_RFSETTING_MCSM1      0x3C  
+#define CC430_RADIO_RFSETTING_MCSM0      0x10   
+#define CC430_RADIO_RFSETTING_FOCCFG     0x16   
+#define CC430_RADIO_RFSETTING_BSCFG      0x6C   
+#define CC430_RADIO_RFSETTING_AGCCTRL2   0x03
+#define CC430_RADIO_RFSETTING_AGCCTRL1   0x40
+#define CC430_RADIO_RFSETTING_AGCCTRL0   0x91
+#define CC430_RADIO_RFSETTING_WOREVT1    0x80 
+#define CC430_RADIO_RFSETTING_WOREVT0    0x00 
+#define CC430_RADIO_RFSETTING_WORCTRL    0xFB
+#define CC430_RADIO_RFSETTING_FREND1     0x56
+#define CC430_RADIO_RFSETTING_FREND0     0x10
+#define CC430_RADIO_RFSETTING_FSCAL3     0xE9
+#define CC430_RADIO_RFSETTING_FSCAL2     0x2A
+#define CC430_RADIO_RFSETTING_FSCAL1     0x00
+#define CC430_RADIO_RFSETTING_FSCAL0     0x1F
+#define CC430_RADIO_RFSETTING_FSTEST     0x59
+#define CC430_RADIO_RFSETTING_PTEST      0x7F
+#define CC430_RADIO_RFSETTING_AGCTEST    0x3F
+#define CC430_RADIO_RFSETTING_TEST2      0x81
+#define CC430_RADIO_RFSETTING_TEST1      0x35
+#define CC430_RADIO_RFSETTING_TEST0      0x0B
+
+#else
+
+#define CC430_RADIO_RFSETTING_IOCFG2     0x29   //RF_RDYn , active high
+#define CC430_RADIO_RFSETTING_IOCFG1     0x2E   //Tri-State, active high
+#define CC430_RADIO_RFSETTING_IOCFG0     0x06
+#define CC430_RADIO_RFSETTING_FIFOTHR    0x40   //64 bytes in RX FIFO
+#define CC430_RADIO_RFSETTING_SYNC1      0xD3   //default
+#define CC430_RADIO_RFSETTING_SYNC0      0x91   //default
+#define CC430_RADIO_RFSETTING_PKTLEN     0x40   //64 maximum packet length
+#define CC430_RADIO_RFSETTING_PKTCTRL1   0x00   //No address check, no CRC flush, no bytes appended
+#define CC430_RADIO_RFSETTING_PKTCTRL0   0x01   //Varaible packet length, no data Whitening, Normal mode, no CRC calculation and check
+#define CC430_RADIO_RFSETTING_ADDR       0x00   //Device address 0x00
+#define CC430_RADIO_RFSETTING_CHANNR     0xC0   //Channel 192
+#define CC430_RADIO_RFSETTING_FSCTRL1    0x06   //Desired frequency
+#define CC430_RADIO_RFSETTING_FSCTRL0    0x00   //Frequency offset
+#define CC430_RADIO_RFSETTING_FREQ2      0x1D   
+#define CC430_RADIO_RFSETTING_FREQ1      0xEE 
+#define CC430_RADIO_RFSETTING_FREQ0      0x3F 
+#define CC430_RADIO_RFSETTING_MDMCFG4    0xF5
+#define CC430_RADIO_RFSETTING_MDMCFG3    0x83
+#define CC430_RADIO_RFSETTING_MDMCFG2    0x13   //2-GFSK, 30/32 sync word bits detected, Manchester disabled
+#define CC430_RADIO_RFSETTING_MDMCFG1    0x22   //4 preamble bytes
+#define CC430_RADIO_RFSETTING_MDMCFG0    0xF8 
+#define CC430_RADIO_RFSETTING_DEVIATN    0x15
+#define CC430_RADIO_RFSETTING_MCSM2      0x07   //Timeout disabled 
+#define CC430_RADIO_RFSETTING_MCSM1      0x3C  
+#define CC430_RADIO_RFSETTING_MCSM0      0x10   
+#define CC430_RADIO_RFSETTING_FOCCFG     0x16   
+#define CC430_RADIO_RFSETTING_BSCFG      0x6C   
+#define CC430_RADIO_RFSETTING_AGCCTRL2   0x03
+#define CC430_RADIO_RFSETTING_AGCCTRL1   0x40
+#define CC430_RADIO_RFSETTING_AGCCTRL0   0x91
+#define CC430_RADIO_RFSETTING_WOREVT1    0x80 
+#define CC430_RADIO_RFSETTING_WOREVT0    0x00 
+#define CC430_RADIO_RFSETTING_WORCTRL    0xFB
+#define CC430_RADIO_RFSETTING_FREND1     0x56
+#define CC430_RADIO_RFSETTING_FREND0     0x10
+#define CC430_RADIO_RFSETTING_FSCAL3     0xE9
+#define CC430_RADIO_RFSETTING_FSCAL2     0x2A
+#define CC430_RADIO_RFSETTING_FSCAL1     0x00
+#define CC430_RADIO_RFSETTING_FSCAL0     0x1F
+#define CC430_RADIO_RFSETTING_FSTEST     0x59
+#define CC430_RADIO_RFSETTING_PTEST      0x7F
+#define CC430_RADIO_RFSETTING_AGCTEST    0x3F
+#define CC430_RADIO_RFSETTING_TEST2      0x81
+#define CC430_RADIO_RFSETTING_TEST1      0x35
+#define CC430_RADIO_RFSETTING_TEST0      0x0B
+
+#endif /*RECEIVE_OR_TRANSMIT == 1*/
