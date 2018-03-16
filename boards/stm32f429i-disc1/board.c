aa7fad6d403a85b0440bdbd5897119e733a28d81
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-03-16 08:31:57 +0100

    boards/stm32f429i-disc1: add initial support

---
+++ boards/stm32f429i-disc1/board.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_stm32f429i-disc1
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the STM32F429I-DISC1 evaluation board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the boards LEDs */
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+
+    /* initialize the CPU */
+    cpu_init();
+}
