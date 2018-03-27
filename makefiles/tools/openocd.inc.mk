3f145413f5ba21fade4db63fbc50e4bf18250794
cladmi <hartergaetan@gmail.com>
2018-03-27 12:28:35 +0200

    boards/makefiles: Remove '-Otype' from OFLAGS
    * Remove '-Oihex' and '-Obinary' from OFLAGS for all boards
      It is now provided by the Makefile.include rule.

--- makefiles/tools/openocd.inc.mk
+++ makefiles/tools/openocd.inc.mk
@@ -3,7 +3,6 @@ export DEBUGGER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export DEBUGSERVER = $(RIOTBASE)/dist/tools/openocd/openocd.sh
 export RESET ?= $(RIOTBASE)/dist/tools/openocd/openocd.sh
 
-export OFLAGS ?= -O ihex
 export FFLAGS ?= flash
 export DEBUGGER_FLAGS ?= debug
 export DEBUGSERVER_FLAGS ?= debug-server
