af8d9632465586a9537b9cab06b8761270736d22
Kevin Weiss <weiss.kevin604@gmail.com>
2018-03-28 17:01:37 +0200

    Remove #ifdef over UART_ISR

--- cpu/lm4f120/periph/uart.c
+++ cpu/lm4f120/periph/uart.c
@@ -133,7 +133,7 @@ void uart_poweroff(uart_t uart)
 /**
  * The UART interrupt handler.
  */
-void irq_handler(uart_t uart)
+static inline void irq_handler(uart_t uart)
 {
     assert(uart < UART_NUMOF);
 
