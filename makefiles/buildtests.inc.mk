3992556a6200e6b736b3962b75fbaddf6037cbc9
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-04-10 16:46:49 +0200

    fixup! REMOVE ME ONLY TO TEST MURDOCK

--- makefiles/buildtests.inc.mk
+++ makefiles/buildtests.inc.mk
@@ -10,7 +10,7 @@ buildtest:
 		$(COLOR_ECHO) -n "Building for $$board ... " ; \
 		BOARD=$${board} RIOT_CI_BUILD=1 RIOT_VERSION_OVERRIDE=buildtest \
 			$(MAKE) clean >/dev/null 2>&1; \
-			$(MAKE) clean all -j $(NPROC) >/dev/null 2>&1; \
+			$(MAKE) all -j $(NPROC) >/dev/null 2>&1; \
 		RES=$$? ; \
 		if [ $$RES -eq 0 ]; then \
 			$(COLOR_ECHO) "$(COLOR_GREEN)success.$(COLOR_RESET)" ; \
