367190c08d4bc16aba15f3684d61e9e9db97b687
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-04-16 17:37:05 +0200

    tests/driver_io1_xplained: use sdcard in test application

--- tests/driver_io1_xplained/main.c
+++ tests/driver_io1_xplained/main.c
@@ -27,14 +27,31 @@
 #include "periph/gpio.h"
 
 #include "at30tse75x.h"
+#include "sdcard_spi.h"
 #include "io1_xplained.h"
 #include "io1_xplained_params.h"
 
 #define DELAY_1S   (1U) /* 1 seconds delay between each test */
 
+static io1_xplained_t dev;
+
+static void _sd_card_cid(void)
+{
+    puts("SD Card CID info:");
+    printf("MID: %d\n", dev.sdcard.cid.MID);
+    printf("OID: %c%c\n", dev.sdcard.cid.OID[0], dev.sdcard.cid.OID[1]);
+    printf("PNM: %c%c%c%c%c\n",
+           dev.sdcard.cid.PNM[0], dev.sdcard.cid.PNM[1], dev.sdcard.cid.PNM[2],
+           dev.sdcard.cid.PNM[3], dev.sdcard.cid.PNM[4]);
+    printf("PRV: %d\n", dev.sdcard.cid.PRV);
+    printf("PSN: %lu\n", dev.sdcard.cid.PSN);
+    printf("MDT: %d\n", dev.sdcard.cid.MDT);
+    printf("CRC: %d\n", dev.sdcard.cid.CID_CRC);
+    puts("+----------------------------------------+\n");
+}
+
 int main(void)
 {
-    io1_xplained_t dev;
     float temperature;
 
     puts("IO1 Xplained extention test application\n");
@@ -54,6 +71,12 @@ int main(void)
                "+-------------------------------------+\n",
                temperature);
         xtimer_sleep(DELAY_1S);
+
+        /* Card detect pin is inverted */
+        if (!gpio_read(IO1_SDCARD_SPI_PARAM_DETECT)) {
+            _sd_card_cid();
+            xtimer_sleep(DELAY_1S);
+        }
 
         /* set led */
         gpio_set(IO1_LED_PIN);
