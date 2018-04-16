ba809c231ee0a340af7808ee6caf49c0b8baceb7
Koen Zandberg <koen@bergzand.net>
2018-04-16 19:02:04 +0200

    pkg/libcose: add simple unittest application

---
+++ tests/unittests/tests-libcose/tests-libcose.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018 Freie Universität Berlin
+ * Copyright (C) 2018 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the libcose package
+ *
+ */
+#ifndef TESTS_LIBCOSE_H
+#define TESTS_LIBCOSE_H
+
+#include "embUnit/embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ *  @brief   The entry point of this test suite.
+ */
+void tests_libcose(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_LIBCOSE_H */
+/** @} */
