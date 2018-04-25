9f4eea590021177fe7822d933577f6ff2fc6ab6c
Josarn <josuaarndt@live.de>
2018-04-26 00:52:04 +0200

    xtimer tick_conversion.h: uncrustify

--- sys/include/xtimer/tick_conversion.h
+++ sys/include/xtimer/tick_conversion.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 Eistec AB
+ *               2018 Josua Arndt
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -125,9 +126,11 @@ static inline uint64_t _xtimer_usec_from_ticks64(uint64_t ticks) {
 #else
 /* No matching implementation found, try to give meaningful error messages */
 #if ((XTIMER_HZ % 15625) == 0)
-#error Unsupported hardware timer frequency (XTIMER_HZ), missing XTIMER_SHIFT in board.h? See xtimer.h documentation for more info
+#error "Unsupported hardware timer frequency (XTIMER_HZ), missing XTIMER_SHIFT \
+in board.h? See xtimer.h documentation for more info"
 #else
-#error Unknown hardware timer frequency (XTIMER_HZ), check board.h and/or add an implementation in sys/include/xtimer/tick_conversion.h
+#error "Unknown hardware timer frequency (XTIMER_HZ), check board.h and/or add \
+an implementation in sys/include/xtimer/tick_conversion.h"
 #endif
 #endif
 
