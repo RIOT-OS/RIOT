97e1be083063d8935020d714954ed1f1dcb5aec8
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-05-06 07:15:17 +0200

    tests: Add xfa test

---
+++ tests/xfa/main.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     test
+ * @{
+ *
+ * @file
+ * @brief       cross file array (XFA) test application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "xfa.h"
+
+#include "xfatest.h"
+
+XFA_INIT(xfatest_t, xfatest);
+XFA_INIT_CONST(xfatest_t, xfatest_const);
+
+/* hack to force inclusion of otherwise unused compilation units */
+extern int hack1;
+extern int hack2;
+
+int main(void)
+{
+    puts("Cross file array test");
+
+    unsigned n = XFA_LEN(xfatest_t, xfatest);
+    printf("xfatest[%u]:\n", n);
+    for (unsigned i = 0; i < n; i++) {
+        printf("[%u] = %u, \"%s\"\n", i, xfatest[i].val, xfatest[i].text);
+    }
+    n = XFA_LEN(xfatest_t, xfatest_const);
+    printf("xfatest_const[%u]:\n", n);
+    for (unsigned i = 0; i < n; i++) {
+        printf("[%u] = %u, \"%s\"\n", i, xfatest_const[i].val, xfatest_const[i].text);
+    }
+
+    hack1 = hack2;
+
+    return 0;
+}
---
+++ tests/xfa/xfatest2.c
@@ -0,0 +1,7 @@
+#include "xfa.h"
+#include "xfatest.h"
+
+XFA(xfatest, 0) xfatest_t _xfatest2 = { .val = 2, .text = "xfatest2" };
+XFA_CONST(xfatest_const, 0) xfatest_t _xfatest_const2 = { .val = 45, .text = "xfatest_const2" };
+
+int hack2;
