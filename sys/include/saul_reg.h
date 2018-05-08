dcb01a9612a88da554ad8cdb88fae01b361a596a
Matthew Blue <matthew.blue.neuro@gmail.com>
2018-05-07 23:12:11 -0400

    sys/saul_reg: support for saul context parameter

--- sys/include/saul_reg.h
+++ sys/include/saul_reg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Freie Universität Berlin
+ *               2018 Acutam Automation, LLC
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -19,6 +20,7 @@
  * @brief       SAUL registry interface definition
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Matthew Blue <matthew.blue.neuro@gmail.com>
  */
 
 #ifndef SAUL_REG_H
@@ -33,6 +35,13 @@
 extern "C" {
 #endif
 
+#ifndef SAUL_HAVE_CTXT_LIST_T
+/**
+ * @brief   SAUL context list type
+ */
+typedef uint16_t saul_ctxt_list_t;
+#endif
+
 /**
  * @brief   SAUL registry entry
  */
@@ -41,6 +50,7 @@ typedef struct saul_reg {
     void *dev;                      /**< pointer to the device descriptor */
     const char *name;               /**< string identifier for the device */
     saul_driver_t const *driver;    /**< the devices read callback */
+    saul_ctxt_list_t ctxtlist;      /**< bitfield of valid context numbers */
 } saul_reg_t;
 
 /**
@@ -54,6 +64,31 @@ typedef struct {
  * @brief   Export the SAUL registry as global variable
  */
 extern saul_reg_t *saul_reg;
+
+/**
+ * @brief   SAUL pointer and context
+ *
+ * @note    The SAUL registry is comprised of real members and imaginary
+ *          duplicates. If a device is added to the registry, it will add
+ *          a number of duplicates without using additional system resources.
+ *          This is intended to make support easier for devices with an array
+ *          of similar inputs/outputs (such as GPIO expanders, or analog muxes)
+ */
+typedef struct saul_ctxt_ptr {
+    saul_reg_t *reg;    /**< pointer to device in SAUL registry */
+    uint8_t ctxt;       /**< context supplied to device */
+    uint8_t num;        /**< num of enabled device in ctxtlist */
+} saul_ctxt_ptr_t;
+
+/**
+ * @brief   Saul registry iterate function callback type for saul_reg_iter
+ *
+ * @param[in] num         number of the device and context in registry
+ * @param[in] ctxt_ptr    pointer and context of entry
+ * @param[in] arg         generic optional argument
+ */
+typedef void (*saul_reg_iter_t)(unsigned num,
+                                saul_ctxt_ptr_t ctxt_ptr, void *arg);
 
 /**
  * @brief   Register a device with the SAUL registry
@@ -82,12 +117,13 @@ int saul_reg_rm(saul_reg_t *dev);
 /**
  * @brief   Find a device by it's position in the registry
  *
- * @param[in] pos       position to look up
+ * @param[in] pos          position to look up
+ * @param[out] ctxt_ptr    context + pointer to device
  *
  * @return      pointer to the device at position specified by @p pos
  * @return      NULL if no device is registered at that position
  */
-saul_reg_t *saul_reg_find_nth(int pos);
+saul_reg_t *saul_reg_find_nth(saul_ctxt_ptr_t *ctxt_ptr, int pos);
 
 /**
  * @brief   Find the first device of the given type in the registry
@@ -112,28 +148,38 @@ saul_reg_t *saul_reg_find_name(const char *name);
 /**
  * @brief   Read data from the given device
  *
- * @param[in] dev       device to read from
- * @param[out] res      location to store the results in
+ * @param[in] dev     pointer to device to read from
+ * @param[in] ctxt    context for device to write to
+ * @param[out] res    location to store the results in
  *
  * @return      the number of data elements read to @p res [1-3]
  * @return      -ENODEV if given device is invalid
  * @return      -ENOTSUP if read operation is not supported by the device
  * @return      -ECANCELED on device errors
  */
-int saul_reg_read(saul_reg_t *dev, phydat_t *res);
+int saul_reg_read(saul_reg_t *dev, uint8_t ctxt, phydat_t *res);
 
 /**
  * @brief   Write data to the given device
  *
- * @param[in] dev       device to write to
- * @param[in] data      data to write to the device
+ * @param[in] dev     pointer to device to write to
+ * @param[in] ctxt    context for device to write to
+ * @param[in] data    data to write to the device
  *
  * @return      the number of data elements processed by the device
  * @return      -ENODEV if given device is invalid
  * @return      -ENOTSUP if read operation is not supported by the device
  * @return      -ECANCELED on device errors
  */
-int saul_reg_write(saul_reg_t *dev, phydat_t *data);
+int saul_reg_write(saul_reg_t *dev, uint8_t ctxt, phydat_t *data);
+
+/**
+ * @brief   Run a function on every device and context in the registry
+ *
+ * @param[in] func  function to run on each entry
+ * @param[in] arg   argument for the function
+ */
+void saul_reg_iter(saul_reg_iter_t func, void *arg);
 
 #ifdef __cplusplus
 }
--- sys/saul_reg/saul_reg.c
+++ sys/saul_reg/saul_reg.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Freie Universität Berlin
+ *               2018 Acutam Automation, LLC
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -14,6 +15,7 @@
  * @brief       SAUL registry implementation
  *
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Matthew Blue <matthew.blue.neuro@gmail.com>
  *
  * @}
  */
@@ -76,14 +78,48 @@ int saul_reg_rm(saul_reg_t *dev)
     return 0;
 }
 
-saul_reg_t *saul_reg_find_nth(int pos)
+saul_reg_t *saul_reg_find_nth(saul_ctxt_ptr_t *ctxt_ptr, int pos)
 {
-    saul_reg_t *tmp = saul_reg;
+    /* initialize ctxt_ptr */
+    ctxt_ptr->reg = saul_reg;
+    ctxt_ptr->ctxt = 0;
+    ctxt_ptr->num = 0;
 
-    for (int i = 0; (i < pos) && tmp; i++) {
-        tmp = tmp->next;
+    int i = 0;
+
+    while (ctxt_ptr->reg) {
+        /* if ctxtlist is empty, ignore it */
+        if (ctxt_ptr->reg->ctxtlist == 0) {
+            if (i == pos) {
+                return ctxt_ptr->reg;
+            }
+            ctxt_ptr->reg = ctxt_ptr->reg->next;
+            i++;
+            continue;
+        }
+
+        /* if ctxtlist is not empty, examine it bitwise */
+        ctxt_ptr->num = 0;
+        for (unsigned j = 0; j < 8 * sizeof(ctxt_ptr->reg->ctxtlist); j++) {
+            /* check to see if this context bit is enabled */
+            if ((ctxt_ptr->reg->ctxtlist >> j) & 0x1) {
+                if (i == pos) {
+                    ctxt_ptr->ctxt = j;
+                    return ctxt_ptr->reg;
+                }
+
+                /* increment num of enabled item in ctxtlist */
+                ctxt_ptr->num++;
+
+                /* increment list counter, as if context is real list item */
+                i++;
+            }
+        }
+
+        ctxt_ptr->reg = ctxt_ptr->reg->next;
     }
-    return tmp;
+
+    return ctxt_ptr->reg;
 }
 
 saul_reg_t *saul_reg_find_type(uint8_t type)
@@ -112,18 +148,56 @@ saul_reg_t *saul_reg_find_name(const char *name)
     return NULL;
 }
 
-int saul_reg_read(saul_reg_t *dev, phydat_t *res)
+int saul_reg_read(saul_reg_t *dev, uint8_t ctxt, phydat_t *res)
 {
     if (dev == NULL) {
         return -ENODEV;
     }
-    return dev->driver->read(dev->dev, res);
+    return dev->driver->read(dev->dev, ctxt, res);
 }
 
-int saul_reg_write(saul_reg_t *dev, phydat_t *data)
+int saul_reg_write(saul_reg_t *dev, uint8_t ctxt, phydat_t *data)
 {
     if (dev == NULL) {
         return -ENODEV;
     }
-    return dev->driver->write(dev->dev, data);
+    return dev->driver->write(dev->dev, ctxt, data);
+}
+
+void saul_reg_iter(saul_reg_iter_t func, void *arg)
+{
+    saul_ctxt_ptr_t ctxt_ptr = { .reg = saul_reg };
+
+    unsigned i = 0;
+
+    while (ctxt_ptr.reg) {
+        ctxt_ptr.ctxt = 0;
+        ctxt_ptr.num = 0;
+
+        /* if ctxtlist is empty, ignore it */
+        if (ctxt_ptr.reg->ctxtlist == 0) {
+            func(i, ctxt_ptr, arg);
+
+            ctxt_ptr.reg = ctxt_ptr.reg->next;
+            i++;
+            continue;
+        }
+
+        for (; ctxt_ptr.ctxt < 8 * sizeof(ctxt_ptr.reg->ctxtlist);
+             ctxt_ptr.ctxt++) {
+
+            /* check to see if this context bit is enabled */
+            if ((ctxt_ptr.reg->ctxtlist >> ctxt_ptr.ctxt) & 0x1) {
+                func(i, ctxt_ptr, arg);
+
+                /* increment num of enabled item in ctxtlist */
+                ctxt_ptr.num++;
+
+                /* increment list counter, as if context is real list item */
+                i++;
+            }
+        }
+
+        ctxt_ptr.reg = ctxt_ptr.reg->next;
+    }
 }
