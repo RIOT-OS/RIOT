81137b6af0cd1a7fe9dbcb015f2500ac36bf88ed
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-05-14 22:27:10 +0200

    unittests: Add XFA tests

---
+++ tests/unittests/tests-core/tests-core-xfa.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018 Eistec AB
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Declarations for the core/xfa unit test
+ *
+ * @author      Joakim Nohlgård <joakim.nohlgard@eistec.se>
+ */
+#ifndef TESTS_CORE_XFA_H
+#define TESTS_CORE_XFA_H
+
+typedef struct {
+    unsigned val;
+    const char *text;
+} xfatest_t;
+
+#endif /* TESTS_CORE_XFA_H */
+
+/** @} */
--- tests/unittests/tests-core/tests-core.c
+++ tests/unittests/tests-core/tests-core.c
@@ -19,4 +19,5 @@ void tests_core(void)
     TESTS_RUN(tests_core_priority_queue_tests());
     TESTS_RUN(tests_core_byteorder_tests());
     TESTS_RUN(tests_core_ringbuffer_tests());
+    TESTS_RUN(tests_core_xfa_tests());
 }
