9b5d282f61b57db5ee16bd3a06b9e73c5d1e0d7a
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-05-15 13:55:34 +0200

    boards/stm32l46zg-disco: initial support

---
+++ boards/stm32l476g-disco/board.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_stm32l476g-disco
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the STM32L476G-DISCO board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize LEDs */
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+
+    /* initialize the CPU */
+    cpu_init();
+}
