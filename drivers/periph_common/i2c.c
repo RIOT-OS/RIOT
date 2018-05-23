4a24ce82127faacfe24d655b7a2d1917eeb83313
dylad <dylan.laduranty@mesotic.com>
2018-05-23 18:06:05 +0200

    drivers/i2c: add default fallback wrappers
    Signed-off-by: dylad <dylan.laduranty@mesotic.com>

--- drivers/periph_common/i2c.c
+++ drivers/periph_common/i2c.c
@@ -7,7 +7,7 @@
  */
 
 /**
- * @ingroup     drivers
+ * @ingroup     drivers_periph_i2c
  * @{
  *
  * @file
@@ -24,85 +24,57 @@
 
 #ifdef I2C_NUMOF
 
-#ifdef PERIPH_I2C_NEED_READ_REG
+#ifdef PERIPH_I2C_NEED_READ_REGS
 int i2c_read_reg(ic2_t dev, uint16_t addr, uint16_t reg,
                  void *data, uint8_t flags)
 {
-	return i2c_read_regs(dev, addr, reg, data, 1, flags);
+    return i2c_read_regs(dev, addr, reg, data, 1, flags);
 }
-#endif /* PERIPH_I2C_NEED_READ_REG */
 
-#ifdef PERIPH_I2C_NEED_READ_REGS
 int i2c_read_regs(ic2_t dev, uint16_t addr, uint16_t reg,
-                  void *data, size_t len, uint8_t flags);
+                  void *data, size_t len, uint8_t flags)
 {
-	uint16_t data = reg;
-	uint8_t err;
-	/* First set ADDR and register with no stop */
-	err = i2c_write(dev, addr, &data, (flags & I2C_REG16) ? 2 : 1,
-					flags & I2C_NOSTOP );
-	if(err != I2C_ACK)
-	{
-		return err;
-	}
-	/* Then get the data from device */
-    return i2c_read(dev, addr, data, len, flags);
+    uint8_t err;
+    /* First set ADDR and register with no stop */
+    err = i2c_write_bytes(dev, addr, &reg, (flags & I2C_REG16) ? 2 : 1,
+                          flags & I2C_NOSTOP );
+    if (err != I2C_ACK) {
+        return err;
+    }
+    /* Then get the data from device */
+    return i2c_read_bytes(dev, addr, data, len, flags);
 }
 #endif /* PERIPH_I2C_NEED_READ_REGS */
 
-#ifdef PERIPH_I2C_NEED_READ_BYTE
 int i2c_read_byte(ic2_t dev, uint16_t addr, void *data, uint8_t flags)
 {
-	return i2c_read(dev, addr, data, 1, flags);
+    return i2c_read_bytes(dev, addr, data, 1, flags);
 }
-#endif /* PERIPH_I2C_NEED_READ_BYTE */
 
-#ifdef PERIPH_I2C_NEED_READ_BYTES
-int i2c_read_bytes(ic2_t dev, uint16_t addr,
-                   void *data, size_t len, uint8_t flags)
-{
-	return i2c_read(dev, addr, data, len, flags);
-}
-#endif /* PERIPH_I2C_NEED_READ_BYTES */
-
-#ifdef PERIPH_I2C_NEED_WRITE_BYTE
 int i2c_write_byte(i2c_t dev, uint16_t addr, uint8_t data, uint8_t flags)
 {
-	return i2c_write(dev, addr, addr, 1, flags);
+    return i2c_write_bytes(dev, addr, addr, 1, flags);
 }
-#endif /* PERIPH_I2C_NEED_WRITE_BYTE */
 
-#ifdef PERIPH_I2C_NEED_WRITE_BYTES
-int i2c_write_bytes(i2c_t dev, uint16_t addr, const void *data,
-                    size_t len, uint8_t flags)
-{
-	return i2c_write(dev, addr, addr, len, flags);
-}
-#endif /* PERIPH_I2C_NEED_WRITE_BYTES */
-
-#ifdef PERIPH_I2C_NEED_WRITE_REG
+#ifdef PERIPH_I2C_NEED_WRITE_REGS
 int i2c_write_reg(i2c_t dev, uint16_t addr, uint16_t reg,
                   const void *data, size_t len, uint8_t flags)
 {
-	return i2c_write_regs(dev, addr, reg, data, 1);
+    return i2c_write_regs(dev, addr, reg, data, 1);
 }
-#endif /* PERIPH_I2C_NEED_WRITE_REG */
 
-#ifdef PERIPH_I2C_NEED_WRITE_REGS
 int i2c_write_regs(i2c_t dev, uint16_t addr, uint16_t reg,
-                  const void *data, size_t len, uint8_t flags)
+                   const void *data, size_t len, uint8_t flags)
 {
-	uint16_t regp = reg;
-	uint8_t err;
-	/* First set ADDR and register with no stop */
-	err = i2c_write(dev, addr, &regp, (flags & I2C_REG16) ? 2 : 1,
-					flags & I2C_NOSTOP );
-	if(err != I2C_ACK)
-	{
-		return err;
-	}
-	/* Then write data to the device */
-    return i2c_write(dev, addr, data, len, flags);
+    uint8_t err;
+    /* First set ADDR and register with no stop */
+    err = i2c_write_bytes(dev, addr, &reg, (flags & I2C_REG16) ? 2 : 1,
+                          flags & I2C_NOSTOP );
+    if (err != I2C_ACK) {
+        return err;
+    }
+    /* Then write data to the device */
+    return i2c_write_bytes(dev, addr, data, len, flags);
 }
 #endif /* PERIPH_I2C_NEED_WRITE_REGS */
 
