e99010ac3e20797709eda3feb3968f93f59d3072
Hauke Petersen <hauke.petersen@fu-berlin.de>
2018-05-24 11:35:24 +0200

    cpu/nrf5x: include nrfx.h compatibility header

---
+++ cpu/nrf5x_common/include/nrfx.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2018 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         cpu_nrf5x_common
+ * @{
+ *
+ * @file
+ * @brief           nrfx compatibility layer
+ *
+ * @author          Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef NRFX_H
+#define NRFX_H
+
+#include "cpu_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* nothing else to do here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NRFX_H */
+/** @} */
