82ea87557bc053752b4fa05af6bb41bc7481ffec
Matthew Blue <matthew.blue.neuro@gmail.com>
2018-06-03 17:44:56 -0400

    sys/div: calculate magic numbers at compile time

--- sys/include/div.h
+++ sys/include/div.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
- * Copyright (C) 2016 Eistec AB
+ *               2016 Eistec AB
+ *               2018 Acutam Automation, LLC
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -16,35 +17,135 @@
  *
  * @file
  * @ingroup   sys
+ *
  * @author    Kaspar Schleiser <kaspar@schleiser.de>
  * @author    Joakim Nohlgård <joakim.nohlgard@eistec.se>
+ * @author    Matthew Blue <matthew.blue.neuro@gmail.com>
+ *
  * @{
  */
 
 #ifndef DIV_H
 #define DIV_H
 
-#include <assert.h>
 #include <stdint.h>
+
+#include "assert.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- * @brief Approximation of (2**l)/d for d=15625, l=12, 32 bits
+ * @brief   Calculate 8-bit multiplicative inverse
+ *
+ * Result is calculated during compilation (requires const num). Bitshifted by
+ * 9 to give the most representation to the smallest interesting number (3)
+ *
+ * @param[in] num  reciprocal
+ *
+ * @return         result, bitshifted by 9
  */
-#define DIV_H_INV_15625_32    0x431bde83ul
+__attribute__((always_inline)) static inline uint8_t div_inv_8(
+                                             const uint8_t num)
+{
+    /* cannot be represented due to bit shift */
+    assert(num > 2);
+
+    const uint16_t numerator = (1 << 9);
+
+    if ((numerator % num) * 2 >= num) {
+        /* fraction is >= 0.5, so round up */
+        return (uint8_t)(numerator / num) + 1;
+    }
+    else {
+        return (uint8_t)(numerator / num);
+    }
+}
 
 /**
- * @brief Approximation of (2**l)/d for d=15625, l=12, 64 bits
+ * @brief   Calculate 16-bit multiplicative inverse
+ *
+ * Result is calculated during compilation (requires const num). Bitshifted by
+ * 17 to give the most representation to the smallest interesting number (3)
+ *
+ * @param[in] num  reciprocal
+ *
+ * @return         result, bitshifted by 17
  */
-#define DIV_H_INV_15625_64    0x431bde82d7b634dbull
+__attribute__((always_inline)) static inline uint16_t div_inv_16(
+                                             const uint16_t num)
+{
+    /* cannot be represented due to bit shift */
+    assert(num > 2);
+
+    const uint32_t numerator = (1UL << 17);
+
+    if ((numerator % num) * 2 >= num) {
+        /* fraction is >= 0.5, so round up */
+        return (uint16_t)(numerator / num) + 1;
+    }
+    else {
+        return (uint16_t)(numerator / num);
+    }
+}
 
 /**
- * @brief Required shifts for division by 15625, l above
+ * @brief   Calculate 32-bit multiplicative inverse
+ *
+ * Result is calculated during compilation (requires const num). Bitshifted by
+ * 33 to give the most representation to the smallest interesting number (3)
+ *
+ * @param[in] num  reciprocal
+ *
+ * @return         result, bitshifted by 33
  */
-#define DIV_H_INV_15625_SHIFT 12
+__attribute__((always_inline)) static inline uint32_t div_inv_32(
+                                             const uint32_t num)
+{
+    /* cannot be represented due to bit shift */
+    assert(num > 2);
+
+    const uint64_t numerator = (1ULL << 33);
+
+    if ((numerator % num) * 2 >= num) {
+        /* fraction is >= 0.5, so round up */
+        return (uint32_t)(numerator / num) + 1;
+    }
+    else {
+        return (uint32_t)(numerator / num);
+    }
+}
+
+/**
+ * @brief   Calculate 64-bit multiplicative inverse
+ *
+ * Result is calculated during compilation (requires const num). Bitshifted by
+ * 65 to give the most representation to the smallest interesting number (3)
+ *
+ * @param[in] num  reciprocal
+ *
+ * @return         result, bitshifted by 65
+ */
+__attribute__((always_inline)) static inline uint64_t div_inv_64(
+                                             const uint64_t num)
+{
+    /* cannot be represented due to bit shift */
+    assert(num > 2);
+
+    /* find the last bits without using 128-bit ints */
+    const uint64_t numerator = (1ULL << 63);
+    const uint64_t most_sig = (numerator / num) << 2;
+    const uint64_t least_sig = ((numerator % num) << 2) / num;
+
+    if ((((numerator % num) << 2) % num) * 2 >= num) {
+        /* fraction is >= 0.5, so round up */
+        return (most_sig + least_sig + 1);
+    }
+    else {
+        return (most_sig + least_sig);
+    }
+}
 
 /**
  * @internal
@@ -62,123 +163,76 @@ extern "C" {
 uint64_t _div_mulhi64(const uint64_t a, const uint64_t b);
 
 /**
- * @brief Integer divide val by 15625, 64 bit version
+ * @brief   Divide 8-bit number using an inverse
  *
- * @param[in]   val     dividend
- * @return      (val / 15625)
+ * @param[in] val  numerator
+ * @param[in] inv  multiplicative inverse of denominator (bitshifted by 9)
+ *
+ * @return         result
  */
-static inline uint64_t div_u64_by_15625(uint64_t val)
+static inline uint8_t div_u8_by_inv(const uint8_t val, const uint8_t inv)
 {
-    if (val > 16383999997ull) {
-        return (_div_mulhi64(DIV_H_INV_15625_64, val) >> DIV_H_INV_15625_SHIFT);
-    }
-    return (val * DIV_H_INV_15625_32) >> (DIV_H_INV_15625_SHIFT + 32);
+    uint16_t tmp;
+
+    tmp = (uint16_t)val * (uint16_t)inv;
+    tmp >>= 9;
+
+    return (uint8_t)tmp;
 }
 
 /**
- * @brief Integer divide val by 125
+ * @brief   Divide 16-bit number using an inverse
  *
- * This function can be used to convert uint64_t microsecond times (or
- * intervals) to miliseconds and store them in uint32_t variables, with up to
- * ~50 days worth of miliseconds ((2**32*1000) -1).
- * Use e.g.,  ms = div_u64_by_125(microseconds >> 3)
+ * @param[in] val  numerator
+ * @param[in] inv  multiplicative inverse of denominator (bitshifted by 17)
  *
- * @pre val <= 536870911999 ((2**32 * 125) -1)
- *
- * @param[in]   val     dividend
- * @return      (val / 125)
+ * @return         result
  */
-static inline uint32_t div_u64_by_125(uint64_t val)
+static inline uint16_t div_u16_by_inv(const uint16_t val, const uint16_t inv)
 {
-  /* a higher value would overflow the result type */
-  assert(val <= 536870911999LLU);
+    uint32_t tmp;
 
-  uint32_t hi = val >> 32;
-  uint32_t lo = val;
-  uint32_t r = (lo >> 16) + (hi << 16);
-  uint32_t res = r / 125;
-  r = ((r % 125) << 16) + (lo & 0xFFFF);
-  res = (res << 16) + r / 125;
-  return res;
+    tmp = (uint32_t)val * (uint32_t)inv;
+    tmp >>= 17;
+
+    return (uint16_t)tmp;
 }
 
 /**
- * @brief Integer divide val by 1000000
+ * @brief   Divide 32-bit number using an inverse
  *
- * @param[in]   val     dividend
- * @return      (val / 1000000)
+ * @param[in] val  numerator
+ * @param[in] inv  multiplicative inverse of denominator (bitshifted by 33)
+ *
+ * @return         result
  */
-static inline uint64_t div_u64_by_1000000(uint64_t val)
+static inline uint32_t div_u32_by_inv(const uint32_t val, const uint32_t inv)
 {
-    return div_u64_by_15625(val) >> 6;
+    uint64_t tmp;
+
+    tmp = (uint64_t)val * (uint64_t)inv;
+    tmp >>= 33;
+
+    return (uint32_t)tmp;
 }
 
 /**
- * @brief Divide val by (15625/512)
+ * @brief   Divide 64-bit number using an inverse
  *
- * This is used to quantize a 1MHz value to the closest 32768Hz value,
- * e.g., for timers.
+ * @param[in] val  numerator
+ * @param[in] inv  multiplicative inverse of denominator (bitshifted by 65)
  *
- * The algorithm uses the modular multiplicative inverse of 15625 to use only
- * multiplication and bit shifts to perform the division.
- *
- * The result will be equal to the mathematical expression: floor((val * 512) / 15625)
- *
- * @param[in]   val     dividend
- * @return      (val / (15625/512))
+ * @return         result
  */
-static inline uint32_t div_u32_by_15625div512(uint32_t val)
+static inline uint64_t div_u64_by_inv(const uint64_t val, const uint64_t inv)
 {
-    return ((uint64_t)(val) * DIV_H_INV_15625_32) >> (DIV_H_INV_15625_SHIFT + 32 - 9);
-}
-
-/**
- * @brief Divide val by (15625/512)
- *
- * This is used to quantize a 1MHz value to the closest 32768Hz value,
- * e.g., for timers.
- *
- * @param[in]   val     dividend
- * @return      (val / (15625/512))
- */
-static inline uint64_t div_u64_by_15625div512(uint64_t val)
-{
-    /*
-     * This saves around 1400 bytes of ROM on Cortex-M platforms (both ARMv6 and
-     * ARMv7) from avoiding linking against __aeabi_uldivmod and related helpers
-     */
-    if (val > 16383999997ull) {
-        /* this would overflow 2^64 in the multiplication that follows, need to
-         * use the long version */
-        return (_div_mulhi64(DIV_H_INV_15625_64, val) >> (DIV_H_INV_15625_SHIFT - 9));
-    }
-    return (val * DIV_H_INV_15625_32) >> (DIV_H_INV_15625_SHIFT + 32 - 9);
-}
-
-/**
- * @brief Integer divide val by 44488
- *
- * @param[in]   val     dividend
- * @return      (val / 44488)
- */
-static inline uint32_t div_u32_by_44488(uint32_t val)
-{
-    return ((uint64_t)val * 0xBC8F1391UL) >> (15 + 32);
-}
-
-/**
- * @brief Modulo 44488
- *
- * @param[in]   val     dividend
- * @return      (val % 44488)
- */
-static inline uint32_t div_u32_mod_44488(uint32_t val)
-{
-    return val - (div_u32_by_44488(val)*44488);
+    return (_div_mulhi64(val, inv) >> 1);
 }
 
 #ifdef __cplusplus
 }
 #endif
+
 /** @} */
+
 #endif /* DIV_H */
