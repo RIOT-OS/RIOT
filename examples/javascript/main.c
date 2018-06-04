a650a43df24eba33217f5e59f3b7a010324aa4bf
Anthony <anthony.durot@inria.fr>
2018-06-04 16:22:23 +0200

    pkg/jerryscript: update Javascript example up to date

--- examples/javascript/main.c
+++ examples/javascript/main.c
@@ -31,30 +31,34 @@
 
 int js_run(const jerry_char_t *script, size_t script_size)
 {
-    jerry_value_t ret_value;
 
-    /* Initialize engine */
+    jerry_value_t parsed_code, ret_value;
+    int res = 0;
+
+    /* Initialize engine, no flags, default configuration */
+
     jerry_init(JERRY_INIT_EMPTY);
 
     /* Register the print function in the global object. */
-    jerryx_handler_register_global((const jerry_char_t *) "print", jerryx_handler_print);
+
+    jerryx_handler_register_global((const jerry_char_t *) "print",
+                                   jerryx_handler_print);
 
     /* Setup Global scope code */
-    ret_value = jerry_parse(script, script_size, false);
 
-    if (!jerry_value_has_error_flag(ret_value)) {
+    parsed_code = jerry_parse(NULL, 0, script, script_size, JERRY_PARSE_NO_OPTS);
+
+    if (!jerry_value_is_error(parsed_code)) {
         /* Execute the parsed source code in the Global scope */
-        ret_value = jerry_run(ret_value);
+        ret_value = jerry_run(parsed_code);
+        if (jerry_value_is_error(ret_value)) {
+            printf("js_run(): Script Error!");
+            res = -1;
+        }
+        jerry_release_value(ret_value);
     }
 
-    int res = 0;
-
-    if (jerry_value_has_error_flag(ret_value)) {
-        printf("js_run(): Script Error!");
-        res = -1;
-    }
-
-    jerry_release_value(ret_value);
+    jerry_release_value(parsed_code);
 
     /* Cleanup engine */
     jerry_cleanup();
