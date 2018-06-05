bfe988486d534b9fb13a14c592463798934f4262
Josarn <josuaarndt@live.de>
2018-06-05 13:09:40 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! tests/xtimer_usleep_short: add expected runtime

--- tests/xtimer_usleep_short/main.c
+++ tests/xtimer_usleep_short/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2017 HAW-Hamburg
+ *               2018 Josua Arndt
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -14,6 +15,7 @@
  * @brief       xtimer_usleep_short test application
  *
  * @author      Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
+ * @author      Josua Arndt <jarndt@ias.rwth-aachen.de>
  * @}
  */
 #include <stdio.h>
@@ -32,39 +34,41 @@
 int main(void)
 {
     xtimer_sleep(3);
-    printf("This test will call xtimer_usleep for values from %d down to %d\n",
+    printf("This test will call xtimer_usleep for values from %d us down to %d us.\n",
            TEST_USLEEP_MAX, TEST_USLEEP_MIN);
 
-    printf("Expected delay margin is %d us\n",
+    printf("Expected delay margin is %d us.\n",
            TEST_XTIMER_USLEEP_SHORT_SLEEP_MARGIN_US);
 
     uint32_t test_time = 0, sleeping_time = 0, margin_fault = 0;
 
     for (int i = TEST_USLEEP_MAX; i >= TEST_USLEEP_MIN; i--) {
-        printf("going to sleep %d us\n", i);
+
         uint32_t start = xtimer_now_usec();
         xtimer_usleep(i);
         uint32_t slept = xtimer_now_usec() - start;
-        printf("Slept for      %" PRIu32 " us\n", slept);
 
-        if (slept < (unsigned int)i) {
-            puts("Timeout to short");
+        if (slept < (unsigned int)i || 
+            slept > (unsigned int)i + TEST_XTIMER_USLEEP_SHORT_SLEEP_MARGIN_US) {
+            printf("ERROR: Slept for %" PRIu32 " us not in range [%d, %d] us.\n",
+                   slept, i, i + TEST_XTIMER_USLEEP_SHORT_SLEEP_MARGIN_US);
             ++margin_fault;
         }
-        else if (slept > (unsigned int)i + TEST_XTIMER_USLEEP_SHORT_SLEEP_MARGIN_US) {
-            puts("Timeout longer than expected margin.");
-            ++margin_fault;
+        else {
+            printf("OK: Slept for %" PRIu32 " us not in range [%d, %d] us.\n",
+                   slept, i, i + TEST_XTIMER_USLEEP_SHORT_SLEEP_MARGIN_US);
         }
 
         sleeping_time += slept;
         test_time += i;
     }
 
-    printf("Slept for %" PRIu32 " us expected %" PRIu32 " us\n",
+    printf("Slept for %" PRIu32 " us expected %" PRIu32 " us.\n",
            sleeping_time, test_time);
 
     if (margin_fault != 0) {
-        printf("Sleep delay margin was not kept for %" PRIu32 " times\n", margin_fault);
+        printf("Sleep delay margin was not kept for %" PRIu32 " times.\n",
+                margin_fault);
         puts("[FAILED]");
         return 1;
     }
--- tests/xtimer_usleep_short/tests/01-run.py
+++ tests/xtimer_usleep_short/tests/01-run.py
@@ -12,29 +12,30 @@ import pexpect
 
 
 def testfunc(child):
-    child.expect(u"This test will call xtimer_usleep for values from (\d+) down to (\d+)")
+    child.expect("This test will call xtimer_usleep for values from (\d+) us down to (\d+) us.")
     sleep_max = int(child.match.group(1))
     sleep_min = int(child.match.group(2))
+    delay = sleep_max - sleep_min
 
-    i = sleep_max - sleep_min
+    child.expect("Expected delay margin is (\d+) us.")
+    sleep_margin = int(child.match.group(1))
 
-    while (i >= 0):
+    for delay in range(sleep_max, sleep_min, -1):
         try:
-            child.expect(u"going to sleep (\d+) us", timeout=3)
-            sleep_exp = int(child.match.group(1))
-
-            child.expect(u"Slept for      (\d+) us", timeout=3)
+            child.expect("\w+: Slept for \d+ us not in range \[\d+, \d+\] us.",
+                         timeout=20)
 
         except pexpect.TIMEOUT:
-            print("xtimer stuck when trying to sleep %d us" % (sleep_exp))
+            print("xtimer stuck when trying to sleep %d us or failed.".format(delay))
             print("[FAILED]")
-            break
-        i = i - 1
+            exit(1)
 
-    child.expect(u"Slept for (\d+) us expected (\d+) us")
-
-    child.expect(u"[SUCCESS]", timeout=3)
-
+    child.expect("Slept for \d+ us expected \d+ us.")
+    child.expect("((Sleep delay margin was not kept for \d+ times.)|(\[SUCCESS\]))")
+    msg = child.match.group(1)
+    if msg != "[SUCCESS]" :
+        child.expect("[FAILED]")
+        exit(1)
 
 if __name__ == "__main__":
     sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
