b36afc3964fcba1fbbd5e45d2f004973fbbb684e
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-06-14 23:32:04 +0200

    make: Clean up WERROR handling

--- makefiles/cflags.inc.mk
+++ makefiles/cflags.inc.mk
@@ -57,8 +57,8 @@ endif
 # Forbid common symbols to prevent accidental aliasing.
 CFLAGS += -fno-common
 
-# Enable all default warnings
-CFLAGS += -Wall
+# Enable all default warnings and all extra warnings
+CFLAGS += -Wall -Wextra
 
 # Warn if a user-supplied include directory does not exist.
 CFLAGS += -Wmissing-include-dirs
