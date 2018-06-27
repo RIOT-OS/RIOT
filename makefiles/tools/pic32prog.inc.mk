be4569e271e29f673b03f96737d28ad76446bd99
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-06-27 10:53:09 +0200

    pic32prog.inc.mk: add udev rule for pic32 programmer
    Copy the udev rule from dist/tools/pic32prog/doc.md

--- makefiles/tools/pic32prog.inc.mk
+++ makefiles/tools/pic32prog.inc.mk
@@ -15,6 +15,18 @@
 # Should be done from a Windows computer/virtual machine as described here
 #
 # https://github.com/RIOT-OS/RIOT/blob/master/dist/tools/pic32prog/doc.md
+#
+#
+# Udev rule
+# ---------
+#
+# Add yourself to the `plugdev` group, add the following `udev` rule to
+# `/etc/udev/rules.d/26-microchip.rules` and reboot.
+#
+# ```
+# # Adapted from http://en.microstickplus.com/mplabx-on-linux
+# ATTR{idVendor}=="04d8", MODE="664", GROUP="plugdev"
+# ```
 
 RIOT_PIC32PROG = $(RIOTTOOLS)/pic32prog/pic32prog
 PIC32PROG ?= $(RIOT_PIC32PROG)
