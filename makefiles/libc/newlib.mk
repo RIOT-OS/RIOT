4c85cb4a53c2531dabba3187bda651edf13b4d5e
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-07-03 09:44:44 +0200

    newlib.mk: add newlib.h helper function
    Add a function that returns the absolute path to directories if they contain
    newlib.h

--- makefiles/libc/newlib.mk
+++ makefiles/libc/newlib.mk
@@ -28,6 +28,10 @@ export LINKFLAGS += -lc
 
 # Search for Newlib include directories
 
+# Function that returns absolute path to directories that contain newlib.h
+# It returns the directory without trailing slash
+dir_contains_newlib_h = $(realpath $(dir $(wildcard $(addsuffix /newlib.h, $(1)))))
+
 # Try to search for newlib in the standard search path of the compiler for includes
 ifeq (,$(NEWLIB_INCLUDE_DIR))
   COMPILER_INCLUDE_PATHS := $(shell $(PREFIX)gcc -v -x c -E /dev/null 2>&1 | \
