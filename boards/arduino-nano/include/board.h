f4ce87c13e287e20bf7f7b7f2b26b5fd10b47a4c
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2018-07-04 22:26:10 +0200

    boards: Add support for the Arduino Nano
    The Arduino Nano board is the cheapest member of the Arduino family and used the
    same MCU as the Arduino Uno. It differs in the form factor (the Nano is much
    smaller), it uses an integrated FT232RL TTL adapter instead of an ATmega16u2 to
    provide access to the serial console via USB, and it uses a different
    bootloader (which occupies 2 KiB of the 32 KiB flash instead of 0.5 KiB occupied
    on the Arduino Uno). This commit mostly copy pastes code from the Arduino Uno.

---
+++ boards/arduino-nano/include/board.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017   Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_arduino-nano
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the Arduino Uno board
+ *
+ * @author      Martine Lenders <m.lenders@fu-berlin.de>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "board_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
