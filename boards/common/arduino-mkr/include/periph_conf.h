771d25ed1ca76d9c5ae7ade3adf99a3c7b6ab28d
smlng <s@mlng.net>
2018-07-12 13:32:33 +0200

    boards/arduino-mkr*: fix order in struct
            C++ requires to init struct attributes in order of there definition
            in the respective struct.

--- boards/common/arduino-mkr/include/periph_conf.h
+++ boards/common/arduino-mkr/include/periph_conf.h
@@ -218,8 +218,8 @@ static const i2c_conf_t i2c_config[] = {
     {
         .dev      = &(SERCOM0->I2CM),
         .speed    = I2C_SPEED_NORMAL,
-        .sda_pin  = GPIO_PIN(PA, 8),
         .scl_pin  = GPIO_PIN(PA, 9),
+        .sda_pin  = GPIO_PIN(PA, 8),
         .mux      = GPIO_MUX_C,
         .gclk_src = GCLK_CLKCTRL_GEN_GCLK0,
         .flags    = I2C_FLAG_NONE
