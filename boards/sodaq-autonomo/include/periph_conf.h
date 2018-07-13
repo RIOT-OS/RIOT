0da00cdf4a6628b740b014a058ca1c048a465c96
smlng <s@mlng.net>
2018-07-13 11:21:34 +0200

    boards/sodaq-autonomo: fix attr order in i2c_config

--- boards/sodaq-autonomo/include/periph_conf.h
+++ boards/sodaq-autonomo/include/periph_conf.h
@@ -217,8 +217,8 @@ static const i2c_conf_t i2c_config[] = {
     {
         .dev      = &(SERCOM2->I2CM),
         .speed    = I2C_SPEED_NORMAL,
-        .sda_pin  = GPIO_PIN(PA, 12),
         .scl_pin  = GPIO_PIN(PA, 13),
+        .sda_pin  = GPIO_PIN(PA, 12),
         .mux      = GPIO_MUX_C,
         .gclk_src = GCLK_CLKCTRL_GEN_GCLK0,
         .flags    = I2C_FLAG_NONE
