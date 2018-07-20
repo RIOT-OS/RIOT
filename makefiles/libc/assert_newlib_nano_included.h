87aba27955a2408a3be14eee12e0b7d35885d9d5
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-07-20 09:05:06 +0200

    Revert "newlib.mk: add optional sanity check for newlib-nano header"
    This reverts commit 0e14df5406202107ad8d9bfe4541f64a888d0151.

--- makefiles/libc/assert_newlib_nano_included.h
+++
@@ -1,5 +0,0 @@
-/* Warn if newlib_nano is requested but not actually used */
-#include <newlib.h>
-#ifndef _NANO_FORMATTED_IO
-#warning newlib-nano requested but not included by the build system
-#endif
