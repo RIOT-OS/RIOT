9720cd857d750fc1e3b926345df49b2dca1040e9
Martine Lenders <m.lenders@fu-berlin.de>
2018-08-06 14:47:43 +0200

    gnrc_ipv6_nib: include SLAAC debug addr_str into #ifdef

--- sys/net/gnrc/network_layer/ipv6/nib/_nib-slaac.c
+++ sys/net/gnrc/network_layer/ipv6/nib/_nib-slaac.c
@@ -24,9 +24,9 @@
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
 
+#if GNRC_IPV6_NIB_CONF_6LN || GNRC_IPV6_NIB_CONF_SLAAC
 static char addr_str[IPV6_ADDR_MAX_STR_LEN];
 
-#if GNRC_IPV6_NIB_CONF_6LN || GNRC_IPV6_NIB_CONF_SLAAC
 void _auto_configure_addr(gnrc_netif_t *netif, const ipv6_addr_t *pfx,
                           uint8_t pfx_len)
 {
