0972082b8ae03eb05fd8f1f6caf0aa564644a8df
Martine Lenders <m.lenders@fu-berlin.de>
2018-08-07 18:42:38 +0200

    make: provide toolchain support for buildtests

--- makefiles/buildtests.inc.mk
+++ makefiles/buildtests.inc.mk
@@ -9,17 +9,19 @@ buildtest:
 	@ \
 	RESULT=true ; \
 	for board in $(BOARDS); do \
-		$(COLOR_ECHO) -n "Building for $$board ... " ; \
-		BOARD=$${board} RIOT_CI_BUILD=1 RIOT_VERSION_OVERRIDE=buildtest \
-			$(MAKE) clean all -j $(NPROC) $(BUILDTEST_MAKE_REDIRECT); \
-		RES=$$? ; \
-		if [ $$RES -eq 0 ]; then \
-			$(COLOR_ECHO) "$(COLOR_GREEN)success.$(COLOR_RESET)" ; \
-		else \
-			$(COLOR_ECHO) "$(COLOR_RED)failed!$(COLOR_RESET)" ; \
-			RESULT=false ; \
-		fi ; \
-		$(MAKE) clean-intermediates >/dev/null 2>&1 || true; \
+		if BOARD=$${board} $(MAKE) check-toolchain-supported > /dev/null 2>&1; then \
+			$(COLOR_ECHO) -n "Building for $$board ... " ; \
+			BOARD=$${board} RIOT_CI_BUILD=1 RIOT_VERSION_OVERRIDE=buildtest \
+				$(MAKE) clean all -j $(NPROC) $(BUILDTEST_MAKE_REDIRECT); \
+			RES=$$? ; \
+			if [ $$RES -eq 0 ]; then \
+				$(COLOR_ECHO) "$(COLOR_GREEN)success.$(COLOR_RESET)" ; \
+			else \
+				$(COLOR_ECHO) "$(COLOR_RED)failed!$(COLOR_RESET)" ; \
+				RESULT=false ; \
+			fi ; \
+			$(MAKE) clean-intermediates >/dev/null 2>&1 || true; \
+		fi; \
 	done ; \
 	$${RESULT}
 endif # BUILD_IN_DOCKER
