b8d96103163840d04901b4dc0ad4e43c6575904f
smlng <s@mlng.net>
2018-08-17 17:04:26 +0200

    netstats_l2: remove obsolete struct from netdev_t

--- drivers/include/net/netdev.h
+++ drivers/include/net/netdev.h
@@ -200,9 +200,6 @@ extern "C" {
 #include "iolist.h"
 #include "net/netopt.h"
 
-#ifdef MODULE_NETSTATS_L2
-#include "net/netstats.h"
-#endif
 #ifdef MODULE_L2FILTER
 #include "net/l2filter.h"
 #endif
@@ -280,9 +277,6 @@ struct netdev {
     void* context;                          /**< ptr to network stack context */
 #ifdef MODULE_NETDEV_LAYER
     netdev_t *lower;                        /**< ptr to the lower netdev layer */
-#endif
-#ifdef MODULE_NETSTATS_L2
-    netstats_t stats;                       /**< transceiver's statistics */
 #endif
 #ifdef MODULE_L2FILTER
     l2filter_t filter[L2FILTER_LISTSIZE];   /**< link layer address filters */
