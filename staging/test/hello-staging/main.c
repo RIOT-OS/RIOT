47b2fc4b72326119f78a09843cdd5506009af0bf
Kaspar Schleiser <kaspar@schleiser.de>
2018-08-31 00:52:06 +0200

    staging: add staging/test/hello-world test application

---
+++ staging/test/hello-staging/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2018 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     staging
+ * @{
+ *
+ * @file
+ * @brief       Hello Staging application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello Staging!\n");
+
+    return 0;
+}
