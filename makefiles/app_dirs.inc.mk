b4fdd2b1d6ca9662225c45db034754ea3e4056d2
Kaspar Schleiser <kaspar@schleiser.de>
2018-08-31 00:51:11 +0200

    makefiles: application_dirs: include staging/ applications if enabled

--- makefiles/app_dirs.inc.mk
+++ makefiles/app_dirs.inc.mk
@@ -3,14 +3,22 @@
 # fallback so empty RIOTBASE won't lead to "/examples/"
 RIOTBASE ?= .
 
+APPLICATION_DIR_PATTERNS += \
+	$(RIOTBASE)/bootloaders/*/Makefile \
+	$(RIOTBASE)/examples/*/Makefile \
+	$(RIOTBASE)/tests/*/Makefile
+
+ifeq (1, $(ENABLE_STAGING))
+  include $(RIOTBASE)/staging/staging.mk
+  APPLICATION_DIR_PATTERNS += $(STAGING_APPLICATION_MAKEFILES)
+endif
+
 # 1. use wildcard to find Makefiles
 # 2. use patsubst to drop trailing "/"
 # 3. use patsubst to drop possible leading "./"
 # 4. sort
 APPLICATION_DIRS := $(sort $(patsubst ./%,%,$(patsubst %/,%,$(dir $(wildcard \
-	$(RIOTBASE)/bootloaders/*/Makefile \
-	$(RIOTBASE)/examples/*/Makefile    \
-	$(RIOTBASE)/tests/*/Makefile       \
+	$(APPLICATION_DIR_PATTERNS) \
 	)))))
 
 info-applications:
