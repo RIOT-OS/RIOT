b17f43a3f2e00edf53160de80237707a8c61b21f
smlng <s@mlng.net>
2018-09-07 14:02:13 +0200

    cpu/esp8266: fix periph/timer provides timer_set

--- cpu/esp8266/include/periph_cpu.h
+++ cpu/esp8266/include/periph_cpu.h
@@ -161,6 +161,11 @@ typedef enum {
 #endif /* MODULE_PERIPH_SPI */
 /** @} */
 
+/**
+ * @brief   Prevent shared timer functions from being used
+ */
+#define PERIPH_TIMER_PROVIDES_SET
+
 #ifdef __cplusplus
 }
 #endif
