8b1fbaf286a5e2e83eec8f3f3a5aee4af8bff9dd
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-09-24 09:17:00 +0200

    mips32r2_common: Conditional some syscalls for MIPS newlib
    _init, _fini, _exit, _sbrk_r are provided by the toolchain.

--- cpu/mips32r2_common/newlib_syscalls_mips_uhi/syscalls.c
+++ cpu/mips32r2_common/newlib_syscalls_mips_uhi/syscalls.c
@@ -46,6 +46,8 @@ extern char _sheap; /* start of the heap */
 extern char _eheap; /* end of the heap */
 char *heap_top = &_sheap + 4;
 
+/* MIPS newlib crt implements _init, _fini, and _exit, and manages the heap */
+#ifndef __mips__
 /**
  * @brief Free resources on NewLib de-initialization, not used for RIOT
  */
@@ -97,6 +99,8 @@ void *_sbrk_r(struct _reent *r, ptrdiff_t incr)
     irq_restore(state);
     return res;
 }
+
+#endif /*__mips__*/
 
 /**
 * @brief Get the process-ID of the current thread
