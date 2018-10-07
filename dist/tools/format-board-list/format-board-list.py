5ec259028da9d3f834a13ab55731b60f9443ac0f
Lucas Jenss <lucas@x3ro.de>
2018-10-07 20:34:08 +0200

    fixup! Add support for auto-formatting and validating board lists in Makefiles

--- dist/tools/format-board-list/format-board-list.py
+++ dist/tools/format-board-list/format-board-list.py
@@ -2,20 +2,35 @@
 import os
 import sys
 import subprocess
+import re
 from pprint import pprint
 from tempfile import NamedTemporaryFile
 import shutil
+
+TARGET_VARIABLES = ['BOARD_INSUFFICIENT_MEMORY', 'BOARD_BLACKLIST', 'BOARD_WHITELIST']
 
 def chdir_to_riot_root():
 	SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 	RIOT_DIR = os.path.realpath(SCRIPT_DIR + "/../../..")
 	os.chdir(RIOT_DIR)
 
-TARGET_VARIABLE = 'BOARD_INSUFFICIENT_MEMORY := '
 
 class BoardList:
-	def __init__(self):
+	def __init__(self, first_line_of_board_list):
 		self.boards = []
+		self.target_variable = None
+		self.variable_operation = None
+		self.regex = re.compile('([A-Z_]+)\\s*([\\+:]?=)(.+)')
+		self.parse_first_line(first_line_of_board_list)
+
+	def parse_first_line(self, line):
+		m = self.regex.match(line)
+		assert m is not None, "Regex should match '%s'" % (line)
+		assert m.group(1) in TARGET_VARIABLES, "Match '%s' should be in TARGET_VARIABLES" % (m.group(1))
+
+		self.target_variable = m.group(1)
+		self.variable_operation = m.group(2)
+		self.add_boards_from_line(m.group(3))
 
 	def add_boards_from_line(self, line):
 		boards = self._extract_boards(line)
@@ -29,13 +44,14 @@ class BoardList:
 		return boards
 
 	def __str__(self):
-		spacing = len(TARGET_VARIABLE) * ' '
-		return TARGET_VARIABLE + (" \\\n" + spacing).join(self.boards)
+		prefix = "%s %s " % (self.target_variable, self.variable_operation)
+		spacing = len(prefix) * ' '
+		return prefix + (" \\\n" + spacing).join(self.boards)
 
 
-def get_affected_filenames():
+def get_affected_filenames(target_variable):
 	try:
-		output = subprocess.check_output(["git", "grep", TARGET_VARIABLE]).decode("utf-8").strip()
+		output = subprocess.check_output(["git", "grep", target_variable]).decode("utf-8").strip()
 	except subprocess.CalledProcessError as e:
 		print("broke:\n", e.output) # TODO
 
@@ -44,40 +60,39 @@ def get_affected_filenames():
 	filenames = list(filter(lambda filename: os.path.basename(filename) == 'Makefile', filenames))
 	return filenames
 
+
 def has_more_boards(line):
 	return line.rstrip().endswith('\\')
 
-def format_makefile_board_list(source_file, target_file):
-	board_list = BoardList()
-
-	# Write to the temp file unmodified until we reach the board list
-	while True:
-		current_line = source_file.readline()
-		if TARGET_VARIABLE in current_line:
-			break;
-		target_file.write(current_line)
-
-	# Extract the board list
-	current_line = current_line[len(TARGET_VARIABLE):]
-	board_list.add_boards_from_line(current_line)
-
-	while has_more_boards(current_line):
-		current_line = source_file.readline()
-		board_list.add_boards_from_line(current_line)
-
-	# Write the board list to formatted file
-	target_file.write(str(board_list))
-	target_file.write('\n')
-
-	# Write the rest of the file unmodified
+def consume_until_board_list(source_file, target_file):
+	# Write to the temp file unmodified until we reach a board list or EOF
 	current_line = source_file.readline()
 	while current_line is not '':
+		if any(target_variable in current_line for target_variable in TARGET_VARIABLES):
+			return BoardList(current_line), current_line
+
 		target_file.write(current_line)
 		current_line = source_file.readline()
 
-	target_file.flush()
-	return board_list
+	return None, current_line
 
+def format_makefile_board_lists(source_file, target_file):
+
+	while True:
+		board_list, current_line = consume_until_board_list(source_file, target_file)
+
+		if board_list is None:
+			break
+
+		while has_more_boards(current_line):
+			current_line = source_file.readline()
+			board_list.add_boards_from_line(current_line)
+
+		# Write the board list to formatted file
+		target_file.write(str(board_list))
+		target_file.write('\n')
+
+	target_file.flush()
 
 
 if len(sys.argv) != 2 or sys.argv[1] not in ['validate', 'format']:
@@ -90,12 +105,22 @@ if len(sys.argv) != 2 or sys.argv[1] not in ['validate', 'format']:
 
 mode = sys.argv[1]
 chdir_to_riot_root()
-filenames = get_affected_filenames()
+
+filenames = []
+for target_variable in TARGET_VARIABLES:
+	filenames.extend(get_affected_filenames(target_variable))
+filenames = set(filenames)
+
 formatting_errors = False
 for filename in filenames:
 	source_file = open(filename, 'r', encoding="utf-8")
 	temp_file = NamedTemporaryFile(mode='w+', prefix='formatted-makefile-', encoding="utf-8")
-	format_makefile_board_list(source_file, temp_file)
+
+	try:
+		format_makefile_board_lists(source_file, temp_file)
+	except AssertionError as e:
+		print("Error in file %s: %s" % (source_file, e))
+		sys.exit(1)
 
 	if mode == 'validate':
 		diff = subprocess.Popen(['diff', '-u', filename, temp_file.name], stdout=subprocess.PIPE)
