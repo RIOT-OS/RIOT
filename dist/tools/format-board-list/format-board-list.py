5803e5cffad22dc9f068cba20c6fc583299e1723
Lucas Jenss <lucas@x3ro.de>
2018-10-09 12:01:23 +0200

    fixup! Add support for auto-formatting and validating board lists in Makefiles

--- dist/tools/format-board-list/format-board-list.py
+++ dist/tools/format-board-list/format-board-list.py
@@ -7,30 +7,41 @@ from pprint import pprint
 from tempfile import NamedTemporaryFile
 import shutil
 
-TARGET_VARIABLES = ['BOARD_INSUFFICIENT_MEMORY', 'BOARD_BLACKLIST', 'BOARD_WHITELIST']
+
+MAKEFILE_LAST_LINE = 'include $(RIOTBASE)/Makefile.include'
 
 def chdir_to_riot_root():
 	SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 	RIOT_DIR = os.path.realpath(SCRIPT_DIR + "/../../..")
 	os.chdir(RIOT_DIR)
 
+class BoardListVariable:
+	def __init__(self, name):
+		self.name = name
+		self.regex = re.compile('%s\\s*([\\+:]?=)(.+)' % (self.name))
+
+	def match(self, line):
+		return self.regex.match(line)
+
+TARGET_VARIABLES = [
+	BoardListVariable('BOARD_INSUFFICIENT_MEMORY'),
+	BoardListVariable('BOARD_BLACKLIST'),
+	BoardListVariable('BOARD_WHITELIST')
+]
 
 class BoardList:
-	def __init__(self, first_line_of_board_list):
+	def __init__(self, matched_variable, first_line_of_board_list):
 		self.boards = []
-		self.target_variable = None
+		self.matched_variable = matched_variable
 		self.variable_operation = None
-		self.regex = re.compile('([A-Z_]+)\\s*([\\+:]?=)(.+)')
 		self.parse_first_line(first_line_of_board_list)
 
 	def parse_first_line(self, line):
-		m = self.regex.match(line)
+		m = self.matched_variable.match(line)
 		assert m is not None, "Regex should match '%s'" % (line)
-		assert m.group(1) in TARGET_VARIABLES, "Match '%s' should be in TARGET_VARIABLES" % (m.group(1))
 
-		self.target_variable = m.group(1)
-		self.variable_operation = m.group(2)
-		self.add_boards_from_line(m.group(3))
+		self.variable_operation = m.group(1)
+		self.add_boards_from_line(m.group(2))
 
 	def add_boards_from_line(self, line):
 		boards = self._extract_boards(line)
@@ -45,14 +56,14 @@ class BoardList:
 
 	def __str__(self):
 		self.boards.sort()
-		prefix = "%s %s " % (self.target_variable, self.variable_operation)
+		prefix = "%s %s " % (self.matched_variable.name, self.variable_operation)
 		spacing = len(prefix) * ' '
 		return prefix + (" \\\n" + spacing).join(self.boards)
 
 
 def get_affected_filenames(target_variable):
 	try:
-		output = subprocess.check_output(["git", "grep", target_variable]).decode("utf-8").strip()
+		output = subprocess.check_output(["git", "grep", target_variable.name]).decode("utf-8").strip()
 	except subprocess.CalledProcessError as e:
 		print("broke:\n", e.output) # TODO
 
@@ -66,22 +77,50 @@ def has_more_boards(line):
 	return line.rstrip().endswith('\\')
 
 def consume_until_board_list(source_file, target_file):
-	# Write to the temp file unmodified until we reach a board list or EOF
-	current_line = source_file.readline()
-	while current_line is not '':
-		if any(target_variable in current_line for target_variable in TARGET_VARIABLES):
-			return BoardList(current_line), current_line
+	current_line = 'x'
+	previous_line = ''
 
-		target_file.write(current_line)
+	# When we're moving things around within the Makefile, such as a board list
+	# or the base Makefile include, we need to skip newlines afterwards, since
+	# otherwise we'd have several newlines in a row, e.g.
+	#
+	# newline
+	# line that is moved to the end of the file
+	# newline
+	#
+	skip_newlines = True
+
+	# Write to the target_file unmodified until we reach a board list or EOF
+	while current_line is not '':
+		previous_line = current_line
 		current_line = source_file.readline()
 
-	return None, current_line
+		if skip_newlines and current_line.strip() is '':
+			continue
+
+		skip_newlines = False
+
+		if current_line.strip() == MAKEFILE_LAST_LINE:
+			current_line = '\n'
+			skip_newlines = True
+			continue
+
+		for target_variable in TARGET_VARIABLES:
+			if target_variable.match(current_line) is not None:
+				return BoardList(target_variable, current_line), current_line, previous_line
+
+		target_file.write(current_line)
+
+	return None, current_line, previous_line
 
 def format_makefile_board_lists(source_file, target_file):
+	board_lists = []
 
+	previous_line = ''
 	while True:
-		board_list, current_line = consume_until_board_list(source_file, target_file)
+		board_list, current_line, previous_line = consume_until_board_list(source_file, target_file)
 
+		# If consume_until_board_list returns None we've reached the end of the file
 		if board_list is None:
 			break
 
@@ -89,10 +128,19 @@ def format_makefile_board_lists(source_file, target_file):
 			current_line = source_file.readline()
 			board_list.add_boards_from_line(current_line)
 
-		# Write the board list to formatted file
-		target_file.write(str(board_list))
+		board_lists.append(board_list)
+		previous_line = current_line
+
+	if previous_line.strip() != '':
 		target_file.write('\n')
 
+	# Write the board list to formatted file
+	for board_list in board_lists:
+		target_file.write(str(board_list))
+		target_file.write('\n\n')
+
+	target_file.write(MAKEFILE_LAST_LINE)
+	target_file.write('\n')
 	target_file.flush()
 
 
