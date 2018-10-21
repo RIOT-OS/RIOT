f4a4f5d0b1a51624e0b8c65c126ac53baf5106d7
Koen Zandberg <koen@bergzand.net>
2018-10-21 21:34:29 +0200

    chacha20poly1305: Provide a test based on the rfc
    Add a test case to the crypto tests testing the chacha20poly1305
    algorithm with the test vectors from rfc 8439.

--- tests/unittests/tests-crypto/tests-crypto.h
+++ tests/unittests/tests-crypto/tests-crypto.h
@@ -48,6 +48,8 @@ Test *tests_crypto_chacha_tests(void);
 
 Test *tests_crypto_poly1305_tests(void);
 
+Test *tests_crypto_chacha20poly1305_tests(void);
+
 static inline int compare(const uint8_t *a, const uint8_t *b, uint8_t len)
 {
     int result = 1;
