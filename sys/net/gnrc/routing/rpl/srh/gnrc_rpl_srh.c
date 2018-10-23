d8317580df6798438cfc70b6c449626078433bb3
Martine Lenders <m.lenders@fu-berlin.de>
2018-10-23 15:05:38 +0200

    gnrc_rpl_srh: uncrustify code

--- sys/net/gnrc/routing/rpl/srh/gnrc_rpl_srh.c
+++ sys/net/gnrc/routing/rpl/srh/gnrc_rpl_srh.c
@@ -75,23 +75,28 @@ int gnrc_rpl_srh_process(ipv6_hdr_t *ipv6, gnrc_rpl_srh_t *rh)
 
     assert(rh->seg_left > 0);
     num_addr = (((rh->len * 8) - GNRC_RPL_SRH_PADDING(rh->pad_resv) -
-               (16 - GNRC_RPL_SRH_COMPRE(rh->compr))) /
-               (16 - GNRC_RPL_SRH_COMPRI(rh->compr))) + 1;
+                 (16 - GNRC_RPL_SRH_COMPRE(rh->compr))) /
+                (16 - GNRC_RPL_SRH_COMPRI(rh->compr))) + 1;
 
     DEBUG("RPL SRH: %u addresses in the routing header\n", (unsigned) num_addr);
 
     if (rh->seg_left > num_addr) {
-        DEBUG("RPL SRH: number of segments left > number of addresses - discard\n");
+        DEBUG("RPL SRH: number of segments left > number of addresses - "
+              "discard\n");
         /* TODO ICMP Parameter Problem - Code 0 */
         return GNRC_IPV6_EXT_RH_ERROR;
     }
 
     current_pos = num_addr - new_seg_left;
-    pref_elided = new_seg_left ? GNRC_RPL_SRH_COMPRI(rh->compr) : GNRC_RPL_SRH_COMPRE(rh->compr);
+    pref_elided = (new_seg_left)
+                ? GNRC_RPL_SRH_COMPRI(rh->compr)
+                : GNRC_RPL_SRH_COMPRE(rh->compr);
     compri_addr_len = sizeof(ipv6_addr_t) - GNRC_RPL_SRH_COMPRI(rh->compr);
     addr_len = sizeof(ipv6_addr_t) - pref_elided;
     memcpy(&addr, &ipv6->dst, pref_elided);
-    memcpy(&addr.u8[pref_elided], &addr_vec[(current_pos - 1) * compri_addr_len], addr_len);
+    memcpy(&addr.u8[pref_elided],
+           &addr_vec[(current_pos - 1) * compri_addr_len],
+           addr_len);
 
     if (ipv6_addr_is_multicast(&ipv6->dst) || ipv6_addr_is_multicast(&addr)) {
         DEBUG("RPL SRH: found a multicast address - discard\n");
@@ -105,7 +110,8 @@ int gnrc_rpl_srh_process(ipv6_hdr_t *ipv6, gnrc_rpl_srh_t *rh)
         return GNRC_IPV6_EXT_RH_ERROR;
     }
     rh->seg_left = new_seg_left;
-    memcpy(&addr_vec[(current_pos - 1) * compri_addr_len], &ipv6->dst.u8[pref_elided], addr_len);
+    memcpy(&addr_vec[(current_pos - 1) * compri_addr_len],
+           &ipv6->dst.u8[pref_elided], addr_len);
 
     DEBUG("RPL SRH: Next hop: %s at position %d\n",
           ipv6_addr_to_str(addr_str, &addr, sizeof(addr_str)), current_pos);
