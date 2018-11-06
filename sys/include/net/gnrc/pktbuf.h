f2760c033c5f332be076b25aa212aca4007c3d65
Martine Lenders <m.lenders@fu-berlin.de>
2018-11-06 15:43:43 +0100

    gnrc_pktbuf: deprecate gnrc_pktbuf_duplicate_upto

--- sys/include/net/gnrc/pktbuf.h
+++ sys/include/net/gnrc/pktbuf.h
@@ -285,6 +285,11 @@ gnrc_pktsnip_t *gnrc_pktbuf_reverse_snips(gnrc_pktsnip_t *pkt);
  *
  *        The original snip is keeped as is except `users` decremented.
  *
+ * @deprecated  This function breaks the abstraction of `gnrc_pktbuf` and its
+ *              only user within the RIOT code base `gnrc_ipv6_ext` is going to
+ *              be reworked so it isn't needed anymore.
+ *              It will be removed after the 2019.04 release.
+ *
  * @param[in,out] pkt   The snip to duplicate.
  * @param[in]     type  The type of snip to stop duplication.
  *
