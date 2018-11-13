a3d6640e257f89acc1cd98f156f3b238deb47b3f
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2018-11-13 15:50:29 +0100

    squash contikimac update timing parameters for better reliability

--- sys/net/link_layer/contikimac/contikimac_params_BPSK40.c
+++ sys/net/link_layer/contikimac/contikimac_params_BPSK40.c
@@ -35,7 +35,7 @@ const contikimac_params_t contikimac_params_BPSK40 = {
     .inter_packet_interval = ACK_TIMEOUT,               /* T_i = Ack timeout */
     .after_ed_scan_timeout = LONGEST_FRAME_TIME + 1000, /* > T_l */
     .after_ed_scan_interval = (ACK_TIMEOUT * 3) / 4,    /* < T_i */
-    .listen_timeout = ACK_TIMEOUT + 1000,               /* > T_i */
-    .rx_timeout = LONGEST_FRAME_TIME + 500,             /* > T_l */
-    .cca_count_max = 3,                                 /* = n_c */
+    .listen_timeout = 2 * ACK_TIMEOUT,                  /* > T_i */
+    .rx_timeout = LONGEST_FRAME_TIME + ACK_TIMEOUT,     /* > T_l */
+    .cca_count_max = 4,                                 /* = n_c */
 };
