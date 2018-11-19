b2407e458363b70b5caa6dd1e3be71846eccc4f3
Martine Lenders <m.lenders@fu-berlin.de>
2018-11-19 16:46:45 +0100

    dist/pythonlibs: expose functions to setup/teardown pexpect spawn
    This allows for re-usability of those functions in other contexts.

--- dist/pythonlibs/testrunner/__init__.py
+++ dist/pythonlibs/testrunner/__init__.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
+# Copyright (C) 2018-19 Freie Universität Berlin
+#               2017 Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
 #               2016 Kaspar Schleiser <kaspar@schleiser.de>
 #               2014 Martine Lenders <mlenders@inf.fu-berlin.de>
 #
@@ -7,51 +8,16 @@
 # directory for more details.
 
 import os
-import signal
 import sys
-import subprocess
-import time
-from traceback import extract_tb, print_tb
-
+from traceback import print_tb
 import pexpect
 
-PEXPECT_PATH = os.path.dirname(pexpect.__file__)
-RIOTBASE = (os.environ.get('RIOTBASE') or
-            os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                         "..", "..", "..")))
-
-# Setting an empty 'TESTRUNNER_START_DELAY' environment variable use the
-# default value (3)
-MAKE_TERM_STARTED_DELAY = int(os.environ.get('TESTRUNNER_START_DELAY') or 3)
-
-
-def list_until(l, cond):
-    return l[:([i for i, e in enumerate(l) if cond(e)][0])]
-
-
-def find_exc_origin(exc_info):
-    pos = list_until(extract_tb(exc_info),
-                     lambda frame: frame[0].startswith(PEXPECT_PATH)
-                     )[-1]
-    return (pos[3], os.path.relpath(os.path.abspath(pos[0]), RIOTBASE), pos[1])
+from .spawn import find_exc_origin, setup_child, teardown_child
 
 
 def run(testfunc, timeout=10, echo=True, traceback=False):
-    env = os.environ.copy()
-    child = pexpect.spawnu("make term", env=env, timeout=timeout, codec_errors='replace')
-
-    # on many platforms, the termprog needs a short while to be ready...
-    time.sleep(MAKE_TERM_STARTED_DELAY)
-
-    if echo:
-        child.logfile = sys.stdout
-
-    try:
-        subprocess.check_output(('make', 'reset'), env=env,
-                                stderr=subprocess.PIPE)
-    except subprocess.CalledProcessError:
-        # make reset yields error on some boards even if successful
-        pass
+    child = setup_child(timeout, env=os.environ,
+                        logfile=sys.stdout if echo else None)
     try:
         testfunc(child)
     except pexpect.TIMEOUT:
@@ -62,16 +28,12 @@ def run(testfunc, timeout=10, echo=True, traceback=False):
         return 1
     except pexpect.EOF:
         trace = find_exc_origin(sys.exc_info()[2])
-        print("Unexpected end of file in expect script at \"%s\" (%s:%d)" % trace)
+        print("Unexpected end of file in expect script at \"%s\" (%s:%d)" %
+              trace)
         if traceback:
             print_tb(sys.exc_info()[2])
         return 1
     finally:
         print("")
-        try:
-            os.killpg(os.getpgid(child.pid), signal.SIGKILL)
-        except ProcessLookupError:
-            print("Process already stopped")
-
-        child.close()
+        teardown_child(child)
     return 0
