fd752db645b3141e4a83e8933b9a7a5b0e46f47f
Martine Lenders <mail@martine-lenders.eu>
2018-11-25 16:02:23 +0100

    slipdev: expose marker byte definitions

--- drivers/slipdev/include/slipdev_params.h
+++ drivers/slipdev/include/slipdev_params.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Freie Universität Berlin
+ * Copyright (C) 2018-2020 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -7,13 +7,11 @@
  */
 
 /**
- * @defgroup
- * @ingroup
- * @brief
+ * @ingroup drivers_slipdev
  * @{
  *
  * @file
- * @brief
+ * @brief   Default configuration for the SLIP device driver
  *
  * @author  Martine Lenders <m.lenders@fu-berlin.de>
  */
@@ -22,6 +20,9 @@
 
 #include "board.h"
 #include "slipdev.h"
+#ifdef MODULE_SLIPDEV_STDIO
+#include "stdio_uart.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,11 +33,19 @@ extern "C" {
  * @{
  */
 #ifndef SLIPDEV_PARAM_UART
-#define SLIPDEV_PARAM_UART      (UART_DEV(0))
-#endif
+# ifndef MODULE_SLIPDEV_STDIO
+#  define SLIPDEV_PARAM_UART        (UART_DEV(0))
+# else  /* MODULE_SLIPDEV_STDIO */
+#  define SLIPDEV_PARAM_UART        (STDIO_UART_DEV)
+# endif /* MODULE_SLIPDEV_STDIO */
+#endif  /* SLIPDEV_PARAM_UART */
 #ifndef SLIPDEV_PARAM_BAUDRATE
-#define SLIPDEV_PARAM_BAUDRATE  (115200U)
-#endif
+# ifndef MODULE_SLIPDEV_STDIO
+#  define SLIPDEV_PARAM_BAUDRATE    (115200U)
+# else  /* MODULE_SLIPDEV_STDIO */
+#  define SLIPDEV_PARAM_BAUDRATE    (STDIO_UART_BAUDRATE)
+# endif /* MODULE_SLIPDEV_STDIO */
+#endif  /* SLIPDEV_PARAM_BAUDRATE */
 
 #ifndef SLIPDEV_PARAMS
 #define SLIPDEV_PARAMS          { .uart = SLIPDEV_PARAM_UART,   \
@@ -46,6 +55,9 @@ extern "C" {
 
 /**
  * @brief   slipdev configuration
+ *
+ * The first element in this array will be used to multiplex stdio if
+ * `slipdev_stdio` is included.
  */
 static const slipdev_params_t slipdev_params[] = {
     SLIPDEV_PARAMS
