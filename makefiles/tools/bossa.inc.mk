f4e715c9fa33779cbf8a1ab02b26510b033e6474
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2018-12-19 19:35:29 +0100

    makefiles/bossa.inc.mk: use FLASHFILE
    Update to use FLASHFILE as file to be flashed on the board.

--- makefiles/tools/bossa.inc.mk
+++ makefiles/tools/bossa.inc.mk
@@ -1,7 +1,6 @@
+FLASHFILE ?= $(BINFILE)
 FLASHER ?= $(RIOTTOOLS)/bossa/bossac
-FFLAGS  ?= -p $(PORT) -e -i -w -v -b -R $(HEXFILE)
-
-HEXFILE = $(BINFILE)
+FFLAGS  ?= -p $(PORT) -e -i -w -v -b -R $(FLASHFILE)
 
 # some arduino boards need to toggle the serial interface a little bit to get
 # them ready for flashing...
