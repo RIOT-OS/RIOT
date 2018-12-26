0c12206503df6627cd8ec23e2bd3bd64a27a3685
Gunar Schorcht <gunar@schorcht.net>
2018-12-26 16:05:27 +0100

    cpu/esp8266: cpu configuration tuned
    With the new ETS task handling thread, the stack sizes could be down sized.

--- cpu/esp8266/include/cpu_conf.h
+++ cpu/esp8266/include/cpu_conf.h
@@ -29,18 +29,40 @@ extern "C" {
 #endif
 
 /**
- * @brief   Stack size configuration
+ * @name   Stack size configuration
  * @{
  */
 #ifdef MODULE_ESP_SDK_INT_HANDLING
+
+#ifndef THREAD_EXTRA_STACKSIZE_PRINTF
 #define THREAD_EXTRA_STACKSIZE_PRINTF (0)
-#define THREAD_STACKSIZE_DEFAULT      (2048)
-#define THREAD_STACKSIZE_IDLE         (2048)
-#else
-#define THREAD_EXTRA_STACKSIZE_PRINTF (0)
-#define THREAD_STACKSIZE_DEFAULT      (2048)
-#define THREAD_STACKSIZE_IDLE         (2048)
 #endif
+#ifndef THREAD_STACKSIZE_DEFAULT
+#define THREAD_STACKSIZE_DEFAULT      (1536)
+#endif
+#ifndef THREAD_STACKSIZE_IDLE
+#define THREAD_STACKSIZE_IDLE         (1536)
+#endif
+#ifndef THREAD_STACKSIZE_MAIN
+#define THREAD_STACKSIZE_MAIN         (3072)
+#endif
+
+#else /* MODULE_ESP_SDK_INT_HANDLING */
+
+#ifndef THREAD_EXTRA_STACKSIZE_PRINTF
+#define THREAD_EXTRA_STACKSIZE_PRINTF (0)
+#endif
+#ifndef THREAD_STACKSIZE_DEFAULT
+#define THREAD_STACKSIZE_DEFAULT      (1024)
+#endif
+#ifndef THREAD_STACKSIZE_IDLE
+#define THREAD_STACKSIZE_IDLE         (1024)
+#endif
+#ifndef THREAD_STACKSIZE_MAIN
+#define THREAD_STACKSIZE_MAIN         (3072)
+#endif
+
+#endif /* MODULE_ESP_SDK_INT_HANDLING */
 /** @} */
 
 /**
