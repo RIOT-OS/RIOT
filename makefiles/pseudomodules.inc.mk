f44db674a6aa8132369f04253f0768b8b3f571f4
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-12-30 12:19:56 +0100

    drivers/lpsxxx: unify lps331ap and lps25h drivers

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -118,6 +118,10 @@ PSEUDOMODULES += vcnl4010
 PSEUDOMODULES += vcnl4020
 PSEUDOMODULES += vcnl4040
 
+# include variants of lpsxxx drivers as pseudo modules
+PSEUDOMODULES += lps331ap
+PSEUDOMODULES += lps25hb
+
 # add all pseudo random number generator variants as pseudomodules
 PSEUDOMODULES += prng_%
 
