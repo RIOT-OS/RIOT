5b4a26cd6932b8235cdd77f7607727f9a0d6a9db
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-01-25 14:17:12 +0100

    pkg/semtech-loramac: add timer calibration functions

--- pkg/semtech-loramac/contrib/semtech_loramac_getset.c
+++ pkg/semtech-loramac/contrib/semtech_loramac_getset.c
@@ -253,6 +253,26 @@ uint8_t semtech_loramac_get_tx_mode(semtech_loramac_t *mac)
     return mac->cnf;
 }
 
+void semtech_loramac_set_system_max_rx_error(semtech_loramac_t *mac, int error)
+{
+    MibRequestConfirm_t mibReq;
+    mutex_lock(&mac->lock);
+    mibReq.Type = MIB_SYSTEM_MAX_RX_ERROR;
+    mibReq.Param.SystemMaxRxError = error;
+    LoRaMacMibSetRequestConfirm(&mibReq);
+    mutex_unlock(&mac->lock);
+}
+
+void semtech_loramac_set_min_rx_symbols(semtech_loramac_t *mac, int min_rx)
+{
+    MibRequestConfirm_t mibReq;
+    mutex_lock(&mac->lock);
+    mibReq.Type = MIB_MIN_RX_SYMBOLS;
+    mibReq.Param.MinRxSymbols = min_rx;
+    LoRaMacMibSetRequestConfirm(&mibReq);
+    mutex_unlock(&mac->lock);
+}
+
 static void _semtech_loramac_set_rx2_params(semtech_loramac_channel_params_t params)
 {
     Rx2ChannelParams_t p;
