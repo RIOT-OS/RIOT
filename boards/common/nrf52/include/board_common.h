740814a62cfd7f5a62c03958783e2b2e99015016
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-01-28 16:22:25 +0100

    boards/common/nrf52: Documented flashing

--- boards/common/nrf52/include/board_common.h
+++ boards/common/nrf52/include/board_common.h
@@ -10,6 +10,30 @@
  * @defgroup    boards_common_nrf52 NRF52 common
  * @ingroup     boards_common
  * @brief       Shared files and configuration for all nRF52 boards.
+ *
+ * # Flashing
+ * ## Using JLink
+ * By default the nRF52 family of boards is flashed using the Segger's JLink
+ * tool. Due to its proprietary nature, only pre-build binary blobs are
+ * available for only the most common systems. You can use OpenOCD on those
+ * systems instead. If the JLink tool is installed on your system, you can
+ * compile and flash your application by running:
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * make BOARD=<nRF52_BOARD> flash
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * ## Using OpenOCD
+ * As of January 2018, the latest stable version of OpenOCD (version 0.10.0) is
+ * not able to flash nRF52 boards. Thus, you will currently need to build the
+ * current development version of OpenOCD in order to flash your boards using
+ * OpenOCD. If the development version of OpenOCD is installed, you can build
+ * and flash your application by running:
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * make BOARD=<nRF52_BOARD> PROGRAMMER=openocd flash
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
  * @{
  *
  * @file
