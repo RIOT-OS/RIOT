4b009649c65053d2f0bcd220fc7d0f86b9ddf311
Gunar Schorcht <gunar@schorcht.net>
2019-02-03 18:59:27 +0100

    cpu/atmega_common: changes for common heap command

--- cpu/atmega_common/cpu.c
+++ cpu/atmega_common/cpu.c
@@ -112,6 +112,28 @@ void cpu_init(void)
     periph_init();
 }
 
+struct __freelist {
+    size_t size;
+    struct __freelist *next;
+};
+
+extern struct __freelist *__flp;
+extern char *__malloc_heap_start;
+extern char *__malloc_heap_end;
+extern char *__brkval;
+
+void heap_stats(void)
+{
+    int heap_size = __malloc_heap_end - __malloc_heap_start;
+    int free = __malloc_heap_end - __brkval;
+    struct __freelist *fp;
+    for (fp = __flp; fp; fp = fp->next) {
+        free += fp->size;
+    }
+    printf("heap: %d (used %d, free %d) [bytes]\n",
+           heap_size, heap_size - free, free);
+}
+
 /* This is a vector which is aliased to __vector_default,
  * the vector executed when an ISR fires with no accompanying
  * ISR handler. This may be used along with the ISR() macro to
