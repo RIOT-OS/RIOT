ad75b2b1c839539d3c9f54606356d95bc323c13a
Martine Lenders <m.lenders@fu-berlin.de>
2019-02-11 15:15:26 +0100

    gnrc_sixlowpan_frag: add intervals to public type
    The fragment intervals are also required by the VRB, so make them part
    of the public representation of a reassembly buffer.

--- sys/net/gnrc/network_layer/sixlowpan/frag/rbuf.h
+++ sys/net/gnrc/network_layer/sixlowpan/frag/rbuf.h
@@ -51,7 +51,6 @@ extern "C" {
  */
 typedef struct {
     gnrc_sixlowpan_rbuf_t super;        /**< exposed part of the reassembly buffer */
-    gnrc_sixlowpan_rbuf_int_t *ints;    /**< intervals of the fragment */
     uint32_t arrival;                   /**< time in microseconds of arrival of
                                          *   last received fragment */
 } rbuf_t;
