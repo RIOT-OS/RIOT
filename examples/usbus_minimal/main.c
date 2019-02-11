8246b09f107feb07a3594c7efa6ae2c4540d1df4
Koen Zandberg <koen@bergzand.net>
2019-02-11 13:46:02 +0100

    usbus_minimal: Adapt example to auto_init

--- examples/usbus_minimal/main.c
+++ examples/usbus_minimal/main.c
@@ -19,26 +19,11 @@
  */
 
 #include <stdio.h>
-#include "usb/usbus.h"
-
-static char _stack[USBUS_STACKSIZE];
-
-static usbus_t usbus;
-/* TODO: remove as soon as we have decent auto_init */
-#include "periph_conf.h"
-#include "sam_usb.h"
 
 int main(void)
 {
     puts("RIOT USB stack example application");
 
-    /* TODO: remove as soon as we have decent auto_init */
-    usbdev_t *usbdev_ctx = usbdev_get_ctx(0);
-    /* start usb stack */
-    usbus_init(&usbus, usbdev_ctx);
-    usbus_create(_stack, sizeof(_stack), USBUS_PRIO, USBUS_TNAME, &usbus);
-
-    /* start shell */
     puts("Started USB stack!");
     return 0;
 }
