e4ad2b06b4f4da512dd4a232412b2356bddd6328
Koen Zandberg <koen@bergzand.net>
2019-02-13 18:54:13 +0100

    fixup! fixup! fixup! fixup! usbdev: Introduce a common API for USB peripherals

--- drivers/include/usb/usbdev.h
+++ drivers/include/usb/usbdev.h
@@ -27,25 +27,28 @@
  * The driver interface is split in two separate parts. One part is a global USB
  * device interface, the other is an endpoint control API.
  *
- * This can manage parts of the USB interface itself such as
+ * The usb device driver can manage parts of the USB interface itself such as
  * the pull up resistor state or the USB speed.
  *
  * The endpoint control API manages a single endpoint. This allows for a modular
- * approach where multiple functions/interfaces can be multiplexed over a single
- * USB interface. Each interface can be implemented as a separate module.
+ * approach where multiple USB functions/interfaces can be multiplexed over a
+ * single USB interface. Each interface can be implemented as a separate module.
  * The interface handler does not have to care about the usb device itself or
  * it's endpoint number. It can simply request an available endpoint from the
- * usb device with the usbdev_driver::new_ep driver.
+ * usb device with the @ref usbdev_new_ep function.
  *
- * Data transmission is done by setting an address and max packet size in the
- * endpoint. Received data from the host ends up at this address automatically
+ * Data transmission is done by requesting the endpoint with a max packet size.
+ * Memory for this buffer is allocated from dedicated memory of the MCU USB
+ * peripheral or from a buffer allocated by the peripheral specific usbdev
+ * struct. Received data from the host ends up at this buffer automatically
  * by the low level drivers. Signalling that the data at the specified address
- * is ready to be reused is done with the usbdev_endpoint_driver::ready
- * function.
+ * is ready to be reused is done with the @ref usbdev_ep_ready function by 
+ * supplying a size of 0 for the @p len argument.
  *
  * For transmitting data back to the host, a similar approach is used. The data
  * to be transmitted is written to the specified address and the
- * usbdev_endpoint_driver::ready function is called with the size of the data.
+ * @ref usbdev_ep_ready function is called with the size of the data as @p len
+ * argument.
  *
  * This approach of setting the address and only indicating new data available
  * is done to allow the low level USB peripheral to use DMA to transfer the data
@@ -54,7 +57,10 @@
  * A callback function is required for signalling events from the driver. The
  * @ref USBDEV_EVENT_ESR is special in that it indicates that the USB peripheral
  * had an interrupt that needs to be serviced in a non-interrupt context. This
- * requires the usbdev_driver::esr function to be called.
+ * requires the usbdev_esr function to be called. In the case that the event is
+ * signalled via the @ref usbdev_t::epcb callback, the @ref usbdev_ep_esr
+ * should be called to service the endpoint specific events from the
+ * peripheral.
  *
  * @file
  * @brief       Definitions low-level USB driver interface
@@ -71,14 +77,23 @@ extern "C" {
 
 #include <stdint.h>
 #include <stddef.h>
+
+#include "assert.h"
 #include "usb.h"
 #include "usb/usbopt.h"
 
+/**
+ * @brief usbdev_t forward declaration
+ */
 typedef struct usbdev usbdev_t;
+
+/**
+ * @brief usbdev_ep_t forward declaration
+ */
 typedef struct usbdev_ep usbdev_ep_t;
 
 /**
- * Statically allocated buffer space for endpoints.
+ * @brief Statically allocated buffer space for endpoints.
  *
  * When the device doesn't have dedicated memory for endpoint buffers, a
  * buffer of this size is allocated to contain the endpoint buffers. Only
@@ -89,7 +104,7 @@ typedef struct usbdev_ep usbdev_ep_t;
 #endif
 
 /**
- * Number of USB IN and OUT endpoints allocated
+ * @brief Number of USB IN and OUT endpoints allocated
  *
  * Configures the number of endpoints allocated. An equal number of IN and OUT
  * endpoints are allocated
@@ -100,12 +115,12 @@ typedef struct usbdev_ep usbdev_ep_t;
 
 
 /**
- * @brief   Possible event types that are send from the device driver to the
+ * @brief   List of event types that can be send from the device driver to the
  *          upper layer
  */
 typedef enum {
     /**
-     * @brief Driver needs it's ISR handled
+     * @brief Driver needs it's ESR (event service routine) handled
      */
     USBDEV_EVENT_ESR = 1,
 
@@ -120,6 +135,13 @@ typedef enum {
      *  - Endpoint configuration, including stall settings
      */
     USBDEV_EVENT_RESET,
+
+    /**
+     * @brief Start of Frame received
+     *
+     * A Start of Frame (SoF) packet is received from the host.
+     */
+    USBDEV_EVENT_SOF,
 
     /**
      * @brief Transaction completed event.
@@ -156,46 +178,50 @@ typedef enum {
 typedef void (*usbdev_event_cb_t)(usbdev_t *usbdev, usbdev_event_t event);
 
 /**
- * @brief usbdev device descriptor
- */
-struct usbdev {
-    const struct usbdev_driver *driver;     /**< usbdev driver struct   */
-    usbdev_event_cb_t cb;                   /**< Event callback supplied by
-                                              *  upper layer */
-    void *context;                          /**< Ptr to the thread context  */
-};
-
-/**
  * @brief   Event callback for signaling endpoint events to upper layers
  *
  * @param[in] ep            usbdev endpoint context
  * @param[in] event         type of the event
  */
-typedef void (*usbdev_ep_event_cb_t)(usbdev_ep_t *ep, usbdev_event_t event);
+typedef void (*usbdev_ep_event_cb_t)(usbdev_ep_t *ep,
+                                     usbdev_event_t event);
+
+/**
+ * @brief usbdev device descriptor
+ */
+struct usbdev {
+    const struct usbdev_driver *driver; /**< usbdev driver struct           */
+    usbdev_event_cb_t cb;               /**< Event callback supplied by
+                                          *  upper layer                    */
+    usbdev_ep_event_cb_t epcb;          /**< Endpoint event callback for
+                                          *  upper layer                    */
+    void *context;                      /**< Ptr to the thread context      */
+};
 
 /**
  * @brief usbdev endpoint descriptor
  */
 struct usbdev_ep {
-    const struct usbdev_ep_driver *driver;  /**< Endpoint driver struct     */
-    void *context;                          /**< Ptr to the thread context  */
-    uint8_t *buf;                           /**< Ptr to the data buffer     */
-    size_t len;                             /**< Size of the data buffer    */
-    usbdev_ep_event_cb_t cb;                /**< Endpoint event callback for
-                                              *  upper layer                */
-    usb_ep_dir_t dir;                       /**< Endpoint direction         */
-    usb_ep_type_t type;                     /**< Endpoint type              */
-    uint8_t num;                            /**< Endpoint number            */
+    usbdev_t *dev;      /**< USB device this endpoint belongs to            */
+    uint8_t *buf;       /**< Ptr to the data buffer                         */
+    size_t len;         /**< Size of the data buffer in bytes               */
+    usb_ep_dir_t dir;   /**< Endpoint direction                             */
+    usb_ep_type_t type; /**< Endpoint type                                  */
+    uint8_t num;        /**< Endpoint number                                */
 };
 
+/**
+ * @brief usbdev driver functions
+ *
+ * Helpers (such as @ref usbdev_init) are provided and should be used instead.
+ * Directly calling these functions is not recommended.
+ */
 typedef struct usbdev_driver {
 
     /**
      * @brief Initialize the USB peripheral device
      *
-     * This initializes the USB device but should not enable the USB pull up.
-     *
-     * @pre `(dev != NULL)`
+     * This initializes the USB device but must not enable the USB pull up.
      *
      * @param[in]   dev     USB device descriptor
      */
@@ -207,22 +233,18 @@ typedef struct usbdev_driver {
      * requesting an endpoint of @ref USB_EP_TYPE_CONTROL must always return
      * endpoint 0 of the specified direction
      *
-     * @pre `(dev != NULL)`
-     *
      * @param[in]   dev     USB device descriptor
      * @param[in]   type    USB endpoint type
      * @param[in]   dir     USB endpoint direction
      * @param[in]   buf_len optimal USB endpoint buffer size
      *
-     * @return              USB endpoint descriptor
+     * @return              ptr to the new USB endpoint descriptor
      * @return              NULL on error
      */
     usbdev_ep_t *(*new_ep)(usbdev_t *dev, usb_ep_type_t type, usb_ep_dir_t dir, size_t buf_len);
 
     /**
-     * @brief   Get an option value from a given usb device endpoint
-     *
-     * @pre `(dev != NULL)`
+     * @brief   Get an option value from a given usb device
      *
      * @param[in]   dev     USB device descriptor
      * @param[in]   opt     option type
@@ -230,15 +252,13 @@ typedef struct usbdev_driver {
      * @param[in]   max_len maximal amount of byte that fit into @p value
      *
      * @return              number of bytes written to @p value
-     * @return              `< 0` on error, 0 on success
+     * @return              `< 0` on error 0
      */
     int (*get)(usbdev_t *usbdev, usbopt_t opt,
                void *value, size_t max_len);
 
     /**
-     * @brief   Set an option value for a given usb device endpoint
-     *
-     * @pre `(dev != NULL)`
+     * @brief   Set an option value for a given usb device
      *
      * @param[in] dev       USB device descriptor
      * @param[in] opt       option type
@@ -246,42 +266,33 @@ typedef struct usbdev_driver {
      * @param[in] value_len the length of @p value
      *
      * @return              number of bytes used from @p value
-     * @return              `< 0` on error, 0 on success
+     * @return              `< 0` on error
      */
     int (*set)(usbdev_t *usbdev, usbopt_t opt,
                const void *value, size_t value_len);
+
     /**
      * @brief a driver's user-space event service handler
      *
      * This function will be called from a USB stack's loop when being
      * notified by usbdev_event_isr.
      *
-     * @pre `(dev != NULL)`
-     *
      * @param[in]   dev     USB device descriptor
      */
     void (*esr)(usbdev_t *dev);
-
-} usbdev_driver_t;
-
-typedef struct usbdev_ep_driver {
 
     /**
      * @brief Initialize the USB endpoint
      *
      * This initializes the USB endpoint with the settings from the
-     * @ref usbdev_ep_t.
-     *
-     * @pre `(dev != NULL)`
+     * @ref usbdev_ep.
      *
      * @param[in]   ep      USB endpoint descriptor
      */
-    void (*init)(usbdev_ep_t *ep);
+    void (*ep_init)(usbdev_ep_t *ep);
 
     /**
      * @brief   Get an option value from a given usb device endpoint
-     *
-     * @pre `(dev != NULL)`
      *
      * @param[in]   ep      USB endpoint descriptor
      * @param[in]   opt     option type
@@ -289,15 +300,13 @@ typedef struct usbdev_ep_driver {
      * @param[in]   max_len maximum number of byte that fit into @p value
      *
      * @return              number of bytes written to @p value
-     * @return              `< 0` on error, 0 on success
+     * @return              `< 0` on error
      */
-    int (*get)(usbdev_ep_t *ep, usbopt_ep_t opt,
+    int (*ep_get)(usbdev_ep_t *ep, usbopt_ep_t opt,
                void *value, size_t max_len);
 
     /**
      * @brief   Set an option value for a given usb device endpoint
-     *
-     * @pre `(dev != NULL)`
      *
      * @param[in] ep        USB endpoint descriptor
      * @param[in] opt       option type
@@ -305,9 +314,9 @@ typedef struct usbdev_ep_driver {
      * @param[in] value_len the length of @p value
      *
      * @return              number of bytes used from @p value
-     * @return              `< 0` on error, 0 on success
+     * @return              `< 0` on error
      */
-    int (*set)(usbdev_ep_t *ep, usbopt_ep_t opt,
+    int (*ep_set)(usbdev_ep_t *ep, usbopt_ep_t opt,
                const void *value, size_t value_len);
 
     /**
@@ -318,18 +327,214 @@ typedef struct usbdev_ep_driver {
      *
      * @param[in] ep        USB endpoint descriptor to service
      */
-    void (*esr)(usbdev_ep_t *ep);
+    void (*ep_esr)(usbdev_ep_t *ep);
 
     /**
      * @brief Signal data buffer ready for data transmission
      *
-     * This clears the stall setting in the endpoint if enabled.
+     * This clears the stall setting in the endpoint if that is enabled.
      *
      * @param[in] ep        USB endpoint descriptor
      * @param[in] len       length of the data to be transmitted
      */
     int (*ready)(usbdev_ep_t *ep, size_t len);
-} usbdev_ep_driver_t;
+} usbdev_driver_t;
+
+/**
+ * @brief Initialize the USB peripheral device
+ *
+ * @see @ref usbdev_driver_t::init
+ *
+ * @pre `(dev != NULL)`
+ *
+ * @param dev   usb device to initialize
+ */
+static inline void usbdev_init(usbdev_t *dev)
+{
+    assert(dev);
+    dev->driver->init(dev);
+}
+
+/**
+ * @brief Retrieve an USB endpoint of the specified type
+ *
+ * @see @ref usbdev_driver_t::new_ep
+ *
+ * @pre `(dev != NULL)`
+ *
+ * @param[in]   dev     USB device descriptor
+ * @param[in]   type    USB endpoint type
+ * @param[in]   dir     USB endpoint direction
+ * @param[in]   buf_len optimal USB endpoint buffer size
+ *
+ * @return              ptr to the new USB endpoint descriptor
+ * @return              NULL on error
+ */
+static inline usbdev_ep_t * usbdev_new_ep(usbdev_t *dev, usb_ep_type_t type,
+                                          usb_ep_dir_t dir, size_t buf_len)
+{
+    assert(dev);
+    return dev->driver->new_ep(dev, type, dir, buf_len);
+}
+
+/**
+ * @brief   Get an option value from a given usb device
+ *
+ * @see @ref usbdev_driver_t::get
+ *
+ * @pre `(dev != NULL)`
+ *
+ * @param[in]   dev     USB device descriptor
+ * @param[in]   opt     option type
+ * @param[out]  value   pointer to store the option's value in
+ * @param[in]   max_len maximal amount of byte that fit into @p value
+ *
+ * @return              number of bytes written to @p value
+ * @return              `< 0` on error 0
+ */
+static inline int usbdev_get(usbdev_t *dev, usbopt_ep_t opt,
+                             void *value, size_t max_len)
+{
+    assert(dev);
+    return dev->driver->get(dev, opt, value, max_len);
+}
+
+/**
+ * @brief   Set an option value for a given usb device
+ *
+ * @see @ref usbdev_driver_t::set
+ *
+ * @pre `(dev != NULL)`
+ *
+ * @param[in] dev       USB device descriptor
+ * @param[in] opt       option type
+ * @param[in] value     value to set
+ * @param[in] value_len the length of @p value
+ *
+ * @return              number of bytes used from @p value
+ * @return              `< 0` on error
+ */
+static inline int usbdev_set(usbdev_t *dev, usbopt_ep_t opt,
+                             const void *value, size_t value_len)
+{
+    assert(dev);
+    return dev->driver->set(dev, opt, value, value_len);
+}
+
+/**
+ * @brief a driver's user-space event service handler
+ *
+ * @see @ref usbdev_driver_t::esr
+ *
+ * @pre `(dev != NULL)`
+ *
+ * @param[in]   dev     USB device descriptor
+ */
+static inline void usbdev_esr(usbdev_t *dev)
+{
+    assert(dev);
+    dev->driver->esr(dev);
+}
+
+/**
+ * @brief Initialize the USB endpoint
+ *
+ * @see @ref usbdev_driver_t::ep_init
+ *
+ * @pre `(ep != NULL)`
+ * @pre `(ep->dev != NULL)`
+ *
+ * @param[in]   ep      USB endpoint descriptor
+ */
+static inline void usbdev_ep_init(usbdev_ep_t *ep)
+{
+    assert(ep);
+    assert(ep->dev);
+    ep->dev->driver->ep_init(ep);
+}
+
+/**
+ * @brief   Get an option value from a given usb device endpoint
+ *
+ * @see @ref usbdev_driver_t::ep_get
+ *
+ * @pre `(ep != NULL)`
+ * @pre `(ep->dev != NULL)`
+ *
+ * @param[in]   ep      USB endpoint descriptor
+ * @param[in]   opt     option type
+ * @param[out]  value   pointer to store the option's value in
+ * @param[in]   max_len maximum number of byte that fit into @p value
+ *
+ * @return              number of bytes written to @p value
+ * @return              `< 0` on error
+ */
+static inline int usbdev_ep_get(usbdev_ep_t *ep, usbopt_ep_t opt,
+                                void *value, size_t max_len)
+{
+    assert(ep);
+    assert(ep->dev);
+    return ep->dev->driver->ep_get(ep, opt, value, max_len);
+}
+
+/**
+ * @brief   Set an option value for a given usb device endpoint
+ *
+ * @see @ref usbdev_driver_t::ep_set
+ *
+ * @pre `(ep != NULL)`
+ * @pre `(ep->dev != NULL)`
+ *
+ * @param[in] ep        USB endpoint descriptor
+ * @param[in] opt       option type
+ * @param[in] value     pointer to the value
+ * @param[in] value_len the length of @p value
+ *
+ * @return              number of bytes used from @p value
+ * @return              `< 0` on error
+ */
+static inline int usbdev_ep_set(usbdev_ep_t *ep, usbopt_ep_t opt,
+                                const void *value, size_t value_len)
+{
+    assert(ep);
+    assert(ep->dev);
+    return ep->dev->driver->ep_set(ep, opt, value, value_len);
+}
+
+/**
+ * @brief an endpoint's user-space event handler
+ *
+ * @see @ref usbdev_driver_t::ep_esr
+ *
+ * @pre `(ep != NULL)`
+ * @pre `(ep->dev != NULL)`
+ *
+ * @param[in] ep        USB endpoint descriptor to service
+ */
+static inline void usbdev_ep_esr(usbdev_ep_t *ep)
+{
+    assert(ep);
+    assert(ep->dev);
+    ep->dev->driver->ep_esr(ep);
+}
+
+/**
+ * @brief Signal data buffer ready for data transmission
+ *
+ * @see @ref usbdev_driver_t::ready
+ *
+ * @pre `(ep != NULL)`
+ * @pre `(ep->dev != NULL)`
+ *
+ * @param[in] ep        USB endpoint descriptor
+ * @param[in] len       length of the data to be transmitted
+ */
+static inline int usbdev_ep_ready(usbdev_ep_t *ep, size_t len)
+{
+    assert(ep);
+    assert(ep->dev);
+    return ep->dev->driver->ready(ep, len);
+}
 
 #ifdef __cplusplus
 }
