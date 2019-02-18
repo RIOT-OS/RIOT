dc301383e4e9731e26bf43d6b343562cc1a9c775
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2019-02-18 10:01:14 +0100

    tests/events: Test event_get with event_wait

--- tests/events/main.c
+++ tests/events/main.c
@@ -33,24 +33,24 @@
 static char stack[STACKSIZE];
 
 static unsigned order = 0;
-static uint32_t before;
+static uint32_t timeout1_start;
+static uint32_t timeout2_start;
 
 static void callback(event_t *arg);
 static void custom_callback(event_t *event);
-static void timed_callback(void *arg);
+static void timed_callback1(void *arg);
+static void timed_callback2(void *arg);
 static void forbidden_callback(void *arg);
 static void delayed_callback1(event_t *arg);
-static void delayed_callback2(event_t *arg);
 
 static event_t event = { .handler = callback };
 static event_t event2 = { .handler = callback };
 static event_t delayed_event1 = { .handler = delayed_callback1 };
-static event_t delayed_event2 = { .handler = delayed_callback2 };
 
 static void callback(event_t *arg)
 {
     order++;
-    assert(order == 3);
+    assert(order == 2);
     assert(arg == &event);
     printf("triggered 0x%08x\n", (unsigned)arg);
 }
@@ -61,26 +61,39 @@ typedef struct {
 } custom_event_t;
 
 static custom_event_t custom_event = { .super.handler = custom_callback, .text = "CUSTOM CALLBACK" };
-static event_callback_t event_callback = EVENT_CALLBACK_INIT(timed_callback, 0x12345678);
+static event_callback_t event_callback1 = EVENT_CALLBACK_INIT(timed_callback1, 0x12345678);
+static event_callback_t event_callback2 = EVENT_CALLBACK_INIT(timed_callback2, 0x12348765);
 static event_callback_t noevent_callback = EVENT_CALLBACK_INIT(forbidden_callback, 0);
 
 static void custom_callback(event_t *event)
 {
     order++;
-    assert(order == 4);
+    assert(order == 3);
     assert(event == (event_t *)&custom_event);
     custom_event_t *custom_event = (custom_event_t *)event;
     printf("triggered custom event with text: \"%s\"\n", custom_event->text);
 }
 
-static void timed_callback(void *arg)
+static void timed_callback1(void *arg)
+{
+    order++;
+    assert(order == 4);
+    assert(arg == event_callback1.arg);
+    uint32_t now = xtimer_now_usec();
+    assert((now - timeout1_start >= 100000LU));
+    printf("triggered timed callback with arg 0x%08x after %" PRIu32 "us\n",
+           (unsigned)arg, now - timeout1_start);
+}
+
+static void timed_callback2(void *arg)
 {
     order++;
     assert(order == 5);
-    assert(arg == event_callback.arg);
+    assert(arg == event_callback2.arg);
     uint32_t now = xtimer_now_usec();
-    assert((now - before >= 100000LU));
-    printf("triggered timed callback with arg 0x%08x after %" PRIu32 "us\n", (unsigned)arg, now - before);
+    assert((now - timeout2_start >= 100000LU));
+    printf("triggered timed callback with arg 0x%08x after %" PRIu32 "us\n",
+           (unsigned)arg, now - timeout2_start);
     puts("[SUCCESS]");
 }
 
@@ -103,23 +116,21 @@ static void delayed_callback1(event_t *arg)
     printf("triggered delayed event %p\n", (void *)arg);
 }
 
-static void delayed_callback2(event_t *arg)
-{
-    order++;
-    assert(order == 2);
-    assert(arg == &delayed_event2);
-    printf("triggered delayed event %p\n", (void *)arg);
-}
-
 static void *claiming_thread(void *arg)
 {
     event_queue_t *dq = (event_queue_t *)arg;
+    event_t *ev;
 
     printf("claiming event queue %p\n", (void *)dq);
     event_queue_claim(dq);
     printf("launching event queue %p\n", (void *)dq);
-    event_loop(dq);
 
+    /* this tests that the non-blocking event wait can be used with the blocking
+     * wait (called from event_loop) */
+    while(!(ev = event_get(dq))) {}
+    ev->handler(ev);
+
+    event_loop(dq);
     return NULL;
 }
 
@@ -132,10 +143,15 @@ int main(void)
     printf("initializing detached event queue %p\n", (void *)&dq);
     event_queue_init_detached(&dq);
 
-    printf("posting %p\n", (void *)&delayed_event1);
+    printf("posting %p to detached event queue\n", (void *)&delayed_event1);
     event_post(&dq, &delayed_event1);
-    printf("posting %p\n", (void *)&delayed_event2);
-    event_post(&dq, &delayed_event2);
+
+    event_timeout_t event_timeout1;
+
+    puts("posting timed callback with timeout 1sec to detached event queue");
+    event_timeout_init(&event_timeout1, &dq, (event_t *)&event_callback1);
+    timeout1_start = xtimer_now_usec();
+    event_timeout_set(&event_timeout1, (1 * US_PER_SEC));
 
     printf("running thread that will claim event queue %p\n", (void *)&dq);
     thread_create(stack, sizeof(stack), PRIO, 0, claiming_thread, &dq, "ct");
@@ -154,12 +170,12 @@ int main(void)
     puts("posting custom event");
     event_post(&queue, (event_t *)&custom_event);
 
-    event_timeout_t event_timeout;
+    event_timeout_t event_timeout2;
 
     puts("posting timed callback with timeout 1sec");
-    event_timeout_init(&event_timeout, &queue, (event_t *)&event_callback);
-    before = xtimer_now_usec();
-    event_timeout_set(&event_timeout, (1 * US_PER_SEC));
+    event_timeout_init(&event_timeout2, &queue, (event_t *)&event_callback2);
+    timeout2_start = xtimer_now_usec();
+    event_timeout_set(&event_timeout2, (1 * US_PER_SEC));
 
     event_timeout_t event_timeout_canceled;
 
