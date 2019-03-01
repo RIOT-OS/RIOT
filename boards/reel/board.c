1bd65cc1d5590aeb3806954d76ab97af5077a1c1
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-03-01 15:40:11 +0100

    boards: add support for Phytec 'reel board'

---
+++ boards/reel/board.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_reel
+ * @{
+ *
+ * @file
+ * @brief       Board initialization for the Phytec 'reel board'
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+void board_init(void)
+{
+    /* setup on-board LEDs */
+    NRF_P0->DIRSET = (LED_MASK_P0);
+    NRF_P0->OUTSET = (LED_MASK_P0);
+    NRF_P1->DIRSET = (LED_MASK_P1);
+    NRF_P1->OUTSET = (LED_MASK_P1);
+
+    /* initialize the CPU */
+    cpu_init();
+}
