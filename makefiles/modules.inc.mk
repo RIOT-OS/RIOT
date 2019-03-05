45b00c53c693505c0734e974e9a6f4aad39aa07b
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-03-05 15:48:28 +0100

    makefiles/modules.inc.mk: factorize in _ALLMODULES
    Factorize the reused value in a private variable.
    
    I define it private as somehow 'USEPKG' is supposed to be removed so the
    variables can be removed later.

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -1,9 +1,11 @@
-ED = $(addprefix MODULE_,$(sort $(USEMODULE) $(USEPKG)))
+_ALLMODULES = $(sort $(USEMODULE) $(USEPKG))
+
+ED = $(addprefix MODULE_,$(_ALLMODULES))
 EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
-NO_PSEUDOMODULES := $(filter $(NO_PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG)))
-REALMODULES = $(filter-out $(PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG))) $(NO_PSEUDOMODULES)
+NO_PSEUDOMODULES := $(filter $(NO_PSEUDOMODULES), $(_ALLMODULES))
+REALMODULES = $(filter-out $(PSEUDOMODULES), $(_ALLMODULES)) $(NO_PSEUDOMODULES)
 BASELIBS += $(REALMODULES:%=$(BINDIR)/%.a)
 
 CFLAGS += $(EXTDEFINES)
