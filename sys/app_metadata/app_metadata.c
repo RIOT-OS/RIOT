965418e124926f3e49467706984938c69ff6f1e1
MrKevinWeiss <weiss.kevin604@gmail.com>
2019-03-05 14:18:30 +0100

    sys/app_metadata: Add app_metadata module
    This allows a access to application metadata such as BOARD, CPU, etc.
    It prints the contents to the stdio in a standard json form

---
+++ sys/app_metadata/app_metadata.c
@@ -0,0 +1,31 @@
+/**
+Copyright (C) 2019, HAW Hamburg.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * @ingroup sys_app_metadata
+ * @{
+ * @file
+ * @brief   Prints application metadata such as BOARD, CPU, OS_VERSION.
+ * @author  Kevin Weiss <kevin.weiss@haw-hamburg.de>
+ * @}
+ */
+
+#include <stdio.h>
+
+void app_metadata_print_json(void)
+{
+    puts("{\"cmd\": \"app_metadata_print_json()\"}");
+    printf("{\"data\": {\"APP_NAME\": \"%s\"}}\n", RIOT_APPLICATION);
+    printf("{\"data\": {\"BOARD\": \"%s\"}}\n", RIOT_BOARD);
+    printf("{\"data\": {\"CPU\": \"%s\"}}\n", RIOT_CPU);
+#ifdef APP_SHELL_FMT
+    printf("{\"data\": {\"APP_SHELL_FMT\": \"%s\"}}\n", APP_SHELL_FMT);
+#endif
+    printf("{\"data\": {\"MCU\": \"%s\"}}\n", RIOT_MCU);
+    printf("{\"data\": {\"OS_VERSION\": \"%s\"}}\n", RIOT_VERSION);
+
+    printf("{\"result\": \"SUCCESS\"}\n");
+}
