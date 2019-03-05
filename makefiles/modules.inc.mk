f27eef379d4c17c8d806da7f62df68dd5d579b31
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-03-05 15:52:33 +0100

    makefiles/modules.inc.mk: do not export BASELIBS
    BASELIBS is used only in the main Makefile.include or included files.
    As it is not used in sub make executions or scripts it does not need to
    be exported.

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -4,6 +4,6 @@ EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
 NO_PSEUDOMODULES := $(filter $(NO_PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG)))
 REALMODULES = $(filter-out $(PSEUDOMODULES), $(sort $(USEMODULE) $(USEPKG))) $(NO_PSEUDOMODULES)
-export BASELIBS += $(REALMODULES:%=$(BINDIR)/%.a)
+BASELIBS += $(REALMODULES:%=$(BINDIR)/%.a)
 
 CFLAGS += $(EXTDEFINES)
