fb8d3a395f892f9ae30d3b6215b7760b96579d6a
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-03-05 15:57:13 +0100

    makefiles/modules.inc.mk: group CFLAGS handling
    Put all lines related to CFLAGS handling together.

--- makefiles/modules.inc.mk
+++ makefiles/modules.inc.mk
@@ -1,11 +1,11 @@
 _ALLMODULES = $(sort $(USEMODULE) $(USEPKG))
 
+# Define MODULE_MODULE_NAME preprocessor macros for all modules.
 ED = $(addprefix MODULE_,$(_ALLMODULES))
 EXTDEFINES = $(addprefix -D,$(shell echo '$(ED)' | tr 'a-z-' 'A-Z_'))
+CFLAGS += $(EXTDEFINES)
 
 # filter "pseudomodules" from "real modules", but not "no_pseudomodules"
 NO_PSEUDOMODULES := $(filter $(NO_PSEUDOMODULES), $(_ALLMODULES))
 REALMODULES = $(filter-out $(PSEUDOMODULES), $(_ALLMODULES)) $(NO_PSEUDOMODULES)
 BASELIBS += $(REALMODULES:%=$(BINDIR)/%.a)
-
-CFLAGS += $(EXTDEFINES)
