b634dd4977790fe4fd1e8025a7160fddd292bed0
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-03-08 17:45:08 +0100

    netdev/lora: add netdev_lora_rx_info structure

---
+++ drivers/include/net/netdev/lora.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for
+ * more details.
+ */
+
+/**
+ * @defgroup    drivers_netdev_lora LoRa drivers
+ * @ingroup     drivers_netdev_api
+ * @{
+ *
+ * @file
+ * @brief       Definitions for netdev common LoRa code
+ *
+ * @author      José Ignacio Alamos <jose.alamos@haw-hamburg.de>
+ */
+
+
+#ifndef NET_NETDEV_LORA_H
+#define NET_NETDEV_LORA_H
+
+#include <stdint.h>
+
+#include "net/netdev.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Received LoRa packet status information
+ */
+typedef struct {
+    uint8_t rssi;           /**< RSSI of a received packet */
+    int8_t snr;             /**< S/N ratio */
+} netdev_lora_rx_info_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NET_NETDEV_LORA_H */
+/** @} */
