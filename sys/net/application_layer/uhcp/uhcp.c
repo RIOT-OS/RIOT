dba51edc69bd65fdea27256ddaecb1df96130086
Kaspar Schleiser <kaspar@schleiser.de>
2019-03-19 23:41:36 +0100

    net/uhcp[cd]: use modules to select client/server code

--- sys/net/application_layer/uhcp/uhcp.c
+++ sys/net/application_layer/uhcp/uhcp.c
@@ -39,8 +39,8 @@ void uhcp_handle_udp(uint8_t *buf, size_t len, uint8_t *src, uint16_t port, uhcp
         LOG_ERROR("error: wrong protocol version.\n");
     }
 
-    switch (type) {
-#ifdef UHCP_SERVER
+    switch(type) {
+#ifdef MODULE_UHCPD
         case UHCP_REQ:
             if (len < sizeof(uhcp_req_t)) {
                 LOG_ERROR("error: request too small\n");
@@ -50,7 +50,7 @@ void uhcp_handle_udp(uint8_t *buf, size_t len, uint8_t *src, uint16_t port, uhcp
             }
             break;
 #endif
-#ifdef UHCP_CLIENT
+#ifdef MODULE_UHCPC
         case UHCP_PUSH:
             {
                 uhcp_push_t *push = (uhcp_push_t*)hdr;
@@ -70,7 +70,7 @@ void uhcp_handle_udp(uint8_t *buf, size_t len, uint8_t *src, uint16_t port, uhcp
     }
 }
 
-#ifdef UHCP_SERVER
+#ifdef MODULE_UHCPD
 extern char _prefix[16];
 extern unsigned _prefix_len;
 void uhcp_handle_req(uhcp_req_t *req, uint8_t *src, uint16_t port, uhcp_iface_t iface)
@@ -89,9 +89,9 @@ void uhcp_handle_req(uhcp_req_t *req, uint8_t *src, uint16_t port, uhcp_iface_t 
         LOG_ERROR("uhcp_handle_req(): udp_sendto() res=%i\n", res);
     }
 }
-#endif /* UHCP_SERVER */
+#endif /* MODULE_UHCPD */
 
-#ifdef UHCP_CLIENT
+#ifdef MODULE_UHCPC
 void uhcp_handle_push(uhcp_push_t *req, uint8_t *src, uint16_t port, uhcp_iface_t iface)
 {
     char addr_str[INET6_ADDRSTRLEN];
