0a72098c281a5a5d7697c3a997f3011d033eabfc
Koen Zandberg <koen@bergzand.net>
2019-03-19 20:30:55 +0100

    arduino-zero: add usbdev feature

--- boards/arduino-zero/include/periph_conf.h
+++ boards/arduino-zero/include/periph_conf.h
@@ -238,6 +238,20 @@ static const i2c_conf_t i2c_config[] = {
 #define RTT_RUNSTDBY        (1)         /* Keep RTT running in sleep states */
 /** @} */
 
+/**
+ * @name USB peripheral configuration
+ * @{
+ */
+static const sam0_common_usb_config_t sam_usbdev_config[] = {
+    {
+        .dm     = GPIO_PIN(PA, 24),
+        .dp     = GPIO_PIN(PA, 25),
+        .d_mux  = GPIO_MUX_G,
+        .device = &USB->DEVICE,
+    }
+};
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
