a72d0ef3e84e211ce06da9504151e92385ef657a
Martine Lenders <mail@martine-lenders.eu>
2019-03-25 12:45:09 +0100

    gnrc_netif: add packet to queue when device is busy
    ... and also send on send error (i.e. when *medium* was busy)

--- sys/include/net/gnrc/netif.h
+++ sys/include/net/gnrc/netif.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Freie Universität Berlin
+ * Copyright (C) 2017-20 Freie Universität Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -59,8 +59,8 @@
 #if IS_USED(MODULE_GNRC_NETIF_MAC)
 #include "net/gnrc/netif/mac.h"
 #endif
-#ifdef MODULE_GNRC_NETIF_PKTQ
-#include "net/gnrc/pktqueue.h"
+#if IS_USED(MODULE_GNRC_NETIF_PKTQ)
+#include "net/gnrc/netif/pktq/type.h"
 #endif
 #include "net/ndp.h"
 #include "net/netdev.h"
@@ -70,9 +70,6 @@
 #endif
 #include "rmutex.h"
 #include "net/netif.h"
-#ifdef MODULE_GNRC_NETIF_PKTQ
-#include "xtimer.h"
-#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -178,15 +175,13 @@ typedef struct {
 #if IS_USED(MODULE_GNRC_NETIF_6LO) || defined(DOXYGEN)
     gnrc_netif_6lo_t sixlo;                 /**< 6Lo component */
 #endif
-#if defined(MODULE_GNRC_NETIF_PKTQ) || DOXYGEN
+#if IS_USED(MODULE_GNRC_NETIF_PKTQ) || defined(DOXYGEN)
     /**
      * @brief   Packet queue for sending
      *
      * @note    Only available with @ref net_gnrc_netif_pktq.
      */
-    gnrc_pktqueue_t *send_queue;
-    msg_t dequeue_msg;                      /**< message for gnrc_netif_t::dequeue_timer to send */
-    xtimer_t dequeue_timer;                 /**< timer to schedule next sending of queued packets */
+    gnrc_netif_pktq_t send_queue;
 #endif
     uint8_t cur_hl;                         /**< Current hop-limit for out-going packets */
     uint8_t device_type;                    /**< Device type */
