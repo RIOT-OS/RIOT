818097f0dc3817470db23ce2b3aaa85c2942a9ea
Martine Lenders <mail@martine-lenders.eu>
2019-03-25 14:33:01 +0100

    tests: provide unittests for gnrc_netif_pktq

---
+++ tests/unittests/tests-gnrc_netif_pktq/tests-gnrc_netif_pktq.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup unittests
+ * @{
+ *
+ * @file
+ * @brief   unittests for the `gnrc_netif_pktq` module
+ *
+ * @author  Martine Lenders <m.lenders@fu-berlin.de>
+ */
+#ifndef TESTS_GNRC_NETIF_PKTQ_H
+#define TESTS_GNRC_NETIF_PKTQ_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_pktqueue(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_GNRC_NETIF_PKTQ_H */
+/** @} */
