b9a572f1b0baeb93162edeff662af010d27a11d9
Gunar Schorcht <gunar@schorcht.net>
2019-03-27 08:54:40 +0100

    boards/esp32: SPI configuration approach changed
    SPI devices are now configured using static array in header files instead of static variables in implementation to be able to define SPI_NUMOF using the size of the array instead of a variable.

--- boards/common/esp32/include/periph_conf_common.h
+++ boards/common/esp32/include/periph_conf_common.h
@@ -172,6 +172,30 @@ static const gpio_t pwm1_channels[] = PWM1_GPIOS;
  */
 
 /**
+ * @brief   Static array with configuration for declared I2C devices
+ */
+static const spi_conf_t spi_config[] = {
+#ifdef SPI0_CTRL
+    {
+        .ctrl = SPI0_CTRL,
+        .sck = SPI0_SCK,
+        .mosi = SPI0_MOSI,
+        .miso = SPI0_MISO,
+        .cs = SPI0_CS0,
+    },
+#endif
+#ifdef SPI1_CTRL
+    {
+        .ctrl = SPI1_CTRL,
+        .sck = SPI1_SCK,
+        .mosi = SPI1_MOSI,
+        .miso = SPI1_MISO,
+        .cs = SPI1_CS0,
+    },
+#endif
+};
+
+/**
  * @brief Number of SPI interfaces
  *
  * The number of SPI interfaces is determined from board-specific peripheral
@@ -179,7 +203,7 @@ static const gpio_t pwm1_channels[] = PWM1_GPIOS;
  *
  * @note SPI_NUMOF definition must not be changed.
  */
-#define SPI_NUMOF   (spi_bus_num)
+#define SPI_NUMOF   (sizeof(spi_config) / sizeof(spi_config[0]))
 
 /** @} */
 
