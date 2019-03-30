8ef9b8717d3341bec59490569baa46ea39585a06
Benjamin Valentin <benpicco@googlemail.com>
2019-03-30 23:14:11 +0100

    sam0_common: make RTC implementation common across all sam0 MCUs
    The currently supported SAM0 MCUs (samd21, saml21, saml1x) share the
    same RTC peripheral, yet each of them carries it's own copy of the RTC
    driver.
    
    Unify the drivers and move them to sam0_common.

--- cpu/sam0_common/periph/rtc.c
+++ cpu/sam0_common/periph/rtc.c
@@ -7,11 +7,13 @@
  */
 
 /**
- * @ingroup     cpu_saml21
+ * @ingroup     cpu_sam0_common
  * @ingroup     drivers_periph_rtc
  * @{
+ *
  * @file
  * @brief       Low-level RTC driver implementation
+ *
  * @author      Baptiste Clenet <bapclenet@gmail.com>
  * @autor       ported to SAML21 by FWX <FWX@dialine.fr>
  * @}
@@ -41,13 +43,45 @@ static rtc_state_t rtc_callback;
 * Thanks to this, the user will be able to set time in 2000's*/
 static uint16_t reference_year = 100;
 
-void rtc_init(void)
+static void _wait_syncbusy(void)
 {
-    /* Turn on power manager for RTC */
-    MCLK->APBAMASK.reg |= MCLK_APBAMASK_RTC | MCLK_APBAMASK_OSC32KCTRL;
-    /* DISABLE RTC MASTER */
-    rtc_poweroff();
+#ifdef REG_RTC_MODE2_SYNCBUSY
+    while (RTC->MODE2.SYNCBUSY.reg) {}
+#else
+    while (RTC->MODE2.STATUS.bit.SYNCBUSY) {}
+#endif
+}
 
+static inline void _rtc_set_enabled(bool on)
+{
+#ifdef REG_RTC_MODE2_CTRLA
+    RTC->MODE2.CTRLA.bit.ENABLE = on;
+#else
+    RTC->MODE2.CTRL.bit.ENABLE = on;
+#endif
+    _wait_syncbusy();
+}
+
+#ifdef CPU_SAMD21
+static void _rtc_clock_setup(void)
+{
+    /* RTC uses External 32,768KHz Oscillator (OSC32K isn't accurate enough p1075/1138)*/
+    SYSCTRL->XOSC32K.reg =  SYSCTRL_XOSC32K_ONDEMAND |
+                            SYSCTRL_XOSC32K_EN32K |
+                            SYSCTRL_XOSC32K_XTALEN |
+                            SYSCTRL_XOSC32K_STARTUP(6) |
+                            SYSCTRL_XOSC32K_ENABLE;
+
+    /* Setup clock GCLK2 with OSC32K divided by 32 */
+    GCLK->GENDIV.reg = GCLK_GENDIV_ID(2)|GCLK_GENDIV_DIV(4);
+    GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_XOSC32K | GCLK_GENCTRL_ID(2) | GCLK_GENCTRL_DIVSEL );
+    GCLK->CLKCTRL.reg = (uint32_t)((GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK2 | (RTC_GCLK_ID << GCLK_CLKCTRL_ID_Pos)));
+    while (GCLK->STATUS.bit.SYNCBUSY) {}
+}
+#else
+static void _rtc_clock_setup(void)
+{
+    MCLK->APBAMASK.reg |= MCLK_APBAMASK_OSC32KCTRL;
 
 #if EXTERNAL_OSC32_SOURCE
 
@@ -58,7 +92,7 @@ void rtc_init(void)
                             | OSC32KCTRL_XOSC32K_ENABLE;
 
     /* Wait XOSC32K Ready */
-    while (OSC32KCTRL->STATUS.bit.XOSC32KRDY==0);
+    while (OSC32KCTRL->STATUS.bit.XOSC32KRDY==0) {}
 
     /* RTC source clock is external oscillator at 1kHz */
     OSC32KCTRL->RTCCTRL.reg = OSC32KCTRL_RTCCTRL_RTCSEL_XOSC1K;
@@ -66,8 +100,8 @@ void rtc_init(void)
 #endif /* EXTERNAL_OSC32_SOURCE */
 
 #if INTERNAL_OSC32_SOURCE
- uint32_t * pCalibrationArea;
- uint32_t osc32kcal;
+    uint32_t * pCalibrationArea;
+    uint32_t osc32kcal;
 
     /* Read OSC32KCAL, calibration data for OSC32 !!! */
     pCalibrationArea = (uint32_t*) NVMCTRL_OTP5;
@@ -80,7 +114,7 @@ void rtc_init(void)
                            | OSC32KCTRL_OSC32K_ENABLE;
 
     /* Wait OSC32K Ready */
-    while (OSC32KCTRL->STATUS.bit.OSC32KRDY==0);
+    while (OSC32KCTRL->STATUS.bit.OSC32KRDY==0) {}
 
     /* RTC uses internal 32,768KHz Oscillator */
     OSC32KCTRL->RTCCTRL.reg = OSC32KCTRL_RTCCTRL_RTCSEL_OSC1K;
@@ -94,22 +128,31 @@ void rtc_init(void)
     OSC32KCTRL->RTCCTRL.reg = OSC32KCTRL_RTCCTRL_RTCSEL_ULP1K;
 
 #endif /* ULTRA_LOW_POWER_INTERNAL_OSC_SOURCE */
+}
+#endif /* CPU_SAMD21 - Clock Setup */
 
-    /* Software Reset the RTC */
-    RTC->MODE2.CTRLA.bit.SWRST = 1;
-    /* Wait end of reset */
-    while (RTC->MODE2.CTRLA.bit.SWRST);
+void rtc_init(void)
+{
+    _rtc_clock_setup();
+    rtc_poweron();
+    _rtc_set_enabled(0);
 
     /* RTC config with RTC_MODE2_CTRL_CLKREP = 0 (24h) */
-    RTC->MODE2.CTRLA.reg = RTC_MODE2_CTRLA_PRESCALER_DIV1024 |  /* CLK_RTC_CNT = 1KHz / 1024 -> 1Hz */
-                           RTC_MODE2_CTRLA_CLOCKSYNC         |  /* Clock Read Synchronization Enable */
-                           RTC_MODE2_CTRLA_MODE_CLOCK;          /* Mode 2: Clock/Calendar */
+#ifdef REG_RTC_MODE2_CTRLA
+    RTC->MODE2.CTRLA.reg = RTC_MODE2_CTRLA_PRESCALER_DIV1024   /* CLK_RTC_CNT = 1KHz / 1024 -> 1Hz */
+                         | RTC_MODE2_CTRLA_CLOCKSYNC           /* Clock Read Synchronization Enable */
+                         | RTC_MODE2_CTRLA_MODE_CLOCK;
+#else
+    RTC->MODE2.CTRL.reg = RTC_MODE2_CTRL_PRESCALER_DIV1024
+                        | RTC_MODE2_CTRL_MODE_CLOCK;
+#endif
+    RTC->MODE2.INTENSET.reg = RTC_MODE2_INTENSET_OVF;
 
     /* Clear interrupt flags */
     RTC->MODE2.INTFLAG.reg |= RTC_MODE2_INTFLAG_OVF;
     RTC->MODE2.INTFLAG.reg |= RTC_MODE2_INTFLAG_ALARM0;
 
-    rtc_poweron();
+    _rtc_set_enabled(1);
 }
 
 int rtc_set_time(struct tm *time)
@@ -118,16 +161,16 @@ int rtc_set_time(struct tm *time)
         return -1;
     }
     else {
-        while (RTC->MODE2.SYNCBUSY.bit.CLOCK);
         RTC->MODE2.CLOCK.reg = RTC_MODE2_CLOCK_YEAR(time->tm_year - reference_year)
                              | RTC_MODE2_CLOCK_MONTH(time->tm_mon + 1)
                              | RTC_MODE2_CLOCK_DAY(time->tm_mday)
                              | RTC_MODE2_CLOCK_HOUR(time->tm_hour)
                              | RTC_MODE2_CLOCK_MINUTE(time->tm_min)
                              | RTC_MODE2_CLOCK_SECOND(time->tm_sec);
-        while (RTC->MODE2.SYNCBUSY.bit.CLOCK);
     }
-   return 0;
+
+    _wait_syncbusy();
+    return 0;
 }
 
 int rtc_get_time(struct tm *time)
@@ -163,8 +206,9 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
                                            | RTC_MODE2_ALARM_MINUTE(time->tm_min)
                                            | RTC_MODE2_ALARM_SECOND(time->tm_sec);
         RTC->MODE2.Mode2Alarm[0].MASK.reg = RTC_MODE2_MASK_SEL(6);
-        while (RTC->MODE2.SYNCBUSY.bit.ALARM0);
     }
+
+    _wait_syncbusy();
 
     /* Setup interrupt */
     NVIC_EnableIRQ(RTC_IRQn);
@@ -180,7 +224,7 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
 
 int rtc_get_alarm(struct tm *time)
 {
- RTC_MODE2_ALARM_Type alarm;
+    RTC_MODE2_ALARM_Type alarm;
 
     /* Read alarm register in one time */
     alarm.reg = RTC->MODE2.Mode2Alarm[0].ALARM.reg;
@@ -208,14 +252,20 @@ void rtc_clear_alarm(void)
 
 void rtc_poweron(void)
 {
-    RTC->MODE2.CTRLA.bit.ENABLE = 1;
-    while (RTC->MODE2.SYNCBUSY.bit.ENABLE);
+#ifdef MCLK
+    MCLK->APBAMASK.reg |= MCLK_APBAMASK_RTC;
+#else
+    PM->APBAMASK.reg |= PM_APBAMASK_RTC;
+#endif
 }
 
 void rtc_poweroff(void)
 {
-    RTC->MODE2.CTRLA.bit.ENABLE = 0;
-    while (RTC->MODE2.SYNCBUSY.bit.ENABLE);
+#ifdef MCLK
+    MCLK->APBAMASK.reg &= ~MCLK_APBAMASK_RTC;
+#else
+    PM->APBAMASK.reg &= ~PM_APBAMASK_RTC;
+#endif
 }
 
 void isr_rtc(void)
