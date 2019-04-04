0e1816a83e0bad78fa08e13dfee53ccb0644a75b
Martine Lenders <m.lenders@fu-berlin.de>
2019-04-04 11:52:48 +0200

    fixup! fixup! tests: provide tests for ubasic package

--- pkg/ubasic/ubasic.mk
+++ pkg/ubasic/ubasic.mk
@@ -1,6 +1,6 @@
+MODULE = ubasic
+
 SRC := tokenizer.c ubasic.c
 NO_AUTO_SRC := 1
-
-MODULE = ubasic
 
 include $(RIOTBASE)/Makefile.base
--- pkg/ubasic/ubasic_tests.mk
+++ pkg/ubasic/ubasic_tests.mk
@@ -1,6 +1,6 @@
+MODULE = ubasic_tests
+
 SRC := tests.c
 NO_AUTO_SRC := 1
-
-MODULE = ubasic_tests
 
 include $(RIOTBASE)/Makefile.base
