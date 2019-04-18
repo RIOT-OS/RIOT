43dedfcd2c52c15fb083e0d984c23c6f6e550cb6
Martine Lenders <m.lenders@fu-berlin.de>
2019-04-18 12:21:47 +0200

    gnrc_sixlowpan_frag/rbuf: document undocumented functions
    This adds documentation for the following undocumented functions:
    
    - rbuf_rm()
    - rbuf_entry_empty()

--- sys/net/gnrc/network_layer/sixlowpan/frag/rbuf.h
+++ sys/net/gnrc/network_layer/sixlowpan/frag/rbuf.h
@@ -96,8 +96,28 @@ void rbuf_add(gnrc_netif_hdr_t *netif_hdr, gnrc_pktsnip_t *frag,
  */
 void rbuf_gc(void);
 
+/**
+ * @brief   Unsets a reassembly buffer entry (but does not free
+ *          rbuf_t::super::pkt)
+ *
+ * This functions sets rbuf_t::super::pkt to NULL and removes all rbuf::ints.
+ *
+ * @param[in] rbuf  A reassembly buffer entry
+ *
+ * @internal
+ */
 void rbuf_rm(rbuf_t *rbuf);
 
+/**
+ * @brief   Checks if a reassembly buffer entry is unset
+ *
+ * @param[in] rbuf  A reassembly buffer entry
+ *
+ * @return  true, if @p rbuf is empty (i.e. rbuf->super.pkt is NULL).
+ * @return  false, if @p rbuf is in use.
+ *
+ * @internal
+ */
 static inline bool rbuf_entry_empty(const rbuf_t *rbuf) {
     return (rbuf->super.pkt == NULL);
 }
