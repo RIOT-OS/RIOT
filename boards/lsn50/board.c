5f1ee2d8b6dfbb277e692f8dd1b8af273e0ea3e4
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-05-08 16:54:49 +0200

    boards/lsn50: add initial support

---
+++ boards/lsn50/board.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2019 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_lsn50
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the LSN50 board
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+}
