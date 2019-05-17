26999188171276720ef4d18bdb5004d57c54b8f2
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-05-17 13:52:34 +0200

    boards/tools: remove exporting FLASHER/FFLAGS
    FLASHER and FFLAGS are evaluated by the main Makefile.include or by file
    included by it. Their value does not need to be exported.
    
    This will also prevent evaluating 'PORT' for FFLAGS when not needed.
    
    Testing
    -------
    
    `git diff --word-diff` only reports `export` being removed.
    
    `git show --stat` reports `84 insertions(+), 84 deletions(-)`
    Which is the same amount as lines that where matching
    `export[[:blank::]]\+VARIABLE`.

--- makefiles/vars.inc.mk
+++ makefiles/vars.inc.mk
@@ -69,8 +69,8 @@ export WERROR                # Treat all compiler warnings as errors if set to 1
 
 export GITCACHE              # path to git-cache executable
 export GIT_CACHE_DIR         # path to git-cache cache directory
-export FLASHER               # The command to call on "make flash".
-export FFLAGS                # The parameters to supply to FLASHER.
+# FLASHER                    # The command to call on "make flash".
+# FFLAGS                     # The parameters to supply to FLASHER.
 export FLASH_ADDR            # Define an offset to flash code into ROM memory.
 # TERMPROG                   # The command to call on "make term".
 # TERMFLAGS                  # Additional parameters to supply to TERMPROG.
