406ecb404cf352d9084510d2e1f6efc6b71f58b4
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-05-20 16:32:32 +0200

    WIP CLEANUP TESTING NEEDED pkg/pkg.mk: use intermediate state files
    Rely on file creation and dependencies instead of .PHONY targets.
    Files will be rebuilt when changing version as the main `Makefile` will
    have been updated. All steps are re-done on version change.
    
    When deleting patches, the '.prepare' step should be redone thanks
    to the included 'patch-dep.inc' file (TODO TEST ME).
    
    Implementation in order:
    
    * '.git': means the repository has been cloned.
    * '.git-downloaded': Fetches the wanted version
    * '.git-prepared': will clean checkout the version and apply patches

--- pkg/pkg.mk
+++ pkg/pkg.mk
@@ -1,6 +1,9 @@
 #
 # Include this file if your Package needs to be checked out by git
 #
+# A package is up to date when its '.git-prepared' file is up to date.
+# Any change to the package makefile will force updating the repository
+#
 # Packages should include this file just after defining the PKG_* variables
 # This will ensure the variables are defined in the Makefile.
 ifneq (,$(.DEFAULT_GOAL))
@@ -20,8 +23,8 @@ ifeq (,$(PKG_LICENSE))
   $(error PKG_LICENSE not defined)
 endif
 
-PKG_DIR?=$(CURDIR)
-PKG_BUILDDIR?=$(PKGDIRBASE)/$(PKG_NAME)
+PKG_DIR ?= $(CURDIR)
+PKG_BUILDDIR ?= $(PKGDIRBASE)/$(PKG_NAME)
 PKG_SOURCE_LOCAL ?= $(PKG_SOURCE_LOCAL_$(shell echo $(PKG_NAME) | tr a-z- A-Z_))
 
 # allow overriding package source with local folder (useful during development)
@@ -29,51 +32,58 @@ ifneq (,$(PKG_SOURCE_LOCAL))
   include $(RIOTBASE)/pkg/local.mk
 else
 
-.PHONY: prepare git-download clean git-ensure-version
-
-prepare: git-download
-
-ifneq (,$(wildcard $(PKG_DIR)/patches))
-git-download: $(PKG_BUILDDIR)/.git-patched
-else
-git-download: git-ensure-version
-endif
-
 GITFLAGS ?= -c user.email=buildsystem@riot -c user.name="RIOT buildsystem"
 GITAMFLAGS ?= --no-gpg-sign --ignore-whitespace
 
-ifneq (,$(wildcard $(PKG_DIR)/patches))
-$(PKG_BUILDDIR)/.git-patched: git-ensure-version $(PKG_DIR)/Makefile $(PKG_DIR)/patches/*.patch
+.PHONY: all prepare git-download clean distclean
+
+PKG_PREPARED = $(PKG_BUILDDIR)/.git-prepared
+PKG_PATCHES = $(sort $(wildcard $(PKG_DIR)/patches/*.patch))
+PKG_PATCH_DEP_INC = $(PKG_BUILDDIR)/.git-patch-dep.inc
+
+# Declare 'all' first to have it being the default target
+all: $(PKG_PREPARED)
+prepare: $(PKG_PREPARED)
+
+# Compat with current packages
+git-download: $(PKG_PREPARED)
+
+
+# Create the makefile include file only on request
+# It ensures rebuild of '.git-prepared' on patches/ deletion
+.PHONY: _pkg_inc_file
+_pkg_inc_file: | $(PKG_BUILDDIR)/.git
+	@echo "$(PKG_BUILDDIR)/.git-prepared: $(PKG_PATCHES)" > $(PKG_PATCH_DEP_INC)
+	@for patch in $(PKG_PATCHES); do echo "$(patch):" >> $(PKG_PATCH_DEP_INC); done
+
+# Prepare the package
+# * clean, without removing the pre-steps state files
+# * checkout the wanted base commit
+# * apply patches if there are any. (If none, it does nothing)
+$(PKG_BUILDDIR)/.git-prepared: $(PKG_PATCHES) $(PKG_BUILDDIR)/.git-downloaded $(MAKEFILE_LIST) | _pkg_inc_file
+	git -C $(PKG_BUILDDIR) clean -xdff '**' ':!.git-downloaded' ':!.git-patch-dep.inc'
 	git -C $(PKG_BUILDDIR) checkout -f $(PKG_VERSION)
-	git $(GITFLAGS) -C $(PKG_BUILDDIR) am $(GITAMFLAGS) "$(PKG_DIR)"/patches/*.patch
-	touch $@
-endif
+	git $(GITFLAGS) -C $(PKG_BUILDDIR) am $(GITAMFLAGS) $(PKG_PATCHES) </dev/null
+	@touch $@
 
-git-ensure-version: $(PKG_BUILDDIR)/.git-downloaded
-	if [ $(shell git -C $(PKG_BUILDDIR) rev-parse HEAD) != $(PKG_VERSION) ] ; then \
-		git -C $(PKG_BUILDDIR) clean -xdff ; \
-		git -C $(PKG_BUILDDIR) fetch "$(PKG_URL)" "$(PKG_VERSION)" ; \
-		git -C $(PKG_BUILDDIR) checkout -f $(PKG_VERSION) ; \
-		touch $(PKG_BUILDDIR)/.git-downloaded ; \
-	fi
+$(PKG_BUILDDIR)/.git-downloaded: $(MAKEFILE_LIST) | $(PKG_BUILDDIR)/.git
+	git -C $(PKG_BUILDDIR) fetch $(PKG_URL) $(PKG_VERSION)
+	echo $(PKG_VERSION) > $@
 
-$(PKG_BUILDDIR)/.git-downloaded:
+$(PKG_BUILDDIR)/.git: $(MAKEFILE_LIST)
 	rm -Rf $(PKG_BUILDDIR)
 	mkdir -p $(PKG_BUILDDIR)
-	$(GITCACHE) clone "$(PKG_URL)" "$(PKG_VERSION)" "$(PKG_BUILDDIR)"
-	touch $@
+	$(GITCACHE) clone $(PKG_URL) $(PKG_VERSION) $(PKG_BUILDDIR)
 
 clean::
-	@test -d $(PKG_BUILDDIR) && { \
-		rm $(PKG_BUILDDIR)/.git-patched ; \
-		git -C $(PKG_BUILDDIR) clean -f ; \
-		git -C $(PKG_BUILDDIR) checkout "$(PKG_VERSION)"; \
-		make $(PKG_BUILDDIR)/.git-patched ; \
-		touch $(PKG_BUILDDIR)/.git-downloaded ; \
-	} > /dev/null 2>&1 || true
+	@test -d $(PKG_BUILDDIR) && \
+		git -C $(PKG_BUILDDIR) clean -xdff || \
+		true
 
 distclean::
-	rm -rf "$(PKG_BUILDDIR)"
+	rm -rf $(PKG_BUILDDIR)
+
+-include $(PKG_PATCH_DEP_INC)
 
 # Reset goal for package
 .DEFAULT_GOAL =
