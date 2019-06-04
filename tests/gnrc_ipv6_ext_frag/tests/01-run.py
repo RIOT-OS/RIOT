e62bb9c414517b72a24313a8fd17dd8933102a79
Martine Lenders <m.lenders@fu-berlin.de>
2019-06-04 19:59:39 +0200

    tests: provide tests for gnrc_ipv6_ext_frag fragmentation

--- tests/gnrc_ipv6_ext_frag/tests/01-run.py
+++ tests/gnrc_ipv6_ext_frag/tests/01-run.py
@@ -14,10 +14,14 @@ import sys
 import subprocess
 import time
 
-from scapy.all import Ether, IPv6, IPv6ExtHdrFragment, sendp
+from scapy.all import Ether, ICMPv6PacketTooBig, IPv6, IPv6ExtHdrFragment, \
+                      UDP, raw, sendp, srp1
 from testrunner import run
 
 
+RECV_BUFSIZE = 2 * 1500
+TEST_SAMPLE = b"This is a test. Failure might sometimes be an option, but " \
+              b"not today. "
 EXT_HDR_NH = {
     IPv6ExtHdrFragment: 44,
   }
@@ -52,6 +56,13 @@ def stop_udp_server(child):
     # either way: it is stopped
     child.expect(["Success: stopped UDP server",
                   "Error: server was not running"])
+
+
+def udp_send(child, addr, port, length, num=1, delay=1000000):
+    child.sendline("udp send {addr}%6 {port} {length} {num} {delay}"
+                   .format(**vars()))
+    child.expect("Success: send {length} byte to \[[0-9a-f:]+\]:{port}"
+                 .format(**vars()))
 
 
 def check_and_search_output(cmd, pattern, res_group, *args, **kwargs):
@@ -145,6 +156,165 @@ def test_reass_offset_too_large(child, iface, hw_dst, ll_dst, ll_src):
     pktbuf_empty(child)
 
 
+def test_ipv6_ext_frag_shell_test_0(child, s, iface, ll_dst):
+    child.sendline("test {} 0".format(ll_dst))
+    data, _ = s.recvfrom(RECV_BUFSIZE)
+    assert data == TEST_SAMPLE
+    pktbuf_empty(child)
+
+
+def test_ipv6_ext_frag_shell_test_1(child, s, iface, ll_dst):
+    child.sendline("test {} 1".format(ll_dst))
+    data, _ = s.recvfrom(RECV_BUFSIZE)
+    offset = 0
+    while (offset < len(data)):
+        assert data[offset:(offset + len(TEST_SAMPLE))] == TEST_SAMPLE
+        offset += len(TEST_SAMPLE)
+    pktbuf_empty(child)
+
+
+def test_ipv6_ext_frag_send_success(child, s, iface, ll_dst):
+    length = get_host_mtu(iface)
+    port = s.getsockname()[1]
+    udp_send(child, ll_dst, port, length)
+    data, _ = s.recvfrom(length)
+    assert len(data) == length
+    pktbuf_empty(child)
+
+
+def test_ipv6_ext_frag_send_last_fragment_filled(child, s, iface, ll_dst):
+    # every fragment has an IPv6 header and a fragmentation header so subtract
+    # them
+    mtu = get_host_mtu(iface) - len(IPv6() / IPv6ExtHdrFragment())
+    # first fragment has UDP header (so subtract it) and is rounded down to
+    # the nearest multiple of 8
+    length = (mtu - len(UDP())) & 0xfff8
+    # second fragment fills the whole available MTU
+    length += mtu
+    port = s.getsockname()[1]
+    udp_send(child, ll_dst, port, length)
+    data, _ = s.recvfrom(length)
+    assert len(data) == length
+    pktbuf_empty(child)
+
+
+def test_ipv6_ext_frag_send_last_fragment_only_one_byte(child, s,
+                                                        iface, ll_dst):
+    mtu = get_host_mtu(iface)
+    # subtract IPv6 and UDP header as they are not part of the UDP payload
+    length = (mtu - len(IPv6() / UDP()))
+    length += 1
+    port = s.getsockname()[1]
+    udp_send(child, ll_dst, port, length)
+    data, _ = s.recvfrom(length)
+    assert len(data) == length
+    pktbuf_empty(child)
+
+
+def test_ipv6_ext_frag_send_full_pktbuf(child, s, iface, ll_dst):
+    length = pktbuf_size(child)
+    # remove some slack for meta-data and header and 1 addition fragment header
+    length -= (len(IPv6() / IPv6ExtHdrFragment() / UDP()) +
+               (len(IPv6() / IPv6ExtHdrFragment())) + 96)
+    port = s.getsockname()[1]
+    # trigger neighbor discovery so it doesn't fill the packet buffer
+    udp_send(child, ll_dst, port, 1)
+    data, _ = s.recvfrom(1)
+    last_nd = time.time()
+    count = 0
+    while True:
+        if (time.time() - last_nd) > 5:
+            # trigger neighbor discovery so it doesn't fill the packet buffer
+            udp_send(child, ll_dst, port, 1)
+            data, _ = s.recvfrom(1)
+            last_nd = time.time()
+        udp_send(child, ll_dst, port, length)
+        count += 1
+        try:
+            data, _ = s.recvfrom(length)
+        except socket.timeout:
+            # 8 is the alignment unit of the packet buffer
+            # and 20 the size of a packet snip, so take next multiple of 8 to
+            # 28
+            length -= 24
+        else:
+            break
+        finally:
+            pktbuf_empty(child)
+    assert(count > 1)
+
+
+def _fwd_setup(child, ll_dst, g_src, g_dst):
+    # check if interface is configured properly
+    child.sendline("ifconfig 7")
+    child.expect(r"MTU:(\d+)")
+    mtu = int(child.match.group(1))
+    # configure routes
+    child.sendline("nib route add 7 {}/128 fe80::1".format(g_dst))
+    child.sendline("nib route add 6 {}/128 {}".format(g_src, ll_dst))
+    child.sendline("nib route")
+    child.expect(r"{}/128 via fe80::1 dev #7".format(g_dst))
+    child.expect(r"{}/128 via {} dev #6".format(g_src, ll_dst))
+    child.sendline("nib neigh add 7 fe80::1")
+    child.sendline("nib neigh")
+    child.expect(r"fe80::1 dev #7 lladdr\s+-")
+    # get TAP MAC address
+    child.sendline("ifconfig 6")
+    child.expect("HWaddr: ([0-9A-F:]+)")
+    hwaddr = child.match.group(1)
+    # consume MTU for later calls of `ifconfig 7`
+    child.expect(r"MTU:(\d+)")
+    return mtu, hwaddr
+
+
+def _fwd_teardown(child):
+    # remove route
+    child.sendline("nib neigh del 7 fe80::1")
+    child.sendline("nib route del 7 affe::/64")
+
+
+def test_ipv6_ext_frag_fwd_success(child, s, iface, ll_dst):
+    mtu, dst_mac = _fwd_setup(child, ll_dst, "beef::1", "affe::1")
+    payload_fit = mtu - len(IPv6() / IPv6ExtHdrFragment() / UDP())
+    pkt = Ether(dst=dst_mac) / IPv6(src="beef::1", dst="affe::1") / \
+        IPv6ExtHdrFragment(m=True, id=0x477384a9) / \
+        UDP(sport=1337, dport=1337) / ("x" * payload_fit)
+    # fill missing fields
+    pkt = Ether(raw(pkt))
+    sendp(pkt, verbose=0, iface=iface)
+    # check hexdump of mock device
+    ipv6 = pkt[IPv6]
+    ipv6.hlim -= 1  # the packet will have passed a hop
+    # segment packet as GNRC does
+    segments = [bytes(ipv6)[:40], bytes(ipv6.payload)]
+    for seg in segments:
+        addr = 0
+        for i in range(0, len(seg), 16):
+            bs = seg[i:i+16]
+            exp_str = ("{:08X}" + ("  {:02X}") * len(bs)).format(addr, *bs)
+            child.expect_exact(exp_str)
+            addr += 16
+    _fwd_teardown(child)
+
+
+def test_ipv6_ext_frag_fwd_too_big(child, s, iface, ll_dst):
+    mtu, dst_mac = _fwd_setup(child, ll_dst, "beef::1", "affe::1")
+    assert(get_host_mtu(iface) > mtu)
+    payload_fit = get_host_mtu(iface) - len(IPv6() / IPv6ExtHdrFragment() /
+                                            UDP())
+    pkt = srp1(Ether(dst=dst_mac) / IPv6(src="beef::1", dst="affe::1") /
+               IPv6ExtHdrFragment(m=True, id=0x477384a9) /
+               UDP(sport=1337, dport=1337) / ("x" * payload_fit),
+               timeout=2, verbose=0, iface=iface)
+    # packet should not be fragmented further but an ICMPv6 error should be
+    # returned instead
+    assert(pkt is not None)
+    assert(ICMPv6PacketTooBig in pkt)
+    assert(IPv6ExtHdrFragment in pkt)
+    assert(pkt[IPv6ExtHdrFragment].id == 0x477384a9)
+    _fwd_teardown(child)
+
+
 def testfunc(child):
     tap = get_bridge(os.environ["TAP"])
 
@@ -152,12 +322,45 @@ def testfunc(child):
     print("." * int(child.match.group(1)), end="", flush=True)
 
     lladdr_src = get_host_lladdr(tap)
+
+    def run_sock_test(func, s):
+        if child.logfile == sys.stdout:
+            func(child, s, tap, lladdr_src)
+        else:
+            try:
+                func(child, s, tap, lladdr_src)
+                print(".", end="", flush=True)
+            except PermissionError:
+                print("\n\x1b[1;33mSkipping {} because of missing "
+                      "privileges\x1b[0m".format(func.__name__))
+            except Exception as e:
+                print("FAILED")
+                raise e
+
+    child.expect(r"Sending UDP test packets to port (\d+)")
+
+    port = int(child.match.group(1))
+    with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as s:
+        res = socket.getaddrinfo("{}%{}".format(lladdr_src, tap), port)
+        s.bind(res[0][4])
+        s.settimeout(.3)
+        run_sock_test(test_ipv6_ext_frag_shell_test_0, s)
+        run_sock_test(test_ipv6_ext_frag_shell_test_1, s)
+        run_sock_test(test_ipv6_ext_frag_send_success, s)
+        run_sock_test(test_ipv6_ext_frag_send_last_fragment_filled, s)
+        run_sock_test(test_ipv6_ext_frag_send_last_fragment_only_one_byte, s)
+        run_sock_test(test_ipv6_ext_frag_send_full_pktbuf, s)
+        run_sock_test(test_ipv6_ext_frag_fwd_success, s)
+        run_sock_test(test_ipv6_ext_frag_fwd_too_big, s)
+
     if os.environ.get("BOARD", "") != "native":
         # ethos currently can't handle the larger, rapidly sent packets by the
         # IPv6 fragmentation of the Linux Kernel
-        print("SUCCESS for unittests.")
-        print("Skipping interaction tests due to ethos bug.")
+        print("SUCCESS")
+        print("Skipping datagram reception tests due to ethos bug.")
         return
+
+    # datagram reception tests
     res = 1
     count = 0
     while res:
@@ -197,4 +400,9 @@ def testfunc(child):
 
 
 if __name__ == "__main__":
-    sys.exit(run(testfunc, timeout=1, echo=False))
+    if os.geteuid() != 0:
+        print("\x1b[1;31mThis test requires root privileges.\n"
+              "It's constructing and sending Ethernet frames.\x1b[0m\n",
+              file=sys.stderr)
+        sys.exit(1)
+    sys.exit(run(testfunc, timeout=2, echo=False))
