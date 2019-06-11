dc0b0520d9aa3910c1d388227c2c36f4ba51d344
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-06-11 17:06:00 +0200

    examples/ccn-lite-relay: remove use of MODULE_NETIF macro
    Since gnrc_netif module is loaded, this is not needed

--- examples/ccn-lite-relay/main.c
+++ examples/ccn-lite-relay/main.c
@@ -50,11 +50,9 @@ int main(void)
         return -1;
     }
 
-#ifdef MODULE_NETIF
     gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
                                                           gnrc_pktdump_pid);
     gnrc_netreg_register(GNRC_NETTYPE_CCN_CHUNK, &dump);
-#endif
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
