a9c7e31d1701e76ae15464bebe66089710f3cd24
francisco <femolina@uc.cl>
2019-06-12 16:22:24 +0200

    stm32f4/cpu_conf: add stm32f4 flashpage and flashsector config
    -stm32f4 doesn't have pages but sectors, flashpage is used as a
     wrapper around sectors

--- cpu/stm32f4/include/cpu_conf.h
+++ cpu/stm32f4/include/cpu_conf.h
@@ -55,6 +55,41 @@ extern "C" {
 #define CPU_FLASH_BASE                  FLASH_BASE
 /** @} */
 
+/**
+ * @brief   Flash sector configuration
+ *
+ * @{
+ */
+#if (defined(CPU_LINE_STM32F429xx) || defined(CPU_LINE_STM32F437xx)) && \
+    (STM32_FLASHSIZE == (2048*1024))
+#define FLASH_DUAL_BANK           (1)
+#else
+#define FLASH_DUAL_BANK           (0)
+#endif
+#define FLASHSECTOR_SIZE_MIN      (16*1024U)
+/** @} */
+
+/**
+ * @brief   Flash page configuration
+ *
+ *          NOTE: STM32F4 flash is organized in sectors, FLASHPAGE_* is
+ *                defined as a wrapper over sectors.
+ *
+ * @{
+ */
+/* To keep the same flashpage functionality an arbitrary 1K < FLASHSECTOR_SIZE_MIN
+ * (size of smaller sector) is defined
+ */
+#define FLASHPAGE_SIZE            (1024)
+#define FLASHPAGE_NUMOF           (STM32_FLASHSIZE / FLASHPAGE_SIZE)
+/* The minimum block size which can be written is 4B. However, the erase
+ * depends on the specific sector.
+ */
+#define FLASHPAGE_RAW_BLOCKSIZE    (4U)
+/* Writing should be always 4 bytes aligned */
+#define FLASHPAGE_RAW_ALIGNMENT    (4U)
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
