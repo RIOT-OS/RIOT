78b1ff35a131d6ace2c031dbff15297c772991bc
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-06-12 16:49:38 +0200

    uniflash.inc.mk: add variables for configuration files
    Refactoring to use common variables for the configuration files.
    
    This will allow overwriting the files more easily from outside.

--- makefiles/tools/uniflash.inc.mk
+++ makefiles/tools/uniflash.inc.mk
@@ -1,11 +1,15 @@
 # http://www.ti.com/tool/uniflash
 FLASHFILE ?= $(ELFFILE)
 
+UNIFLASH_CONFIG_CCXML ?= $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).ccxml
+UNIFLASH_CONFIG_DAT ?= $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).dat
+UNIFLASH_CONFIG_GDB ?= $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_gdb.conf
+
 export UNIFLASH_PATH ?= "UNIFLASH_PATH unconfigured"
 # check which uniflash version is available, either 4.x or 3.x
 ifneq ("$(wildcard $(UNIFLASH_PATH)/dslite.sh)","")
   FLASHER ?= $(UNIFLASH_PATH)/dslite.sh
-  FFLAGS  = --config $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).ccxml $(FLASHFILE)
+  FFLAGS  = --config $(UNIFLASH_CONFIG_CCXML) $(FLASHFILE)
   # configure uniflash for resetting target
   # xds110reset path changed in version UniFlash v4.4.0
   # Try to detect the newest one and fallback to only 'xds110reset'
@@ -15,15 +19,15 @@ ifneq ("$(wildcard $(UNIFLASH_PATH)/dslite.sh)","")
   RESET ?= $(XDS110RESET)
 else
   FLASHER = $(UNIFLASH_PATH)/uniflash.sh
-  FFLAGS  = -ccxml $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).ccxml -program $(FLASHFILE)
+  FFLAGS  = -ccxml $(UNIFLASH_CONFIG_CCXML) -program $(FLASHFILE)
   # configure uniflash for resetting target
   RESET ?= $(UNIFLASH_PATH)/uniflash.sh
-  RESET_FLAGS ?= -ccxml $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).ccxml -reset
+  RESET_FLAGS ?= -ccxml $(UNIFLASH_CONFIG_CCXML) -reset
 endif
 # configure the debug server
 DEBUGSERVER = $(UNIFLASH_PATH)/ccs_base/common/uscif/gdb_agent_console
-DEBUGSERVER_FLAGS = -p 3333 $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_$(XDEBUGGER).dat
+DEBUGSERVER_FLAGS = -p 3333 $(UNIFLASH_CONFIG_DAT)
 
 # configure the debugging tool
 DEBUGGER = $(PREFIX)gdb
-DEBUGGER_FLAGS = -x $(RIOTBOARD)/$(BOARD)/dist/$(CPU_MODEL)_gdb.conf $(ELFFILE)
+DEBUGGER_FLAGS = -x $(UNIFLASH_CONFIG_GDB) $(ELFFILE)
