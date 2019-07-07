1e7409a4f0d3662ccbd6e89580e3fa3100f4c77f
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-07-07 13:09:13 +0200

    boards/nucleo*: cleanup board doxygen group definition
    These boards were providing an empty doc.txt and the group definition was still in the periph_conf.h file

--- boards/nucleo-l433rc/include/periph_conf.h
+++ boards/nucleo-l433rc/include/periph_conf.h
@@ -7,9 +7,7 @@
  */
 
 /**
- * @defgroup    boards_nucleo-l433rc STM32 Nucleo-L433RC
- * @ingroup     boards_common_nucleo64
- * @brief       Support for the STM32 Nucleo-L433RC
+ * @ingroup     boards_nucleo-l433rc
  * @{
  *
  * @file
