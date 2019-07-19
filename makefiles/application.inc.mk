b4da3b464ff65017c8cff8d9dd65c194ebcf8060
Kaspar Schleiser <kaspar@schleiser.de>
2019-07-19 12:20:28 +0200

    make: pass BLOBS to makefiles/application.inc.mk
    For regular modules, adding files to BLOBS is sufficient to create the
    corresponding headers.
    
    Application modules are different, as they use a minimal makefile
    (makefiles.application.inc.mk) to build, thus application level
    variables are not available.
    
    This commit makes Makefile.include pass BLOBS to the application
    Makefile as APPLICATION_BLOBS, and application.inc.mk use that variable
    as value for BLOBS.
    
    The indirection is necessary so submakefiles (e.g., those visited by
    DIRS) do not hard override BLOBS.

--- makefiles/application.inc.mk
+++ makefiles/application.inc.mk
@@ -3,4 +3,17 @@ MODULE = $(APPLICATION_MODULE)
 DIRS += $(RIOTCPU)/$(CPU) $(RIOTBOARD)/$(BOARD)
 DIRS += $(RIOTBASE)/core $(RIOTBASE)/drivers $(RIOTBASE)/sys
 
+# For regular modules, adding files to BLOBS to their Makefile is sufficient to
+# create the corresponding headers.
+#
+# Application modules are different, as they use this makefile to build, thus
+# application level variables are not available unless exported.
+#
+# But exporting e.g., BLOBS, would pre-set the variable for all
+# submakefiles.
+#
+# As workaround, $(RIOTBASE)/Makefile.include passes BLOBS to this
+# Makefile as APPLICATION_BLOBS.
+BLOBS = $(APPLICATION_BLOBS)
+
 include $(RIOTBASE)/Makefile.base
