c08801eb657250307d698119f89fdb664b1c1dcc
Francisco Molina <femolina@uc.cl>
2019-07-24 16:27:37 +0200

    makefiles/riotboot: pass DEBUG_ADAPTER_ID to bootloader recipes

--- makefiles/boot/riotboot.mk
+++ makefiles/boot/riotboot.mk
@@ -84,7 +84,7 @@ riotboot/flash-bootloader: riotboot/bootloader/flash
 riotboot/bootloader/%:
 	$(Q)/usr/bin/env -i \
 		QUIET=$(QUIET)\
-		PATH=$(PATH) BOARD=$(BOARD) \
+		PATH=$(PATH) BOARD=$(BOARD) DEBUG_ADAPTER_ID=$(DEBUG_ADAPTER_ID)\
 			$(MAKE) --no-print-directory -C $(RIOTBOOT_DIR) $*
 
 # Generate a binary file from the bootloader which fills all the
