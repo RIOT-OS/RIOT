edf946747fc845695a68a4f7fc8c5b62b1609197
Wlad Meixner <wlad.meixner@protonmail.com>
2019-07-30 07:27:38 +0200

    CC3200: fix mistake in comment

--- boards/cc3200-launchxl/dist/flasher.sh
+++ boards/cc3200-launchxl/dist/flasher.sh
@@ -4,7 +4,7 @@ TOOL_DIR=$1
 PORT=$2
 ELFILE=$3
 
-# change folder since cc3200prog expects dlls in its pwd
+# change folder since cc3200prog expects dlls in its working directory
 cd ${TOOL_DIR}
 echo "[FLASHING] CC3200: port="${PORT}" file="${ELFILE}
 # flash binary
