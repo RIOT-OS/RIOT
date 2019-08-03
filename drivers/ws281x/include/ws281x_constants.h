e9f3b8da96119b13962d4db87dced3a7e393f623
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-08-04 00:33:19 +0200

    drivers/ws281x: Add GPIO ABC backend
    Added a backend for the ws281x using the GPIO Advanced Bitbanging Capabilities
    (ABC) API.

--- drivers/ws281x/include/ws281x_constants.h
+++ drivers/ws281x/include/ws281x_constants.h
@@ -19,14 +19,64 @@
 #ifndef WS281X_CONSTANTS_H
 #define WS281X_CONSTANTS_H
 
+#ifdef MODULE_WS281X_GPIO_ABC
+#include "periph/gpio_abc.h"
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#ifdef MODULE_WS281X_GPIO_ABC
+/*
+ * The high time to encode a zero must not be longer than 450ns
+ * (300ns + 150ns tolerance on the SK6812), otherwise it will be confused with
+ * a one.
+ */
+#if GPIO_ABC_MIN_PULSE_LEN > 450
+#error "NeoPixel: GPIO ABC is unable to generate short enough pulses"
+#endif
+
+/*
+ * In order to support both variants, the timing tolerance is only 100ns instead
+ * of 150ns, see explanation below
+ */
+#if GPIO_ABC_ACCURACY_NS > 100
+#error "NeoPixel: GPIO ABC is unable to provide the accuracy required"
+#endif
+#endif /* MODULE_WS281X_GPIO_ABC */
 
 /**
  * @name    Timing parameters for WS2812/SK6812 RGB LEDs
  * @{
  */
+/**
+ * @brief   Time in nanoseconds to pull the data line high when encoding a zero
+ *
+ * For the WS2812 it is 350ns ± 150ns, for the SK6812 it is 300ns ± 150ns.
+ * We choose 325ns to remain compatible with both (± 125ns tolerance).
+ */
+#define WS281X_TICKS_ZERO_HIGH          (GPIO_ABC_TICKS(325))
+/**
+ * @brief   Time in nanoseconds to pull the data line low when encoding a zero
+ *
+ * For the WS2812 it is 800ns ± 150ns, for the SK6812 it is 900ns ± 150ns.
+ * We choose 850ns to remain compatible with both (± 100ns tolerance).
+ */
+#define WS281X_TICKS_ZERO_LOW           (GPIO_ABC_TICKS(850))
+/**
+ * @brief   Time in nanoseconds to pull the data line high when encoding a one
+ *
+ * For the WS2812 it is 700ns ± 150ns, for the SK6812 it is 600ns ± 150ns.
+ * We choose 650ns to remain compatible with both (± 100ns tolerance).
+ */
+#define WS281X_TICKS_ONE_HIGH           (GPIO_ABC_TICKS(650))
+/**
+ * @brief   Time in nanoseconds to pull the data line low when encoding a one
+ *
+ * For both the WS2812 and the SK6812 it is 600ns ± 150ns.
+ */
+#define WS281X_TICKS_ONE_LOW            (GPIO_ABC_TICKS(600))
 /**
  * @brief   Time in microseconds to pull the data line low to signal end of data
  *
