254934dfa6b51740a86938ace3f4c75a166fc0b1
fjmolinas <femolina@uc.cl>
2019-08-04 10:52:26 +0200

    cpu/nrf5x_common: add flashpage_raw_support

--- cpu/nrf5x_common/periph/flashpage.c
+++ cpu/nrf5x_common/periph/flashpage.c
@@ -23,12 +23,37 @@
 #include "assert.h"
 #include "periph/flashpage.h"
 
+void flashpage_write_raw(void *target_addr, const void *data, size_t len)
+{
+    /* assert multiples of FLASHPAGE_RAW_BLOCKSIZE are written and no less of
+       that length. */
+    assert(!(len % FLASHPAGE_RAW_BLOCKSIZE));
+
+    /* ensure writes are aligned */
+    assert(!(((unsigned)target_addr % FLASHPAGE_RAW_ALIGNMENT) ||
+            ((unsigned)data % FLASHPAGE_RAW_ALIGNMENT)));
+
+    /* ensure the length doesn't exceed the actual flash size */
+    assert(((unsigned)target_addr + len) <
+           (CPU_FLASH_BASE + (FLASHPAGE_SIZE * FLASHPAGE_NUMOF)) + 1);
+
+    uint32_t *page_addr = target_addr;
+    const uint32_t *data_addr = data;
+
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+    for (unsigned i = 0; i < (len / FLASHPAGE_RAW_BLOCKSIZE); i++) {
+        *page_addr++ = data_addr[i];
+    }
+
+    /* finish up */
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+}
+
 void flashpage_write(int page, const void *data)
 {
     assert(page < (int)FLASHPAGE_NUMOF);
 
     uint32_t *page_addr = (uint32_t *)flashpage_addr(page);
-    const uint32_t *data_addr = data;
 
     /* erase given page */
     NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
@@ -37,12 +62,6 @@ void flashpage_write(int page, const void *data)
 
     /* write data to page */
     if (data != NULL) {
-        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
-        for (unsigned i = 0; i < (FLASHPAGE_SIZE / 4); i++) {
-            *page_addr++ = data_addr[i];
-        }
+        flashpage_write_raw(page_addr, data, FLASHPAGE_SIZE);
     }
-
-    /* finish up */
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
 }
