ba00269dc9a87bb302fe104092231dc46f18f48c
Gunar Schorcht <gunar@schorcht.net>
2019-08-09 18:23:05 +0200

    sys/auto_init/can: cleanup for STM32 CAN controller
    Initialization code for module `can_stm32` is obsolete since the STM32 CAN controller driver isn't a module anymore.

--- sys/auto_init/can/auto_init_can.c
+++ sys/auto_init/can/auto_init_can.c
@@ -63,9 +63,4 @@ void auto_init_candev(void)
     extern void auto_init_esp_can(void);
     auto_init_esp_can();
 #endif
-
-#ifdef MODULE_CAN_STM32
-    extern void auto_init_can_stm32(void);
-    auto_init_can_stm32();
-#endif
 }
