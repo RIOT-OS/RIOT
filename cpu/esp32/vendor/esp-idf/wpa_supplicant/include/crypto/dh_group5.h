ee349edf3be07bea4fc21f2819c9eb3578f4dc98
Gunar Schorcht <gunar@schorcht.net>
2019-08-11 13:06:57 +0200

    cpu/esp32: add SDK crypto headers and rename
    To resolve the conflicts between `wpa-supplicant` crypto functions (part of the ESP32 SDK) and RIOT's `crypto` and `hashes` modules, the crypto function headers from ESP32 SDK are added to vendor code and the crypto functions are renamed using the prefix `wpa_`.

---
+++ cpu/esp32/vendor/esp-idf/wpa_supplicant/include/crypto/dh_group5.h
@@ -0,0 +1,33 @@
+/*
+ * Diffie-Hellman group 5 operations
+ * Copyright (c) 2009, Jouni Malinen <j@w1.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Alternatively, this software may be distributed under the terms of BSD
+ * license.
+ *
+ * See README and COPYING for more details.
+ */
+
+#ifndef DH_GROUP5_H
+#define DH_GROUP5_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "wpa/wpabuf.h"
+
+void * wpa_dh5_init(struct wpabuf **priv, struct wpabuf **publ);
+struct wpabuf * wpa_dh5_derive_shared(void *ctx, const struct wpabuf *peer_public,
+				                      const struct wpabuf *own_private);
+void wpa_dh5_free(void *ctx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DH_GROUP5_H */
