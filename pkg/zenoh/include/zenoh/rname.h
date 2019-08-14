a2d73d47fc87131616b50c75c3f6f3183a2e1499
Brenton Chetty <brent7984@gmail.com>
2019-08-14 15:29:10 +0200

    Zenoh - Clean up header files

--- pkg/zenoh/include/zenoh/rname.h
+++
@@ -1,6 +0,0 @@
-#ifndef ZENOH_C_RNAME_H_
-#define ZENOH_C_RNAME_H_
-
-int intersect(char *c1, char *c2);
-
-#endif /* ZENOH_C_RNAME_H_ */
--- pkg/zenoh/include/zenoh/unix/types.h
+++
@@ -1,6 +0,0 @@
-#ifndef ZENOH_C_UNIX_NET_H_
-#define ZENOH_C_UNIX_NET_H_
-
-typedef int z_socket_t;
-
-#endif /* ZENOH_C_UNIX_NET_H_ */
