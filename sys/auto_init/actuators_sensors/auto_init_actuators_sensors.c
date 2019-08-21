e1936d831e85f3d0fee33c5beb0cb63b55c1d9dd
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-08-21 09:11:26 +0200

    sys/auto_init: Add modules for sensors & actuators
    - Added module auto_init_actuators_sensors to auto-initialize actuator
      and sensor drivers
        - Auto-initialization for actuator/sensor driver `foo` will be
          implemented in submodule `auto_init_actuators_sensors_foo`
        - SAUL registration is also performed by that hook, if
          `auto_init_saul` is used
    - Pseudo-module `auto_init_sensors_actuators_default` is used to pull in
      sensor and actuator auto_init submodules for all drivers used
    - Made `auto_init_saul` depend on `auto_init_sensors_actuators_default`

---
+++ sys/auto_init/actuators_sensors/auto_init_actuators_sensors.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2020 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup sys_auto_init_actuators_sensors
+ *
+ * @{
+ * @file
+ * @brief   Implementation of the auto initialization of actuators and sensors
+ *
+ * @author  Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ *
+ * @}
+ */
+#include <stdint.h>
+#include <stdio.h>
+
+#include "auto_init.h"
+
+void auto_init_actuators_sensors(void)
+{
+}
