7a1796e803dbea6a7bf3d801f8baa8e60eb3ad2e
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2019-08-27 13:32:32 +0200

    fixup! drivers/eeprom: Common API to access EEPROM memory

--- drivers/eeprom/include/eeprom_params.h
+++ drivers/eeprom/include/eeprom_params.h
@@ -36,26 +36,44 @@ extern const eeprom_driver_t eeprom_driver_cpu;
 
 #ifdef MODULE_AT24CXXX
 extern const eeprom_driver_t eeprom_driver_at24cxxx;
-static at24cxxx_t _at24cxxx_handle;
 #endif
+
+#ifndef INTERNAL_EEPROM_DEVS
+#ifdef MODULE_PERIPH_EEPROM
+/**
+ * @brief   Internal EEPROM device(s)
+ */
+#define INTERNAL_EEPROM_DEVS        { \
+                                        .driver = &eeprom_driver_cpu, \
+                                        .params = NULL, \
+                                        .handle = NULL, \
+                                    },
+#endif /* defined(MODULE_PERIPH_EEPROM) */
+#endif /* !defined(INTERNAL_EEPROM_DEVS) */
+
+#ifndef AT24CXXX_EEPROM_DEVS
+#ifdef MODULE_AT24CXXX
+static at24cxxx_t _at24cxxx_handle;
+/**
+ * @brief   External AT24CXXX EEPROM device(s)
+ */
+#define AT24CXXX_EEPROM_DEVS        { \
+                                        .driver = &eeprom_driver_at24cxxx, \
+                                        .params = &at24cxxx_params[0], \
+                                        .handle = &_at24cxxx_handle, \
+                                    }
+#endif /* defined(MODULE_AT24CXXX) */
+#endif /* !defined(AT24CXXX_EEPROM_DEVS) */
 
 /**
  * @brief   The EEPROM drivers
  */
 static const eeprom_dev_t eeprom_devs[] = {
-#ifdef MODULE_PERIPH_EEPROM
-    {
-        .driver = &eeprom_driver_cpu,
-        .params = NULL,
-        .handle = NULL,
-    },
+#ifdef INTERNAL_EEPROM_DEVS
+    INTERNAL_EEPROM_DEVS,
 #endif
-#ifdef MODULE_AT24CXXX
-    {
-        .driver = &eeprom_driver_at24cxxx,
-        .params = &at24cxxx_params[0],
-        .handle = &_at24cxxx_handle,
-    }
+#ifdef AT24CXXX_EEPROM_DEVS
+    AT24CXXX_EEPROM_DEVS,
 #endif
     /* dummy to get this compiled for now. TODO: Remove this */
     {
