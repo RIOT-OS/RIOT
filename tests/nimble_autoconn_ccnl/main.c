28acb901ab4064e1302703807b34348ff8244dc8
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-08-29 17:22:20 +0200

    tests: add nimble_autoconn_ccnl test

---
+++ tests/nimble_autoconn_ccnl/main.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Run NDN-over-BLE using the 'autoconn' BLE connection manager
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "shell.h"
+
+int main(void)
+{
+    puts("NDN-over-BLE with autoconn BLE connection manager");
+
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* should never be reached */
+    return 0;
+}
