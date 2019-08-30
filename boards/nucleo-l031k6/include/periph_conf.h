d6b6b7a529aea5b1e1b95d497a23363c0e746293
Marius <marius@twostairs.com>
2019-08-30 18:34:21 +0200

    board/nucleo-l031k6: Add i2c to periph_conf

--- boards/nucleo-l031k6/include/periph_conf.h
+++ boards/nucleo-l031k6/include/periph_conf.h
@@ -127,6 +127,29 @@ static const spi_conf_t spi_config[] = {
 #define ADC_NUMOF           (7U)
 /** @} */
 
+/**
+ * @name I2C configuration
+ * @{
+ */
+static const i2c_conf_t i2c_config[] = {
+    {
+        .dev            = I2C1,
+        .speed          = I2C_SPEED_NORMAL,
+        .scl_pin        = GPIO_PIN(PORT_B, 6),
+        .sda_pin        = GPIO_PIN(PORT_B, 7),
+        .scl_af         = GPIO_AF1,
+        .sda_af         = GPIO_AF1,
+        .bus            = APB1,
+        .rcc_mask       = RCC_APB1ENR_I2C1EN,
+        .irqn           = I2C1_IRQn
+    }
+};
+
+#define I2C_0_ISR           isr_i2c1
+
+#define I2C_NUMOF           ARRAY_SIZE(i2c_config)
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
