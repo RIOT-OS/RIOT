bf410c84f15471ede2bd6c6cab63f879d5e4e6a0
Benjamin Valentin <benpicco@googlemail.com>
2019-09-01 17:25:58 +0200

    pkg/libb2: define HAVE_ALIGNED_ACCESS_REQUIRED based on CPU
    Most architectures do not support unaligned memory access, so set the define accordingly.

--- pkg/libb2/include/config.h
+++ pkg/libb2/include/config.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "cpu.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -11,6 +13,10 @@ extern "C" {
 /* Test for a little-endian machine */
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define NATIVE_LITTLE_ENDIAN
+#endif
+
+#ifndef CPU_HAS_UNALIGNED_ACCESS
+#define HAVE_ALIGNED_ACCESS_REQUIRED
 #endif
 
 #ifdef __cplusplus
