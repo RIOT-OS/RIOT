a07f4aae6b2c1e4a9d34d665481290edf10b23e8
Daniele Lacamera <dan@danielinux.net>
2019-09-02 11:51:41 +0200

    posix sockets:
    AF_INET number to match C-library defines

--- sys/include/net/af.h
+++ sys/include/net/af.h
@@ -33,14 +33,11 @@ enum {
 #define AF_UNIX     AF_UNIX     /**< unspecified address family (as macro) */
     AF_PACKET,                  /**< packet family */
 #define AF_PACKET   AF_PACKET   /**< packet family (as macro) */
-    AF_INET,                    /**< internetwork address family: UDP, TCP, etc. */
+    AF_INET = 4,                    /**< internetwork address family: UDP, TCP, etc. */
 #define AF_INET     AF_INET     /**< internetwork address family: UDP, TCP, etc. (as macro) */
-    AF_INET6,                   /**< internetwork address family with IPv6: UDP, TCP, etc. */
+    AF_INET6  = 10,                   /**< internetwork address family with IPv6: UDP, TCP, etc. */
 #define AF_INET6    AF_INET6    /**< internetwork address family with IPv6: UDP, TCP, etc.
                                  *    (as macro) */
-    AF_NUMOF,                   /**< maximum number of address families on this system */
-#define AF_NUMOF    AF_NUMOF    /**< maximum number of address families on this system (as macro) */
-#define AF_MAX      AF_NUMOF    /**< alias for @ref AF_NUMOF */
 };
 
 #ifdef __cplusplus
