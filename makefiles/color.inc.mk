87a90f2996e8091cb5a98c252be223232f544cac
Juan Carrano <j.carrano@fu-berlin.de>
2019-09-02 17:57:42 +0200

    makefiles/color: Add color functions.
    c_xxxx functions can be used to wrap a piece of text in a
    $(COLOR_X)...$(COLOR_RESET) block, thereby avoiding the easy mistake
    of forgetting to place a COLOR_RESET.

--- makefiles/color.inc.mk
+++ makefiles/color.inc.mk
@@ -23,3 +23,11 @@ ifeq ($(CC_NOCOLOR),0)
   COLOR_PURPLE := $(ANSI_PURPLE)
   COLOR_RESET  := $(ANSI_RESET)
 endif
+
+# Colorizer functions:
+#  These functions wrap a block of text in $(COLOR_X)...$(COLOR_RESET).
+#  Do not nest calls to this functions or the colors will be wrong.
+c_green = $(COLOR_GREEN)$(1)$(COLOR_RESET)
+c_red = $(COLOR_RED)$(1)$(COLOR_RESET)
+c_yellow = $(COLOR_YELLOW)$(1)$(COLOR_RESET)
+c_purple = $(COLOR_PURPLE)$(1)$(COLOR_RESET)
