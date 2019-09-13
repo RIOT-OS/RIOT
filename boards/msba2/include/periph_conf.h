RIOT-OS/RIOT

commit df27dbef7a6b4004b8ea6845f11f6e6559971d53

cpu: Moved stdio_init() into cpu_init()
- Removed stdio_init() from newlib's _init(), as this is too late in the boot
  process to allow DEBUG()ing during periph_init()
- Added stdio_init() to the various cpu_init() routines of the ARM CPUs just
  before periph_init()

sys/newlib_syscalls_default/syscalls.c
@@ -69,7 +69,7 @@ char *heap_top = &_sheap + 4;
  */
 void _init(void)
 {
-    stdio_init();
+    /* nothing to do here */
 }
 
 /**

cpu/cc2538/cpu.c
@@ -21,6 +21,7 @@
 
 #include "cpu.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 
 #define BIT(n)          ( 1UL << (n) )
 
@@ -48,6 +49,8 @@ void cpu_init(void)
     SYS_CTRL->I_MAP = 1;
     /* initialize the clock system */
     cpu_clock_init();
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
     /* trigger static peripheral initialization */
     periph_init();
 }

cpu/cc26x0/cpu.c
@@ -20,6 +20,7 @@
 #include "cpu.h"
 #include "periph_conf.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 
 #ifndef HF_CLOCK_SOURCE
 #define HF_CLOCK_SOURCE DDI_0_OSC_CTL0_SCLK_HF_SRC_SEL_RCOSC /* set 48MHz RCOSC */
@@ -43,6 +44,9 @@ void cpu_init(void)
 
     /* initialize the system clock */
     cpu_clock_init();
+
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
 
     /* trigger static peripheral initialization */
     periph_init();

cpu/efm32/cpu.c
@@ -22,6 +22,7 @@
 #include "cpu.h"
 #include "periph_conf.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 
 #include "em_chip.h"
 #include "em_cmu.h"
@@ -171,6 +172,9 @@ void cpu_init(void)
 
     /* initialize power management interface */
     pm_init();
+
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
 
     /* trigger static peripheral initialization */
     periph_init();

cpu/ezr32wg/cpu.c
@@ -20,6 +20,7 @@
 #include "cpu.h"
 #include "periph_conf.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 
 /**
  * @brief   Configure clock sources and the CPU frequency
@@ -60,6 +61,8 @@ void cpu_init(void)
     cortexm_init();
     /* Initialise clock sources and generic clocks */
     clk_init();
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
     /* trigger static peripheral initialization */
     periph_init();
 }

cpu/kinetis/cpu.c
@@ -19,6 +19,7 @@
 
 #include "cpu.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 #ifdef MODULE_PERIPH_MCG
 #include "mcg.h"
 #endif
@@ -41,6 +42,10 @@ void cpu_init(void)
     /* initialize the CPU clocking provided by the MCG module */
     kinetis_mcg_init();
 #endif
+
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
+
     /* trigger static peripheral initialization */
     periph_init();
 }

cpu/lm4f120/cpu.c
@@ -24,6 +24,7 @@
 #include "irq.h"
 #include "periph/init.h"
 #include "periph_conf.h"
+#include "stdio_base.h"
 
 /**
  * @brief Initialize the CPU, set IRQ priorities
@@ -35,6 +36,9 @@ void cpu_init(void)
 
     /* initialize the clock system */
     cpu_clock_init(CLOCK_SOURCE);
+
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
 
     /* trigger static peripheral initialization */
     periph_init();

cpu/lpc1768/cpu.c
@@ -19,6 +19,7 @@
 
 #include "cpu.h"
 #include "periph/init.h"
+#include "stdio_base.h"
 
 /**
  * @brief Initialize the CPU, set IRQ priorities
@@ -27,6 +28,8 @@ void cpu_init(void)
 {
     /* initialize the Cortex-M core */
     cortexm_init();
+    /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
+    stdio_init();
     /* trigger static peripheral initialization */
     periph_init();
 }

commit 0339db3c2f98b6f55bf599802cfafd84ad7c7274

cpu: introduce PROVIDES_EARLY_STDIO_INIT
Early stdio init is not implemented for every CPU.

To avoid breaking stdio for CPUs that do not implement it,
introduce a define so that CPUs can advertise support if
they have it implemented, whereas those who don't can fall
back to the old implementation.

This undoes breakage introduces by df27dbef7a6b4004b8ea6845f11f6e6559971d53

boards/msba2/include/periph_conf.h
@@ -26,6 +26,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @name    Clock configuration
  * @{
  */

cpu/cc2538/include/periph_cpu.h
@@ -53,6 +53,11 @@ typedef uint32_t gpio_t;
 /** @} */
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @name    Power management configuration
  * @{
  */

cpu/cc26x0/include/periph_cpu.h
@@ -35,6 +35,11 @@ extern "C" {
 #define CPUID_LEN           (16U)
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @name    Power management configuration
  * @{
  */

cpu/efm32/include/periph_cpu.h
@@ -398,6 +398,11 @@ typedef struct {
 } uart_conf_t;
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   CPU provides own pm_off() function
  */
 #define PROVIDES_PM_LAYERED_OFF

cpu/ezr32wg/include/periph_cpu.h
@@ -39,6 +39,11 @@ typedef uint32_t tim_t;
 /** @} */
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Starting offset of CPU_ID
  */
 #define CPUID_ADDR          (&DEVINFO->UNIQUEL)

cpu/kinetis/include/periph_cpu.h
@@ -125,6 +125,11 @@ typedef uint16_t gpio_t;
 /** @} */
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Prevent shared timer functions from being used
  */
 #define PERIPH_TIMER_PROVIDES_SET

cpu/lm4f120/include/periph_cpu.h
@@ -29,6 +29,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Overwrite the default gpio_t type definition
  * @{
  */

cpu/lpc1768/include/periph_cpu.h
@@ -69,6 +69,11 @@ typedef enum {
 #define PROVIDES_PM_LAYERED_OFF
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Power management configuration
  */
 #define PM_NUM_MODES    (2U)

cpu/nrf51/include/periph_cpu.h
@@ -31,6 +31,11 @@ extern "C" {
 #define CLOCK_CORECLOCK     (16000000U)
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Redefine some peripheral names to unify them between nRF51 and 52
  * @{
  */

cpu/nrf52/include/periph_cpu.h
@@ -36,6 +36,11 @@ extern "C" {
 #define PERIPH_CLOCK        (16000000U)
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Redefine some peripheral names to unify them between nRF51 and 52
  * @{
  */

cpu/sam3/include/periph_cpu.h
@@ -46,6 +46,11 @@ typedef uint32_t gpio_t;
 #define GPIO_PIN(x, y)      (((uint32_t)PIOA + (x << 9)) | y)
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief Declare needed generic SPI functions
  * @{
  */

cpu/samd21/include/periph_cpu.h
@@ -35,6 +35,11 @@ extern "C" {
 #define PM_BLOCKER_INITIAL  { .val_u32 = 0x00000001 }
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @name   SAMD21 sleep modes for PM
  * @{
  */

cpu/samd5x/include/periph_cpu.h
@@ -29,6 +29,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Mapping of pins to EXTI lines, -1 means not EXTI possible
  */
 static const int8_t exti_config[4][32] = {

cpu/saml1x/include/periph_cpu.h
@@ -27,6 +27,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Override the default initial PM blocker
  * @todo   Idle modes are enabled by default, deep sleep mode blocked
  */

cpu/saml21/include/periph_cpu.h
@@ -27,6 +27,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   Mapping of pins to EXTI lines, -1 means not EXTI possible
  */
 #if defined(CPU_MODEL_SAML21E18A) || defined(CPU_MODEL_SAML21E18B) || \

cpu/stm32_common/include/periph_cpu_common.h
@@ -55,6 +55,11 @@ extern "C" {
 #define PROVIDES_PM_LAYERED_OFF
 
 /**
+ * @brief   We provide early initialisation of stdio
+ */
+#define PROVIDES_EARLY_STDIO_INIT
+
+/**
  * @brief   All STM timers have 4 capture-compare channels
  */
 #define TIMER_CHAN          (4U)

sys/newlib_syscalls_default/syscalls.c
@@ -69,7 +69,9 @@ char *heap_top = &_sheap + 4;
  */
 void _init(void)
 {
-    /* nothing to do here */
+#ifndef PROVIDES_EARLY_STDIO_INIT
+    stdio_init();
+#endif
 }
 
 /**