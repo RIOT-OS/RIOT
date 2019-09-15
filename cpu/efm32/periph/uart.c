c8f34c705bfe7ac8dfb083a87177da39dbcb8fb9
Thomas Stilwell <stilwellt@openlabs.co>
2019-09-15 07:51:16 -0500

    cpu/efm32/uart: move reusable code into uart_poweron/off()

--- cpu/efm32/periph/uart.c
+++ cpu/efm32/periph/uart.c
@@ -106,8 +106,6 @@ int uart_init(uart_t dev, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
         /* enable receive interrupt */
         USART_IntEnable(uart, USART_IEN_RXDATAV);
 
-        /* enable peripheral */
-        USART_Enable(uart, usartEnable);
 #ifdef USE_LEUART
     } else {
         LEUART_TypeDef *leuart = (LEUART_TypeDef *) uart_config[dev].dev;
@@ -141,15 +139,14 @@ int uart_init(uart_t dev, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg)
 
         /* enable receive interrupt */
         LEUART_IntEnable(leuart, LEUART_IEN_RXDATAV);
-
-        /* enable peripheral */
-        LEUART_Enable(leuart, leuartEnable);
     }
 #endif
 
     /* enable the interrupt */
     NVIC_ClearPendingIRQ(uart_config[dev].irq);
     NVIC_EnableIRQ(uart_config[dev].irq);
+
+    uart_poweron(dev);
 
     return 0;
 }
@@ -210,10 +207,36 @@ void uart_write(uart_t dev, const uint8_t *data, size_t len)
 void uart_poweron(uart_t dev)
 {
     CMU_ClockEnable(uart_config[dev].cmu, true);
+
+#ifdef USE_LEUART
+    if (_is_usart(dev)) {
+#endif
+        USART_TypeDef *usart = uart_config[dev].dev;
+        USART_Enable(usart, usartEnable);
+#ifdef USE_LEUART
+    }
+    else {
+        LEUART_TypeDef *leuart = uart_config[dev].dev;
+        LEUART_Enable(leuart, leuartEnable);
+    }
+#endif
 }
 
 void uart_poweroff(uart_t dev)
 {
+#ifdef USE_LEUART
+    if (_is_usart(dev)) {
+#endif
+        USART_TypeDef *usart = uart_config[dev].dev;
+        USART_Enable(usart, usartDisable);
+#ifdef USE_LEUART
+    }
+    else {
+        LEUART_TypeDef *leuart = uart_config[dev].dev;
+        LEUART_Enable(leuart, leuartDisable);
+    }
+#endif
+
     CMU_ClockEnable(uart_config[dev].cmu, false);
 }
 
