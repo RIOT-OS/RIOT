6c80dd3cd53f0aa22e31c45cc5c0547ac86f8ef9
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-09-26 08:10:13 +0200

    tools/bossa: add and configure preflash-delay when required

--- makefiles/tools/bossa.inc.mk
+++ makefiles/tools/bossa.inc.mk
@@ -13,9 +13,18 @@ ifneq (,$(BOSSA_ARDUINO_PREFLASH))
 
   PREFLASHER ?= stty
   PREFFLAGS  ?= $(STTY_FLAG) $(PROG_DEV) raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
-  FLASHDEPS += preflash
+  ifneq (,$(PREFLASH_DELAY))
+    FLASHDEPS += preflash-delay
+  else
+    FLASHDEPS += preflash
+  endif
 
+  RESETFFLASG ?= $(STTY_FLAG) $(PORT) raw ispeed 600 ospeed 600 cs8 -cstopb ignpar eol 255 eof 255
+  RESET ?= $(PREFLASHER) $(RESETFFLASG)
 endif
+
+preflash-delay: preflash
+	sleep $(PREFLASH_DELAY)
 
 # if we go with the default (BOSSA shipped with RIOT), we download and build
 # the tool if not already done
