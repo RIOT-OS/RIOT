09faa328f93d5911fb30aaa4565380f9bb4acd7f
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-09-27 11:23:39 +0200

    tests: add distinct test app for nrfmin driver

---
+++ tests/driver_nrfmin/tests/01-run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+from testrunner import run
+
+
+def testfunc(child):
+    child.expect("All up, running the shell now")
+    child.sendline("ifconfig")
+    child.expect(r"Iface\s+(\d+)\s+HWaddr:")
+
+
+if __name__ == "__main__":
+    sys.exit(run(testfunc, timeout=1, echo=False))
