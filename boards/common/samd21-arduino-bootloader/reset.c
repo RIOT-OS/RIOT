c7f6d53773c4fcb0265a8d06020102ba276efba2
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-09-30 17:16:00 +0200

    boards/samd21-arduino-bootloader: add common board module
    This module implements the 2 functions called when requesting a board 'reset in application' and 'board reset in bootloader' actions.
    This module will also configure the behaviour of bossa flasher and has a dependency on USBUS CDC ACM module for providing STDIO over USB

---
+++ boards/common/samd21-arduino-bootloader/reset.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C)  2019 Inria
+ *                2019 Kees Bakker, SODAQ
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_common
+ * @{
+ * @file
+ * @brief       Board common implementations for managing an Arduino bootloader
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ * @author      Kees Bakker <kees@sodaq.com>
+ *
+ * @}
+ */
+
+#define USB_H_USER_IS_RIOT_INTERNAL
+
+#include "usb_board_reset.h"
+
+
+#define SAMD21_DOUBLE_TAP_ADDR              (0x20007FFCUL)
+#define SAMD21_DOUBLE_TAP_MAGIC_NUMBER      (0x07738135UL)
+
+void usb_board_reset_in_bootloader(void)
+{
+    /* The Arduino bootloader checks for a magic number in SRAM to remain in
+       bootloader mode.
+       See
+       https://github.com/arduino/ArduinoCore-samd/blob/master/bootloaders/zero/board_definitions_arduino_mkr1000.h#L38
+       and
+       https://github.com/arduino/ArduinoCore-samd/blob/master/bootloaders/zero/main.c#L94
+       for implementation details. */
+    uint32_t *reset_addr = (uint32_t *)SAMD21_DOUBLE_TAP_ADDR;
+    *reset_addr = (uint32_t)SAMD21_DOUBLE_TAP_MAGIC_NUMBER;
+
+    usb_board_reset_in_application();
+}
