160752a4f1a395a83f37aa1cc8a0413fa93c7d5b
Francisco Molina <femolina@uc.cl>
2019-10-08 09:03:27 +0200

    makefiles/vars.inc.mk: add and export CPU_ARCH

--- makefiles/vars.inc.mk
+++ makefiles/vars.inc.mk
@@ -10,6 +10,7 @@ export APPLICATION_MODULE    # The application module name.
 export BOARD                 # The board to compile the application for.
 export CPU                   # The CPU, set by the board's Makefile.features.
 export CPU_MODEL             # The specific identifier of the used CPU, used for some CPU implementations to differentiate between different memory layouts. Set by the board's Makefile.features.
+export CPU_ARCH              # The specific identifier of the core presented in the CPU
 export MCU                   # The MCU, set by the board's Makefile.include, or defaulted to the same value as CPU.
 export INCLUDES              # The extra include paths, set by the various Makefile.include files.
 export CXXINCLUDES           # The extra include paths for c++, set by the various Makefile.include files.
