347a0fc804b7cf5fd7a36b7528b6a218b39f841a
Francisco Molina <femolina@uc.cl>
2019-10-08 15:26:11 +0200

    cpu/ boards/: remove exports for CPU_FAM

--- cpu/kinetis/kinetis-info.mk
+++ cpu/kinetis/kinetis-info.mk
@@ -32,16 +32,16 @@ export KINETIS_SPEED := $(word 11, $(KINETIS_INFO))
 ifeq ($(KINETIS_CORE), Z)
   # Cortex-M0+
   CPU_ARCH = cortex-m0plus
-  export CPU_FAMILY = klx
+  CPU_FAMILY = klx
 else ifeq ($(KINETIS_CORE), D)
   # Cortex-M4
   CPU_ARCH = cortex-m4
-  export CPU_FAMILY = kx
+  CPU_FAMILY = kx
 else ifeq ($(KINETIS_CORE), F)
   # Cortex-M4F or Cortex-M7
   # TODO: Add floating point support
   CPU_ARCH = cortex-m4
-  export CPU_FAMILY = kx
+  CPU_FAMILY = kx
 endif
 
 # RAM sizes are a bit arbitrary, but are usually dependent on ROM size and core speed.
