57377ded76fe55cbce04836232558bb2de6f0404
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-10-12 12:00:11 +0100

    cpu: mips_pic32mz: Include vendor header in cpu_conf.h
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- cpu/mips_pic32mz/include/cpu_conf.h
+++ cpu/mips_pic32mz/include/cpu_conf.h
@@ -21,6 +21,12 @@
 #ifndef CPU_CONF_H
 #define CPU_CONF_H
 
+#ifdef CPU_MODEL_P32MZ2048EFG100
+#include "vendor/p32mz2048efg100.h"
+#else
+#error "No CPU headers for the defined CPU_MODEL found"
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
