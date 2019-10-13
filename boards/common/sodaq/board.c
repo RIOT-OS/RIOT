f9631953cb67a92928d8e021b43f3a413bfd0217
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-10-13 17:18:08 +0200

    boards: add common sodaq module

---
+++ boards/common/sodaq/board.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Kees Bakker, SODAQ
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_common_sodaq
+ * @{
+ *
+ * @file
+ * @brief       Board common implementations for the SODAQ boards
+ *
+ * @author      Kees Bakker <kees@sodaq.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the on-board LED */
+    gpio_init(LED0_PIN, GPIO_OUT);
+
+    /* initialize the CPU */
+    cpu_init();
+}
