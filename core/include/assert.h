54e915eec57ce0ead81945ffd2e3f221dff22e03
Gunar Schorcht <gunar@schorcht.net>
2019-10-19 15:52:59 +0200

    core/assert: allow multiple static_asserts
    Uses an own scope for the definition of the enum const to allow multiple `static_assert` statements within the same function in non-C11 environments.

--- core/include/assert.h
+++ core/include/assert.h
@@ -119,7 +119,7 @@ NORETURN void _assert_failure(const char *file, unsigned line);
  * Generates a division by zero compile error when cond is false
  */
 #define static_assert(cond, ...) \
-    enum { static_assert_failed_on_div_by_0 = 1 / (!!(cond)) }
+    { enum { static_assert_failed_on_div_by_0 = 1 / (!!(cond)) }; }
 #endif
 #endif
 
