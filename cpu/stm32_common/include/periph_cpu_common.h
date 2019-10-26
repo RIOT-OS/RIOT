799823b630681be7ee38c3d7bd54dc387ccfd9b4
Koen Zandberg <koen@bergzand.net>
2019-10-26 23:44:48 +0200

    stm32/cpu: Add functions for low power mode clock config

--- cpu/stm32_common/include/periph_cpu_common.h
+++ cpu/stm32_common/include/periph_cpu_common.h
@@ -603,6 +603,22 @@ void periph_clk_en(bus_t bus, uint32_t mask);
  * @param[in] bus       bus the peripheral is connected to
  * @param[in] mask      bit in the RCC enable register
  */
+void periph_lpclk_dis(bus_t bus, uint32_t mask);
+
+/**
+ * @brief   Enable the given peripheral clock in low power mode
+ *
+ * @param[in] bus       bus the peripheral is connected to
+ * @param[in] mask      bit in the RCC enable register
+ */
+void periph_lpclk_en(bus_t bus, uint32_t mask);
+
+/**
+ * @brief   Disable the given peripheral clock in low power mode
+ *
+ * @param[in] bus       bus the peripheral is connected to
+ * @param[in] mask      bit in the RCC enable register
+ */
 void periph_clk_dis(bus_t bus, uint32_t mask);
 
 /**
