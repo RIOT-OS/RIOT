195379a9b83573f203f48c1757957ed1e540d77b
fabian18 <fabian.huessler@ovgu.de>
2019-11-05 12:29:42 +0100

    drivers/ina3221: INA3221 driver implementation

---
+++ drivers/ina3221/include/ina3221_defines.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_ina3221
+ * @{
+ *
+ * @file
+ * @brief       Internal definitions for Texas Instruments INA3221
+ *              three-channel, high-side current and bus voltage
+ *              monitor
+ *
+ * @author      Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef INA3221_DEFINES_H
+#define INA3221_DEFINES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define INA3221_REG_ADDR_LEN               (1)                  /**< Register address length */
+#define INA3221_REG_LEN                    (2)                  /**< Register length */
+
+#define INA3221_SHUNT_VOLTAGE_PRECISION_UV (40)                 /**< Shunt voltage measurement precision */
+#define INA3221_BUS_VOLTAGE_PRECISION_MV   (8)                  /**< Bus voltage measurement precision */
+
+#define INA3221_MANUFACTURER_ID            (0x5449)             /**< INA3221 manufacturer ID */
+#define INA3221_DIE_ID                     (0x3220)             /**< INA3221 DIE ID */
+
+/**
+ * @name INA3221 Limits
+ * @{
+ */
+#define INA3221_MAX_SHUNT_REG_VAL          ((int16_t)(0x7FF8))  /**< 0111 1111 1111 1000 */
+#define INA3221_MIN_SHUNT_REG_VAL          ((int16_t)(0x8000))  /**< 1000 0000 0000 0000 */
+#define INA3221_MAX_BUS_REG_VAL            ((int16_t)(0x7FF8))  /**< 0111 1111 1111 1000 */
+#define INA3221_MIN_BUS_REG_VAL            ((int16_t)(0x8000))  /**< 1000 0000 0000 0000 */
+#define INA3221_MAX_SHUNT_SUM_REG_VAL      ((int16_t)(0x7FFE))  /**< 0111 1111 1111 1110 */
+#define INA3221_MIN_SHUNT_SUM_REG_VAL      ((int16_t)(0x8000))  /**< 1000 0000 0000 0000 */
+#define INA3221_MAX_SHUNT_UV               (163800)             /**< Max. measurable shunt voltage value */
+#define INA3221_MIN_SHUNT_UV               (-163840)            /**< Min. measurable shunt voltage value */
+#define INA3221_MAX_BUS_MV                 (26000)              /**< Max. measurable bus voltage value */
+#define INA3221_MIN_BUS_MV                 (0)                  /**< Min. measurable bus voltage value */
+#define INA3221_MAX_SHUNT_SUM_UV           (655320)             /**< Max. measurable shunt sum voltage value */
+#define INA3221_MIN_SHUNT_SUM_UV           (-655360)            /**< Min. measurable shunt sum voltage value */
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INA3221_DEFINES_H */
+/** @} */
---
+++ drivers/ina3221/include/ina3221_regs.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2019 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_ina3221
+ * @{
+ *
+ * @file
+ * @brief       Register definitions for Texas Instruments INA3221
+ *              three-channel, high-side current and bus voltage
+ *              monitor
+ *
+ * @author      Fabian Hüßler <fabian.huessler@ovgu.de>
+ */
+
+#ifndef INA3221_REGS_H
+#define INA3221_REGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @brief INA3221 internal register addresses
+ */
+typedef enum ina3221_reg {
+    INA3221_REG_CONFIGURATION           = 0x00,     /**< Configuration register                         (R/W) */
+    INA3221_REG_CH1_SHUNT_VOLTAGE       = 0x01,     /**< Channel 1 shunt voltage register               (R)   */
+    INA3221_REG_CH1_BUS_VOLTAGE         = 0x02,     /**< Channel 1 bus voltage register                 (R)   */
+    INA3221_REG_CH2_SHUNT_VOLTAGE       = 0x03,     /**< Channel 2 shunt voltage register               (R)   */
+    INA3221_REG_CH2_BUS_VOLTAGE         = 0x04,     /**< Channel 2 bus voltage register                 (R)   */
+    INA3221_REG_CH3_SHUNT_VOLTAGE       = 0x05,     /**< Channel 3 shunt voltage register               (R)   */
+    INA3221_REG_CH3_BUS_VOLTAGE         = 0x06,     /**< Channel 3 bus voltage register                 (R)   */
+    INA3221_REG_CH1_CRIT_ALERT_LIMIT    = 0x07,     /**< Channel 1 voltage thresholt for critical alert (R/W) */
+    INA3221_REG_CH1_WARN_ALERT_LIMIT    = 0x08,     /**< Channel 1 voltage thresholt for warning alert  (R/W) */
+    INA3221_REG_CH2_CRIT_ALERT_LIMIT    = 0x09,     /**< Channel 2 voltage thresholt for critical alert (R/W) */
+    INA3221_REG_CH2_WARN_ALERT_LIMIT    = 0x0A,     /**< Channel 2 voltage thresholt for warning alert  (R/W) */
+    INA3221_REG_CH3_CRIT_ALERT_LIMIT    = 0x0B,     /**< Channel 3 voltage thresholt for critical alert (R/W) */
+    INA3221_REG_CH3_WARN_ALERT_LIMIT    = 0x0C,     /**< Channel 3 voltage thresholt for warning alert  (R/W) */
+    INA3221_REG_SHUNT_VOLTAGE_SUM       = 0x0D,     /**< Shunt voltage sum register over all channels   (R)   */
+    INA3221_REG_SHUNT_VOLTAGE_SUM_LIMIT = 0x0E,     /**< Shunt voltage sum threshold for critical alert (R/W) */
+    INA3221_REG_MASK_ENABLE             = 0x0F,     /**< Channel enable and status register             (R/W) */
+    INA3221_REG_PV_UPPER_LIMIT          = 0x10,     /**< Pover valid upper limit register               (R/W) */
+    INA3221_REG_PV_LOWER_LIMIT          = 0x11,     /**< Power valid lower limit register               (R/W) */
+    INA3221_REG_MANUFACTURER_ID         = 0xFE,     /**< Manufacturer ID (=0x5449)                      (R)   */
+    INA3221_REG_DIE_ID                  = 0xFF      /**< Die ID (=0x3220)                               (R)   */
+} ina3221_reg_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INA3221_REGS_H */
+/** @} */
