4d4f6ae1d61dd2d66d18305fb1faba43e621ea74
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2019-11-07 16:02:37 +0100

    net/ipv6: Return string pointer when splitting IPv6 interface.
    Right now 'ipv6_addr_split_iface' assumes that the interface specifier
    will always be a number (based on GNRC way of identifying interfaces),
    but this may not be always the case.In order to be able to use the
    Network Interface API, interfaces should be referred by their name.
    
    This changes 'ipv6_addr_split_iface' so it returns a pointer to the
    string that specifies the interface.

--- sys/include/net/ipv6/addr.h
+++ sys/include/net/ipv6/addr.h
@@ -730,6 +730,20 @@ char *ipv6_addr_to_str(char *result, const ipv6_addr_t *addr, uint8_t result_len
 ipv6_addr_t *ipv6_addr_from_str(ipv6_addr_t *result, const char *addr);
 
 /**
+ * @brief split IPv6 address string representation and return remaining string
+ *
+ * Will change @p separator position in @p addr_str to '\0'
+ *
+ * @param[in,out]   addr_str    Address to split
+ * @param[in]       separator   Separator char to use
+ *
+ * @return      string following the first occurrence of @p separator in
+ *              @p addr_str.
+ * @return      NULL if @p separator was not found.
+ */
+char *ipv6_addr_split_str(char *addr_str, char separator);
+
+/**
  * @brief split IPv6 address string representation
  *
  * @note Will change @p seperator position in @p addr_str to '\0'
@@ -741,7 +755,7 @@ ipv6_addr_t *ipv6_addr_from_str(ipv6_addr_t *result, const char *addr);
  * @return      atoi(string after split)
  * @return      @p _default if no string after @p seperator
  */
-int ipv6_addr_split(char *addr_str, char seperator, int _default);
+int ipv6_addr_split_int(char *addr_str, char seperator, int _default);
 
 /**
  * @brief split IPv6 prefix string representation
@@ -753,7 +767,7 @@ int ipv6_addr_split(char *addr_str, char seperator, int _default);
  */
 static inline int ipv6_addr_split_prefix(char *addr_str)
 {
-    return ipv6_addr_split(addr_str, '/', 128);
+    return ipv6_addr_split_int(addr_str, '/', 128);
 }
 
 /**
@@ -762,11 +776,12 @@ static inline int ipv6_addr_split_prefix(char *addr_str)
  * E.g., "fe80::1%5" returns "5", changes @p addr_str to "fe80::1"
  *
  * @param[in,out]   addr_str Address to split
- * @return          interface number or -1 if none specified
+ * @return          string containing the interface specifier.
+ * @return          NULL if no interface was specified.
  */
-static inline int ipv6_addr_split_iface(char *addr_str)
+static inline char *ipv6_addr_split_iface(char *addr_str)
 {
-    return ipv6_addr_split(addr_str, '%', -1);
+    return ipv6_addr_split_str(addr_str, '%');
 }
 
 /**
