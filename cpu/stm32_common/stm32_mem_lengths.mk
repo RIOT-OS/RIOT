ef9363a50909cc294fa3eabad6ca8c6d6a10d880
Jue <me@jue.yt>
2019-11-09 14:07:54 +0100

    cpu/stm32_common: fixed RAM_LEN for stm32f103_c

--- cpu/stm32_common/stm32_mem_lengths.mk
+++ cpu/stm32_common/stm32_mem_lengths.mk
@@ -81,7 +81,9 @@ ifeq ($(STM32_TYPE), F)
           RAM_LEN = 10K
         else ifneq (, $(filter $(STM32_ROMSIZE), 8 B))
           RAM_LEN = 20K
-        else ifneq (, $(filter $(STM32_ROMSIZE), C D E))
+        else ifneq (, $(filter $(STM32_ROMSIZE), C))
+          RAM_LEN = 48K
+        else ifneq (, $(filter $(STM32_ROMSIZE), D E))
           RAM_LEN = 64K
         else ifneq (, $(filter $(STM32_ROMSIZE), F G))
           RAM_LEN = 96K
