6afda7917cdabba7627ec063c7c3698017280681
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-11-15 21:40:08 +0000

    boards: pic32-wifire: Do not mux pins in board_init
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- boards/pic32-wifire/wifire.c
+++ boards/pic32-wifire/wifire.c
@@ -19,13 +19,6 @@ extern void dummy(void);
 
 void board_init(void)
 {
-    /*
-     * Setup pin mux for UART4 this is the one connected
-     * to the ftdi chip (usb<->uart)
-     */
-    U4RXR =   0xb;            /* connect pin RPF2 to UART 4 RX */
-    RPF8R =   0x2;            /* connect pin RPF8 to UART 4 TX */
-
     /* Turn off all LED's */
     gpio_init(LED1_PIN, GPIO_OUT);
     gpio_init(LED2_PIN, GPIO_OUT);
