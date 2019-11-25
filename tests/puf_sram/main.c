0da8c8f094a60d7b19ac9691a3c9e0e76b2cc6f1
Benjamin Valentin <benpicco@beuth-hochschule.de>
2019-11-25 02:08:59 +0100

    tests/puf_sram: add newline in printf
    puts() will already print the newline itself, but for printf()
    we'll have to include it manually.

--- tests/puf_sram/main.c
+++ tests/puf_sram/main.c
@@ -25,7 +25,7 @@ int main(void)
 {
     puts("Start: Test random number generator");
 
-    printf("Success: Data for puf_sram_seed: [0x%08" PRIX32 "]", puf_sram_seed);
+    printf("Success: Data for puf_sram_seed: [0x%08" PRIX32 "]\n", puf_sram_seed);
 
     puts("End: Test finished");
     return 0;
