708edb40f8f63b56319f4c6477cbb027d5b2f809
fabian18 <fabian.huessler@ovgu.de>
2019-12-01 18:00:54 +0100

    pkg/openthread/contrib: adaptation for multiple at86rf2xx types

--- pkg/openthread/contrib/openthread.c
+++ pkg/openthread/contrib/openthread.c
@@ -27,18 +27,19 @@
 
 #ifdef MODULE_AT86RF2XX
 #include "at86rf2xx.h"
-#include "at86rf2xx_params.h"
+#include "at86rf2xx_devs.h"
 #endif
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
 #ifdef MODULE_AT86RF2XX     /* is mutual exclusive with above ifdef */
-#define OPENTHREAD_NETIF_NUMOF        ARRAY_SIZE(at86rf2xx_params)
+#define OPENTHREAD_NETIF_NUMOF        AT86RF2XX_NUM
 #endif
 
+/* Only one type of transceiver is expected here */
 #ifdef MODULE_AT86RF2XX
-static at86rf2xx_t at86rf2xx_dev;
+static at86rf2xx_devs_t at86rf2xx_devs;
 #endif
 
 static uint8_t rx_buf[OPENTHREAD_NETDEV_BUFLEN];
@@ -50,12 +51,12 @@ void openthread_bootstrap(void)
     /* init random */
     ot_random_init();
 
-    /* setup netdev modules */
 #ifdef MODULE_AT86RF2XX
-    at86rf2xx_setup(&at86rf2xx_dev, &at86rf2xx_params[0]);
-    netdev_t *netdev = (netdev_t *) &at86rf2xx_dev;
+    /* setup netdev modules */
+    at86rf2xx_setup_devs(&at86rf2xx_devs);
+    /* take whatever is the first device */
+    netdev_t *netdev = (netdev_t *) at86rf2xx_devs.mem_devs;
 #endif
-
     openthread_radio_init(netdev, tx_buf, rx_buf);
     openthread_netdev_init(ot_thread_stack, sizeof(ot_thread_stack), THREAD_PRIORITY_MAIN - 5, "openthread", netdev);
 }
