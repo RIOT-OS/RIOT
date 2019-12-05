535fff26aedf9044b29db307ded9c9dbc8e24911
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-12-05 22:31:59 +0000

    cpu: mips: Refactor EIC
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- cpu/mips_pic32_common/periph/timer.c
+++ cpu/mips_pic32_common/periph/timer.c
@@ -34,9 +34,7 @@
 #include "div.h"
 #include <sys/time.h>
 
-#ifdef EIC_IRQ
-#include "eic_irq.h"
-#endif
+#include "eic.h"
 
 /*
  * setting TIMER_ACCURACY_SHIFT lower will improve accuracy
@@ -70,7 +68,48 @@
 static timer_isr_ctx_t timer_isr_ctx;
 volatile unsigned int counter;
 volatile unsigned int compares[CHANNELS];
-static volatile int spurious_int;
+
+static void timer_isr(void)
+{
+    IFS0CLR =_IFS0_CTIF_MASK;
+
+    uint32_t status = irq_disable();
+    counter += TIMER_ACCURACY;
+    irq_restore(status);
+
+    if (counter == compares[0]) {
+        /*
+         * The Xtimer code expects the ISR to take some time
+         * but our counter is a fake software one, so bump it a
+         * bit to give the impression some time elapsed in the ISR.
+         * Without this the callback ( _shoot(timer) on xtimer_core.c )
+         * never fires.
+         */
+        counter += TIMER_ACCURACY;
+        timer_isr_ctx.cb(timer_isr_ctx.arg, 0);
+
+        if (sched_context_switch_request) {
+            thread_yield();
+        }
+    }
+    if (counter == compares[1]) {
+        timer_isr_ctx.cb(timer_isr_ctx.arg, 1);
+
+        if (sched_context_switch_request) {
+            thread_yield();
+        }
+    }
+    if (counter == compares[2]) {
+        timer_isr_ctx.cb(timer_isr_ctx.arg, 2);
+
+        if (sched_context_switch_request) {
+            thread_yield();
+        }
+    }
+
+    mips_setcompare(mips_getcount() + TICKS_PER_US * TIMER_ACCURACY);
+}
+
 
 /*
  * The mips toolchain C library does not implement gettimeofday()
@@ -111,12 +150,9 @@ int timer_init(tim_t dev, unsigned long freq, timer_cb_t cb, void *arg)
     mips32_bc_c0(C0_CAUSE, CR_DC);
 
     /* Enable Timer Interrupts */
-#ifdef EIC_IRQ
-    eic_irq_configure(EIC_IRQ_TIMER);
-#else
-    mips32_bs_c0(C0_STATUS, SR_HINT5);
-#endif
-
+    set_external_isr_cb(_CORE_TIMER_VECTOR, timer_isr);
+    eic_configure_priority(_CORE_TIMER_VECTOR, 1, 0);
+    eic_enable(_CORE_TIMER_VECTOR);
 
     return 0;
 }
@@ -188,96 +224,4 @@ void timer_stop(tim_t dev)
 {
     (void)dev;
     mips32_bs_c0(C0_CAUSE, CR_DC);
-}
-
-void timer_irq_enable(tim_t dev)
-{
-    (void)dev;
-#ifdef EIC_IRQ
-    eic_irq_enable(EIC_IRQ_TIMER);
-#else
-    mips32_bs_c0(C0_STATUS, SR_HINT5);
-#endif
-
-}
-
-void timer_irq_disable(tim_t dev)
-{
-    (void)dev;
-#ifdef EIC_IRQ
-    eic_irq_disable(EIC_IRQ_TIMER);
-#else
-    mips32_bc_c0(C0_STATUS, SR_HINT5);
-#endif
-}
-
-/* note Compiler inserts GP context save + restore code (to current stack). */
-#ifdef EIC_IRQ
-/*
- * This is a hack - currently the toolchain does not support correct placement
- * of EIC mode vectors (it is coming though) But we can support non-vectored EIC
- * mode and note the default PIC32 interrupt controller (which uses EIC +
- * MCU-ASE) defaults to non vectored mode anyway with all interrupts coming via
- * vector 0 which is equivalent to 'sw0' in 'VI' mode.
- *
- * Thus all EIC interrupts should be decoded here (currently only Timer is
- * used)
- *
- * When toolchain support is available we could move to full vector mode but
- * this does take up significant space (MCU-ASE provides 256 vectors at 32B
- * spacing (the default) that's 8KB of vector space!), So a single entry point
- * may be better anyway.
- *
- */
-void __attribute__ ((interrupt("vector=sw0"), keep_interrupts_masked)) _mips_isr_sw0(void)
-#else
-void __attribute__ ((interrupt("vector=hw5"))) _mips_isr_hw5(void)
-#endif
-{
-    register int cr = mips_getcr();
-
-    if (cr & CR_TI) {
-#ifdef EIC_IRQ
-        eic_irq_ack(EIC_IRQ_TIMER);
-#endif
-        uint32_t status = irq_disable();
-        counter += TIMER_ACCURACY;
-        irq_restore(status);
-
-        if (counter == compares[0]) {
-            /*
-             * The Xtimer code expects the ISR to take some time
-             * but our counter is a fake software one, so bump it a
-             * bit to give the impression some time elapsed in the ISR.
-             * Without this the callback ( _shoot(timer) on xtimer_core.c )
-             * never fires.
-             */
-            counter += TIMER_ACCURACY;
-            timer_isr_ctx.cb(timer_isr_ctx.arg, 0);
-
-            if (sched_context_switch_request) {
-                thread_yield();
-            }
-        }
-        if (counter == compares[1]) {
-            timer_isr_ctx.cb(timer_isr_ctx.arg, 1);
-
-            if (sched_context_switch_request) {
-                thread_yield();
-            }
-        }
-        if (counter == compares[2]) {
-            timer_isr_ctx.cb(timer_isr_ctx.arg, 2);
-
-            if (sched_context_switch_request) {
-                thread_yield();
-            }
-        }
-
-        mips_setcompare(mips_getcount() + TICKS_PER_US * TIMER_ACCURACY);
-
-    }
-    else {
-        spurious_int++;
-    }
 }
