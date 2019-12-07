0a33cfe2ad05616b2e4e0a3fce20acab8fd5a1eb
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-12-07 11:06:55 +0000

    boards: pic32-clicker: Do not mux pins in board_init
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

--- boards/pic32-clicker/clicker.c
+++ boards/pic32-clicker/clicker.c
@@ -15,13 +15,6 @@ extern void dummy(void);
 
 void board_init(void)
 {
-    /*
-     * Setup pin mux for UART3 this is the one connected
-     * to the mickroBUS
-     */
-    U3RXR =    0x2;          /*connect pin RPF5 to UART3 RX*/
-    RPF4R =    0x1;          /*connect pin RPF4 to UART3 TX*/
-
     /* Turn off all LED's */
     gpio_init(LED1_PIN, GPIO_OUT);
     gpio_init(LED2_PIN, GPIO_OUT);
