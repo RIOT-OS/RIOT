c70a14ff89e3fce339a1589e6aea04703f31757e
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-01-08 14:40:20 +0100

    boards/remote*,firefly: use common config headers

--- boards/remote-reva/include/periph_conf.h
+++ boards/remote-reva/include/periph_conf.h
@@ -22,70 +22,16 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
-#include "periph_common.h"
+#include "cfg_adc_default.h"
+#include "cfg_clk_default.h"
+#include "cfg_i2c_default.h"
+#include "cfg_spi_default.h"
+#include "cfg_timer_default.h"
+#include "cfg_uart_default.h"
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
-
-/**
- * @name I2C configuration
- * @{
- */
-#define I2C_IRQ_PRIO            1
-
-static const i2c_conf_t i2c_config[] = {
-    {
-        .speed = I2C_SPEED_FAST,    /**< bus speed */
-        .scl_pin = GPIO_PIN(2, 3),  /**< GPIO_PC3 */
-        .sda_pin = GPIO_PIN(2, 2)   /**< GPIO_PC2 */
-    },
-};
-
-#define I2C_NUMOF               ARRAY_SIZE(i2c_config)
-/** @} */
-
-/**
- * @name SPI configuration
- * @{
- */
-static const spi_conf_t spi_config[] = {
-    {
-        .num      = 0,
-        .mosi_pin = GPIO_PIN(1, 1),
-        .miso_pin = GPIO_PIN(1, 3),
-        .sck_pin  = GPIO_PIN(1, 2),
-        .cs_pin   = GPIO_PIN(1, 5)
-    },
-    {
-        .num      = 1,
-        .mosi_pin = GPIO_PIN(2, 5),
-        .miso_pin = GPIO_PIN(2, 6),
-        .sck_pin  = GPIO_PIN(2, 4),
-        .cs_pin   = GPIO_PIN(0, 7)
-    }
-};
-
-#define SPI_NUMOF           ARRAY_SIZE(spi_config)
-/** @} */
-
-/**
- * @name ADC configuration
- * @{
- */
-#define SOC_ADC_ADCCON3_EREF  SOC_ADC_ADCCON3_EREF_AVDD5
-
-static const adc_conf_t adc_config[] = {
-    GPIO_PIN(0, 5), /**< GPIO_PA5 = ADC1_PIN */
-    GPIO_PIN(0, 4), /**< GPIO_PA4 = ADC2_PIN */
-    /* voltage divider with 5/3 relationship to allow 5V sensors */
-    GPIO_PIN(0, 2), /**< GPIO_PA2 = ADC3_PIN */
-};
-
-#define ADC_NUMOF           ARRAY_SIZE(adc_config)
-/** @} */
-
-
 
 #ifdef __cplusplus
 } /* end extern "C" */
