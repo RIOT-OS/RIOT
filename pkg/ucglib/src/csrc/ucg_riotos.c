e79b411bc060065d86db286561c8ab96ac410322
Gunar Schorcht <gunar@schorcht.net>
2020-01-17 12:46:37 +0100

    pkg: use inline function for GPIO comparison
    The extensible GPIO API requires the comparison of structured GPIO types. This means that inline functions must be used instead of direct comparisons. For the migration process, packages must first be changed so that they use the inline comparison functions.

--- pkg/ucglib/src/csrc/ucg_riotos.c
+++ pkg/ucglib/src/csrc/ucg_riotos.c
@@ -54,7 +54,7 @@ static void ucg_enable_pins(gpio_t *pins, uint32_t pins_enabled)
 
     for (i = 0; i < 32; i++) {
         if (pins_enabled & ((uint32_t)1 << i)) {
-            if (pins[i] != GPIO_UNDEF) {
+            if (!gpio_is_undef(pins[i])) {
                 if (i < UCG_PIN_COUNT) {
                     gpio_init(pins[i], GPIO_OUT);
                 }
