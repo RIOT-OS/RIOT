f716cb1d3982fabc455cabd9f64fc9bdd92623ed
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2020-01-29 11:58:49 +0100

    makefiles/kconfig: Allow to override SHOULD_RUN_KCONFIG

--- makefiles/kconfig.mk
+++ makefiles/kconfig.mk
@@ -67,7 +67,7 @@ $(GENERATED_DIR): $(CLEAN)
 # configuration via Kconfig is disabled by default). Should this change, the
 # check would not longer be valid, and Kconfig would have to run on every
 # build.
-SHOULD_RUN_KCONFIG := $(or $(wildcard $(APPDIR)/*.config), $(wildcard $(APPDIR)/Kconfig), $(wildcard $(KCONFIG_MERGED_CONFIG)), $(filter menuconfig, $(MAKECMDGOALS)))
+SHOULD_RUN_KCONFIG ?= $(or $(wildcard $(APPDIR)/*.config), $(wildcard $(APPDIR)/Kconfig), $(wildcard $(KCONFIG_MERGED_CONFIG)), $(filter menuconfig, $(MAKECMDGOALS)))
 
 ifneq (,$(SHOULD_RUN_KCONFIG))
 # Add configuration header to build dependencies
