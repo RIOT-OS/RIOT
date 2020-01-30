fdc9e4af728f21c75e64c74ea27a9ae0742c478e
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-01-30 16:10:08 +0100

    boards/samr21-xpro: provide board_get_eui64()
    The board comes with an EU-64 stored inside EDBG.
    It is also printed on a label on the board.

--- boards/samr21-xpro/board.c
+++ boards/samr21-xpro/board.c
@@ -48,3 +48,44 @@ void board_init(void)
     /* initialize the CPU */
     cpu_init();
 }
+
+#ifdef MODULE_PERIPH_I2C
+#include "periph/i2c.h"
+#include "net/l2util.h"
+#include "net/netif_ids.h"
+
+#define DEV_I2C     I2C_DEV(0)
+#define EDBG_ADDR   (0x28)
+#define TOKEN_EUI64 (0xD2)
+#define TOKEN_EXT   (0xE1)
+
+/* EDBG provides an EUI-64 for the internal radio */
+size_t board_get_eui64(netif_id_t driver, unsigned idx, eui64_t *addr)
+{
+    if (driver != AT86RF2XX_DEVUID) {
+        return 0;
+    }
+
+    if (idx != 0) {
+        return 0;
+    }
+
+    i2c_acquire(DEV_I2C);
+
+    /* dummy read form another token */
+    if (i2c_read_regs(0, EDBG_ADDR, TOKEN_EXT, addr, 2, 0)) {
+        goto error;
+    }
+
+    if (i2c_read_regs(0, EDBG_ADDR, TOKEN_EUI64, addr, sizeof(*addr), 0)) {
+        goto error;
+    }
+
+    i2c_release(DEV_I2C);
+    return sizeof(eui64_t);
+
+error:
+    i2c_release(DEV_I2C);
+    return 0;
+}
+#endif
