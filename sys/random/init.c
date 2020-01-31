5c8ab98140024c7ac64886782917b34efce3330a
Sören Tempel <soeren+git@soeren-tempel.net>
2020-01-31 15:03:24 +0100

    random: separate random.c and init.c
    This requires less code to be compiled when recompiling init.c on each
    build with a new RANDOM_SEED_DEFAULT value.

---
+++ sys/random/init.c
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2017 Kaspar Schleiser <kaspar@schleiser.de>
+ *               2019 Freie Universität Berlin
+ *               2020 Sören Tempel <tempel@uni-bremen.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdint.h>
+
+#include "log.h"
+#include "random.h"
+
+#ifdef MODULE_PUF_SRAM
+#include "puf_sram.h"
+#endif
+#ifdef MODULE_PERIPH_HWRNG
+#include "periph/hwrng.h"
+#endif
+#ifdef MODULE_PERIPH_CPUID
+#include "luid.h"
+#endif
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+void auto_init_random(void)
+{
+    uint32_t seed;
+#ifdef MODULE_PUF_SRAM
+    /* TODO: hand state to application? */
+    if (puf_sram_state) {
+        LOG_WARNING("random: PUF SEED not fresh\n");
+    }
+    seed = puf_sram_seed;
+#elif defined (MODULE_PERIPH_HWRNG)
+    hwrng_read(&seed, 4);
+#elif defined (MODULE_PERIPH_CPUID)
+    luid_get(&seed, 4);
+#else
+    LOG_WARNING("random: NO SEED AVAILABLE!\n");
+    seed = RANDOM_SEED_DEFAULT;
+#endif
+    DEBUG("random: using seed value %u\n", (unsigned)seed);
+    random_init(seed);
+}
--- sys/random/random.c
+++ sys/random/random.c
@@ -26,39 +26,8 @@
 #include "random.h"
 #include "bitarithm.h"
 
-#ifdef MODULE_PUF_SRAM
-#include "puf_sram.h"
-#endif
-#ifdef MODULE_PERIPH_HWRNG
-#include "periph/hwrng.h"
-#endif
-#ifdef MODULE_PERIPH_CPUID
-#include "luid.h"
-#endif
-
 #define ENABLE_DEBUG (0)
 #include "debug.h"
-
-void auto_init_random(void)
-{
-    uint32_t seed;
-#ifdef MODULE_PUF_SRAM
-    /* TODO: hand state to application? */
-    if (puf_sram_state) {
-        LOG_WARNING("random: PUF SEED not fresh\n");
-    }
-    seed = puf_sram_seed;
-#elif defined (MODULE_PERIPH_HWRNG)
-    hwrng_read(&seed, 4);
-#elif defined (MODULE_PERIPH_CPUID)
-    luid_get(&seed, 4);
-#else
-    LOG_WARNING("random: NO SEED AVAILABLE!\n");
-    seed = RANDOM_SEED_DEFAULT;
-#endif
-    DEBUG("random: using seed value %u\n", (unsigned)seed);
-    random_init(seed);
-}
 
 void random_bytes(uint8_t *target, size_t n)
 {
