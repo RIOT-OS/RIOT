a9b21ed91138147645e70ae197cac80bf59edeb2
Hendrik van Essen <hendrik.ve@fu-berlin.de>
2020-02-07 13:08:23 +0100

    tests/shell: remove redundant parentheses

--- tests/shell/tests/01-run.py
+++ tests/shell/tests/01-run.py
@@ -40,23 +40,23 @@ DLE = '\x16'
 CONTROL_C = DLE+'\x03'
 CONTROL_D = DLE+'\x04'
 
+PROMPT = '> '
+
 CMDS = (
-    (CONTROL_C, ('>')),
-    ('start_test', ('[TEST_START]')),
-    ('end_test', ('[TEST_END]')),
-    ('\n', ('>')),
+    ('start_test', '[TEST_START]'),
+    (CONTROL_C, PROMPT),
+    ('\n', PROMPT),
     ('123456789012345678901234567890123456789012345678901234567890',
-        ('shell: command not found: '
-         '123456789012345678901234567890123456789012345678901234567890')),
-    ('unknown_command', ('shell: command not found: unknown_command')),
+     'shell: command not found: '
+     '123456789012345678901234567890123456789012345678901234567890'),
+    ('unknown_command', 'shell: command not found: unknown_command'),
     ('help', EXPECTED_HELP),
-    ('echo a string', ('\"echo\"\"a\"\"string\"')),
+    ('echo a string', '\"echo\"\"a\"\"string\"'),
     ('ps', EXPECTED_PS),
     ('help', EXPECTED_HELP),
-    ('reboot', ('test_shell.'))
+    ('reboot', 'test_shell.'),
+    ('end_test', '[TEST_END]'),
 )
-
-PROMPT = '> '
 
 BOARD = os.environ['BOARD']
 
