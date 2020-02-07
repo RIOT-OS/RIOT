8cafcc3ebf2ad6be27f79bbef71308b68a729052
Martine S. Lenders <m.lenders@fu-berlin.de>
2020-02-07 16:51:42 +0100

    tests: provide tests for 6LBR DHCPv6 client

---
+++ tests/gnrc_dhcpv6_client_6lbr/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2020 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ *
+ * @file
+ * @author  Martine Lenders <m.lenders@fu-berlin.de>
+ */
+
+#include "shell.h"
+
+int main(void)
+{
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    /* should be never reached */
+    return 0;
+}
+
+/** @} */
