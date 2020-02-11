a0462488e8e1d088d9e9b2133c08541f50bb7c10
Francisco Molina <femolina@uc.cl>
2020-02-11 22:31:00 +0100

    cpu/efm32: add families/%/Makefile.include
    Move INCLUDES and VECTORS_O to Makefile.include duplicate EFM32_HEADER
    while cpu.txt is not migrated.

--- cpu/efm32/efm32-info.mk
+++ cpu/efm32/efm32-info.mk
@@ -1,10 +1,6 @@
 # Find the header file that should exist if the CPU is supported.
 EFM32_HEADER = $(wildcard $(RIOTCPU)/efm32/families/*/include/vendor/$(CPU_MODEL).h)
 
-ifeq (,$(EFM32_HEADER))
-  $(error Header file for $(CPU_MODEL) is missing)
-endif
-
 # Lookup up CPU information using grep.
 EFM32_INFO = $(shell grep $(CPU_MODEL) $(shell dirname $(EFM32_HEADER))/../../cpus.txt)
 
