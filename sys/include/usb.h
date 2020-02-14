c0cc03a09ab943ecb26497dc1bc32825234eaa12
chrysn <chrysn@fsfe.org>
2020-02-14 13:59:31 +0100

    usb: Adapt error message to newly present build variables

--- sys/include/usb.h
+++ sys/include/usb.h
@@ -51,7 +51,7 @@ extern "C" {
 #define CONFIG_USB_PID INTERNAL_PERIPHERAL_PID
 #else
 #error Please configure your vendor and product IDs. For development, you may \
-    set CONFIG_USB_VID=0x1209 CONFIG_USB_PID=0x7D01.
+    set USB_VID=${USB_VID_TESTING} USB_PID=${USB_PID_TESTING}.
 #endif
 #endif
 
