4b33300dce6008eddb15dd64aa5141566b982c7d
fabian18 <fabian.huessler@ovgu.de>
2020-02-23 10:05:26 +0100

    fixup! pkg/openthread/contrib: adaptation for multiple at86rf2xx types

--- pkg/openthread/contrib/openthread.c
+++ pkg/openthread/contrib/openthread.c
@@ -27,19 +27,26 @@
 
 #ifdef MODULE_AT86RF2XX
 #include "at86rf2xx.h"
-#include "at86rf2xx_devs.h"
+#include "at86rf2xx_params.h"
+#define OPENTHREAD_NETIF_NUMOF        AT86RF2XX_NUM
 #endif
 
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
-#ifdef MODULE_AT86RF2XX     /* is mutual exclusive with above ifdef */
-#define OPENTHREAD_NETIF_NUMOF        AT86RF2XX_NUM
-#endif
-
 /* Only one type of transceiver is expected here */
-#ifdef MODULE_AT86RF2XX
-static at86rf2xx_devs_t at86rf2xx_devs;
+#ifdef MODULE_AT86RF212B
+static at86rf212b_t at86rf212b_dev;
+#elif defined  MODULE_AT86RF231
+static at86rf231_t at86rf231_dev;
+#elif defined MODULE_AT86RF232
+static at86rf232_t at86rf232_dev;
+#elif defined MODULE_AT86RF233
+static at86rf233_t at86rf233_dev;
+#elif defined MODULE_AT86RFA1
+static at86rfa1_t at86rfa1_dev;
+#elif defined MODULE_AT86RFR2
+static at86rfr2_t at86rfr2_dev;
 #endif
 
 static uint8_t rx_buf[OPENTHREAD_NETDEV_BUFLEN];
@@ -51,12 +58,27 @@ void openthread_bootstrap(void)
     /* init random */
     ot_random_init();
 
-#ifdef MODULE_AT86RF2XX
-    /* setup netdev modules */
-    at86rf2xx_setup_devs(&at86rf2xx_devs);
-    /* take whatever is the first device */
-    netdev_t *netdev = (netdev_t *) at86rf2xx_devs.mem_devs;
+    netdev_t *netdev = NULL;
+#ifdef MODULE_AT86RF212B
+    at86rf212b_setup(&at86rf212b_dev, at86rf212b_params, 1);
+    netdev = (netdev_t *)&at86rf212b_dev.base.netdev;
+#elif defined  MODULE_AT86RF231
+    at86rf231_setup(&at86rf212b_dev, at86rf231_params, 1);
+    netdev = (netdev_t *)&at86rf231_dev.base.netdev;
+#elif defined MODULE_AT86RF232
+    at86rf232_setup(&at86rf232_dev, at86rf232_params, 1);
+    netdev = (netdev_t *)&at86rf232_dev.base.netdev;
+#elif defined MODULE_AT86RF233
+    at86rf233_setup(&at86rf233_dev, at86rf233_params, 1);
+    netdev = (netdev_t *)&at86rf233_dev.base.netdev;
+#elif defined MODULE_AT86RFA1
+    at86rfa1_setup(&at86rfa1_dev);
+    netdev = (netdev_t *)&at86rfa1_dev.base.netdev;
+#elif defined MODULE_AT86RFR2
+    at86rfr2_setup(&at86rfr2_dev);
+    netdev = (netdev_t *)&at86rfr2_dev.base.netdev;
 #endif
+    assert(netdev);
     openthread_radio_init(netdev, tx_buf, rx_buf);
     openthread_netdev_init(ot_thread_stack, sizeof(ot_thread_stack), THREAD_PRIORITY_MAIN - 5, "openthread", netdev);
 }
