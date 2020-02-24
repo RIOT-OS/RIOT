895eb943d875717b25ff00a7dce57f607dfc4ae2
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-02-24 17:56:11 +0100

    cpu/sam0_common: add cpu_pm_cb_enter()/leave()
    This allows to implement needed work-arounds surrounding sleep on
    a per-MCU basis.

--- cpu/samd21/cpu.c
+++ cpu/samd21/cpu.c
@@ -90,6 +90,18 @@ uint32_t sam0_gclk_freq(uint8_t id)
     }
 }
 
+void cpu_pm_cb_enter(int deep)
+{
+    (void) deep;
+    /* will be called before entering sleep */
+}
+
+void cpu_pm_cb_leave(int deep)
+{
+    (void) deep;
+    /* will be called after wake-up */
+}
+
 /**
  * @brief   Configure clock sources and the cpu frequency
  */
