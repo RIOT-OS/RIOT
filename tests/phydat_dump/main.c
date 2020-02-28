db04bc1af46dcb72d2e5f193f26940ab0d09e78d
Gunar Schorcht <gunar@schorcht.net>
2020-02-28 13:22:48 +0100

    tests: add phydat dump test

---
+++ tests/phydat_dump/main.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2020 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ *
+ * @file
+ * @brief       Visual test for the conversion from phydat to a string
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ *
+ */
+
+#include <stdio.h>
+
+#include "phydat.h"
+
+typedef struct {
+    phydat_t dat;
+    uint8_t dim;
+} _phydat_test_t;
+
+_phydat_test_t _test_vector[] =
+{
+    { .dim = 1, .dat = { .val = { 1234 }, .unit = UNIT_TEMP_C, .scale = -2 } },
+    { .dim = 1, .dat = { .val = { 1234 }, .unit = UNIT_TEMP_F, .scale = -2 } },
+    { .dim = 1, .dat = { .val = { 1234 }, .unit = UNIT_TEMP_K, .scale = -1 } },
+    { .dim = 1, .dat = { .val = { 1234 }, .unit = UNIT_LUX, .scale = -1 } },
+    { .dim = 3, .dat = { .val = { 1234, 13456, -14567 }, .unit = UNIT_M, .scale = 0 } },
+    { .dim = 3, .dat = { .val = { 1234, 13456, -14567 }, .unit = UNIT_M, .scale = 1 } },
+    { .dim = 3, .dat = { .val = { 1234, 13456, -14567 }, .unit = UNIT_M, .scale = 3 } },
+    { .dim = 3, .dat = { .val = { 1234, 13456, -14567 }, .unit = UNIT_M, .scale = -2 } },
+    { .dim = 3, .dat = { .val = { 1234, 13456, -14567 }, .unit = UNIT_M, .scale = -3 } },
+    { .dim = 1, .dat = { .val = { -12345 }, .unit = UNIT_M2, .scale = -5 } },
+    { .dim = 1, .dat = { .val = { -12345 }, .unit = UNIT_M3, .scale = -6 } },
+    { .dim = 3, .dat = { .val = { 12, 34, 5678}, .unit = UNIT_G, .scale = -2 } },
+    { .dim = 3, .dat = { .val = { 123, 345, 678 }, .unit = UNIT_DPS, .scale = -3 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_GR, .scale = -1 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_A, .scale = 3 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_V, .scale = 6 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_W, .scale = 7 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_GS, .scale = -1 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_DBM, .scale = -3 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_COULOMB, .scale = 0 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_F, .scale = -6 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_F, .scale = -7 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PH, .scale = -2 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_BAR, .scale = 0 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PA, .scale = 2 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_CD, .scale = -2 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_CTS, .scale = -3 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PERCENT, .scale = -3 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PERMILL, .scale = -4 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PPM, .scale = -6 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_PPB, .scale = -9 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_GPM3, .scale = -1 } },
+    { .dim = 1, .dat = { .val = { 12345 }, .unit = UNIT_CPM3, .scale = -1 } },
+    { .dim = 3, .dat = { .val = { 56, 34, 12 }, .unit = UNIT_TIME, .scale = -1 } },
+    { .dim = 3, .dat = { .val = { 27, 2, 2020 }, .unit = UNIT_DATE, .scale = -1 } },
+};
+
+int main(void)
+{
+    puts("Visual phydat test application");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(_test_vector); i++) {
+        phydat_dump(&_test_vector[i].dat, _test_vector[i].dim);
+    }
+    return 0;
+}
