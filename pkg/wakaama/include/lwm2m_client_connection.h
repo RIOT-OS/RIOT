5c65409dfc13829a8f1ddedb51aac1ebae5db9bb
Gunar Schorcht <gunar@schorcht.net>
2020-03-04 13:28:23 +0100

    pkg/wakaama: fix broken links

--- pkg/wakaama/include/lwm2m_client_connection.h
+++ pkg/wakaama/include/lwm2m_client_connection.h
@@ -5,10 +5,10 @@
 * are made available under the terms of the Eclipse Public License v1.0
 * and Eclipse Distribution License v1.0 which accompany this distribution.
 *
-* The Eclipse Public License is available at
+* The Eclipse Public License is available at:
 *    http://www.eclipse.org/legal/epl-v10.html
-* The Eclipse Distribution License is available at
-*    http://www.eclipse.org/org/documents/edl-v10.php.
+* The Eclipse Distribution License is available at:
+*    http://www.eclipse.org/org/documents/edl-v10.php
 *
 * Contributors:
 *    Simon Bernard - initial API and implementation
