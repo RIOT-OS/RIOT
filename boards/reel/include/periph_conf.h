0573db8a85195d4cf080413edbe0e3b5c8ba18e5
Koen Zandberg <koen@bergzand.net>
2020-03-07 17:10:54 +0100

    reel: Enable the nRF52 built-in DC/DC converter

--- boards/reel/include/periph_conf.h
+++ boards/reel/include/periph_conf.h
@@ -66,6 +66,11 @@ static const spi_conf_t spi_config[] = {
 #define SPI_NUMOF           ARRAY_SIZE(spi_config)
 /** @} */
 
+/**
+ * @brief Enable the internal DC/DC converter
+ */
+#define NRF5X_ENABLE_DCDC
+
 #ifdef __cplusplus
 }
 #endif
