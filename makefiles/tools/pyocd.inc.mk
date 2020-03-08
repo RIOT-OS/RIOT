2977b06a8d5573fe81cb9736eee64113b2c81203
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-03-08 15:03:18 +0100

    makefiles/pyocd: only export FLASH_TARGET_TYPE when required

--- makefiles/tools/pyocd.inc.mk
+++ makefiles/tools/pyocd.inc.mk
@@ -3,11 +3,19 @@ DEBUGGER = $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 DEBUGSERVER = $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 RESET ?= $(RIOTBASE)/dist/tools/pyocd/pyocd.sh
 
+FLASH_TARGET_TYPE ?=
 FLASHFILE ?= $(HEXFILE)
 FFLAGS ?= flash $(FLASHFILE)
 DEBUGGER_FLAGS ?= debug $(ELFFILE)
 DEBUGSERVER_FLAGS ?= debug-server
 RESET_FLAGS ?= reset
+
+PYOCD_TARGETS = debug% flash% reset
+
+# Export FLASH_TARGET_TYPE to required targets only if not empty
+ifneq (,$(FLASH_TARGET_TYPE))
+  $(call target-export-variables,$(PYOCD_TARGETS),FLASH_TARGET_TYPE)
+endif
 
 # Add serial matching command, only if DEBUG_ADAPTER_ID was specified
 ifneq (,$(DEBUG_ADAPTER_ID))
