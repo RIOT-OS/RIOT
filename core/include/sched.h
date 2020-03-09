05d4ab8c92658935bc234a38a1761a836f1510f7
smlng <s@mlng.net>
2020-03-09 15:55:12 +0100

    doc: fix undefined typedef docu
    Rework documentation of enum with typedef, i.e. remove grouping
    as it is not required.

--- core/include/sched.h
+++ core/include/sched.h
@@ -96,8 +96,8 @@
 typedef struct _thread thread_t;
 
 /**
- * @name Thread states supported by RIOT
- * @{
+ * @enum thread_status_t
+ * Thread states supported by RIOT
  */
 typedef enum {
     STATUS_STOPPED,                 /**< has terminated                           */
@@ -115,7 +115,6 @@ typedef enum {
     STATUS_PENDING,                 /**< waiting to be scheduled to run           */
     STATUS_NUMOF                    /**< number of supported thread states        */
 } thread_status_t;
-/** @} */
 
 /**
  * @name Helpers to work with thread states
