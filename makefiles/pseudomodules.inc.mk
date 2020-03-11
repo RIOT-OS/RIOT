a7f044255c87b22b058a0ac0686526926eca619a
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-03-11 14:04:44 +0100

    at86rf215: fix module handling

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -113,9 +113,10 @@ NO_PSEUDOMODULES += at24cxxx
 
 # include variants of the AT86RF2xx drivers as pseudo modules
 PSEUDOMODULES += at86rf23%
-PSEUDOMODULES += at86rf212b
+PSEUDOMODULES += at86rf21%
 PSEUDOMODULES += at86rfa1
 PSEUDOMODULES += at86rfr2
+NO_PSEUDOMODULES += at86rf215
 
 # include variants of the BMX280 drivers as pseudo modules
 PSEUDOMODULES += bmp280_i2c
