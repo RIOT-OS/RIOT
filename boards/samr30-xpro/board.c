8034342c5483d3aa0de749db8255f9def9335faa
Juergen Fitschen <me@jue.yt>
2020-03-12 18:14:23 +0100

    boards/samr30-xpro: add function to configure antenna switch

--- boards/samr30-xpro/board.c
+++ boards/samr30-xpro/board.c
@@ -28,6 +28,18 @@
 
 void led_init(void);
 
+void board_antenna_config(uint8_t antenna)
+{
+    if (antenna == RFCTL_ANTENNA_EXT){
+        gpio_clear(RFCTL1_PIN);
+        gpio_set(RFCTL2_PIN);
+    }
+    else if (antenna == RFCTL_ANTENNA_BOARD){
+        gpio_set(RFCTL1_PIN);
+        gpio_clear(RFCTL2_PIN);
+    }
+}
+
 void board_init(void)
 {
     /* initialize the CPU */
@@ -35,6 +47,12 @@ void board_init(void)
 
     /* initialize the boards LEDs */
     led_init();
+
+    /* initialize the on-board antenna switch */
+    gpio_init(RFCTL1_PIN, GPIO_OUT);
+    gpio_init(RFCTL2_PIN, GPIO_OUT);
+    /* set default antenna switch configuration */
+    board_antenna_config(RFCTL_ANTENNA_DEFAULT);
 }
 
 
