7bc15acee25447b1102c1b3e70067995833ef5c3
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2020-03-19 14:06:47 +0100

    build system: Restructure dependency resolution
    Goals:
    - Untangle dependency resolution and feature checking for better maintainability
    - Improve performance of "make info-boards-supported"
    
    Changes:
    - Makefile.dep
        - Dropped handling of default modules and recursion
        - Now only dependencies of the current set of used modules and pkgs are
          added
      ==> External recursion is needed to catch transient dependencies
    - Changed Makefile.features:
        - Dropped checking of provided features
        - Dropped populating FEATURES_USED with provided features that are required
          or optional
        - Dropped populating FEATURES_MISSING with required but not provided
          features
        - Dropped adding modules implementing used features to USE_MODULE
      ==> This now only populates FEATURES_PROVIDED, nothing more
    - Added makefiles/features_check.inc.mk:
        - This performs the population of FEATURES_USED and FEATURES_MISSING now
    - Added makefiles/features_modules.inc.mk:
        - This performs now the addition of modules implementing used features
    - Added makefiles/dependency_resolution.inc.mk:
        - This now performs the recursion required to catch transient dependencies
        - Also the feature check is performed recursively to handle also required
          and optional features of the transient dependencies
        - DEFAULT_MODULES are added repeatedly to allow it to be extended based on
          used features and modules
          ==> This allows modules to have optional dependencies, as these
              dependencies can be blacklisted
    - Use simply expanded variables instead of recursively expended variables
      (`foo := $(bar)` instead `foo = $(bar)`) for internal variables during feature
      resolution. This improves performance significantly for
      `make info-boards-supported`.
    - Reduce dependency resolution steps in `make info-boards-supported`
        - Globally resolve dependencies without any features (including arch)
          provided
          ==> This results in the common subset of feature requirements and modules
              used
            - But for individual boards additional modules might be used on top due
              to architecture specific dependencies or optional features
        - Boards not supporting this subset of commonly required features are not
          supported, so no additional dependency resolution is needed for them
        - For each board supporting the common set of requirements a complete
          dependency resolution is still needed to also catch architecture specific
          hacks
             - But this resolution is seeded with the common set of dependencies to
               speed this up

--- makefiles/info-global.inc.mk
+++ makefiles/info-global.inc.mk
@@ -1,6 +1,16 @@
 .PHONY: info-buildsizes info-buildsizes-diff info-features-missing \
         info-boards-features-missing
 
+# Perform dependency resolution without having included
+# $(RIOTBASE)/Makefile.features for now. This results in no optional modules and
+# no architecture specific handling being done. The result will be a subset of
+# used modules and requirements that is present in for *all* boards, so this can
+# be cached to speed up individual dependency checks
+include $(RIOTMAKE)/defaultmodules.inc.mk
+# add default modules
+USEMODULE += $(filter-out $(DISABLE_MODULE),$(DEFAULT_MODULE))
+include $(RIOTMAKE)/dependency_resolution.inc.mk
+
 BOARDSDIR_GLOBAL := $(BOARDSDIR)
 USEMODULE_GLOBAL := $(USEMODULE)
 USEPKG_GLOBAL := $(USEPKG)
@@ -27,6 +37,7 @@ define board_unsatisfied_features
 
   # Remove board specific variables set by Makefile.features/Makefile.dep
   FEATURES_PROVIDED :=
+  FEATURES_USED :=
 
   # Undefine variables that must not be defined when starting.
   # Some are sometime set as `?=`
@@ -40,20 +51,28 @@ define board_unsatisfied_features
   endif
 
   include $(RIOTBASE)/Makefile.features
-
-  include $(RIOTMAKE)/defaultmodules.inc.mk
-  USEMODULE += $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
-
-  include $(RIOTBASE)/Makefile.dep
-
+  # FEATURES_USED must be populated first in this case so that dependency
+  # resolution can take optional features into account during the first pass.
+  # Also: This allows us to skip resolution if already a missing feature is
+  # detected
+  include $(RIOTMAKE)/features_check.inc.mk
   ifneq (,$$(FEATURES_MISSING))
+    # Skip full dependency resolution, as even without optional modules features
+    # and architecture specific limitations already some features are missing
     BOARDS_FEATURES_MISSING += "$(1) $$(FEATURES_MISSING)"
     BOARDS_WITH_MISSING_FEATURES += $(1)
-  endif
+  else
+    include $(RIOTMAKE)/dependency_resolution.inc.mk
 
-  ifneq (,$$(FEATURES_USED_BLACKLISTED))
-    BOARDS_FEATURES_USED_BLACKLISTED += "$(1) $$(FEATURES_USED_BLACKLISTED)"
-    BOARDS_WITH_BLACKLISTED_FEATURES += $(1)
+    ifneq (,$$(FEATURES_MISSING))
+      BOARDS_FEATURES_MISSING += "$(1) $$(FEATURES_MISSING)"
+      BOARDS_WITH_MISSING_FEATURES += $(1)
+    endif
+
+    ifneq (,$$(FEATURES_USED_BLACKLISTED))
+      BOARDS_FEATURES_USED_BLACKLISTED += "$(1) $$(FEATURES_USED_BLACKLISTED)"
+      BOARDS_WITH_BLACKLISTED_FEATURES += $(1)
+    endif
   endif
 
   ifneq (,$$(DEPENDENCY_DEBUG))
