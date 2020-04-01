d2e03ab5ca902ebe56ee6bbb0b1c6f41060fda6b
Francisco Molina <femolina@uc.cl>
2020-04-01 14:24:59 +0200

    makefiles/defaultmodules.inc.mk: add BOARDs, CPUs defaultmodules.inc.mk
    Allow for BOARDs and CPUs to declare DEFAULT_MODULEs that might have
    dependencies and therefore require being handled before parsing
    Makefile.dep.
    
    DEFAULT_MODULEs declared in %/defaultmodules.inc.mk can only be disabled
    at application level. In other cases the behaviour is undefined.

--- makefiles/defaultmodules.inc.mk
+++ makefiles/defaultmodules.inc.mk
@@ -5,5 +5,8 @@ DEFAULT_MODULE += auto_init
 # Initialize all used peripherals by default
 DEFAULT_MODULE += periph_init
 
+-include $(BOARDSDIR)/$(BOARD)/defaultmodules.inc.mk
+-include $(RIOTCPU)/$(CPU)/defaultmodules.inc.mk
+
 # Handle inclusion of DEFAULT_MODULEs once
 USEMODULE := $(USEMODULE) $(filter-out $(DISABLE_MODULE), $(DEFAULT_MODULE))
