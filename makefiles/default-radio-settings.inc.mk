743c99ff30ba2c3ceca5decee6f688950cd2c2f1
Jiawei-Lu <50105190+Jiawei-Lu@users.noreply.github.com>
2020-04-03 22:21:18 +0100

    Update default-radio-settings.inc.mk
    New feature: EU ISM 868Mhz frequency Band is supported for Sam R30 Xpro.
    Easily, 
     make  BOARD=samr30-xpro ISM=868 willchange the default channel and page number into 0. Test has been down by sending UPD between two samr30-xpro and it works.

--- makefiles/default-radio-settings.inc.mk
+++ makefiles/default-radio-settings.inc.mk
@@ -1,26 +1,30 @@
-# ISM = 868Mhz Channel & Page Switch for EU area
-ifeq (868,$(ISM))
-  ifneq (,$(filter at86rf212b,$(USEMODULE)))    # radio is IEEE 802.15.4 sub-GHz
-    EU_CHANNEL ?= 0
-    CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_CHANNEL=$(UK_CHANNEL)
-    EU_PAGE ?= 0
-    CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_PAGE=$(UK_PAGE)
-    # As Issues/PRS reference #13526 has already sloved, we don't need change the SPI_CLK anaymore
-    # SCLK := SPI_CLK_1MHZ
-    # CFLAGS += -DAT86RF2XX_PARAM_SPI_CLK=$(SCLK)
+ifneq (EU,$(ISM))
+  # Default seeting which working on US (Channel 26 for 2.4 GHz, Channel 5/Page 2 for sub-GHz)
+  ifneq (,$(DEFAULT_CHANNEL))
+    ifneq (,$(filter cc110x,$(USEMODULE)))        # radio is cc110x sub-GHz
+      CFLAGS += -DCC110X_DEFAULT_CHANNEL=$(DEFAULT_CHANNEL)
+    endif
+    ifneq (,$(filter at86rf212b,$(USEMODULE)))    # radio is IEEE 802.15.4 sub-GHz
+      CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_CHANNEL=$(DEFAULT_CHANNEL)
+    else                                          # radio is IEEE 802.15.4 2.4 GHz
+      CFLAGS += -DIEEE802154_DEFAULT_CHANNEL=$(DEFAULT_CHANNEL)
+    endif
   endif
-endif
-ifneq (,$(DEFAULT_CHANNEL))
+else      # Switching to ISM-EU if ISM=EU when make (sub-GHz channel = 0 / sub-GHz page = 0)
   ifneq (,$(filter cc110x,$(USEMODULE)))        # radio is cc110x sub-GHz
     CFLAGS += -DCC110X_DEFAULT_CHANNEL=$(DEFAULT_CHANNEL)
   endif
-  ifneq (,$(filter at86rf212b,$(USEMODULE)))    # radio is IEEE 802.15.4 sub-GHz
-    CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_CHANNEL=$(DEFAULT_CHANNEL)
+  ifneq (,$(filter at86rf212b,$(USEMODULE)))    # radio sub-GHz channel = 0 / sub-GHz page = 0
+    EU_CHANNEL ?= 0
+    CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_CHANNEL=$(EU_CHANNEL)
+    EU_PAGE ?= 0
+    CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_PAGE=$(EU_PAGE)
   else                                          # radio is IEEE 802.15.4 2.4 GHz
     CFLAGS += -DIEEE802154_DEFAULT_CHANNEL=$(DEFAULT_CHANNEL)
   endif
 endif
-  # Set a custom PAN ID if needed
+
+# Set a custom PAN ID if needed
 ifneq (,$(DEFAULT_PAN_ID))
   CFLAGS += -DIEEE802154_DEFAULT_PANID=$(DEFAULT_PAN_ID)
 endif
