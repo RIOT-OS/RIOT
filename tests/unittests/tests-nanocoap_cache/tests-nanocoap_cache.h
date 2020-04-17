b4cae9d09de2381905e8bec8af647658a6f71ef5
Cenk Gündoğan <mail+dev@gundogan.net>
2020-04-17 13:50:06 +0200

    nanocoap: add unittests for nanocoap-cache

---
+++ tests/unittests/tests-nanocoap_cache/tests-nanocoap_cache.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unit tests for the nanocoap-cache module
+ *
+ * @author      Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
+ */
+#ifndef TESTS_NANOCOAP_CACHE_H
+#define TESTS_NANOCOAP_CACHE_H
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_nanocoap_cache(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_NANOCOAP_CACHE_H */
+/** @} */
