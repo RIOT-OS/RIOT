ed38bd86d4891eabec78b5b6c7a06c73cb70108a
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2020-04-21 20:21:04 +0200

    makefiles/kconfiglib: Use RIOT specific menuconfig script

--- makefiles/tools/kconfiglib.inc.mk
+++ makefiles/tools/kconfiglib.inc.mk
@@ -1,13 +1,14 @@
 # Define tools to use
-MENUCONFIG ?= $(RIOTTOOLS)/kconfiglib/menuconfig.py
+MENUCONFIG ?= $(RIOTTOOLS)/kconfiglib/riot_menuconfig.py
+BASE_MENUCONFIG ?= $(RIOTTOOLS)/kconfiglib/menuconfig.py
 GENCONFIG ?= $(RIOTTOOLS)/kconfiglib/genconfig.py
 MERGECONFIG ?= $(RIOTTOOLS)/kconfiglib/merge_config.py
 
-$(MENUCONFIG):
+$(BASE_MENUCONFIG):
 	@echo "[INFO] Kconfiglib not found - getting it"
 	@make -C $(RIOTTOOLS)/kconfiglib
 	@echo "[INFO] Kconfiglib downloaded"
 
-$(GENCONFIG): $(MENUCONFIG)
+$(GENCONFIG): $(BASE_MENUCONFIG)
 
-$(MERGECONFIG): $(MENUCONFIG)
+$(MERGECONFIG): $(BASE_MENUCONFIG)
