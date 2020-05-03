36f6de3ce6a8010a3c07d4ccacf58667db78c71c
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-03 15:26:34 +0200

    cpu/stm32: unify riotboot specific configuration

--- cpu/stm32/stm32l4_riotboot.mk
+++
@@ -1,5 +0,0 @@
-# "The Vector table must be naturally aligned to a power of two whose alignment
-# value is greater than or equal to number of Exceptions supported x 4"
-# CPU_IRQ_NUMOFF for stm32l4 boards is < 91+16 so (107*4 bytes = 428 bytes ~= 0x200)
-# RIOTBOOT_HDR_LEN can be set to 0x200
-RIOTBOOT_HDR_LEN ?= 0x200
