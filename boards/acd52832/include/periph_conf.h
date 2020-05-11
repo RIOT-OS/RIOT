825c4c8cb72c9e034f689e80b92d9bef9794e47e
Koen Zandberg <koen@bergzand.net>
2020-05-11 21:06:48 +0200

    acd52832: Adapt SPI periph config

--- boards/acd52832/include/periph_conf.h
+++ boards/acd52832/include/periph_conf.h
@@ -54,10 +54,11 @@ extern "C" {
  */
 static const spi_conf_t spi_config[] = {
     {
-        .dev  = NRF_SPI0,
+        .dev  = NRF_SPIM0,
         .sclk = 4,
         .mosi = 3,
-        .miso = 13
+        .miso = 13,
+        .ppi = 0
     }
 };
 
