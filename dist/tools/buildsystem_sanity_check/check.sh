0a4beec345727ecf7ebfe0c011abaa44480b3333
Juergen Fitschen <me@jue.yt>
2020-05-23 16:35:05 +0200

    buildsystem_sanity_check: don't touch PSEUDOMODULES in Makefile.dep

--- dist/tools/buildsystem_sanity_check/check.sh
+++ dist/tools/buildsystem_sanity_check/check.sh
@@ -253,6 +253,18 @@ check_files_in_boards_not_reference_board_var() {
         | error_with_message 'Code in boards/ should not use $(BOARDS) to reference files since this breaks external BOARDS changing BOARDSDIR"'
 }
 
+check_no_pseudomules_in_makefile_dep() {
+    local patterns=()
+    local pathspec=()
+
+    patterns+=(-e 'PSEUDOMODULES[\t ]*[+:]*=')
+
+    pathspec+=('**/Makefile.dep')
+
+    git -C "${RIOTBASE}" grep "${patterns[@]}" -- "${pathspec[@]}" \
+        | error_with_message "Don't define PSEUDOMODULES in Makefile.dep"
+}
+
 error_on_input() {
     ! grep ''
 }
@@ -268,6 +280,7 @@ all_checks() {
     checks_tests_application_not_defined_in_makefile
     checks_develhelp_not_defined_via_cflags
     check_files_in_boards_not_reference_board_var
+    check_no_pseudomules_in_makefile_dep
 }
 
 main() {
