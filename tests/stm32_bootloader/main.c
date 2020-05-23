43585804b76074ab46d00985cdbdcf7aa0f417b6
Benjamin Valentin <benpicco@beuth-hochschule.de>
2020-05-23 16:04:30 +0200

    tests/stm32_bootloader: add test for STM32 bootloader mode

---
+++ tests/stm32_bootloader/main.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Benjamin Valentin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Test application to activate the STM32 bootloader mode.
+ *
+ * @author      Benjamin Valentin <benpicco@googlemail.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "shell.h"
+#include "shell_commands.h"
+
+int main(void)
+{
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+
+    puts("STM32 bootloader test application.");
+
+    /* bootloader command is provided by default */
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
