ccdcc3679c2c4c4168dd2c0fc4397470b0783b76
Martine S. Lenders <mail@martine-lenders.eu>
2020-05-26 20:37:42 +0200

    tinydtls: provide implementation for sock_dtls_get_udp_sock

--- pkg/tinydtls/contrib/sock_dtls.c
+++ pkg/tinydtls/contrib/sock_dtls.c
@@ -259,6 +259,12 @@ int sock_dtls_create(sock_dtls_t *sock, sock_udp_t *udp_sock,
     return 0;
 }
 
+sock_udp_t *sock_dtls_get_udp_sock(sock_dtls_t *sock)
+{
+    assert(sock);
+    return sock->udp_sock;
+}
+
 int sock_dtls_session_init(sock_dtls_t *sock, const sock_udp_ep_t *ep,
                            sock_dtls_session_t *remote)
 {
