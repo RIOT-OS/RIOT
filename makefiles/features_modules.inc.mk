24facae6fba0185b01d40978c5d41cdfcbb9c057
Kaspar Schleiser <kaspar@schleiser.de>
2020-06-04 21:25:58 +0200

    makefiles/features_modules: add cortexm_svc

--- makefiles/features_modules.inc.mk
+++ makefiles/features_modules.inc.mk
@@ -25,3 +25,6 @@ USEMODULE += $(filter cpu_check_address, $(FEATURES_USED))
 ifneq (,$(filter periph_%, $(USEMODULE)))
   USEMODULE += periph_common
 endif
+
+# select cortexm_svc pseudomodule if the corresponding feature is used
+USEMODULE += $(filter cortexm_svc, $(FEATURES_USED))
