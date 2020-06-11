35bf7d5b41e728904200c2298cc5445ec1fa13cc
Koen Zandberg <koen@bergzand.net>
2020-06-11 21:01:43 +0200

    same54-xpro: add DMA triggers to spi config

--- boards/same54-xpro/include/periph_conf.h
+++ boards/same54-xpro/include/periph_conf.h
@@ -162,6 +162,10 @@ static const spi_conf_t spi_config[] = {
         .miso_pad = SPI_PAD_MISO_3,
         .mosi_pad = SPI_PAD_MOSI_0_SCK_1,
         .gclk_src = SAM0_GCLK_48MHZ,
+#ifdef MODULE_PERIPH_DMA
+        .tx_trigger = SERCOM4_DMAC_ID_TX,
+        .rx_trigger = SERCOM4_DMAC_ID_RX,
+#endif
 
     },
     {    /* EXT2, EXT3 */
@@ -175,6 +179,10 @@ static const spi_conf_t spi_config[] = {
         .miso_pad = SPI_PAD_MISO_3,
         .mosi_pad = SPI_PAD_MOSI_0_SCK_1,
         .gclk_src = SAM0_GCLK_48MHZ,
+#ifdef MODULE_PERIPH_DMA
+        .tx_trigger = SERCOM6_DMAC_ID_TX,
+        .rx_trigger = SERCOM6_DMAC_ID_RX,
+#endif
     }
 };
 
