1c49a43e7d8796eb1219ae60d44770894a954933
Jiawei-Lu <50105190+Jiawei-Lu@users.noreply.github.com>
2020-06-12 14:54:18 +0100

    Update default-radio-settings.inc.mk

--- makefiles/default-radio-settings.inc.mk
+++ makefiles/default-radio-settings.inc.mk
@@ -2,22 +2,23 @@
 # with FBAND=868 changes channel/page setting only if transceiver is at86rf212b
 ifndef CONFIG_KCONFIG_MODULE_IEEE802154
   ifeq (868, $(FBAND))
-    # radio is at86rf212b sub GHz at FBAND=868MHz (channel = 0 / page = 0）
-    ifneq (,$(filter at86rf212b,$(USEMODULE)))     
+    # radio is at86rf212b sub GHz at FBAND=868MHz (channel = 0 / page = 0)
+    ifneq (,$(filter at86rf212b,$(USEMODULE)))
       CFLAGS += -DCONFIG_IEEE802154_DEFAULT_SUBGHZ_CHANNEL=0
-      CFLAGS += -DCONFIG_IEEE802154_DEFAULT_SUBGHZ_PAGE=0
+      CFLAGS += -DCONFIG_IEEE802154_DEFAULT_SUBGHZ_PAGE=2
     # if not using samr30-xpro radio is the same as default IEEE 802.15.4 2.4 GHz
-    else                                        # radio is IEEE 802.15.4 2.4 GHz
-      GHZ_CHANNEL ?= 26                                        
+    else
+    # radio is IEEE 802.15.4 2.4 GHz
+      GHZ_CHANNEL ?= 26
       CFLAGS += -DCONFIG_IEEE802154_DEFAULT_CHANNEL=$(GHZ_CHANNEL)
     endif
   # Applying default setting if FBAND is 915MHz (Channel = 26 for 2.4 GHz, Channel = 5/Page = 2 for sub-GHz)
-  else                                          
+  else
     ifneq (,$(DEFAULT_CHANNEL))
-      ifneq (,$(filter cc110x,$(USEMODULE)))        # radio is cc110x sub-GHz
+      ifneq (,$(filter cc110x,$(USEMODULE)))# radio is cc110x sub-GHz
         CFLAGS += -DCONFIG_CC110X_DEFAULT_CHANNEL=$(DEFAULT_CHANNEL)
       endif
-    endif 
+    endif
   endif
   ifneq (,$(DEFAULT_PAN_ID))
     CFLAGS += -DCONFIG_IEEE802154_DEFAULT_PANID=$(DEFAULT_PAN_ID)
