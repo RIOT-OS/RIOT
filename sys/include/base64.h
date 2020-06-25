f8ac003bbfe11956578dd2189827686c27374d06
Maciej Jurczak <mjurczak@gmail.com>
2020-06-25 22:52:38 +0200

    base64: Fixed decode buffer size estimation
    Fixed required result buffer size underestimation in base64_estimate_decode_size() function.

--- sys/include/base64.h
+++ sys/include/base64.h
@@ -41,7 +41,7 @@ extern "C" {
  */
 static inline size_t base64_estimate_decode_size(size_t base64_in_size)
 {
-    return ((base64_in_size / 4) * 3);
+    return (((base64_in_size + 3) / 4) * 3);
 }
 
 /**
