4ae2c60a8878101ef4fef6439a050bfa3d94054b
chrysn <chrysn@fsfe.org>
2020-06-30 18:02:19 +0200

    coapfileserver: Add support for ETag
    The note on partially cached versions is not applicable yet (for lack of
    block-wise support), but will become relevant.

--- sys/net/application_layer/coapfileserver/coapfileserver.c
+++ sys/net/application_layer/coapfileserver/coapfileserver.c
@@ -24,16 +24,47 @@
 /** Maximum length of an expressible path, including the trailing 0 character. */
 #define COAPFILESERVER_PATH_MAX (64)
 
-static ssize_t coapfileserver_file_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx);
-static ssize_t coapfileserver_directory_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx);
+/** Constant ETag length */
+#define ETAG_LENGTH 8
+
+/** Data extracted from a request on a file */
+struct requestdata {
+    /** 0-terminated expanded file name in the VFS */
+    char namebuf[COAPFILESERVER_PATH_MAX];
+    bool etag_sent;
+    uint8_t etag[ETAG_LENGTH];
+};
+
+/* See stat_etag */
+union stattag {
+    struct stat stat;
+    struct {
+        uint8_t etag[ETAG_LENGTH];
+        /* This will be as many repetitions of ETAG_LENGTH that together
+         * with the original etag field id's larger than the stat */
+        uint8_t etag_padding[(sizeof(struct stat) - 1) / ETAG_LENGTH * ETAG_LENGTH];
+    };
+};
+
+/** Build an ETag based on the given file's VFS stat. If the stat fails,
+ * returns the error and leaves stattag in any state; otherwise there's an etag
+ * in the stattag's field */
+static int stat_etag(char *filename, union stattag *stattag);
+
+/* These are almost but but not quite coap_handler_t -- they require the
+ * request to be already fully read and digested into the last argument. */
+
+static ssize_t coapfileserver_file_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, struct requestdata *request);
+static ssize_t coapfileserver_directory_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, struct requestdata *request);
 /** Create a CoAP response for a given errno (eg. EACCESS -> 4.03 Forbidden
  * etc., defaulting to 5.03 Internal Server Error) */
 static size_t coapfileserver_errno_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, int err);
 
 ssize_t coapfileserver_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx) {
     struct coapfileserver_entry *entry = (struct coapfileserver_entry *)ctx;
-    char namebuf[COAPFILESERVER_PATH_MAX];
-    /** Index in namebuf. Must not point at the last entry as that will be
+    struct requestdata request;
+    request.etag_sent = false;
+    /** Index in request.namebuf. Must not point at the last entry as that will be
      * zeroed to get a 0-terminated string. */
     size_t namelength = 0;
     bool trailing_slash = false;
@@ -43,7 +74,7 @@ ssize_t coapfileserver_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *
     uint8_t errorcode = COAP_CODE_INTERNAL_SERVER_ERROR;
 
     uint8_t strip_remaining = entry->strip_path;
-    namelength += namebuf - strncpy(namebuf, entry->nameprefix, sizeof(namebuf) - 1);
+    namelength += request.namebuf - strncpy(request.namebuf, entry->nameprefix, sizeof(request.namebuf) - 1);
 
     coap_optpos_t opt;
     bool is_first = true;
@@ -80,14 +111,28 @@ ssize_t coapfileserver_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *
                     goto error;
                 }
                 size_t newlength = namelength + 1 + optlen;
-                if (newlength > sizeof(namebuf) - 1) {
+                if (newlength > sizeof(request.namebuf) - 1) {
                     /* Path too long, therefore can't exist in this mapping */
                     errorcode = COAP_CODE_PATH_NOT_FOUND;
                     goto error;
                 }
-                namebuf[namelength] = '/';
-                memcpy(&namebuf[namelength] + 1, value, optlen);
+                request.namebuf[namelength] = '/';
+                memcpy(&request.namebuf[namelength] + 1, value, optlen);
                 namelength = newlength;
+                break;
+            case COAP_OPT_ETAG:
+                if (optlen != sizeof(request.etag)) {
+                    /* Can't be a matching tag, no use in carrying that */
+                    continue;
+                }
+                if (request.etag_sent) {
+                    /* We can reasonably only check for a limited sized set,
+                     * and it size is 1 here (sending multiple ETags is
+                     * possible but rare) */
+                    continue;
+                }
+                request.etag_sent = true;
+                memcpy(request.etag, value, sizeof(request.etag));
                 break;
             default:
                 if (opt.opt_num & 1) {
@@ -99,39 +144,50 @@ ssize_t coapfileserver_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *
         }
     }
 
-    namebuf[namelength] = '\0';
+    request.namebuf[namelength] = '\0';
     bool is_directory = trailing_slash | !any_component;
 
     /* Note to self: As we parse more options than just Uri-Path, we'll likely
      * pass a struct pointer later. So far, those could even be hooked into the
      * resource list, but that'll go away once we parse more options */
     if (is_directory) {
-        return coapfileserver_directory_handler(pdu, buf, len, namebuf);
+        return coapfileserver_directory_handler(pdu, buf, len, &request);
     } else {
-        return coapfileserver_file_handler(pdu, buf, len, namebuf);
+        return coapfileserver_file_handler(pdu, buf, len, &request);
     }
 
 error:
     return gcoap_response(pdu, buf, len, errorcode);
 }
 
-static ssize_t coapfileserver_file_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx) {
+static ssize_t coapfileserver_file_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, struct requestdata *request)
+{
     /**
      * ToDo:
      *
      * * Blockwise
      *
-     * * ETag
-     *
      * * Error handling on late read errors
      */
-    const char *name = (char *)ctx;
+    int err;
 
-    int fd = vfs_open(name, O_RDONLY, 0);
+    union stattag stattag;
+    err = stat_etag(request->namebuf, &stattag);
+    if (err < 0)
+        return coapfileserver_errno_handler(pdu, buf, len, err);
+
+    if (request->etag_sent && memcmp(stattag.etag, request->etag, ETAG_LENGTH) == 0) {
+        gcoap_resp_init(pdu, buf, len, COAP_CODE_VALID);
+        coap_opt_add_opaque(pdu, COAP_OPT_ETAG, stattag.etag, ETAG_LENGTH);
+        return coap_opt_finish(pdu, COAP_OPT_FINISH_NONE);
+    }
+
+    int fd = vfs_open(request->namebuf, O_RDONLY, 0);
     if (fd < 0)
         return coapfileserver_errno_handler(pdu, buf, len, fd);
 
     gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    coap_opt_add_opaque(pdu, COAP_OPT_ETAG, stattag.etag, ETAG_LENGTH);
     size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
 
     int read = vfs_read(fd, pdu->payload, pdu->payload_len);
@@ -147,7 +203,8 @@ static ssize_t coapfileserver_file_handler(coap_pkt_t *pdu, uint8_t *buf, size_t
     return resp_len + read;
 }
 
-static ssize_t coapfileserver_directory_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx) {
+static ssize_t coapfileserver_directory_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, struct requestdata *request)
+{
     /**
      * ToDo:
      *
@@ -159,10 +216,9 @@ static ssize_t coapfileserver_directory_handler(coap_pkt_t *pdu, uint8_t *buf, s
      *
      * * Directories don't have their trailing slashes yet
      */
-    const char *name = (char *)ctx;
     vfs_DIR dir;
 
-    int err = vfs_opendir(&dir, name);
+    int err = vfs_opendir(&dir, request->namebuf);
     if (err != 0) {
         return coapfileserver_errno_handler(pdu, buf, len, err);
     }
@@ -214,4 +270,27 @@ static size_t coapfileserver_errno_handler(coap_pkt_t *pdu, uint8_t *buf, size_t
     };
     DEBUG("coapfileserver: Rejecting error %d (%s) as %d.%02d\n", err, strerror(err), code >> 5, code & 0x1f);
     return gcoap_response(pdu, buf, len, code);
+}
+
+static int stat_etag(char *filename, union stattag *stattag)
+{
+    int err;
+
+    memset(stattag, 0, sizeof(union stattag));
+    err = vfs_stat(filename, &stattag->stat);
+    if (err < 0)
+        return err;
+
+    /* Normalizing fields whose value can change without affecting the ETag */
+    stattag->stat.st_nlink = 0;
+    memset(&stattag->stat.st_atim, 0, sizeof(stattag->stat.st_atim));
+
+    /* Build a compact ETag by XORing the stat onto itself */
+    for (unsigned int i = 0; i < sizeof(stattag->etag_padding) / sizeof(stattag->etag); ++i) {
+        for (int j = 0; j < ETAG_LENGTH; ++j) {
+            stattag->etag[j] ^= stattag->etag_padding[i * ETAG_LENGTH + j];
+        }
+    }
+
+    return 0;
 }
