900e4b61dcf43bacb694acbd7e31bd774899f655
nagrawal <nagrawal@ibr.cs.tu-bs.de>
2020-07-06 16:04:44 +0200

    driver/scd30: Add driver for Sensirion SCD30
    Created tests for Sensirion scd30 driver
    
    Moved Makefile.dep and Makefile.include as per new spec

--- drivers/saul/init_devs/init.c
+++ drivers/saul/init_devs/init.c
@@ -215,6 +215,10 @@ void saul_init_devs(void)
         extern void auto_init_qmc5883l(void);
         auto_init_qmc5883l();
     }
+    if (IS_USED(MODULE_SCD30)) {
+        extern void auto_init_scd30(void);
+        auto_init_scd30();
+    }
     if (IS_USED(MODULE_SHT2X)) {
         extern void auto_init_sht2x(void);
         auto_init_sht2x();
