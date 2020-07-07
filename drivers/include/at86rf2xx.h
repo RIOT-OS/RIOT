53eedc283bd49320fecf4d119c6a5067a38c7987
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-07-07 20:15:56 +0200

    drivers/at86rf2xx: register driver with netdev

--- drivers/include/at86rf2xx.h
+++ drivers/include/at86rf2xx.h
@@ -294,8 +294,10 @@ typedef struct {
  *
  * @param[out] dev          device descriptor
  * @param[in]  params       parameters for device initialization
+ * @param[in]  index        index of @p params in a global parameter struct array.
+ *                          If initialized manually, pass a unique identifier instead.
  */
-void at86rf2xx_setup(at86rf2xx_t *dev, const at86rf2xx_params_t *params);
+void at86rf2xx_setup(at86rf2xx_t *dev, const at86rf2xx_params_t *params, uint8_t index);
 
 /**
  * @brief   Trigger a hardware reset and configure radio with default values
