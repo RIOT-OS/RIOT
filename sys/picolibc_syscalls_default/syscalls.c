a75cd2ccb3908586ecd36e38723de2b88bad5cbe
Keith Packard <keithp@keithp.com>
2020-07-08 10:37:06 -0700

    picolibc: Add VFS syscall wrappers
    This provides the POSIX api used by picolibc stdio
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- sys/picolibc_syscalls_default/syscalls.c
+++ sys/picolibc_syscalls_default/syscalls.c
@@ -85,16 +85,6 @@ FILE *const __iob[] = {
     &picolibc_stdio,    /* stderr */
 };
 
-/*
- * All output is directed to stdio_uart, independent of the given file descriptor.
- * The write call will further block until the byte is actually written to the UART.
- */
-_READ_WRITE_RETURN_TYPE write(int fd, const void *data, size_t count)
-{
-    (void) fd;
-    return stdio_write(data, count);
-}
-
 #include <thread.h>
 /**
  * @brief Get the process-ID of the current thread
@@ -106,11 +96,100 @@ pid_t getpid(void)
     return thread_getpid();
 }
 
+#if MODULE_VFS
+#include "vfs.h"
+
+/**
+ * @brief Open a file
+ *
+ * This is a wrapper around @c vfs_open
+ *
+ * @param name  file name to open
+ * @param flags flags, see man 3p open
+ * @param mode  mode, file creation mode if the file is created when opening
+ *
+ * @return      fd number (>= 0) on success
+ * @return      -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+int open(const char *name, int flags, int mode)
+{
+    int fd = vfs_open(name, flags, mode);
+    if (fd < 0) {
+        /* vfs returns negative error codes */
+        errno = -fd;
+        return -1;
+    }
+    return fd;
+}
+
+/**
+ * @brief Read bytes from an open file
+ *
+ * This is a wrapper around @c vfs_read
+ *
+ * @param[in]  fd     open file descriptor obtained from @c open()
+ * @param[out] dest   destination buffer
+ * @param[in]  count  maximum number of bytes to read
+ *
+ * @return       number of bytes read on success
+ * @return       -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+_READ_WRITE_RETURN_TYPE read(int fd, void *dest, size_t count)
+{
+    int res = vfs_read(fd, dest, count);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return res;
+}
+
+/**
+ * @brief Write bytes to an open file
+ *
+ * This is a wrapper around @c vfs_write
+ *
+ * @param[in]  fd     open file descriptor obtained from @c open()
+ * @param[in]  src    source data buffer
+ * @param[in]  count  maximum number of bytes to write
+ *
+ * @return       number of bytes written on success
+ * @return       -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+_READ_WRITE_RETURN_TYPE write(int fd, const void *src, size_t count)
+{
+    int res = vfs_write(fd, src, count);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return res;
+}
+
+/**
+ * @brief Close an open file
+ *
+ * This is a wrapper around @c vfs_close
+ *
+ * If this call returns an error, the fd should still be considered invalid and
+ * no further attempt to use it shall be made, not even to retry @c close()
+ *
+ * @param[in]  fd     open file descriptor obtained from @c open()
+ *
+ * @return       0 on success
+ * @return       -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
 int close(int fd)
 {
-    (void) fd;
-    errno = ENODEV;
-    return -1;
+    int res = vfs_close(fd);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return res;
 }
 
 /**
@@ -127,3 +206,121 @@ clock_t times(struct tms *ptms)
 
     return (-1);
 }
+
+/**
+ * @brief Query or set options on an open file
+ *
+ * This is a wrapper around @c vfs_fcntl
+ *
+ * @param[in]  fd     open file descriptor obtained from @c open()
+ * @param[in]  cmd    fcntl command, see man 3p fcntl
+ * @param[in]  arg    argument to fcntl command, see man 3p fcntl
+ *
+ * @return       0 on success
+ * @return       -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+int fcntl (int fd, int cmd, int arg)
+{
+    int res = vfs_fcntl(fd, cmd, arg);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return res;
+}
+
+/**
+ * @brief Seek to position in file
+ *
+ * This is a wrapper around @c vfs_lseek
+ *
+ * @p whence determines the function of the seek and should be set to one of
+ * the following values:
+ *
+ *  - @c SEEK_SET: Seek to absolute offset @p off
+ *  - @c SEEK_CUR: Seek to current location + @p off
+ *  - @c SEEK_END: Seek to end of file + @p off
+ *
+ * @param[in]  fd       open file descriptor obtained from @c open()
+ * @param[in]  off      seek offset
+ * @param[in]  whence   determines the seek method, see detailed description
+ *
+ * @return the new seek location in the file on success
+ * @return -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+off_t lseek(int fd, _off_t off, int whence)
+{
+    int res = vfs_lseek(fd, off, whence);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return res;
+}
+
+/**
+ * @brief Get status of an open file
+ *
+ * This is a wrapper around @c vfs_fstat
+ *
+ * @param[in]  fd       open file descriptor obtained from @c open()
+ * @param[out] buf      pointer to stat struct to fill
+ *
+ * @return 0 on success
+ * @return -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+int fstat(int fd, struct stat *buf)
+{
+    int res = vfs_fstat(fd, buf);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Status of a file (by name)
+ *
+ * This is a wrapper around @c vfs_fstat
+ *
+ * @param[in]  name     path to file
+ * @param[out] buf      pointer to stat struct to fill
+ *
+ * @return 0 on success
+ * @return -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+int stat(const char *name, struct stat *st)
+{
+    int res = vfs_stat(name, st);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return 0;
+}
+
+/**
+ * @brief  Unlink (delete) a file
+ *
+ * @param[in]  path     path to file to be deleted
+ *
+ * @return 0 on success
+ * @return -1 on error, @c errno set to a constant from errno.h to indicate the error
+ */
+int unlink(const char *path)
+{
+    int res = vfs_unlink(path);
+    if (res < 0) {
+        /* vfs returns negative error codes */
+        errno = -res;
+        return -1;
+    }
+    return 0;
+}
+
+#endif /* MODULE_VFS */
