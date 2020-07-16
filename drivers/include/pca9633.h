9d068d582abe1637d1c24340f512ad767e2479be
Hendrik van Essen <hendrik.ve@fu-berlin.de>
2020-07-16 14:44:51 +0200

    drivers/pca9633: make enum anonymous

--- drivers/include/pca9633.h
+++ drivers/include/pca9633.h
@@ -68,10 +68,10 @@ typedef struct {
 /**
  * @brief   PCA9633 driver error codes
  */
-typedef enum {
+enum {
     PCA9633_OK              = 0,    /**< Success */
     PCA9633_ERROR_I2C       = 1,    /**< I2C communication error */
-} pca9685_error_t;
+};
 
 /**
  * @brief   PCA9633 PWM channel definitions
