7bf65d9c741f6ff2fe2ae21f18cb5126f6fad760
denis-l <0>
2020-07-21 09:41:13 +0500

    modbus-rtu:  use debug instead printf. add macro for turn on/off use slave, master

--- examples/modbus/main.c
+++ examples/modbus/main.c
@@ -7,14 +7,19 @@
 #include "thread.h"
 #include "xtimer.h"
 #include "msg.h"
+#define ENABLE_DEBUG (1)
+#include "debug.h"
+
+
+#define USE_SLAVE
+#define USE_MASTER
 
 #define SLAVE_ID 1
 #define BAUDRATE 115200
 
-#define SLAVE_UART 2
+#ifdef USE_MASTER
 #define MASTER_UART 0
-
-static char stack_master[400];
+static char stack_master[600];
 static kernel_pid_t pid_master;
 static modbus_rtu_t master;
 static uint16_t regs_master1[16];
@@ -26,8 +31,11 @@ static modbus_rtu_message_t message_master = {
     .data_size = sizeof(regs_master1),
     .count = 16
 };
+#endif /* USE_MASTER */
 
-static char stack_slave[400];
+#ifdef USE_SLAVE
+#define SLAVE_UART 2
+static char stack_slave[550];
 static kernel_pid_t pid_slave;
 static modbus_rtu_t slave;
 static uint16_t regs_slave[16];
@@ -35,17 +43,20 @@ static modbus_rtu_message_t message_slave = {
     .data = regs_slave,
     .data_size = sizeof(regs_slave)
 };
+#endif  /* USE_SLAVE */
 
+#ifdef USE_MASTER
 static void init_master(void)
 {
     master.uart = UART_DEV(MASTER_UART);
     master.timeout = 1000000;
     master.id = 0;
-    master.pin_rts = GPIO_PIN(PORT_A, 1);
+    master.pin_rts = GPIO_UNDEF;
+    /* master.pin_rts = GPIO_PIN(PORT_A, 1); */
     master.pin_rts_enable = 0;
 
     if (modbus_rtu_init(&master, BAUDRATE)) {
-        puts("fail UART init");
+        DEBUG("fail UART init\n");
     }
 }
 
@@ -54,7 +65,7 @@ static void *thread_master(void *arg __attribute__((unused)))
     int res __attribute__((unused));
 
     init_master();
-    // return NULL;
+    /* return NULL; */
     while (1) {
         xtimer_usleep(master.rx_timeout * 3);
         srand(xtimer_now_usec());
@@ -62,119 +73,135 @@ static void *thread_master(void *arg __attribute__((unused)))
             regs_master1[i] = rand();
         }
 
-        puts("try MB_FC_WRITE_REGISTERS");
+        DEBUG("try MB_FC_WRITE_REGISTERS\n");
         message_master.data = regs_master1;
         message_master.func = MB_FC_WRITE_REGISTERS;
         res = modbus_rtu_send_request(&master, &message_master);
         if (res) {
-            printf("fail MB_FC_WRITE_REGISTERS %d\n", res);
+            DEBUG("fail MB_FC_WRITE_REGISTERS %d\n", res);
         }
         else {
             xtimer_usleep(master.rx_timeout * 3);
-            puts("try MB_FC_READ_REGISTERS");
+            DEBUG("try MB_FC_READ_REGISTERS\n");
             message_master.data = regs_master2;
             message_master.func = MB_FC_READ_REGISTERS;
             res = modbus_rtu_send_request(&master, &message_master);
             if (res ||
                 memcmp(regs_master1, regs_master2,
                        message_master.count * 2) != 0) {
-                printf("fail MB_FC_READ_REGISTERS %d\n", res);
+                DEBUG("fail MB_FC_READ_REGISTERS %d\n", res);
             }
             else {}
         }
 
-        puts("try MB_FC_READ_DISCRETE_INPUT");
+        DEBUG("try MB_FC_READ_DISCRETE_INPUT\n");
         message_master.data = regs_master1;
         message_master.func = MB_FC_READ_DISCRETE_INPUT;
         res = modbus_rtu_send_request(&master, &message_master);
+        #ifdef USE_SLAVE
         if (res ||
             memcmp(regs_master1, regs_slave, sizeof(regs_master1)) != 0) {
-            printf("fail MB_FC_READ_DISCRETE_INPUT %d\n", res);
+            DEBUG("fail MB_FC_READ_DISCRETE_INPUT %d\n", res);
         }
+        #else /* USE_SLAVE */
+        if (res) {
+            DEBUG("fail MB_FC_READ_DISCRETE_INPUT %d\n", res);
+        }
+        #endif /* USE_SLAVE */
         else {}
 
-        puts("try MB_FC_READ_INPUT_REGISTER");
+        DEBUG("try MB_FC_READ_INPUT_REGISTER\n");
         message_master.data = regs_master1;
         message_master.func = MB_FC_READ_INPUT_REGISTER;
         res = modbus_rtu_send_request(&master, &message_master);
+        #ifdef USE_SLAVE
         if (res || message_master.data[0] != regs_slave[0]) {
-            printf("fail MB_FC_READ_INPUT_REGISTER %d\n", res);
+            DEBUG("fail MB_FC_READ_INPUT_REGISTER %d\n", res);
         }
+        #else /* USE_SLAVE */
+        if (res) {
+            DEBUG("fail MB_FC_READ_INPUT_REGISTER %d\n", res);
+        }
+        #endif /* USE_SLAVE */
         else {}
 
-        puts("try MB_FC_WRITE_COILS");
+        DEBUG("try MB_FC_WRITE_COILS\n");
         message_master.data = regs_master1;
         message_master.func = MB_FC_WRITE_COILS;
         res = modbus_rtu_send_request(&master, &message_master);
         if (res) {
-            printf("fail MB_FC_WRITE_COILS %d\n", res);
+            DEBUG("fail MB_FC_WRITE_COILS %d\n", res);
         }
         else {
             xtimer_usleep(master.rx_timeout * 3);
-            puts("try MB_FC_READ_COILS");
+            DEBUG("try MB_FC_READ_COILS\n");
             message_master.data = regs_master2;
             message_master.func = MB_FC_READ_COILS;
             res = modbus_rtu_send_request(&master, &message_master);
             if (res ||
                 memcmp(regs_master1, regs_master2,
                        message_master.count / 8) != 0) {
-                printf("out: %x in: %x\n", regs_master1[0], regs_master2[0]);
-                printf("fail MB_FC_READ_COILS %d\n", res);
+                DEBUG("out: %x in: %x\n", regs_master1[0], regs_master2[0]);
+                DEBUG("fail MB_FC_READ_COILS %d\n", res);
             }
             else {}
         }
 
-        puts("try MB_FC_WRITE_COIL");
+        DEBUG("try MB_FC_WRITE_COIL\n");
         message_master.data = regs_master1;
         regs_master1[0] = 1;
         message_master.func = MB_FC_WRITE_COIL;
         res = modbus_rtu_send_request(&master, &message_master);
         if (res) {
-            printf("fail MB_FC_WRITE_COIL %d\n", res);
+            DEBUG("fail MB_FC_WRITE_COIL %d\n", res);
         }
         else {
             xtimer_usleep(master.rx_timeout * 3);
-            puts("try MB_FC_READ_COILS");
+            DEBUG("try MB_FC_READ_COILS\n");
             message_master.data = regs_master2;
             message_master.func = MB_FC_READ_COILS;
             res = modbus_rtu_send_request(&master, &message_master);
             if (res || (!!regs_master1[0]) != (!!regs_master2[0] & 1)) {
-                printf("fail MB_FC_READ_COILS_2 %d\n", res);
+                DEBUG("fail MB_FC_READ_COILS_2 %d\n", res);
             }
             else {}
         }
 
-        puts("try MB_FC_WRITE_REGISTER");
+        DEBUG("try MB_FC_WRITE_REGISTER\n");
         message_master.data = regs_master1;
         message_master.func = MB_FC_WRITE_REGISTER;
         res = modbus_rtu_send_request(&master, &message_master);
         if (res) {
-            printf("fail MB_FC_WRITE_REGISTER %d\n", res);
+            DEBUG("fail MB_FC_WRITE_REGISTER %d\n", res);
         }
         else {
             xtimer_usleep(master.rx_timeout * 3);
-            puts("try MB_FC_READ_INPUT_REGISTER");
+            DEBUG("try MB_FC_READ_INPUT_REGISTER\n");
             message_master.data = regs_master2;
             message_master.func = MB_FC_READ_REGISTERS;
             res = modbus_rtu_send_request(&master, &message_master);
             if (res || regs_master1[0] != regs_master2[0]) {
-                printf("fail MB_FC_READ_REGISTERS_2 %d\n", res);
+                DEBUG("fail MB_FC_READ_REGISTERS_2 %d\n", res);
             }
             else {}
         }
     }
     return NULL;
 }
+#endif /* USE_MASTER */
 
+
+#ifdef USE_SLAVE
 static void init_slave(void)
 {
     slave.uart = UART_DEV(SLAVE_UART);
     slave.id = SLAVE_ID;
-    slave.pin_rts = GPIO_PIN(PORT_B, 15);
+    slave.pin_rts = GPIO_UNDEF;
+    /* slave.pin_rts = GPIO_PIN(PORT_B, 15); */
     slave.pin_rts_enable = 1;
 
     if (modbus_rtu_init(&slave, BAUDRATE)) {
-        puts("fail UART init");
+        DEBUG("fail UART init\n");
     }
 }
 
@@ -185,19 +212,19 @@ static void *thread_slave(void *arg __attribute__((unused)))
     init_slave();
     while (1) {
         res = modbus_rtu_poll(&slave, &message_slave);
-        // printf("func: %u; addr: %u; count: %u\n", message_slave.func, message_slave.addr, message_slave.count);
+        /* DEBUG("func: %u; addr: %u; count: %u\n", message_slave.func, message_slave.addr, message_slave.count); */
         if (res) {
-            printf("fail poll %d\n", res);
+            DEBUG("fail poll %d\n", res);
         }
         else {
-            // assert(message_slave.id == SLAVE_ID);
-            // assert(message_slave.func == message_master.func);
-            // assert(message_slave.addr == message_master.addr);
-            // assert(message_slave.count == message_master.count);
-            // assert(message_slave.data == regs_slave);
+            /* assert(message_slave.id == SLAVE_ID); */
+            /* assert(message_slave.func == message_master.func); */
+            /* assert(message_slave.addr == message_master.addr); */
+            /* assert(message_slave.count == message_master.count); */
+            /* assert(message_slave.data == regs_slave); */
             res = modbus_rtu_send_response(&slave, &message_slave);
             if (res) {
-                printf("fail response %d\n", res);
+                DEBUG("fail response %d\n", res);
             }
             else {}
         }
@@ -205,15 +232,20 @@ static void *thread_slave(void *arg __attribute__((unused)))
 
     return NULL;
 }
+#endif /* USE_SLAVE */
 
 int main(void)
 {
+#ifdef USE_SLAVE
     pid_slave = thread_create(stack_slave, sizeof(stack_slave),
                               THREAD_PRIORITY_MAIN - 1, 0, thread_slave, NULL,
                               NULL);
+#endif /* USE_SLAVE */
+
+#ifdef USE_MASTER
     pid_master = thread_create(stack_master, sizeof(stack_master),
                                THREAD_PRIORITY_MAIN - 1, 0, thread_master, NULL,
                                NULL);
-
+#endif /* USE_MASTER */
     return 0;
 }
