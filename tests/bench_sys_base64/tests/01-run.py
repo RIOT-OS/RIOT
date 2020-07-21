3420fcf4367b5cee9f5d3de2bf698d33b115fc6a
Kaspar Schleiser <kaspar@schleiser.de>
2020-07-21 13:27:32 +0200

    tests/bench_sys_base64: reduce test iterations (10000 -> 1000)

--- tests/bench_sys_base64/tests/01-run.py
+++ tests/bench_sys_base64/tests/01-run.py
@@ -13,8 +13,8 @@ from testrunner import run
 def testfunc(child):
     child.expect_exact("Verifying that base64 encoding works for benchmark input: OK\r\n")
     child.expect_exact("Verifying that base64 decoding works for benchmark input: OK\r\n")
-    child.expect(r"Encoding 10\.000 x 96 bytes \(128 bytes in base64\): [0-9]+ µs\r\n")
-    child.expect(r"Decoding 10\.000 x 96 bytes \(128 bytes in base64\): [0-9]+ µs\r\n")
+    child.expect(r"Encoding 1\.000 x 96 bytes \(128 bytes in base64\): [0-9]+ µs\r\n")
+    child.expect(r"Decoding 1\.000 x 96 bytes \(128 bytes in base64\): [0-9]+ µs\r\n")
 
 
 if __name__ == "__main__":
