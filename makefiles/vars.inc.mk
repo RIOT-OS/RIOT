180ee6efc94fe94bf5ac8bf5d29f287ceb46643b
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2020-07-27 18:16:11 +0200

    makefiles: introduce KCONFIG_ADD_CONFIG variable
    This holds a list of .config files to be merged. For more details see
    kconfig.mk.

--- makefiles/vars.inc.mk
+++ makefiles/vars.inc.mk
@@ -118,3 +118,4 @@ export LAZYSPONGE_FLAGS      # Parameters supplied to LAZYSPONGE.
 export AFL_FLAGS             # Additional command-line flags passed to afl during fuzzing.
 
 # LOG_LEVEL                  # Logging level as integer (NONE: 0, ERROR: 1, WARNING: 2, INFO: 3, DEBUG: 4, default: 3)
+# KCONFIG_ADD_CONFIG         # List of .config files to be merged used by Boards and CPUs. See kconfig.mk
