5a00e2e36c42fe456c447fad0ecb4b19eab6dde4
Kaspar Schleiser <kaspar@schleiser.de>
2020-07-29 12:00:30 +0200

    cpu/msp430_common: use __stack for ISR stacks

--- cpu/msp430_common/include/cpu.h
+++ cpu/msp430_common/include/cpu.h
@@ -50,11 +50,6 @@ extern "C" {
 extern volatile int __irq_is_in;
 
 /**
- * @brief   Memory used as stack for the interrupt context
- */
-extern char __isr_stack[ISR_STACKSIZE];
-
-/**
  * @brief   Save the current thread context from inside an ISR
  */
 static inline void __attribute__((always_inline)) __save_context(void)
@@ -102,8 +97,9 @@ static inline void __attribute__((always_inline)) __restore_context(void)
  */
 static inline void __attribute__((always_inline)) __enter_isr(void)
 {
+    extern char __stack;    /* defined by linker script to end of RAM */
     __save_context();
-    __asm__("mov.w %0,r1" : : "i"(__isr_stack + ISR_STACKSIZE));
+    __asm__("mov.w %0,r1" : : "i"(&__stack));
     __irq_is_in = 1;
 }
 
