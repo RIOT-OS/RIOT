8271741267ae65064a9bdff3f71a71c888b5b509
Keith Packard <keithp@keithp.com>
2020-07-30 16:26:34 -0700

    picolibc: Set PICOLIBC_FLOAT_PRINTF=1 to enable float printf/scanf code
    This makes RIOT use the integer-only printf/scanf code by default and
    includes a new make parameter to select the full floating point
    version. This saves about 6kB of text space when building hello-world
    for the microbit board.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- makefiles/libc/picolibc.mk
+++ makefiles/libc/picolibc.mk
@@ -16,6 +16,10 @@ ifeq (1,$(USE_PICOLIBC))
   LINKFLAGS += -Wl,--defsym=__heap_end=_eheap
   LINKFLAGS += -Wl,--defsym=__heap_start=_sheap
   CFLAGS += -specs=picolibc.specs
+  ifneq (1,$(PICOLIBC_FLOAT_PRINTF))
+   CFLAGS += -DPICOLIBC_INTEGER_PRINTF_SCANF
+   LINKFLAGS += -DPICOLIBC_INTEGER_PRINTF_SCANF
+  endif
 endif
 
 LINKFLAGS += -lc
