c1f0a12d3087e66274a15f1000760e793d5726c8
Oppila-ms <manikandan@oppila.in>
2020-08-03 20:23:47 +0530

    rtt added

--- boards/omote/board.c
+++ boards/omote/board.c
@@ -26,7 +26,6 @@ void board_init(void)
     leds_init();
     /* initialize the CPU */
     cpu_init();
-    
     /* The boot pin must be set to input otherwise it may lock the bootloader */
     gpio_init(BOOT_PIN, GPIO_IN);
 
