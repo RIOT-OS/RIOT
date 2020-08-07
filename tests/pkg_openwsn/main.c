afeeb5b56511c63419863e091d3d2896e557e976
Francisco Molina <femolina@uc.cl>
2020-08-07 16:03:10 +0200

    tests/pkg_openwsn: adapt to sock api

--- tests/pkg_openwsn/main.c
+++ tests/pkg_openwsn/main.c
@@ -21,41 +21,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "od.h"
 #include "shell.h"
 
-#include "opendefs.h"
-#include "04-TRAN/udp.h"
-#include "cross-layers/openqueue.h"
-
-udp_resource_desc_t uinject_vars;
-
-void uinject_sendDone(OpenQueueEntry_t *msg, owerror_t error)
-{
-    (void)error;
-
-    printf("msg.l2_sendDoneError: %x\n", msg->l2_sendDoneError);
-    openqueue_freePacketBuffer(msg);
-    puts("Send success");
-}
-
-void uinject_receive(OpenQueueEntry_t *pkt)
-{
-    printf("Received %i bytes on port %i\n", (int)pkt->length,
-           pkt->l4_destination_port);
-    od_hex_dump(pkt->payload, pkt->length, OD_WIDTH_DEFAULT);
-    openqueue_freePacketBuffer(pkt);
-}
-
-void uinject_init(void)
-{
-    uinject_vars.port = WKP_UDP_INJECT;
-    uinject_vars.callbackReceive = &uinject_receive;
-    uinject_vars.callbackSendDone = &uinject_sendDone;
-    openudp_register(&uinject_vars);
-}
-
 extern int udp_cmd(int argc, char **argv);
+extern void udp_cli_init(void);
 
 static const shell_command_t shell_commands[] = {
     { "udp", "Send data over UDP and listen on UDP ports", udp_cmd },
@@ -69,7 +38,7 @@ int main(void)
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
-    uinject_init();
+    udp_cli_init();
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
