75dbad847c91e6b10fac90d18135b3fada968986
Francisco Molina <femolina@uc.cl>
2020-08-07 11:39:06 +0200

    makefiles/tools/cc2538-bsl.inc.mk: specify BAUD for reset

--- makefiles/tools/cc2538-bsl.inc.mk
+++ makefiles/tools/cc2538-bsl.inc.mk
@@ -4,7 +4,7 @@ FFLAGS_OPTS ?=
 PROG_BAUD ?= 500000	# default value in cc2538-bsl
 FFLAGS  = -p "$(PROG_DEV)" $(FFLAGS_OPTS) -e -w -v -b $(PROG_BAUD) $(FLASHFILE)
 
-RESET ?= $(FLASHER) -p "$(PROG_DEV)" $(FFLAGS_OPTS)
+RESET ?= $(FLASHER) -p "$(PROG_DEV)" $(FFLAGS_OPTS) -b $(PROG_BAUD)
 
 ifeq ($(RIOTTOOLS)/cc2538-bsl/cc2538-bsl.py,$(FLASHER))
   FLASHDEPS += $(FLASHER)
