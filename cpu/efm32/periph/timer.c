cbf78fe3d416bfebc1a6c0c664408b46dcb2bf00
Francisco Molina <femolina@uc.cl>
2020-08-18 15:41:04 +0200

    cpu/efm32: allow running both LETIMER and regular timer

--- cpu/efm32/periph/timer.c
+++ cpu/efm32/periph/timer.c
@@ -165,7 +165,7 @@ int timer_set_absolute(tim_t dev, int channel, unsigned int value)
     if (!_is_letimer(dev)) {
         TIMER_TypeDef *tim = timer_config[dev].timer.dev;
 
-        if (channel < 0 || channel >= TIMER_CHANNEL_NUMOF) {
+        if (channel < 0 || channel >= timer_config[dev].channel_numof) {
             return -1;
         }
 
@@ -278,15 +278,12 @@ void timer_start(tim_t dev)
     }
 }
 
-#ifdef TIMER_0_ISR
-void TIMER_0_ISR(void)
+static void _timer_isr(tim_t dev)
 {
-    tim_t dev = 0;
-
     if (_is_letimer(dev)) {
         LETIMER_TypeDef *tim = timer_config[dev].timer.dev;
 
-        for (int i = 0; i < TIMER_CHANNEL_NUMOF; i++) {
+        for (int i = 0; i < timer_config[dev].channel_numof; i++) {
             if (tim->IF & (LETIMER_IF_COMP0 << i))
             {
                 LETIMER_IntDisable(tim, LETIMER_IEN_COMP0 << i);
@@ -298,7 +295,7 @@ void TIMER_0_ISR(void)
     else {
         TIMER_TypeDef *tim = timer_config[dev].timer.dev;
 
-        for (int i = 0; i < TIMER_CHANNEL_NUMOF; i++) {
+        for (int i = 0; i < timer_config[dev].channel_numof; i++) {
             if (tim->IF & (TIMER_IF_CC0 << i)) {
                 tim->CC[i].CTRL = _TIMER_CC_CTRL_MODE_OFF;
                 tim->IFC = (TIMER_IFC_CC0 << i);
@@ -308,4 +305,17 @@ void TIMER_0_ISR(void)
     }
     cortexm_isr_end();
 }
+
+#ifdef TIMER_0_ISR
+void TIMER_0_ISR(void)
+{
+    _timer_isr(0);
+}
 #endif /* TIMER_0_ISR */
+
+#ifdef TIMER_1_ISR
+void TIMER_1_ISR(void)
+{
+    _timer_isr(1);
+}
+#endif /* TIMER_1_ISR */
