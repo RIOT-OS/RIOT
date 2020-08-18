0d60b3370a444ada5b57253dcd25046a65cf6bbc
Francisco Molina <femolina@uc.cl>
2020-08-18 13:41:23 +0200

    cpu/stm32/f1/rtt: some fixes to rtt_set_alarm
    - disable alarm before setting a new one
    - save cb and argument context before enabling the ISR

--- cpu/stm32/periph/rtt_f1.c
+++ cpu/stm32/periph/rtt_f1.c
@@ -123,6 +123,12 @@ void rtt_set_alarm(uint32_t alarm, rtt_cb_t cb, void *arg)
 {
     _rtt_enter_config_mode();
 
+    /* Disable alarm*/
+    RTT_DEV->CRH &= ~RTC_CRH_ALRIE;
+    /* Save new cb and argument */
+    alarm_cb = cb;
+    alarm_arg = arg;
+
     /* Set the alarm MSB word */
     RTT_DEV->ALRH = alarm >> 16;
     /* Set the alarm LSB word */
@@ -132,9 +138,6 @@ void rtt_set_alarm(uint32_t alarm, rtt_cb_t cb, void *arg)
     RTT_DEV->CRH |= RTC_CRH_ALRIE;
 
     _rtt_leave_config_mode();
-
-    alarm_cb = cb;
-    alarm_arg = arg;
 }
 
 void rtt_clear_alarm(void)
