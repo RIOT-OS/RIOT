228a71f96e897270287d225620b6dd97c02ed5d2
hugues <hlarrive@laas.fr>
2020-08-27 23:27:42 +0200

    boards/common/blxxxpill/include/periph_conf.h: reorder adc_config[]

--- boards/common/blxxxpill/include/periph_conf.h
+++ boards/common/blxxxpill/include/periph_conf.h
@@ -73,18 +73,18 @@ extern "C" {
  * @{
  */
 static const adc_conf_t adc_config[] = {
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 0), .chan = 0 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 1), .chan = 1 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 4), .chan = 4 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 5), .chan = 5 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 6), .chan = 6 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_A, 7), .chan = 7 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_B, 0), .chan = 8 },
-    { .dev = 0, .pin = GPIO_PIN(PORT_B, 1), .chan = 9 },
+    { .pin = GPIO_PIN(PORT_A, 0), .dev = 0, .chan = 0 },
+    { .pin = GPIO_PIN(PORT_A, 1), .dev = 0, .chan = 1 },
+    { .pin = GPIO_PIN(PORT_A, 4), .dev = 0, .chan = 4 },
+    { .pin = GPIO_PIN(PORT_A, 5), .dev = 0, .chan = 5 },
+    { .pin = GPIO_PIN(PORT_A, 6), .dev = 0, .chan = 6 },
+    { .pin = GPIO_PIN(PORT_A, 7), .dev = 0, .chan = 7 },
+    { .pin = GPIO_PIN(PORT_B, 0), .dev = 0, .chan = 8 },
+    { .pin = GPIO_PIN(PORT_B, 1), .dev = 0, .chan = 9 },
     /* ADC Temperature channel */
-    { .dev = 0, .pin = GPIO_UNDEF, .chan = 16 },
+    { .pin = GPIO_UNDEF, .dev = 0, .chan = 16 },
     /* ADC VREF channel */
-    { .dev = 0, .pin = GPIO_UNDEF, .chan = 17 },
+    { .pin = GPIO_UNDEF, .dev = 0, .chan = 17 },
 };
 
 #define ADC_NUMOF           ARRAY_SIZE(adc_config)
