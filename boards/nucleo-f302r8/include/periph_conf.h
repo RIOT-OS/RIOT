dd8eec540ab28744c01e1c7fcb5ed76bff9a24f5
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-08-31 10:48:52 +0200

    boards/stm32f3*: adapt to new clock configuration

--- boards/nucleo-f302r8/include/periph_conf.h
+++ boards/nucleo-f302r8/include/periph_conf.h
@@ -24,7 +24,14 @@
 #define PERIPH_CONF_H
 
 /* This board provides an LSE */
-#define CLOCK_LSE            (1)
+#ifndef CONFIG_BOARD_HAS_LSE
+#define CONFIG_BOARD_HAS_LSE    1
+#endif
+
+/* This board provides an HSE */
+#ifndef CONFIG_BOARD_HAS_HSE
+#define CONFIG_BOARD_HAS_HSE    1
+#endif
 
 #include "periph_cpu.h"
 #include "f1f3/cfg_clock_default.h"
--- boards/stm32f3discovery/include/periph_conf.h
+++ boards/stm32f3discovery/include/periph_conf.h
@@ -19,6 +19,11 @@
 #ifndef PERIPH_CONF_H
 #define PERIPH_CONF_H
 
+/* This board provides an HSE */
+#ifndef CONFIG_BOARD_HAS_HSE
+#define CONFIG_BOARD_HAS_HSE    1
+#endif
+
 #include "periph_cpu.h"
 #include "f1f3/cfg_clock_default.h"
 
