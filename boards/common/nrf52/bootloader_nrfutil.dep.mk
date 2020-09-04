0debe57566c9c449b55f25ee9f540eb3bbc5d1a8
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2020-09-04 16:14:11 +0200

    common/samd21-arduino-bootloader: use common stdio CDC ACM config

--- boards/common/nrf52/bootloader_nrfutil.dep.mk
+++ boards/common/nrf52/bootloader_nrfutil.dep.mk
@@ -1,6 +1,6 @@
-include $(RIOTBOARD)/common/makefiles/stdio_cdc_acm.mk
+include $(RIOTBOARD)/common/makefiles/stdio_cdc_acm.dep.mk
 
-# enable bootloader reset over USB, requires USB bootloader to be used
+# enable bootloader reset over USB, requires CDC ACM to be used
 ifneq (,$(filter stdio_cdc_acm,$(USEMODULE)))
   USEMODULE += usb_board_reset
 endif
