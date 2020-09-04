705ed923babbd4ca80c90ec29adf017fe637211c
Jose Alamos <jose.alamos@haw-hamburg.de>
2020-09-04 11:25:02 +0200

    auto_init/at86rf2xx: add support for SubMAC

--- sys/net/gnrc/netif/init_devs/auto_init_at86rf2xx.c
+++ sys/net/gnrc/netif/init_devs/auto_init_at86rf2xx.c
@@ -28,6 +28,11 @@
 #endif
 #include "net/gnrc.h"
 
+#include "event.h"
+
+#include "net/ieee802154/radio.h"
+#include "net/netdev/ieee802154_submac.h"
+
 #include "at86rf2xx.h"
 #include "at86rf2xx_params.h"
 
@@ -42,31 +47,68 @@
 
 #define AT86RF2XX_NUM ARRAY_SIZE(at86rf2xx_params)
 
-static at86rf2xx_t at86rf2xx_devs[AT86RF2XX_NUM];
 static gnrc_netif_t _netif[AT86RF2XX_NUM];
 static char _at86rf2xx_stacks[AT86RF2XX_NUM][AT86RF2XX_MAC_STACKSIZE];
+
+#if IS_USED(MODULE_IEEE802154_RADIO_HAL)
+typedef struct {
+    event_t event;
+    at86rf2xx_t dev;
+} at86rf2xx_bh_t;
+
+static at86rf2xx_bh_t at86rf2xx_devs[AT86RF2XX_NUM];
+static netdev_ieee802154_submac_t at86rf2xx_submac[AT86RF2XX_NUM];
+
+void at86rf2xx_irq_handler(void *dev);
+
+static void _isr_at86rf2xx(void *arg)
+{
+    at86rf2xx_bh_t *bh = container_of(arg, at86rf2xx_bh_t, dev);
+    int index = bh - at86rf2xx_devs;
+    event_post(&_netif[index].evq, &bh->event);
+}
+
+/* This is our Bottom-Half processor */
+static void _irq_event_handler(event_t *event)
+{
+    at86rf2xx_bh_t *bh = (at86rf2xx_bh_t*) event;
+    at86rf2xx_irq_handler(&bh->dev);
+}
+#else
+static at86rf2xx_t at86rf2xx_devs[AT86RF2XX_NUM];
+#endif
 
 void auto_init_at86rf2xx(void)
 {
     for (unsigned i = 0; i < AT86RF2XX_NUM; i++) {
         LOG_DEBUG("[auto_init_netif] initializing at86rf2xx #%u\n", i);
 
+        netdev_t *netdev;
+#if IS_USED(MODULE_IEEE802154_RADIO_HAL)
+        netdev_ieee802154_submac_init(&at86rf2xx_submac[i], &(at86rf2xx_devs[i].dev.dev));
+        at86rf2xx_init(&at86rf2xx_devs[i].dev, &at86rf2xx_params[i], _isr_at86rf2xx);
+        at86rf2xx_devs[i].event.handler = _irq_event_handler;
+        netdev = (netdev_t*) &at86rf2xx_submac[i];
+#else
         at86rf2xx_setup(&at86rf2xx_devs[i], &at86rf2xx_params[i], i);
+        netdev = (netdev_t*) at86rf2xx_devs[i];
+#endif
+
 #if defined(MODULE_GNRC_GOMACH)
         gnrc_netif_gomach_create(&_netif[i], _at86rf2xx_stacks[i],
                                  AT86RF2XX_MAC_STACKSIZE,
                                  AT86RF2XX_MAC_PRIO, "at86rf2xx-gomach",
-                                 (netdev_t *)&at86rf2xx_devs[i]);
+                                 netdev);
 #elif defined(MODULE_GNRC_LWMAC)
         gnrc_netif_lwmac_create(&_netif[i], _at86rf2xx_stacks[i],
                                 AT86RF2XX_MAC_STACKSIZE,
                                 AT86RF2XX_MAC_PRIO, "at86rf2xx-lwmac",
-                                (netdev_t *)&at86rf2xx_devs[i]);
+                                netdev);
 #else
         gnrc_netif_ieee802154_create(&_netif[i], _at86rf2xx_stacks[i],
                                      AT86RF2XX_MAC_STACKSIZE,
                                      AT86RF2XX_MAC_PRIO, "at86rf2xx",
-                                     (netdev_t *)&at86rf2xx_devs[i]);
+                                     netdev);
 #endif
     }
 }
