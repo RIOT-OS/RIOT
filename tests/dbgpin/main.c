4a09455bb548b25baf9e990d68dc04e580d12d8e
Hauke Petersen <hauke.petersen@fu-berlin.de>
2020-09-15 14:46:24 +0200

    tests: initial import of dgbpin test

---
+++ tests/dbgpin/main.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Test for the dbgpin module
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include "xtimer.h"
+
+#define TICK        (5 * US_PER_MS)     /* -> 5ms */
+
+int main(void)
+{
+    printf("Found %i configured debug pin(s)\n", dbgpin_cnt());
+
+    for (unsigned p = 0; p < dbgpin_cnt(); p++) {
+        printf("Testing pin %u\n", p);
+        dbgpin_set(p);
+        xtimer_usleep(TICK);
+        dbgpin_clr(p);
+        xtimer_usleep(TICK);
+        dbgpin_tgl(p);
+        xtimer_usleep(2 * TICK);
+        dbgpin_tgl(p);
+        xtimer_usleep(TICK);
+        dbgpin_pulse(p);
+        xtimer_usleep(TICK);
+        for (unsigned i = 2; i <= 5; i++) {
+            dbgpin_sig(p, i);
+            xtimer_usleep(TICK);
+        }
+    }
+
+    puts("Test successful.");
+
+    return 0;
+}
