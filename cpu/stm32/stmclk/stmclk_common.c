42f71914a5e5e412ea489eb94de25983b6ee9652
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-10-20 22:13:50 +0200

    cpu/stm32: simplify stmclk_disable_hsi function
    There is no need to check for CLOCK_HSE or to check if HSI is used as SYSCLK, this is already checked at compile time in the clock initialization code

--- cpu/stm32/stmclk/stmclk_common.c
+++ cpu/stm32/stmclk/stmclk_common.c
@@ -64,13 +64,7 @@ void stmclk_enable_hsi(void)
 
 void stmclk_disable_hsi(void)
 {
-    /* we only disable the HSI clock if not used as input for the PLL and if
-     * not used directly as system clock */
-    if (CLOCK_HSE) {
-        if ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI) {
-            RCC->CR &= ~(RCC_CR_HSION);
-        }
-    }
+    RCC->CR &= ~RCC_CR_HSION;
 }
 
 void stmclk_enable_lfclk(void)
