3cac6e0979468ba56659291fd1cd11096611589d
Martine Lenders <m.lenders@fu-berlin.de>
2020-10-28 10:37:31 +0100

    gnrc_nettype: remove GNRC_NETTYPE_IOVEC
    NETTYPE is unused in the code base since
    9fb2f541baca469e34fa01b004d6f19385700ce9 and deprecated since
    744f078b3a506609476715c41460ed039f40620c

--- sys/include/net/gnrc/nettype.h
+++ sys/include/net/gnrc/nettype.h
@@ -49,15 +49,6 @@ extern "C" {
  */
 typedef enum {
     /**
-     * @brief   Not so much protocol but data type that is passed to network
-     *          devices using the netdev interface
-     *
-     * @deprecated  Unused since https://github.com/RIOT-OS/RIOT/pull/11193.
-     *              Will be removed after 2020.10 release.
-     *
-     */
-    GNRC_NETTYPE_IOVEC = -2,
-    /**
      * @brief   Protocol is as defined in @ref gnrc_netif_hdr_t. Not usable with
      *          @ref net_gnrc_netreg
      */
