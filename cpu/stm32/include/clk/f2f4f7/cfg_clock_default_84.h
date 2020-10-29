a90016740c8a5c2af2c644b08dd88e59bca3c8e4
Benjamin Valentin <benpicco@beuth-hochschule.de>
2020-10-29 17:30:22 +0100

    cpu/stm32/clk/f2f4f7: add config for 25 MHz HSE

--- cpu/stm32/include/clk/f2f4f7/cfg_clock_default_84.h
+++ cpu/stm32/include/clk/f2f4f7/cfg_clock_default_84.h
@@ -33,14 +33,20 @@ extern "C" {
  * @name    Clock PLL settings (84MHz)
  * @{
  */
-/* The following parameters configure a 84MHz system clock with HSE (8MHz or
-   16MHz) or HSI (16MHz) as PLL input clock */
+/* The following parameters configure a 84MHz system clock with HSE (8MHz, 16MHz
+   or 25MHz) or HSI (16MHz) as PLL input clock */
 #ifndef CONFIG_CLOCK_PLL_M
+#if IS_ACTIVE(CONFIG_BOARD_HAS_HSE) && (CLOCK_HSE == MHZ(25))
+#define CONFIG_CLOCK_PLL_M              (25)
+#else
 #define CONFIG_CLOCK_PLL_M              (4)
+#endif
 #endif
 #ifndef CONFIG_CLOCK_PLL_N
 #if IS_ACTIVE(CONFIG_BOARD_HAS_HSE) && (CLOCK_HSE == MHZ(8))
 #define CONFIG_CLOCK_PLL_N              (168)
+#elif IS_ACTIVE(CONFIG_BOARD_HAS_HSE) && (CLOCK_HSE == MHZ(25))
+#define CONFIG_CLOCK_PLL_N              (336)
 #else
 #define CONFIG_CLOCK_PLL_N              (84)
 #endif
