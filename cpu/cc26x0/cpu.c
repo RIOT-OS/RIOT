b8a88750861617cda21d405f4c5339c40653f8a2
lutgaru <lutgaru@gmail.com>
2020-10-31 17:34:23 -0700

    cpu/cc26x0: implement setuptrimdevice function for cc26x0

--- cpu/cc26x0/cpu.c
+++ cpu/cc26x0/cpu.c
@@ -21,6 +21,7 @@
 #include "periph_conf.h"
 #include "periph/init.h"
 #include "stdio_base.h"
+#include "vendor/driverlib/setup.h"
 
 #ifndef HF_CLOCK_SOURCE
 #define HF_CLOCK_SOURCE DDI_0_OSC_CTL0_SCLK_HF_SRC_SEL_RCOSC /* set 48MHz RCOSC */
@@ -30,11 +31,6 @@
 #endif
 
 /**
- *@brief Configure the MCU system clock
- */
-static void cpu_clock_init(void);
-
-/**
  * @brief Initialize the CPU, set IRQ priorities
  */
 void cpu_init(void)
@@ -42,21 +38,12 @@ void cpu_init(void)
     /* initialize the Cortex-M core */
     cortexm_init();
 
-    /* initialize the system clock */
-    cpu_clock_init();
+    /* Final trim of device */
+    SetupTrimDevice();
 
     /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
     stdio_init();
 
     /* trigger static peripheral initialization */
     periph_init();
-}
-
-static void cpu_clock_init(void)
-{
-    AON_WUC->AUXCTL |= AUXCTL_AUX_FORCE_ON; /* power on AUX_PD */
-    while(!(AON_WUC->PWRSTAT & PWRSTAT_AUX_PD_ON)); /* wait for AUX_PD to be powered on */
-    AUX_WUC->MODCLKEN0 |= MODCLKEN0_AUX_DDI0_OSC_EN; /* turn on oscillator interface clock */
-
-    DDI_0_OSC->CTL0 |= HF_CLOCK_SOURCE | LF_CLOCK_SOURCE; /* configure HF and LF clocks */
 }
