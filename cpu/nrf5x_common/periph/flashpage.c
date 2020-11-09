e1766492669cd9ddd710a45f90070683f0892c21
Koen Zandberg <koen@bergzand.net>
2020-11-09 16:44:21 +0100

    nrf5x: Adapt to flashpage/flashpage_pagewise API

--- cpu/nrf5x_common/periph/flashpage.c
+++ cpu/nrf5x_common/periph/flashpage.c
@@ -23,33 +23,7 @@
 #include "assert.h"
 #include "periph/flashpage.h"
 
-void flashpage_write_raw(void *target_addr, const void *data, size_t len)
-{
-    /* assert multiples of FLASHPAGE_RAW_BLOCKSIZE are written and no less of
-       that length. */
-    assert(!(len % FLASHPAGE_RAW_BLOCKSIZE));
-
-    /* ensure writes are aligned */
-    assert(!(((unsigned)target_addr % FLASHPAGE_RAW_ALIGNMENT) ||
-            ((unsigned)data % FLASHPAGE_RAW_ALIGNMENT)));
-
-    /* ensure the length doesn't exceed the actual flash size */
-    assert(((unsigned)target_addr + len) <
-           (CPU_FLASH_BASE + (FLASHPAGE_SIZE * FLASHPAGE_NUMOF)) + 1);
-
-    uint32_t *page_addr = target_addr;
-    const uint32_t *data_addr = data;
-
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
-    for (unsigned i = 0; i < (len / FLASHPAGE_RAW_BLOCKSIZE); i++) {
-        *page_addr++ = data_addr[i];
-    }
-
-    /* finish up */
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
-}
-
-void flashpage_write(int page, const void *data)
+void flashpage_erase(unsigned page)
 {
     assert(page < (int)FLASHPAGE_NUMOF);
 
@@ -59,9 +33,30 @@ void flashpage_write(int page, const void *data)
     NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
     NRF_NVMC->ERASEPAGE = (uint32_t)page_addr;
     while (NRF_NVMC->READY == 0) {}
+}
 
-    /* write data to page */
-    if (data != NULL) {
-        flashpage_write_raw(page_addr, data, FLASHPAGE_SIZE);
+void flashpage_write(void *target_addr, const void *data, size_t len)
+{
+    /* assert multiples of FLASHPAGE_WRITE_BLOCK_SIZE are written and no less of
+       that length. */
+    assert(!(len % FLASHPAGE_WRITE_BLOCK_SIZE));
+
+    /* ensure writes are aligned */
+    assert(!(((unsigned)target_addr % FLASHPAGE_WRITE_BLOCK_ALIGNMENT) ||
+            ((unsigned)data % FLASHPAGE_WRITE_BLOCK_ALIGNMENT)));
+
+    /* ensure the length doesn't exceed the actual flash size */
+    assert(((unsigned)target_addr + len) <
+           (CPU_FLASH_BASE + (FLASHPAGE_SIZE * FLASHPAGE_NUMOF)) + 1);
+
+    uint32_t *page_addr = target_addr;
+    const uint32_t *data_addr = data;
+
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+    for (unsigned i = 0; i < (len / FLASHPAGE_WRITE_BLOCK_SIZE); i++) {
+        *page_addr++ = data_addr[i];
     }
+
+    /* finish up */
+    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
 }
