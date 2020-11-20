57ffd6660510af1b5f9443fa2512b36dfb796815
Bas Stottelaar <basstottelaar@gmail.com>
2020-11-21 00:53:29 +0100

    boards/e180-zg120b-tb: add initial support

---
+++ boards/e180-zg120b-tb/board.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015-2020 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_e180-zg120b-tb
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations E180-ZG120B-TB board
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "board_common.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+
+#ifndef RIOTBOOT
+    /* initialize the LEDs */
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+
+    /* initialize the push buttons */
+    gpio_init(PB0_PIN, GPIO_IN);
+    gpio_init(PB1_PIN, GPIO_IN);
+    gpio_init(PB2_PIN, GPIO_IN);
+#endif
+}
