3cca8f4ee27ef1131611356c130c82d6f8546234
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-11-20 22:02:32 +0100

    makefiles/tools/renode: control renode via Makefile only

--- makefiles/tools/renode.inc.mk
+++ makefiles/tools/renode.inc.mk
@@ -1,2 +1,11 @@
-EMULATOR ?= $(RIOTTOOLS)/renode/run-renode.sh
-EMULATOR_FLAGS ?= start
+RENODE ?= renode
+RENODE_IMAGE_FILE ?= $(ELFFILE)
+RENODE_BOARD_CONFIG ?= $(BOARDDIR)/dist/board.resc
+
+# Use renode interactive commands to specify the image file and board config
+RENODE_FLAGS += -e "set image_file '$(RENODE_IMAGE_FILE)'"
+RENODE_FLAGS += -e "include @$(RENODE_BOARD_CONFIG)"
+
+# Set emulator variables
+EMULATOR_FLAGS ?= $(RENODE_FLAGS)
+EMULATOR ?= $(RENODE)
