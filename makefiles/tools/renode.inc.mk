dcaa9eb59c232392921fc809143a8a72435440f1
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-11-21 18:12:43 +0100

    makefiles/tools/renode: improve debugger management
    When using the debug target, the GDB server is spawned automatically in the background. This logic is moved in a external script

--- makefiles/tools/renode.inc.mk
+++ makefiles/tools/renode.inc.mk
@@ -1,8 +1,6 @@
 RENODE ?= renode
 RENODE_IMAGE_FILE ?= $(ELFFILE)
 RENODE_BOARD_CONFIG ?= $(BOARDDIR)/dist/board.resc
-DEBUGGER ?= gdb-multiarch
-DEBUGGER_PORT ?= 3333
 
 # Use renode interactive commands to specify the image file and board config
 RENODE_CONFIG_FLAGS += -e "set image_file '$(RENODE_IMAGE_FILE)'"
@@ -10,8 +8,20 @@ RENODE_CONFIG_FLAGS += -e "include @$(RENODE_BOARD_CONFIG)"
 
 # Set emulator variables
 EMULATOR_FLAGS ?= $(RENODE_CONFIG_FLAGS) -e start
-EMULATOR_DEBUG_FLAGS += $(RENODE_CONFIG_FLAGS)
-EMULATOR_DEBUG_FLAGS += -e "machine StartGdbServer $(DEBUGGER_PORT) true"
-
 EMULATOR ?= $(RENODE)
-DEBUGGER_FLAGS ?= $(ELFFILE) -ex "target remote :$(DEBUGGER_PORT)" -ex "monitor start"
+
+# Configure the debugger
+GDB_PORT ?= 3333
+EMULATOR_PIDFILE ?=
+ifneq (,$(EMULATOR_PIDFILE))
+  $(info Using Renode pid file $(EMULATOR_PIDFILE))
+  RENODE_CONFIG_FLAGS += --pid-file $(EMULATOR_PIDFILE)
+endif
+RENODE_DEBUG_FLAGS += $(RENODE_CONFIG_FLAGS)
+RENODE_DEBUG_FLAGS += -e "machine StartGdbServer $(GDB_PORT) true"
+
+DEBUGSERVER ?= $(EMULATOR)
+DEBUGSERVER_FLAGS ?= $(RENODE_DEBUG_FLAGS)
+
+DEBUGGER_FLAGS ?= $(BOARD) $(APPDIR) $(ELFFILE) $(GDB_PORT) "-ex \"monitor start\""
+DEBUGGER ?= $(RIOTTOOLS)/renode/renode-debug.sh
