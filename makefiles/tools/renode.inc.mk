4707de2a5f7e942991d4d40a4f92e01401a7c87d
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-11-21 13:39:16 +0100

    makefiles/renode: add debugger config

--- makefiles/tools/renode.inc.mk
+++ makefiles/tools/renode.inc.mk
@@ -1,11 +1,17 @@
 RENODE ?= renode
 RENODE_IMAGE_FILE ?= $(ELFFILE)
 RENODE_BOARD_CONFIG ?= $(BOARDDIR)/dist/board.resc
+DEBUGGER ?= gdb-multiarch
+DEBUGGER_PORT ?= 3333
 
 # Use renode interactive commands to specify the image file and board config
-RENODE_FLAGS += -e "set image_file '$(RENODE_IMAGE_FILE)'"
-RENODE_FLAGS += -e "include @$(RENODE_BOARD_CONFIG)"
+RENODE_CONFIG_FLAGS += -e "set image_file '$(RENODE_IMAGE_FILE)'"
+RENODE_CONFIG_FLAGS += -e "include @$(RENODE_BOARD_CONFIG)"
 
 # Set emulator variables
-EMULATOR_FLAGS ?= $(RENODE_FLAGS)
+EMULATOR_FLAGS ?= $(RENODE_CONFIG_FLAGS) -e start
+EMULATOR_DEBUG_FLAGS += $(RENODE_CONFIG_FLAGS)
+EMULATOR_DEBUG_FLAGS += -e "machine StartGdbServer $(DEBUGGER_PORT) true"
+
 EMULATOR ?= $(RENODE)
+DEBUGGER_FLAGS ?= $(ELFFILE) -ex "target remote :$(DEBUGGER_PORT)" -ex "monitor start"
