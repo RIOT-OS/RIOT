631ae45dc01930c4f757e892e5dece4449a5e4ae
Martine Lenders <m.lenders@fu-berlin.de>
2020-12-09 13:00:04 +0100

    test

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -23,10 +23,11 @@
 
 int main(void)
 {
-    puts("Hello World!");
+	puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
     return 0;
 }
+
