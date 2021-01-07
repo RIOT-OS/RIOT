2683bb264bad90682da008e8363e7c610d638c18
Alexandre Abadie <alexandre.abadie@inria.fr>
2021-01-07 18:46:03 +0100

    tests/cond_order: clean python test script
    The testrunner module path is already added the PYTHONPATH, so can be imported like a regular module

--- tests/cond_order/tests/01-run.py
+++ tests/cond_order/tests/01-run.py
@@ -7,8 +7,9 @@
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-import os
 import sys
+
+from testrunner import run
 
 NUM_THREADS = 5
 
@@ -43,6 +44,4 @@ def testfunc(child):
 
 
 if __name__ == "__main__":
-    sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
-    from testrunner import run
     sys.exit(run(testfunc))
