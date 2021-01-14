2d94a34fd87c32910ff79e87ae554f9a2a7481a2
Vincent Dupont <vincent@otakeys.com>
2021-01-14 11:38:34 +0100

    fixup! fixup! shell: add syslog commands

--- sys/shell/commands/sc_syslog.c
+++ sys/shell/commands/sc_syslog.c
@@ -20,13 +20,15 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "syslog.h"
 
-static void _syslog_usage(char **argv)
+static void _syslog_usage(void)
 {
-    printf("%s list\n", argv[0]);
-    printf("%s getmask\n", argv[0]);
+    puts("syslog list");
+    puts("syslog hostname <name>");
+    puts("syslog logd <level> <string>");
 }
 
 static int _list_handler(int argc, char **argv)
@@ -47,18 +49,51 @@ static int _list_handler(int argc, char **argv)
     return 0;
 }
 
+static int _hostname_handler(int argc, char **argv)
+{
+    static char hostname[32] = "";
+
+    if (argc < 2) {
+        _syslog_usage();
+        return 1;
+    }
+
+    strncpy(hostname, argv[1], sizeof(hostname) - 1);
+
+    syslog_set_hostname(hostname);
+
+    return 0;
+}
+
+static int _logd_handler(int argc, char **argv)
+{
+    if (argc != 3) {
+        _syslog_usage();
+        return 1;
+    }
+    syslog(syslog_default_entry, atoi(argv[1]), "%s", argv[2]);
+
+    return 0;
+}
+
 int _syslog_handler(int argc, char **argv)
 {
     if (argc < 2) {
-        _syslog_usage(argv);
+        _syslog_usage();
         return 1;
     }
     if (strcmp(argv[1], "list") == 0) {
         return _list_handler(argc - 1, argv + 1);
     }
+    else if (strcmp(argv[1], "hostname") == 0) {
+        return _hostname_handler(argc - 1, argv + 1);
+    }
+    else if (strcmp(argv[1], "logd") == 0) {
+        return _logd_handler(argc - 1, argv + 1);
+    }
     else {
-        printf("%s: unsupported sub-command: %s\n", argv[0], argv[1]);
-        _syslog_usage(argv);
+        printf("syslog: unsupported sub-command: %s\n", argv[1]);
+        _syslog_usage();
         return 1;
     }
 }
