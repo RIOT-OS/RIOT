d5062cfd4e31379fa5bcca232bf86bc99d0e2315
Jose Alamos <jose@alamos.cc>
2021-01-20 10:46:36 +0100

    ci/vera++/rules/L004: remove warning support
    This reverts commit f3a934a169abd299f593c9e60d6ef53da1d68dfb.

--- dist/tools/vera++/scripts/rules/L004.tcl
+++ dist/tools/vera++/scripts/rules/L004.tcl
@@ -3,17 +3,11 @@
 
 set maxLength [getParameter "max-line-length" 100]
 
-#MOD: We add here a line length thershold for line length
-set maxLengthWarn [getParameter "max-line-length-warn" 80]
-
 foreach f [getSourceFileNames] {
     set lineNumber 1
     foreach line [getAllLines $f] {
         if {[string length $line] > $maxLength} {
             report $f $lineNumber "line is longer than ${maxLength} characters"
-        } elseif {[string length $line] > $maxLengthWarn} {
-            # puts won't make vera++ return error code when invoked with --error
-            puts "$f:$lineNumber: warning: line is longer than $maxLengthWarn characters"
         }
         incr lineNumber
     }
