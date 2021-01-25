416526c6ac131a55398f556efa2bcc1a9aafa8c2
Akshai M <akshai.m@fu-berlin.de>
2021-01-25 23:02:48 +0100

    drivers/adcxx1c : Move `ADCXX1C_PARAM_LOW_LIMIT` to `CONFIG_`

--- drivers/adcxx1c/include/adcxx1c_params.h
+++ drivers/adcxx1c/include/adcxx1c_params.h
@@ -61,8 +61,8 @@ extern "C" {
 #endif
 
 /** @brief  Low limit for the alert */
-#ifndef ADCXX1C_PARAM_LOW_LIMIT
-#define ADCXX1C_PARAM_LOW_LIMIT  (0)
+#ifndef CONFIG_ADCXX1C_PARAM_LOW_LIMIT
+#define CONFIG_ADCXX1C_PARAM_LOW_LIMIT          (0)
 #endif
 
 /** @brief  High limit for the alert */
@@ -85,7 +85,7 @@ extern "C" {
                                   .bits       = ADCXX1C_PARAM_BITS,       \
                                   .cycle      = ADCXX1C_PARAM_CYCLE,      \
                                   .alert_pin  = ADCXX1C_PARAM_ALERT_PIN,  \
-                                  .low_limit  = ADCXX1C_PARAM_LOW_LIMIT,  \
+                                  .low_limit  = CONFIG_ADCXX1C_PARAM_LOW_LIMIT,  \
                                   .high_limit = ADCXX1C_PARAM_HIGH_LIMIT, \
                                   .hysteresis = ADCXX1C_PARAM_HYSTERESIS }
 #endif
