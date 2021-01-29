1843f4267b87a6e5427ddb1a3f6ae2b549b960e7
Hendrik van Essen <hendrik.ve@fu-berlin.de>
2021-01-29 01:39:07 +0100

    examples/wolfmqtt_snclient: add example from wolfmqtt pkg

---
+++ examples/wolfmqtt_snclient/user_settings.h
@@ -0,0 +1,51 @@
+/* user_settings.h : custom configuration for wolfmqtt */
+
+#ifndef WOLFMQTT_USER_SETTINGS_H
+#define WOLFMQTT_USER_SETTINGS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* MANDATORY */
+#undef  RIOT_OS
+#define RIOT_OS
+
+#undef  WOLFMQTT_SN
+#define WOLFMQTT_SN
+
+#define WOLFMQTT_USE_HOSTNAME 0
+
+#if WOLFMQTT_USE_HOSTNAME
+    #undef DEFAULT_MQTT_HOST
+    #ifdef SOCK_HAS_IPV6
+        #define DEFAULT_MQTT_HOST   "TODO" /* if you know a public mqttsn service put it here */
+    #else
+        #define DEFAULT_MQTT_HOST   "TODO" /* if you know a public mqttsn service put it here */
+    #endif
+#else
+    #ifdef SOCK_HAS_IPV4
+        #undef  DEFAULT_MQTT_HOST_IPV4
+        #define DEFAULT_MQTT_HOST_IPV4   "192.168.2.113" /* local */
+    #endif
+    #ifdef SOCK_HAS_IPV6
+        #undef  DEFAULT_MQTT_HOST_IPV6
+        #define DEFAULT_MQTT_HOST_IPV6   "2003:e9:f44:2f25:95dc:c243:6b3b:a257" /* local */
+    #endif
+#endif
+
+#undef  DEFAULT_MQTT_PORT
+#define DEFAULT_MQTT_PORT   10000
+
+/* OPTIONAL */
+#undef  TEST_MESSAGE
+#define TEST_MESSAGE    "wolfmqtt snclient on RIOT OS"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* WOLFMQTT_USER_SETTINGS_H */
+
