2bca4d3ac3e4edd39dff67ed1b6609c47c040155
Jean Pierre Dudey <me@jeandudey.tech>
2021-02-08 17:26:18 +0100

    boards: add cc1350-launchpad
    Signed-off-by: Jean Pierre Dudey <me@jeandudey.tech>

---
+++ boards/cc1350-launchpad/board.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C)    2021 Jean Pierre Dudey
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup         boards_cc1350_launchpad
+ * @{
+ *
+ * @file
+ * @brief           Board specific implementations for TI CC1350 LaunchPad
+ *
+ * @author          Jean Pierre Dudey <jeandudey@hotmail.com>
+ */
+
+#include "cpu.h"
+#include "board.h"
+
+/**
+ * @brief           Initialise the board.
+ */
+
+void board_init(void)
+{
+    cpu_init();
+
+    gpio_init(LED0_PIN, GPIO_OUT);
+    gpio_init(LED1_PIN, GPIO_OUT);
+}
