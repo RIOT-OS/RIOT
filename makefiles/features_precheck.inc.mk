29d5dfe36ad258a4055010311e9739a8cfa72cdf
Karl Fessel <karl.fessel@ovgu.de>
2021-02-12 14:30:09 +0100

    Blacklist non available, Fix: select optional features only from usable

--- makefiles/features_precheck.inc.mk
+++ makefiles/features_precheck.inc.mk
@@ -1,17 +1,15 @@
 # Check if provided features are available
 
-ifneq (,$(filter picolibc,$(FEATURES_PROVIDED)) $(filter newlib,$(FEATURES_PROVIDED)))
-  ifneq (,$(filter picolibc,$(FEATURES_PROVIDED)))
-    # Test if picolibc.specs is available
-    ifneq ($(shell $(LINK) -specs=picolibc.specs -E - 2>/dev/null >/dev/null </dev/null ; echo $$?),0)
-      FEATURES_PROVIDED := $(filter-out picolibc,$(FEATURES_PROVIDED))
-    endif
-  endif
+ifneq (,$(filter picolibc,$(FEATURES_PROVIDED)))
+  # Test if picolibc.specs is available
+  ifneq ($(shell $(LINK) -specs=picolibc.specs -E - 2>/dev/null >/dev/null </dev/null ; echo $$?),0)
+    FEATURES_BLACKLIST += picolibc
+ endif
+endif
 
-  ifneq (,$(filter newlib,$(FEATURES_PROVIDED)))
-    # Test if nosys.specs is available
-    ifneq ($(shell $(LINK) -specs=nosys.specs -E - 2>/dev/null >/dev/null </dev/null ; echo $$?),0)
-      FEATURES_PROVIDED := $(filter-out newlib,$(FEATURES_PROVIDED))
-    endif
+ifneq (,$(filter newlib,$(FEATURES_PROVIDED)))
+  # Test if nosys.specs is available
+  ifneq ($(shell $(LINK) -specs=nosys.specs -E - 2>/dev/null >/dev/null </dev/null ; echo $$?),0)
+    FEATURES_BLACKLIST := newlib
   endif
 endif
