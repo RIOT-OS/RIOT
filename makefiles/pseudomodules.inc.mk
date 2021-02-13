f3c1106c9eba00a0cdf7104b63f29196e976d9e3
Benjamin Valentin <benpicco@googlemail.com>
2021-02-13 23:37:02 +0100

    cpu/nrf52: add SAUL driver for VDDH sensor

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -121,6 +121,7 @@ PSEUDOMODULES += saul_adc
 PSEUDOMODULES += saul_default
 PSEUDOMODULES += saul_gpio
 PSEUDOMODULES += saul_nrf_temperature
+PSEUDOMODULES += saul_nrf_vddh
 PSEUDOMODULES += saul_pwm
 PSEUDOMODULES += scanf_float
 PSEUDOMODULES += sched_cb
