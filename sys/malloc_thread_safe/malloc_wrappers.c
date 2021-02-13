05befa6a4c7b107b2af17812354c9f257aa7d4a9
Jim Paris <jim@jim.sh>
2021-02-12 22:54:18 -0500

    malloc_thread_safe: use recursive mutex
    When building with picolibc (using FEATURES_REQUIRED += picolibc),
    realloc(NULL, x) hangs.  realloc calls malloc(x), and so the code
    in malloc_wrappers.c ends up trying to lock the mutex twice.
    
    Switch to a recursive mutex so that this works.

--- sys/malloc_thread_safe/malloc_wrappers.c
+++ sys/malloc_thread_safe/malloc_wrappers.c
@@ -16,47 +16,47 @@
 
 #include "assert.h"
 #include "irq.h"
-#include "mutex.h"
+#include "rmutex.h"
 
 extern void *__real_malloc(size_t size);
 extern void __real_free(void *ptr);
 extern void *__real_calloc(size_t nmemb, size_t size);
 extern void *__real_realloc(void *ptr, size_t size);
 
-static mutex_t _lock;
+static rmutex_t _lock = RMUTEX_INIT;
 
 void *__wrap_malloc(size_t size)
 {
     assert(!irq_is_in());
-    mutex_lock(&_lock);
+    rmutex_lock(&_lock);
     void *ptr = __real_malloc(size);
-    mutex_unlock(&_lock);
+    rmutex_unlock(&_lock);
     return ptr;
 }
 
 void __wrap_free(void *ptr)
 {
     assert(!irq_is_in());
-    mutex_lock(&_lock);
+    rmutex_lock(&_lock);
     __real_free(ptr);
-    mutex_unlock(&_lock);
+    rmutex_unlock(&_lock);
 }
 
 void *__wrap_calloc(size_t nmemb, size_t size)
 {
     assert(!irq_is_in());
-    mutex_lock(&_lock);
+    rmutex_lock(&_lock);
     void *ptr = __real_calloc(nmemb, size);
-    mutex_unlock(&_lock);
+    rmutex_unlock(&_lock);
     return ptr;
 }
 
 void *__wrap_realloc(void *ptr, size_t size)
 {
     assert(!irq_is_in());
-    mutex_lock(&_lock);
+    rmutex_lock(&_lock);
     void *new = __real_realloc(ptr, size);
-    mutex_unlock(&_lock);
+    rmutex_unlock(&_lock);
     return new;
 }
 
