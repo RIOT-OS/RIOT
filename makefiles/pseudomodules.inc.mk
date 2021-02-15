f69fcc162b2d0f8f21b37bb0c34d163a4f15eef0
Hauke Petersen <hauke.petersen@fu-berlin.de>
2021-02-15 15:45:03 +0100

    make: introduce VDD_LC_FILTER_x feature

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -147,6 +147,7 @@ PSEUDOMODULES += stm32mp1_eng_mode
 PSEUDOMODULES += suit_transport_%
 PSEUDOMODULES += suit_storage_%
 PSEUDOMODULES += sys_bus_%
+PSEUDOMODULES += vdd_lc_filter_%
 PSEUDOMODULES += wakaama_objects_%
 PSEUDOMODULES += wifi_enterprise
 PSEUDOMODULES += xtimer_on_ztimer
