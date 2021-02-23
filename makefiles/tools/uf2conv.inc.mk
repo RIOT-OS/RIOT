3b9682788cbb0faa95841bff7c7472d53bb63a5d
Alexandre Abadie <alexandre.abadie@inria.fr>
2021-02-23 15:29:24 +0100

    tools/uf2conv: add deprecation warning about FFLAGS_OPTS

--- makefiles/tools/uf2conv.inc.mk
+++ makefiles/tools/uf2conv.inc.mk
@@ -1,5 +1,11 @@
 FLASHFILE ?= $(HEXFILE)
 
+# Warn about deprecated variables
+ifneq (,$(FFLAGS_OPTS))
+  $(warning Warning! FFLAGS_OPTS is deprecated use UF2CONV_FLAGS)
+  UF2CONV_FLAGS ?= $(FFLAGS_OPTS)
+endif
+
 FLASHER ?= $(RIOTTOOLS)/uf2/uf2conv.py
 FFLAGS  ?= $(UF2CONV_FLAGS) $(FLASHFILE)
 
