b1d0fe0b2be061e22b757d644d09309d904ee738
Alexandre Abadie <alexandre.abadie@inria.fr>
2021-02-23 15:28:47 +0100

    tools/cc2538-bsl: add deprecation warning about FFLAGS_OPTS

--- makefiles/tools/cc2538-bsl.inc.mk
+++ makefiles/tools/cc2538-bsl.inc.mk
@@ -3,6 +3,12 @@ FLASHFILE ?= $(CC2538_BSL_FLASHFILE)
 CC2538_BSL ?= $(RIOTTOOLS)/cc2538-bsl/cc2538-bsl.py
 PROG_BAUD ?= 500000	# default value in cc2538-bsl
 
+# Warn about deprecated variables
+ifneq (,$(FFLAGS_OPTS))
+  $(warning Warning! FFLAGS_OPTS is deprecated use CC2538_BSL_FLAGS_OPTS)
+  CC2538_BSL_FLAGS_OPTS ?= $(FFLAGS_OPTS)
+endif
+
 CC2538_BSL_FLAGS_OPTS ?=
 CC2538_BSL_FLAGS  = $(if $(IMAGE_OFFSET), -a $(shell printf "0x%08x" $$(($(IMAGE_OFFSET) + $(ROM_START_ADDR)))))
 CC2538_BSL_FLAGS += -p "$(PROG_DEV)" $(CC2538_BSL_FLAGS_OPTS) --write-erase -v -b $(PROG_BAUD) $(FLASHFILE)
