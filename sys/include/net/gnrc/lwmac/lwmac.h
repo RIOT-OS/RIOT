9e55c2d9e9d06e07bbc500ddee1fc5b49710f630
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2021-03-02 17:12:01 +0100

    sys/net/gnrc/lwmac: add missing const qualifier
    The name of the thread running the MAC can be stored in ROM.

--- sys/include/net/gnrc/lwmac/lwmac.h
+++ sys/include/net/gnrc/lwmac/lwmac.h
@@ -344,7 +344,7 @@ extern "C" {
  * @return  negative number on error
  */
 int gnrc_netif_lwmac_create(gnrc_netif_t *netif, char *stack, int stacksize,
-                            char priority, char *name, netdev_t *dev);
+                            char priority, const char *name, netdev_t *dev);
 #ifdef __cplusplus
 }
 #endif
