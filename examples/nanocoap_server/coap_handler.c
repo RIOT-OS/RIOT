26c922550c9429ca5f6febabda2204fee7201744
Lars <lars.kowoll@haw-hamburg.de>
2021-03-11 10:10:53 +0100

    examples/nanocoap_server: add check for payload size

--- examples/nanocoap_server/coap_handler.c
+++ examples/nanocoap_server/coap_handler.c
@@ -97,11 +97,16 @@ static ssize_t _riot_value_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, vo
     case COAP_PUT:
     case COAP_POST:
     {
-        /* convert the payload to an integer and update the internal value */
-        char payload[16] = { 0 };
-        memcpy(payload, (char*)pkt->payload, pkt->payload_len);
-        internal_value = strtol(payload, NULL, 10);
-        code = COAP_CODE_CHANGED;
+        if (pkt->payload_len <= 15) {
+            /* convert the payload to an integer and update the internal value */
+            char payload[16] = { 0 };
+            memcpy(payload, (char*)pkt->payload, pkt->payload_len);
+            internal_value = strtol(payload, NULL, 10);
+            code = COAP_CODE_CHANGED;
+        }
+        else {
+            code = COAP_CODE_BAD_REQUEST;
+        }
     }
     }
 
