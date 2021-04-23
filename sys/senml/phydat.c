9d61bdbb06c7756463af352815eea48bf544e412
Silke Hofstra <silke@slxh.eu>
2021-04-23 11:32:36 +0200

    sys/senml: add SenML modules
    Add a basic SenML module and submodules with support for:
    
    - Encoding SenML values as CBOR using NanoCBOR.
    - Converting from Phydat to SenML.
    - Reading and encoding SAUL sensors.

---
+++ sys/senml/phydat.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2021 Silke Hofstra
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdint.h>
+
+#include "math.h"
+#include "senml.h"
+#include "senml/phydat.h"
+
+
+
+static uint8_t phydat_unit_to_senml_unit(uint8_t unit)
+{
+    switch (unit) {
+    /* Compatible units */
+    case UNIT_TEMP_C:  return SENML_UNIT_CELSIUS;
+    case UNIT_TEMP_K:  return SENML_UNIT_KELVIN;
+    case UNIT_LUX:     return SENML_UNIT_LUX;
+    case UNIT_M:       return SENML_UNIT_METER;
+    case UNIT_M2:      return SENML_UNIT_SQUARE_METER;
+    case UNIT_M3:      return SENML_UNIT_CUBIC_METER;
+    case UNIT_GR:      return SENML_UNIT_GRAM;
+    case UNIT_A:       return SENML_UNIT_AMPERE;
+    case UNIT_V:       return SENML_UNIT_VOLT;
+    case UNIT_W:       return SENML_UNIT_WATT;
+    case UNIT_T:       return SENML_UNIT_TESLA;
+    case UNIT_COULOMB: return SENML_UNIT_COULOMB;
+    case UNIT_F:       return SENML_UNIT_FARAD;
+    case UNIT_OHM:     return SENML_UNIT_OHM;
+    case UNIT_PH:      return SENML_UNIT_PH;
+    case UNIT_PA:      return SENML_UNIT_PASCAL;
+    case UNIT_CD:      return SENML_UNIT_CANDELA;
+
+    /* Compatible Secondary units */
+    case UNIT_DBM:     return SENML_UNIT_DECIBEL_MILLIWATT;
+    case UNIT_PERCENT: return SENML_UNIT_PERCENT;
+    case UNIT_PERMILL: return SENML_UNIT_PERMILLE;
+    case UNIT_PPM:     return SENML_UNIT_PARTS_PER_MILLION;
+    case UNIT_PPB:     return SENML_UNIT_PARTS_PER_BILLION;
+
+    /* Incompatible units */
+    case UNIT_TEMP_F:  return SENML_UNIT_NONE;      /* use K or Cel instead */
+    case UNIT_GS:      return SENML_UNIT_NONE;      /* use T instead */
+    case UNIT_G:       return SENML_UNIT_NONE;      /* use m/s2 instead */
+    case UNIT_BAR:     return SENML_UNIT_NONE;      /* use Pa or hPa instead */
+    case UNIT_TIME:    return SENML_UNIT_NONE;      /* split into second/minute/hour */
+    case UNIT_DATE:    return SENML_UNIT_NONE;      /* split into day/month/year */
+    case UNIT_GPM3:    return SENML_UNIT_NONE;      /* use kg/m3 instead */
+    case UNIT_DPS:     return SENML_UNIT_NONE;      /* no alternative */
+    case UNIT_CPM3:    return SENML_UNIT_NONE;      /* no alternative */
+    default:           return SENML_UNIT_NONE;
+    }
+}
+
+void phydat_to_senml_bool(senml_bool_value_t *senml, const phydat_t *phydat, const uint8_t dim)
+{
+    senml->value = phydat->val[dim] == 1;
+    senml->attr.unit = SENML_UNIT_NONE;
+}
+
+void phydat_to_senml_float(senml_value_t *senml, const phydat_t *phydat, const uint8_t dim)
+{
+    float value = (float)(phydat->val[dim]);
+
+    if (phydat->scale) {
+        value *= pow(10, phydat->scale);
+    }
+
+    switch (phydat->unit) {
+    /* time conversion */
+    case UNIT_TIME:
+        senml->attr.unit = (dim == 0)
+                          ? SENML_UNIT_SECOND
+                          : (dim == 1)
+                            ? SENML_UNIT_MINUTE
+                            : SENML_UNIT_HOUR;
+        break;
+
+    /* simple conversions */
+    case UNIT_TEMP_F:
+        /* convert fahrenheit to kelvin */
+        value = (value + 459.67) * (5. / 9.);
+        senml->attr.unit = SENML_UNIT_KELVIN;
+        break;
+    case UNIT_G:
+        /* convert gravitational acceleration to acceleration */
+        value *= 9.80665;
+        senml->attr.unit = SENML_UNIT_METER_PER_SQUARE_SECOND;
+        break;
+    case UNIT_BAR:
+        value *= 100000;
+        senml->attr.unit = SENML_UNIT_PASCAL;
+        break;
+    case UNIT_GPM3:
+        value *= 0.001;
+        senml->attr.unit = SENML_UNIT_KILOGRAM_PER_CUBIC_METER;
+        break;
+    case UNIT_GS:
+        value *= 0.0001;
+        senml->attr.unit = SENML_UNIT_TESLA;
+        break;
+
+    /* compatible (or not converted) */
+    default:
+        senml->attr.unit = phydat_unit_to_senml_unit(phydat->unit);
+        break;
+    }
+
+    senml->value = senml_float(value);
+}
+
+void phydat_to_senml_decimal(senml_value_t *senml, const phydat_t *phydat, const uint8_t dim)
+{
+    int32_t m = phydat->val[dim];
+    int32_t e = phydat->scale;
+
+    switch (phydat->unit) {
+    /* time conversion */
+    case UNIT_TIME:
+        senml->attr.unit = (dim == 0)
+                          ? SENML_UNIT_SECOND
+                          : (dim == 1)
+                            ? SENML_UNIT_MINUTE
+                            : SENML_UNIT_HOUR;
+        break;
+
+    /* simple conversions */
+    case UNIT_BAR:
+        e += 5;
+        senml->attr.unit = SENML_UNIT_PASCAL;
+        break;
+    case UNIT_GPM3:
+        e -= 3;
+        senml->attr.unit = SENML_UNIT_KILOGRAM_PER_CUBIC_METER;
+        break;
+    case UNIT_GS:
+        e -= 4;
+        senml->attr.unit = SENML_UNIT_TESLA;
+        break;
+
+    /* compatible, or not converted */
+    default:
+        senml->attr.unit = phydat_unit_to_senml_unit(phydat->unit);
+        break;
+    }
+
+    senml->value = senml_decfrac(m, e);
+}
---
+++ tests/senml_phydat/main.c
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2021 Silke Hofstra
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       SenML Phydat tests
+ *
+ * @author      Silke Hofstra <silke@slxh.eu>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "embUnit.h"
+#include "senml/phydat.h"
+
+#define ENABLE_DEBUG (0)
+#include "debug.h"
+
+#ifndef PRIi64
+#define PRIi64 "lli"
+#endif
+
+typedef struct {
+    phydat_t phydat;
+    senml_value_t senml;
+} time_test_t;
+
+typedef struct {
+    phydat_t phydat;
+    senml_value_t senml1;
+    senml_value_t senml2;
+    uint8_t dim;
+} value_test_t;
+
+#define senml_s(s) { .attr = { .unit = SENML_UNIT_SECOND }, \
+                     .value = { SENML_TYPE_NUMERIC_INT, { .i = s } } }
+
+#define senml_f(v, u) { .attr = { .unit = u }, \
+                        .value = { SENML_TYPE_NUMERIC_FLOAT, { .f = v } } }
+
+#define senml_df(m, e, u) { .attr = { .unit = u }, \
+                            .value = { SENML_TYPE_NUMERIC_DECFRAC, { .df = { e, m } } } }
+
+static value_test_t value_tests[] = {
+    {
+        .phydat = { { 360, 0, 0 }, UNIT_M, 6 },
+        .senml1 = senml_f(360e6, SENML_UNIT_METER),
+        .senml2 = senml_df(360, 6, SENML_UNIT_METER),
+        .dim = 0
+    },
+    {
+        .phydat = { { 864, 0, 0 }, UNIT_TIME, 2 },
+        .senml1 = senml_f(86400, SENML_UNIT_SECOND),
+        .senml2 = senml_df(864, 2, SENML_UNIT_SECOND),
+        .dim = 0
+    },
+    {
+        .phydat = { { 0, 144, 0 }, UNIT_TIME, 1 },
+        .senml1 = senml_f(1440, SENML_UNIT_MINUTE),
+        .senml2 = senml_df(144, 1, SENML_UNIT_MINUTE),
+        .dim = 1
+    },
+    {
+        .phydat = { { 0, 0, 24 }, UNIT_TIME, 0 },
+        .senml1 = senml_f(24, SENML_UNIT_HOUR),
+        .senml2 = senml_df(24, 0, SENML_UNIT_HOUR),
+        .dim = 2
+    },
+    {
+        .phydat = { { 0, 0, 0 }, UNIT_TEMP_F, 3 },
+        .senml1 = senml_f(255.37, SENML_UNIT_KELVIN),
+        .senml2 = senml_df(0, 3, SENML_UNIT_NONE),
+    },
+    {
+        .phydat = { { 314, 0, 0 }, UNIT_G, -2 },
+        .senml1 = senml_f(30.792881, SENML_UNIT_METER_PER_SQUARE_SECOND),
+        .senml2 = senml_df(314, -2, SENML_UNIT_NONE),
+    },
+    {
+        .phydat = { { 988, 0, 0 }, UNIT_BAR, -3 },
+        .senml1 = senml_f(98.8e3, SENML_UNIT_PASCAL),
+        .senml2 = senml_df(988, 2, SENML_UNIT_PASCAL),
+    },
+    {
+        .phydat = { { 193, 0, 0 }, UNIT_GPM3, 5 },
+        .senml1 = senml_f(19.3e3, SENML_UNIT_KILOGRAM_PER_CUBIC_METER),
+        .senml2 = senml_df(193, 2, SENML_UNIT_KILOGRAM_PER_CUBIC_METER),
+    },
+    {
+        .phydat = { { 220, 0, 0 }, UNIT_GS, 3 },
+        .senml1 = senml_f(22, SENML_UNIT_TESLA),
+        .senml2 = senml_df(220, -1, SENML_UNIT_TESLA),
+    }
+};
+
+void test_phydat_to_senml_float(void)
+{
+    senml_value_t res;
+
+    for (size_t i = 0; i < ARRAY_SIZE(value_tests); i++) {
+        senml_value_t *exp = &(value_tests[i].senml1);
+
+        phydat_to_senml_float(&res, &(value_tests[i].phydat), value_tests[i].dim);
+
+        DEBUG("Float: %" PRIi16 "e%" PRIi16 " %s -> %.f %s\n",
+              value_tests[i].phydat.val[value_tests[i].dim], value_tests[i].phydat.scale,
+              phydat_unit_to_str(value_tests[i].phydat.unit),
+              res.value.value.f,
+              senml_unit_to_str(res.attr.unit));
+
+        TEST_ASSERT_EQUAL_STRING(senml_unit_to_str(exp->attr.unit),
+                                 senml_unit_to_str(res.attr.unit));
+        TEST_ASSERT_EQUAL_INT(exp->value.type, res.value.type);
+        TEST_ASSERT_EQUAL_INT((int)roundf(exp->value.value.f * 100),
+                              (int)roundf(res.value.value.f * 100));
+    }
+}
+
+void test_phydat_to_senml_decimal(void)
+{
+    senml_value_t res;
+
+    for (size_t i = 0; i < ARRAY_SIZE(value_tests); i++) {
+        senml_value_t *exp = &(value_tests[i].senml2);
+
+        phydat_to_senml_decimal(&res, &(value_tests[i].phydat), value_tests[i].dim);
+
+        DEBUG("Decimal: %" PRIi16 "e%" PRIi16 " %s -> %" PRIi32 "e%" PRIi32 " %s\n",
+              value_tests[i].phydat.val[value_tests[i].dim], value_tests[i].phydat.scale,
+              phydat_unit_to_str(value_tests[i].phydat.unit),
+              res.value.value.df.m, res.value.value.df.e,
+              senml_unit_to_str(res.attr.unit));
+
+        TEST_ASSERT_EQUAL_STRING(senml_unit_to_str(exp->attr.unit),
+                                 senml_unit_to_str(res.attr.unit));
+        TEST_ASSERT_EQUAL_INT(exp->value.type, res.value.type);
+        TEST_ASSERT_EQUAL_INT(exp->value.value.df.m, res.value.value.df.m);
+        TEST_ASSERT_EQUAL_INT(exp->value.value.df.e, res.value.value.df.e);
+    }
+}
+
+
+Test *tests_senml(void)
+{
+    EMB_UNIT_TESTFIXTURES(fixtures) {
+/* Don't run this test on CPUs with unpredictable rounding */
+#if !defined(__AVR__) && !defined(CPU_MSP430FXYZ)
+        new_TestFixture(test_phydat_to_senml_float),
+#endif
+        new_TestFixture(test_phydat_to_senml_decimal),
+    };
+    EMB_UNIT_TESTCALLER(senml_tests, NULL, NULL, fixtures);
+    return (Test *)&senml_tests;
+}
+
+int main(void)
+{
+    TESTS_START();
+    TESTS_RUN(tests_senml());
+    TESTS_END();
+    return 0;
+}
