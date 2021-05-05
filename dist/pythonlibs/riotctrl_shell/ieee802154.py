dd22ca6f5f125ea562fb11d42c46cd35bafd9824
Lars <lars.kowoll@haw-hamburg.de>
2021-05-05 12:02:41 +0200

    dist/pythonlibs/riotctrl_shell/ieee802154.py removed unnecessary code

--- dist/pythonlibs/riotctrl_shell/ieee802154.py
+++ dist/pythonlibs/riotctrl_shell/ieee802154.py
@@ -6,42 +6,14 @@
 
 import re
 
-from riotctrl.shell import ShellInteraction, ShellInteractionParser
-
-
-# ==== Parsers ====
-
-class ConfigPhyParser(ShellInteractionParser):
-    def __init__(self):
-        self.c_change = re.compile(r"Success! Channel: (?P<channel>\d+) was selected")
-
-    @staticmethod
-    def _add_channel(res, reply):
-        reply["channel"] = int(reply["channel"])
-        reply["ttl"] = int(reply["ttl"])
-        if reply.get("rtt") is not None:
-            reply["rtt"] = float(reply["rtt"])
-        else:
-            reply.pop("rtt", None)
-        if reply.get("dup"):
-            reply["dup"] = True
-        else:
-            reply.pop("dup", None)
-        if reply.get("rssi") is not None:
-            reply["rssi"] = int(reply["rssi"])
-        else:
-            reply.pop("rssi", None)
-        if "replies" in res:
-            res["replies"].append(reply)
-        else:
-            res["replies"] = [reply]
+from riotctrl.shell import ShellInteraction
 
 # ==== ShellInteractions ====
 
 class Config_phy(ShellInteraction):
-    def config_phy(self, channel, tx_pow, timeout=None, async_=False):
-        cmd = "config_phy {channel} {tx_pow}" \
-              .format(channel=channel, tx_pow=tx_pow)
+    def config_phy(self, phy_mode, channel, tx_pow, timeout=None, async_=False):
+        cmd = "config_phy {phy_mode} {channel} {tx_pow}" \
+              .format(phy_mode=phy_mode, channel=channel, tx_pow=tx_pow)
         
         try:
             res = self.cmd(cmd, timeout=timeout, async_=async_)
@@ -76,19 +48,6 @@ class Print_addr(ShellInteraction):
         return addr
 
 class Txtsnd(ShellInteraction):
-    def txtsnd(self, long_addr, timeout=None, async_=False):
-        cmd = "txtsnd {long_addr}" \
-              .format(long_addr=long_addr)
-
-        try:
-            res = self.cmd(cmd, timeout=timeout, async_=async_)
-        except Exception as e:
-            print(str(e))
-            print("Exception")
-
-        if "succeeded" not in res and "busy" not in res:
-            raise RuntimeError(res)
-
     def tx_mode(self, command, timeout=None, async_=False):
         cmd = "tx_mode {command}" \
                 .format(command=command)
@@ -128,9 +87,9 @@ class Txtsnd(ShellInteraction):
         if "Success" not in res:
             raise RuntimeError(res)
 
-    def spam(self, long_addr, len, number_of_packets, time_betweeen_packets, timeout=None, async_=False):
-        cmd = "spam {long_addr} {len} {number_of_packets} {time_betweeen_packets}" \
-              .format(long_addr=long_addr, len=len, number_of_packets=number_of_packets, time_betweeen_packets=time_betweeen_packets)
+    def txtsnd(self, long_addr, number_of_packets, time_betweeen_packets, timeout=None, async_=False):
+        cmd = "txtsnd {long_addr} {number_of_packets} {time_betweeen_packets}" \
+              .format(long_addr=long_addr, number_of_packets=number_of_packets, time_betweeen_packets=time_betweeen_packets)
 
         try:
             res = self.cmd(cmd, timeout=timeout, async_=async_)
