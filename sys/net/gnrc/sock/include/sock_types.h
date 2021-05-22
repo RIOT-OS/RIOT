6fba0d970c6e9665f58c140b23c32ed77c603ae5
Simon Brummer <simon.brummer@posteo.de>
2021-05-22 13:41:03 +0200

    gnrc_sock_tcp: integrate gnrc_tcp

--- sys/net/gnrc/sock/include/sock_types.h
+++ sys/net/gnrc/sock/include/sock_types.h
@@ -28,12 +28,14 @@
 #include "mbox.h"
 #include "net/af.h"
 #include "net/gnrc.h"
+#include "net/gnrc/tcp.h"
 #include "net/gnrc/netreg.h"
 #ifdef SOCK_HAS_ASYNC
 #include "net/sock/async/types.h"
 #endif
 #include "net/sock/ip.h"
 #include "net/sock/udp.h"
+#include "net/sock/tcp.h"
 
 #ifdef __cplusplus
 extern "C" {
