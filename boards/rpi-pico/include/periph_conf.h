d778e2be0dc258aa993fa9a530aaad06cac4cca8
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2021-06-01 21:31:52 +0200

    boards/rpi-pico: add the Raspberry Pi Pico
    Co-authored-by: Fabian Hüßler <fabian.huessler@st.ovgu.de>
    Co-authored-by: nickw96 <nick.weiler@st.ovgu.de>
    Co-authored-by: MaestroOnICe <justus.krebs@st.ovgu.de>
    Co-authored-by: Franz2000 <franz.freitag@st.ovgu.de>

---
+++ boards/rpi-pico/include/periph_conf.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup         boards_rpi_pico
+ * @{
+ *
+ * @file
+ * @brief           Configuration of CPU peripherals for the Raspberry Pi Pico
+ * @author          Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include <stdint.h>
+
+#include "cpu.h"
+#include "periph_cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const uart_conf_t uart_config[] = {
+    {
+        .dev = UART0,
+        .rx_pin = GPIO_PIN(0, 1),
+        .tx_pin = GPIO_PIN(0, 0),
+        .irqn = UART0_IRQ_IRQn
+    },
+    {
+        .dev = UART1,
+        .rx_pin = GPIO_PIN(0, 9),
+        .tx_pin = GPIO_PIN(0, 8),
+        .irqn = UART1_IRQ_IRQn
+    }
+};
+
+#define UART_0_ISR      (isr_uart0)
+#define UART_1_ISR      (isr_uart1)
+
+#define UART_NUMOF      ARRAY_SIZE(uart_config)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
