34097c10245c478368cf89f51b8571c6863b6088
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2021-06-02 14:49:07 +0200

    net/eui_provider: only use every provider once

--- sys/net/link_layer/eui_provider/eui_provider.c
+++ sys/net/link_layer/eui_provider/eui_provider.c
@@ -18,11 +18,26 @@
 #include "luid.h"
 #include "net/eui_provider.h"
 
+struct netdev_id {
+    uint8_t type;
+    uint8_t index;
+};
+
 void netdev_eui48_get(netdev_t *netdev, eui48_t *addr)
 {
+    __attribute__((unused))
+    static struct netdev_id used[EUI48_PROVIDER_NUMOF];
     unsigned i = EUI48_PROVIDER_NUMOF;
+
     while (i--) {
 #ifdef MODULE_NETDEV_REGISTER
+        /* EUI provider already used */
+        if (used[i].type  != NETDEV_ANY &&
+           (used[i].type  != netdev->type ||
+            used[i].index != netdev->index)) {
+            continue;
+        }
+
         if (eui48_conf[i].type != netdev->type &&
             eui48_conf[i].type != NETDEV_ANY) {
             continue;
@@ -36,6 +51,10 @@ void netdev_eui48_get(netdev_t *netdev, eui48_t *addr)
         (void) netdev;
 #endif
         if (eui48_conf[i].provider(i, addr) == 0) {
+#ifdef MODULE_NETDEV_REGISTER
+            used[i].type  = netdev->type;
+            used[i].index = netdev->index;
+#endif
             return;
         }
     }
@@ -45,9 +64,19 @@ void netdev_eui48_get(netdev_t *netdev, eui48_t *addr)
 
 void netdev_eui64_get(netdev_t *netdev, eui64_t *addr)
 {
+    __attribute__((unused))
+    static struct netdev_id used[EUI64_PROVIDER_NUMOF];
     unsigned i = EUI64_PROVIDER_NUMOF;
+
     while (i--) {
 #ifdef MODULE_NETDEV_REGISTER
+        /* EUI provider already used */
+        if (used[i].type  != NETDEV_ANY &&
+           (used[i].type  != netdev->type ||
+            used[i].index != netdev->index)) {
+            continue;
+        }
+
         if (eui64_conf[i].type != netdev->type &&
             eui64_conf[i].type != NETDEV_ANY) {
             continue;
@@ -61,6 +90,10 @@ void netdev_eui64_get(netdev_t *netdev, eui64_t *addr)
         (void) netdev;
 #endif
         if (eui64_conf[i].provider(i, addr) == 0) {
+#ifdef MODULE_NETDEV_REGISTER
+            used[i].type  = netdev->type;
+            used[i].index = netdev->index;
+#endif
             return;
         }
     }
