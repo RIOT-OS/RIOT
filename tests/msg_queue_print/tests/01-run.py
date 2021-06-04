0a3c9b694b500de0f3256cbb2abfd6f3cc37eb99
Francisco Molina <femolina@uc.cl>
2021-06-04 09:50:19 +0200

    tests/msg_queue_print: fix test on hw

--- tests/msg_queue_print/tests/01-run.py
+++ tests/msg_queue_print/tests/01-run.py
@@ -8,17 +8,21 @@
 
 # @author      Julian Holzwarth <julian.holzwarth@fu-berlin.de>
 
+import os
 import sys
 from testrunner import run
 
 
 def testfunc(child):
     child.expect("No message queue")
-    child.expect("Message queue of thread 2")
+    child.expect(r"Message queue of thread \d+\r\n")
     child.expect_exact('size: 8 (avail: 0)')
-    child.expect("Message queue of thread 2")
+    child.expect(r"Message queue of thread \d+\r\n")
     child.expect_exact('size: 8 (avail: 8)')
-    child.expect_exact('type: 0x0000, content: 0 ((nil))')
+    if os.environ.get('BOARD') == 'native':
+        child.expect_exact('type: 0x0000, content: 0 ((nil))')
+    else:
+        child.expect(r'type: 0x0000, content: 0 \((0x)?0+\)')
     child.expect_exact('type: 0x0001, content: 1 (0x1)')
     child.expect_exact('type: 0x0002, content: 2 (0x2)')
     child.expect_exact('type: 0x0003, content: 3 (0x3)')
