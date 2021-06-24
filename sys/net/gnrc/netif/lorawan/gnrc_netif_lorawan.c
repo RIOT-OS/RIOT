5808a16f23ee56eb5e6bc4a3d320427f67f1f1c9
Jose Alamos <jose@alamos.cc>
2021-06-24 15:21:05 +0200

    gnrc_netif/lorawan: drop netif header on send

--- sys/net/gnrc/netif/lorawan/gnrc_netif_lorawan.c
+++ sys/net/gnrc/netif/lorawan/gnrc_netif_lorawan.c
@@ -311,12 +311,15 @@ static int _send(gnrc_netif_t *netif, gnrc_pktsnip_t *payload)
         dst = gnrc_netif_hdr_get_dst_addr(netif_hdr);
 
         assert(payload->type == GNRC_NETTYPE_NETIF);
-        head = payload->next;
         port = dst[0];
 
         if (netif_hdr->dst_l2addr_len != sizeof(port)) {
             goto end;
         }
+
+        /* Remove the netif hdr snip and point to the MSDU */
+        head = gnrc_pktbuf_remove_snip(payload, payload);
+
     }
     else {
         head = payload;
