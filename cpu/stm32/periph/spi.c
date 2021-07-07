d3aa6ca00bbaf1c7be6c7c2f6ac3db4fd7dddf4a
Akshai M <akshai.m@fu-berlin.de>
2021-07-07 17:24:35 +0200

    stm32/periph/spi : Add check for GPIO_UNDEF

--- cpu/stm32/periph/spi.c
+++ cpu/stm32/periph/spi.c
@@ -130,16 +130,33 @@ void spi_init(spi_t bus)
 void spi_init_pins(spi_t bus)
 {
 #ifdef CPU_FAM_STM32F1
-    gpio_init_af(spi_config[bus].sclk_pin, GPIO_AF_OUT_PP);
-    gpio_init_af(spi_config[bus].mosi_pin, GPIO_AF_OUT_PP);
-    gpio_init(spi_config[bus].miso_pin, GPIO_IN);
+
+    if (gpio_is_valid(spi_config[bus].sclk_pin)) {
+        gpio_init_af(spi_config[bus].sclk_pin, GPIO_AF_OUT_PP);
+    }
+
+    if (gpio_is_valid(spi_config[bus].mosi_pin)) {
+        gpio_init_af(spi_config[bus].mosi_pin, GPIO_AF_OUT_PP);
+    }
+
+    if (gpio_is_valid(spi_config[bus].miso_pin)) {
+        gpio_init(spi_config[bus].miso_pin, GPIO_IN);
+    }
 #else
-    gpio_init(spi_config[bus].mosi_pin, GPIO_OUT);
-    gpio_init(spi_config[bus].miso_pin, GPIO_IN);
-    gpio_init(spi_config[bus].sclk_pin, GPIO_OUT);
-    gpio_init_af(spi_config[bus].mosi_pin, spi_config[bus].mosi_af);
-    gpio_init_af(spi_config[bus].miso_pin, spi_config[bus].miso_af);
-    gpio_init_af(spi_config[bus].sclk_pin, spi_config[bus].sclk_af);
+    if (gpio_is_valid(spi_config[bus].mosi_pin)) {
+        gpio_init(spi_config[bus].mosi_pin, GPIO_OUT);
+        gpio_init_af(spi_config[bus].mosi_pin, spi_config[bus].mosi_af);
+    }
+
+    if (gpio_is_valid(spi_config[bus].miso_pin)) {
+        gpio_init(spi_config[bus].miso_pin, GPIO_IN);
+        gpio_init_af(spi_config[bus].miso_pin, spi_config[bus].miso_af);
+    }
+
+    if (gpio_is_valid(spi_config[bus].sclk_pin)) {
+        gpio_init(spi_config[bus].sclk_pin, GPIO_OUT);
+        gpio_init_af(spi_config[bus].sclk_pin, spi_config[bus].sclk_af);
+    }
 #endif
 }
 
@@ -183,12 +200,20 @@ int spi_init_with_gpio_mode(spi_t bus, spi_gpio_mode_t mode)
     /* This has no effect on STM32F1 */
     return ret;
 #else
-    ret += gpio_init(spi_config[bus].mosi_pin, mode.mosi);
-    ret += gpio_init(spi_config[bus].miso_pin, mode.miso);
-    ret += gpio_init(spi_config[bus].sclk_pin, mode.sclk);
-    gpio_init_af(spi_config[bus].mosi_pin, spi_config[bus].mosi_af);
-    gpio_init_af(spi_config[bus].miso_pin, spi_config[bus].miso_af);
-    gpio_init_af(spi_config[bus].sclk_pin, spi_config[bus].sclk_af);
+    if (gpio_is_valid(spi_config[bus].mosi_pin)) {
+        ret += gpio_init(spi_config[bus].mosi_pin, mode.mosi);
+        gpio_init_af(spi_config[bus].mosi_pin, spi_config[bus].mosi_af);
+    }
+
+    if (gpio_is_valid(spi_config[bus].miso_pin)) {
+        ret += gpio_init(spi_config[bus].miso_pin, mode.miso);
+        gpio_init_af(spi_config[bus].miso_pin, spi_config[bus].miso_af);
+    }
+
+    if (gpio_is_valid(spi_config[bus].sclk_pin)) {
+        ret += gpio_init(spi_config[bus].sclk_pin, mode.sclk);
+        gpio_init_af(spi_config[bus].sclk_pin, spi_config[bus].sclk_af);
+    }
     return ret;
 #endif
 }
