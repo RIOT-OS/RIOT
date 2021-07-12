fa28e7cbf6b7f826261045ac7fedf181f28c252b
Akshai M <akshai.m@fu-berlin.de>
2021-07-12 18:16:15 +0200

    fixup! fixup! fixup! fixup! fixup! Move ADR_ACK_CNT to MLME - To be squashed

--- sys/net/gnrc/link_layer/lorawan/gnrc_lorawan_mcps.c
+++ sys/net/gnrc/link_layer/lorawan/gnrc_lorawan_mcps.c
@@ -258,9 +258,9 @@ size_t gnrc_lorawan_build_uplink(gnrc_lorawan_t *mac, iolist_t *payload,
     lorawan_hdr_set_adr(lw_hdr,mac->mlme.adr);
 
     /* Set `ADRACKReq` bit and ADR_ACK_CNT */
-    if (mac->mlme.adr && mac->last_dr) {
-        lorawan_hdr_set_adr_ack_req(lw_hdr,
-                                    mac->mlme.adr_ack_cnt >= CONFIG_LORAMAC_DEFAULT_ADR_ACK_LIMIT);
+    if (mac->mlme.adr && mac->last_dr && \
+        (mac->mlme.adr_ack_cnt >= CONFIG_LORAMAC_DEFAULT_ADR_ACK_LIMIT)) {
+        lorawan_hdr_set_adr_ack_req(lw_hdr, true);
     }
 
     lorawan_hdr_set_ack(lw_hdr, mac->mcps.ack_requested);
