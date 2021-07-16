f4004d3bb10b0a4f2fe9062def03c70be6506273
Jose Alamos <jose@alamos.cc>
2021-07-16 18:30:08 +0200

    unittests/gnrc_lorawan: add initial unittests

---
+++ tests/unittests/tests-gnrc_lorawan/tests-gnrc_lorawan.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the ``gnrc_lorawan`` module
+ *
+ * @author      José I. Álamos <jose.alamos@haw-hamburg.de>
+ */
+#ifndef TESTS_GNRC_LORAWAN_H
+#define TESTS_GNRC_LORAWAN_H
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_gnrc_lorawan(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_GNRC_LORAWAN_H */
