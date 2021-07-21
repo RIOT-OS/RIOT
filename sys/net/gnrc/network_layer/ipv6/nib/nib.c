ac0b36384f1407a41516e0930e6a72d035950dd4
Benjamin Valentin <benpicco@beuth-hochschule.de>
2021-07-21 23:32:57 +0200

    gnrc_ipv6_nib: improve debug output

--- sys/net/gnrc/network_layer/ipv6/nib/nib.c
+++ sys/net/gnrc/network_layer/ipv6/nib/nib.c
@@ -221,8 +221,9 @@ int gnrc_ipv6_nib_get_next_hop_l2addr(const ipv6_addr_t *dst,
         unsigned iface = (node == NULL) ? 0 : _nib_onl_get_if(node);
 
         if ((node != NULL) || _on_link(dst, &iface)) {
-            DEBUG("nib: %s is on-link or in NC, start address resolution\n",
-                  ipv6_addr_to_str(addr_str, dst, sizeof(addr_str)));
+            DEBUG("nib: %s is %s, start address resolution\n",
+                  ipv6_addr_to_str(addr_str, dst, sizeof(addr_str)),
+                  node ? "in NC" : "on-link");
             /* on-link prefixes return their interface */
             if (!ipv6_addr_is_link_local(dst) && (iface != 0)) {
                 /* release pre-assumed netif */
