f2995240d4295a5f6bab82591f7e76a0a7d84b6a
Francisco Molina <femolina@uc.cl>
2021-07-21 11:23:47 +0200

    cpu/stm32: handle parsing stm32wlex CPU_MODELs

--- cpu/stm32/stm32_info.mk
+++ cpu/stm32/stm32_info.mk
@@ -12,15 +12,20 @@ STM32_INFO     := $(shell echo $(CPU_MODEL_UPPERCASE) | sed -E -e 's/^STM32(F|L|
 STM32_TYPE     = $(word 1, $(STM32_INFO))
 STM32_FAMILY   = $(word 2, $(STM32_INFO))
 STM32_MODEL    = $(word 3, $(STM32_INFO))
-ifneq (MP, $(STM32_TYPE))
+ifneq (,$(filter W,$(STM32_TYPE)))
+  STM32_MODEL2   = $(word 4, $(STM32_INFO))
+  STM32_MODEL3   = $(word 5, $(STM32_INFO))
+  STM32_PINCOUNT = $(word 6, $(STM32_INFO))
+  STM32_ROMSIZE  = $(word 7, $(STM32_INFO))
+else ifneq (,$(filter MP,$(STM32_TYPE)))
+  STM32_MODEL2   = $(word 6, $(STM32_INFO))
+  STM32_PINCOUNT = $(word 7, $(STM32_INFO))$(word 8, $(STM32_INFO))
+else
   STM32_MODEL2   = $(word 4, $(STM32_INFO))
   STM32_MODEL3   = $(word 5, $(STM32_INFO))
   STM32_PINCOUNT = $(word 6, $(STM32_INFO))
   STM32_ROMSIZE  = $(word 7, $(STM32_INFO))
   STM32_RAMMOD   = $(word 8, $(STM32_INFO))
-else
-  STM32_MODEL2   = $(word 6, $(STM32_INFO))
-  STM32_PINCOUNT = $(word 7, $(STM32_INFO))$(word 8, $(STM32_INFO))
 endif
 
 CPU_FAM = $(call lowercase,$(STM32_TYPE)$(STM32_FAMILY))
