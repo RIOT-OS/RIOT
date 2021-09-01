ced32680839194371de938b2c08bc459d1e529d6
Martine Lenders <m.lenders@fu-berlin.de>
2021-09-01 11:54:51 +0200

    tests/gnrc_dhcpv6_client: remove sudo requirement

--- tests/gnrc_dhcpv6_client/dhcpv6_server.sh
+++ tests/gnrc_dhcpv6_client/dhcpv6_server.sh
@@ -6,11 +6,6 @@
 # Distributed under terms of the MIT license.
 #
 
-if [ "$EUID" -ne 0 ]; then
-  echo -e "\033[31;1mRequire root since kea uses some PID and Lock files in /var\033[0m" >&2
-  exit 1
-fi
-
 if ! command -v kea-dhcp6; then
   echo -e "\033[31;1mCommand kea-dhcp6 required\033[0m" >&2
   exit 1
@@ -56,7 +51,8 @@ EOF
         python3 -c "$convert_json"
     fi
     sleep 1 # sleep to let TAP become active
-    KEA_PIDFILE_DIR=${TMPDIR} KEA_LOCKFILE_DIR=${TMPDIR} \
+    cd "$TMPDIR" || exit 1
+    KEA_PIDFILE_DIR=. KEA_LOCKFILE_DIR=. \
         kea-dhcp6 -p "$1" -c "$config"
 }
 
