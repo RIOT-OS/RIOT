7d084a43ff5a1776b26a03c99c81fb47678e4b7c
Francisco Molina <femolina@uc.cl>
2021-09-06 10:57:52 +0200

    sys/include/event/periodic: add count

--- sys/include/event/periodic.h
+++ sys/include/event/periodic.h
@@ -44,16 +44,24 @@ extern "C" {
 #endif
 
 /**
+ * @brief Run the periodic event forever
+ */
+#define EVENT_PERIODIC_FOREVER      0
+
+/**
  * @brief   Timeout Event structure
  */
 typedef struct {
     ztimer_periodic_t timer;    /**< ztimer object used for timeout */
     event_queue_t *queue;       /**< event queue to post event to */
     event_t *event;             /**< event to post after timeout */
+    uint32_t count;             /**< times the event should repeat itself */
 } event_periodic_t;
 
 /**
- * @brief    Initialize a periodic event timeout
+ * @brief   Initialize a periodic event timeout
+ *
+ * @note: On init the periodic event is to to run forever.
  *
  * @param[in]       event_periodic  event_periodic object to initialize
  * @param[in]       clock           the clock to configure this timer on
@@ -73,14 +81,30 @@ void event_periodic_init(event_periodic_t *event_periodic, ztimer_clock_t *clock
  * @note: the used event_periodic struct must stay valid until after the timeout
  *        event has been processed!
  *
+ * @note: this function does not touch the current count value.
+ *
  * @param[in]   event_periodic   event_timout context object to use
  * @param[in]   interval         period length for the event
  */
-static inline void event_periodic_start(event_periodic_t *event_periodic,
-                                        uint32_t interval)
+static inline void event_periodic_start(event_periodic_t *event_periodic, uint32_t interval)
 {
     event_periodic->timer.interval = interval;
     ztimer_periodic_start(&event_periodic->timer);
+}
+
+/**
+ * @brief   Set the amount of times the periodic event should repeat itself.
+ *
+ * @param[in]   event_periodic   event_timout context object to use
+ * @param[in]   count            times the event should repeat itself,
+ *                               EVENT_PERIODIC_FOREVER to run for ever.
+ */
+static inline void event_periodic_set_count(event_periodic_t *event_periodic, uint32_t count)
+{
+    unsigned state = irq_disable();
+
+    event_periodic->count = count;
+    irq_restore(state);
 }
 
 /**
@@ -90,6 +114,10 @@ static inline void event_periodic_start(event_periodic_t *event_periodic,
  * timer. If the timer has already fired before calling this function, the
  * connected event will be put already into the given event queue and this
  * function does not have any effect.
+ *
+ * @note Calling this function does not touch event_periodic->count, if the
+ * periodic event was not set to run for ever and did run until expiration,
+ * then count will be != 0 after this function is called.
  *
  * @param[in]   event_periodic  event_periodic_timeout context object to use
  */
