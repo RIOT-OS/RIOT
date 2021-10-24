8da267de72e369a2f3a8a78a897ec68d8d994082
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2021-10-24 13:21:09 +0200

    sys/bitfield: add bf_find_first_{set, unset}()
    At functions to get the first (un)set bit in a bitfield.

--- sys/bitfield/bitfield.c
+++ sys/bitfield/bitfield.c
@@ -13,6 +13,7 @@
  * @brief       Bitfield auxiliary functions
  *
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Benjamin Valentin <benjamin.valentin@ml-pa.com>
  *
  * @}
  */
@@ -21,18 +22,24 @@
 #include "bitfield.h"
 #include "irq.h"
 
+static inline unsigned _skip_bytes(const uint8_t field[], unsigned nbytes, uint8_t byte)
+{
+    unsigned i = 0;
+    for (unsigned j = 0; (j < nbytes) && (field[j] == byte); j++) {
+        i += 8;
+    }
+    return i;
+}
+
 int bf_get_unset(uint8_t field[], size_t size)
 {
     int result = -1;
-    int nbytes = (size + 7) / 8;
-    size_t i = 0;
+    unsigned nbytes = (size + 7) / 8;
 
     unsigned state = irq_disable();
 
     /* skip full bytes */
-    for (int j = 0; (j < nbytes) && (field[j] == 255); j++) {
-        i += 8;
-    }
+    unsigned i = _skip_bytes(field, nbytes, 0xff);
 
     for (; i < size; i++) {
         if (!bf_isset(field, i)) {
@@ -44,4 +51,32 @@ int bf_get_unset(uint8_t field[], size_t size)
 
     irq_restore(state);
     return result;
+}
+
+int bf_find_first_set(const uint8_t field[], size_t size)
+{
+    unsigned nbytes = (size + 7) / 8;
+    unsigned i = _skip_bytes(field, nbytes, 0);
+
+    for (; i < size; i++) {
+        if (bf_isset(field, i)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int bf_find_first_unset(const uint8_t field[], size_t size)
+{
+    unsigned nbytes = (size + 7) / 8;
+    unsigned i = _skip_bytes(field, nbytes, 0xff);
+
+    for (; i < size; i++) {
+        if (!bf_isset(field, i)) {
+            return i;
+        }
+    }
+
+    return -1;
 }
