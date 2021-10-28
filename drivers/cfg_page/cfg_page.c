908898ee8f8ae5f87401ecee22d0ed85b976a1d6
Michael Richardson <mcr@sandelman.ca>
2021-10-28 15:00:04 -0400

    cfg_page: deal with keys that are bigger than one byte

--- drivers/cfg_page/cfg_page.c
+++ drivers/cfg_page/cfg_page.c
@@ -275,12 +275,29 @@ int cfg_page_get_value(cfg_page_desc_t *cpd, uint32_t wantedkey, nanocbor_value_
 
 static void _cfg_page_splat_key(nanocbor_value_t okey1, int keysize)
 {
-  *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT;
+  switch(keysize) {
+  case 1:
+    *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT;
+    break;
+
+  case 1+2:
+    *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT+NANOCBOR_SIZE_BYTE;
+    break;
+
+  case 1+4:
+    *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT+NANOCBOR_SIZE_SHORT;
+    break;
+
+  case 1+8:
+    *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT+NANOCBOR_SIZE_WORD;
+    break;
+
+  case 1+16:
+    *((uint8_t *)okey1.cur) = NANOCBOR_TYPE_UINT+NANOCBOR_SIZE_WORD;
+    break;
+  }
   if(keysize > 0) {
-    /*
-     * XXX this is not right.  It needs to make a TYPE_UINT which is
-     * multiple bytes
-     */
+    /* make a zero of a bigger size */
     int i;
     for(i=1; i < keysize; i++) {
       ((uint8_t *)okey1.cur)[i] = 0;
