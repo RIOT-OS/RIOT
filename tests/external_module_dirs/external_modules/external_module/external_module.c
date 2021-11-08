798e04ef7ca0556eb54316dc2c70f5ed865bbfd4
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2021-11-08 12:43:20 +0100

    tests/external_module_dirs: add auto-init function

--- tests/external_module_dirs/external_modules/external_module/external_module.c
+++ tests/external_module_dirs/external_modules/external_module/external_module.c
@@ -20,5 +20,19 @@
  */
 
 #include "external_module.h"
+#include "auto_init_priorities.h"
 
+#define AUTO_INIT_PRIO_EXTERNAL_MODULE                          \
+        AUTO_INIT_PRIO_ADD_AFTER_MODULE(AUTO_INIT_PRIO_GP_RNG,  \
+                                        AUTO_INIT_PRIO_RANDOM,  \
+                                        AUTO_INIT_PRIO_1)
+
+AUTO_INIT_MODULE(external_module, AUTO_INIT_PRIO_EXTERNAL_MODULE, auto_init_external_module);
+
+bool external_module_initialized = false;
 char *external_module_message = "Linking worked";
+
+void auto_init_external_module(void)
+{
+    external_module_initialized = true;
+}
