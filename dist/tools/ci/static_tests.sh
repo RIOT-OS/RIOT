d9387fd82d44b419dd86457ad2fbe213ea23d8c2
Benjamin Valentin <benpicco@beuth-hochschule.de>
2021-11-28 21:43:34 +0100

    static-tests: disable cppcheck
    cppcheck produces too many false positives to be useful.
    This is likely due to a configuration error (not all header files are included?)
    but until this is sorted out, disable the tool as it currently does more harm
    than good.

--- dist/tools/ci/static_tests.sh
+++ dist/tools/ci/static_tests.sh
@@ -116,7 +116,9 @@ DIFFFILTER="MR" ERROR_EXIT_CODE=0 run ./dist/tools/licenses/check.sh
 DIFFFILTER="AC" run ./dist/tools/licenses/check.sh
 run ./dist/tools/doccheck/check.sh
 run ./dist/tools/externc/check.sh
-run ./dist/tools/cppcheck/check.sh
+# broken configuration produces many false positives
+# TODO: fix config and re-enable
+# run ./dist/tools/cppcheck/check.sh
 run ./dist/tools/vera++/check.sh
 run ./dist/tools/coccinelle/check.sh
 run ./dist/tools/flake8/check.sh
