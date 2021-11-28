afb5e043bef006df8ff57eb7eeed03313669cb19
Gunar Schorcht <gunar@schorcht.net>
2021-11-28 12:22:00 +0100

    drivers/vl6180x: add SAUL integration

--- drivers/saul/init_devs/init.c
+++ drivers/saul/init_devs/init.c
@@ -335,4 +335,8 @@ void saul_init_devs(void)
         extern void auto_init_veml6070(void);
         auto_init_veml6070();
     }
+    if (IS_USED(MODULE_VL6180X)) {
+        extern void auto_init_vl6180x(void);
+        auto_init_vl6180x();
+    }
 }
