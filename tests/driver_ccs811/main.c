23724816bee1ee868be3cd7447353506a800f14a
Gunar Schorcht <gunar@schorcht.net>
2021-12-04 13:43:03 +0100

    tests/drivers_ccs811: small cleanups in documentation

--- tests/driver_ccs811/main.c
+++ tests/driver_ccs811/main.c
@@ -14,12 +14,12 @@
  *
  * The test application demonstrates the use of the CCS811 using
  *
- * - data-ready status function ```ccs811_data_ready``` to wait for
+ * - data-ready status function `ccs811_data_ready` to wait for
  *   new data and
  * - default configuration parameters, that is, the measurement mode
- *   ```CCS811_MODE_1S``` with one measurement per second.
+ *   `CCS811_MODE_1S` with one measurement per second.
  *
- * Please refer ```$(RIOTBASE)/tests/driver_ccs811_full``` to learn how
+ * Please refer `$(RIOTBASE)/tests/driver_ccs811_full` to learn how
  * to use the CCS811 with interrupts.
  */
 
