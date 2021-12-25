332f97098780021d0258aa3894ced6795f6b4d3a
Gunar Schorcht <gunar@schorcht.net>
2021-12-25 13:32:29 +0100

    boards/feather-m0: use UART_UNDEF to enable Serial over STDIO

--- boards/feather-m0/include/arduino_board.h
+++ boards/feather-m0/include/arduino_board.h
@@ -32,6 +32,13 @@ extern "C" {
 #define ARDUINO_LED         (13)
 
 /**
+ * @brief   On-board serial port mapping, stdio is used for Serial
+ */
+#ifndef ARDUINO_UART_DEV
+#define ARDUINO_UART_DEV    UART_UNDEF
+#endif
+
+/**
  * @brief   Look-up table for the Arduino's digital pins
  */
 static const gpio_t arduino_pinmap[] = {
