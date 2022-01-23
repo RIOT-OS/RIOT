40efa06eb474229d15dd01c20c230fe78d909587
chrysn <chrysn@fsfe.org>
2022-01-23 15:56:35 +0100

    examples/rust-gcoap: Add CoAP SAUL demo from module examples

--- examples/rust-gcoap/src/lib.rs
+++ examples/rust-gcoap/src/lib.rs
@@ -22,6 +22,7 @@ fn main() {
     let handler = coap_message_demos::full_application_tree(None)
         .below(&["ps"], riot_coap_handler_demos::ps::ps_tree())
         .below(&["vfs"], riot_coap_handler_demos::vfs::vfs("/const"))
+        .below(&["saul"], riot_coap_handler_demos::saul::SaulHandler::new(&["saul"]))
         .with_wkc()
         ;
     let mut handler = riot_wrappers::coap_handler::GcoapHandler(handler);
