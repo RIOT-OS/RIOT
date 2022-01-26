3f95f39d885d3104e3224aa8ebf0b599ec4433f1
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2022-01-26 16:37:48 +0100

    sys/net/sock_util: add sock_{udp,tcp}_name2ep() wrapper

--- sys/include/net/sock/util.h
+++ sys/include/net/sock/util.h
@@ -150,6 +150,25 @@ static inline int sock_tcp_str2ep(sock_tcp_ep_t *ep_out, const char *str)
 }
 
 /**
+ * @brief    Convert string to TCP endpoint
+ *           If the `sock_dns` module is used, this will do a DNS lookup
+ *           if @p str is not an IP address.
+ *
+ * Takes eg., "exampl.com:80" or "[2001:db8::1]:1234" and converts it into
+ * the corresponding UDP endpoint structure.
+ *
+ * @param[out]  ep_out  endpoint structure to fill
+ * @param[in]   str     string to read from
+ *
+ * @returns     0 on success
+ * @returns     <0 otherwise
+ */
+static inline int sock_tcp_name2ep(sock_tcp_ep_t *ep_out, const char *str)
+{
+    return sock_tl_name2ep(ep_out, str);
+}
+
+/**
  * @brief    Convert string to UDP endpoint
  *
  * Takes eg., "[2001:db8::1]:1234" and converts it into the corresponding UDP
@@ -164,6 +183,25 @@ static inline int sock_tcp_str2ep(sock_tcp_ep_t *ep_out, const char *str)
 static inline int sock_udp_str2ep(sock_udp_ep_t *ep_out, const char *str)
 {
     return sock_tl_str2ep(ep_out, str);
+}
+
+/**
+ * @brief    Convert string to UDP endpoint
+ *           If the `sock_dns` module is used, this will do a DNS lookup
+ *           if @p str is not an IP address.
+ *
+ * Takes eg., "exampl.com:80" or "[2001:db8::1]:1234" and converts it into
+ * the corresponding UDP endpoint structure.
+ *
+ * @param[out]  ep_out  endpoint structure to fill
+ * @param[in]   str     string to read from
+ *
+ * @returns     0 on success
+ * @returns     <0 otherwise
+ */
+static inline int sock_udp_name2ep(sock_udp_ep_t *ep_out, const char *str)
+{
+    return sock_tl_name2ep(ep_out, str);
 }
 
 /**
