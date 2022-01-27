1e8209f80ebe8812471cdc8db5c48ca77052232e
chrysn <chrysn@fsfe.org>
2022-01-27 11:14:21 +0100

    pseudomodules: Documentation for board_software_reset

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -22,7 +22,17 @@
 
 PSEUDOMODULES += atomic_utils
 PSEUDOMODULES += base64url
+
+## @defgroup pseudomodule_board_software_reset board_software_reset
+## @brief Use any software-only reset button on the board to reboot
+##
+## Some boards have reset buttons that are not wired to the MCU's reset line,
+## but merely are configured to cause a reset by convention.
+##
+## If this module is active, the button will be configured thusly (and then not
+## be advertised in any other capacity, e.g. through @ref sys_auto_init_saul).
 PSEUDOMODULES += board_software_reset
+
 PSEUDOMODULES += can_mbox
 PSEUDOMODULES += can_pm
 PSEUDOMODULES += can_raw
