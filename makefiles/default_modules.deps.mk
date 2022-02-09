25e670e263635c107c41454d140463a811516035
Francisco Molina <femolina@uc.cl>
2022-02-09 14:25:04 +0100

    makefiles/dependency_resolution: add outer loop for DEFAULT_MODULE deps
    Some DEFAULT_MODULEs in tree have dependencies of their own. This is
    usually done for aliases of init modules, and do not have dependencies
    themselves.
    
    This adds the final stage to dependency resolutions where DEFAULT_MODULEs
    dependencies MAY be included. These included modules MUST NOT have
    dependencies themselves.
    
    This allows for modules to disable DEFAULT_MODULEs during dependencies
    resolution independent of the inclusion order.
    
    auto_init and periph_init modules are moved to this outer-loop, allowing
    therefore for modules to disable them during the dependency resolution

---
+++ makefiles/default_modules.deps.mk
@@ -0,0 +1,3 @@
+# This files contains dependencies for default modules. They are parsed at the
+# end of the dependency loop. They MAY inlcude new modules, but this modules
+# MUST NOT have dependencies themselfs.
