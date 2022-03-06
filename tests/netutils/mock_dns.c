ffd93c9b1bda55345f86bbdc9ec8678a9ab0130d
Hendrik van Essen <hendrik.ve@fu-berlin.de>
2022-03-06 15:41:57 +0100

    sys/net/netutils: add netutils_get_ipv4()

--- tests/netutils/mock_dns.c
+++ tests/netutils/mock_dns.c
@@ -17,26 +17,33 @@
 #include <string.h>
 
 #include "net/af.h"
+#include "net/ipv4/addr.h"
 #include "net/ipv6/addr.h"
 #include "net/sock/dns.h"
 
 int sock_dns_query(const char *domain_name, void *addr_out, int family)
 {
-    const ipv6_addr_t a = { {
+    const ipv4_addr_t addr_ipv4 = { { 0x5d, 0xb8, 0xd8, 0x22 } };
+
+    const ipv6_addr_t addr_ipv6 = { {
             0x26, 0x06, 0x28, 0x00, 0x02, 0x20, 0x00, 0x01,
             0x02, 0x48, 0x18, 0x93, 0x25, 0xc8, 0x19, 0x46
         }
     };
 
-    if (family != AF_INET6) {
-        return -EAFNOSUPPORT;
-    }
-
     if (strcmp(domain_name, "example.com")) {
         return -ENOTSUP;
     }
 
-    memcpy(addr_out, &a, sizeof(a));
-    return 0;
+    switch (family) {
+    case AF_INET:
+        memcpy(addr_out, &addr_ipv4, sizeof(addr_ipv4));
+        return 0;
+    case AF_INET6:
+        memcpy(addr_out, &addr_ipv6, sizeof(addr_ipv6));
+        return 0;
+    default:
+        return -EAFNOSUPPORT;
+    }
 }
 /** @} */
