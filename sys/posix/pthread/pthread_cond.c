0fa155150062fe8af0b688bb40b068930eec6899
Francisco Molina <femolina@uc.cl>
2022-03-08 09:18:06 +0100

    sys/posix: convert to ztimer64_usec

--- sys/posix/pthread/pthread_cond.c
+++ sys/posix/pthread/pthread_cond.c
@@ -22,7 +22,8 @@
 
 #include "pthread_cond.h"
 #include "thread.h"
-#include "xtimer.h"
+#include "ztimer64.h"
+#include "timex.h"
 #include "sched.h"
 #include "irq.h"
 #include "debug.h"
@@ -126,17 +127,17 @@ int pthread_cond_timedwait(pthread_cond_t *cond, mutex_t *mutex, const struct ti
         return EINVAL;
     }
 
-    uint64_t now = xtimer_now_usec64();
+    uint64_t now = ztimer64_now(ZTIMER64_USEC);
     uint64_t then = ((uint64_t)abstime->tv_sec * US_PER_SEC) +
                     (abstime->tv_nsec / NS_PER_US);
 
     int ret = 0;
     if (then > now) {
-        xtimer_t timer;
+        ztimer64_t timer;
         priority_queue_node_t n;
 
         _init_cond_wait(cond, &n);
-        xtimer_set_wakeup64(&timer, (then - now), thread_getpid());
+        ztimer64_set_wakeup(ZTIMER64_USEC, &timer, (then - now), thread_getpid());
 
         mutex_unlock_and_sleep(mutex);
 
@@ -148,7 +149,7 @@ int pthread_cond_timedwait(pthread_cond_t *cond, mutex_t *mutex, const struct ti
             irq_restore(old_state);
             ret = ETIMEDOUT;
         }
-        xtimer_remove(&timer);
+        ztimer64_remove(ZTIMER64_USEC, &timer);
     }
     else {
         mutex_unlock(mutex);
