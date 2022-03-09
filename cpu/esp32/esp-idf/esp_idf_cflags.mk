9282e055ae32cedb5944b6b803f6c51efbbd0b36
Gunar Schorcht <gunar@schorcht.net>
2022-03-09 16:49:50 +0100

    cpu/esp32: ESP32 variant independent makefiles

--- cpu/esp32/esp-idf/esp_idf_cflags.mk
+++ cpu/esp32/esp-idf/esp_idf_cflags.mk
@@ -16,11 +16,27 @@ CFLAGS += -Wno-implicit-function-declaration
 # required for esp_wifi (components/esp_event/esp_event.c)
 CFLAGS += -Wno-old-style-declaration
 
-# required for esp-wifi (components/efuse/esp32/esp_efuse_utility.c)
+# required for esp_wifi (components/efuse/esp32/esp_efuse_utility.c)
 # required for esp_idf_heap (components/heap/multi_heap.c)
 # required for esp_idf_wpa_supplicant
 CFLAGS += -Wno-old-style-definition
 
+# required for esp_idf_common (components/bootloader_support/src/bootloader_flash.c)
+CFLAGS += -Wno-unused-variable
+
+# required for esp_idf_spi_flash (components/spi_flash/partition.c)
+CFLAGS += -Wno-enum-compare
+
 # vendor code contains casts that increase alignment requirements. Let's hope
 # those are false positives.
 CFLAGS += -Wno-cast-align
+
+# additional CFLAGS required for RISC-V architecture
+ifneq (,$(filter riscv32%,$(TARGET_ARCH)))
+  INCLUDES += -I$(ESP32_SDK_DIR)/components/riscv/include
+  CFLAGS += -DCONFIG_IDF_TARGET_ARCH_RISCV
+  CFLAGS += -march=rv32imc
+  CFLAGS += -Wno-error=format=
+  CFLAGS += -nostartfiles
+  CFLAGS += -Wno-format
+endif
