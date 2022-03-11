ee175fb5836482a4867594213ef146e513479ec8
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-03-11 10:48:12 +0100

    cpu/samx21: allow to override #ifndef PM_BLOCKER_INITIAL

--- cpu/saml1x/include/periph_cpu.h
+++ cpu/saml1x/include/periph_cpu.h
@@ -37,7 +37,9 @@ extern "C" {
  * @brief   Override the default initial PM blocker
  * @todo   Idle modes are enabled by default, deep sleep mode blocked
  */
+#ifndef PM_BLOCKER_INITIAL
 #define PM_BLOCKER_INITIAL  0x00000001
+#endif
 
 /**
  * @name   SAML1x GCLK definitions
