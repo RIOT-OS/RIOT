997d5a2b1b0be81a5046203517d1db2cf1b44ce8
Kaspar Schleiser <kaspar@schleiser.de>
2022-03-11 13:09:17 +0100

    tests/unittests/tests-core: make use of XFA accessors

--- tests/unittests/tests-core/tests-core-xfa.c
+++ tests/unittests/tests-core/tests-core-xfa.c
@@ -23,21 +23,63 @@ XFA_USE_CONST(xfatest_t, xfatest_use_const);
 /* Verifying that cross file array linking is correct by iterating over an external array */
 static void test_xfa_data(void)
 {
-    unsigned n = XFA_LEN(xfatest_t, xfatest);
+    unsigned n = xfatest_len();
     TEST_ASSERT_EQUAL_INT(2, n);
     unsigned found = 0;
     for (unsigned k = 0; k < n; ++k) {
         /* we do not want to enforce the order of the data elements */
-        switch (xfatest[k].val) {
+        switch (xfatest_get_copy(k).val) {
             case 12345:
                 /* tests-core-xfa-data1.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest1", xfatest[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest1", xfatest_get_copy(k).text);
                 TEST_ASSERT(!(found & BIT0));
                 found |= BIT0;
                 break;
             case 0xbeef:
                 /* tests-core-xfa-data2.c */
-                TEST_ASSERT_EQUAL_STRING("another test string", xfatest[k].text);
+                TEST_ASSERT_EQUAL_STRING("another test string", xfatest_get_copy(k).text);
+                TEST_ASSERT(!(found & BIT1));
+                found |= BIT1;
+                break;
+            default:
+                break;
+        }
+    }
+    TEST_ASSERT_EQUAL_INT((1U << n) - 1, found);
+}
+
+/* Verifying that rw cross file arrays can be changed */
+static void test_xfa_data_change(void)
+{
+    unsigned n = xfatest_len();
+    TEST_ASSERT_EQUAL_INT(2, n);
+    for (unsigned k = 0; k < n; ++k) {
+        /* we do not want to enforce the order of the data elements */
+        xfatest_t *entry = xfatest_get_ptr(k);
+        switch (entry->val) {
+            case 12345:
+                entry->val = 54321;
+                break;
+            case 0xbeef:
+                entry->val = 0xfeeb;
+                break;
+            default:
+                break;
+        }
+    }
+    unsigned found = 0;
+    for (unsigned k = 0; k < n; ++k) {
+        /* we do not want to enforce the order of the data elements */
+        switch (xfatest_get_copy(k).val) {
+            case 54321:
+                /* tests-core-xfa-data1.c */
+                TEST_ASSERT_EQUAL_STRING("xfatest1", xfatest_get_copy(k).text);
+                TEST_ASSERT(!(found & BIT0));
+                found |= BIT0;
+                break;
+            case 0xfeeb:
+                /* tests-core-xfa-data2.c */
+                TEST_ASSERT_EQUAL_STRING("another test string", xfatest_get_copy(k).text);
                 TEST_ASSERT(!(found & BIT1));
                 found |= BIT1;
                 break;
@@ -55,15 +97,15 @@ static void test_xfa_const_data(void)
     unsigned found = 0;
     for (unsigned k = 0; k < n; ++k) {
         /* we do not want to enforce the order of the data elements */
-        switch (xfatest_const[k].val) {
+        switch (xfatest_const_get_copy(k).val) {
             case 0xcafe:
                 /* tests-core-xfa-data1.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest_const1", xfatest_const[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest_const1", xfatest_const_get_copy(k).text);
                 ++found;
                 break;
             case 32444:
                 /* tests-core-xfa-data2.c */
-                TEST_ASSERT_EQUAL_STRING("const string xfa 2", xfatest_const[k].text);
+                TEST_ASSERT_EQUAL_STRING("const string xfa 2", xfatest_const_get_copy(k).text);
                 ++found;
                 break;
             default:
@@ -80,20 +122,20 @@ static void test_xfa_use_data(void)
     unsigned found = 0;
     for (unsigned k = 0; k < n; ++k) {
         /* we do not want to enforce the order of the data elements */
-        switch (xfatest_use[k].val) {
+        switch (xfatest_use_get_copy(k).val) {
             case 3333:
                 /* tests-core-xfa-data1.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest_use1", xfatest_use[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest_use1", xfatest_use_get_copy(k).text);
                 ++found;
                 break;
             case 555:
                 /* tests-core-xfa-data1.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest use again", xfatest_use[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest use again", xfatest_use_get_copy(k).text);
                 ++found;
                 break;
             case 11111:
                 /* tests-core-xfa-data2.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest_use2", xfatest_use[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest_use2", xfatest_use_get_copy(k).text);
                 ++found;
                 break;
             default:
@@ -110,15 +152,15 @@ static void test_xfa_use_const_data(void)
     unsigned found = 0;
     for (unsigned k = 0; k < n; ++k) {
         /* we do not want to enforce the order of the data elements */
-        switch (xfatest_use_const[k].val) {
+        switch (xfatest_use_const_get_copy(k).val) {
             case 4444:
                 /* tests-core-xfa-data1.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest_use_const1", xfatest_use_const[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest_use_const1", xfatest_use_const_get_copy(k).text);
                 ++found;
                 break;
             case 22222:
                 /* tests-core-xfa-data2.c */
-                TEST_ASSERT_EQUAL_STRING("xfatest_use_const2", xfatest_use_const[k].text);
+                TEST_ASSERT_EQUAL_STRING("xfatest_use_const2", xfatest_use_const_get_copy(k).text);
                 ++found;
                 break;
             default:
@@ -132,6 +174,7 @@ Test *tests_core_xfa_tests(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
         new_TestFixture(test_xfa_data),
+        new_TestFixture(test_xfa_data_change),
         new_TestFixture(test_xfa_const_data),
         new_TestFixture(test_xfa_use_data),
         new_TestFixture(test_xfa_use_const_data),
