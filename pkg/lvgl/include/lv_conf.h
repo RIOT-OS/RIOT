8bc8febe2188f3f2a96bcb1bc085ebe3e4f400a4
Nicole Faerber <nicole.faerber@id3p.com>
2022-03-18 16:00:25 +0100

    Use system/OS malloc/free instead of LVGL internal memory bufer management

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -62,7 +62,7 @@ extern "C" {
  *=========================*/
 
 /*1: use custom malloc/free, 0: use the built-in `lv_mem_alloc()` and `lv_mem_free()`*/
-#define LV_MEM_CUSTOM      0
+#define LV_MEM_CUSTOM      1
 #if LV_MEM_CUSTOM == 0
 /*Size of the memory available for `lv_mem_alloc()` in bytes (>= 2kB)*/
 #if IS_USED(MODULE_LVGL_EXTRA_THEME_DEFAULT_GROW)
