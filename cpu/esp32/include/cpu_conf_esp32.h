97ab0cd76d5497c27de5b52ad86aa0a39a59c155
Gunar Schorcht <gunar@schorcht.net>
2022-03-21 01:57:27 +0100

    cpu/esp32: split cpu_conf.h into ESP32x and ESP32 parts
    Split the CPU configuration in cpu_conf.h into a part that is common for all ESP32x SoCs and a part that is specific for ESP32.

---
+++ cpu/esp32/include/cpu_conf_esp32.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_esp32
+ * @ingroup     config
+ * @brief       Compile-time configuration macros for ESP32 SoCs
+ * @{
+ *
+ * @file
+ * @brief       ESP32 specific compile-time configuration
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ */
+
+#ifndef CPU_CONF_ESP32_H
+#define CPU_CONF_ESP32_H
+
+/** Number of DRAM sections that can be used as heap. */
+#define NUM_HEAPS (4)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CPU_CONF_ESP32_H */
+/** @} */
