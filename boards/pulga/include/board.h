0a0d627791986f113a4f3a408b85311169df25eb
Geovane Fedrecheski <geonnave@gmail.com>
2022-04-06 09:49:26 -0300

    boards/pulga: fix leds and buttons definitions

--- boards/pulga/include/board.h
+++ boards/pulga/include/board.h
@@ -40,12 +40,12 @@ extern "C" {
 #define LED1_MASK           (1 << 14)
 #define LED_MASK            (LED0_MASK | LED1_MASK)
 
-#define LED0_ON             (LED_PORT->OUTCLR = LED0_MASK)
-#define LED0_OFF            (LED_PORT->OUTSET = LED0_MASK)
+#define LED0_OFF            (LED_PORT->OUTCLR = LED0_MASK)
+#define LED0_ON             (LED_PORT->OUTSET = LED0_MASK)
 #define LED0_TOGGLE         (LED_PORT->OUT   ^= LED0_MASK)
 
-#define LED1_ON             (LED_PORT->OUTCLR = LED1_MASK)
-#define LED1_OFF            (LED_PORT->OUTSET = LED1_MASK)
+#define LED1_OFF            (LED_PORT->OUTCLR = LED1_MASK)
+#define LED1_ON             (LED_PORT->OUTSET = LED1_MASK)
 #define LED1_TOGGLE         (LED_PORT->OUT   ^= LED1_MASK)
 /** @} */
 
@@ -55,7 +55,7 @@ extern "C" {
  * A Macronix MX25R6435F is present on the board
  * @{
  */
-/* XXX: comment out since there is no flash anymore in Pulga X V1.0
+/* XXX: comment out since, in Pulga X V1.0, the flash is not routed
 #define NRF52840DK_NOR_PAGE_SIZE          (256)
 #define NRF52840DK_NOR_PAGES_PER_SECTOR   (16)
 #define NRF52840DK_NOR_SECTOR_COUNT       (2048)
@@ -77,9 +77,9 @@ extern mtd_dev_t *mtd0;
  * @name    Button pin configuration
  * @{
  */
-#define BTN0_PIN            GPIO_PIN(1, 11)
+#define BTN0_PIN            GPIO_PIN(1, 12)
 #define BTN0_MODE           GPIO_IN_PU
-#define BTN1_PIN            GPIO_PIN(1, 12)
+#define BTN1_PIN            GPIO_PIN(1, 11)
 #define BTN1_MODE           GPIO_IN_PU
 /** @} */
 
