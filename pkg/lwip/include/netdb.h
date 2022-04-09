17ced9e5569493f4e6852cc291c53c7c1e605659
mariem.charrada <mariem.charrada@ml-pa.com>
2022-04-09 03:07:38 +0200

    pkg/lwip: Adjust LWIP.

---
+++ pkg/lwip/include/netdb.h
@@ -0,0 +1,14 @@
+#ifndef NETDB_H
+#define NETDB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lwip/netdb.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NETDB_H */
---
+++ pkg/lwip/include/netinet/in.h
@@ -0,0 +1,14 @@
+#ifndef NETINET_IN_H
+#define NETINET_IN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lwip/inet.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NETINET_IN_H */
