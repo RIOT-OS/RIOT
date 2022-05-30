d6d5d935a66bcd804f185f569f0b3f6d35b7fd81
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2022-05-30 17:31:06 +0200

    cpu/sam0_common: adc: derive GPIO pin from AIN number
    ADC pins are fixed on sam0

--- cpu/sam0_common/periph/adc.c
+++ cpu/sam0_common/periph/adc.c
@@ -252,9 +252,33 @@ int adc_init(adc_t line)
         return -1;
     }
 
+#ifdef ADC0
+    const uint8_t adc = adc_channels[line].dev == ADC1 ? 1 : 0;
+#else
+    const uint8_t adc = 0;
+#endif
+
     _prep();
-    gpio_init(adc_channels[line].pin, GPIO_IN);
-    gpio_init_mux(adc_channels[line].pin, GPIO_MUX_B);
+
+    uint8_t muxpos = (adc_channels[line].muxpos & ADC_INPUTCTRL_MUXPOS_Msk)
+                   >> ADC_INPUTCTRL_MUXPOS_Pos;
+    uint8_t muxneg = (adc_channels[line].muxpos & ADC_INPUTCTRL_MUXNEG_Msk)
+                   >> ADC_INPUTCTRL_MUXNEG_Pos;
+
+    /* configure positive input pin */
+    if (muxpos < 0x18) {
+        assert(muxpos < ARRAY_SIZE(sam0_adc_pins[adc]));
+        gpio_init(sam0_adc_pins[adc][muxpos], GPIO_IN);
+        gpio_init_mux(sam0_adc_pins[adc][muxpos], GPIO_MUX_B);
+    }
+
+    /* configure negative input pin */
+    if (adc_channels[line].muxpos & ADC_INPUTCTRL_DIFFMODE) {
+        assert(muxneg < ARRAY_SIZE(sam0_adc_pins[adc]));
+        gpio_init(sam0_adc_pins[adc][muxneg], GPIO_IN);
+        gpio_init_mux(sam0_adc_pins[adc][muxneg], GPIO_MUX_B);
+    }
+
     _done();
 
     return 0;
