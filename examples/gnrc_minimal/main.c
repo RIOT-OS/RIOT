b564f957579d82d6c6086d8032fffa0d5e2aeb5c
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2022-06-02 12:38:33 +0200

    examples/gnrc_minimal: use netifs_print_ipv6()

--- examples/gnrc_minimal/main.c
+++ examples/gnrc_minimal/main.c
@@ -24,29 +24,17 @@
 #include "net/ipv6/addr.h"
 #include "net/gnrc.h"
 #include "net/gnrc/netif.h"
+#include "net/netif.h"
 
 int main(void)
 {
 
     puts("RIOT network stack example application");
 
-    /* get interfaces and print their addresses */
-    gnrc_netif_t *netif = NULL;
-    while ((netif = gnrc_netif_iter(netif))) {
-        ipv6_addr_t ipv6_addrs[CONFIG_GNRC_NETIF_IPV6_ADDRS_NUMOF];
-        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
-                                  sizeof(ipv6_addrs));
-
-        if (res < 0) {
-            continue;
-        }
-        for (unsigned i = 0; i < (unsigned)(res / sizeof(ipv6_addr_t)); i++) {
-            char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
-
-            ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
-            printf("My address is %s\n", ipv6_addr);
-        }
-    }
+    /* print all IPv6 addresses */
+    printf("{\"IPv6 addresses\": [\"");
+    netifs_print_ipv6("\", \"");
+    puts("\"]}");
 
     /* main thread exits */
     return 0;
