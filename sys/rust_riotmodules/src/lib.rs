68985aa6be4c1d41516fae0ee3eab9af4860aaf2
chrysn <chrysn@fsfe.org>
2022-06-11 03:07:26 +0200

    rust_riotmodules: pub use instead of extern crate
    See-Also: https://github.com/RIOT-OS/RIOT/pull/16833#issuecomment-1071243210

--- sys/rust_riotmodules/src/lib.rs
+++ sys/rust_riotmodules/src/lib.rs
@@ -1,13 +1,14 @@
 #![no_std]
 
-// As we're pulling all crates in only for their side effects of having symbols (be that to place
-// them in XFA symbols or to make them available for C) all these crates have to be extern-crate'd
-// to be pulled in because they are not used on the laguage level.
+// Crates in here are pub used in case they make anything accessible to Rust applications directly;
+// they can then access the crates as `rust_riotmodules::lsm303agr` or similar.
+//
+// (Also, if they were not pub used, they'd need to be extern crate'd).
 
 // This list should be as auto-generated / -maintained as the one in Cargo.toml
 
 #[cfg(feature = "riot-module-lsm303agr")]
-extern crate riot_module_lsm303agr;
+pub use riot_module_lsm303agr as lsm303agr;
 
 #[cfg(feature = "riot-module-shell-democommands")]
-extern crate riot_module_shell_democommands;
+pub use riot_module_shell_democommands as democommands;
