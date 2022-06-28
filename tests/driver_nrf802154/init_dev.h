37bdc32e30a542bc8276045cc3b2bb643e672b8c
MrKevinWeiss <weiss.kevin604@gmail.com>
2022-06-28 18:21:04 +0200

    tests/driver_nrf802154: Initial driver test

---
+++ tests/driver_nrf802154/init_dev.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2022 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief   Device-specific test header file NRF802154 IEEE 802.15.4 device driver
+ *
+ * @author  Kevin Weiss <kevin.weiss@haw-hamburg.de>
+ */
+#ifndef INIT_DEV_H
+#define INIT_DEV_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NRF802154_NUM                   1
+#define NETDEV_IEEE802154_MINIMAL_NUMOF NRF802154_NUM
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INIT_DEV_H */
+/** @} */
