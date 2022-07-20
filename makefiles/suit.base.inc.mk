363cf8c54662dd32b7e11207e5992724402bbaa7
Daniel Lockau <daniel.lockau@ml-pa.com>
2022-07-20 12:35:33 +0200

    makefiles/suit: avoid unwanted key creation trigger
    This fixes a trigger of re-creation of an already existing SUIT
    key when combining a build target with the clean target, e.g.
    `make clean all`.

--- makefiles/suit.base.inc.mk
+++ makefiles/suit.base.inc.mk
@@ -30,7 +30,7 @@ SUIT_PUB_HDR_DIR = $(dir $(SUIT_PUB_HDR))
 CFLAGS += -I$(SUIT_PUB_HDR_DIR)
 BUILDDEPS += $(SUIT_PUB_HDR)
 
-$(SUIT_SEC): $(CLEAN)
+$(SUIT_SEC): | $(CLEAN)
 	$(Q)echo suit: generating key in $(SUIT_KEY_DIR)
 	$(Q)mkdir -p $(SUIT_KEY_DIR)
 	$(Q)$(RIOTBASE)/dist/tools/suit/gen_key.py $(SUIT_SEC) $(SUIT_SEC_PASSWORD)
