70d427da0b14a183da03e3d4fdde53e5a292751c
Gunar Schorcht <gunar@schorcht.net>
2022-08-01 17:42:14 +0200

    cpu/esp32: move common ESP-IDF configurations to common file

--- cpu/esp32/include/sdkconfig_esp32c3.h
+++ cpu/esp32/include/sdkconfig_esp32c3.h
@@ -11,7 +11,7 @@
  * @{
  *
  * @file
- * @brief       SDK configuration used by the ESP-IDF for ESP32C3
+ * @brief       SDK configuration used by the ESP-IDF for ESP32-C3 SoC variant (family)
  *
  * The SDK configuration can be partially overridden by application-specific
  * board configuration.
@@ -29,7 +29,7 @@ extern "C" {
 #endif
 
 /**
- * @name    Clock configuration
+ * @name ESP32-C3 specific clock configuration
  * @{
  */
 
@@ -51,158 +51,53 @@ extern "C" {
 #endif
 
 /**
- * @brief   Defines the CPU frequency [values = 2, 5, 10, 10, 40, 80, 160]
+ * @brief Defines the CPU frequency [values = 2, 5, 10, 20, 40, 80, 160]
  */
 #ifndef CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ 80
+#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     80
 #endif
+
 /**
- * @brief   Mapping configured ESP32 default clock to CLOCK_CORECLOCK define
+ * @brief Mapping configured ESP32-C3 default clock to CLOCK_CORECLOCK define
  */
 #define CLOCK_CORECLOCK     (1000000UL * CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ)
 /** @} */
 
 /**
- * Default console configuration
- *
- * STDIO_UART_BAUDRATE is used as CONFIG_CONSOLE_UART_BAUDRATE and
- * can be overridden by an application specific configuration.
+ * ESP32-C3 specific RTC clock configuration
  */
-#define CONFIG_CONSOLE_UART_NUM 0
-#define CONFIG_ESP_CONSOLE_UART_NUM     CONFIG_CONSOLE_UART_NUM
-
-#ifndef CONFIG_CONSOLE_UART_BAUDRATE
-#define CONFIG_CONSOLE_UART_BAUDRATE    STDIO_UART_BAUDRATE
-#endif
-
-/**
- * Log output configuration (DO NOT CHANGE)
- */
-#ifndef CONFIG_LOG_DEFAULT_LEVEL
-#define CONFIG_LOG_DEFAULT_LEVEL    LOG_LEVEL
-#endif
-#define CONFIG_LOG_MAXIMUM_LEVEL    LOG_LEVEL
-
-/**
- * RTC clock configuration
- */
-#ifdef MODULE_ESP_RTC_TIMER_32K
-#define CONFIG_ESP32_RTC_CLK_SRC_EXT_CRYS       1
-#endif
-
 #define CONFIG_ESP32C3_RTC_CLK_CAL_CYCLES       (8 * 1024)
 
 /**
- * System specific configuration (DO NOT CHANGE)
+ * ESP32-C3 specific EFUSE configuration
  */
-#ifdef MODULE_NEWLIB_NANO
-#define CONFIG_NEWLIB_NANO_FORMAT               1
-#endif
-
-#define CONFIG_ESP_SYSTEM_CHECK_INT_LEVEL_4     1
-#define CONFIG_ESP_SYSTEM_EVENT_QUEUE_SIZE      32
-#define CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE 2560
-
-#define CONFIG_ESP_TIME_FUNCS_USE_ESP_TIMER     1
-#define CONFIG_ESP_TIMER_IMPL_SYSTIMER          1
-#define CONFIG_ESP_TIMER_INTERRUPT_LEVEL        1
-#define CONFIG_ESP_TIMER_TASK_STACK_SIZE        3584
-#define CONFIG_TIMER_TASK_STACK_SIZE            CONFIG_ESP_TIMER_TASK_STACK_SIZE
-
-#define CONFIG_APP_BUILD_USE_FLASH_SECTIONS     1
-#define CONFIG_APP_BUILD_BOOTLOADER 1
-#define CONFIG_APP_BUILD_GENERATE_BINARIES 1
-#define CONFIG_APP_BUILD_TYPE_APP_2NDBOOT 1
-#define CONFIG_APP_BUILD_USE_FLASH_SECTIONS 1
-
 #define CONFIG_EFUSE_MAX_BLK_LEN                256
 
-#define CONFIG_PARTITION_TABLE_OFFSET           0x8000
-#define CONFIG_PARTITION_TABLE_CUSTOM_FILENAME  "partitions.csv"
-#define CONFIG_PARTITION_TABLE_FILENAME         "partitions_singleapp.csv"
-#define CONFIG_PARTITION_TABLE_SINGLE_APP       1
-
 /**
- * Bluetooth configuration (DO NOT CHANGE)
+ * ESP32-C3 specific MAC configuration
  */
-#define CONFIG_BT_ENABLED                       0
-#define CONFIG_BT_RESERVE_DRAM                  0
-
-/**
- * SPI Flash driver configuration (DO NOT CHANGE)
- */
-#define CONFIG_SPI_FLASH_ROM_DRIVER_PATCH                   1
-#define CONFIG_SPI_FLASH_USE_LEGACY_IMPL                    1
-#define CONFIG_SPI_FLASH_DANGEROUS_WRITE_ABORTS             1
-#define CONFIG_SPI_FLASH_ENABLE_ENCRYPTED_READ_WRITE        1
-#define CONFIG_SPI_FLASH_ERASE_YIELD_DURATION_MS            20
-#define CONFIG_SPI_FLASH_ERASE_YIELD_TICKS                  1
-#define CONFIG_SPI_FLASH_ROM_DRIVER_PATCH                   1
-#define CONFIG_SPI_FLASH_SUPPORT_BOYA_CHIP                  1
-#define CONFIG_SPI_FLASH_SUPPORT_GD_CHIP                    1
-#define CONFIG_SPI_FLASH_SUPPORT_ISSI_CHIP                  1
-#define CONFIG_SPI_FLASH_SUPPORT_MXIC_CHIP                  1
-#define CONFIG_SPI_FLASH_SUPPORT_WINBOND_CHIP               1
-#define CONFIG_SPI_FLASH_WRITE_CHUNK_SIZE                   8192
-#define CONFIG_SPI_FLASH_WRITING_DANGEROUS_REGIONS_ABORTS   CONFIG_SPI_FLASH_DANGEROUS_WRITE_ABORTS
-#define CONFIG_SPI_FLASH_YIELD_DURING_ERASE                 1
-
-/**
- * Serial flasher config (defined by CFLAGS, only sanity check here)
- */
-#if !defined(CONFIG_FLASHMODE_DOUT) && \
-    !defined(CONFIG_FLASHMODE_DIO) && \
-    !defined(CONFIG_FLASHMODE_QOUT) && \
-    !defined(CONFIG_FLASHMODE_QIO)
-#error "Flash mode not configured"
-#endif
-
-/**
- * Wi-Fi driver configuration (DO NOT CHANGE)
- */
-#ifdef MODULE_ESP_WIFI_ANY
-#define CONFIG_ESP32_WIFI_ENABLED               1
-#endif
-#if defined(MODULE_ESP_WIFI_AP) || defined(MODULE_ESP_NOW)
-#define CONFIG_ESP_WIFI_SOFTAP_SUPPORT          1
-#endif
-
-#define CONFIG_ESP32_PHY_MAX_WIFI_TX_POWER      CONFIG_ESP_PHY_MAX_WIFI_TX_POWER
-#define CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED      1
-#define CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED      1
-#define CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM 32
-#define CONFIG_ESP32_WIFI_DYNAMIC_TX_BUFFER     1
-#define CONFIG_ESP32_WIFI_DYNAMIC_TX_BUFFER_NUM 32
-#define CONFIG_ESP32_WIFI_ENABLED               1
-#define CONFIG_ESP32_WIFI_ENABLE_WPA3_SAE       1
-#define CONFIG_ESP32_WIFI_IRAM_OPT              1
-#define CONFIG_ESP32_WIFI_MGMT_SBUF_NUM         32
-#if MODULE_ESP_IDF_NVS_ENABLED
-#define CONFIG_ESP32_WIFI_NVS_ENABLED           1
-#endif
-#define CONFIG_ESP32_WIFI_RX_BA_WIN             6
-#define CONFIG_ESP32_WIFI_RX_IRAM_OPT           1
-#define CONFIG_ESP32_WIFI_SOFTAP_BEACON_MAX_LEN 752
-#define CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM  10
-#define CONFIG_ESP32_WIFI_TX_BA_WIN             6
-#define CONFIG_ESP32_WIFI_TX_BUFFER_TYPE        1
-
-#define CONFIG_ESP_MAC_ADDR_UNIVERSE_WIFI_AP    1
 #define CONFIG_ESP_MAC_ADDR_UNIVERSE_WIFI_STA   1
-#define CONFIG_ESP_PHY_MAX_WIFI_TX_POWER        20
+#define CONFIG_ESP_MAC_ADDR_UNIVERSE_WIFI_AP    1
+#define CONFIG_ESP_MAC_ADDR_UNIVERSE_BT         1
+#define CONFIG_ESP_MAC_ADDR_UNIVERSE_ETH        1
+#define CONFIG_ESP32C3_UNIVERSAL_MAC_ADDRESSES  4
 
 /**
- * PHY configuration
+ * ESP32-C3 specific system configuration (DO NOT CHANGE)
  */
-#if MODULE_ESP_IDF_NVS_ENABLED
-#define CONFIG_ESP_PHY_CALIBRATION_AND_DATA_STORAGE     1
-#endif
+#define CONFIG_ESP_TIMER_IMPL_SYSTIMER          1
 
-#define CONFIG_ESP_PHY_MAX_TX_POWER                     20
-#define CONFIG_ESP_PHY_MAX_WIFI_TX_POWER                20
+#define CONFIG_ESP32C3_DEBUG_OCDAWARE           1
+#define CONFIG_ESP32C3_REV_MIN                  3
 
-#define CONFIG_ESP32_PHY_CALIBRATION_AND_DATA_STORAGE   CONFIG_ESP_PHY_CALIBRATION_AND_DATA_STORAGE
-#define CONFIG_ESP32_PHY_MAX_WIFI_TX_POWER              CONFIG_ESP_PHY_MAX_WIFI_TX_POWER
+#define CONFIG_ESP32C3_BROWNOUT_DET             1
+#define CONFIG_ESP32C3_BROWNOUT_DET_LVL         7
+
+/**
+ * ESP32-C3 specific sleep configuration (DO NOT CHANGE)
+ */
+#define CONFIG_ESP_SLEEP_POWER_DOWN_FLASH       1
+#define CONFIG_ESP_SLEEP_GPIO_RESET_WORKAROUND  1
 
 #ifdef __cplusplus
 }
