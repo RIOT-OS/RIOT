7af36a32c4cdc4530cfbd490a04aca1fe5b8be89
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2022-08-08 09:55:20 +0200

    boards/common/stm32: clean up LED definitions
    Let boards only define the port and pin number of each LEDs. The common
    definitions in `stm32_leds.h` will provide `LED<x>_ON`, `LED<x>_OFF`,
    `LED<x>_TOGGLE`, `LED<x>_PIN`, `LED<x>_MASK` and `LED<x>_PORT`.
    
    In addition to code de-duplication, this also makes it easier to use
    LEDs in GPIO LL, which can be beneficial for super low overhead
    debugging output - e.g. when a bug is timing sensitive and `DEBUG()`
    would spent to much time for stdio to reproduce a bug.

--- boards/common/nucleo32/include/board.h
+++ boards/common/nucleo32/include/board.h
@@ -33,16 +33,15 @@ extern "C" {
  * @name Macros for controlling the on-board LED (LD3).
  * @{
  */
-#define LED0_PIN            GPIO_PIN(PORT_B, 3)
-#define LED0_MASK           (1 << 3)
-#define LED0_ON             (GPIOB->BSRR = LED0_MASK)
-#define LED0_OFF            (GPIOB->BSRR = (LED0_MASK << 16))
-#define LED0_TOGGLE         (GPIOB->ODR  ^= LED0_MASK)
+#define LED0_PIN_NUM        3
+#define LED0_PORT_NUM       PORT_B
 /** @} */
 
 #ifdef __cplusplus
 }
 #endif
+
+#include "stm32_leds.h"
 
 #endif /* BOARD_H */
 /** @} */
