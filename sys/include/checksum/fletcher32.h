31df2ba0aa80a19d84b03ae2e333ac512cc6e1d3
Koen Zandberg <koen@bergzand.net>
2022-09-17 22:28:01 +0200

    Fletcher32: Add incremental API

--- sys/include/checksum/fletcher32.h
+++ sys/include/checksum/fletcher32.h
@@ -15,6 +15,7 @@
  *
  * @file
  * @author      Joakim Nohlgård <joakim.nohlgard@eistec.se>
+ * @author      Koen Zandberg <koen@bergzand.net>
  */
 
 #ifndef CHECKSUM_FLETCHER32_H
@@ -26,6 +27,14 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @brief Fletcher's 32 bit checksum context struct
+ */
+typedef struct {
+    uint32_t sum1;  /**< First sum of the checksum */
+    uint32_t sum2;  /**< Second sum of the checksum */
+} fletcher32_ctx_t;
 
 /**
  * @brief Fletcher's 32 bit checksum
@@ -42,6 +51,37 @@ extern "C" {
  * @return 32 bit sized hash in the interval [1..2^32]
  */
 uint32_t fletcher32(const uint16_t *buf, size_t words);
+
+/**
+ * @brief Initialize a fletcher32 context
+ *
+ * Multi-part version of @ref fletcher32.
+ *
+ * @param[in]   ctx     fletcher32 context to initialize
+ */
+void fletcher32_init(fletcher32_ctx_t *ctx);
+
+/**
+ * @brief Incrementally update the fletcher32 context with new data. Can be an arbitrary amount of
+ * times with new data to checksum.
+ *
+ * @note @p words is the number of 16 bit words in the buffer
+ * @note @p data should contain an integer number of 16 bit words
+ *
+ * @param[in]   ctx     fletcher32 context
+ * @param[in]   data    Data to add to the context
+ * @param[in]   words   Length of the data in 16 bit words
+ */
+void fletcher32_update(fletcher32_ctx_t *ctx, const void *data, size_t words);
+
+/**
+ * @brief Finalize the checksum operation and return the checksum
+ *
+ * @param[in]   ctx     fletcher32 context
+ *
+ * @return 32 bit sized hash in the interval [1..2^32]
+ */
+uint32_t fletcher32_finish(fletcher32_ctx_t *ctx);
 
 #ifdef __cplusplus
 }
