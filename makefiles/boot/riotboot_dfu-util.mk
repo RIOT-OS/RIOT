3d4e8d6a0d088b76fa04fa575b976741ea577bc4
Gunar Schorcht <gunar@schorcht.net>
2022-11-27 17:36:28 +0100

    makefiles/boot/riotboot-dfu-util: add tinyusb_dfu support

--- makefiles/boot/riotboot_dfu-util.mk
+++ makefiles/boot/riotboot_dfu-util.mk
@@ -1,6 +1,7 @@
-ifneq (,$(filter usbus_dfu,$(USEMODULE)))
-  ifeq (,$(filter riotboot_usb_dfu,$(USEMODULE)))
-    # If module usbus_dfu is used but not module riotboot_usb_dfu, the
+ifneq (,$(filter usbus_dfu tinyusb_dfu,$(USEMODULE)))
+  ifeq (,$(filter riotboot_usb_dfu riotboot_tinyusb_dfu,$(USEMODULE)))
+    # If module usbus_dfu or module tinyusb_dfu is used but neither
+    # module riotboot_usb_dfu nor module riotboot_tinyusb_dfu, the
     # application uses DFU Runtime and dfu-util as programmer to flash the
     # application with the bootloader riotboot_dfu which uses the VID/PID pair
     # allocated for the RIOT bootloader https://pid.codes/1209/7D02/
