009afd11732ebd259577474646675873448fc8e5
Gunar Schorcht <gunar@schorcht.net>
2022-12-13 00:58:29 +0100

    drivers/sht2x: extend Kconfig by sensor parameters

--- drivers/sht2x/include/sht2x_params.h
+++ drivers/sht2x/include/sht2x_params.h
@@ -13,7 +13,7 @@
  *
  * @{
  * @file
- * @brief       Default configuration for SHT2X
+ * @brief       Default configuration for SHT2x humidity and temperature sensor
  *
  * @author      Kees Bakker <kees@sodaq.com>
  * @author      George Psimenos <gp7g14@soton.ac.uk>
@@ -30,26 +30,70 @@
 extern "C" {
 #endif
 
+#if !DOXYGEN
+/* Mapping of Kconfig defines to the respective driver enumeration values */
+
+#if CONFIG_SHT2X_RES_12_14BIT
+#define CONFIG_SHT2X_RESOLUTION     (SHT2X_RES_12_14BIT)
+#elif CONFIG_SHT2X_RES_11_11BIT
+#define CONFIG_SHT2X_RESOLUTION     (SHT2X_RES_11_11BIT)
+#elif CONFIG_SHT2X_RES_10_13BIT
+#define CONFIG_SHT2X_RESOLUTION     (SHT2X_RES_10_13BIT)
+#elif CONFIG_SHT2X_RES_8_12BIT
+#define CONFIG_SHT2X_RESOLUTION     (SHT2X_RES_8_12BIT)
+#else
+#define CONFIG_SHT2X_RESOLUTION     (SHT2X_RES_12_14BIT)
+#endif
+
+#if CONFIG_SHT2X_MEASURE_MODE_HOLD
+#define CONFIG_SHT2X_MEASURE_MODE   (SHT2X_MEASURE_MODE_HOLD)
+#elif CONFIG_SHT2X_MEASURE_MODE_NO_HOLD
+#define CONFIG_SHT2X_MEASURE_MODE   (SHT2X_MEASURE_MODE_NO_HOLD)
+#else
+#define CONFIG_SHT2X_MEASURE_MODE   (SHT2X_MEASURE_MODE_NO_HOLD)
+#endif
+
+#ifndef CONFIG_SHT2X_CRC_MODE
+#define CONFIG_SHT2X_CRC_MODE       (1)
+#endif
+
+#endif /* !DOXYGEN */
+
 /**
- * @brief   Set default configuration parameters for the SHT2X
+ * @name   Default SHT2x hardware configuration
  * @{
  */
 #ifndef SHT2X_PARAM_I2C_DEV
+/** I2C device used */
 #define SHT2X_PARAM_I2C_DEV         (I2C_DEV(0))
 #endif
+
 #ifndef SHT2X_PARAM_I2C_ADDR
+/** I2C slave slave of the SHT2x sensor */
 #define SHT2X_PARAM_I2C_ADDR        (0x40)
 #endif
+/** @} */
+
+/**
+ * @name   Default sensor configuration for the SHT2x sensor
+ * @{
+ */
 #ifndef SHT2X_PARAM_RESOLUTION
-#define SHT2X_PARAM_RESOLUTION      (SHT2X_RES_12_14BIT)
-#endif
-#ifndef SHT2X_PARAM_MEASURE_MODE
-#define SHT2X_PARAM_MEASURE_MODE    (SHT2X_MEASURE_MODE_HOLD)
-#endif
-#ifndef SHT2X_PARAM_CRC_MODE
-#define SHT2X_PARAM_CRC_MODE        (1)
+/** SHT2x resolution */
+#define SHT2X_PARAM_RESOLUTION      (CONFIG_SHT2X_RESOLUTION)
 #endif
 
+#ifndef SHT2X_PARAM_MEASURE_MODE
+/** SHT2x measurement mode */
+#define SHT2X_PARAM_MEASURE_MODE    (CONFIG_SHT2X_MEASURE_MODE)
+#endif
+
+#ifndef SHT2X_PARAM_CRC_MODE
+/** SHT2x CRC mode */
+#define SHT2X_PARAM_CRC_MODE        (CONFIG_SHT2X_CRC_MODE)
+#endif
+
+/** Default SHT2x parameter set */
 #define SHT2X_PARAMS_DEFAULT        {.i2c_dev = SHT2X_PARAM_I2C_DEV,  \
                                      .i2c_addr = SHT2X_PARAM_I2C_ADDR, \
                                      .resolution = SHT2X_PARAM_RESOLUTION, \
@@ -58,6 +102,7 @@ extern "C" {
                                     }
 
 #ifndef SHT2X_SAUL_INFO
+/** Default SAUL device info */
 #define SHT2X_SAUL_INFO             { .name = "sht2x" }
 #endif
 /**@}*/
