bed322778232feb5c79815a99e3f23a6a390b8e8
Benjamin Valentin <benjamin.valentin@bht-berlin.de>
2023-01-03 00:26:12 +0100

    tests/pkg_littlefs2: move to vfs_littlefs2

--- tests/vfs_littlefs2/main.c
+++ tests/vfs_littlefs2/main.c
@@ -11,7 +11,6 @@
  *
  * @file
  */
-#include "fs/littlefs2_fs.h"
 #include "vfs.h"
 #include "mtd.h"
 #include "board.h"
@@ -121,82 +120,77 @@ static mtd_dev_t dev = {
 static mtd_dev_t *_dev = (mtd_dev_t*) &dev;
 #endif /* MTD_0 */
 
-static littlefs2_desc_t littlefs_desc;
+extern vfs_mount_t test_vfs_mount;
+extern void test_vfs_init(mtd_dev_t *dev);
 
-static vfs_mount_t _test_littlefs_mount = {
-    .fs = &littlefs2_file_system,
-    .mount_point = "/test-littlefs",
-    .private_data = &littlefs_desc,
-};
-
-static void test_littlefs_setup(void)
+static void test_vfs_setup(void)
 {
-    littlefs_desc.dev = _dev;
-    vfs_mount(&_test_littlefs_mount);
+    test_vfs_init(_dev);
+    vfs_mount(&test_vfs_mount);
 }
 
-static void test_littlefs_teardown(void)
+static void test_vfs_teardown(void)
 {
-    vfs_unlink("/test-littlefs/test.txt");
-    vfs_unlink("/test-littlefs/test0.txt");
-    vfs_unlink("/test-littlefs/test1.txt");
-    vfs_unlink("/test-littlefs/a/test2.txt");
-    vfs_rmdir("/test-littlefs/a");
-    vfs_umount(&_test_littlefs_mount);
+    vfs_unlink("/test-vfs/test.txt");
+    vfs_unlink("/test-vfs/test0.txt");
+    vfs_unlink("/test-vfs/test1.txt");
+    vfs_unlink("/test-vfs/a/test2.txt");
+    vfs_rmdir("/test-vfs/a");
+    vfs_umount(&test_vfs_mount);
 }
 
-static void tests_littlefs_format(void)
+static void tests_vfs_format(void)
 {
     int res;
-    vfs_umount(&_test_littlefs_mount);
+    vfs_umount(&test_vfs_mount);
     res = mtd_erase(_dev, 0, _dev->page_size * _dev->pages_per_sector * _dev->sector_count);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_mount(&_test_littlefs_mount);
+    res = vfs_mount(&test_vfs_mount);
     TEST_ASSERT(res < 0);
 
     /* 1. format an invalid file system (failed mount) */
-    res = vfs_format(&_test_littlefs_mount);
+    res = vfs_format(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_mount(&_test_littlefs_mount);
+    res = vfs_mount(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(&_test_littlefs_mount);
+    res = vfs_umount(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 
     /* 2. format a valid file system */
-    res = vfs_format(&_test_littlefs_mount);
+    res = vfs_format(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_mount_umount(void)
+static void tests_vfs_mount_umount(void)
 {
     int res;
-    res = vfs_umount(&_test_littlefs_mount);
+    res = vfs_umount(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_mount(&_test_littlefs_mount);
+    res = vfs_mount(&test_vfs_mount);
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_open_close(void)
+static void tests_vfs_open_close(void)
 {
     int res;
-    res = vfs_open("/test-littlefs/test.txt", O_CREAT | O_RDWR, 0);
+    res = vfs_open("/test-vfs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(res >= 0);
 
     res = vfs_close(res);
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_write(void)
+static void tests_vfs_write(void)
 {
     const char buf[] = "TESTSTRING";
     char r_buf[2 * sizeof(buf)];
 
     int res;
-    int fd = vfs_open("/test-littlefs/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-vfs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_write(fd, buf, sizeof(buf));
@@ -212,7 +206,7 @@ static void tests_littlefs_write(void)
     res = vfs_close(fd);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    fd = vfs_open("/test-littlefs/test.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-vfs/test.txt", O_RDONLY, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_read(fd, r_buf, sizeof(r_buf));
@@ -223,12 +217,12 @@ static void tests_littlefs_write(void)
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_unlink(void)
+static void tests_vfs_unlink(void)
 {
     const char buf[] = "TESTSTRING";
 
     int res;
-    int fd = vfs_open("/test-littlefs/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-vfs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_write(fd, buf, sizeof(buf));
@@ -237,30 +231,30 @@ static void tests_littlefs_unlink(void)
     res = vfs_close(fd);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test-littlefs/test.txt");
+    res = vfs_unlink("/test-vfs/test.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_readdir(void)
+static void tests_vfs_readdir(void)
 {
     const char buf0[] = "TESTSTRING";
     const char buf1[] = "TESTTESTSTRING";
     const char buf2[] = "TESTSTRINGSTRING";
 
     int res;
-    int fd0 = vfs_open("/test-littlefs/test0.txt", O_CREAT | O_RDWR, 0);
+    int fd0 = vfs_open("/test-vfs/test0.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd0 >= 0);
 
-    int fd1 = vfs_open("/test-littlefs/test1.txt", O_CREAT | O_RDWR, 0);
+    int fd1 = vfs_open("/test-vfs/test1.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd1 >= 0);
 
-    int fd2 = vfs_open("/test-littlefs/a/test2.txt", O_CREAT | O_RDWR, 0);
+    int fd2 = vfs_open("/test-vfs/a/test2.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd2 < 0);
 
-    res = vfs_mkdir("/test-littlefs/a", 0);
+    res = vfs_mkdir("/test-vfs/a", 0);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    fd2 = vfs_open("/test-littlefs/a/test2.txt", O_CREAT | O_RDWR, 0);
+    fd2 = vfs_open("/test-vfs/a/test2.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd2 >= 0);
 
     res = vfs_write(fd0, buf0, sizeof(buf0));
@@ -282,7 +276,7 @@ static void tests_littlefs_readdir(void)
     TEST_ASSERT_EQUAL_INT(0, res);
 
     vfs_DIR dirp;
-    res = vfs_opendir(&dirp, "/test-littlefs");
+    res = vfs_opendir(&dirp, "/test-vfs");
     TEST_ASSERT_EQUAL_INT(0, res);
 
     vfs_dirent_t entry;
@@ -301,7 +295,7 @@ static void tests_littlefs_readdir(void)
     res = vfs_closedir(&dirp);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_opendir(&dirp, "/test-littlefs/a");
+    res = vfs_opendir(&dirp, "/test-vfs/a");
     TEST_ASSERT_EQUAL_INT(0, res);
     nb_files = 0;
     do {
@@ -314,29 +308,29 @@ static void tests_littlefs_readdir(void)
     TEST_ASSERT_EQUAL_INT(1, nb_files);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test-littlefs/test0.txt");
+    res = vfs_unlink("/test-vfs/test0.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test-littlefs/test1.txt");
+    res = vfs_unlink("/test-vfs/test1.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test-littlefs/a/test2.txt");
+    res = vfs_unlink("/test-vfs/a/test2.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_closedir(&dirp);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_rmdir("/test-littlefs/a");
+    res = vfs_rmdir("/test-vfs/a");
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_rename(void)
+static void tests_vfs_rename(void)
 {
     const char buf[] = "TESTSTRING";
     char r_buf[2 * sizeof(buf)];
 
     int res;
-    int fd = vfs_open("/test-littlefs/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-vfs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_write(fd, buf, sizeof(buf));
@@ -352,13 +346,13 @@ static void tests_littlefs_rename(void)
     res = vfs_close(fd);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_rename("/test-littlefs/test.txt", "/test-littlefs/test1.txt");
+    res = vfs_rename("/test-vfs/test.txt", "/test-vfs/test1.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    fd = vfs_open("/test-littlefs/test.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-vfs/test.txt", O_RDONLY, 0);
     TEST_ASSERT(fd < 0);
 
-    fd = vfs_open("/test-littlefs/test1.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-vfs/test1.txt", O_RDONLY, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_lseek(fd, 0, SEEK_SET);
@@ -371,29 +365,26 @@ static void tests_littlefs_rename(void)
     res = vfs_close(fd);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test-littlefs/test1.txt");
+    res = vfs_unlink("/test-vfs/test1.txt");
     TEST_ASSERT_EQUAL_INT(0, res);
 }
 
-static void tests_littlefs_statvfs(void)
+static void tests_vfs_statvfs(void)
 {
     const char buf[] = "TESTSTRING";
     struct statvfs stat1;
     struct statvfs stat2;
 
-    int res = vfs_statvfs("/test-littlefs/", &stat1);
+    int res = vfs_statvfs("/test-vfs/", &stat1);
     TEST_ASSERT_EQUAL_INT(0, res);
-    TEST_ASSERT_EQUAL_INT(_dev->page_size *
-                          _dev->pages_per_sector *
-                          littlefs_desc.sectors_per_block,
-                          stat1.f_bsize);
     TEST_ASSERT_EQUAL_INT(_dev->page_size * _dev->pages_per_sector, stat1.f_frsize);
     TEST_ASSERT((_dev->pages_per_sector * _dev->page_size * _dev->sector_count) >=
                           stat1.f_blocks);
 
-    int fd = vfs_open("/test-littlefs/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-vfs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
+    /* write TESTSTRING 128 times */
     for (int i = 0; i < 128; ++i) {
         res = vfs_write(fd, buf, sizeof(buf));
         TEST_ASSERT(res == sizeof(buf));
@@ -402,44 +393,40 @@ static void tests_littlefs_statvfs(void)
     res = vfs_close(fd);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_statvfs("/test-littlefs/", &stat2);
+    res = vfs_statvfs("/test-vfs/", &stat2);
     TEST_ASSERT_EQUAL_INT(0, res);
 
-    TEST_ASSERT_EQUAL_INT(_dev->page_size *
-                          _dev->pages_per_sector *
-                          littlefs_desc.sectors_per_block,
-                          stat2.f_bsize);
     TEST_ASSERT_EQUAL_INT(_dev->page_size * _dev->pages_per_sector, stat2.f_frsize);
     TEST_ASSERT(stat1.f_bfree > stat2.f_bfree);
     TEST_ASSERT(stat1.f_bavail > stat2.f_bavail);
 }
 
-Test *tests_littlefs(void)
+Test *tests_vfs(void)
 {
 #ifndef USE_MTD_0
     memset(dummy_memory, 0xff, sizeof(dummy_memory));
 #endif
 
     EMB_UNIT_TESTFIXTURES(fixtures) {
-        new_TestFixture(tests_littlefs_format),
-        new_TestFixture(tests_littlefs_mount_umount),
-        new_TestFixture(tests_littlefs_open_close),
-        new_TestFixture(tests_littlefs_write),
-        new_TestFixture(tests_littlefs_unlink),
-        new_TestFixture(tests_littlefs_readdir),
-        new_TestFixture(tests_littlefs_rename),
-        new_TestFixture(tests_littlefs_statvfs),
+        new_TestFixture(tests_vfs_format),
+        new_TestFixture(tests_vfs_mount_umount),
+        new_TestFixture(tests_vfs_open_close),
+        new_TestFixture(tests_vfs_write),
+        new_TestFixture(tests_vfs_unlink),
+        new_TestFixture(tests_vfs_readdir),
+        new_TestFixture(tests_vfs_rename),
+        new_TestFixture(tests_vfs_statvfs),
     };
 
-    EMB_UNIT_TESTCALLER(littlefs_tests, test_littlefs_setup, test_littlefs_teardown, fixtures);
+    EMB_UNIT_TESTCALLER(vfs_tests, test_vfs_setup, test_vfs_teardown, fixtures);
 
-    return (Test *)&littlefs_tests;
+    return (Test *)&vfs_tests;
 }
 
 int main(void)
 {
     TESTS_START();
-    TESTS_RUN(tests_littlefs());
+    TESTS_RUN(tests_vfs());
     TESTS_END();
     return 0;
 }
