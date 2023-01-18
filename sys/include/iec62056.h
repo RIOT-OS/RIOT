7ef687807bbe67a8dcb2aa9e8ce270d2a5ba82b1
Koen Zandberg <koen@bergzand.net>
2023-01-18 11:32:59 +0100

    iec62056: Add initial module

---
+++ sys/include/iec62056.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Koen Zandberg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_iec62056 IEC62056
+ * @ingroup     sys
+ * @brief       Data exchange for meter reading, tariff and load control
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      Koen Zandberg <koen@bergzand.net>
+ */
+
+#ifndef IEC62056_H
+#define IEC62056_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IEC62056_H */
+/** @} */
