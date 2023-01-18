6db950b793ca820ccfe27f473e6572d94c4a2c61
Koen Zandberg <koen@bergzand.net>
2023-01-18 11:38:38 +0100

    tests/sys/iec62056: add test application

---
+++ tests/sys/iec62056/tests/01-run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 Koen Zandberg <koen@bergzand.net>
+# SPDX-License-Identifier: LGPL-2.1-only
+
+import sys
+from testrunner import run_check_unittests
+
+
+if __name__ == "__main__":
+    sys.exit(run_check_unittests())
