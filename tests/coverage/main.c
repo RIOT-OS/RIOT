ce479d8c73296162556e1820385cf540dda1ade6
Sören Tempel <tempel@uni-bremen.de>
2023-01-31 16:21:03 +0100

    tests: add test application for coverage module

---
+++ tests/coverage/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 University of Bremen
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief Test application for the coverage module
+ *
+ * @author Sören Tempel <tempel@uni-bremen.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "coverage.h"
+
+int main(void)
+{
+    puts("Hello, World!");
+    return 0;
+}
