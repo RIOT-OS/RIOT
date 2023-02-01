2b2506c052e889ada0344c33ddccd79d2ab95764
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2023-02-01 15:57:45 +0100

    build system: Fix linker feature test with newlib 4.3.0
    Before the build system used something like
    
        echo "int main(){} void _exit(int n) {(void)n;while(1);}" | LC_ALL=C $(LINK) -xc - -o /dev/null -lc -Wall -Wextra -pedantic <FLAGS_TO_TEST>
    
    to check for flags supported by the compiler (used as linker frontend).
    This however no longer works with newlib 4.3.0, which requires the
    symbols `_sbrk_r`, `_close_r`, `_lseek_r`, `_read_r`, and `_write_r` to
    be present. Instead, now a new file `minimal_linkable.c` was added that
    adds all the missing symbols and is used in the linker feature tests
    instead.

--- makefiles/libc/picolibc.mk
+++ makefiles/libc/picolibc.mk
@@ -2,7 +2,7 @@ ifneq (,$(filter picolibc,$(USEMODULE)))
   # Test if picolibc.specs is available
   ifeq ($(shell $(LINK) -specs=picolibc.specs -E - 2>/dev/null >/dev/null </dev/null ; echo $$?),0)
     USE_PICOLIBC = 1
-    ifeq ($(shell echo "int main(){} void _exit(int n) {(void)n;while(1);}" | LC_ALL=C $(LINK) -xc - -o /dev/null -lc -specs=picolibc.specs -Wall -Wextra -pedantic 2>&1 | grep -q "use of wchar_t values across objects may fail" ; echo $$?),0)
+    ifeq (LC_ALL=C $(LINK) $(RIOTTOOLS)/testprogs/minimal_linkable.c -o /dev/null -lc -specs=picolibc.specs -Wall -Wextra -pedantic 2>&1 | grep -q "use of wchar_t values across objects may fail" ; echo $$?),0)
         CFLAGS += -fshort-wchar
         LINKFLAGS += -Wl,--no-wchar-size-warning
     endif
@@ -31,7 +31,7 @@ ifeq (1,$(USE_PICOLIBC))
   # picolibc. But since (as of now) RIOT only supports disabling the execute of
   # all RAM via the `mpu_noexec_ram` module, permissions of the segments are
   # ignored anyway. So for now, we just simply disable the warning.
-  LINKER_SUPPORTS_NOWARNRWX ?= $(shell echo "int main(){} void _exit(int n) {(void)n;while(1);}" | LC_ALL=C $(LINK) -xc - -o /dev/null -lc -Wall -Wextra -pedantic -Wl,--no-warn-rwx-segments 2> /dev/null && echo 1 || echo 0)
+  LINKER_SUPPORTS_NOWARNRWX ?= $(shell LC_ALL=C $(LINK) $(RIOTTOOLS)/testprogs/minimal_linkable.c -o /dev/null -lc -Wall -Wextra -pedantic -Wl,--no-warn-rwx-segments 2> /dev/null && echo 1 || echo 0)
   ifeq (1,$(LINKER_SUPPORTS_NOWARNRWX))
     LINKFLAGS += -Wl,--no-warn-rwx-segments
   endif
