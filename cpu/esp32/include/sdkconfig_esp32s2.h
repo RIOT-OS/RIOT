ed5bfb7392cb5c4607b3066f57030f1b37af4a88
Benjamin Valentin <benjamin.valentin@bht-berlin.de>
2023-02-01 15:39:00 +0100

    cpu/esp32: drop CONFIG_ESP_PHY_ENABLE_USB for ESP32S2

--- cpu/esp32/include/sdkconfig_esp32s2.h
+++ cpu/esp32/include/sdkconfig_esp32s2.h
@@ -106,7 +106,6 @@ extern "C" {
  * ESP32-S2 specific USB configuration
  */
 #ifdef MODULE_ESP_IDF_USB
-#define CONFIG_ESP_PHY_ENABLE_USB               1
 #define CONFIG_USB_OTG_SUPPORTED                1
 #endif
 /**
