2913456ab384650b906870333a72a74075432bea
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2023-02-01 14:33:39 +0100

    tests/unittests: Add test for the CONCAT(...) macro

--- tests/unittests/tests-core/tests-core-macros.c
+++ tests/unittests/tests-core/tests-core-macros.c
@@ -12,6 +12,7 @@
 #include "embUnit.h"
 #include "tests-core.h"
 #include "macros/math.h"
+#include "macros/utils.h"
 
 static void test_math_signof(void)
 {
@@ -78,6 +79,20 @@ static void test_math_div_round_inf(void)
     TEST_ASSERT_EQUAL_INT(1536, MATH_ALIGN(1514, 64));
 }
 
+/* Use macro HEX_15 here to ensure that CONCAT indeed expands its arguments
+ * prior to the concatenation */
+#define HEX_15  F
+static void test_concat(void)
+{
+    TEST_ASSERT(0xF                == CONCAT(0x, HEX_15));
+    TEST_ASSERT(0xFE               == CONCAT(0x, HEX_15, E));
+    TEST_ASSERT(0xFED              == CONCAT(0x, HEX_15, E, D));
+    TEST_ASSERT(0xFEDC             == CONCAT(0x, HEX_15, E, D, C));
+    TEST_ASSERT(0xFEDCB            == CONCAT(0x, HEX_15, E, D, C, B));
+    TEST_ASSERT(0xFEDCBA           == CONCAT(0x, HEX_15, E, D, C, B, A));
+    TEST_ASSERT(0xFEDCBA9          == CONCAT(0x, HEX_15, E, D, C, B, A, 9));
+}
+
 Test *tests_core_macros_tests(void)
 {
     EMB_UNIT_TESTFIXTURES(fixtures) {
@@ -85,6 +100,7 @@ Test *tests_core_macros_tests(void)
         new_TestFixture(test_math_div_round),
         new_TestFixture(test_math_div_round_up),
         new_TestFixture(test_math_div_round_inf),
+        new_TestFixture(test_concat),
    };
 
     EMB_UNIT_TESTCALLER(core_macros_tests, NULL, NULL, fixtures);
