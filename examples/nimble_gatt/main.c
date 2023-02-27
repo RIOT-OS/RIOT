ebaa18b62d5ac512d98f033c0b7754cec0631d9d
Augusto Vaccarelli <augustovaccarelli@gmail.com>
2023-02-27 13:55:07 -0300

    mixed Nimble GATT Example and Nimble Heart Sensor

--- examples/nimble_gatt/main.c
+++ examples/nimble_gatt/main.c
@@ -52,6 +52,21 @@
 #include "services/gap/ble_svc_gap.h"
 #include "services/gatt/ble_svc_gatt.h"
 
+#include <stdint.h>
+#include "event/timeout.h"
+#include "net/bluetil/ad.h"
+#include "timex.h"
+
+
+#define HRS_FLAGS_DEFAULT       (0x01)      /* 16-bit BPM value */
+#define SENSOR_LOCATION         (0x02)      /* wrist sensor */
+#define UPDATE_INTERVAL         (250U)
+#define BPM_MIN                 (80U)
+#define BPM_MAX                 (210U)
+#define BPM_STEP                (2)
+#define BAT_LEVEL               (42U)
+
+
 /* Macros for frames to be read */
 
 #define ACC_FRAMES	10 /* 10 Frames are available every 100ms @ 100Hz */ //Datasheet says you can take measurementes at 1600 Hz
@@ -123,6 +138,29 @@ void user_delay(uint32_t period)
 
 #define STR_ANSWER_BUFFER_SIZE 100
 
+static struct __attribute__((packed)) {
+    uint8_t flags;
+    uint16_t bpm;
+} _hr_data = { HRS_FLAGS_DEFAULT, (BPM_MIN + BPM_STEP) };
+
+static event_queue_t _eq;
+static event_t _update_evt;
+static event_timeout_t _update_timeout_evt;
+
+static uint16_t _conn_handle;
+static uint16_t _hrs_val_handle;
+
+static int step = BPM_STEP;
+
+static int _hrs_handler(uint16_t conn_handle, uint16_t attr_handle,
+                        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static int _bas_handler(uint16_t conn_handle, uint16_t attr_handle,
+                        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static void _start_updating(void);
+static void _stop_updating(void);
+
 /* UUID = 1bce38b3-d137-48ff-a13e-033e14c7a335 */
 static const ble_uuid128_t gatt_svr_svc_rw_demo_uuid
         = BLE_UUID128_INIT(0x35, 0xa3, 0xc7, 0x14, 0x3e, 0x03, 0x3e, 0xa1, 0xff,
@@ -179,6 +217,23 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
         }, }
     },
     {
+        /* Heart Rate Service */
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID16_DECLARE(BLE_GATT_SVC_HRS),
+        .characteristics = (struct ble_gatt_chr_def[]) { {
+            .uuid = BLE_UUID16_DECLARE(BLE_GATT_CHAR_HEART_RATE_MEASURE),
+            .access_cb = _hrs_handler,
+            .val_handle = &_hrs_val_handle,
+            .flags = BLE_GATT_CHR_F_NOTIFY,
+        }, {
+            .uuid = BLE_UUID16_DECLARE(BLE_GATT_CHAR_BODY_SENSE_LOC),
+            .access_cb = _hrs_handler,
+            .flags = BLE_GATT_CHR_F_READ,
+        }, {
+            0, /* no more characteristics in this service */
+        }, }
+    },
+    {
         /* Service: Read/Write Demo */
         .type = BLE_GATT_SVC_TYPE_PRIMARY,
         .uuid = (ble_uuid_t*) &gatt_svr_svc_rw_demo_uuid.u,
@@ -197,9 +252,123 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
         }, }
     },
     {
+        /* Battery Level Service */
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID16_DECLARE(BLE_GATT_SVC_BAS),
+        .characteristics = (struct ble_gatt_chr_def[]) { {
+            .uuid = BLE_UUID16_DECLARE(BLE_GATT_CHAR_BATTERY_LEVEL),
+            .access_cb = _bas_handler,
+            .flags = BLE_GATT_CHR_F_READ,
+        }, {
+            0, /* no more characteristics in this service */
+        }, }
+    },
+    {
         0, /* No more services */
     },
 };
+
+static int _hrs_handler(uint16_t conn_handle, uint16_t attr_handle,
+                        struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    (void)conn_handle;
+    (void)attr_handle;
+    (void)arg;
+
+    if (ble_uuid_u16(ctxt->chr->uuid) != BLE_GATT_CHAR_BODY_SENSE_LOC) {
+        return BLE_ATT_ERR_UNLIKELY;
+    }
+
+    puts("[READ] heart rate service: body sensor location value");
+
+    uint8_t loc = SENSOR_LOCATION;
+    int res = os_mbuf_append(ctxt->om, &loc, sizeof(loc));
+    return (res == 0) ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
+}
+
+static int _bas_handler(uint16_t conn_handle, uint16_t attr_handle,
+                        struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    (void)conn_handle;
+    (void)attr_handle;
+    (void)arg;
+
+    puts("[READ] battery level service: battery level value");
+
+    uint8_t level = BAT_LEVEL;  /* this battery will never drain :-) */
+    int res = os_mbuf_append(ctxt->om, &level, sizeof(level));
+    return (res == 0) ? 0 : BLE_ATT_ERR_INSUFFICIENT_RES;
+}
+
+static int gap_event_cb(struct ble_gap_event *event, void *arg)
+{
+    (void)arg;
+
+    switch (event->type) {
+        case BLE_GAP_EVENT_CONNECT:
+            if (event->connect.status) {
+                _stop_updating();
+                nimble_autoadv_start();
+                return 0;
+            }
+            _conn_handle = event->connect.conn_handle;
+            break;
+
+        case BLE_GAP_EVENT_DISCONNECT:
+            _stop_updating();
+            nimble_autoadv_start();
+            break;
+
+        case BLE_GAP_EVENT_SUBSCRIBE:
+            if (event->subscribe.attr_handle == _hrs_val_handle) {
+                if (event->subscribe.cur_notify == 1) {
+                    _start_updating();
+                }
+                else {
+                    _stop_updating();
+                }
+            }
+            break;
+    }
+
+    return 0;
+}
+
+static void _start_updating(void)
+{
+    event_timeout_set(&_update_timeout_evt, UPDATE_INTERVAL);
+    puts("[NOTIFY_ENABLED] heart rate service");
+}
+
+static void _stop_updating(void)
+{
+    event_timeout_clear(&_update_timeout_evt);
+    puts("[NOTIFY_DISABLED] heart rate service");
+}
+
+static void _hr_update(event_t *e)
+{
+    (void)e;
+    struct os_mbuf *om;
+
+    /* our mock-up heart rate is going up and down */
+    if ((_hr_data.bpm == BPM_MIN) || (_hr_data.bpm == BPM_MAX)) {
+        step *= -1;
+    }
+    _hr_data.bpm += step;
+
+    printf("[NOTIFY] heart rate service: measurement %i\n", (int)_hr_data.bpm);
+
+    /* send heart rate data notification to GATT client */
+    om = ble_hs_mbuf_from_flat(&_hr_data, sizeof(_hr_data));
+    assert(om != NULL);
+    int res = ble_gattc_notify_custom(_conn_handle, _hrs_val_handle, om);
+    assert(res == 0);
+    (void)res;
+
+    /* schedule next update event */
+    event_timeout_set(&_update_timeout_evt, UPDATE_INTERVAL);
+}
 
 static int gatt_svr_chr_access_device_info_manufacturer(
         uint16_t conn_handle, uint16_t attr_handle,
@@ -320,8 +489,8 @@ static int gatt_svr_chr_access_rw_demo(
 
         if (ctxt->op == BLE_GATT_ACCESS_OP_READ_CHR && strcmp(rm_demo_write_data, "oi") ==0 ) {
             
-            snprintf(str_answer, STR_ANSWER_BUFFER_SIZE,
-                     "Hello World!"); //A crude way to display a command recognition 
+            //A crude way to display a command recognition 
+            sprintf(str_answer, "%3.3f, %3.3f, %3.3f", (accel_data[1].x / AC), (accel_data[1].y /AC), (accel_data[1].z /AC));
             puts(str_answer);
 
             rc = os_mbuf_append(ctxt->om, &str_answer, strlen(str_answer));
@@ -421,9 +590,53 @@ int main(void)
     /* Check rslt for any error codes */
     i2c_release(dev);
 
-    while(rslt == 0 && 0) {
+    puts("NimBLE Heart Rate Sensor Example");
+
+    int res = 0;
+    (void)res;
+
+    /* setup local event queue (for handling heart rate updates) */
+    event_queue_init(&_eq);
+    _update_evt.handler = _hr_update;
+    event_timeout_ztimer_init(&_update_timeout_evt, ZTIMER_MSEC, &_eq, &_update_evt);
+
+    /* verify and add our custom services */
+    res = ble_gatts_count_cfg(gatt_svr_svcs);
+    assert(res == 0);
+    res = ble_gatts_add_svcs(gatt_svr_svcs);
+    assert(res == 0);
+
+    /* set the device name */
+    ble_svc_gap_device_name_set(NIMBLE_AUTOADV_DEVICE_NAME);
+    /* reload the GATT server to link our added services */
+    ble_gatts_start();
+
+    struct ble_gap_adv_params advp;
+    memset(&advp, 0, sizeof(advp));
+
+    advp.conn_mode = BLE_GAP_CONN_MODE_UND;
+    advp.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    advp.itvl_min  = BLE_GAP_ADV_FAST_INTERVAL1_MIN;
+    advp.itvl_max  = BLE_GAP_ADV_FAST_INTERVAL1_MAX;
+
+    /* set advertise params */
+    nimble_autoadv_set_ble_gap_adv_params(&advp);
+
+    /* configure and set the advertising data */
+    uint16_t hrs_uuid = BLE_GATT_SVC_HRS;
+    nimble_autoadv_add_field(BLE_GAP_AD_UUID16_INCOMP, &hrs_uuid, sizeof(hrs_uuid));
+
+    nimble_auto_adv_set_gap_cb(&gap_event_cb, NULL);
+
+    /* start to advertise this node */
+    nimble_autoadv_start();
+
+    /* run an event loop for handling the heart rate update events */
+    event_loop(&_eq);
+
+    while(rslt == 0) {
         /* Wait for 100ms for the FIFO to fill */
-        user_delay(10);
+        user_delay(100);
 
         /* It is VERY important to reload the length of the FIFO memory as after the
          * call to bmi160_get_fifo_data(), the bmi.fifo->length contains the
@@ -453,25 +666,6 @@ int main(void)
 
     int lenki = sprintf(rm_demo_write_data, "%f, %f, %f", (accel_data[1].x / AC), (accel_data[1].y /AC), (accel_data[1].z /AC));
     printf("%d \n", lenki);
-
-    puts("NimBLE GATT Server Example");
-
-    int rc = 0;
-    (void)rc;
-
-    /* verify and add our custom services */
-    rc = ble_gatts_count_cfg(gatt_svr_svcs);
-    assert(rc == 0);
-    rc = ble_gatts_add_svcs(gatt_svr_svcs);
-    assert(rc == 0);
-
-    /* set the device name */
-    ble_svc_gap_device_name_set(NIMBLE_AUTOADV_DEVICE_NAME);
-    /* reload the GATT server to link our added services */
-    ble_gatts_start();
-
-    /* start to advertise this node */
-    nimble_autoadv_start();
 
     return 0;
 }
@@ -505,15 +699,15 @@ void do_read(void)
 
 void log_readings(void)
 {
-//#ifdef PULGA_USE_RINGBUFFER
-    /*for (size_t i = 0; i < rlen; i++) {
+#ifdef PULGA_USE_RINGBUFFER
+    for (size_t i = 0; i < rlen; i++) {
         printf("[Acc_readings] readings_buffer[%d]: ", i);
         printf("Acc_x: %f ", ((float)readings_bufferX[i])/ AC);
         printf("Acc_y: %f ", ((float)readings_bufferY[i])/ AC);
         printf("Acc_z: %f ", ((float)readings_bufferZ[i])/ AC);
         printf("\n");
-    }*/
-//#else
+    }
+#else
     for (size_t i = 0; i < ACC_FRAMES; i++) {
         printf("[Acc_readings] readings_buffer[%d]: ", i);
         printf("Acc_x: %f ", ((float)readings_bufferX[i])/ AC);
@@ -521,12 +715,12 @@ void log_readings(void)
         printf("Acc_z: %f ", ((float)readings_bufferZ[i])/ AC);
         printf("\n");
     }
-//#endif
+#endif
 }
 void read_and_show_Acc_values(void)
 {
     do_read();
 //#if(LOG_LEVEL==4)
-    log_readings();
+//    log_readings();
 //#endif
 }
