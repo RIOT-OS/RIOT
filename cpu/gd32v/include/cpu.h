f6cb8b99e63a5708f12071f5c3ca6b77e7039b2d
Gunar Schorcht <gunar@schorcht.net>
2023-03-19 16:38:51 +0100

    cpu/gd32v: add riotboot support

--- cpu/gd32v/include/cpu.h
+++ cpu/gd32v/include/cpu.h
@@ -24,6 +24,23 @@
 extern "C" {
 #endif
 
+/**
+ * @brief   Returns the address of running application in flash
+ */
+static inline uint32_t cpu_get_image_baseaddr(void)
+{
+    extern uint8_t _start;
+    return (uint32_t)&_start;
+}
+
+/**
+ * @brief   Starts another image in flash
+ */
+static inline void cpu_jump_to_image(uint32_t addr)
+{
+    __asm__ volatile ("jr %0" :: "r" (addr));
+}
+
 #ifdef __cplusplus
 }
 #endif
