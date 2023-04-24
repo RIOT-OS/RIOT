0078ab45226301da226b3dddc817ed933190dd4c
Benjamin Valentin <benpicco@beuth-hochschule.de>
2023-04-24 16:50:47 +0200

    makefiles/features_modules: ignore periph_spidev_linux

--- makefiles/features_modules.inc.mk
+++ makefiles/features_modules.inc.mk
@@ -100,7 +100,8 @@ USEMODULE += $(filter arduino_pwm, $(FEATURES_USED))
 
 # always register a peripheral driver as a required feature when the corresponding
 # module is requested
-PERIPH_IGNORE_MODULES += periph_usbdev_clk periph_gpio_mock periph_gpio_linux
+PERIPH_IGNORE_MODULES += periph_usbdev_clk periph_gpio_mock periph_gpio_linux periph_spidev_linux
+
 ifneq (,$(filter periph_%,$(DEFAULT_MODULE)))
   FEATURES_REQUIRED += $(filter-out $(PERIPH_IGNORE_MODULES),$(filter periph_%,$(USEMODULE)))
 endif
