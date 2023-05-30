b40269d9eeb9a89c74a45f7096b96a9410916109
MrKevinWeiss <weiss.kevin604@gmail.com>
2023-05-30 16:18:09 +0200

    tests/sys/ztimer_optional_rtc: Initial test

---
+++ tests/sys/ztimer_optional_rtc/main.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 TUBA Freiberg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief    high level ztimer test application
+ *
+ * @author   Karl Fessel <karl.fessel@ovgu.de>
+ *
+ *
+ * @}
+ */
+
+#include "ztimer.h"
+
+int main(void)
+{
+    printf("SUCCESS!\n");
+    return 0;
+}
