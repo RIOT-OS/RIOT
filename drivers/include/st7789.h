7417aa14c73a6f16dd182d46499fc99fd674dd68
Gunar Schorcht <gunar@schorcht.net>
2023-07-13 16:39:54 +0200

    drivers: separate st7789 driver

---
+++ drivers/include/st7789.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg
+ *               2021 Francisco Molina
+ *               2023 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    drivers_st7789 ST7789 display driver
+ * @ingroup     drivers_display
+ *
+ * @brief       Driver for the ST7789 display controller
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      Koen Zandberg <koen@bergzand.net>
+ * @author      Francisco Molina <francois-xavier.molina@inria.fr>
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ *
+ * The ST7789 is a generic display driver for small RGB displays. The driver
+ * implemented here operates over SPI to communicate with the device.
+ *
+ * The device requires colors to be send in big endian RGB-565 format. The
+ * @ref CONFIG_LCD_LE_MODE compile time option can switch this, but only use this
+ * when strictly necessary. This option will slow down the driver as it
+ * certainly can't use DMA anymore, every short has to be converted before
+ * transfer.
+ */
+
+
+#ifndef ST7789_H
+#define ST7789_H
+
+#include "lcd.h"
+
+#ifdef MODULE_DISP_DEV
+#include "disp_dev.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name    ST7789 display rotation modes
+ * @{
+ */
+#define ST7789_ROTATION_VERT            0                   /**< Vertical mode */
+#define ST7789_ROTATION_VERT_FLIP       LCD_MADCTL_MX | \
+                                        LCD_MADCTL_MY       /**< Flipped vertical */
+#define ST7789_ROTATION_HORZ            LCD_MADCTL_MV | \
+                                        LCD_MADCTL_MX       /**< Horizontal mode */
+#define ST7789_ROTATION_HORZ_FLIP       LCD_MADCTL_MV | \
+                                        LCD_MADCTL_MY       /**< Horizontal flipped */
+/** @} */
+
+/**
+ * @brief   Device descriptor for a st7789
+ *
+ */
+typedef struct {
+    lcd_t dev;                    /**< Pointer to the common lcd device */
+} st7789_t;
+
+/**
+ * @brief   LCD device operations table
+ */
+extern const lcd_driver_t lcd_st7789_driver;
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* ST7789_H */
+/** @} */
---
+++ drivers/st7789/include/st7789_internal.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     drivers_st7789
+ * @{
+ *
+ * @file
+ * @brief       Device driver implementation for the ST7789 display controller
+ *
+ * @author      Koen Zandberg <koen@bergzand.net>
+ *
+ * @}
+ */
+
+#ifndef ST7789_INTERNAL_H
+#define ST7789_INTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lcd_internal.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ST7789_INTERNAL_H */
