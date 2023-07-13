b5b4ad4a69e8486fda76e1bafe0aa263e6889697
Gunar Schorcht <gunar@schorcht.net>
2023-07-13 14:11:55 +0200

    sys/auto_init/screen: rename st7735 to more generic st77xx

--- sys/auto_init/screen/auto_init_st77xx.c
+++ sys/auto_init/screen/auto_init_st77xx.c
@@ -26,34 +26,34 @@
 #include "lcd.h"
 #include "lcd_disp_dev.h"
 
-#include "st7735.h"
-#include "st7735_params.h"
+#include "st77xx.h"
+#include "st77xx_params.h"
 
-#ifndef ST7735_NUMOF
-#define ST7735_NUMOF                0
+#ifndef ST77XX_NUMOF
+#define ST77XX_NUMOF                0
 #endif
-#ifndef ST7735_SCREEN_NUMOF
-#define ST7735_SCREEN_NUMOF         0
+#ifndef ST77XX_SCREEN_NUMOF
+#define ST77XX_SCREEN_NUMOF         0
 #endif
 
-static st7735_t st7735_devs[ST7735_NUMOF];
+static st77xx_t st77xx_devs[ST77XX_NUMOF];
 
-static disp_dev_reg_t disp_dev_entries[ST7735_NUMOF];
+static disp_dev_reg_t disp_dev_entries[ST77XX_NUMOF];
 
-void auto_init_st7735(void)
+void auto_init_st77xx(void)
 {
-    assert(ST7735_NUMOF == ST7735_SCREEN_NUMOF);
+    assert(ST77XX_NUMOF == ST77XX_SCREEN_NUMOF);
 
-    for (size_t i = 0; i < ST7735_NUMOF; i++) {
-        st7735_devs[i].dev.driver = &lcd_st7735_driver;
+    for (size_t i = 0; i < ST77XX_NUMOF; i++) {
+        st77xx_devs[i].dev.driver = &lcd_st77xx_driver;
         LOG_DEBUG("[auto_init_screen] initializing st7735 #%u\n", i);
-        if (lcd_init(&st7735_devs[i].dev, &st7735_params[i]) < 0) {
+        if (lcd_init(&st77xx_devs[i].dev, &st77xx_params[i]) < 0) {
             LOG_ERROR("[auto_init_screen] error initializing st7735 #%u\n", i);
             continue;
         }
 
-        disp_dev_entries[i].dev = (disp_dev_t *) &st7735_devs[i].dev;
-        disp_dev_entries[i].screen_id = st7735_screen_ids[i];
+        disp_dev_entries[i].dev = (disp_dev_t *) &st77xx_devs[i].dev;
+        disp_dev_entries[i].screen_id = st77xx_screen_ids[i];
         disp_dev_entries[i].dev->driver = &lcd_disp_dev_driver;
 
         /* add to disp_dev registry */
