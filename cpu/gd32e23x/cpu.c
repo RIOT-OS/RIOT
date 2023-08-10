c7e52a3335f8b7033c72885d530688d9371be934
Jason Parker <jason.parker@bissell.com>
2023-08-10 12:19:38 -0400

    populate core cpu files

---
+++ cpu/gd32e23x/cpu.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2023 Bissell
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+/**
+ * @ingroup     cpu_gd32e23x
+ * @{
+ *
+ * @file
+ * @brief       GD32E23X CPU initialization
+ *
+ * @author      Jason Parker <jason.parker@bissell.com>
+ */
+// #include "stdio_uart.h"
+// #include "periph/init.h"
+// #include "irq_arch.h"
+// #include "periph_cpu.h"
+// #include "periph_conf.h"
+
+// #define ENABLE_DEBUG 0
+// #include "debug.h"
+
+// extern void __libc_init_array(void);
+
+void cpu_init(void)
+{
+    // TODO !
+    // gd32e23x_clock_init();
+    // cortexm_init();
+    // stdio_init();
+    // periph_init();
+}
