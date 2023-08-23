c31c769b9c2934c0c33ed6e074ca2e99ce7327fc
Jason Parker <jason.parker@bissell.com>
2023-08-23 09:11:14 -0400

    initial population of /boards/gd32vf103v-eval

---
+++ boards/gd32vf103v-eval/include/board.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2023 BISSELL Homecare, Inc.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_gd32vf103v-eval
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the GD32VF103V-EVAL
+ *
+ * @author      Jason Parker <jason.parker@bissell.com>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+#include "periph_conf.h"
+#include "periph_cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Put here board specific definitions:
+ - LEDs/buttons macros,
+ - on-board drivers parameters
+ - xtimer configuration
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
---
+++ boards/gd32vf103v-eval/include/periph_conf.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 BISSELL Homecare, Inc.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_gd32vf103v-eval
+ * @{
+ *
+ * @file
+ * @brief       Configuration of CPU peripherals for GD32VF103V-EVAL board
+ *
+ * @author      Jason Parker <jason.parker@bissell.com>
+ */
+
+#ifndef PERIPH_CONF_H
+#define PERIPH_CONF_H
+
+#include <stdint.h>
+
+#include "cpu.h"
+#include "periph_cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* put here the board peripherals definitions:
+- Available clocks
+- Timers
+- UARTs
+- PWMs
+- SPIs
+- I2C
+- ADC
+- RTC
+- RTT
+etc
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H */
+/** @} */
