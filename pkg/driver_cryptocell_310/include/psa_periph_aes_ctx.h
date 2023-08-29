c01d6897692a08622590db1c63e82ea7c916c84a
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2023-08-29 18:54:57 +0200

    pkg: Add cryptocell driver and nrf52 HW features

---
+++ pkg/driver_cryptocell_310/include/psa_periph_aes_ctx.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     pkg_driver_cryptocell_310
+ * @{
+ *
+ * @file
+ * @brief       CryptoCell 310 driver specific AES contexts
+ *
+ * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
+ *
+ */
+#ifndef PSA_PERIPH_AES_CTX_H
+#define PSA_PERIPH_AES_CTX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ssi_aes.h"
+#include "kernel_defines.h"
+
+#if IS_USED(MODULE_PERIPH_CIPHER_AES_128_CBC) || DOXYGEN
+/**
+ * @brief   Map driver specific AES context to PSA context
+ */
+typedef SaSiAesUserContext_t psa_cipher_aes_128_ctx_t;
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PSA_PERIPH_AES_CTX_H */
+/** @} */
