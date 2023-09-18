a5c4075c8fec11f21628878e9b7e1f90e25e2b2e
Maik Lorenz <lorenz.maik@web.de>
2023-09-18 15:09:59 +0200

    Create new package and module for matter

---
+++ sys/include/matter.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2023 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_matter Matter
+ * @ingroup     sys
+ * @brief       Matter Core module
+ *
+ * @{
+ *
+ * @file
+ *
+ * @author      Maik Lorenz <lorenz.maik@web.de>
+ */
+
+#ifndef MATTER_H
+#define MATTER_H
+
+/* Add header includes here */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Declare the API of the module */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MATTER_H */
+/** @} */
---
+++ sys/matter/matter.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2023 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     module_matter
+ * @{
+ *
+ * @file
+ * @brief       Matter implementation
+ *
+ * @author      Maik Lorenz <lorenz.maik@web.de>
+ *
+ * @}
+ */
+
+#include "matter.h"
+#include <stdio.h>
+
+/* Implementation of the module */
