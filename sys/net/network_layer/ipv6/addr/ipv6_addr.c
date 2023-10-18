a3d6e8168a030a59e833a410415b84e46b46f2fd
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2023-10-18 16:55:18 +0200

    net/ipv6: optimize ipv6_addr_init_prefix()

--- sys/net/network_layer/ipv6/addr/ipv6_addr.c
+++ sys/net/network_layer/ipv6/addr/ipv6_addr.c
@@ -78,11 +78,12 @@ void ipv6_addr_init_prefix(ipv6_addr_t *out, const ipv6_addr_t *prefix,
     }
 
     bytes = bits / 8;
+    bits  = bits % 8;
 
     memcpy(out, prefix, bytes);
 
-    if (bits % 8) {
-        uint8_t mask = 0xff << (8 - (bits - (bytes * 8)));
+    if (bits) {
+        uint8_t mask = 0xff << (8 - bits);
 
         out->u8[bytes] &= ~mask;
         out->u8[bytes] |= (prefix->u8[bytes] & mask);
