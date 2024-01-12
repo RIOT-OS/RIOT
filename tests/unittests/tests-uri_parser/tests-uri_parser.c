a6a6e3169902a0a60c2735a8e1757820ce6c1cf5
Frederik Haxel <haxel@fzi.de>
2024-01-12 11:59:48 +0100

    tests/unittests/tests-uri_parser: Added PRIuSIZE

--- tests/unittests/tests-uri_parser/tests-uri_parser.c
+++ tests/unittests/tests-uri_parser/tests-uri_parser.c
@@ -57,9 +57,10 @@ static int _compare_string_buffer(const char *name, const char *input_uri, const
         (strncmp(actual_str, expected_str, actual_len) == 0)) {
         return 0;
     }
-    printf(
-        "\nWith given input uri '%s', expected %s '%s' with length '%" PRIuSIZE "' but got '%.*s' with length '%d'\n",
-        input_uri, name, expected_str, strlen(expected_str), actual_len, actual_str, actual_len);
+    printf("\nWith given input uri '%s', expected %s '%s' with length '%"
+           PRIuSIZE "' but got '%.*s' with length '%" PRIuSIZE "'\n",
+           input_uri, name, expected_str, strlen(expected_str),
+           (unsigned) actual_len, actual_str, actual_len);
     return -1;
 }
 
