02b9cd185b7ebda874cdadbc48159b2171ce400d
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2024-01-16 19:21:38 +0100

    nanocoap: make token const in coap_build_hdr()

--- sys/include/net/nanocoap.h
+++ sys/include/net/nanocoap.h
@@ -1922,7 +1922,7 @@ ssize_t coap_block2_build_reply(coap_pkt_t *pkt, unsigned code,
  *
  * @returns      length of resulting header
  */
-ssize_t coap_build_hdr(coap_hdr_t *hdr, unsigned type, uint8_t *token,
+ssize_t coap_build_hdr(coap_hdr_t *hdr, unsigned type, const void *token,
                        size_t token_len, unsigned code, uint16_t id);
 
 /**
