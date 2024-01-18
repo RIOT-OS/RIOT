1c24462cd9b8a2725d9a94387c2c7819f9e4bd79
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2024-01-18 14:59:34 +0100

    nanocoap: implement coap_find_uri_query()

--- sys/net/application_layer/nanocoap/nanocoap.c
+++ sys/net/application_layer/nanocoap/nanocoap.c
@@ -439,6 +439,45 @@ int coap_get_blockopt(coap_pkt_t *pkt, uint16_t option, uint32_t *blknum, uint8_
     return (blkopt & 0x8) ? 1 : 0;
 }
 
+bool coap_find_uri_query(coap_pkt_t *pkt, const char *key, const char **value, size_t *len)
+{
+    uint8_t *opt_pos = NULL;
+
+    while (1) {
+        int len_query;
+        const void *key_data = coap_iterate_option(pkt, COAP_OPT_URI_QUERY,
+                                                   &opt_pos, &len_query);
+        if (key_data == NULL) {
+            return false;
+        }
+
+        const char *separator = memchr(key_data, '=', len_query);
+        size_t len_key = separator
+                       ? (separator - (char *)key_data)
+                       : len_query;
+
+        if (memcmp(key, key_data, len_key)) {
+            continue;
+        }
+
+        if (value == NULL) {
+            return true;
+        }
+
+        assert(len);
+        if (separator) {
+            *value = separator + 1;
+            *len = len_query - len_key - 1;
+        } else {
+            *value = NULL;
+            *len   = 0;
+        }
+        return true;
+    }
+
+    return false;
+}
+
 bool coap_has_unprocessed_critical_options(const coap_pkt_t *pkt)
 {
     for (unsigned i = 0; i < sizeof(pkt->opt_crit); ++i){
