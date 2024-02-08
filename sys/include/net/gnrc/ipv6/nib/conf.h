843b501eeb79533ec8fc13e43b3f1c4ad9ed6d4b
xnumad <34810600+xnumad@users.noreply.github.com>
2024-02-08 16:29:48 +0100

    finalize: doccheck

--- sys/include/net/gnrc/ipv6/nib/conf.h
+++ sys/include/net/gnrc/ipv6/nib/conf.h
@@ -192,6 +192,10 @@ extern "C" {
 #  define CONFIG_GNRC_IPV6_NIB_SLAAC                  1
 #endif
 
+/**
+ * @brief Use temporary addresses (rfc8981)
+ * @see [RFC 8981](https://www.rfc-editor.org/rfc/rfc8981.html)
+ */
 #ifndef CONFIG_GNRC_IPV6_NIB_SLAAC_TEMPORARY_ADDRESSES
 #define CONFIG_GNRC_IPV6_NIB_SLAAC_TEMPORARY_ADDRESSES 0
 #endif
