d6e6c4f7a162eea240ec3e44f6fd870cf28c14f7
Dylan Laduranty <dylan.laduranty@mesotic.com>
2024-03-14 22:00:00 +0100

    treewide: remove deprecated i2c_scan module
    Signed-off-by: Dylan Laduranty <dylan.laduranty@mesotic.com>

--- makefiles/deprecated_modules.inc.mk
+++ makefiles/deprecated_modules.inc.mk
@@ -1,6 +1,5 @@
 # Add deprecated modules here
 # Keep this list ALPHABETICALLY SORTED!!!!111elven
-DEPRECATED_MODULES += i2c_scan # use shell_cmd_i2c_scan instead
 DEPRECATED_MODULES += md5sum # use shell_cmd_md5sum instead
 DEPRECATED_MODULES += nice # use shell_cmd_nice instead
 DEPRECATED_MODULES += random_cmd # use shell_cmd_random instead
--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -237,13 +237,6 @@ PSEUDOMODULES += gnrc_sixlowpan_router_default
 PSEUDOMODULES += gnrc_sock_async
 PSEUDOMODULES += gnrc_sock_check_reuse
 PSEUDOMODULES += gnrc_txtsnd
-## @defgroup pseudomodule_i2c_scan i2c_scan
-## @ingroup sys_shell_commands
-## @{
-## @deprecated  Use module `shell_cmd_i2c_scan` instead;
-##              will be removed after 2023.07 release.
-PSEUDOMODULES += i2c_scan
-## @}
 PSEUDOMODULES += ieee802154_security
 PSEUDOMODULES += ieee802154_submac
 PSEUDOMODULES += ipv4
