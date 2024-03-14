ade7d4125db25dcccc7dde2185db49cc07a2fa84
Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
2024-03-14 15:19:02 +0100

    fixup! fixup! fixup! fixup! fixup! sys/psa_crypto: ed25519 private key {ex,im}port

--- tests/sys/psa_crypto/main.c
+++ tests/sys/psa_crypto/main.c
@@ -31,8 +31,10 @@ void addFailurePSA(const char *func, psa_status_t errcode, long line, const char
     addFailure(msg, line, file);
 }
 
-#define TEST_ASSERT_PSA(func_) { psa_status_t ret = func_; if (ret != PSA_SUCCESS) { addFailurePSA(#func_, ret, __LINE__, __FILE__); goto cleanup; } }
-#define TEST_ASSERT_PSA_WITHIN_CLEANUP(func_) { psa_status_t ret = func_; if (ret != PSA_SUCCESS) { addFailurePSA(#func_, ret, __LINE__, __FILE__); } }
+#define TEST_ASSERT_PSA(func_, do_) { psa_status_t ret = func_; if (ret != PSA_SUCCESS) { addFailurePSA(#func_, ret, __LINE__, __FILE__); do_; } }
+#define TEST_ASSERT_PSA_CLEANUP(func_) TEST_ASSERT_PSA(func_, goto cleanup)
+#define TEST_ASSERT_PSA_RETURN(func_) TEST_ASSERT_PSA(func_, return)
+#define TEST_ASSERT_PSA_CONTINUE(func_) TEST_ASSERT_PSA(func_, )
 
 /*
  * A second call to psa_crypto_init() should not reset key data.
@@ -46,22 +48,22 @@ static void test_init_twice(void)
     uint8_t key_data[PSA_EXPORT_PUBLIC_KEY_OUTPUT_SIZE(key_type, key_bits)];
     size_t key_data_len;
 
-    TEST_ASSERT_PSA(psa_crypto_init());
-
     psa_key_id_t key_id = PSA_KEY_ID_NULL;
     psa_key_attributes_t key_attr = psa_key_attributes_init();
     psa_set_key_algorithm(&key_attr, key_alg);
     psa_set_key_usage_flags(&key_attr, PSA_KEY_USAGE_EXPORT);
     psa_set_key_bits(&key_attr, key_bits);
     psa_set_key_type(&key_attr, key_type);
-    TEST_ASSERT_PSA(psa_generate_key(&key_attr, &key_id));
 
-    TEST_ASSERT_PSA(psa_export_public_key(key_id, key_data, sizeof(key_data), &key_data_len));
-    TEST_ASSERT_PSA(psa_crypto_init());
-    TEST_ASSERT_PSA(psa_export_public_key(key_id, key_data, sizeof(key_data), &key_data_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_crypto_init());
+    TEST_ASSERT_PSA_CLEANUP(psa_generate_key(&key_attr, &key_id));
+
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id, key_data, sizeof(key_data), &key_data_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_crypto_init());
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id, key_data, sizeof(key_data), &key_data_len));
 
 cleanup:
-    TEST_ASSERT_PSA_WITHIN_CLEANUP(psa_destroy_key(key_id));
+    TEST_ASSERT_PSA_CONTINUE(psa_destroy_key(key_id));
 }
 
 /**
@@ -99,29 +101,29 @@ static void test_exported_key_is_identical_when_imported_again_ed25519(void)
     psa_set_key_type(&key_attr, key_type);
 
     // randomly generate keypair
-    TEST_ASSERT_PSA(psa_crypto_init());
-    TEST_ASSERT_PSA(psa_generate_key(&key_attr, &key_id));
+    TEST_ASSERT_PSA_CLEANUP(psa_crypto_init());
+    TEST_ASSERT_PSA_CLEANUP(psa_generate_key(&key_attr, &key_id));
 
     // sign msg with generated keypair
-    TEST_ASSERT_PSA(psa_sign_message(key_id, key_alg, msg, sizeof(msg), sig, sizeof(sig), &sig_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_sign_message(key_id, key_alg, msg, sizeof(msg), sig, sizeof(sig), &sig_len));
 
     // export public and private key, then free slot
-    TEST_ASSERT_PSA(psa_export_public_key(key_id, pubkey, sizeof(pubkey), &pubkey_len));
-    TEST_ASSERT_PSA(psa_export_key(key_id, privkey, sizeof(privkey), &privkey_len));
-    TEST_ASSERT_PSA(psa_destroy_key(key_id));
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id, pubkey, sizeof(pubkey), &pubkey_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_export_key(key_id, privkey, sizeof(privkey), &privkey_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_destroy_key(key_id));
 
     // import private key and compare public keys
     psa_set_key_usage_flags(&key_attr, PSA_KEY_USAGE_SIGN_MESSAGE);
-    TEST_ASSERT_PSA(psa_import_key(&key_attr, privkey, privkey_len, &key_id));
-    TEST_ASSERT_PSA(psa_export_public_key(key_id, pubkey2, sizeof(pubkey2), &pubkey2_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_import_key(&key_attr, privkey, privkey_len, &key_id));
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id, pubkey2, sizeof(pubkey2), &pubkey2_len));
     TEST_ASSERT(pubkey_len == pubkey2_len && memcmp(pubkey, pubkey2, pubkey_len) == 0);
 
     // sign msg with imported key and compare signatures
-    TEST_ASSERT_PSA(psa_sign_message(key_id, key_alg, msg, sizeof(msg), sig2, sizeof(sig2), &sig2_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_sign_message(key_id, key_alg, msg, sizeof(msg), sig2, sizeof(sig2), &sig2_len));
     TEST_ASSERT(sig_len == sig2_len && memcmp(sig, sig2, sig_len) == 0);
 
 cleanup:
-    TEST_ASSERT_PSA_WITHIN_CLEANUP(psa_destroy_key(key_id));
+    TEST_ASSERT_PSA_CONTINUE(psa_destroy_key(key_id));
 }
 
 /**
@@ -149,29 +151,30 @@ static void test_export_public_key_ed25519(void)
     psa_set_key_bits(&key_attr, key_bits);
     psa_set_key_type(&key_attr, key_type);
 
-    // randomly generate keypair
-    TEST_ASSERT_PSA(psa_crypto_init());
-    TEST_ASSERT_PSA(psa_generate_key(&key_attr, &key_id));
-
-    // export public key
-    TEST_ASSERT_PSA(psa_export_public_key(key_id, pubkey, sizeof(pubkey), &pubkey_len));
-
-    // import public key into single-key slot
     psa_key_id_t key_id2 = PSA_KEY_ID_NULL;
     psa_set_key_type(&key_attr, key_type2);
-    TEST_ASSERT_PSA(psa_import_key(&key_attr, pubkey, pubkey_len, &key_id2));
+
+    // randomly generate keypair
+    TEST_ASSERT_PSA_CLEANUP(psa_crypto_init());
+    TEST_ASSERT_PSA_CLEANUP(psa_generate_key(&key_attr, &key_id));
+
+    // export public key
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id, pubkey, sizeof(pubkey), &pubkey_len));
+
+    // import public key into single-key slot
+    TEST_ASSERT_PSA_CLEANUP(psa_import_key(&key_attr, pubkey, pubkey_len, &key_id2));
 
     // export public key from single slot using `psa_export_key()` and compare
-    TEST_ASSERT_PSA(psa_export_key(key_id2, pubkey2, sizeof(pubkey2), &pubkey2_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_export_key(key_id2, pubkey2, sizeof(pubkey2), &pubkey2_len));
     TEST_ASSERT(pubkey_len == pubkey2_len && memcmp(pubkey, pubkey2, pubkey_len) == 0);
 
     // export public key from single slot using `psa_export_public_key()` and compare again
-    TEST_ASSERT_PSA(psa_export_public_key(key_id2, pubkey2, sizeof(pubkey2), &pubkey2_len));
+    TEST_ASSERT_PSA_CLEANUP(psa_export_public_key(key_id2, pubkey2, sizeof(pubkey2), &pubkey2_len));
     TEST_ASSERT(pubkey_len == pubkey2_len && memcmp(pubkey, pubkey2, pubkey_len) == 0);
 
 cleanup:
-    TEST_ASSERT_PSA_WITHIN_CLEANUP(psa_destroy_key(key_id));
-    TEST_ASSERT_PSA_WITHIN_CLEANUP(psa_destroy_key(key_id2));
+    TEST_ASSERT_PSA_CONTINUE(psa_destroy_key(key_id));
+    TEST_ASSERT_PSA_CONTINUE(psa_destroy_key(key_id2));
 }
 
 static Test *tests_psa_crypto(void)
