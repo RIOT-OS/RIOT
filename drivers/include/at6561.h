ccd7fecde7accf9a6448748234361a6893924add
Firas Hamdi <firas.hamdi@ml-pa.com>
2024-03-19 09:28:25 +0100

    fix CI check errors

--- drivers/include/at6561.h
+++ drivers/include/at6561.h
@@ -7,26 +7,44 @@
  */
 
 /**
- * @ingroup         drivers_at6561
+ * @defgroup        drivers_at6561 AT6561 CAN transceiver driver
+ * @ingroup         drivers_can
+ * @ingroup         drivers_can_trx
+ *
+ * @brief           Driver for AT6561 CAN transceiver
+ * @{
  *
  * @file
- * @brief           Driver for AT6561 CAN transceiver
  *
  * @author          Firas Hamdi <firas.hamdi@ml-pa.com>
  */
 
+#ifndef AT6561_H
+#define AT6561_H
+
 #include "periph/gpio.h"
 #include "can/can_trx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * @brief AT6561 CAN transceiver descriptor
  */
 typedef struct at6561_trx {
-	/* AT6561 standby pin */
-	gpio_t stby_pin;
-	/* AT6561 interface */
-	can_trx_t trx;
+    gpio_t stby_pin;            /**< AT6561 standby pin */
+    can_trx_t trx;              /**< AT6561 interface */
 } at6561_trx_t;
 
-/* AT6561 driver */
+/**
+ * @brief AT6561 driver
+ */
 extern const trx_driver_t at6561_driver;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AT6561_H */
+/** @} */
