39bcb60651bdee5218c084c6c74b5b290ff63823
riot <riot@example.com>
2024-03-26 19:07:54 +0100

    wip

--- tests/periph_pdm/testing_pdm_conversion.py
+++ tests/periph_pdm/testing_pdm_conversion.py
@@ -14,7 +14,7 @@ import time
 str1 = "Start of the new buffer".encode('ascii')
 str2= "End of the new buffer".encode('ascii')
 
-start_index_buf = 0
+start_index_buf = 0    
 stop_index_buf = 0
 start_found = False
 buffer_number = 0
@@ -31,20 +31,17 @@ while start:
     value = line.strip()
     data[key] = value
     if line.find(str1) != -1:
-        #print("Start Recording")
         start_found = True
         start_index_buf = key
     if line.find(str2) != -1:
-
         if start_found:
             stop_index_buf = key
             buffer_number = stop_index_buf - start_index_buf - 2
             start = False
-            #print("Stop recording")
             break
     key +=1
 
-print(start_index_buf)
+print(start_index_buf)     
 print(stop_index_buf)
 print(buffer_number)
 ser.close()
@@ -52,8 +49,6 @@ ser.close()
 # Extract the data within the specified range
 data_to_plot = [int(data[i]) for i in range(start_index_buf+1, stop_index_buf)]
 data_to_bytes = [int(data[i]).to_bytes(2,byteorder="little",signed=True) for i in range(start_index_buf+1, stop_index_buf)]
-data_to_export = [(data[i]) for i in range(start_index_buf+1, stop_index_buf)]
-#print(data_to_bytes)
 
 # Save the data to 16-b file
 # Open a file in binary write mode
@@ -61,22 +56,6 @@ with open("binary_data.bin", "wb") as f:
     for binary_data in data_to_bytes:
         # Write the binary data to the file
         f.write(binary_data)
-
-# Sample rate (Hz)
-sample_rate = 16000
-
-# Open a new wave file
-with wave.open("output_wave.wav", "w") as output_wave:
-    # Set parameters for the wave file
-    output_wave.setnchannels(1)  # Mono
-    output_wave.setsampwidth(2)  # 2 bytes per sample for 16-bit audio
-    output_wave.setframerate(sample_rate)
-
-    # Convert data_to_plot to numpy array
-    data_np = np.array(data_to_bytes)
-
-    # Convert data to bytes and write to the wave file
-    output_wave.writeframes(data_np)
 
 # Sample rate (Hz)
 samplerate = 16000
@@ -94,6 +73,7 @@ audio = np.array([left_channel]).T
 # Convert to (little-endian) 16 bit integers.
 audio = (audio * (2 ** 15 - 1)).astype("<h")
 
+# Save the data as a wave format
 with wave.open("sound_new_buf.wav", "w") as f:
     # Mono Channel
     f.setnchannels(1)
