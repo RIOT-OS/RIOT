0acb666da680a5a457dfa5659a2e341a4b4e6283
Martine Lenders <m.lenders@fu-berlin.de>
2024-03-27 10:50:23 +0100

    gnrc_mac: deprecate module

--- makefiles/deprecated_modules.inc.mk
+++ makefiles/deprecated_modules.inc.mk
@@ -1,4 +1,5 @@
 # Add deprecated modules here
 # Keep this list ALPHABETICALLY SORTED!!!!111elven
+DEPRECATED_MODULES += gnrc_mac
 DEPRECATED_MODULES += sema_deprecated
 DEPRECATED_MODULES += ztimer_now64
