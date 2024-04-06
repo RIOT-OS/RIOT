8b00112774704b1c2e01e6e372c87eae1285106a
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-04-06 15:13:20 +0200

    fixup! sys/shell: add registry cli

--- sys/shell/cmds/registry.c
+++ sys/shell/cmds/registry.c
@@ -40,8 +40,7 @@
 #define REGISTRY_CLI_PATH_SEPARATOR    '/'
 
 static int _parse_string_path(const char *string_path,
-                              registry_int_path_t *registry_path,
-                              registry_int_path_type_t *registry_path_type)
+                              registry_int_path_t *registry_path)
 {
     char *ptr = (char *)string_path;
 
@@ -53,10 +52,10 @@ static int _parse_string_path(const char *string_path,
     registry_namespace_id_t namespace_id = strtol(ptr, &ptr, 10);
 
     if (*ptr == '\0') {
-        registry_path->namespace_path = (registry_namespace_int_path_t) {
+        registry_path->type = REGISTRY_INT_PATH_TYPE_NAMESPACE;
+        registry_path->value.namespace_path = (registry_namespace_int_path_t) {
             .namespace_id = namespace_id,
         };
-        *registry_path_type = REGISTRY_INT_PATH_TYPE_NAMESPACE;
         return 0;
     }
 
@@ -65,11 +64,11 @@ static int _parse_string_path(const char *string_path,
     registry_schema_id_t schema_id = strtol(ptr, &ptr, 10);
 
     if (*ptr == '\0') {
-        registry_path->schema_path = (registry_schema_int_path_t) {
+        registry_path->type = REGISTRY_INT_PATH_TYPE_SCHEMA;
+        registry_path->value.schema_path = (registry_schema_int_path_t) {
             .namespace_id = namespace_id,
             .schema_id = schema_id,
         };
-        *registry_path_type = REGISTRY_INT_PATH_TYPE_SCHEMA;
         return 0;
     }
 
@@ -78,12 +77,12 @@ static int _parse_string_path(const char *string_path,
     registry_instance_id_t instance_id = strtol(ptr, &ptr, 10);
 
     if (*ptr == '\0') {
-        registry_path->instance_path = (registry_instance_int_path_t) {
+        registry_path->type = REGISTRY_INT_PATH_TYPE_INSTANCE;
+        registry_path->value.instance_path = (registry_instance_int_path_t) {
             .namespace_id = namespace_id,
             .schema_id = schema_id,
             .instance_id = instance_id,
         };
-        *registry_path_type = REGISTRY_INT_PATH_TYPE_INSTANCE;
         return 0;
     }
 
@@ -92,22 +91,20 @@ static int _parse_string_path(const char *string_path,
     registry_group_or_parameter_id_t group_or_parameter_id = strtol(ptr, &ptr, 10);
 
     if (*ptr == '\0') {
-        registry_path->group_or_parameter_path = (registry_group_or_parameter_int_path_t) {
+        registry_path->type = REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER;
+        registry_path->value.group_or_parameter_path = (registry_group_or_parameter_int_path_t) {
             .namespace_id = namespace_id,
             .schema_id = schema_id,
             .instance_id = instance_id,
             .group_or_parameter_id = group_or_parameter_id,
         };
-        *registry_path_type = REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER;
         return 0;
     }
 
     return 0;
 }
 
-static int _export_cb(const registry_export_cb_data_t *data,
-                      const registry_export_cb_data_type_t data_type,
-                      const void *context)
+static int _export_cb(const registry_node_t *node, const void *context)
 {
     (void)context;
 
@@ -116,46 +113,46 @@ static int _export_cb(const registry_export_cb_data_t *data,
     /* the more nesting we have, the more indentation we need. => highest nesting level first */
     size_t indentation = 0;
 
-    switch (data_type) {
-    case REGISTRY_EXPORT_PARAMETER:
-    case REGISTRY_EXPORT_GROUP:
+    switch (node->type) {
+    case REGISTRY_NODE_PARAMETER:
+    case REGISTRY_NODE_GROUP:
         indentation++;
         __attribute__ ((fallthrough));
 
-    case REGISTRY_EXPORT_INSTANCE:
+    case REGISTRY_NODE_INSTANCE:
         indentation++;
         __attribute__ ((fallthrough));
 
-    case REGISTRY_EXPORT_SCHEMA:
+    case REGISTRY_NODE_SCHEMA:
         indentation++;
         __attribute__ ((fallthrough));
 
-    case REGISTRY_EXPORT_NAMESPACE:
+    case REGISTRY_NODE_NAMESPACE:
         indentation++;
     }
 
     printf("%*c\b", ((indentation - 1) * 2) + 1, ' ');
 
     /* print the path element, that is currently being exported */
-    switch (data_type) {
-    case REGISTRY_EXPORT_NAMESPACE:
-        printf("%d %s\n", data->namespace->id, data->namespace->name);
+    switch (node->type) {
+    case REGISTRY_NODE_NAMESPACE:
+        printf("%d %s\n", node->location.namespace->id, node->location.namespace->name);
         break;
 
-    case REGISTRY_EXPORT_SCHEMA:
-        printf("%d %s\n", data->schema->id, data->schema->name);
+    case REGISTRY_NODE_SCHEMA:
+        printf("%d %s\n", node->location.schema->id, node->location.schema->name);
         break;
 
-    case REGISTRY_EXPORT_INSTANCE:
-        printf("%d %s\n", data->instance->id, data->instance->name);
+    case REGISTRY_NODE_INSTANCE:
+        printf("%d %s\n", node->instance->id, node->instance->name);
         break;
 
-    case REGISTRY_EXPORT_GROUP:
-        printf("%d %s (group)\n", data->group->id, data->group->name);
+    case REGISTRY_NODE_GROUP:
+        printf("%d %s (group)\n", node->location.group->id, node->location.group->name);
         break;
 
-    case REGISTRY_EXPORT_PARAMETER:
-        printf("%d %s\n", data->parameter.data->id, data->parameter.data->name);
+    case REGISTRY_NODE_PARAMETER:
+        printf("%d %s\n", node->location.parameter->id, node->location.parameter->name);
         break;
     }
 
@@ -165,12 +162,7 @@ static int _export_cb(const registry_export_cb_data_t *data,
 static int _registry(int argc, char **argv)
 {
     registry_int_path_t path;
-    registry_int_path_type_t path_type;
-    registry_namespace_t *namespace;
-    registry_schema_t *schema;
-    registry_instance_t *instance;
-    registry_group_t *group;
-    registry_parameter_t *parameter;
+    registry_node_t node;
     registry_value_t value;
 
     if (argc == 1) {
@@ -179,26 +171,24 @@ static int _registry(int argc, char **argv)
     }
 
     if (strcmp(argv[1], "get") == 0) {
-        if (_parse_string_path(argv[2], &path, &path_type) < 0) {
+        if (_parse_string_path(argv[2], &path) < 0) {
             printf("usage: %s %s <path>\n", argv[0], argv[1]);
             return 1;
         }
 
-        if (path_type != REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER) {
+        if (path.type != REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER) {
             return -EINVAL;
         }
 
         /* get instance and parameter of the path */
-        int res = registry_from_group_or_parameter_int_path(&path.group_or_parameter_path,
-                                                            &path_type, NULL, NULL, &instance,
-                                                            &group, &parameter);
+        int res = registry_node_from_int_path(&path, &node);
 
-        if (path_type != REGISTRY_INT_PATH_TYPE_PARAMETER) {
+        if (node.type != REGISTRY_NODE_PARAMETER) {
             return -EINVAL;
         }
 
         if (res == 0) {
-            res = registry_get(instance, parameter, &value);
+            res = registry_get(&node, &value);
 
             if (res == 0) {
                 /* get the length of the value as a string */
@@ -218,27 +208,25 @@ static int _registry(int argc, char **argv)
         return 1;
     }
     else if (strcmp(argv[1], "set") == 0) {
-        if (_parse_string_path(argv[2], &path, &path_type) < 0) {
+        if (_parse_string_path(argv[2], &path) < 0) {
             printf("usage: %s %s <path> <value>\n", argv[0], argv[1]);
             return 1;
         }
 
-        if (path_type != REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER) {
+        if (path.type != REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER) {
             return -EINVAL;
         }
 
         /* get instance and parameter of the path */
-        int res = registry_from_group_or_parameter_int_path(&path.group_or_parameter_path,
-                                                            &path_type, NULL, NULL, &instance,
-                                                            &group, &parameter);
+        int res = registry_node_from_int_path(&path, &node);
 
-        if (path_type != REGISTRY_INT_PATH_TYPE_PARAMETER) {
+        if (node.type != REGISTRY_NODE_PARAMETER) {
             return -EINVAL;
         }
 
         if (res == 0) {
             /* get value from the registry, to know its correct type and size */
-            res = registry_get(instance, parameter, &value);
+            res = registry_get(&node, &value);
 
             if (res == 0) {
                 /* convert the string into the correct value type */
@@ -250,7 +238,7 @@ static int _registry(int argc, char **argv)
                 value.buf = new_value_buf;
 
                 /* set the new value in the registry */
-                registry_set(instance, parameter, value.buf, value.buf_len);
+                registry_set(&node, value.buf, value.buf_len);
                 return 0;
             }
         }
@@ -259,61 +247,17 @@ static int _registry(int argc, char **argv)
         return 1;
     }
     else if (strcmp(argv[1], "commit") == 0) {
-        if (_parse_string_path(argv[2], &path, &path_type) < 0) {
+        if (_parse_string_path(argv[2], &path) < 0) {
             printf("usage: %s %s <path>\n", argv[0], argv[1]);
             return 1;
         }
 
-        int res = 0;
+        int res = registry_node_from_int_path(&path, &node);
 
-        /* commit depending on the path type */
-        switch (path_type) {
-        case REGISTRY_INT_PATH_TYPE_NAMESPACE:
-            res = registry_from_namespace_int_path(&path.namespace_path, &namespace);
-            if (res == 0) {
-                res = registry_commit_namespace(namespace);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_SCHEMA:
-            res = registry_from_schema_int_path(&path.schema_path, &namespace, &schema);
-            if (res == 0) {
-                res = registry_commit_schema(schema);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_INSTANCE:
-            res = registry_from_instance_int_path(&path.instance_path, &namespace, &schema,
-                                                  &instance);
-            if (res == 0) {
-                res = registry_commit_instance(instance);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_GROUP:
-            /* should not happen as we don't yet know if it is a group or a parameter */
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_PARAMETER:
-            /* should not happen as we don't yet know if it is a group or a parameter */
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER:
-            res = registry_from_group_or_parameter_int_path(&path.group_or_parameter_path,
-                                                            &path_type,
-                                                            &namespace, &schema, &instance,
-                                                            &group, &parameter);
-            if (res == 0) {
-                if (path_type == REGISTRY_INT_PATH_TYPE_GROUP) {
-                    res = registry_commit_group(instance, group);
-                }
-                else if (path_type == REGISTRY_INT_PATH_TYPE_PARAMETER) {
-                    res = registry_commit_parameter(instance, parameter);
-                }
-            }
-            break;
+        if (res == 0) {
+            res = registry_commit(&node);
         }
-
+        
         if (res != 0) {
             printf("error: %d\n", res);
             return 1;
@@ -324,70 +268,27 @@ static int _registry(int argc, char **argv)
     else if (strcmp(argv[1], "export") == 0) {
         /* if the path is invalid, it can also just be non existent, so other arguments like -r need to be checked */
         bool invalid_path = false;
-        if (_parse_string_path(argv[2], &path, &path_type) < 0) {
+        if (_parse_string_path(argv[2], &path) < 0) {
             invalid_path = true;
         }
         if (invalid_path && strcmp(argv[2], "-r") != 0) {
-            printf("usage: %s %s <path> [-r <recursion depth>]\n", argv[0], argv[1]);
+            printf("usage: %s %s <path> [-r <tree traversal depth>]\n", argv[0], argv[1]);
             return 1;
         }
 
         /* the argv index of -r varies depending on if a path was specified or not */
-        int recursion_level = 0;
+        int tree_traversal_level = 0;
         if (invalid_path && argc > 3 && strcmp(argv[2], "-r") == 0) {
-            recursion_level = atoi(argv[3]);
+            tree_traversal_level = atoi(argv[3]);
         }
         else if (argc > 4 && strcmp(argv[3], "-r") == 0) {
-            recursion_level = atoi(argv[4]);
+            tree_traversal_level = atoi(argv[4]);
         }
 
-        int res = 0;
+        int res = registry_node_from_int_path(&path, &node);
 
-        switch (path_type) {
-        case REGISTRY_INT_PATH_TYPE_NAMESPACE:
-            res = registry_from_namespace_int_path(&path.namespace_path, &namespace);
-            if (res == 0) {
-                res = registry_export_namespace(namespace, _export_cb, recursion_level, NULL);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_SCHEMA:
-            res = registry_from_schema_int_path(&path.schema_path, &namespace, &schema);
-            if (res == 0) {
-                res = registry_export_schema(schema, _export_cb, recursion_level, NULL);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_INSTANCE:
-            res = registry_from_instance_int_path(&path.instance_path, &namespace, &schema,
-                                                  &instance);
-            if (res == 0) {
-                res = registry_export_instance(instance, _export_cb, recursion_level, NULL);
-            }
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_GROUP:
-            /* should not happen as we don't yet know if it is a group or a parameter */
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_PARAMETER:
-            /* should not happen as we don't yet know if it is a group or a parameter */
-            break;
-
-        case REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER:
-            res = registry_from_group_or_parameter_int_path(&path.group_or_parameter_path,
-                                                            &path_type,
-                                                            &namespace, &schema, &instance,
-                                                            &group, &parameter);
-            if (res == 0) {
-                if (path_type == REGISTRY_INT_PATH_TYPE_GROUP) {
-                    res = registry_export_group(instance, group, _export_cb, recursion_level, NULL);
-                }
-                else if (path_type == REGISTRY_INT_PATH_TYPE_PARAMETER) {
-                    res = registry_export_parameter(instance, parameter, _export_cb, NULL);
-                }
-            }
-            break;
+        if (res == 0) {
+            res = registry_export(&node, _export_cb, tree_traversal_level, NULL);
         }
 
         if (res != 0) {
@@ -412,61 +313,19 @@ static int _registry(int argc, char **argv)
         int res = 0;
 
         if (argc > 2) {
-            if (_parse_string_path(argv[2], &path, &path_type) < 0) {
+            if (_parse_string_path(argv[2], &path) < 0) {
                 printf("usage: %s %s [path]\n", argv[0], argv[1]);
                 return 1;
             }
 
-            /* commit depending on the path type */
-            switch (path_type) {
-            case REGISTRY_INT_PATH_TYPE_NAMESPACE:
-                res = registry_from_namespace_int_path(&path.namespace_path, &namespace);
-                if (res == 0) {
-                    res = registry_save_namespace(namespace);
-                }
-                break;
-
-            case REGISTRY_INT_PATH_TYPE_SCHEMA:
-                res = registry_from_schema_int_path(&path.schema_path, &namespace, &schema);
-                if (res == 0) {
-                    res = registry_save_schema(schema);
-                }
-                break;
-
-            case REGISTRY_INT_PATH_TYPE_INSTANCE:
-                res = registry_from_instance_int_path(&path.instance_path, &namespace, &schema,
-                                                      &instance);
-                if (res == 0) {
-                    res = registry_save_instance(instance);
-                }
-                break;
-
-            case REGISTRY_INT_PATH_TYPE_GROUP:
-                /* should not happen as we don't yet know if it is a group or a parameter */
-                break;
-
-            case REGISTRY_INT_PATH_TYPE_PARAMETER:
-                /* should not happen as we don't yet know if it is a group or a parameter */
-                break;
-
-            case REGISTRY_INT_PATH_TYPE_GROUP_OR_PARAMETER:
-                res = registry_from_group_or_parameter_int_path(&path.group_or_parameter_path,
-                                                                &path_type,
-                                                                &namespace, &schema, &instance,
-                                                                &group, &parameter);
-                if (res == 0) {
-                    if (path_type == REGISTRY_INT_PATH_TYPE_GROUP) {
-                        res = registry_save_group(instance, group);
-                    }
-                    else if (path_type == REGISTRY_INT_PATH_TYPE_PARAMETER) {
-                        res = registry_save_parameter(instance, parameter);
-                    }
-                }
-                break;
+            res = registry_node_from_int_path(&path, &node);
+            if (res == 0) {
+                res = registry_save(&node);
             }
         }
         else {
-            res = registry_save();
+            // save everything
+            res = registry_save(NULL);
         }
 
         if (res != 0) {
