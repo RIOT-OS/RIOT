f1cbcf51e5887a61028a7cd3862abaf3f759fd3b
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-04-12 15:05:44 +0200

    fixup! examples: add registry example
    Add more comments and address review

--- examples/registry_cli/main.c
+++ examples/registry_cli/main.c
@@ -11,7 +11,8 @@
  * @{
  *
  * @file
- * @brief       RIOT Registry example application
+ * @brief       RIOT registry CLI example application to demonstrate how
+ *              to use the RIOT registry using the CLI.
  *
  * @author      Lasse Rosenow <lasse.rosenow@haw-hamburg.de>
  *
@@ -34,34 +35,49 @@
 #include "registry/string_path.h"
 
 /* this callback is usually implemented drivers such as an RGB LED driver */
-int rgb_led_instance_commit_cb(const registry_commit_cb_scope_t scope,
-                                 const registry_group_or_parameter_id_t *group_or_parameter_id,
-                                 const void *context)
+static int shared_commit_cb(const registry_commit_cb_scope_t scope,
+                            const registry_group_or_parameter_id_t *group_or_parameter_id,
+                            const void *context)
 {
-    (void)context;
+    /* since this function is shared by multiple commit_cb functions, we path the instance in the context */
+    const registry_instance_t *instance = context;
+    const registry_sys_rgb_led_instance_t *data = instance->data;
+
     printf("RGB instance commit_cb was executed on ");
 
-    /* check how much of the registry instance was committed */
+    /* check how much of the registry instance was committed and print the new values */
     switch (scope)
     {
     case REGISTRY_COMMIT_INSTANCE:
         /* in this case the whole instance and all parameters inside of it was committed */
-        printf("the whole instance");
+        printf("the whole instance: ID: %d\n", instance->id);
+        printf("\tParameter ID: 0, VALUE: %d\n", data->red);
+        printf("\tParameter ID: 1, VALUE: %d\n", data->green);
+        printf("\tParameter ID: 2, VALUE: %d\n", data->blue);
         break;
     case REGISTRY_COMMIT_GROUP:
         /* in this case a group and all parameters inside of it was committed */
         printf("a group: %d\n", *group_or_parameter_id);
+        /* this instance does not have groups, so no need to print anything here */
         break;
     case REGISTRY_COMMIT_PARAMETER:
         /* in this case only a single parameter was committed */
-        printf("a single parameter: %d\n", *group_or_parameter_id);
-        break;
+        printf("a single parameter: ID: %d,", *group_or_parameter_id);
+        switch (*group_or_parameter_id)
+        {
+        case 0: printf(" VALUE: %d\n", data->red); break;
+        case 1: printf(" VALUE: %d\n", data->green); break;
+        case 2: printf(" VALUE: %d\n", data->blue); break;
+        }
     }
 
     return 0;
 }
 
 /* create instances of a configuration schema, to expose configuration parameters to the registry */
+static int rgb_led_instance_0_commit_cb(const registry_commit_cb_scope_t scope,
+                                 const registry_group_or_parameter_id_t *group_or_parameter_id,
+                                 const void *context);
 static registry_sys_rgb_led_instance_t rgb_led_instance_0_data = {
     .red = 0,
     .green = 255,
@@ -70,11 +86,20 @@ static registry_sys_rgb_led_instance_t rgb_led_instance_0_data = {
 static registry_instance_t rgb_led_instance_0 = {
     .name = "rgb-0",
     .data = &rgb_led_instance_0_data,
-    .commit_cb = &rgb_led_instance_commit_cb,
+    .commit_cb = &rgb_led_instance_0_commit_cb,
 };
+static int rgb_led_instance_0_commit_cb(const registry_commit_cb_scope_t scope,
+                                 const registry_group_or_parameter_id_t *group_or_parameter_id,
+                                 const void *context) 
+{
+    (void)context;
+    return shared_commit_cb(scope, group_or_parameter_id, &rgb_led_instance_0);
+}
 
-/* this instance uses the same commit_cb function as instance 0 just for simplicity */
-/* normally each instance should have its own */
+/* create an instance for a second RGB LED */
+static int rgb_led_instance_1_commit_cb(const registry_commit_cb_scope_t scope,
+                                 const registry_group_or_parameter_id_t *group_or_parameter_id,
+                                 const void *context);
 static registry_sys_rgb_led_instance_t rgb_led_instance_1_data = {
     .red = 90,
     .green = 4,
@@ -83,8 +108,15 @@ static registry_sys_rgb_led_instance_t rgb_led_instance_1_data = {
 static registry_instance_t rgb_led_instance_1 = {
     .name = "rgb-1",
     .data = &rgb_led_instance_1_data,
-    .commit_cb = &rgb_led_instance_commit_cb,
+    .commit_cb = &rgb_led_instance_1_commit_cb,
 };
+static int rgb_led_instance_1_commit_cb(const registry_commit_cb_scope_t scope,
+                                 const registry_group_or_parameter_id_t *group_or_parameter_id,
+                                 const void *context)
+{
+    (void)context;
+    return shared_commit_cb(scope, group_or_parameter_id, &rgb_led_instance_1);
+}
 
 /* configure the registry storage to use littlefs2 */
 static littlefs2_desc_t fs_desc = {
