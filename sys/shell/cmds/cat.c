a2f2a0e6371bcf6500280594665821045d96e855
Joshua DeWeese <jdeweese@primecontrols.com>
2024-04-12 12:00:00 -0400

    sys/shell/cmds: add cat and tee cmds
    This patch moves these shell cmds out of the filesystem example into the
    shell cmds folder. This is done to make them available to all
    applications.

---
+++ sys/shell/cmds/cat.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 OTA keys S.A.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_shell_commands
+ * @{
+ *
+ * @file
+ * @brief       Print the content of a file
+ *
+ * @author      Vincent Dupont <vincent@otakeys.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+/*#include <string.h>*/
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "shell.h"
+
+static int _cat(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    /* With newlib or picolibc, low-level syscalls are plugged to RIOT vfs
+     * on native, open/read/write/close/... are plugged to RIOT vfs */
+#if defined(MODULE_NEWLIB) || defined(MODULE_PICOLIBC)
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (fread(&c, 1, 1, f) != 0) {
+        putchar(c);
+    }
+    fclose(f);
+#else
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (read(fd, &c, 1) != 0) {
+        putchar(c);
+    }
+    close(fd);
+#endif
+    fflush(stdout);
+    return 0;
+}
+
+SHELL_COMMAND(cat, "print the content of a file", _cat);
