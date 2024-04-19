c9573fc70ad6c0e5a9e9bb49a292abef7a15b7d2
Maik Lorenz <lorenz.maik@web.de>
2024-04-19 12:59:41 +0200

    examples/rust-matter-light: Make periph_rtc feature optional. If not supported, get seconds since UNIX epoch from ztimer_sec

--- examples/rust-matter-light/utils.c
+++ examples/rust-matter-light/utils.c
@@ -1,13 +1,24 @@
+#include <stdint.h>
+#include "modules.h"
+
+#if IS_USED(MODULE_PERIPH_RTC)
 #include "periph/rtc.h"
-#include <time.h>
+#else
+#include "ztimer.h"
+#endif
 
 // RIOT_EPOCH(2020) in seconds since UNIX Epoch
 #define RIOT_EPOCH_SECS 1577833200;
 
 uint32_t get_seconds_since_unix_epoch(void) {
-    // Get time since RIOT_EPOCH from RTC
+#if IS_USED(MODULE_PERIPH_RTC)
+    // Get seconds since RIOT_EPOCH from RTC
     struct tm now;
     rtc_get_time(&now);
-    // Add to RIOT_EPOCH to get current seconds since UNIX epoch
-    return rtc_mktime(&now) + RIOT_EPOCH_SECS;
+    // Add to RIOT_EPOCH to get seconds since UNIX epoch
+    return RIOT_EPOCH_SECS + rtc_mktime(&now);
+#else
+    // Get elapsed seconds from ztimer
+    return RIOT_EPOCH_SECS + ztimer_now(ZTIMER_SEC);
+#endif
 }
