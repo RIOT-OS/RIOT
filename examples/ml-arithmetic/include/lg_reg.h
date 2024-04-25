9a96169ded313142cea6d9b780dd231fc9e1f310
LukeTheEngineer <LukasJacksonEG@gmail.com>
2024-04-24 20:02:21 -0500

    examples/ml-arithmetic: More additions to logistic regression

--- examples/ml-arithmetic/include/lg_reg.h
+++ examples/ml-arithmetic/include/lg_reg.h
@@ -1,31 +1,77 @@
-#ifndef LOGISTIC_REGRESSION_H
-#define LOGISTIC_REGRESSION_H
+/*
+ * Copyright (C) 2014 Lukas R. Jackson, Vanguard Defense International
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+#ifndef INCLUDE_LOGISTIC_REGRESSION_H
+#define INCLUDE_LOGISTIC_REGRESSION_H
 
-// Structure to hold data points
+/**
+ * @ingroup     lg_reg.h
+ * @{
+ *
+ * @file
+ * @brief       Declarations for logistic regression
+ *
+ * @author      Lukas R. Jackson <LukasJacksonEG@gmail.com>
+ *
+ */
+
 typedef struct {
-    double x;
-    double y;
+    double x;   ///< Feature value
+    double y;   ///< Target value
 } Point;
 
-// Structure to hold parameters
 typedef struct {
-    double slope;
-    double intercept;
+    double slope;       ///< Slope parameter of the logistic regression model
+    double intercept;   ///< Intercept parameter of the logistic regression model
 } Parameters;
 
-// Sigmoid function
+/**
+ * @brief Sigmoid function.
+ *
+ * @param z Input value.
+ * @return The sigmoid of the input value.
+ */
 double sigmoid(double z);
 
-// Hypothesis function
+/**
+ * @brief Hypothesis function.
+ *
+ * @param x Input feature value.
+ * @param params Parameters of the logistic regression model.
+ * @return The hypothesis value based on the input feature and model parameters.
+ */
 double hypothesis(double x, Parameters params);
 
-// Cost function
+/**
+ * @brief Cost function for logistic regression.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @param params Parameters of the logistic regression model.
+ * @return The cost of the model based on the given data and parameters.
+ */
 double cost_function(Point *data, int num_points, Parameters params);
 
-// Update parameters using gradient descent
+/**
+ * @brief Update parameters using gradient descent.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @param params Pointer to the parameters of the logistic regression model to be updated.
+ */
 void gradient_descent(Point *data, int num_points, Parameters *params);
 
-// Logistic regression function
+/**
+ * @brief Perform logistic regression on the given data.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @return Parameters of the logistic regression model.
+ */
 Parameters logistic_regression(Point *data, int num_points);
 
-#endif /* LOGISTIC_REGRESSION_H */
+#endif /* INCLUDE_LOGISTIC_REGRESSION_H */
--- examples/ml-arithmetic/lg_reg.c
+++ examples/ml-arithmetic/lg_reg.c
@@ -1,43 +1,77 @@
+/**
+ * @ingroup     lg_reg.c
+ * @{
+ *
+ * @file
+ * @brief       Definitions for logistic regression functions
+ *
+ * @author      Lukas R. Jackson <LukasJacksonEG@gmail.com>
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "include/lg_reg.h"
 
-#define LEARNING_RATE 0.01
-#define MAX_ITERATIONS 10000
-#define EPSILON 0.0001
+#define LEARNING_RATE 0.01      ///< Learning rate for gradient descent
+#define MAX_ITERATIONS 10000    ///< Maximum number of iterations for gradient descent
+#define EPSILON 0.0001          ///< Convergence threshold for gradient descent
 
-// Sigmoid function
+/**
+ * @brief Sigmoid function.
+ *
+ * @param z Input value.
+ * @return The sigmoid of the input value.
+ */
 double sigmoid(double z)
 {
     return 1.0 / (1.0 + exp(-z));
 }
 
-// Hypothesis function
+/**
+ * @brief Hypothesis function.
+ *
+ * @param x Input feature value.
+ * @param params Parameters of the logistic regression model.
+ * @return The hypothesis value based on the input feature and model parameters.
+ */
 double hypothesis(double x, Parameters params)
 {
     return sigmoid(params.slope * x + params.intercept);
 }
 
-// Cost function
+/**
+ * @brief Cost function for logistic regression.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @param params Parameters of the logistic regression model.
+ * @return The cost of the model based on the given data and parameters.
+ */
 double cost_function(Point *data, int num_points, Parameters params)
 {
     double cost = 0.0;
-    for (int i = 0; i < num_points; i++)
-    {
+
+    for (int i = 0; i < num_points; i++) {
         double h = hypothesis(data[i].x, params);
         cost += -data[i].y * log(h) - (1 - data[i].y) * log(1 - h);
     }
     return cost / num_points;
 }
 
-// Update parameters using gradient descent
+/**
+ * @brief Update parameters using gradient descent.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @param params Pointer to the parameters of the logistic regression model to be updated.
+ */
 void gradient_descent(Point *data, int num_points, Parameters *params)
 {
     double slope_gradient = 0.0;
     double intercept_gradient = 0.0;
-    for (int i = 0; i < num_points; i++)
-    {
+
+    for (int i = 0; i < num_points; i++) {
         double h = hypothesis(data[i].x, *params);
         slope_gradient += (h - data[i].y) * data[i].x;
         intercept_gradient += (h - data[i].y);
@@ -46,19 +80,24 @@ void gradient_descent(Point *data, int num_points, Parameters *params)
     params->intercept -= LEARNING_RATE * intercept_gradient / num_points;
 }
 
-// Logistic regression function
+/**
+ * @brief Perform logistic regression on the given data.
+ *
+ * @param data Array of points (feature-value pairs).
+ * @param num_points Number of points in the dataset.
+ * @return Parameters of the logistic regression model.
+ */
 Parameters logistic_regression(Point *data, int num_points)
 {
-    Parameters params = {0, 0}; // Initial parameters
+    Parameters params = { 0, 0 }; // Initial parameters
     int iterations = 0;
     double prev_cost = INFINITY;
-    while (iterations < MAX_ITERATIONS)
-    {
+
+    while (iterations < MAX_ITERATIONS) {
         gradient_descent(data, num_points, &params);
         double current_cost = cost_function(data, num_points, params);
-        if (fabs(prev_cost - current_cost) < EPSILON)
-        {
-            break; // Convergence criteria
+        if (fabs(prev_cost - current_cost) < EPSILON) {
+            break; // Convergence criteria met
         }
         prev_cost = current_cost;
         iterations++;
