e54d95e0ddf0293ab7257ffa6f1d45f3cbedb177
Lukas R. Jackson <Lukasjacksoneg@gmail.com>
2024-04-26 08:01:33 -0500

    Update matrix.h

--- examples/basic-arithmetic/include/matrix.h
+++ examples/basic-arithmetic/include/matrix.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2023 Lukas R. Jackson, Vanguard Defense International
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+#ifndef INCLUDE_MATRIX_H
+#define INCLUDE_MATRIX_H
+
+/**
+ * @defgroup    matrix multiplication 
+ * @brief       Function declarations for matrix multiplication.
+ * @{
+ */
+
+/**
+ * @file        matrix.h
+ * @author      Lukas R. Jackson <LukasJacksonEG@gmail.com>
+ */
+
 #define ROWS_A 2
 #define COLS_A 3
 #define ROWS_B 3
@@ -6,3 +27,7 @@
 void matrix_multiply(int A[ROWS_A][COLS_A], int B[ROWS_B][COLS_B], int result[ROWS_A][COLS_B]);
 
 void print_matrix(int rows, int cols, int matrix[rows][cols]);
+
+/** @} */
+
+#endif /* INCLUDE_MATRIX_H */
