11293d373db424f38c6bfff5a05c013ae29a6839
Lukas R. Jackson <Lukasjacksoneg@gmail.com>
2024-04-26 08:06:53 -0500

    examples/matrix.c: Documented functions
    To ensure compliance with RIOT standards, I've added proper copyright notices and function docs

--- examples/basic-arithmetic/matrix.c
+++ examples/basic-arithmetic/matrix.c
@@ -1,6 +1,31 @@
+/*
+ * Copyright (C) 2023 Lukas R. Jackson, Vanguard Defense International
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup     Matrix multiplication
+ * @brief        Definitions for matrix multiplication functions
+ * @{
+ * @file         matrix.c
+ * @author       Lukas R. Jackson <LukasJacksonEG@gmail.com>
+ *
+ */
+
 #include "include/matrix.h"
 #include <stdio.h>
 
+/**
+ * @brief Multiply two matrices.
+ * This function multiplies two matrices A and B and stores the result in the
+ * provided result matrix.
+ * @param A The first matrix of size ROWS_A x COLS_A.
+ * @param B The second matrix of size ROWS_B x COLS_B.
+ * @param result The result matrix of size ROWS_A x COLS_B.
+ */
 void matrix_multiply(int A[ROWS_A][COLS_A], int B[ROWS_B][COLS_B], int result[ROWS_A][COLS_B])
 {
     for (int i = 0; i < ROWS_A; ++i)
@@ -16,6 +41,13 @@ void matrix_multiply(int A[ROWS_A][COLS_A], int B[ROWS_B][COLS_B], int result[RO
     }
 }
 
+/**
+ * @brief Print a matrix.
+ * This function prints the elements of the provided matrix to the standard output.
+ * @param rows The number of rows in the matrix.
+ * @param cols The number of columns in the matrix.
+ * @param matrix The matrix to be printed.
+ */
 void print_matrix(int rows, int cols, int matrix[rows][cols])
 {
     for (int i = 0; i < rows; ++i)
@@ -27,3 +59,4 @@ void print_matrix(int rows, int cols, int matrix[rows][cols])
         printf("\n");
     }
 }
+
