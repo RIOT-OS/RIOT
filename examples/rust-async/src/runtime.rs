d0c941ca37886c5fa30221d35c13febf1e69fdd2
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-28 11:11:18 +0700

    Update 'runtime.rs'

--- examples/rust-async/src/runtime.rs
+++ examples/rust-async/src/runtime.rs
@@ -1,4 +1,3 @@
-//use riot_wrappers::println;
 use crate::executor::Executor;
 use crate::util::get_static;
 
@@ -17,13 +16,6 @@ async fn task_callback_stream() {
     //crate::callback::process_callback_stream().await.unwrap();
 }
 
-/*
-#[embassy_executor::task]
-async fn task_gcoap_server_stream() {
-    xbd::process_gcoap_server_stream().await.unwrap();
-}
-*/
-
 const RUNTIME_THROTTLE_MS: u32 = 100;
 
 pub struct Runtime(Executor);
@@ -35,10 +27,9 @@ impl Runtime {
 
     pub fn run(&mut self) -> ! {
         get_static(self).0.run(|spawner| {
-            spawner.spawn(task_main()).unwrap();
             spawner.spawn(task_shell_stream()).unwrap();
+            spawner.spawn(task_main()).unwrap();
             spawner.spawn(task_callback_stream()).unwrap();
-            // spawner.spawn(task_gcoap_server_stream()).unwrap();
         });
     }
 }
