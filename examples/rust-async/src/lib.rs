69690a5d910545b88f84731fe9291eb9e59fa82d
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-29 08:46:45 +0700

    Move server init stuff into `async_main()`

--- examples/rust-async/src/lib.rs
+++ examples/rust-async/src/lib.rs
@@ -13,27 +13,26 @@ mod shell;
 mod server;
 
 use riot_wrappers::{riot_main, println};
-use util::get_static;
+use util::{sleep_msec, get_static};
 
 riot_main!(main);
 
 fn main() {
-    server::init_vfs();
-
-    // We can assume that both `handler` and `listener` are never dropped since
-    // `Runtime::run()` is a diverging function.
-    let mut handler = server::new_gcoap_handler();
-    let mut listener = server::new_gcoap_listener(get_static(&mut handler));
-
-    riot_wrappers::gcoap::register(get_static(&mut listener));
-    println!("CoAP server ready");
-
     runtime::Runtime::new().run();
 }
 
 async fn async_main() {
-    use riot_wrappers::ztimer::{Clock, Ticks};
+    server::init_vfs();
+    let mut handler = server::new_gcoap_handler();
+    let mut listener = server::new_gcoap_listener(get_static(&mut handler));
+    riot_wrappers::gcoap::register(get_static(&mut listener));
+    println!("CoAP server ready");
 
-    Clock::msec().sleep_async(Ticks(2_000)).await;
-    server::announce()
+    sleep_msec(2000).await;
+    server::announce();
+
+    // this prevents `handler` and `listener` from dropping
+    loop { sleep_msec(2000).await; }
+
+    // should be never reached
 }
