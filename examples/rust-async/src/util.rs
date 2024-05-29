970a156a023db2c238bf8a68e93be7f3bec3ad29
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-29 08:47:18 +0700

    Add `sleep_msec()`

--- examples/rust-async/src/util.rs
+++ examples/rust-async/src/util.rs
@@ -4,3 +4,9 @@ macro_rules! static_borrow_mut { ($x:expr) => (unsafe { &mut *core::ptr::addr_of
 pub fn to_raw<T>(x: &mut T) -> *mut () { x as *mut _ as _ }
 pub fn static_from_raw<T>(p: *mut ()) -> &'static mut T { unsafe { core::mem::transmute(p) } }
 pub fn get_static<T>(x: &mut T) -> &'static mut T { static_from_raw(to_raw(x)) }
+
+pub async fn sleep_msec(ms: u32) {
+    use riot_wrappers::ztimer::*;
+
+    Clock::msec().sleep_async(Ticks(ms)).await;
+}
