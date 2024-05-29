5da9004a6b9428611482fd2d0a84d514736f17f0
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-29 13:12:42 +0700

    Cleanup

--- examples/rust-async/src/alias.rs
+++ examples/rust-async/src/alias.rs
@@ -11,7 +11,6 @@ pub const TABLE_ALIAS_ENUMERATED: &[&str] = &[
     "ping ::1",
 ];
 
-//---- provably to be refactored like: function_alias!(...)
 pub const TABLE_ALIAS_FUNCTION: &[&str] = &[
     "f0",
     "f1",
@@ -26,7 +25,6 @@ pub async fn run_function_alias(name: &str) {
         _ => println!("oops, code for function alias [{}] is missing!", name),
     }
 }
-//----
 
 async fn test_async_sleep() {
     use riot_wrappers::ztimer::*;
@@ -40,12 +38,4 @@ async fn test_async_sleep() {
 
 async fn test_async_client() {
     println!("test_async_client(): TODO");
-
-/* ok
-active interface from PID KernelPID(6) ("gnrc_netdev_tap")
-    Address fe80:0000:0000:0000:a4ec:98ff:fe0e:af24
-
-$ libcoap/local/bin/coap-client -m get coap://[fe80:0000:0000:0000:a4ec:98ff:fe0e:af24%tap1]/.well-known/core
-<>;ct=0;title="Landing page"</time>;ct=0;title="Clock"</poem>;sz=1338</cbor>;ct=60</ps/>;title="Process list"</vfs/>;ct=0</saul/>;title="SAUL index"
-*/
 }
