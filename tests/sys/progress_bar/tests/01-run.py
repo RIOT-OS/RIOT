67ae5035c8123c83bb338b49b01786bcab6591b6
Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
2024-06-25 21:46:28 +0200

    test/sys/progress_bar: temporary printout to debug flaky test

--- tests/sys/progress_bar/tests/01-run.py
+++ tests/sys/progress_bar/tests/01-run.py
@@ -24,6 +24,8 @@ def testfunc(child):
         progress_str += EMPTY_CHARACTER * (LENGTH - ratio)
         check_str = 'Progress bar 0 |{}| {:3}%'.format(
             progress_str, i)
+        # todo: temporary printout for debugging this flaky test
+        print("EXPECTS:", check_str)
         child.expect_exact(check_str)
     child.expect_exact("Done!")
 
