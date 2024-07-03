1105b74295e8f12cf6af250a2c2cff937c59d497
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2024-07-03 14:22:28 +0200

    Remove traces of in-RIOT secure firmware

--- examples/blinky/merge_flash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-mergehex -m bin/nrf9160dk-ns/blinky.elf ../../riot_secure/bin/nrf9160dk/riot_secure_image.elf -o merged_blinky.hex
-
-arm-none-eabi-objcopy -I ihex -O binary merged_blinky.hex merged_blinky.bin
-
-nrfjprog -f nrf91 --program merged_blinky.hex --sectorerase --verify --reset
