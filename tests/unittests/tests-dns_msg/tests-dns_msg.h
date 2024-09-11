0463b2705f90f927d3fc32d2ecf399418bf1f621
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2024-09-11 14:16:01 +0200

    unittests: add test for dns_msg
    Co-Authored-By: Martine Lenders <martine.lenders@tu-dresden.de>

---
+++ tests/unittests/tests-dns_msg/tests-dns_msg.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2024 ML!PA Consulting GmbH
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the `dns_msg` module
+ *
+ * @author      Benjamin Valentin <benjamin.valentin@ml-pa.com>
+ */
+#ifndef TESTS_DNS_MSG_H
+#define TESTS_DNS_MSG_H
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_dns_msg(void);
+
+/**
+ * @brief   Generates tests for dns_msg
+ *
+ * @return  embUnit tests if successful, NULL if not.
+ */
+Test *tests_dns_msg_tests(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_DNS_MSG_H */
+/** @} */
