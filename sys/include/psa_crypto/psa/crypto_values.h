c391e307e7b71a7ce0c391b3187ddd9957a0fff0
Armin Wolf <W_Armin@gmx.de>
2024-10-04 00:11:33 +0200

    sys/psa_crypto: Split asymmetric encryption algorithm definitions into separate file
    Move the asymmetric encryption algorithm macro definitions into a
    separate file so that other PSA crypto backends can use them without
    pulling in all the other definitions.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- sys/include/psa_crypto/psa/crypto_values.h
+++ sys/include/psa_crypto/psa/crypto_values.h
@@ -35,28 +35,13 @@ extern "C" {
 #include "psa/cipher/algorithm.h"
 #include "psa/aead/algorithm.h"
 #include "psa/key_derivation/algorithm.h"
+#include "psa/asymmetric_encryption/algorithm.h"
 #include "psa/asymmetric_signature/algorithm.h"
 
-/**
- * @brief   Category for asymmetric encryption algorithms
- */
-#define PSA_ALG_CATEGORY_ASYMMETRIC_ENCRYPTION  ((psa_algorithm_t)0x07000000)
 /**
  * @brief   Category for key agreement algorithms
  */
 #define PSA_ALG_CATEGORY_KEY_AGREEMENT          ((psa_algorithm_t)0x09000000)
-
-/**
- * @brief   Whether the specified algorithm is an asymmetric encryption algorithm, also known as
- *          public-key encryption algorithm.
- *
- * @param   alg An algorithm identifier: a value of type @ref psa_algorithm_t.
- *
- * @return  1 if alg is an asymmetric encryption algorithm,
- *          0 otherwise.
- */
-#define PSA_ALG_IS_ASYMMETRIC_ENCRYPTION(alg) \
-    (((alg) & PSA_ALG_CATEGORY_MASK) == 0x07000000)
 
 /**
  * @brief   Whether the specified algorithm is an elliptic curve Diffie-Hellman algorithm.
@@ -117,17 +102,6 @@ extern "C" {
     (((alg) & 0x7f00ffff) == 0x09000000)
 
 /**
- * @brief   Whether the specified algorithm is an RSA OAEP encryption algorithm.
- *
- * @param   alg An algorithm identifier: a value of type @ref psa_algorithm_t.
- *
- * @return  1 if alg is a RSA OAEP algorithm
- *          0 otherwise
- */
-#define PSA_ALG_IS_RSA_OAEP(alg) \
-    (((alg) & ~0x000000ff) == 0x07000300)
-
-/**
  * @brief   Macro to build a combined algorithm that chains a key agreement with a key derivation.
  *
  * @details A combined key agreement algorithm is used with a multi-part key derivation operation,
@@ -181,19 +155,6 @@ extern "C" {
  */
 #define PSA_ALG_KEY_AGREEMENT_GET_KDF(alg) \
     ((psa_algorithm_t)((alg) & 0xfe00ffff))
-
-/**
- * @brief   The RSA PKCS#1 v1.5 asymmetric encryption algorithm.
- *
- * @details This encryption scheme is defined by PKCS #1: RSA Cryptography Specifications
- *          Version 2.2 [RFC8017](https://tools.ietf.org/html/rfc8017.html) §7.2 under the
- *          name RSAES-PKCS-v1_5.
- *
- *          @b Compatible @b key @b types
- *          - @ref PSA_KEY_TYPE_RSA_KEY_PAIR
- *          - @ref PSA_KEY_TYPE_RSA_PUBLIC_KEY (asymmetric encryption only)
- */
-#define PSA_ALG_RSA_PKCS1V15_CRYPT ((psa_algorithm_t)0x07000200)
 
 /**
  * @brief   The finite-field Diffie-Hellman (DH) key agreement algorithm.
@@ -295,31 +256,6 @@ extern "C" {
  *          - @ref PSA_ECC_FAMILY_MONTGOMERY
  */
 #define PSA_ALG_ECDH ((psa_algorithm_t)0x09020000)
-
-/**
- * @brief   The RSA OAEP asymmetric encryption algorithm.
- *
- * @details This encryption scheme is defined by
- *          [RFC8017](https://tools.ietf.org/html/rfc8017.html) §7.1 under the name RSAES-OAEP,
- *          with the following options:
- *          -   The mask generation function MGF1 defined in
- *              [RFC8017](https://tools.ietf.org/html/rfc8017.html) Appendix B.2.1.
- *          -   The specified hash algorithm is used to hash the label, and for the mask generation
- *              function.
- *
- *          @b Compatible @b key @b types
- *          - @ref PSA_KEY_TYPE_RSA_KEY_PAIR
- *          - @ref PSA_KEY_TYPE_RSA_PUBLIC_KEY (asymmetric encryption only)
- *
- * @param   hash_alg    A hash algorithm: a value of type @ref psa_algorithm_t such that
- *                      @ref PSA_ALG_IS_HASH(@p hash_alg) is true. The hash algorithm is used for
- *                      MGF1.
- *
- * @return  The corresponding RSA OAEP encryption algorithm.
- *          Unspecified if @c hash_alg is not a supported hash algorithm.
- */
-#define PSA_ALG_RSA_OAEP(hash_alg) \
-    ((psa_algorithm_t)(0x07000300 | ((hash_alg) & 0x000000ff)))
 
 /**
  * @brief   Finite-field Diffie-Hellman groups defined for TLS in RFC 7919.
