23d21a220cabbf563fbcfef307f4049ff17e369b
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2024-11-06 19:18:30 +0100

    tests/build_system/xfa: improve test coverage
    This increases the test coverage for XFA:
    
    - Multiple entries are added from a single file
    - Priorities are assigned to enforce a given order
    - The size of the `struct` to add is changed to no longer be a power of
      2
    
    (cherry picked from commit 28392245944655e72f7569ca1639d07ee234cb1f)

--- tests/build_system/xfa/xfatest2.c
+++ tests/build_system/xfa/xfatest2.c
@@ -1,5 +1,10 @@
 #include "xfa.h"
 #include "xfatest.h"
 
-XFA(xfatest, 0) xfatest_t _xfatest2 = { .val = 2, .text = "xfatest2" };
-XFA_CONST(xfatest_const, 0) xfatest_t _xfatest_const2 = { .val = 45, .text = "xfatest_const2" };
+XFA(xfatest, 1) xfatest_t _xfatest2 = { .val = 2, .text = "xfatest2", .letter = 'b' };
+XFA(xfatest, 3) xfatest_t _xfatest4 = { .val = 4, .text = "xfatest4", .letter = 'd' };
+XFA(xfatest, 2) xfatest_t _xfatest3 = { .val = 3, .text = "xfatest3", .letter = 'c' };
+
+XFA_CONST(xfatest_const, 1) xfatest_t _xfatest_const2 = { .val = 45, .text = "xfatest_const2", .letter = 'b' };
+XFA_CONST(xfatest_const, 3) xfatest_t _xfatest_const4 = { .val = 44, .text = "xfatest_const4", .letter = 'd' };
+XFA_CONST(xfatest_const, 2) xfatest_t _xfatest_const3 = { .val = 42, .text = "xfatest_const3", .letter = 'c' };
