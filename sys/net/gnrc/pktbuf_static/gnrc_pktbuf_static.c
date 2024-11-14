83f5b261a8655f58998b31b43c6d9d864cdd9bfa
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2024-11-14 15:28:32 +0100

    sys/net/gnrc_pktbuf: detect use after free if canary is in metadata

--- sys/net/gnrc/pktbuf_static/gnrc_pktbuf_static.c
+++ sys/net/gnrc/pktbuf_static/gnrc_pktbuf_static.c
@@ -218,6 +218,13 @@ gnrc_pktsnip_t *gnrc_pktbuf_start_write(gnrc_pktsnip_t *pkt)
         mutex_unlock(&gnrc_pktbuf_mutex);
         return NULL;
     }
+
+    if (CONFIG_GNRC_PKTBUF_CHECK_USE_AFTER_FREE &&
+        pkt->users == GNRC_PKTBUF_CANARY) {
+        puts("gnrc_pktbuf: use after free detected\n");
+        DEBUG_BREAKPOINT(3);
+    }
+
     if (pkt->users > 1) {
         gnrc_pktsnip_t *new;
         new = _create_snip(pkt->next, pkt->data, pkt->size, pkt->type);
