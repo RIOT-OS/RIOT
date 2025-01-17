6dee14013cb6c22294f57f0788ad88bcfb56cb39
BarschusRochus <19max60@web.de>
2025-01-17 16:09:25 +0100

    t2t: add logging, include tlv header size in check, cleanup

--- sys/net/nfc/t2t/t2t.c
+++ sys/net/nfc/t2t/t2t.c
@@ -14,7 +14,9 @@
  */
 static int create_default_dynamic_memory_layout(nfc_t2t_t *tag, bool read_only){
 // calculate size of dynamic lock bytes and amnt of lock bits
+    LOG_DEBUG("Creating default lock bytes\n");
     if(tag->memory_size <= NFC_T2T_STATIC_MEMORY_SIZE){
+        LOG_ERROR("Tag size too small to add dynamic memory layout\n");
         return -1;
     }
     uint32_t dyn_lock_bytes;
@@ -49,6 +51,8 @@ static int create_default_dynamic_memory_layout(nfc_t2t_t *tag, bool read_only){
     tag->extra.default_lock_bytes = dyn_lock_bytes;
     tag->extra.custom_lock_bytes = 0;
     tag->extra.custom_reserved_bytes = 0;
+
+    LOG_DEBUG("Wrote %ld lock bits in %ld lock bytes\n", dyn_lock_bits, dyn_lock_bytes);
     
     return 0;
 }
@@ -104,6 +108,7 @@ int t2t_create_uid(nfc_t2t_t *tag){
         uid->uid[7] = 0XFF;
         uid->uid[8] = 0XFF;
         uid->uid[9] = NFC_T2T_VERSION_1_1; //find a norm to verify this
+        LOG_DEBUG("Wrote 4 Byte UID\n");
     }else if(sn->sn_length == NFC_ISO14443A_UID_DOUBLE_SIZE){
         uid->uid[3] = NFC_T2T_CASCADE_TAG_BYTE ^ uid->uid[0] ^ uid->uid[1] ^ uid->uid[2];
         uid->uid[4] = sn->sn[3];
@@ -112,6 +117,7 @@ int t2t_create_uid(nfc_t2t_t *tag){
         uid->uid[7] = sn->sn[6];
         uid->uid[8] = uid->uid[4]^ uid->uid[5] ^ uid->uid[6] ^ uid->uid[7];
         uid->uid[9] = NFC_T2T_VERSION_1_1; //find a norm to verify this
+        LOG_DEBUG("Wrote 7 Byte UID\n");
     }else{
         uid->uid[3] = sn->sn[3];
         uid->uid[4] = sn->sn[4];
@@ -120,6 +126,7 @@ int t2t_create_uid(nfc_t2t_t *tag){
         uid->uid[7] = sn->sn[7];
         uid->uid[8] = sn->sn[8];
         uid->uid[9] = sn->sn[9];
+        LOG_DEBUG("Wrote 10 Byte UID\n");
     }
     tag->uid = uid;
     return 0;
@@ -195,6 +202,7 @@ static uint8_t calculate_bytes_per_page(uint32_t mem_size){
 int t2t_create_type_2_tag(nfc_t2t_t *tag, t2t_sn_t *sn, t2t_cc_t *cc, t2t_static_lock_bytes_t *lb, 
                             uint32_t memory_size, uint8_t *memory)
 {
+    LOG_DEBUG("Creating type 2 tag\n");
     tag->memory = memory;
     tag->memory_size = memory_size;
     memset(tag->memory, 0x00, tag->memory_size);
@@ -202,9 +210,11 @@ int t2t_create_type_2_tag(nfc_t2t_t *tag, t2t_sn_t *sn, t2t_cc_t *cc, t2t_static
         tag->dynamic_layout = true;
         tag->data_area_size = memory_size-(NFC_T2T_SIZE_UID+NFC_T2T_SIZE_STATIC_LOCK_BYTES+NFC_T2T_SIZE_CC);
         tag->extra.bytes_per_page = calculate_bytes_per_page(memory_size);
+        LOG_DEBUG("Using dynamic memory layout\n");
     }else{
         tag->dynamic_layout = false;
         tag->data_area_size = NFC_T2T_SIZE_STATIC_DATA_AREA;
+        LOG_DEBUG("Using static memory layout\n");
     }
     tag->data_area_start = memory + NFC_T2T_START_STATIC_DATA_AREA;
     tag->data_area_cursor = tag->data_area_start;
@@ -239,6 +249,8 @@ int t2t_create_type_2_tag(nfc_t2t_t *tag, t2t_sn_t *sn, t2t_cc_t *cc, t2t_static
         create_default_dynamic_memory_layout(tag, false); //sets dynamic lock bits at end of data area and lowers data_area_size
     }
 
+    LOG_DEBUG("Created tag with %ld bytes data area\n", tag->data_area_size);
+
     return 0;
 }
 
@@ -266,11 +278,14 @@ int t2t_create_default_tag_with_ndef(nfc_t2t_t *tag, uint32_t memory_size, uint8
 }
 
 int t2t_create_tag_from_given_memory(nfc_t2t_t *tag, uint32_t memory_size, uint8_t *memory, uint8_t uid_size){
+    LOG_DEBUG("Creating type 2 tag from given memory portion\n");
     int error = 0;
     if (memory == NULL){
+        LOG_ERROR("Invalid pointer to memory region\n");
         return -1;
     }
     if(memory_size < NFC_T2T_STATIC_MEMORY_SIZE){
+        LOG_ERROR("Smallest possible memory size is 64 byte, given is %ld\n", memory_size);
         return -1; //debatable - technically small NDEF messages could fit in less than 64 byte
     }
     tag->memory = memory;
@@ -287,27 +302,33 @@ int t2t_create_tag_from_given_memory(nfc_t2t_t *tag, uint32_t memory_size, uint8
     tag->uid = (t2t_uid_t*) (tag->memory);
     tag->lb = (t2t_static_lock_bytes_t*) (tag->memory + NFC_T2T_SIZE_UID);
     tag->cc = (t2t_cc_t*) (tag->memory + NFC_T2T_SIZE_UID + NFC_T2T_SIZE_STATIC_LOCK_BYTES);
-    if(uid_size == NFC_ISO14443A_UID_SINGLE_SIZE 
+    if(uid_size == NFC_ISO14443A_UID_SINGLE_SIZE
             || uid_size == NFC_ISO14443A_UID_DOUBLE_SIZE 
             || uid_size == NFC_ISO14443A_UID_TRIPLE_SIZE){
         tag->sn.sn_length = uid_size;
     }else{
+        LOG_ERROR("Invalid uid size given, defaulting to 10 byte UID\n");
         tag->sn.sn_length = NFC_ISO14443A_UID_TRIPLE_SIZE; //default to maximum
     }
     memcpy(tag->sn.sn, tag->uid, tag->sn.sn_length);
+
+    LOG_DEBUG("Created type 2 tag from memory portion\n");
     
-    return 0;
+    return error;
 }
 
 int t2t_handle_read(nfc_t2t_t *tag, uint8_t block_no, uint8_t *buf){
     uint8_t *block_address = block_no_to_address(block_no, tag);
     if((uint32_t) block_address >= ((uint32_t) tag->memory + tag->memory_size)){
+        LOG_DEBUG("Block number too large, outside of valid memory region\n");
         return -1;
     }else if(((uint32_t) block_address + 16) > ((uint32_t) tag->memory + tag->memory_size)){
+        LOG_DEBUG("End of region outside of tag memory, filling up with %#x\n", NFC_TLV_TYPE_TERMINATOR);
         memset(buf, NFC_TLV_TYPE_TERMINATOR, NFC_T2T_READ_RETURN_BYTES);
         memcpy(buf, block_address, (((uint32_t) tag->memory + tag->memory_size) - (uint32_t) block_address));
 
     }else{
+        LOG_DEBUG("Reading 4 blocks beginning at block %d\n", block_no);
         memcpy(buf, block_address, NFC_T2T_READ_RETURN_BYTES);
     }
     
@@ -316,29 +337,33 @@ int t2t_handle_read(nfc_t2t_t *tag, uint8_t block_no, uint8_t *buf){
 
 int t2t_handle_write(nfc_t2t_t *tag, uint8_t block_no, uint8_t const *buf){
     if(!t2t_is_writeable(tag)){
+        LOG_ERROR("Tag is in read only state, aborting write\n");
         return -1;
     }
     uint8_t *block_address = block_no_to_address(block_no, tag);
     printf("address: %p\n",block_address);
     if((uint32_t) block_address >= ((uint32_t) tag->memory + tag->memory_size)){
+        LOG_ERROR("Block number too high, outside of valid memory region, aborting write\n");
         return -1;
     }
     if(tag->current_sector == 0x0){
         if(block_no == 3){ //write to CC block causes bitwise OR instead of overwriting
-        tag->cc->magic_number   |= buf[0];
-        tag->cc->version_number |= buf[1];
-        tag->cc->memory_size    |= buf[2];
-        tag->cc->read_write_access |= buf[3];
-    }else if(block_no == 2){//same should go for lock bytes, also no overwrite of UID
-        tag->lb->lock_byte1 |= buf[3];
-        tag->lb->lock_byte2 |= buf[4];
-    }else if(block_no < 2){ //5.2 does not list uid to be overwritten
-        return -1; //debatable if error or just ignored ok
+            tag->cc->magic_number   |= buf[0];
+            tag->cc->version_number |= buf[1];
+            tag->cc->memory_size    |= buf[2];
+            tag->cc->read_write_access |= buf[3];
+        }else if(block_no == 2){//same should go for lock bytes, also no overwrite of UID
+            tag->lb->lock_byte1 |= buf[3];
+            tag->lb->lock_byte2 |= buf[4];
+        }else if(block_no < 2){ //5.2 does not list uid to be overwritten
+            LOG_ERROR("Can't overwrite UID, aborting write\n");
+            return -1; //debatable if error or just ignored ok
+        }else{
+            LOG_ERROR("Writing content to tag memory\n");
+            memcpy(block_address, buf, NFC_T2T_BLOCK_SIZE);
+        }
     }else{
-        memcpy(block_address, buf, NFC_T2T_BLOCK_SIZE);
-    }
-    }else{
-        printf("writing things\n");
+        LOG_ERROR("Writing content to tag memory\n");
         memcpy(block_address, buf, NFC_T2T_BLOCK_SIZE);
     }
     
@@ -348,6 +373,7 @@ int t2t_handle_write(nfc_t2t_t *tag, uint8_t block_no, uint8_t const *buf){
 
 int t2t_handle_sector_select(nfc_t2t_t *tag, uint8_t sector){
     if(sector > (tag->memory_size /NFC_T2T_BLOCKS_PER_SECTOR)){
+        LOG_ERROR("Sector number too high\n");
         return -1;
     }
     tag->current_sector = sector;
@@ -360,6 +386,7 @@ int t2t_create_null_tlv(nfc_t2t_t *tag){
         tag->data_area_cursor++;
         return 0;
     }else{
+        LOG_ERROR("Not enough space in tag to create NULL TLV\n");
         return -1;
     }
     
@@ -371,15 +398,19 @@ int t2t_create_terminator_tlv(nfc_t2t_t *tag){
         tag->data_area_cursor++;
         return 0;
     }else{
+        LOG_ERROR("Not enough space in tag to create Terminator TLV\n");
         return -1;
     }
 }
 
 int t2t_create_memory_control_tlv(nfc_t2t_t *tag, uint8_t * data, uint32_t size){
+    LOG_DEBUG("Creating memory control TLV\n");
     if(size > tag->data_area_size){
+        LOG_ERROR("Size of reserved memory too large for tag area, aborting\n");
         return -1;
     }
     if(tag->dynamic_layout == false){
+        LOG_ERROR("Tag has no dynamic memory layout, aborting\n");
         return -2;
     }
     tag->data_area_cursor[0] = NFC_TLV_TYPE_MEMORY_CTRL;
@@ -409,11 +440,14 @@ int t2t_create_memory_control_tlv(nfc_t2t_t *tag, uint8_t * data, uint32_t size)
     tag->extra.custom_reserved_bytes += size;
     memcpy(start_address, data, size);
 
+    LOG_DEBUG("Wrote %ld byte of reserved memory information to end of tag memory\n", size);
+
     return 0;
 }
 
 static int t2t_test_and_remove_terminator_tlv(nfc_t2t_t *tag){
     if(tag->data_area_cursor <= tag->data_area_start){
+        LOG_DEBUG("No terminator TLV found\n");
         return 0;
     }
     uint8_t * before = tag->data_area_cursor -1;
@@ -426,11 +460,13 @@ static int t2t_test_and_remove_terminator_tlv(nfc_t2t_t *tag){
 
 int write_tlv_length(uint8_t *buf, uint16_t length){
     if(length > 0xFF){ //==> length 0xFF and to be interpreted as 2 bytes
+        LOG_DEBUG("Writing three byte NDEF length TLV\n");
         buf[0] = 0xFF;
         buf[1] = (length >> 8) & 0x00FF; //most significant Byte - left 8 bit
         buf[2] = (length & 0x00FF); //lsB - right 8 bit
         return 3;
     }else{
+        LOG_DEBUG("Writing one byte NDEF length TLV\n");
         buf[0] = (uint8_t) length;
         return 1;
     }
@@ -441,29 +477,28 @@ int t2t_create_ndef_tlv(nfc_t2t_t *tag, uint16_t length){
     tag->data_area_cursor[pos] = NFC_TLV_TYPE_NDEF_MSG;
     pos++;
     pos += write_tlv_length(&tag->data_area_cursor[pos], length);
-    if(pos < 2){ // write_tlv returns -1 if smth went wrong
-        return -1;
-    }
     tag->data_area_cursor += pos;
-    return pos; //either 1 or 4
+    return pos; //either 2 or 4
 }
 
 
 int t2t_add_ndef_msg(nfc_t2t_t *tag, ndef_t const *msg){
-    if(free_space_in_data_area(tag) <= msg->buffer.cursor){
+
+    uint32_t msg_length = msg->buffer.cursor;
+    uint32_t tlv_header_size = 1;
+    if(msg_length >= 0xFF){
+        tlv_header_size = 3;
+    }
+    if(free_space_in_data_area(tag) < (msg_length + tlv_header_size)){
         LOG_ERROR("Not enough space in data area for NDEF message\n");
         return -1;
     }
-    int tlv_header_size = 0;
     // check for already existing terminator tlv
     t2t_test_and_remove_terminator_tlv(tag);
-    tlv_header_size = t2t_create_ndef_tlv(tag, msg->buffer.cursor);
-    if (tlv_header_size < 0){
-        return -1;
-    }
     //write msg into tag mem
-    memcpy(tag->data_area_cursor, msg->buffer.memory,msg->buffer.cursor);
-    tag->data_area_cursor += msg->buffer.cursor;
+    t2t_create_ndef_tlv(tag, msg_length);
+    memcpy(tag->data_area_cursor, msg->buffer.memory,msg_length);
+    tag->data_area_cursor += msg_length;
     //add terminator
     t2t_create_terminator_tlv(tag);
     return 0;
@@ -471,8 +506,7 @@ int t2t_add_ndef_msg(nfc_t2t_t *tag, ndef_t const *msg){
 
 int t2t_add_empty_ndef_tlv(nfc_t2t_t *tag){
     int error = 0;
-    error = t2t_test_and_remove_terminator_tlv(tag);
-    if(error) return error;
+    t2t_test_and_remove_terminator_tlv(tag);
     error = t2t_create_ndef_tlv(tag, 0);
     if(error) return error;
     error = t2t_create_terminator_tlv(tag);
@@ -496,12 +530,14 @@ void t2t_dump_tag_memory(nfc_t2t_t *tag){
 }
 
 int t2t_clear_data_area(nfc_t2t_t *tag){
+    LOG_DEBUG("Setting data area of type 2 tag to 0x00\n");
     memset(tag->data_area_start, 0x00, tag->data_area_size);
     tag->data_area_cursor = tag->data_area_start;
     return 0;
 }
 
 int t2t_clear_mem(nfc_t2t_t *tag){
+    LOG_DEBUG("Setting complete memory of type 2 tag to 0x00\n");
     tag->uid = NULL;
     tag->lb = NULL;
     tag->cc = NULL;
@@ -515,25 +551,3 @@ int t2t_clear_mem(nfc_t2t_t *tag){
 uint32_t t2t_get_size(nfc_t2t_t *tag){
     return tag->memory_size;
 }
-
-
-
-/*
-int t2t_create_dynamic_lock_control_tlv(nfc_t2t_t *tag, int amnt_lock_bits){
-    
-    //write magic number
-    tag->data_area_cursor[0] = (uint8_t) NFC_TLV_TYPE_LOCK_CTRL;
-    tag->data_area_cursor ++;
-    //write size
-    tag->data_area_cursor[0] = (uint8_t) NFC_TLV_SIZE_TYPE_LOCK_CTRL_TLV;
-    tag->data_area_cursor ++;
-    // calculate lock bytes and check if enough mem
-    if(amnt_lock_bits){
-        printf("use for compiler's sake\n");
-    }
-    //write actual lock_ctl data
-        // find beginning of lock/mem_ctl bytes -> cc data area
-        // check if enough space before
-        // start at right location
-    return 0;}
-*/
