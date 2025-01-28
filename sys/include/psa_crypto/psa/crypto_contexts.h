24280d005946286e4f61e6f79f53b0e1b444aba0
Armin Wolf <W_Armin@gmx.de>
2025-01-28 18:59:01 +0100

    sys/psa_crypto: Split cipher type definitions into separate file
    TODO
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- sys/include/psa_crypto/psa/crypto_contexts.h
+++ sys/include/psa_crypto/psa/crypto_contexts.h
@@ -27,42 +27,7 @@ extern "C" {
 #include "psa/crypto_includes.h"
 
 #include "hash/types.h"
-
-#if IS_USED(MODULE_PSA_CIPHER)
-/**
- * @brief   Structure containing the cipher contexts needed by the application.
- */
-typedef union {
-#if IS_USED(MODULE_PSA_CIPHER_AES_128_ECB) ||\
-    IS_USED(MODULE_PSA_CIPHER_AES_128_CBC) ||\
-    defined(DOXYGEN)
-    psa_cipher_aes_128_ctx_t aes_128;   /**< AES 128 context*/
-#endif
-#if IS_USED(MODULE_PSA_CIPHER_AES_192_CBC) || defined(DOXYGEN)
-    psa_cipher_aes_192_ctx_t aes_192;   /**< AES 192 context*/
-#endif
-#if IS_USED(MODULE_PSA_CIPHER_AES_256_CBC) || defined(DOXYGEN)
-    psa_cipher_aes_256_ctx_t aes_256;   /**< AES 256 context*/
-#endif
-} psa_cipher_context_t;
-#endif
-
-#if IS_USED(MODULE_PSA_SECURE_ELEMENT)
-/**
- * @brief   Structure containing the secure element specific cipher contexts needed by the
- *          application.
- */
-typedef struct {
-    psa_encrypt_or_decrypt_t direction; /**< Direction of this cipher operation */
-    /** Structure containing a driver specific cipher context */
-    union driver_context {
-        unsigned dummy; /**< Make the union non-empty even with no supported algorithms. */
-    #if IS_USED(MODULE_PSA_SECURE_ELEMENT_ATECCX08A) || defined(DOXYGEN)
-        atca_aes_cbc_ctx_t atca_aes_cbc;    /**< ATCA AES CBC context*/
-    #endif
-    } drv_ctx;  /**< SE specific cipher operation context */
-} psa_se_cipher_context_t;
-#endif
+#include "cipher/types.h"
 
 #ifdef __cplusplus
 }
