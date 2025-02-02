24d1a853dcfaf1a059456cd38c57644d775ab315
Bas Stottelaar <basstottelaar@gmail.com>
2025-02-02 14:23:26 +0100

    drivers/gp8xxx: add initial support
    The Guestgood GP8xxx I2C DACs are easy-to-use voltage and current
    DACs with standardized output ranges. All supported variants have
    very similar I2C protocol.
    
    This commit adds support for a generic driver (gp8xxx) and uses a
    information structure to record capabilities. Multiple driver
    instances can work simultaneously on the same I2C bus.
    
    SAUL support is also added.

---
+++ drivers/gp8xxx/include/gp8xxx_constants.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Bas Stottelaar <basstottelaar@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#pragma once
+
+/**
+ * @ingroup     drivers_gp8xxx
+ * @{
+ *
+ * @file
+ * @brief       Internal addresses, registers and constants
+ *
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name GP8xxx registers
+ * @{
+ */
+#define GP8XXX_REG_OUTPUT           (0x01)
+#define GP8XXX_REG_CHANNEL          (0x02)
+/** @} */
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
