f9cc15e4f3298c1c37eda8169ed8f966a7dfd824
dylad <dylan.laduranty@mesotic.com>
2025-02-08 13:15:19 +0100

    cpu/sam4s: introduce initial support
    Signed-off-by: dylad <dylan.laduranty@mesotic.com>

---
+++ cpu/sam4s/include/periph_cpu.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025 Mesotic SAS
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     cpu_sam4s
+ * @{
+ *
+ * @file
+ * @brief       CPU specific definitions for internal peripheral handling
+ *
+ * @author      Dylan Laduranty <dylan.laduranty@mesotic.com>
+ *
+ */
+
+#ifndef PERIPH_CPU_H
+#define PERIPH_CPU_H
+
+#include "periph_cpu_common.h"
+#include "macros/units.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CPU_H */
+/** @} */
