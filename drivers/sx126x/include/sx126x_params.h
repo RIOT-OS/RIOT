e15a65e3a75182737ffc9a061938855c0b686fa9
Fabian Hüßler <fabian.huessler@ml-pa.com>
2025-02-25 19:59:33 +0100

    drivers/sx126x: add support for Dio2 and Dio3

--- drivers/sx126x/include/sx126x_params.h
+++ drivers/sx126x/include/sx126x_params.h
@@ -63,7 +63,11 @@ extern "C" {
 #  define SX126X_PARAM_DIO1                 GPIO_PIN(1, 4)  /* D5 */
 #endif
 
-#ifndef SX126X_PARAM_REGULATOR
+#if !defined(SX126X_PARAM_REGULATOR) || defined(DOXYGEN)
+/**
+ * @brief   Regulator type which can be
+ *          SX126X_REG_MODE_LDO or SX126X_REG_MODE_DCDC
+ */
 #  define SX126X_PARAM_REGULATOR            SX126X_REG_MODE_DCDC
 #endif
 
@@ -73,6 +77,48 @@ extern "C" {
 
 #ifndef SX126X_PARAM_TX_PA_MODE
 #  define SX126X_PARAM_TX_PA_MODE           SX126X_RF_MODE_TX_LPA
+#endif
+
+#if !defined(SX126X_PARAM_DIO2_MODE) || defined(DOXYGEN)
+/**
+ * @brief   DIO2 pin mode which can be
+ *          SX126X_DIO2_UNUSED, SX126X_DIO2_IRQ or SX126X_DIO2_RF_SWITCH
+ */
+#  define SX126X_PARAM_DIO2_MODE            SX126X_DIO2_UNUSED
+#endif
+
+#if !defined(SX126X_PARAM_DIO3_MODE) || defined(DOXYGEN)
+/**
+ * @brief   DIO3 pin mode which can be
+ *          SX126X_DIO3_UNUSED, SX126X_DIO3_IRQ or SX126X_DIO3_TCXO
+ */
+#  define SX126X_PARAM_DIO3_MODE            SX126X_DIO3_UNUSED
+#endif
+
+#if !defined(SX126X_PARAM_TCXO_VOLTAGE) || defined(DOXYGEN)
+/**
+ * @brief   TCXO voltage is configured to be 200 mV below the supply voltage.
+ *
+ * This means that even if tcxoVoltage is configured above the supply voltage,
+ * the supply voltage will be limited by: VDDop > VTCXO + 200 mV
+ */
+#  define SX126X_PARAM_TCXO_VOLTAGE         SX126X_TCXO_CTRL_3_0V
+#endif
+
+#if !defined(SX126X_PARAM_TCXO_TIMEOUT) || defined(DOXYGEN)
+/**
+ * @brief   Timeout for tcxo stabilization in 15.625 µs steps
+ *          The default value is 256 (4ms).
+ */
+#  define SX126X_PARAM_TCXO_TIMEOUT         256
+#endif
+
+#if !defined(SX126X_PARAM_DIO3_ARG) || defined(DOXYGEN)
+/**
+ * @brief   DIO3 argument if mode is SX126X_DIO3_TCXO
+ */
+#  define SX126X_PARAM_DIO3_ARG             { .tcxo_volt = SX126X_PARAM_TCXO_VOLTAGE, \
+                                              .tcxo_timeout = SX126X_PARAM_TCXO_TIMEOUT }
 #endif
 
 #ifndef SX126X_PARAM_TYPE
@@ -93,22 +139,49 @@ extern "C" {
 
 #if IS_USED(MODULE_SX126X_RF_SWITCH)
 #  define SX126X_SET_RF_MODE  .set_rf_mode = SX126X_PARAM_SET_RF_MODE_CB,
-#  define SX126X_TX_PA_MODE   .tx_pa_mode = SX126X_PARAM_TX_PA_MODE
+#  define SX126X_TX_PA_MODE   .tx_pa_mode = SX126X_PARAM_TX_PA_MODE,
 #else
 #  define SX126X_SET_RF_MODE
 #  define SX126X_TX_PA_MODE
 #endif
 
+#if IS_USED(MODULE_SX126X_DIO2) || defined(DOXYGEN)
+/**
+ * @brief   DIO2 pin mode
+ */
+#  define SX126X_DIO2_MODE      .dio2_mode = SX126X_PARAM_DIO2_MODE,
+#else
+#  define SX126X_DIO2_MODE
+#endif
+
+#if IS_USED(MODULE_SX126X_DIO3) || defined(DOXYGEN)
+/**
+ * @brief   DIO3 pin mode
+ */
+#  define SX126X_DIO3_MODE      .dio3_mode = SX126X_PARAM_DIO3_MODE,
+/**
+ * @brief   DIO3 pin argument
+ */
+#  define SX126X_DIO3_ARG       .dio3_arg = SX126X_PARAM_DIO3_ARG,
+#else
+#  define SX126X_DIO3_MODE
+#  define SX126X_DIO3_ARG
+#endif
+
 #ifndef SX126X_PARAMS
-#  define SX126X_PARAMS           { .spi = SX126X_PARAM_SPI,            \
-                                    .nss_pin = SX126X_PARAM_SPI_NSS,    \
-                                    .reset_pin = SX126X_PARAM_RESET,    \
-                                    .busy_pin = SX126X_PARAM_BUSY,      \
-                                    .dio1_pin = SX126X_PARAM_DIO1,      \
-                                    .type     = SX126X_PARAM_TYPE,      \
-                                    .regulator = SX126X_PARAM_REGULATOR, \
-                                    SX126X_SET_RF_MODE \
-                                    SX126X_TX_PA_MODE}
+#  define SX126X_PARAMS           { .spi = SX126X_PARAM_SPI,                      \
+                                    .nss_pin = SX126X_PARAM_SPI_NSS,              \
+                                    .reset_pin = SX126X_PARAM_RESET,              \
+                                    .busy_pin = SX126X_PARAM_BUSY,                \
+                                    .dio1_pin = SX126X_PARAM_DIO1,                \
+                                    .type     = SX126X_PARAM_TYPE,                \
+                                    .regulator = SX126X_PARAM_REGULATOR,          \
+                                    SX126X_SET_RF_MODE                            \
+                                    SX126X_TX_PA_MODE                             \
+                                    SX126X_DIO2_MODE                              \
+                                    SX126X_DIO3_MODE                              \
+                                    SX126X_DIO3_ARG                               \
+                                   }
 #endif
 
 /**@}*/
