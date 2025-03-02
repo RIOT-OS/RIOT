8f89df491ccccd049ba974d57d3140ac24d57bf4
Gunar Schorcht <gunar@schorcht.net>
2025-03-02 11:10:00 +0100

    boards: common board definitions for ESP32-S2, ESP32-S2, ESP32-C3
    Adds `board_common_esp32*.h` files in which definitions can be made that are common for all boards with a certain ESP32x SoC variant.

---
+++ boards/common/esp32s3/include/board_common_esp32s3.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2025 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_common_esp32s3
+ * @brief       Board definitions that are common for all ESP32-S3 boards.
+ *
+ * This file contains board configurations that are valid for all ESP32-S3.
+ *
+ * For detailed information about the configuration of ESP32-S3 boards, see
+ * section \ref esp32_peripherals "Common Peripherals".
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ * @file
+ * @{
+ */
+
+#ifndef BOARD_COMMON_ESP32S3_H
+#define BOARD_COMMON_ESP32S3_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !DOXYGEN
+
+/**
+ * @name    ztimer Configuration valid for all ESP32-S3 boards
+ * @{
+ */
+#if CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ <= 80
+#define CONFIG_ZTIMER_USEC_ADJUST_SET   (880/CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ)
+#define CONFIG_ZTIMER_USEC_ADJUST_SLEEP ((CONFIG_ZTIMER_USEC_ADJUST_SET >> 2) * 5)
+#else
+#define CONFIG_ZTIMER_USEC_ADJUST_SET   (960/CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ)
+#define CONFIG_ZTIMER_USEC_ADJUST_SLEEP (CONFIG_ZTIMER_USEC_ADJUST_SET)
+#endif
+/** @} */
+
+#endif /* !DOXYGEN */
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
+
+#endif /* BOARD_COMMON_ESP32S3_H */
+/** @} */
