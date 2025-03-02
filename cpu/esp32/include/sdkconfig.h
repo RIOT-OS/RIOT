027f718eada0803e53729e936aaf123bc7d5cf79
Gunar Schorcht <gunar@schorcht.net>
2025-03-02 12:15:40 +0100

    cpu/esp32/sdkconfig: migration to ESP-IDF v5.4

--- cpu/esp32/include/sdkconfig.h
+++ cpu/esp32/include/sdkconfig.h
@@ -22,15 +22,6 @@
  */
 
 /*
- * Some files in ESP-IDF use functions from `stdlib.h` without including the
- * header. To avoid having to patch all these files, `stdlib.h` is included
- * in this header file, which in turn is included by every ESP-IDF file.
- */
-#if !defined(__ASSEMBLER__) && !defined(LD_FILE_GEN)
-#include <stdlib.h>
-#endif
-
-/*
  * The SoC capability definitions are often included indirectly in the
  * ESP-IDF files, although all ESP-IDF files require them. Since not all
  * ESP-IDF header files are included in RIOT, the SoC capability definitions
@@ -38,7 +29,9 @@
  * capabilities are included in this file and are thus available to all
  * ESP-IDF files. This avoids to update vendor code.
  */
+#ifndef LD_FILE_GEN
 #include "soc/soc_caps.h"
+#endif
 
 /**
  * @brief   SDK version number
@@ -64,6 +57,8 @@
 #endif
 #define CONFIG_ESP_CONSOLE_UART_BAUDRATE    STDIO_UART_BAUDRATE
 
+#define CONFIG_ESP_CONSOLE_ROM_SERIAL_PORT_NUM CONFIG_ESP_CONSOLE_UART_NUM
+
 /**
  * Log output configuration (DO NOT CHANGE)
  */
@@ -88,11 +83,16 @@
 #define CONFIG_ESP_TIMER_TASK_STACK_SIZE        3584
 #define CONFIG_ESP_TIMER_INTERRUPT_LEVEL        1
 #define CONFIG_TIMER_TASK_STACK_SIZE            CONFIG_ESP_TIMER_TASK_STACK_SIZE
+#define CONFIG_ESP_TIMER_TASK_AFFINITY          0
+#define CONFIG_ESP_TIMER_ISR_AFFINITY_CPU0      1
 
 #define CONFIG_APP_BUILD_TYPE_APP_2NDBOOT       1
 #define CONFIG_APP_BUILD_GENERATE_BINARIES      1
 #define CONFIG_APP_BUILD_BOOTLOADER             1
 #define CONFIG_APP_BUILD_USE_FLASH_SECTIONS     1
+#define CONFIG_APP_COMPILE_TIME_DATE            1
+#define CONFIG_APP_EXCLUDE_PROJECT_VER_VAR      1
+#define CONFIG_APP_RETRIEVE_LEN_ELF_SHA         9
 
 #define CONFIG_PARTITION_TABLE_CUSTOM_FILENAME  "partitions.csv"
 #define CONFIG_PARTITION_TABLE_FILENAME         "partitions_singleapp.csv"
@@ -103,33 +103,49 @@
  * BLE driver configuration (DO NOT CHANGE)
  */
 #ifdef MODULE_ESP_BLE
-#define CONFIG_ESP32_WIFI_ENABLED                       1   /* WiFi module has to be enabled */
-#define CONFIG_BT_ENABLED                               1
-#define CONFIG_BT_CONTROLLER_ONLY                       1
+#define CONFIG_BT_ENABLED                       1
+#define CONFIG_BT_CONTROLLER_ENABLED            1
+#define CONFIG_BT_CONTROLLER_ONLY               1
+#define CONFIG_ESP_COEX_ENABLED                 1
+#define CONFIG_ESP_WIFI_ENABLED                 1   /* WiFi module has to be enabled */
+#define CONFIG_SOC_BT_SUPPORTED                 SOC_BT_SUPPORTED
+#define CONFIG_SOC_PM_SUPPORT_BT_PD             SOC_PM_SUPPORT_BT_PD
+#define CONFIG_SOC_PM_SUPPORT_BT_WAKEUP         SOC_PM_SUPPORT_BT_WAKEUP
+
+#ifndef MODULE_ESP_WIFI_ANY
+#define CONFIG_ESP_PHY_ENABLED                  1
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_MAX_ACTIVE_TIME             10
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_MIN_ACTIVE_TIME             50
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_WAIT_BROADCAST_DATA_TIME    15
+#endif
+
 #else
-#define CONFIG_BT_ENABLED                               0
+#define CONFIG_BT_ENABLED                       0
 #endif
 
 /**
  * SPI RAM configuration (DO NOT CHANGE)
  */
 #ifdef  MODULE_ESP_SPI_RAM
+#define CONFIG_SPIRAM                           1
 #define CONFIG_SPIRAM_TYPE_AUTO                 1
 #define CONFIG_SPIRAM_SIZE                      -1
 #define CONFIG_SPIRAM_SPEED_40M                 1
-#define CONFIG_SPIRAM                           1
+#define CONFIG_SPIRAM_SPEED                     40
 #define CONFIG_SPIRAM_BOOT_INIT                 1
-#define CONFIG_SPIRAM_USE_MALLOC                0   /* using malloc requires QStaticQueue */
+#define CONFIG_SPIRAM_USE_MALLOC                1   /* using malloc requires QStaticQueue */
+#define CONFIG_SPIRAM_USE_CAPS_ALLOC            0   /* using cap instead of malloc */
 #define CONFIG_SPIRAM_MEMTEST                   1
 #define CONFIG_SPIRAM_MALLOC_ALWAYSINTERNAL     16384
 #define CONFIG_SPIRAM_MALLOC_RESERVE_INTERNAL   32768
+#define CONFIG_SOC_SPIRAM_SUPPORTED             SOC_SPIRAM_SUPPORTED
+#define CONFIG_ESP_SLEEP_PSRAM_LEAKAGE_WORKAROUND 1
 #endif
 
 /**
  * SPI Flash driver configuration (DO NOT CHANGE)
  */
 #define CONFIG_SPI_FLASH_ROM_DRIVER_PATCH           1
-#define CONFIG_SPI_FLASH_USE_LEGACY_IMPL            1
 #define CONFIG_SPI_FLASH_DANGEROUS_WRITE_ABORTS     1
 #define CONFIG_SPI_FLASH_YIELD_DURING_ERASE         1
 #define CONFIG_SPI_FLASH_ERASE_YIELD_DURATION_MS    20
@@ -142,6 +158,15 @@
 #define CONFIG_SPI_FLASH_SUPPORT_BOYA_CHIP          1
 #define CONFIG_SPI_FLASH_SUPPORT_TH_CHIP            1
 #define CONFIG_SPI_FLASH_SUPPORT_MXIC_OPI_CHIP      1
+
+/**
+ * RTC Clock configuration
+ */
+#ifdef MODULE_ESP_RTC_TIMER_32K
+#define CONFIG_RTC_CLK_SRC_EXT_CRYS                 1
+#else
+#define CONFIG_RTC_CLK_SRC_INT_RC                   1
+#endif
 
 /**
  * Ethernet driver configuration (DO NOT CHANGE)
@@ -164,32 +189,58 @@
  * Wi-Fi driver configuration (DO NOT CHANGE)
  */
 #ifdef MODULE_ESP_WIFI_ANY
-#define CONFIG_ESP32_WIFI_ENABLED               1
-#define CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM  10
-#define CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM 32
-#define CONFIG_ESP32_WIFI_DYNAMIC_TX_BUFFER     1
-#define CONFIG_ESP32_WIFI_TX_BUFFER_TYPE        1
-#define CONFIG_ESP32_WIFI_DYNAMIC_TX_BUFFER_NUM 32
-#define CONFIG_ESP32_WIFI_CACHE_TX_BUFFER_NUM   32  /* required when CONFIG_SPIRAM_USE_MALLOC=0 */
-#define CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED      1
-#define CONFIG_ESP32_WIFI_TX_BA_WIN             6
-#define CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED      1
-#define CONFIG_ESP32_WIFI_RX_BA_WIN             6
+#define CONFIG_CRYPTO_INTERNAL                      1
+#define CONFIG_ESP_PHY_ENABLED                      1
+#define CONFIG_ESP_WIFI_ENABLED                     1
+
+#define CONFIG_ESP_WIFI_AMPDU_RX_ENABLED            1
+#define CONFIG_ESP_WIFI_AMPDU_TX_ENABLED            1
+#define CONFIG_ESP_WIFI_AUTH_WPA2_PSK               1
+#define CONFIG_ESP_WIFI_CACHE_TX_BUFFER_NUM         32
+#define CONFIG_ESP_WIFI_DYNAMIC_RX_BUFFER_NUM       32
+#define CONFIG_ESP_WIFI_DYNAMIC_RX_MGMT_BUF         0
+#define CONFIG_ESP_WIFI_DYNAMIC_TX_BUFFER           1
+#define CONFIG_ESP_WIFI_DYNAMIC_TX_BUFFER_NUM       32
+#define CONFIG_ESP_WIFI_ENABLE_SAE_PK               0   /* default 1 for WPA3 */
+#define CONFIG_ESP_WIFI_ENABLE_WPA3_OWE_STA         0   /* default 1 for WPA3 */
+#define CONFIG_ESP_WIFI_ENABLE_WPA3_SAE             0   /* default 1 for WPA3 */
+#define CONFIG_ESP_WIFI_ESPNOW_MAX_ENCRYPT_NUM      7
+#define CONFIG_ESP_WIFI_GMAC_SUPPORT                1
+#define CONFIG_ESP_WIFI_IRAM_OPT                    0   /* default 1 */
+#define CONFIG_ESP_WIFI_MBEDTLS_CRYPTO              0   /* default 1 for WPA3 */
+#define CONFIG_ESP_WIFI_MBEDTLS_TLS_CLIENT          0   /* default 1 for WPA3 */
+#define CONFIG_ESP_WIFI_MGMT_SBUF_NUM               32
+#define CONFIG_ESP_WIFI_PW_ID                       ""
+#define CONFIG_ESP_WIFI_RX_BA_WIN                   6
+#define CONFIG_ESP_WIFI_RX_IRAM_OPT                 0   /* default 1 */
+#define CONFIG_ESP_WIFI_RX_MGMT_BUF_NUM_DEF         5
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_MAX_ACTIVE_TIME 10
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_MIN_ACTIVE_TIME 50
+#define CONFIG_ESP_WIFI_SLP_DEFAULT_WAIT_BROADCAST_DATA_TIME    15
+#define CONFIG_ESP_WIFI_SOFTAP_BEACON_MAX_LEN       752
+#define CONFIG_ESP_WIFI_STA_DISCONNECTED_PM_ENABLE  1
+#define CONFIG_ESP_WIFI_STATIC_RX_BUFFER_NUM        10
+#define CONFIG_ESP_WIFI_STATIC_RX_MGMT_BUFFER       1
+#define CONFIG_ESP_WIFI_TASK_PINNED_TO_CORE_0       1
+#define CONFIG_ESP_WIFI_TX_BA_WIN                   6
+#define CONFIG_ESP_WIFI_TX_BUFFER_TYPE              1
+
 #if defined(MODULE_ESP_IDF_NVS_FLASH) && !defined(CPU_FAM_ESP32C3)
-#define CONFIG_ESP32_WIFI_NVS_ENABLED           1
+#define CONFIG_ESP_WIFI_NVS_ENABLED                 1
 #endif
-#define CONFIG_ESP32_WIFI_TASK_PINNED_TO_CORE_0 1
-#define CONFIG_ESP32_WIFI_SOFTAP_BEACON_MAX_LEN 752
-#define CONFIG_ESP32_WIFI_MGMT_SBUF_NUM         32
-#define CONFIG_ESP32_WIFI_IRAM_OPT              1
-#define CONFIG_ESP32_WIFI_RX_IRAM_OPT           1
-#define CONFIG_ESP32_WIFI_ENABLE_WPA3_SAE       1
+
 #if defined(MODULE_ESP_WIFI_AP) || defined(MODULE_ESP_NOW)
-#define CONFIG_ESP_WIFI_SOFTAP_SUPPORT          1
+#define CONFIG_ESP_WIFI_SOFTAP_SUPPORT              1
 #endif
-#ifdef MODULE_ESP_BLE
-#define CONFIG_ESP32_WIFI_SW_COEXIST_ENABLE     1
+
+#if defined(MODULE_ESP_WIFI_ENTERPRISE)
+#define CONFIG_ESP_WIFI_ENTERPRISE_SUPPORT          1
 #endif
+
+#if defined(MODULE_ESP_BLE)
+#define CONFIG_ESP_COEX_SW_COEXIST_ENABLE           1
+#endif
+
 #endif
 
 /**
@@ -201,9 +252,6 @@
 
 #define CONFIG_ESP_PHY_MAX_TX_POWER                     20
 #define CONFIG_ESP_PHY_MAX_WIFI_TX_POWER                20
-
-#define CONFIG_ESP32_PHY_CALIBRATION_AND_DATA_STORAGE   CONFIG_ESP_PHY_CALIBRATION_AND_DATA_STORAGE
-#define CONFIG_ESP32_PHY_MAX_WIFI_TX_POWER              CONFIG_ESP_PHY_MAX_WIFI_TX_POWER
 
 /**
  * Flashpage configuration
@@ -243,8 +291,6 @@
 #define CONFIG_LCD_PANEL_IO_FORMAT_BUF_SIZE             CONFIG_LCD_DATA_BUF_SIZE
 #endif
 
-#endif /* DOXYGEN */
-
 /**
  * @brief   Include ESP32x family specific SDK configuration
  */
@@ -259,6 +305,80 @@
 #else
 #error "ESP32x family implementation missing"
 #endif
+
+#ifndef CONFIG_MMU_PAGE_SIZE
+#define CONFIG_MMU_PAGE_SIZE_64KB   1
+#define CONFIG_MMU_PAGE_SIZE        0x10000
+#endif
+
+#ifndef CONFIG_FREERTOS_NUMBER_OF_CORES
+#define CONFIG_FREERTOS_NUMBER_OF_CORES 1
+#endif
+
+#define CONFIG_ESP_DEBUG_OCDAWARE       1
+
+#define CONFIG_ADC_SUPPRESS_DEPRECATE_WARN  1
+
+#define CONFIG_HEAP_POISONING_DISABLED              1
+#define CONFIG_HEAP_TRACING_OFF                     1
+#define CONFIG_LOG_TAG_LEVEL_CACHE_BINARY_MIN_HEAP  1
+
+#define CONFIG_ULP_COPROC_RESERVE_MEM       0
+
+#ifdef SOC_RTC_MEM_SUPPORTED
+#define CONFIG_SOC_RTC_MEM_SUPPORTED        1
+#endif
+#ifdef SOC_RTC_FAST_MEM_SUPPORTED
+#define CONFIG_SOC_RTC_FAST_MEM_SUPPORTED   1
+#endif
+#ifdef SOC_RTC_SLOW_SUPPORTED
+#define CONFIG_SOC_RTC_SLOW_MEM_SUPPORTED   1
+#endif
+
+/**
+ * SDMMC Host configuration
+ */
+#ifdef SOC_SDMMC_HOST_SUPPORTED
+
+#define CONFIG_SOC_SDMMC_HOST_SUPPORTED     SOC_SDMMC_HOST_SUPPORTED
+#define CONFIG_SOC_SDMMC_DELAY_PHASE_NUM    SOC_SDMMC_DELAY_PHASE_NUM
+#define CONFIG_SOC_SDMMC_NUM_SLOTS          SOC_SDMMC_NUM_SLOTS
+#define CONFIG_SOC_SDMMC_SUPPORT_XTAL_CLOCK SOC_SDMMC_SUPPORT_XTAL_CLOCK
+#define CONFIG_SOC_SDMMC_USE_GPIO_MATRIX    SOC_SDMMC_USE_GPIO_MATRIX
+#define CONFIG_SOC_SDMMC_USE_IOMUX          SOC_SDMMC_USE_IOMUX
+
+#endif /* SOC_SDMMC_HOST_SUPPORTED */
+
+/**
+ * USB Serial/JTAG configuration
+ */
+#ifdef SOC_USB_SERIAL_JTAG_SUPPORTED
+
+#define CONFIG_SOC_USB_SERIAL_JTAG_SUPPORTED            1
+
+#define CONFIG_SOC_EFUSE_DIS_USB_JTAG                   SOC_EFUSE_DIS_USB_JTAG
+#define CONFIG_SOC_EFUSE_HARD_DIS_JTAG                  SOC_EFUSE_HARD_DIS_JTAG
+#define CONFIG_SOC_EFUSE_SOFT_DIS_JTAG                  SOC_EFUSE_SOFT_DIS_JTAG
+
+#ifndef CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG
+#define CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG              0
+#endif
+
+#ifndef CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG
+#define CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG    1
+#endif
+
+#define CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED      (CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG || \
+                                                         CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG)
+
+#define CONFIG_USJ_ENABLE_USB_SERIAL_JTAG               CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED
+
+#endif /* SOC_USB_SERIAL_JTAG_SUPPORTED */
+
+/* should be RIOT_APPLICATION but PROJECT_NAME must be less than 24 characters */
+#define PROJECT_NAME "RIOT-OS Application"
+
+#endif /* DOXYGEN */
 
 #ifdef __cplusplus
 extern "C" {
--- cpu/esp32/include/sdkconfig_esp32s3.h
+++ cpu/esp32/include/sdkconfig_esp32s3.h
@@ -34,40 +34,46 @@ extern "C" {
 
 /* Mapping of Kconfig defines to the respective enumeration values */
 #if CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_2
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     2
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     2
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_5
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     5
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     5
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_10
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     10
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     10
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_20
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     20
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     20
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_40
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     40
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     40
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_80
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     80
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     80
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_160
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     160
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     160
 #elif CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ_240
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     240
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     240
 #endif
 
 /**
  * @brief Defines the CPU frequency [values = 2, 5, 10, 10, 40, 80, 160, 240]
  */
-#ifndef CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ
-#define CONFIG_ESP32S3_DEFAULT_CPU_FREQ_MHZ     80
+#ifndef CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     80
 #endif
+
 /** @} */
 
 /**
  * ESP32-S3 specific RTC clock configuration
  */
-#define CONFIG_ESP32S3_RTC_CLK_CAL_CYCLES       (8 * 1024)
+#define CONFIG_RTC_CLK_CAL_CYCLES           1024
+
+#ifdef MODULE_ESP_RTC_TIMER_32K
+#endif
 
 /**
  * ESP32-S3 specific EFUSE configuration
  */
 #define CONFIG_EFUSE_MAX_BLK_LEN                256
+#define CONFIG_ESP_EFUSE_BLOCK_REV_MIN_FULL     0
+#define CONFIG_ESP_EFUSE_BLOCK_REV_MAX_FULL     199
 
 /**
  * ESP32-S3 specific MAC configuration
@@ -83,27 +89,27 @@ extern "C" {
  */
 #define CONFIG_ESPTOOLPY_FLASHFREQ_80M          1
 #define CONFIG_ESPTOOLPY_FLASHFREQ              "80m"
+#define CONFIG_ESPTOOLPY_FLASH_SAMPLE_MODE_STR  1
 
 /**
  * ESP32-S3 specific system configuration (DO NOT CHANGE)
  */
-#define CONFIG_ESP_TIMER_IMPL_SYSTIMER          1
-#define CONFIG_ESP_CONSOLE_MULTIPLE_UART        1
-
-#define CONFIG_ESP32S3_DEBUG_OCDAWARE           1
-
-#define CONFIG_ESP32S3_BROWNOUT_DET             1
-#define CONFIG_ESP32S3_BROWNOUT_DET_LVL         7
-
-#define CONFIG_ESP32S3_DEEP_SLEEP_WAKEUP_DELAY  2000
 #define CONFIG_ESP32S3_TRACEMEM_RESERVE_DRAM    0x0
-#define CONFIG_ESP32S3_ULP_COPROC_RESERVE_MEM   0
+
+#define CONFIG_ESP_BROWNOUT_DET                 1
+#define CONFIG_ESP_BROWNOUT_DET_LVL             7
+#define CONFIG_ESP_CONSOLE_MULTIPLE_UART        1
+#define CONFIG_ESP_DEBUG_OCDAWARE               1
+#define CONFIG_ESP_TIMER_IMPL_SYSTIMER          1
+
+#define CONFIG_ULP_COPROC_RESERVE_MEM   0
 
 /**
  * ESP32-S3 specific sleep configuration (DO NOT CHANGE)
  */
-#define CONFIG_ESP_SLEEP_RTC_BUS_ISO_WORKAROUND 1
-#define CONFIG_ESP_SLEEP_GPIO_RESET_WORKAROUND  1
+#define CONFIG_ESP_SLEEP_RTC_BUS_ISO_WORKAROUND         1
+#define CONFIG_ESP_SLEEP_GPIO_RESET_WORKAROUND          1
+#define CONFIG_ESP_SLEEP_WAIT_FLASH_READY_EXTRA_DELAY   2000
 
 /**
  * ESP32-S3 specific USB configuration
@@ -117,15 +123,13 @@ extern "C" {
  * ESP32-S3 specific SPI RAM configuration
  */
 #ifdef  MODULE_ESP_SPI_RAM
-#define CONFIG_ESP32S3_SPIRAM_SUPPORT           1
 #ifdef MODULE_ESP_SPI_OCT
 #define CONFIG_SPIRAM_MODE_OCT                  1
 #else
 #define CONFIG_SPIRAM_MODE_QUAD                 1
 #endif
-#define CONFIG_DEFAULT_PSRAM_CLK_IO             30
-#define CONFIG_DEFAULT_PSRAM_CS_IO              26
-#define CONFIG_SPIRAM_SUPPORT                   CONFIG_ESP32S3_SPIRAM_SUPPORT
+#define CONFIG_SPIRAM_CLK_IO                    30
+#define CONFIG_SPIRAM_CS_IO                     26
 #endif
 
 /**
@@ -148,7 +152,9 @@ extern "C" {
  * ESP32-S3 BLE driver configuration (DO NOT CHANGE)
  */
 #ifdef MODULE_ESP_BLE
-#define CONFIG_BT_CONTROLLER_ONLY                       1
+#define CONFIG_BT_ALARM_MAX_NUM                         50
+#define CONFIG_BT_BLE_CCA_MODE                          0
+#define CONFIG_BT_BLE_CCA_MODE_NONE                     1
 #define CONFIG_BT_CTRL_ADV_DUP_FILT_MAX                 30
 #define CONFIG_BT_CTRL_BLE_ADV_REPORT_DISCARD_THRSHOLD  20
 #define CONFIG_BT_CTRL_BLE_ADV_REPORT_FLOW_CTRL_NUM     100
@@ -159,15 +165,18 @@ extern "C" {
 #define CONFIG_BT_CTRL_BLE_STATIC_ACL_TX_BUF_NB         0
 #define CONFIG_BT_CTRL_CE_LENGTH_TYPE_EFF               0
 #define CONFIG_BT_CTRL_CE_LENGTH_TYPE_ORIG              1
+#define CONFIG_BT_CTRL_CHAN_ASS_EN                      1
 #define CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_DIS    1
 #define CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_EFF    0
-#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_EFF           10
-#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_P3            1
+#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_EFF           11
+#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_P9            1
+#define CONFIG_BT_CTRL_DUPL_SCAN_CACHE_REFRESH_PERIOD   0
 #define CONFIG_BT_CTRL_HCI_MODE_VHCI                    1
 #define CONFIG_BT_CTRL_HCI_TL                           1
 #define CONFIG_BT_CTRL_HCI_TL_EFF                       1
 #define CONFIG_BT_CTRL_HW_CCA_EFF                       0
 #define CONFIG_BT_CTRL_HW_CCA_VAL                       20
+#define CONFIG_BT_CTRL_LE_PING_EN                       1
 #define CONFIG_BT_CTRL_MODE_EFF                         1
 #define CONFIG_BT_CTRL_PINNED_TO_CORE                   0
 #define CONFIG_BT_CTRL_PINNED_TO_CORE_0                 1
@@ -180,8 +189,6 @@ extern "C" {
 #define CONFIG_BT_CTRL_SLEEP_MODE_EFF                   0
 #define CONFIG_BT_CTRL_TX_ANTENNA_INDEX_0               1
 #define CONFIG_BT_CTRL_TX_ANTENNA_INDEX_EFF             0
-#define CONFIG_BT_ENABLED                               1
-#define CONFIG_BT_SOC_SUPPORT_5_0                       1
 #endif
 
 #ifdef __cplusplus
