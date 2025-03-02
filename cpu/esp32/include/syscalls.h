1aa8534055b68c28fdc8f80d50f083c374ce9e71
Gunar Schorcht <gunar@schorcht.net>
2025-03-02 12:17:07 +0100

    cpu/esp32: fix return type of system_get_time_64

--- cpu/esp32/include/syscalls.h
+++ cpu/esp32/include/syscalls.h
@@ -27,7 +27,7 @@ extern "C" {
 #endif
 
 /** Time since boot in us (64bit version) */
-int64_t system_get_time_64 (void);
+uint64_t system_get_time_64 (void);
 
 /** initialize system watchdog timer and start it */
 void system_wdt_init (void);
