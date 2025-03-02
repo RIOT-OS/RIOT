976f13e5e7065286d820e3c08d4fad23955ccea7
Gunar Schorcht <gunar@schorcht.net>
2025-03-02 11:10:00 +0100

    boards: common board definitions for ESP32-S2, ESP32-S2, ESP32-C3
    Adds `board_common_esp32*.h` files in which definitions can be made that are common for all boards with a certain ESP32x SoC variant.

---
+++ boards/common/esp32s2/include/board_common_esp32s2.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_common_esp32s2
+ * @brief       Board definitions that are common for all ESP32-S2 boards.
+ *
+ * This file contains board configurations that are valid for all ESP32-S2.
+ *
+ * For detailed information about the configuration of ESP32-S2 boards, see
+ * section \ref esp32_peripherals "Common Peripherals".
+ *
+ * @author      Gunar Schorcht <gunar@schorcht.net>
+ * @file
+ * @{
+ */
+
+#ifndef BOARD_COMMON_ESP32S2_H
+#define BOARD_COMMON_ESP32S2_H
+
+#include "board_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
+
+#endif /* BOARD_COMMON_ESP32S2_H */
+/** @} */
