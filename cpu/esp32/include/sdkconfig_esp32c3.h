7086036b1cc00cca604ec260c28d6758d013e420
Gunar Schorcht <gunar@schorcht.net>
2025-03-02 12:15:40 +0100

    cpu/esp32/sdkconfig: migration to ESP-IDF v5.4

--- cpu/esp32/include/sdkconfig_esp32c3.h
+++ cpu/esp32/include/sdkconfig_esp32c3.h
@@ -34,38 +34,44 @@ extern "C" {
 
 /* Mapping of Kconfig defines to the respective enumeration values */
 #if CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_2
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     2
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     2
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_5
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     5
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     5
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_10
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     10
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     10
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_20
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     20
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     20
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_40
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     40
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     40
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_80
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     80
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     80
 #elif CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ_160
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     160
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     160
 #endif
 
 /**
  * @brief Defines the CPU frequency [values = 2, 5, 10, 20, 40, 80, 160]
  */
-#ifndef CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ
-#define CONFIG_ESP32C3_DEFAULT_CPU_FREQ_MHZ     80
+#ifndef CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ
+#define CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ     80
 #endif
+
 /** @} */
 
 /**
  * ESP32-C3 specific RTC clock configuration
  */
-#define CONFIG_ESP32C3_RTC_CLK_CAL_CYCLES       (8 * 1024)
+#define CONFIG_RTC_CLK_CAL_CYCLES               1024
+
+#ifdef MODULE_ESP_RTC_TIMER_32K
+#endif
 
 /**
  * ESP32-C3 specific EFUSE configuration
  */
 #define CONFIG_EFUSE_MAX_BLK_LEN                256
+#define CONFIG_ESP_EFUSE_BLOCK_REV_MIN_FULL     0
+#define CONFIG_ESP_EFUSE_BLOCK_REV_MAX_FULL     199
 
 /**
  * ESP32-C3 specific MAC configuration
@@ -90,8 +96,14 @@ extern "C" {
 /**
  * ESP32-C3 specific sleep configuration (DO NOT CHANGE)
  */
-#define CONFIG_ESP_SLEEP_POWER_DOWN_FLASH       1
-#define CONFIG_ESP_SLEEP_GPIO_RESET_WORKAROUND  1
+#define CONFIG_ESP32C3_LIGHTSLEEP_GPIO_RESET_WORKAROUND 1
+#define CONFIG_ESP_ROM_SUPPORT_DEEP_SLEEP_WAKEUP_STUB   1
+#define CONFIG_ESP_SLEEP_GPIO_RESET_WORKAROUND          1
+#define CONFIG_ESP_SLEEP_GPIO_ENABLE_INTERNAL_RESISTORS 0   /* we realize it */
+#define CONFIG_ESP_SLEEP_FLASH_LEAKAGE_WORKAROUND       1
+#define CONFIG_ESP_SLEEP_POWER_DOWN_FLASH               1
+#define CONFIG_ESP_SLEEP_WAIT_FLASH_READY_EXTRA_DELAY   0
+#define CONFIG_PM_POWER_DOWN_CPU_IN_LIGHT_SLEEP         1
 
 /**
  * ESP32-C3 specific USB configuration
@@ -102,6 +114,9 @@ extern "C" {
  * ESP32-C3 BLE driver configuration (DO NOT CHANGE)
  */
 #ifdef MODULE_ESP_BLE
+#define CONFIG_BT_ALARM_MAX_NUM                         50
+#define CONFIG_BT_BLE_CCA_MODE                          0
+#define CONFIG_BT_BLE_CCA_MODE_NONE                     1
 #define CONFIG_BT_CTRL_ADV_DUP_FILT_MAX                 30
 #define CONFIG_BT_CTRL_BLE_ADV_REPORT_DISCARD_THRSHOLD  20
 #define CONFIG_BT_CTRL_BLE_ADV_REPORT_FLOW_CTRL_NUM     100
@@ -112,17 +127,21 @@ extern "C" {
 #define CONFIG_BT_CTRL_BLE_STATIC_ACL_TX_BUF_NB         0
 #define CONFIG_BT_CTRL_CE_LENGTH_TYPE_EFF               0
 #define CONFIG_BT_CTRL_CE_LENGTH_TYPE_ORIG              1
+#define CONFIG_BT_CTRL_CHAN_ASS_EN                      1
 #define CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_DIS    1
 #define CONFIG_BT_CTRL_COEX_PHY_CODED_TX_RX_TLIM_EFF    0
-#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_EFF           10
-#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_P3            1
+#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_EFF           11
+#define CONFIG_BT_CTRL_DFT_TX_POWER_LEVEL_P9            1
+#define CONFIG_BT_CTRL_DUPL_SCAN_CACHE_REFRESH_PERIOD   0
 #define CONFIG_BT_CTRL_HCI_MODE_VHCI                    1
 #define CONFIG_BT_CTRL_HCI_TL                           1
 #define CONFIG_BT_CTRL_HCI_TL_EFF                       1
 #define CONFIG_BT_CTRL_HW_CCA_EFF                       0
 #define CONFIG_BT_CTRL_HW_CCA_VAL                       20
+#define CONFIG_BT_CTRL_LE_PING_EN                       1
 #define CONFIG_BT_CTRL_MODE_EFF                         1
 #define CONFIG_BT_CTRL_PINNED_TO_CORE                   0
+#define CONFIG_BT_CTRL_PINNED_TO_CORE_0                 1
 #define CONFIG_BT_CTRL_RX_ANTENNA_INDEX_0               1
 #define CONFIG_BT_CTRL_RX_ANTENNA_INDEX_EFF             0
 #define CONFIG_BT_CTRL_SCAN_DUPL_CACHE_SIZE             100
@@ -132,8 +151,7 @@ extern "C" {
 #define CONFIG_BT_CTRL_SLEEP_MODE_EFF                   0
 #define CONFIG_BT_CTRL_TX_ANTENNA_INDEX_0               1
 #define CONFIG_BT_CTRL_TX_ANTENNA_INDEX_EFF             0
-#define CONFIG_BT_ENABLED                               1
-#define CONFIG_BT_SOC_SUPPORT_5_0                       1
+#define CONFIG_BT_NIMBLE_COEX_PHY_CODED_TX_RX_TLIM_DIS  1
 #endif
 
 #ifdef __cplusplus
