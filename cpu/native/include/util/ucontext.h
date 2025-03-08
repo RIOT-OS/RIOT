6e5260c2d59d3510e10c1198165cf28eea574182
carl <115627588+carl-tud@users.noreply.github.com>
2025-03-08 15:47:36 +0100

    cpu/native: enable 64-bit thread arguments

--- cpu/native/include/util/ucontext.h
+++ cpu/native/include/util/ucontext.h
@@ -82,6 +82,51 @@ static inline void _context_set_fptr(ucontext_t *context, uintptr_t func) {
 }
 /** @} */
 
+/* MARK: - 64-bit support for makecontext */
+/**
+ * @name 64-bit support for makecontext
+ * @{
+ */
+#if defined(__LP64__) || defined(DOXYGEN)
+/**
+ * @brief Invokes thread task function
+ *
+ * This function fixes the otherwise necessary cast from `void*` to `int` of the thread
+ * task function pointer. We pass the task func and its argument in registers.
+ * See @ref makecontext64.
+ */
+extern void _start_task_func64(void);
+#endif
+
+/**
+ * @brief Like `makecontext`, allows 64-bit wide function argument on 64-bit platforms
+ * @param context `ucontext_t`
+ * @param func Function to be executed when context is applied
+ * @param arg Function argument, previously limited to `int` width, can now be up to 64 bits wide on 64-bit platforms
+ *
+ * Internally, we circumvent the 32-bit `int` limitation by passing the parameter in a register. This is done using
+ * a custom function defined in `native.S`
+ */
+static inline void makecontext64(ucontext_t *context, void (*func)(void), void* arg) {
+# if defined(__LP64__)
+    /* makecontext accepts int arguments. In RIOT, we use void* for the optional argument.
+     * To not truncate the argument pointer, we pass it in a register to _start_task_func64. */
+    makecontext(context, (void (*)(void))_start_task_func64, 0);
+
+#  if defined(__x86_64__)
+#   if defined(__linux__)
+     context->uc_mcontext.gregs[REG_R14] = (greg_t)func;
+     context->uc_mcontext.gregs[REG_R15] = (greg_t)arg;
+#   endif
+#  endif
+
+# else
+    /* On 32-bit platforms, the width of an int is enough to fit a pointer. */
+    makecontext(context, (void (*)(void))func, 1, arg);
+# endif
+}
+/** @} */
+
 /** @} */
 
 #ifdef __cplusplus
