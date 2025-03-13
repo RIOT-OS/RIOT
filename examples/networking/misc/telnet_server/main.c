50259008513fa397e5f6d118da3e742f8c8715e5
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2025-03-13 10:56:56 +0100

    examples/telnet_server: select stdio_default

--- examples/networking/misc/telnet_server/main.c
+++ examples/networking/misc/telnet_server/main.c
@@ -79,7 +79,6 @@ int main(void)
 
     /* start shell */
     printf("All up, awaiting connection on port %u\n", CONFIG_TELNET_PORT);
-    puts("Local shell disabled");
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
