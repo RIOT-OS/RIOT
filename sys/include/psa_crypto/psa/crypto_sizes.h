1feb1b8dc7051db7b9abeee5c60d0d124e2a82d0
Armin Wolf <W_Armin@gmx.de>
2025-03-25 18:59:18 +0100

    sys/psa_crypto: Split size definitions into separate file
    TODO
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- sys/include/psa_crypto/psa/crypto_sizes.h
+++ sys/include/psa_crypto/psa/crypto_sizes.h
@@ -38,24 +38,7 @@ extern "C" {
 #include "key_agreement/sizes.h"
 #include "key_derivation/sizes.h"
 #include "mac/sizes.h"
-
-/**
- * @brief   Functions to convert bits to bytes
- *
- * @param   bits
- *
- * @return  Number of bytes contained in bits
- */
-#define PSA_BITS_TO_BYTES(bits) (size_t)(((bits) + 7) / 8)
-
-/**
- * @brief   Functions to convert bytes to bits
- *
- * @param   bytes
- *
- * @return  Number of bits contained in bytes
- */
-#define PSA_BYTES_TO_BITS(bytes) ((bytes) * 8)
+#include "sizes.h"
 
 /**
  * @brief   Maximum key size in bytes, determined by the build system.
