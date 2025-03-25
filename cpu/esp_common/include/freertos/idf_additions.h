135952198332780c637455b0f51238641d7e1bd9
Gunar Schorcht <gunar@schorcht.net>
2025-03-25 10:31:21 +0100

    cpu/esp_common/freertos: migration to ESP-IDF v5.4

---
+++ cpu/esp_common/include/freertos/idf_additions.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2024 Gunar Schorcht
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * FreeRTOS to RIOT-OS adaption module for source code compatibility
+ */
+
+#ifndef FREERTOS_IDF_ADDITIONS_H
+#define FREERTOS_IDF_ADDITIONS_H
+
+#ifndef DOXYGEN
+
+#include "freertos/FreeRTOS.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DOXYGEN */
+#endif /* FREERTOS_IDF_ADDITIONS_H */
