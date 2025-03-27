8503ef1a6b19d0cfee9519b7774fcdc23ab398b4
AnnsAnn <git@annsann.eu>
2025-03-27 17:29:10 +0100

    fix: fix color of warn

--- dist/tools/picotool/picotool.sh
+++ dist/tools/picotool/picotool.sh
@@ -12,7 +12,7 @@ udev_warning='[WARNING] Picotool udev rules are not set yet. You won`t be able t
 
 if ! [ -e /etc/udev/rules.d/99-picotool.rules ]
 then
-  printf '\033[1;31m%s\033[0m\n' "$udev_warning"
+  printf '\033[1;31m%b\033[0m\n' "$udev_warning"
 fi
 
 echo "[INFO] Converting $ELFFILE to $UF2FILE"
