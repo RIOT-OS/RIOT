eb1d7a371a7b4e66a3edfc0ee2d1651e3a293414
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2025-04-02 22:39:36 +0200

    fixup! tests/core: add test for context switching from ISR

--- tests/core/isr_context_switch/tests/01-run.py
+++ tests/core/isr_context_switch/tests/01-run.py
@@ -6,15 +6,14 @@
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-# @author      Marian Buschsieweke <marian.buschsieweke@ovgu.de>
+# @author      Marian Buschsieweke <marian.buschsieweke@posteo.net>
 
 import sys
 from testrunner import run
 
 
-# A larger timeout is needed on slower MCUs. On the samr21-xpro, this test
-# takes about 60 seconds
-TIMEOUT = 120
+# use a larger timeout as context switches make take some time on slower MCUs
+TIMEOUT = 60
 
 
 def testfunc(child):
