18c7bdad8941f4b871577aafedb9d6af72b70d83
Fabian Hüßler <fabian.huessler@ml-pa.com>
2025-04-25 16:52:36 +0200

    drivers/sx126x: add shield_sx1262

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -460,6 +460,7 @@ PSEUDOMODULES += shell_cmds_default
 PSEUDOMODULES += shell_hooks
 PSEUDOMODULES += shell_lock_auto_locking
 PSEUDOMODULES += shield_llcc68
+PSEUDOMODULES += shield_sx1262
 PSEUDOMODULES += shield_w5100
 PSEUDOMODULES += slipdev_stdio
 PSEUDOMODULES += slipdev_l2addr
