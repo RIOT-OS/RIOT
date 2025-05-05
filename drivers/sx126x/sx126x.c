0c624bad1f6ea693dfad622f649768ff40e09ad7
Fabian Hüßler <fabian.huessler@ml-pa.com>
2025-05-05 17:52:37 +0200

    drivers/sx126x: image calibration after frequency
    Also changes standby configuration after image calibratin,
    else TCXO switches off afterwards

--- drivers/sx126x/sx126x.c
+++ drivers/sx126x/sx126x.c
@@ -22,6 +22,7 @@
 
 #include "sx126x_netdev.h"
 
+#include "macros/units.h"
 #include "net/lora.h"
 #include "periph/spi.h"
 
@@ -465,11 +466,46 @@ uint32_t sx126x_get_channel(const sx126x_t *dev)
     return dev->channel;
 }
 
+/* 9.2.1 Image Calibration for Specific Frequency Bands */
+static void _cal_img(sx126x_t *dev, uint32_t freq)
+{
+    sx126x_chip_modes_t state = sx126x_get_state(dev);
+    /* don't know what to do with frequencies that don't fit in the intervals from the datasheet */
+    if (freq >= MHZ(902) && freq <= MHZ(928)) {
+        /* 902 - 928 MHz band and anything upper */
+        sx126x_cal_img_in_mhz(dev, 902, 928);
+    }
+    else if (freq >= MHZ(863) && freq <= MHZ(870)) {
+        /* 863 - 870 MHz band */
+        sx126x_cal_img_in_mhz(dev, 863, 870);
+    }
+    else if (freq >= MHZ(779) && freq <= MHZ(787)) {
+        /* 779 - 787 MHz band */
+        sx126x_cal_img_in_mhz(dev, 779, 787);
+    }
+    else if (freq >= MHZ(470) && freq <= MHZ(510)) {
+        /* 470 - 510 MHz band */
+        sx126x_cal_img_in_mhz(dev, 470, 510);
+    }
+    else if (freq >= MHZ(430) && freq <= MHZ(440)) {
+        /* 430 - 440 MHz band and anything lower */
+        sx126x_cal_img_in_mhz(dev, 430, 440);
+    }
+    else {
+        /* Contact your Semtech representative for the other optimal calibration settings
+           outside of the given frequency bands. */
+    }
+    /* Image calibration sets the chip mode back to STBY_RC */
+    /* When using DIO3, TCXO switches off. */
+    sx126x_set_state(dev, state);
+}
+
 void sx126x_set_channel(sx126x_t *dev, uint32_t freq)
 {
     DEBUG("[sx126x]: sx126x_set_channel %" PRIu32 "Hz \n", freq);
     dev->channel = freq;
     sx126x_set_rf_freq(dev, dev->channel);
+    _cal_img(dev, freq);
 }
 
 uint8_t sx126x_get_bandwidth(const sx126x_t *dev)
