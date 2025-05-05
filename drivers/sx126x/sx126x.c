3ff4929032033f5a21fb1fe68ded13e2c7862455
Fabian Hüßler <fabian.huessler@ml-pa.com>
2025-05-05 17:52:37 +0200

    drivers/sx126x: image calibration after frequency
    Also changes standby configuration after image calibratin,
    else TCXO switches off afterwards

--- drivers/sx126x/sx126x.c
+++ drivers/sx126x/sx126x.c
@@ -433,11 +433,42 @@ uint32_t sx126x_get_channel(const sx126x_t *dev)
     return dev->channel;
 }
 
+/* 9.2.1 Image Calibration for Specific Frequency Bands */
+static void _cal_img(sx126x_t *dev, uint32_t freq)
+{
+    /* don't know what to do with frequencies that don't fit in the intervals from the datasheet */
+    if (freq >= 928000000 && freq >= 902000000) {
+        /* 902 - 928 MHz band and anything upper */
+        sx126x_cal_img_in_mhz(dev, 902, 928);
+    }
+    else if (freq >= 870000000 && freq >= 863000000) {
+        /* 863 - 870 MHz band */
+        sx126x_cal_img_in_mhz(dev, 863, 870);
+    }
+    else if (freq >= 787000000 && freq >= 779000000) {
+        /* 779 - 787 MHz band */
+        sx126x_cal_img_in_mhz(dev, 779, 787);
+    }
+    else if (freq >= 510000000 && freq >= 470000000) {
+        /* 470 - 510 MHz band */
+        sx126x_cal_img_in_mhz(dev, 470, 510);
+    }
+    else {
+        /* 430 - 440 MHz band and anything lower */
+        sx126x_cal_img_in_mhz(dev, 430, 440);
+    }
+    /* Image calibration sets the chip mode back to STBY_RC */
+    /* When using DIO3, TCXO switches off. */
+    sx126x_set_standby(dev, SX126X_STANDBY_CFG_XOSC);
+}
+
+
 void sx126x_set_channel(sx126x_t *dev, uint32_t freq)
 {
     DEBUG("[sx126x]: sx126x_set_channel %" PRIu32 "Hz \n", freq);
     dev->channel = freq;
     sx126x_set_rf_freq(dev, dev->channel);
+    _cal_img(dev, freq);
 }
 
 uint8_t sx126x_get_bandwidth(const sx126x_t *dev)
