5056acb8c01e9523cb41d2b0d04a173296c4dc7c
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2025-05-07 14:44:31 +0200

    fixup! net/hosts: add module for static hostname definitions

--- sys/include/net/hosts.h
+++ sys/include/net/hosts.h
@@ -20,12 +20,23 @@
  */
 
 #include "flash_utils.h"
+#include "net/ipv4/addr.h"
 #include "net/ipv6/addr.h"
 #include "xfa.h"
 
 #ifdef __cplusplus
 extern "C" {
 #else
+
+/**
+ * @brief           A single IPv4 host in the list of hostnames.
+ *
+ * This type is used internally by the @ref HOST_IPV4 macro.
+ */
+typedef struct {
+    FLASH_ATTR const char *name; /**< Name of the host    */
+    const ipv4_addr_t addr;      /**< Address of the host */
+} hostnames_v4_xfa_t;
 
 /**
  * @brief           A single IPv6 host in the list of hostnames.
@@ -36,6 +47,30 @@ typedef struct {
     FLASH_ATTR const char *name; /**< Name of the host    */
     const ipv6_addr_t addr;      /**< Address of the host */
 } hostnames_v6_xfa_t;
+
+/**
+ * @brief   Define IPv4 host
+ *
+ * @note    This is not available from C++, but a trivial C file can easily
+ *          hook up a `extern "C"` function implemented in C++.
+ *
+ * This macro is a helper for defining a hostname -> address mapping and adding
+ * it to the host names XFA (cross file array).
+ *
+ * Example:
+ *
+ * ```.c
+ * #include "hosts.h"
+ * HOST_IPV4(localhost, IPV4_ADDR_LOOPBACK);
+ * ```
+ */
+#define HOST_IPV4(host, ip) \
+    XFA_USE_CONST(hostnames_v4_xfa_t, hostnames_v4_xfa); \
+    static FLASH_ATTR const char _xfa_ ## host ## _hostname[] = #host; \
+    XFA_CONST(hostnames_v4_xfa_t, hostnames_v4_xfa, 0) _xfa_ ## host ## _host = { \
+        .name = _xfa_ ## host ## _hostname, \
+        .addr = ip, \
+    };
 
 /**
  * @brief   Define IPv6 host
@@ -69,11 +104,13 @@ typedef struct {
  * @see HOST_IPV6 macro.
  *
  * @param[in]   host_name       Host name to resolve into address
- * @param[out]  addr_out        Buffer to write result into
- * @param[in]   family          Either AF_INET, AF_INET6 or AF_UNSPEC
+ * @param[out]  addr_out        Buffer to write result into, but be able to accomondate
+ *                              an address of the selected family
+ * @param[in]   family          Either AF_INET (IPv4), AF_INET6 (IPv6) or AF_UNSPEC
+ *                              (either IPv4 or IPv6)
  *
  * @return      the size of the resolved address on success
- * @return      < 0 otherwise
+ * @return      -ENOENT if no matching entry could be found
  */
 int hosts_query(const char *host_name, void *addr_out, int family);
 
