94286a3cb7473330fa4e5413bd1c6e0540a2c7a2
Fabian Hüßler <fabian.huessler@ml-pa.com>
2025-05-28 22:29:11 +0200

    drivers/sdmmc_sdhc: preprocessor directive indentation

--- drivers/sdmmc/sdmmc_sdhc.c
+++ drivers/sdmmc/sdmmc_sdhc.c
@@ -48,45 +48,45 @@
 
 #if defined(CPU_SAMD5X) || defined(CPU_SAME5X)
 
-#ifndef SDHC_CLOCK
-#define SDHC_CLOCK          SAM0_GCLK_MAIN
-#endif
+#  ifndef SDHC_CLOCK
+#    define SDHC_CLOCK          SAM0_GCLK_MAIN
+#  endif
 
-#ifndef SDHC_CLOCK_SLOW
-#define SDHC_CLOCK_SLOW     SAM0_GCLK_TIMER
-#endif
+#  ifndef SDHC_CLOCK_SLOW
+#    define SDHC_CLOCK_SLOW     SAM0_GCLK_TIMER
+#  endif
 
 #else
-#error "CPU not supported"
+#  error "CPU not supported"
 #endif
 
 /* limit the Default and High Speed clock rates for debugging */
 #if CONFIG_SDMMC_CLK_MAX_400KHZ
-#define CONFIG_SDMMC_CLK_MAX        KHZ(400)
+#  define CONFIG_SDMMC_CLK_MAX        KHZ(400)
 #elif CONFIG_SDMMC_CLK_MAX_1MHZ
-#define CONFIG_SDMMC_CLK_MAX        MHZ(1)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(1)
 #elif CONFIG_SDMMC_CLK_MAX_4MHZ
-#define CONFIG_SDMMC_CLK_MAX        MHZ(4)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(4)
 #elif CONFIG_SDMMC_CLK_MAX_10MHZ
-#define CONFIG_SDMMC_CLK_MAX        MHZ(10)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(10)
 #elif CONFIG_SDMMC_CLK_MAX_20MHZ
-#define CONFIG_SDMMC_CLK_MAX        MHZ(20)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(20)
 #elif CONFIG_SDMMC_CLK_MAX_25MHZ
-#define CONFIG_SDMMC_CLK_MAX        MHZ(25)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(25)
 #else
-#define CONFIG_SDMMC_CLK_MAX        MHZ(50)
+#  define CONFIG_SDMMC_CLK_MAX        MHZ(50)
 #endif
 
 /* millisecond timer definitions dependent on active ztimer backend */
 #if IS_USED(MODULE_ZTIMER_MSEC)
-#define _ZTIMER_CLOCK           ZTIMER_MSEC
-#define _ZTIMER_TICKS_PER_MS    1
+#  define _ZTIMER_CLOCK           ZTIMER_MSEC
+#  define _ZTIMER_TICKS_PER_MS    1
 
 #elif IS_USED(MODULE_ZTIMER_USEC)
-#define _ZTIMER_CLOCK           ZTIMER_USEC
-#define _ZTIMER_TICKS_PER_MS    US_PER_MS
+#  define _ZTIMER_CLOCK           ZTIMER_USEC
+#  define _ZTIMER_TICKS_PER_MS    US_PER_MS
 #else
-#error "Either module ztimer_msec or ztimer_usec is needed"
+#  error "Either module ztimer_msec or ztimer_usec is needed"
 #endif
 
 #define _ZTIMER_ACQUIRE()       ztimer_acquire(_ZTIMER_CLOCK)
