41fd70e06fb383405cdd022cd8061246bfcc7b9d
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2025-06-02 19:56:21 +0200

    build-sytem/openocd: detect serial port TTY
    This adds a default `TTY_BOARD_FILTER` for use with the Tigard debugger,
    so that when the programmer is a Tigard that tigard is also used by
    default for the serial connection.
    
    In addition, this allows selecting the TTY via the programmer serial
    number.
    
    Co-authored-by: crasbe <crasbe@gmail.com>

--- makefiles/tools/openocd-adapters/ftdi.inc.mk
+++ makefiles/tools/openocd-adapters/ftdi.inc.mk
@@ -18,3 +18,22 @@ ifeq (swd, $(OPENOCD_TRANSPORT))
     OPENOCD_ADAPTER_INIT += -c 'ftdi layout_signal SWD_EN -data 0'
   endif
 endif
+
+ifeq (tigard,$(OPENOCD_FTDI_ADAPTER))
+  # The Tigard provides both (bitbanging) SWD/JTAG debugger and UART adapter.
+  # When freshly connected, the Tigard will provide to UART interface, of
+  # which the first will be used for UART and the second be reconfigured for
+  # bit-banging JTAG/SWD. We assume the UART of the Tigard is used for serial
+  # by default.
+  TTY_BOARD_FILTER ?= --model 'Tigard V1' --iface-num 0
+
+  # The debug adapter and the UART adapter are the same device, so they have
+  # the same serial number
+  DEBUG_ADAPTER_ID_IS_TTY_SERIAL ?= 1
+
+  # If the user provided a programmer serial, we also use that to detect the
+  # TTY.
+  ifneq (,$(SERIAL))
+    TTY_SELECT_CMD += --serial "$(SERIAL)"
+  endif
+endif
