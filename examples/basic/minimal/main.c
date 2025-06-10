4d7cc7dd9aa31ca371a1f627f98f24dfd4bc32ce
AnnsAnn <git@annsann.eu>
2025-06-10 12:30:01 +0200

    [Temp PR] Cleanup Example

--- examples/basic/minimal/main.c
+++ examples/basic/minimal/main.c
@@ -5,21 +5,21 @@
 #include <string.h>
 #include "periph/gpio.h"
 
-#define UART_UARTFR_RXFF_BITS 1<<6
-#define UART_UARTFR_TXFE_BITS 1<<7
-
 int main(void) {
-    xosc_sleep(1000); // Sleep for 3 secs to connect
+    xosc_sleep(1000); // Sleep for 1s to connect
 
     gpio_init(LED0_PIN_ID, GPIO_OUT); // Initialize the LED pin
     gpio_init(OSC_DEBUG_PIN_ID, GPIO_OUT); // Initialize the debug pin
+
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s CPU.\n", RIOT_CPU);
 
     uint32_t i = 0;
     while (true) {
         gpio_toggle(LED0_PIN_ID); // Toggle the LED
         gpio_toggle(OSC_DEBUG_PIN_ID); // Toggle the debug pin
         printf("Hello World! %u\n", i++); // Print a message to the console
-        xosc_sleep(300); // Sleep for 1 sec
+        xosc_sleep(1000); // Sleep for 1 sec
     }
   return 0;
 }
