bdf2e694c043bdcc15116588c943ca7649ff8350
AnnsAnn <git@annsann.eu>
2025-06-10 12:02:30 +0200

    boards: Raspberry Pi Pico 2 support

--- cpu/rp2350/cpu.c
+++ cpu/rp2350/cpu.c
@@ -1,3 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+/**
+ * @ingroup         cpu_rp2350
+ * @{
+ *
+ * @file
+ * @brief           CPU initialization implementation for the RP2350
+ *
+ * @author          Tom Hert <git@annsann.eu>
+ */
+
 #include "cpu.h"
 
 #include "RP2350.h"
@@ -16,18 +32,43 @@ void gpio_reset(void) {
   reset_component(RESET_IO_BANK0, RESET_IO_BANK0);
 }
 
-void cpu_init(void) {
-  /* initialize the Cortex-M core */
-  //cortexm_init();
-  gpio_reset();
-  //_cpu_reset();
+#define GPIO_FUNC_SIO 5
+// Get it, Pin + Init, hahahaha
+void pinit(void) {
+    // Set LED (25) and Pin 15 so we can debug with them
+    IO_BANK0->GPIO15_CTRL = GPIO_FUNC_SIO;
+    IO_BANK0->GPIO25_CTRL = GPIO_FUNC_SIO;
 
-  /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
-  early_init();
+    // Clear the ISO bits for GPIO15 and GPIO25
+    // Otherwise the GPIOs will not work
+    atomic_clear(&PADS_BANK0->GPIO15, PADS_BANK0_ISO_BITS);
+    atomic_clear(&PADS_BANK0->GPIO15, PADS_BANK0_GPIO0_IE_BITS);
+    atomic_clear(&PADS_BANK0->GPIO25, PADS_BANK0_ISO_BITS);
+
+    // Set the GPIO function for GPIO15 and GPIO25
+    // GPIO25 is used for the LED
+    // GPIO15 is used for debugging via Oscilloscope
+    SIO->GPIO_OE_SET = 1<<15 | 1<<25;
+    SIO->GPIO_OUT = 1<<15 | 1<<25;
+    SIO->GPIO_OUT_CLR = 1UL << 25;
+}
+
+void cpu_init(void) {
+  /* initialize the Cortex-M core, once UART support is moved
+   * to shared driver as currently this will cause unhandled interrupts */
+  /* cortexm_init(); */
+
+  gpio_reset();
+  pinit();
 
   clock_reset();
+
   cpu_clock_init();
+
+  early_init();
 
   /* trigger static peripheral initialization */
   periph_init();
 }
+
+/** @} */
--- cpu/rp2350/include/cpu_conf.h
+++ cpu/rp2350/include/cpu_conf.h
@@ -1,13 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
 #pragma once
 
-#include "cpu_conf_common.h"
+/**
+ * @ingroup         cpu_rp2350
+ * @{
+ * @file
+ * @brief           CPU configuration for the RP2350
+ *
+ * @author          Tom Hert <git@annsann.eu>
+ */
+
 #include "RP2350.h"
 #include "core_cm33.h"
+#include "cpu_conf_common.h"
 
-#define __CHECK_DEVICE_DEFINES
-
-#define CPU_DEFAULT_IRQ_PRIO (1U)
-#define CPU_IRQ_NUMOF (52U)
+#define CPU_DEFAULT_IRQ_PRIO 1u
+#define CPU_IRQ_NUMOF 52u
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,3 +29,5 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+/** @} */
