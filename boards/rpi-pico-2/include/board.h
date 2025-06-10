7f7422eccd5c127e1b6f013b7d695a60b938ef16
AnnsAnn <git@annsann.eu>
2025-06-10 12:19:09 +0200

    Fix: Cleanup leftovers within the board def

--- boards/rpi-pico-2/include/board.h
+++ boards/rpi-pico-2/include/board.h
@@ -11,21 +11,6 @@
 extern "C" {
 #endif
 
-/**
- * @name    LED configuration
- * @{
- */
-#define LED0_PIN        GPIO_PIN(0, 25)
-#define LED0_ON         do {SIO->GPIO_OUT_SET = 1UL << 25;} while (0)
-#define LED0_OFF        do {SIO->GPIO_OUT_CLR = 1UL << 25;} while (0)
-#define LED0_TOGGLE     do {SIO->GPIO_OUT_XOR = 1UL << 25;} while (0)
-#define LED0_NAME       "LED(Green)"
-
-#define PIN_OUT_ON         do {SIO->GPIO_OUT_SET = 1UL << 15;} while (0)
-#define PIN_OUT_OFF        do {SIO->GPIO_OUT_CLR = 1UL << 15;} while (0)
-#define PIN_OUT_TOGGLE     do {SIO->GPIO_OUT_XOR = 1UL << 15;} while (0)
-/** @} */
-
 #ifdef __cplusplus
 }
 #endif
