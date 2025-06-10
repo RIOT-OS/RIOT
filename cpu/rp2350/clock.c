359d3a9efdba0ce5648f1186d0f5c5cba363e240
AnnsAnn <git@annsann.eu>
2025-06-10 12:02:30 +0200

    boards: Raspberry Pi Pico 2 support

--- cpu/rp2350/clock.c
+++ cpu/rp2350/clock.c
@@ -1,51 +1,71 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+/**
+ * @ingroup         cpu_rp2350
+ * @{
+ *
+ * @file
+ * @brief           Clock configuration implementation for the RP2350
+ *
+ * @author          Tom Hert <git@annsann.eu>
+ */
+
 #include "periph_cpu.h"
 
 void clock_reset(void) {
-    // Reset the clock system
+    /* Reset the clock system */
     reset_component(RESET_PLL_SYS, RESET_PLL_SYS);
 }
 
 /**
  * @brief Configures the XOSC and then sets CLK_SYS, PLL_SYS and CLK_PERI to it
- * @warning Make sure to call clock_reset() before this function to reset the clock system
- * @note RP2350 Docs Chapter 8, mostly 8.2 for more details
-*/
-void cpu_clock_init(void) { 
-    // Enable the XOSC
-    xosc_start(); 
+ * @warning Make sure to call clock_reset() before this function to reset the
+ * clock system
+ * @see RP2350 Docs Chapter 8, mostly 8.2 for more details
+ */
+void cpu_clock_init(void) {
+    /* Enable the XOSC */
+    xosc_start();
 
-    /**
-     * Setup the PLL using the XOSC as the reference clock.
-     */
-    //PLL_SYS->CS = REF_DIV; // Set the reference divider
-    PLL_SYS->FBDIV_INT = 125; // Set the feedback divider
+    /* Setup the PLL using the XOSC as the reference clock. */
+    PLL_SYS->FBDIV_INT =
+    PLL_FEEDBACK_DIVIDER_VALUE; /* Set the feedback divider */
 
-    /**
-     * Set the post-dividers for the PLL output.
-     */
+    /* Set the post-dividers for the PLL output.*/
     PLL_SYS->PRIM = PDIV;
-    // Turn on PLL
-    atomic_clear(&PLL_SYS->PWR, PLL_PWR_PD_BITS | PLL_PWR_VCOPD_BITS | PLL_PWR_POSTDIVPD_BITS);
+    /* Turn on PLL */
+    atomic_clear(&PLL_SYS->PWR,
+        PLL_PWR_PD_BITS | PLL_PWR_VCOPD_BITS | PLL_PWR_POSTDIVPD_BITS);
 
-    // sleep 10ms to allow the PLL to stabilize
+    /* sleep 10ms to allow the PLL to stabilize */
     xosc_sleep(10);
 
-    // // Wait for lock
-    // while (!(PLL_SYS->CS & PLL_CS_LOCK_BITS)) {
-    //     // Wait for the PLL to lock
-    // }
+    /* Based on the description in chapter 8 this is something that should be done
+     * However, it appears to cause issues and is not done by other examples on the
+     * internet. This needs to be investigated further. */
 
-    // AUXSRC = 0x0 7:5 && SRC == 0x0 0 
-    // CLOCKS: CLK_SYS_CTRL CLK_PERI_CTRL_ENABLE_BIT
-    CLOCKS->CLK_SYS_CTRL = 1;
+    /* Wait for lock */
+    /* while (!(PLL_SYS->CS & PLL_CS_LOCK_BITS)) { */
+    /* Wait for the PLL to lock */
+    /* } */
 
-    // This register contains one decoded bit for each of the clock sources enumerated in the CTRL SRC field.
-    // The bit does not directly correlate with the value of the SRC field
-    // For example 0x0 is the first bit while 0x1 is the second bit
-    // In some way this makes sense, in some way I lost too much time on this
-    while (CLOCKS->CLK_SYS_SELECTED != 2) {}
+    /* AUXSRC = 0x0 7:5 && SRC == 0x0 0 */
+    CLOCKS->CLK_SYS_CTRL = CLK_SYS_PERI_CTRL_ENABLE_BIT;
 
-    // src: CLOCKS: CLK_PERI_CTRL 
-    // AUXSRC = 0x0 -> CLK_SYS Indirectly through lower line
+    /* This register contains one decoded bit for each of the clock sources
+     * enumerated in the CTRL SRC field. The bit does not directly correlate with
+     * the value of the SRC field For example 0x0 is the first bit while 0x1 is
+     * the second bit. In some way this makes sense, in some way I lost too much
+     * time on this. */
+    while (CLOCKS->CLK_SYS_SELECTED != CLK_SYS_SELECTED_PERI_FIELD_VALUE) {
+    }
+
+    /* AUXSRC = 0x0 -> CLK_SYS Indirectly through lower line */
     CLOCKS->CLK_PERI_CTRL = CLK_PERI_CTRL_ENABLE_BIT;
 }
+
+/** @} */
--- cpu/rp2350/cpu.c
+++ cpu/rp2350/cpu.c
@@ -1,3 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+/**
+ * @ingroup         cpu_rp2350
+ * @{
+ *
+ * @file
+ * @brief           CPU initialization implementation for the RP2350
+ *
+ * @author          Tom Hert <git@annsann.eu>
+ */
+
 #include "cpu.h"
 
 #include "RP2350.h"
@@ -12,22 +28,32 @@
 #define DEBUG_WITH_OSC
 
 void gpio_reset(void) {
-  reset_component(RESET_PADS_BANK0, RESET_PADS_BANK0);
-  reset_component(RESET_IO_BANK0, RESET_IO_BANK0);
+    reset_component(RESET_PADS_BANK0, RESET_PADS_BANK0);
+    reset_component(RESET_IO_BANK0, RESET_IO_BANK0);
 }
 
 void cpu_init(void) {
-  /* initialize the Cortex-M core */
-  //cortexm_init();
-  gpio_reset();
-  //_cpu_reset();
+    /* initialize the Cortex-M core, once UART support is moved
+     * to shared driver as currently this will cause unhandled interrupts */
+    /* cortexm_init(); */
 
-  /* initialize stdio prior to periph_init() to allow use of DEBUG() there */
-  early_init();
+    /* Reset GPIO state */
+    gpio_reset();
 
-  clock_reset();
-  cpu_clock_init();
+    /* Reset clock to default state */
+    clock_reset();
 
-  /* trigger static peripheral initialization */
-  periph_init();
+    /* initialize the CPU clock */
+    cpu_clock_init();
+
+    /* initialize the early peripherals */
+    early_init();
+
+    /* trigger static peripheral initialization */
+    periph_init();
+
+    /* initialize the board */
+    board_init();
 }
+
+/** @} */
