e9e655a299f10c9f5a247657bafdb26b8ef56cb6
Ann🐸 <git@annsann.eu>
2025-06-24 10:31:36 +0200

    Update cpu/rp2350/include/clock_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- cpu/rp2350/include/clock_conf.h
+++ cpu/rp2350/include/clock_conf.h
@@ -94,16 +94,16 @@
 
 #if (PLL_VCO_FEEDBACK_SCALE_MIN < PLL_VCO_FEEDBACK_SCALE_MIN) || \
     (PLL_VCO_FEEDBACK_SCALE_MAX > PLL_VCO_FEEDBACK_SCALE_MAX)
-#error "Value for PLL_VCO_FEEDBACK_SCALE out of range, check config"
+#  error "Value for PLL_VCO_FEEDBACK_SCALE out of range, check config"
 #endif
 #if (PLL_REF_DIV_MIN < PLL_REF_DIV_MIN) || (PLL_REF_DIV_MAX > PLL_REF_DIV_MAX)
-#error "Value for PLLxosc_sleep_REF_DIV out of range, check config"
+#  error "Value for PLLxosc_sleep_REF_DIV out of range, check config"
 #endif
 #if (PLL_POSTDIV_MIN < PLL_POSTDIV_MIN) || (PLL_POSTDIV_MAX > PLL_POSTDIV_MAX)
-#error "Value for PLL_POSTDIV out of range, check config"
+#  error "Value for PLL_POSTDIV out of range, check config"
 #endif
 #if ((CLOCK_XOSC > CLOCK_XOSC_MAX) || (CLOCK_XOSC < CLOCK_XOSC_MIN))
-#error "Value for CLOCK_XOSC out of range, check config"
+#  error "Value for CLOCK_XOSC out of range, check config"
 #endif
 
 /** Post divider for the PLL, calculated based on the post divider values */
