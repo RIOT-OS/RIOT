8ab9956ae5b88bf7ff4f99eda14acdd46b136732
crasbe <crasbe@gmail.com>
2025-06-26 15:49:29 +0200

    sys/net/sntp: migrate from xtimer to ztimer

--- sys/include/net/sntp.h
+++ sys/include/net/sntp.h
@@ -15,6 +15,10 @@
  * @brief       Simple Network Time Protocol (SNTP) implementation
  * @{
  *
+ * @note        The current implementation of SNTP uses @ref sys_ztimer64 with
+ *              microsecond accuracy, which can have a strong impact on
+ *              the power consumption of your device.
+ *
  * @file
  * @brief       SNTP definitions
  *
@@ -27,7 +31,7 @@
 
 #include "net/ntp_packet.h"
 #include "net/sock/udp.h"
-#include "xtimer.h"
+#include "ztimer64.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -45,7 +49,7 @@ extern "C" {
 int sntp_sync(sock_udp_ep_t *server, uint32_t timeout);
 
 /**
- * @brief Get real time offset from system time as returned by @ref xtimer_now64()
+ * @brief Get real time offset from system time as returned by @ref ztimer64_now()
  *
  * @return Real time offset in microseconds relative to 1900-01-01 00:00 UTC
  */
@@ -58,7 +62,8 @@ int64_t sntp_get_offset(void);
  */
 static inline uint64_t sntp_get_unix_usec(void)
 {
-    return (uint64_t)(sntp_get_offset() - (NTP_UNIX_OFFSET * US_PER_SEC) + xtimer_now_usec64());
+    return (uint64_t)(sntp_get_offset() - (NTP_UNIX_OFFSET * US_PER_SEC) + \
+                      ztimer64_now(ZTIMER64_USEC));
 }
 
 #ifdef __cplusplus
