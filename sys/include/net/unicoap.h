6b9e5433e69dc895842418e85bdd89ed400d7c8a
Carl Seifert <115627588+carl-tud@users.noreply.github.com>
2025-07-07 16:43:49 +0200

    net/unicoap: add module basics

---
+++ sys/include/net/unicoap.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2024-2025 Carl Seifert
+ * Copyright (C) 2024-2025 TU Dresden
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for
+ * more details.
+ */
+
+#pragma once
+
+/**
+ * @addtogroup net_unicoap
+ * @{
+ */
+
+/**
+ * @file
+ * @brief  Main header for `unicoap`
+ * @author Carl Seifert <carl.seifert1@mailbox.tu-dresden.de>
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
