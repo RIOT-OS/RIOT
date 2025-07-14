2f29aa6678258fff6e9fb46690f3ec7709bf2712
krzysztof-cabaj <kcabaj@gmail.com>
2025-07-14 14:18:12 +0200

    boards/stm32: change licenses in .h files to SPDX

--- boards/stm32l496g-disco/include/board.h
+++ boards/stm32l496g-disco/include/board.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2018 Inria
- * Copyright (C) 2023 Gunar Schorcht
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2018 Inria
+ * SPDX-FileCopyrightText: 2023 Gunar Schorcht
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
