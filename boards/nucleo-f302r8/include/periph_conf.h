f69f0fdd21f2bc003edbd1ec400cdd7866fd6949
krzysztof-cabaj <kcabaj@gmail.com>
2025-07-14 15:46:19 +0200

    boards/nucleo: move licence in .h files to SPDX

--- boards/nucleo-f302r8/include/periph_conf.h
+++ boards/nucleo-f302r8/include/periph_conf.h
@@ -1,11 +1,8 @@
 /*
- * Copyright (C) 2017 Inria
- * Copyright (C) 2015 Freie Universität Berlin
- * Copyright (C) 2015 Hamburg University of Applied Sciences
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2017 Inria
+ * SPDX-FileCopyrightText: 2015 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2015 Hamburg University of Applied Sciences
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
--- boards/nucleo-l452re/include/periph_conf.h
+++ boards/nucleo-l452re/include/periph_conf.h
@@ -1,12 +1,9 @@
 /*
- * Copyright (C) 2017 Freie Universität Berlin
- * Copyright (C) 2017 Inria
- * Copyright (C) 2017 HAW-Hamburg
- * Copyright (C) 2018 Fundacion Inria Chile
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2017 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2017 Inria
+ * SPDX-FileCopyrightText: 2017 HAW-Hamburg
+ * SPDX-FileCopyrightText: 2018 Fundacion Inria Chile
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
