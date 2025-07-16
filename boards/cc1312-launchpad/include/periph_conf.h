88de716ff5ac2ba53f3f745de052615a33932dbb
krzysztof-cabaj <kcabaj@gmail.com>
2025-07-16 18:29:20 +0200

    boards/cc: move licenses to SPDX format for Texas Instrumnets boards

--- boards/cc1312-launchpad/include/periph_conf.h
+++ boards/cc1312-launchpad/include/periph_conf.h
@@ -1,11 +1,8 @@
 /*
- * Copyright (C)    2016 Nicholas Jackson
- *                  2017 HAW Hamburg
- *                  2020 Locha Inc
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2016 Nicholas Jackson
+ * SPDX-FileCopyrightText: 2017 HAW Hamburg
+ * SPDX-FileCopyrightText: 2020 Locha Inc
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
