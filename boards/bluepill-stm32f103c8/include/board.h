4fce8b5073eecba82bde346636d03c6a385503c9
krzysztof-cabaj <kcabaj@gmail.com>
2025-07-16 22:01:49 +0200

    boards/{black,blue}pill: move licenses to SPDX format

--- boards/bluepill-stm32f103c8/include/board.h
+++ boards/bluepill-stm32f103c8/include/board.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2015 TriaGnoSys GmbH
- *               2017 Alexander Kurth, Sören Tempel, Tristan Bruns
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2015 TriaGnoSys GmbH
+ * SPDX-FileCopyrightText: 2017 Alexander Kurth, Sören Tempel, Tristan Bruns
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
