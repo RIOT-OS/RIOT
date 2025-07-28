8fed5ffd2c2bea7073d21d2658c1b3ec20e82f6d
krzysztof-cabaj <kcabaj@gmail.com>
2025-07-28 09:27:27 +0200

    boards/nrf: move licenses to SPDX format

--- boards/nrf52840dongle/include/periph_conf.h
+++ boards/nrf52840dongle/include/periph_conf.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2019 Christian Amsüss <chrysn@fsfe.org>
- *               2021 Freie Universität Berlin
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2019 Christian Amsüss <chrysn@fsfe.org>
+ * SPDX-FileCopyrightText: 2021 Freie Universität Berlin
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
