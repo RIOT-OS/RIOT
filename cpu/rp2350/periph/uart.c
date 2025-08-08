effd7c249bc48c69ca3ba8ab3d7d414a2ef2b7af
AnnsAnn <git@annsann.eu>
2025-08-08 16:55:49 +0200

    start adapting uart type

--- cpu/rp2350/periph/uart.c
+++ cpu/rp2350/periph/uart.c
@@ -19,18 +19,25 @@
 #include "periph_cpu.h"
 
 #include "regs/uart.h"
+#include <RP2350.h>
 
-#define ENABLE_DEBUG    0
+#define ENABLE_DEBUG 0
 #include "debug.h"
 
 #include "board.h"
 
 static uart_isr_ctx_t ctx[UART_NUMOF];
 
+/* back up values of registers used during uart_poweroff() / uart_poweron() */
+static uint32_t uartibrd;
+static uint32_t uartfbrd;
+static uint32_t uartlcr_h;
+static uint32_t uartcr;
+
 /** Pico1 uart uses non-sdk conform defines
  * @todo Change Pico1 defines if I ever get around to it
  */
-#define UART0_UARTIMSC_RXIM_Msk  (UART_UARTIMSC_RXIM_BITS)
+#define UART0_UARTIMSC_RXIM_Msk (UART_UARTIMSC_RXIM_BITS)
 
 void _irq_enable(uart_t uart)
 {
@@ -42,8 +49,8 @@ void _irq_enable(uart_t uart)
 }
 
 int uart_mode(uart_t uart, uart_data_bits_t data_bits, uart_parity_t parity,
-    uart_stop_bits_t stop_bits) {
-
+              uart_stop_bits_t stop_bits)
+{
     assert((unsigned)uart < UART_NUMOF);
     UART0_Type *dev = uart_config[uart].dev;
 
@@ -52,7 +59,7 @@ int uart_mode(uart_t uart, uart_data_bits_t data_bits, uart_parity_t parity,
 
     /* Disable the UART before changing the mode */
     atomic_clear(&dev->UARTCR, UART_UARTCR_UARTEN_BITS | UART_UARTCR_RXE_BITS |
-        UART_UARTCR_TXE_BITS);
+                                   UART_UARTCR_TXE_BITS);
 
     /* Set the data bits, parity, and stop bits
      * Set to 8 bits (0b11) based on Table 1035 page 976
@@ -61,16 +68,16 @@ int uart_mode(uart_t uart, uart_data_bits_t data_bits, uart_parity_t parity,
     dev->UARTLCR_H = 0b11 << 5;
 
     switch (parity) {
-        case UART_PARITY_NONE:
-            break;
-        // case UART_PARITY_EVEN:
-        //     io_reg_atomic_set(&dev->UARTLCR_H, UART0_UARTLCR_H_EPS_Msk | UART0_UARTLCR_H_PEN_Msk);
-        //     break;
-        // case UART_PARITY_ODD:
-        //     io_reg_atomic_set(&dev->UARTLCR_H, UART0_UARTLCR_H_PEN_Msk);
-        //     break;
-        default:
-            return UART_NOMODE;
+    case UART_PARITY_NONE:
+        break;
+    // case UART_PARITY_EVEN:
+    //     io_reg_atomic_set(&dev->UARTLCR_H, UART0_UARTLCR_H_EPS_Msk | UART0_UARTLCR_H_PEN_Msk);
+    //     break;
+    // case UART_PARITY_ODD:
+    //     io_reg_atomic_set(&dev->UARTLCR_H, UART0_UARTLCR_H_PEN_Msk);
+    //     break;
+    default:
+        return UART_NOMODE;
     }
 
     dev->UARTCR = UART_UARTCR_TXE_BITS | UART_UARTCR_UARTEN_BITS | UART_UARTCR_RXE_BITS;
@@ -78,7 +85,43 @@ int uart_mode(uart_t uart, uart_data_bits_t data_bits, uart_parity_t parity,
     return UART_OK;
 }
 
-int uart_init(uart_t uart, uint32_t baud, uart_rx_cb_t rx_cb, void *arg) {
+static void _reset_uart(uart_t uart)
+{
+    switch (uart) {
+    case 0:
+        /* We reset UART0 here, so we can be sure it is in a known state */
+        reset_component(RESET_UART0, RESET_UART0);
+        break;
+    case 1:
+        /* We reset UART1 here, so we can be sure it is in a known state */
+        reset_component(RESET_UART1, RESET_UART1);
+        break;
+    default:
+        break;
+    }
+}
+
+void uart_init_pins(uart_t uart)
+{
+    assert((unsigned)uart < UART_NUMOF);
+    UART0_Type *dev = uart_config[uart].dev;
+
+    /* Set the UART pins to the correct function */
+    *(uint32_t*)calculate_gpio_io_ctrl_register_addr(uart_config[uart].tx_pin) = FUNCTION_SELECT_UART;
+    *(uint32_t*)calculate_gpio_io_ctrl_register_addr(uart_config[uart].rx_pin) = FUNCTION_SELECT_UART;
+    /* Clear the ISO bits */
+    atomic_clear((uint32_t*)calculate_gpio_pad_register_addr(uart_config[uart].tx_pin), PADS_BANK0_ISO_BITS);
+    atomic_clear((uint32_t*)calculate_gpio_pad_register_addr(uart_config[uart].rx_pin), PADS_BANK0_ISO_BITS);
+
+    /* We reset UART0 here, so we can be sure it is in a known state */
+    _reset_uart(uart);
+
+    dev->UARTIBRD = IBRD;
+    dev->UARTFBRD = FBRD;
+}
+
+int uart_init(uart_t uart, uint32_t baud, uart_rx_cb_t rx_cb, void *arg)
+{
     (void)baud;
 
     if (uart >= UART_NUMOF) {
@@ -89,19 +132,7 @@ int uart_init(uart_t uart, uint32_t baud, uart_rx_cb_t rx_cb, void *arg) {
     ctx[uart].rx_cb = rx_cb;
     ctx[uart].arg = arg;
 
-
-    /* Set the UART pins to the correct function */
-    IO_BANK0->GPIO0_CTRL = FUNCTION_SELECT_UART;
-    IO_BANK0->GPIO1_CTRL = FUNCTION_SELECT_UART;
-    /* Clear the ISO bits */
-    atomic_clear(&PADS_BANK0->GPIO0, PADS_BANK0_ISO_BITS);
-    atomic_clear(&PADS_BANK0->GPIO1, PADS_BANK0_ISO_BITS);
-
-    /* We reset UART0 here, so we can be sure it is in a known state */
-    reset_component(RESET_UART0, RESET_UART0);
-
-    dev->UARTIBRD = IBRD;
-    dev->UARTFBRD = FBRD;
+    uart_init_pins(uart);
 
     if (uart_mode(uart, UART_DATA_BITS_8, UART_PARITY_NONE, UART_STOP_BITS_1) != UART_OK) {
         return UART_NOMODE;
@@ -120,20 +151,36 @@ int uart_init(uart_t uart, uint32_t baud, uart_rx_cb_t rx_cb, void *arg) {
     return UART_OK;
 }
 
-void uart_write(uart_t uart, const uint8_t *data, size_t len) {
-    (void)uart;
+void uart_write(uart_t uart, const uint8_t *data, size_t len)
+{
+    UART0_Type *dev = uart_config[uart].dev;
     for (size_t i = 0; i < len; i++) {
-        UART0->UARTDR = data[i];
+        dev->UARTDR = data[i];
         /* Wait until the TX FIFO is empty before sending the next byte */
-        while (!(UART0->UARTFR & UART_UARTFR_TXFE_BITS)) {
+        while (!(dev->UARTFR & UART_UARTFR_TXFE_BITS)) {
         }
     }
 }
 
-void uart_poweron(uart_t uart) {
-    (void)uart;
+void uart_poweron(uart_t uart)
+{
+    assert((unsigned)uart < UART_NUMOF);
+    /* Get into a save state where we know whats up */
+    _reset_uart(uart);
+    UART0_Type *dev = uart_config[uart].dev;
+    /* Restore config from registers */
+    dev->UARTIBRD = uartibrd;
+    dev->UARTFBRD = uartfbrd;
+    dev->UARTLCR_H = uartlcr_h;
+    dev->UARTCR = uartcr;
+    /* restore IRQs, if needed */
+    if (ctx[uart].rx_cb != NULL) {
+        _irq_enable(uart);
+    }
+    uart_init_pins(uart);
 }
-void uart_poweroff(uart_t uart) {
+void uart_poweroff(uart_t uart)
+{
     (void)uart;
 }
 
@@ -150,7 +197,7 @@ void isr_handler(uint8_t num)
         //     DEBUG_PUTS("[rpx0xx] uart RX error (parity, break, or framing error");
         // }
         // else {
-            ctx[num].rx_cb(ctx[num].arg, (uint8_t)data);
+        ctx[num].rx_cb(ctx[num].arg, (uint8_t)data);
         // }
     }
 }
