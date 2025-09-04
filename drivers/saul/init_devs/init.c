fde241ffb77a85db91e028938cf4ac9013296953
Baptiste Le Duc <baptiste.leduc38@gmail.com>
2025-09-04 16:02:37 +0200

    drivers/ads1x1x: add SAUL support

--- drivers/saul/init_devs/init.c
+++ drivers/saul/init_devs/init.c
@@ -67,6 +67,10 @@ void saul_init_devs(void)
         extern void auto_init_ads101x(void);
         auto_init_ads101x();
     }
+    if (IS_USED(MODULE_ADS1X1X)){
+        extern void auto_init_ads1x1x(void);
+        auto_init_ads1x1x();
+    }
     if (IS_USED(MODULE_ADXL345)) {
         extern void auto_init_adxl345(void);
         auto_init_adxl345();
