e343b1aa68139c6ec45acf37518195e6eb4a730e
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-05 12:12:59 +0200

    cpu/avr: move licenses to SPDX format

--- cpu/atmega_common/periph/eeprom.c
+++ cpu/atmega_common/periph/eeprom.c
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2018 Inria
- *               2023 Hugues Larrive
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2018 Inria
+ * SPDX-FileCopyrightText: 2023 Hugues Larrive
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 /**
--- cpu/avr8_common/include/states_internal.h
+++ cpu/avr8_common/include/states_internal.h
@@ -1,9 +1,6 @@
 /*
- * Copyright (C) 2023 Gerson Fernando Budke
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2023 Gerson Fernando Budke
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
