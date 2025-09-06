3843184182c164a757f02f9d76311681fb82d208
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-06 18:00:38 +0200

    cpu/efm32: move licenses to SPDX format

--- cpu/efm32/cpu.c
+++ cpu/efm32/cpu.c
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2015-2017 Freie Universität Berlin
- *               2022 SSV Software Systems GmbH
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2015-2017 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2022 SSV Software Systems GmbH
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 /**
