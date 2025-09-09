861a05ec2738035e2e01e0eaf4559884c0f00833
Elena Frank <elena.frank@protonmail.com>
2025-09-09 15:21:52 +0200

    gnrc/netif: don't re-append tx_sync snip if tx is done
    If sending is blocking or sending failed, the pkt is freed in the call
    to `_tx_done`. Thus, the previously split of `tx_sync` snip shouldn't be
    re-appended to the already freed pkt.
    Similarily, the `GNRC_NETIF_FLAGS_TX_FROM_PKTQUEUE` shouldn't be set in
    that case.

--- sys/net/gnrc/netif/gnrc_netif.c
+++ sys/net/gnrc/netif/gnrc_netif.c
@@ -1961,11 +1961,11 @@ static void _send(gnrc_netif_t *netif, gnrc_pktsnip_t *pkt, bool push_back)
         /* new API *and* send() was a success --> block netif and memorize
          * frame to free memory later */
         netif->tx_pkt = pkt;
-    }
 
-    gnrc_pkt_append(pkt, tx_sync);
-    if (IS_USED(MODULE_GNRC_NETIF_PKTQ) && push_back) {
-        netif->flags |= GNRC_NETIF_FLAGS_TX_FROM_PKTQUEUE;
+        gnrc_pkt_append(pkt, tx_sync);
+        if (IS_USED(MODULE_GNRC_NETIF_PKTQ) && push_back) {
+            netif->flags |= GNRC_NETIF_FLAGS_TX_FROM_PKTQUEUE;
+        }
     }
 #endif
 }
