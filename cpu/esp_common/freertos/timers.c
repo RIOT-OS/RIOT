ac888a553db1c91196a59693a1b1600b2fe3882e
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-11 21:18:55 +0200

    cpu/esp_common: move licenses to SPDX format

--- cpu/esp_common/freertos/timers.c
+++ cpu/esp_common/freertos/timers.c
@@ -1,11 +1,6 @@
 /*
- * Copyright (C) 2019 Gunar Schorcht
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- *
- * FreeRTOS to RIOT-OS adaption module for source code compatibility
+ * SPDX-FileCopyrightText: 2019 Gunar Schorcht
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #ifndef DOXYGEN
--- cpu/esp_common/include/freertos/portable.h
+++ cpu/esp_common/include/freertos/portable.h
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2022 Gunar Schorcht
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- *
+ * SPDX-FileCopyrightText: 2022 Gunar Schorcht
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+/*
  * FreeRTOS to RIOT-OS adaption module for source code compatibility
  */
 
