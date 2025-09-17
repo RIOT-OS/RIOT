4a3f6d58595311fa5b31d4770b985d75ec40886b
AnnsAnn <git@annsann.eu>
2025-09-17 17:54:09 +0200

    sys/include/net/sock: update guide references
    Co-authored-by: crasbe <crasbe@gmail.com>

--- sys/include/net/sock/tcp.h
+++ sys/include/net/sock/tcp.h
@@ -18,7 +18,7 @@
  *
  * How To Use
  * ----------
- * First you need to @ref including-modules "include" a module that implements
+ * First you need to [include][include-link] a module that implements
  * this API in your application's Makefile. For example the implementation for
  * @ref net_gnrc "GNRC" is called `gnrc_sock_tcp`.
  *
@@ -84,7 +84,7 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
  * Above you see a simple TCP echo server. Don't forget to also
- * @ref including-modules "include" the IPv6 module of your networking
+ * [include][include-link] the IPv6 module of your networking
  * implementation (e.g. `gnrc_ipv6_default` for @ref net_gnrc GNRC) and at least
  * one network device.
  *
@@ -233,7 +233,7 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
  * Above you see a simple TCP echo client. Again: Don't forget to also
- * @ref including-modules "include" the IPv6 module of your networking
+ * [include][include-link] the IPv6 module of your networking
  * implementation (e.g. `gnrc_ipv6_default` for @ref net_gnrc "GNRC") and at
  * least one network device. Ad0)ditionally, for the IPv6 address parsing you need
  * the @ref net_ipv6_addr "IPv6 address module".
@@ -285,6 +285,8 @@
  *     sock_tcp_disconnect(&sock);
  *     return res;
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * [include-link]: https://guide.riot-os.org/advanced_tutorials/creating_application/#including-modules
  *
  * @{
  *
--- sys/include/net/sock/udp.h
+++ sys/include/net/sock/udp.h
@@ -18,7 +18,7 @@
  *
  * How To Use
  * ----------
- * First you need to @ref including-modules "include" a module that implements
+ * First you need to [include][include-link] a module that implements
  * this API in your application's Makefile. For example the implementation for
  * @ref net_gnrc "GNRC" is called `gnrc_sock_udp`.
  *
@@ -61,7 +61,7 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
  * Above you see a simple UDP echo server. Don't forget to also
- * @ref including-modules "include" the IPv6 module of your networking
+ * [include][include-link] the IPv6 module of your networking
  * implementation (e.g. `gnrc_ipv6_default` for @ref net_gnrc GNRC) and at least
  * one network device.
  *
@@ -193,7 +193,7 @@
  * }
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
- * Again: Don't forget to also @ref including-modules "include" the IPv6 module
+ * Again: Don't forget to also [include][include-link] the IPv6 module
  * of your networking implementation (e.g. `gnrc_ipv6_default` for
  * @ref net_gnrc "GNRC") and at least one network device.
  *
@@ -254,6 +254,8 @@
  *
  * Finally, we wait a second before sending out the next "Hello!" with
  * `xtimer_sleep(1)`.
+ *
+ * [include-link]: https://guide.riot-os.org/advanced_tutorials/creating_application/#including-modules
  *
  * @{
  *
