2f242dc96de0721534b83b912f508317e178245c
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-18 21:20:09 +0200

    cpu/gd32v: move licenses to SPDX format

--- cpu/gd32v/periph/gpio.c
+++ cpu/gd32v/periph/gpio.c
@@ -1,12 +1,10 @@
 /*
- * Copyright (C) 2014-2015 Freie Universität Berlin
- *               2020 Koen Zandberg <koen@bergzand.net>
- *               2023 Gunar Schorcht <gunar@schorcht.net>
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * SPDX-FileCopyrightText: 2014-2015 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2020 Koen Zandberg <koen@bergzand.net>
+ * SPDX-FileCopyrightText: 2023 Gunar Schorcht <gunar@schorcht.net>
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
+
 /**
  * @ingroup         cpu_gd32v
  * @{
