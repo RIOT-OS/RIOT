102d1dcb7e9100bf19a81607ec586416e929b7a1
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-19 09:04:46 +0200

    cpu/kinetis: move licenses to SPDX format

--- cpu/kinetis/include/cpu_conf_kinetis_ea.h
+++ cpu/kinetis/include/cpu_conf_kinetis_ea.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2017 Eistec AB
- * Copyright (C) 2018 HERE Deutschland GmbH
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * SPDX-FileCopyrightText: 2017 Eistec AB
+ * SPDX-FileCopyrightText: 2018 HERE Deutschland GmbH
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
