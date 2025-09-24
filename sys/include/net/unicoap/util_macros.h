339d9ad4f289917ab3c01c8f3a722f9ed5751d6d
Carl Seifert <115627588+carl-tud@users.noreply.github.com>
2025-09-24 18:43:59 +0100

    explain black magic

--- sys/include/net/unicoap/util_macros.h
+++ sys/include/net/unicoap/util_macros.h
@@ -24,6 +24,42 @@
 
 #  define _CONCAT3(a, b, c)                      CONCAT3(a, b, c)
 
+
+/* This is best explained by the following two examples.
+ *  --> denotes a proprocessor evaluation step.
+ *
+ *     __unicoap_bit(11)
+ * --> __unicoap_create_bit_or_nothing(11, CONCAT(__unicoap_two_empty_args_, 11))
+ * --> __unicoap_create_bit_or_nothing(11, __unicoap_two_empty_args_11)
+ *                                         \...... __VA_ARGS__ ......./
+ * --> __take_second_arg(__unicoap_two_empty_args_11, (1 << (11)) |)
+ *                       \...... __VA_ARGS__ ......./
+ *  --> (1 << (11)) |
+ *
+ *
+ *     __unicoap_bit()
+ * --> __unicoap_create_bit_or_nothing(, CONCAT(__unicoap_two_empty_args_,))
+ * --> __unicoap_create_bit_or_nothing(, __unicoap_two_empty_args_)
+                                        \.. This is not a token ../
+                                            as above but a real
+                                           preprocessor constant.
+
+ * --> __unicoap_create_bit_or_nothing(,~,)
+                                       \....... Three arguments: empty, tilde, empty
+ * --> __take_second_arg(~,,)
+ *                       \...... __VA_ARGS__ followed by empty, former first ("offset"), argument
+ * -->
+ *     \.... nothing/empty
+ *
+ *
+ * The last trailing OR slash is always followed by a zero:
+ *
+ * UNICOAP_BITFIELD(...) __unicoap_create_bitfield(__VA_ARGS__,,,,,,,,,,,,,,,) 0
+ *
+ * The numerous commas ensure there are always at least 16 arguments, although they may be empty.
+ * See the explainer above how an expansion of __unicoap_bit works with an empty argument.
+ */
+
 #  define __unicoap_create_bit_or_nothing(offset, ...) \
       __take_second_arg(__VA_ARGS__, (1 << (offset)) |)
 
