b95b7791999259a5664bdaa16bb385cbbe6d4f3f
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2025-09-25 13:31:54 +0200

    fixup! doc/starlight: add changelog

--- doc/starlight/src/content.config.ts
+++ doc/starlight/src/content.config.ts
@@ -29,86 +29,78 @@ export function changelogLoader(): Loader {
       // convert the text to an array of lines
       const lines = content.split("\n");
 
-      // Convert all headings to be one level lower
-      // h1 => h2, h2 => h3, h3 => h4
-      // This is necessary, because the heading levels of this file are wrong (for markdown)
-      // We cannot have more han one h1 per document etc.
-      const fileContent = lines
-        .map((line, index) => {
-          const nextLine = lines[index + 1];
+      let currentReleaseHeading: string | null = null;
+      let currentReleaseHeadingIndex: number | null = null;
 
-          // Check if next line contains only "=" characters
-          if (/^=+$/.test(nextLine)) {
-            // Remove the next lines content
-            lines[index + 1] = "";
-            // If yes add a "##" to the current lines beginning
-            return `## ${line}`;
+      for (const [index, line] of lines.entries()) {
+        const nextLine = lines[index + 1];
+
+        // Convert all headings to be one level lower
+        // h1 => h2, h2 => h3, h3 => h4
+        // This is necessary, because the heading levels of this file are wrong (for markdown)
+        // We cannot have more han one h1 per document etc.
+        // Check if next line contains only "=" characters
+        if (/^=+$/.test(nextLine)) {
+          // If yes add a "##" to the current lines beginning
+          lines[index] = `## ${line}`;
+          // Remove the next lines content
+          lines[index + 1] = "";
+        }
+        // Check if next line contains only "-" characters
+        if (/^-+$/.test(nextLine)) {
+          // If yes add a "###" to the current lines beginning
+          lines[index] = `### ${line}`;
+          // Remove the next lines content
+          lines[index + 1] = "";
+        }
+
+        // Check if current line is a RIOT release heading or if we have reached the last line of the file
+        // In this case we have either found the first release heading
+        // Or we have reached the end of the current changelog and will now push it to the content collection
+        if (
+          /(RIOT-.*? - Release Notes)|(Release 2013\.08)/.test(line) ||
+          index == lines.length - 1
+        ) {
+          if (currentReleaseHeading && currentReleaseHeadingIndex) {
+            // Extract the release (YYYY.MM.*) from the release heading
+            const release =
+              currentReleaseHeading.match(/(\d{4}\.\d{2}(\.\d+)?)/)?.[1] || "";
+
+            // Create a slug based on the date
+            const slug = release.replaceAll(".", "-");
+
+            // Generate a date object from the slug (since it is a date)
+            const date = new Date(release);
+
+            // Extract the release code name from the heading if it exists
+            const codeName =
+              currentReleaseHeading.match(/"([^"]+)"/)?.[1] || null;
+
+            // Extract current release content
+            const currentReleaseContent = lines
+              .slice(currentReleaseHeadingIndex + 1, index)
+              .join("\n");
+
+            // Add entry to content collection
+            context.store.set({
+              id: slug,
+              data: {
+                title: `RIOT-${release}${
+                  codeName ? ` "${codeName}"` : ""
+                } - Release Notes`,
+                release: release,
+                codeName: codeName,
+                slug: slug,
+                date: date,
+                markdown: currentReleaseContent,
+              },
+              rendered: await context.renderMarkdown(currentReleaseContent),
+            });
           }
 
-          // Check if next line contains only "-" characters
-          if (/^-+$/.test(nextLine)) {
-            // Remove the next lines content
-            lines[index + 1] = "";
-            // If yes add a "###" to the current lines beginning
-            return `### ${line}`;
-          }
-
-          // Otherwise just keep the line as is
-          return line;
-        })
-        .join("\n");
-
-      // /RIOT-.*? - Release Notes/
-      const releaseHeadings = lines.filter((line) =>
-        /(RIOT-.*? - Release Notes)|(Release 2013\.08)/.test(line)
-      );
-
-      // Iterate over all the release headings and extract the data to create entries for the collection
-      for (const [index, heading] of releaseHeadings.entries()) {
-        // Extract the release (YYYY.MM.*) from the heading
-        const release = heading.match(/(\d{4}\.\d{2}(\.\d+)?)/)?.[1] || "";
-
-        // Create a slug based on the date
-        const slug = release.replaceAll(".", "-");
-
-        // Generate a date object from the slug (since it is a date)
-        const date = new Date(release);
-
-        // Extract the release code name from the heading if it exists
-        const codeName = heading.match(/"([^"]+)"/)?.[1] || null;
-
-        // Extract current release content
-        // Detect index of the heading of the current release in the release-notes.txt file
-        const currentHeading = heading;
-        const nextHeading = releaseHeadings[index + 1];
-
-        // Detect index of the heading of the next release in the release-notes.txt file
-        const startIndex = fileContent.indexOf(currentHeading);
-        const endIndex = fileContent.indexOf(nextHeading);
-
-        // Remove release heading from content (frontmatter already specifies the heading)
-        const currentReleaseContent = fileContent
-          .substring(startIndex, endIndex)
-          .trim()
-          .split("\n")
-          .slice(2)
-          .join("\n");
-
-        // Add entry to content collection
-        context.store.set({
-          id: heading,
-          data: {
-            title: `RIOT-${release}${
-              codeName ? ` "${codeName}"` : ""
-            } - Release Notes`,
-            release: release,
-            codeName: codeName,
-            slug: slug,
-            date: date,
-            markdown: currentReleaseContent,
-          },
-          rendered: await context.renderMarkdown(currentReleaseContent),
-        });
+          currentReleaseHeading = line;
+          currentReleaseHeadingIndex = index;
+        }
       }
     },
   };
