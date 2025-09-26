716f7e1b1f330048c0480818c5d40e4442f2d6ae
krzysztof-cabaj <kcabaj@gmail.com>
2025-09-26 20:06:29 +0200

    cpu/riscv_common: move licenses to SPDX format

--- cpu/riscv_common/include/irq_arch.h
+++ cpu/riscv_common/include/irq_arch.h
@@ -1,11 +1,8 @@
 /*
- * Copyright (C) 2017 Ken Rabold
- *               2020 Inria
- *               2020 Otto-von-Guericke-Universität Magdeburg
- *
- * This file is subject to the terms and conditions of the GNU Lesser General
- * Public License v2.1. See the file LICENSE in the top level directory for more
- * details.
+ * SPDX-FileCopyrightText: 2017 Ken Rabold
+ * SPDX-FileCopyrightText: 2020 Inria
+ * SPDX-FileCopyrightText: 2020 Otto-von-Guericke-Universität Magdeburg
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
