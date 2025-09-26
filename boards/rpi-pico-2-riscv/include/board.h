7963f241150b8d7bdfb4625ee19ade91a5a200ee
AnnsAnn <git@annsann.eu>
2025-09-26 15:17:36 +0200

    boards/rpi-pico-2-riscv: add riscv based rp2350 board

---
+++ boards/rpi-pico-2-riscv/include/board.h
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#pragma once
+
+/**
+ * @ingroup         boards_rpi_pico_2
+ * @{
+ *
+ * @file
+ * @brief           Board specific definitions for the Raspberry Pi Pico 2
+ *
+ * @author          Tom Hert <git@annsann.eu>
+ */
+
+#include "cpu.h"
+#include "periph_conf.h"
+
+#include "periph/gpio.h"
+
+/** GPIO Pin ID for the onboard LED */
+#define LED0_PIN_ID 25u
+#define LED0_ON         gpio_set(LED0_PIN_ID)
+#define LED0_OFF        gpio_clear(LED0_PIN_ID)
+#define LED0_TOGGLE     gpio_toggle(LED0_PIN_ID)
+#define LED0_NAME       "LED(Green)"
+
+#define OSC_DEBUG_PIN_ID 15u
+#define OSC_DEBUG_PIN_ID_2 14u
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Initialize the board, called from the cpu startup code */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
---
+++ boards/rpi-pico-2-riscv/include/gpio_params.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Tom Hert <git@annsann.eu>
+ * SPDX-FileCopyrightText: 2025 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#pragma once
+
+#include "board.h"
+#include "saul/periph.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
