eedc9427a0bd45792c3bdde8252800fd8de6ad19
Carl Seifert <115627588+carl-tud@users.noreply.github.com>
2025-10-19 19:47:56 +0100

    fixup: unittests: unicoap path matching

--- tests/unittests/tests-unicoap/tests-unicoap.c
+++ tests/unittests/tests-unicoap/tests-unicoap.c
@@ -15,14 +15,17 @@
  */
 
 #include "tests-unicoap.h"
+#include <stdio.h>
 
 Test* tests_unicoap_options(void);
 Test* tests_unicoap_pdu(void);
 Test* tests_unicoap_message(void);
+Test* tests_unicoap_matching(void);
 
 void tests_unicoap(void)
 {
     TESTS_RUN(tests_unicoap_pdu());
     TESTS_RUN(tests_unicoap_options());
     TESTS_RUN(tests_unicoap_message());
+    TESTS_RUN(tests_unicoap_matching());
 }
