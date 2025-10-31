2844950a39f3b6a71253b1ddf10b2a3f74050313
crasbe <crasbe@gmail.com>
2025-10-31 18:25:47 +0100

    cpu/stm32: style improvements of rtc_all.c

--- cpu/stm32/periph/rtc_all.c
+++ cpu/stm32/periph/rtc_all.c
@@ -26,109 +26,109 @@
 
 /* map some CPU specific register names */
 #if defined (CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
-#define EN_REG              (RCC->CSR)
-#define EN_BIT              (RCC_CSR_RTCEN)
-#define CLKSEL_MASK         (RCC_CSR_RTCSEL)
-#define CLKSEL_LSE          (RCC_CSR_RTCSEL_LSE)
-#define CLKSEL_LSI          (RCC_CSR_RTCSEL_LSI)
+#  define EN_REG            (RCC->CSR)
+#  define EN_BIT            (RCC_CSR_RTCEN)
+#  define CLKSEL_MASK       (RCC_CSR_RTCSEL)
+#  define CLKSEL_LSE        (RCC_CSR_RTCSEL_LSE)
+#  define CLKSEL_LSI        (RCC_CSR_RTCSEL_LSI)
 #else
-#define EN_REG              (RCC->BDCR)
-#define EN_BIT              (RCC_BDCR_RTCEN)
-#define CLKSEL_MASK         (RCC_BDCR_RTCSEL_0 | RCC_BDCR_RTCSEL_1)
-#define CLKSEL_LSE          (RCC_BDCR_RTCSEL_0)
-#define CLKSEL_LSI          (RCC_BDCR_RTCSEL_1)
+#  define EN_REG            (RCC->BDCR)
+#  define EN_BIT            (RCC_BDCR_RTCEN)
+#  define CLKSEL_MASK       (RCC_BDCR_RTCSEL_0 | RCC_BDCR_RTCSEL_1)
+#  define CLKSEL_LSE        (RCC_BDCR_RTCSEL_0)
+#  define CLKSEL_LSI        (RCC_BDCR_RTCSEL_1)
 #endif
 
 /* map some EXTI register names */
 #if defined(CPU_FAM_STM32L4) || defined(CPU_FAM_STM32WB) || \
     defined(CPU_FAM_STM32G4)
-#define EXTI_REG_RTSR       (EXTI->RTSR1)
-#define EXTI_REG_FTSR       (EXTI->FTSR1)
-#define EXTI_REG_PR         (EXTI->PR1)
-#define EXTI_REG_IMR        (EXTI->IMR1)
+#  define EXTI_REG_RTSR     (EXTI->RTSR1)
+#  define EXTI_REG_FTSR     (EXTI->FTSR1)
+#  define EXTI_REG_PR       (EXTI->PR1)
+#  define EXTI_REG_IMR      (EXTI->IMR1)
 #elif defined(CPU_FAM_STM32G0) || defined(CPU_FAM_STM32U5)
-#define EXTI_REG_RTSR       (EXTI->RTSR1)
-#define EXTI_REG_FTSR       (EXTI->FTSR1)
-#define EXTI_REG_PR         (EXTI->RPR1)
-#define EXTI_REG_IMR        (EXTI->IMR1)
+#  define EXTI_REG_RTSR     (EXTI->RTSR1)
+#  define EXTI_REG_FTSR     (EXTI->FTSR1)
+#  define EXTI_REG_PR       (EXTI->RPR1)
+#  define EXTI_REG_IMR      (EXTI->IMR1)
 #elif defined(CPU_FAM_STM32L5)
-#define EXTI_REG_IMR        (EXTI->IMR1)
+#  define EXTI_REG_IMR      (EXTI->IMR1)
 #else
-#define EXTI_REG_RTSR       (EXTI->RTSR)
-#define EXTI_REG_FTSR       (EXTI->FTSR)
-#define EXTI_REG_PR         (EXTI->PR)
-#define EXTI_REG_IMR        (EXTI->IMR)
+#  define EXTI_REG_RTSR     (EXTI->RTSR)
+#  define EXTI_REG_FTSR     (EXTI->FTSR)
+#  define EXTI_REG_PR       (EXTI->PR)
+#  define EXTI_REG_IMR      (EXTI->IMR)
 #endif
 
 /* map some RTC register names and bitfield */
 #if defined(CPU_FAM_STM32G4) || defined(CPU_FAM_STM32G0)
-#define RTC_REG_ISR         RTC->ICSR
-#define RTC_REG_SR          RTC->SR
-#define RTC_REG_SCR         RTC->SCR
-#define RTC_ISR_RSF         RTC_ICSR_RSF
-#define RTC_ISR_INIT        RTC_ICSR_INIT
-#define RTC_ISR_INITF       RTC_ICSR_INITF
-#define RTC_ISR_ALRAWF      RTC_ICSR_ALRAWF
-#define RTC_ISR_ALRAF       RTC_SR_ALRAF
+#  define RTC_REG_ISR       RTC->ICSR
+#  define RTC_REG_SR        RTC->SR
+#  define RTC_REG_SCR       RTC->SCR
+#  define RTC_ISR_RSF       RTC_ICSR_RSF
+#  define RTC_ISR_INIT      RTC_ICSR_INIT
+#  define RTC_ISR_INITF     RTC_ICSR_INITF
+#  define RTC_ISR_ALRAWF    RTC_ICSR_ALRAWF
+#  define RTC_ISR_ALRAF     RTC_SR_ALRAF
 #elif defined(CPU_FAM_STM32L5)
-#define RTC_REG_ISR         RTC->ICSR
-#define RTC_REG_SR          RTC->SR
-#define RTC_REG_SCR         RTC->SCR
-#define RTC_ISR_RSF         RTC_ICSR_RSF
-#define RTC_ISR_INIT        RTC_ICSR_INIT
-#define RTC_ISR_INITF       RTC_ICSR_INITF
+#  define RTC_REG_ISR       RTC->ICSR
+#  define RTC_REG_SR        RTC->SR
+#  define RTC_REG_SCR       RTC->SCR
+#  define RTC_ISR_RSF       RTC_ICSR_RSF
+#  define RTC_ISR_INIT      RTC_ICSR_INIT
+#  define RTC_ISR_INITF     RTC_ICSR_INITF
 #elif defined(CPU_FAM_STM32U5)
-#define RTC_REG_ISR         RTC->ICSR
-#define RTC_REG_SR          RTC->SR
-#define RTC_REG_SCR         RTC->SCR
-#define RTC_ISR_RSF         RTC_ICSR_RSF
-#define RTC_ISR_INIT        RTC_ICSR_INIT
-#define RTC_ISR_INITF       RTC_ICSR_INITF
-#define RTC_ISR_ALRAF       RTC_SR_ALRAF
+#  define RTC_REG_ISR       RTC->ICSR
+#  define RTC_REG_SR        RTC->SR
+#  define RTC_REG_SCR       RTC->SCR
+#  define RTC_ISR_RSF       RTC_ICSR_RSF
+#  define RTC_ISR_INIT      RTC_ICSR_INIT
+#  define RTC_ISR_INITF     RTC_ICSR_INITF
+#  define RTC_ISR_ALRAF     RTC_SR_ALRAF
 #else
-#define RTC_REG_ISR         RTC->ISR
+#  define RTC_REG_ISR       RTC->ISR
 #endif
 
 /* interrupt line name mapping */
 #if defined(CPU_FAM_STM32F0) || defined(CPU_FAM_STM32L0) || \
     defined(CPU_FAM_STM32L5) || defined(CPU_FAM_STM32U5)
-#define IRQN                (RTC_IRQn)
-#define ISR_NAME            isr_rtc
+#  define IRQN              (RTC_IRQn)
+#  define ISR_NAME          isr_rtc
 #elif defined(CPU_FAM_STM32G0)
-#define IRQN                (RTC_TAMP_IRQn)
-#define ISR_NAME            isr_rtc_tamp
+#  define IRQN              (RTC_TAMP_IRQn)
+#  define ISR_NAME          isr_rtc_tamp
 #else
-#define IRQN                (RTC_Alarm_IRQn)
-#define ISR_NAME            isr_rtc_alarm
+#  define IRQN              (RTC_Alarm_IRQn)
+#  define ISR_NAME          isr_rtc_alarm
 #endif
 
 /* EXTI bitfield mapping */
 #if defined(CPU_FAM_STM32L4)
-#define EXTI_IMR_BIT        (EXTI_IMR1_IM18)
-#define EXTI_FTSR_BIT       (EXTI_FTSR1_FT18)
-#define EXTI_RTSR_BIT       (EXTI_RTSR1_RT18)
-#define EXTI_PR_BIT         (EXTI_PR1_PIF18)
+#  define EXTI_IMR_BIT      (EXTI_IMR1_IM18)
+#  define EXTI_FTSR_BIT     (EXTI_FTSR1_FT18)
+#  define EXTI_RTSR_BIT     (EXTI_RTSR1_RT18)
+#  define EXTI_PR_BIT       (EXTI_PR1_PIF18)
 #elif defined(CPU_FAM_STM32L5)
-#define EXTI_IMR_BIT        (EXTI_IMR1_IM17)
+#  define EXTI_IMR_BIT      (EXTI_IMR1_IM17)
 #elif defined(CPU_FAM_STM32WB) || defined(CPU_FAM_STM32G4)
-#define EXTI_IMR_BIT        (EXTI_IMR1_IM17)
-#define EXTI_FTSR_BIT       (EXTI_FTSR1_FT17)
-#define EXTI_RTSR_BIT       (EXTI_RTSR1_RT17)
-#define EXTI_PR_BIT         (EXTI_PR1_PIF17)
+#  define EXTI_IMR_BIT      (EXTI_IMR1_IM17)
+#  define EXTI_FTSR_BIT     (EXTI_FTSR1_FT17)
+#  define EXTI_RTSR_BIT     (EXTI_RTSR1_RT17)
+#  define EXTI_PR_BIT       (EXTI_PR1_PIF17)
 #elif defined(CPU_FAM_STM32G0) || defined(CPU_FAM_STM32U5)
-#define EXTI_IMR_BIT        (EXTI_IMR1_IM11)
-#define EXTI_FTSR_BIT       (EXTI_FTSR1_FT11)
-#define EXTI_RTSR_BIT       (EXTI_RTSR1_RT11)
-#define EXTI_PR_BIT         (EXTI_RPR1_RPIF11)
+#  define EXTI_IMR_BIT      (EXTI_IMR1_IM11)
+#  define EXTI_FTSR_BIT     (EXTI_FTSR1_FT11)
+#  define EXTI_RTSR_BIT     (EXTI_RTSR1_RT11)
+#  define EXTI_PR_BIT       (EXTI_RPR1_RPIF11)
 #else
-#if defined(CPU_FAM_STM32L0)
-#define EXTI_IMR_BIT        (EXTI_IMR_IM17)
-#else
-#define EXTI_IMR_BIT        (EXTI_IMR_MR17)
-#endif
-#define EXTI_FTSR_BIT       (EXTI_FTSR_TR17)
-#define EXTI_RTSR_BIT       (EXTI_RTSR_TR17)
-#define EXTI_PR_BIT         (EXTI_PR_PR17)
+#  if defined(CPU_FAM_STM32L0)
+#    define EXTI_IMR_BIT    (EXTI_IMR_IM17)
+#  else
+#    define EXTI_IMR_BIT    (EXTI_IMR_MR17)
+#  endif
+#  define EXTI_FTSR_BIT     (EXTI_FTSR_TR17)
+#  define EXTI_RTSR_BIT     (EXTI_RTSR_TR17)
+#  define EXTI_PR_BIT       (EXTI_PR_PR17)
 #endif
 
 /* write protection values */
@@ -147,62 +147,62 @@
 #define ALRM_M_MASK         (RTC_ALRMAR_MNU | RTC_ALRMAR_MNT)
 #define ALRM_S_MASK         (RTC_ALRMAR_SU | RTC_ALRMAR_ST)
 #ifndef RTC_DR_YU_Pos
-#define RTC_DR_YU_Pos       (16U)
+#  define RTC_DR_YU_Pos     (16U)
 #endif
 #ifndef RTC_DR_MU_Pos
-#define RTC_DR_MU_Pos       (8U)
+#  define RTC_DR_MU_Pos     (8U)
 #endif
 #ifndef RTC_DR_DU_Pos
-#define RTC_DR_DU_Pos       (0U)
+#  define RTC_DR_DU_Pos     (0U)
 #endif
 #ifndef RTC_TR_HU_Pos
-#define RTC_TR_HU_Pos       (16U)
+#  define RTC_TR_HU_Pos     (16U)
 #endif
 #ifndef RTC_TR_MNU_Pos
-#define RTC_TR_MNU_Pos      (8U)
+#  define RTC_TR_MNU_Pos    (8U)
 #endif
 #ifndef RTC_TR_SU_Pos
-#define RTC_TR_SU_Pos       (0U)
+#  define RTC_TR_SU_Pos     (0U)
 #endif
 #ifndef RTC_ALRMAR_DU_Pos
-#define RTC_ALRMAR_DU_Pos   (24U)
+#  define RTC_ALRMAR_DU_Pos (24U)
 #endif
 #ifndef RTC_ALRMAR_HU_Pos
-#define RTC_ALRMAR_HU_Pos   (16U)
+#  define RTC_ALRMAR_HU_Pos (16U)
 #endif
 #ifndef RTC_ALRMAR_MNU_Pos
-#define RTC_ALRMAR_MNU_Pos  (8U)
+#  define RTC_ALRMAR_MNU_Pos    (8U)
 #endif
 #ifndef RTC_ALRMAR_SU_Pos
-#define RTC_ALRMAR_SU_Pos   (0U)
+#  define RTC_ALRMAR_SU_Pos (0U)
 #endif
 
 /* figure out sync and async prescaler */
 #if IS_ACTIVE(CONFIG_BOARD_HAS_LSE)
-#define PRE_SYNC            (255)
-#define PRE_ASYNC           (127)
+#  define PRE_SYNC          (255)
+#  define PRE_ASYNC         (127)
 #elif (CLOCK_LSI == 40000)
-#define PRE_SYNC            (319)
-#define PRE_ASYNC           (124)
+#  define PRE_SYNC          (319)
+#  define PRE_ASYNC         (124)
 #elif (CLOCK_LSI == 37000)
-#define PRE_SYNC            (295)
-#define PRE_ASYNC           (124)
+#  define PRE_SYNC          (295)
+#  define PRE_ASYNC         (124)
 #elif (CLOCK_LSI == 32000)
-#define PRE_SYNC            (249)
-#define PRE_ASYNC           (127)
+#  define PRE_SYNC          (249)
+#  define PRE_ASYNC         (127)
 #else
-#error "rtc: unable to determine RTC SYNC and ASYNC prescalers from LSI value"
+#  error "RTC: unable to determine RTC SYNC and ASYNC prescalers from LSI value"
 #endif
 
 /* struct tm counts years since 1900 but RTC has only two-digit year, hence the offset */
 #define YEAR_OFFSET         (RIOT_EPOCH - 1900)
 
 /* Use a magic number to determine the initial RTC source. This will be used
-   to know if a reset of the RTC is required at initialization. */
+ * to know if a reset of the RTC is required at initialization. */
 #if IS_ACTIVE(CONFIG_BOARD_HAS_LSE)
-#define MAGIC_CLCK_NUMBER       (0x1970)
+#  define MAGIC_CLCK_NUMBER     (0x1970)
 #else
-#define MAGIC_CLCK_NUMBER       (0x1971)
+#  define MAGIC_CLCK_NUMBER     (0x1971)
 #endif
 
 static struct {
@@ -256,8 +256,8 @@ void rtc_init(void)
     stmclk_dbp_unlock();
 #if defined(CPU_FAM_STM32L0) || defined(CPU_FAM_STM32L1)
     /* Compare the stored magic number with the current one. If it's different
-       it means the clock source has changed and thus a RTC reset is
-       required. */
+     * it means the clock source has changed and thus a RTC reset is
+     * required. */
     if (RTC->BKP0R != MAGIC_CLCK_NUMBER) {
         RCC->CSR |= RCC_CSR_RTCRST;
         RCC->CSR &= ~RCC_CSR_RTCRST;
