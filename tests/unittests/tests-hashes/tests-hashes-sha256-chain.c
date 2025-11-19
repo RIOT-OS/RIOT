f799f7465ae46d4ce0d368555683f5bf0232678a
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2025-11-19 17:47:59 +0100

    tests/unittests: use SPDX copyright tags
    Co-authored-by: crasbe <crasbe@gmail.com>
    Co-authored-by: Ann🐸 <git@annsann.eu>

--- tests/unittests/tests-hashes/tests-hashes-sha256-chain.c
+++ tests/unittests/tests-hashes/tests-hashes-sha256-chain.c
@@ -1,12 +1,9 @@
 /*
- * Copyright (C) 2016 Martin Landsmann <martin.landsmann@haw-hamburg.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2016 Martin Landsmann <martin.landsmann@haw-hamburg.de>
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
- /**
+/**
  * @ingroup     unittests
  * @{
  *
