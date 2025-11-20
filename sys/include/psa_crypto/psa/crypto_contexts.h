9472cab06f88e85ac381352751a7712ab4f27f32
publicvoid1337 <oliver.fritz-default@protonmail.com>
2025-11-20 21:48:28 +0100

    fixed a lot of mistakes from rebase

--- sys/include/psa_crypto/psa/crypto_contexts.h
+++ sys/include/psa_crypto/psa/crypto_contexts.h
@@ -5,46 +5,22 @@
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
  */
-
+ 
 #pragma once
-
-/**
- * @ingroup     sys_psa_crypto
- * @{
- *
- * @file        crypto_contexts.h
- * @brief       Context definitions for PSA Crypto
- *
- * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
- *
- */
-
+ 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+ 
 #include "kernel_defines.h"
 #include "psa/crypto_includes.h"
-
-#if IS_USED(MODULE_PSA_SECURE_ELEMENT)
-/**
- * @brief   Structure containing the secure element specific cipher contexts needed by the
- *          application.
- */
-typedef struct {
-    psa_encrypt_or_decrypt_t direction; /**< Direction of this cipher operation */
-    /** Structure containing a driver specific cipher context */
-    union driver_context {
-        unsigned dummy; /**< Make the union non-empty even with no supported algorithms. */
-    #if IS_USED(MODULE_PSA_SECURE_ELEMENT_ATECCX08A) || defined(DOXYGEN)
-        atca_aes_cbc_ctx_t atca_aes_cbc;    /**< ATCA AES CBC context*/
-    #endif
-    } drv_ctx;  /**< SE specific cipher operation context */
-} psa_se_cipher_context_t;
-#endif
-
+ 
+#include "aead/types.h"
+#include "hash/types.h"
+#include "cipher/types.h"
+#include "key_derivation/types.h"
+#include "mac/types.h"
+ 
 #ifdef __cplusplus
 }
 #endif
-
-/** @} */
--- sys/include/psa_crypto/psa/crypto_struct.h
+++ sys/include/psa_crypto/psa/crypto_struct.h
@@ -5,88 +5,23 @@
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
  */
-
+ 
 #pragma once
-
-/**
- * @ingroup     sys_psa_crypto
- * @{
- *
- * @file        crypto_struct.h
- * @brief       Structure definitions for PSA Crypto
- *
- * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
- *
- */
-
+ 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+ 
 #include "crypto_types.h"
 #include "crypto_sizes.h"
 #include "crypto_contexts.h"
-
-#if IS_USED(MODULE_PSA_AEAD) || defined(DOXYGEN)
-/**
- * @brief   Structure storing an AEAD operation context
- *
- * @note    Not implemented, yet
- */
-struct psa_aead_operation_s {
-    int dummy;  /**< Not implemented, yet */
-};
-
-/**
- * @brief   This macro returns a suitable initializer for an AEAD operation object of type
- *          @ref psa_aead_operation_t.
- */
-#define PSA_AEAD_OPERATION_INIT { 0 }
-
-/**
- * @brief   Return an initial value for an AEAD operation object.
- *
- * @return  psa_aead_operation_s
- */
-static inline struct psa_aead_operation_s psa_aead_operation_init(void)
-{
-    const struct psa_aead_operation_s v = PSA_AEAD_OPERATION_INIT;
-
-    return v;
-}
-#endif /* MODULE_PSA_AEAD */
-
-#if IS_USED(MODULE_PSA_KEY_DERIVATION) || defined(DOXYGEN)
-/**
- * @brief   This macro returns a suitable initializer for a key derivation operation object of
- *          type @ref psa_key_derivation_operation_t.
- */
-#define PSA_KEY_DERIVATION_OPERATION_INIT { 0 }
-
-/**
- * @brief   Structure storing a key derivation context
- *
- * @note    Not yet implemented
- */
-struct psa_key_derivation_operation_s {
-    int dummy;  /**< Not implemented yet */
-};
-
-/**
- * @brief   Return an initial value for a key derivation operation object.
- *
- * @return  psa_key_derivation_operation_s
- */
-static inline struct psa_key_derivation_operation_s psa_key_derivation_operation_init(void)
-{
-    const struct psa_key_derivation_operation_s v = PSA_KEY_DERIVATION_OPERATION_INIT;
-
-    return v;
-}
-#endif /* MODULE_PSA_KEY_DERIVATION */
-
+ 
+#include "aead/types.h"
+#include "cipher/types.h"
+#include "hash/types.h"
+#include "key_derivation/types.h"
+#include "mac/types.h"
+ 
 #ifdef __cplusplus
 }
 #endif
-
-/** @} */
