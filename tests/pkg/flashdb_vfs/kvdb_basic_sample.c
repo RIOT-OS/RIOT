a0c4319d72fb22f405edf8ea976b057bef97977d
Michel Rottleuthner <michel.rottleuthner@haw-hamburg.de>
2025-11-21 12:43:45 +0100

    fixup! tests/pkg/flashdb_vfs: improve kvdb_basic_sample test and output

--- tests/pkg/flashdb_vfs/kvdb_basic_sample.c
+++ tests/pkg/flashdb_vfs/kvdb_basic_sample.c
@@ -29,24 +29,27 @@ void kvdb_basic_sample(fdb_kvdb_t kvdb)
 
     { /* GET the KV value */
         /* get the "boot_count" KV value */
-        size_t res = fdb_kv_get_blob(kvdb, "boot_count", fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
+        size_t res = fdb_kv_get_blob(kvdb, "boot_count",
+                                     fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
         printf("get the 'boot_count' value is %d %s\n", boot_count,
-                                                          resstr(res == sizeof(boot_count)));
+               resstr(res == sizeof(boot_count)));
     }
 
     { /* CHANGE the KV value */
         /* increase the boot count */
         boot_count ++;
         /* change the "boot_count" KV's value */
-        int res = fdb_kv_set_blob(kvdb, "boot_count", fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
+        int res = fdb_kv_set_blob(kvdb, "boot_count",
+                                  fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
         printf("set the 'boot_count' value to %d %s\n", boot_count,
-                                                        resstr(res == FDB_NO_ERR));
+               resstr(res == FDB_NO_ERR));
     }
 
     { /* GET the KV value */
         int expected = boot_count;
         /* get the "boot_count" KV value */
-        int res = fdb_kv_get_blob(kvdb, "boot_count", fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
+        int res = fdb_kv_get_blob(kvdb, "boot_count",
+                                  fdb_blob_make(&blob, &boot_count, sizeof(boot_count)));
         printf("get the 'boot_count' value is %d %s\n", boot_count,
                resstr(res == sizeof(boot_count) && boot_count == expected));
     }
