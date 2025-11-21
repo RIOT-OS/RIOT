fd5b79f8948bf1e3a69b3de061e493a12a7e43d8
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2025-11-21 07:57:01 +0100

    core: use SPDX copyright tags

--- core/lib/include/ringbuffer.h
+++ core/lib/include/ringbuffer.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2013 Freie Universität Berlin
- * Copyright (C) 2013 INRIA
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2013 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2013 INRIA
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
