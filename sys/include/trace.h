2006023b85dc8b7d051d803d8acfe08f76c3510c
Marian Buschsieweke <maribu@users.noreply.github.com>
2025-11-24 17:35:18 +0100

    Update sys/include/trace.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- sys/include/trace.h
+++ sys/include/trace.h
@@ -1,5 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2020 Kaspar Schleiser <kaspar@schleiser.de>
+ * SPDX-FileCopyrightText: 2020 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2020 Inria
  * SPDX-License-Identifier: LGPL-2.1-only
  */
 
