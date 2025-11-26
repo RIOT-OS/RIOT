738a02ed1569b3fc34538f4080de1a47443253ff
Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
2025-11-26 13:58:32 +0100

    makefiles/utils/test: remove explicit dependency on bash

--- makefiles/utils/test-variables.mk
+++ makefiles/utils/test-variables.mk
@@ -12,8 +12,8 @@ CURRENT_TIME = $(call date_nanoseconds)
 
 $(call target-export-variables,test-exported-variables,$(EXPORTED_VARIABLES))
 test-exported-variables:
-	$(Q)bash -c 'test "$(MY_VARIABLE)" = "$${MY_VARIABLE}" || { echo ERROR: "$(MY_VARIABLE)" != "$${MY_VARIABLE}"; exit 1; }'
-	$(Q)bash -c 'test $(PARSE_TIME) -lt $${CURRENT_TIME} || { echo ERROR: $(PARSE_TIME) \>= $${CURRENT_TIME} >&2; exit 1; }'
+	$(Q)test "$(MY_VARIABLE)" = "$${MY_VARIABLE}" || { echo ERROR: "$(MY_VARIABLE)" != "$${MY_VARIABLE}"; exit 1; }
+	$(Q)test $(PARSE_TIME) -lt $${CURRENT_TIME} || { echo ERROR: $(PARSE_TIME) \>= $${CURRENT_TIME} >&2; exit 1; }
 
 
 MEMOIZED_CURRENT_TIME = $(call memoized,MEMOIZED_CURRENT_TIME,$(call date_nanoseconds))
