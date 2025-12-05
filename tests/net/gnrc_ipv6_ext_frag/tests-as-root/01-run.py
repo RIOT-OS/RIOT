8a5000d8904e77b20b9c0acf81acf2819d1bd262
Martine Lenders <m.lenders@fu-berlin.de>
2025-12-05 17:53:54 +0100

    tests: amend IPv6 reassembly test for 1st fragment repeats
    Co-authored-by: Nils Bernsdorf <nibe00018@stud.uni-saarland.de>
    (cherry picked from commit 0e4c7dcaf52b7d2d89818aab9cf65acf81ae6bc1)

--- tests/net/gnrc_ipv6_ext_frag/tests-as-root/01-run.py
+++ tests/net/gnrc_ipv6_ext_frag/tests-as-root/01-run.py
@@ -156,6 +156,23 @@ def test_reass_offset_too_large(child, iface, hw_dst, ll_dst, ll_src):
     pktbuf_empty(child)
 
 
+def test_reass_first_fragment_repeat(child, iface, hw_dst, ll_dst, ll_src):
+    # Originally proposed by Nils Bernsdorf (Uni Saarland), adapted by Martine Lenders
+    # send the first packet to initialize the reassembly buffer
+    sendp(Ether(dst=hw_dst) / IPv6(dst=ll_dst, src=ll_src) /
+          IPv6ExtHdrFragment(id=0xabcd, nh=0, m=1, offset=0) /
+          (b"A"*(24-8)),
+          iface=iface, verbose=0)
+    # send the a second larger packet (also with offset 0) to trigger the buffer
+    # overflow
+    sendp(Ether(dst=hw_dst) / IPv6(dst=ll_dst, src=ll_src) /
+          IPv6ExtHdrFragment(id=0xabcd, nh=0, m=1, offset=0) /
+          (b"A"*(128-8)),
+          iface=iface, verbose=0)
+    # the fragments should be discarded
+    pktbuf_empty(child)
+
+
 def test_ipv6_ext_frag_shell_test_0(child, s, iface, ll_dst):
     child.sendline("test {} 0".format(ll_dst))
     data, _ = s.recvfrom(RECV_BUFSIZE)
@@ -422,6 +439,7 @@ def testfunc(child):
     run(test_reass_successful_udp)
     run(test_reass_too_short_header)
     run(test_reass_offset_too_large)
+    run(test_reass_first_fragment_repeat)
     print("SUCCESS")
 
 
