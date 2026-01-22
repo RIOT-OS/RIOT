b3bdb3bf78cd27a197e0a09448a64f0ebed7c523
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-01-22 10:19:26 +0100

    fixup! drivers/netdev_ieee802154_submac: remove softack

--- makefiles/deprecated_modules.inc.mk
+++ makefiles/deprecated_modules.inc.mk
@@ -2,3 +2,4 @@
 # Keep this list ALPHABETICALLY SORTED!!!!111elven
 DEPRECATED_MODULES += gnrc_nettype_lorawan
 DEPRECATED_MODULES += sema_deprecated
+DEPRECATED_MODULES += netdev_ieee802154_submac_soft_ack
