52e807b8d5bd92b67f962c42e41450ef3063de19
Bas Stottelaar <basstottelaar@gmail.com>
2026-02-08 23:33:20 +0100

    efm32/i2c: add pull-up for SDA and SCL pins
    Using I2C on a bus without any devices (and/or pull-ups) connected
    can stall the driver, because there will be no interrupt that wakes
    the CPU up from `cortexm_sleep_until_event`. By driving the pins high
    using a very weak internal pull-up, this should not happen.
    
    This commit makes this configurable (like the ESP32), because this
    will consume a bit more energy. For well-defined (custom) boards, this
    should normally not be an issue. However, it could be useful for
    development boards or added resiliency.

--- cpu/efm32/include/periph_cpu.h
+++ cpu/efm32/include/periph_cpu.h
@@ -425,15 +425,16 @@ typedef enum {
  * @brief   I2C device configuration.
  */
 typedef struct {
-    I2C_TypeDef *dev;       /**< USART device used */
-    gpio_t sda_pin;         /**< pin used for SDA */
-    gpio_t scl_pin;         /**< pin used for SCL */
+    I2C_TypeDef *dev;           /**< I2C device used */
+    gpio_t sda_pin;             /**< pin used for SDA */
+    gpio_t scl_pin;             /**< pin used for SCL */
 #if defined(_SILICON_LABS_32B_SERIES_0) || defined(_SILICON_LABS_32B_SERIES_1)
-    uint32_t loc;           /**< location of I2C pins */
+    uint32_t loc;               /**< location of I2C pins */
 #endif
-    CMU_Clock_TypeDef cmu;  /**< the device CMU channel */
-    IRQn_Type irq;          /**< the devices base IRQ channel */
-    uint32_t speed;         /**< the bus speed */
+    uint32_t speed;             /**< the bus speed */
+    CMU_Clock_TypeDef cmu;      /**< the device CMU channel */
+    IRQn_Type irq;              /**< the devices base IRQ channel */
+    bool use_internal_pull_ups; /**< enable internal pull-ups on SDA and SCL pins */
 } i2c_conf_t;
 
 /**
