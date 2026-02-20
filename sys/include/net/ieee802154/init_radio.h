3e3867455d49da1f46edcbb0811f698db9034c71
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-02-20 12:09:36 +0100

    fixup! sys/net/ieee802154: add init_radio function

--- sys/include/net/ieee802154/init_radio.h
+++ sys/include/net/ieee802154/init_radio.h
@@ -26,29 +26,29 @@ extern "C" {
 #endif
 
 /* include available radios */
-#ifdef MODULE_CC2538_RF
+#if defined(MODULE_CC2538_RF)
 #  include "cc2538_rf.h"
 #  define IEEE802154_RADIO_COUNT 1
 #endif
 
-#ifdef MODULE_ESP_IEEE802154
+#if defined(MODULE_ESP_IEEE802154)
 #  include "esp_ieee802154_hal.h"
 #  define IEEE802154_RADIO_COUNT 1
 #endif
 
-#ifdef MODULE_NRF802154
+#if defined(MODULE_NRF802154)
 #  include "nrf802154.h"
 #  define IEEE802154_RADIO_COUNT 1
 #endif
 
-#ifdef MODULE_SOCKET_ZEP
+#if defined(MODULE_SOCKET_ZEP)
 #  include "socket_zep.h"
 #  include "socket_zep_params.h"
 static socket_zep_t _socket_zeps[SOCKET_ZEP_MAX];
 #  define IEEE802154_RADIO_COUNT SOCKET_ZEP_MAX
 #endif
 
-#ifdef MODULE_KW2XRF
+#if defined(MODULE_KW2XRF)
 #  include "kw2xrf.h"
 #  include "kw2xrf_params.h"
 #  define KW2XRF_NUM   ARRAY_SIZE(kw2xrf_params)
@@ -57,7 +57,7 @@ static bhp_event_t kw2xrf_bhp[KW2XRF_NUM];
 #  define IEEE802154_RADIO_COUNT KW2XRF_NUM
 #endif
 
-#ifdef MODULE_MRF24J40
+#if defined(MODULE_MRF24J40)
 #  include "mrf24j40.h"
 #  include "mrf24j40_params.h"
 #  define MRF24J40_NUM    ARRAY_SIZE(mrf24j40_params)
@@ -85,40 +85,40 @@ static inline int ieee802154_radio_init(ieee802154_dev_t *dev, int index, event_
     if (!evq) {
         evq = EVENT_PRIO_HIGHEST;
     }
-#ifdef IEEE802154_RADIO_COUNT
+#if defined(IEEE802154_RADIO_COUNT)
     assert(index < IEEE802154_RADIO_COUNT);
-#  ifdef MODULE_CC2538_RF
+#  if defined(MODULE_CC2538_RF)
     cc2538_rf_hal_setup(dev);
     cc2538_init();
     res++;
 #  endif
 
-#  ifdef MODULE_ESP_IEEE802154
+#  if defined(MODULE_ESP_IEEE802154)
     esp_ieee802154_setup(dev);
     esp_ieee802154_init();
     res++;
 #  endif
 
-#  ifdef MODULE_NRF802154
+#  if defined(MODULE_NRF802154)
     nrf802154_hal_setup(dev);
     nrf802154_init();
     res++;
 #  endif
 
-#  ifdef MODULE_KW2XRF
+#  if defined(MODULE_KW2XRF)
     const kw2xrf_params_t *p = &kw2xrf_params[index];
     bhp_event_init(&kw2xrf_bhp[index], evq, &kw2xrf_radio_hal_irq_handler, dev);
     kw2xrf_init(&kw2xrf_dev[index], p, dev, bhp_event_isr_cb, &kw2xrf_bhp[index]);
     res++;
 #  endif
 
-#  ifdef MODULE_SOCKET_ZEP
+#  if defined(MODULE_SOCKET_ZEP)
     socket_zep_hal_setup(&_socket_zeps[index], dev);
     socket_zep_setup(&_socket_zeps[index], &socket_zep_params[index]);
     res++;
 #  endif
 
-#  ifdef MODULE_MRF24J40
+#  if defined(MODULE_MRF24J40)
     const mrf24j40_params_t *p = &mrf24j40_params[index];
     bhp_event_init(&mrf24j40_bhp[index], evq, &mrf24j40_radio_irq_handler, dev);
     mrf24j40_init(&mrf24j40_dev[index], p, dev, bhp_event_isr_cb, &mrf24j40_bhp[index]);
