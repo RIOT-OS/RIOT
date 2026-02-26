a0b28ddb650c2272ec4eb6fd8abd8fa046021ee0
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2026-02-26 17:07:16 +0100

    makefiles/tools/openocd-adapters: use non-deprecated 'adapter serial'

--- makefiles/tools/openocd-adapters/jlink.inc.mk
+++ makefiles/tools/openocd-adapters/jlink.inc.mk
@@ -3,8 +3,3 @@ OPENOCD_ADAPTER_INIT ?= -c 'source [find interface/jlink.cfg]'
 
 # default to SWD
 OPENOCD_TRANSPORT ?= swd
-
-# Add serial matching command, only if DEBUG_ADAPTER_ID was specified
-ifneq (,$(DEBUG_ADAPTER_ID))
-  OPENOCD_ADAPTER_INIT += -c 'jlink serial $(DEBUG_ADAPTER_ID)'
-endif
