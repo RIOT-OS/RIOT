3068530f45c4e33b42acc2ca6b1f9b33cff2ac0f
Fabian Hüßler <fabian.huessler@ml-pa.com>
2026-03-18 13:59:11 +0100

    sys/net/link_layer/ieee802154: check return of ieee802154_get_dst

--- sys/net/link_layer/ieee802154/ieee802154.c
+++ sys/net/link_layer/ieee802154/ieee802154.c
@@ -288,10 +288,13 @@ int ieee802154_dst_filter(const uint8_t *mhr, uint16_t pan,
                           network_uint16_t short_addr, const eui64_t *ext_addr)
 {
     uint8_t dst_addr[IEEE802154_LONG_ADDRESS_LEN];
-    le_uint16_t dst_pan;
+    le_uint16_t dst_pan = { 0 };
     uint8_t pan_bcast[] = IEEE802154_PANID_BCAST;
 
     int addr_len = ieee802154_get_dst(mhr, dst_addr, &dst_pan);
+    if (addr_len <= 0) {
+        return 1; /* No destination address */
+    }
 
     /* filter PAN ID */
     if ((memcmp(pan_bcast, dst_pan.u8, 2) != 0) &&
