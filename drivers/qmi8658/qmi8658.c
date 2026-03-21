8c58d7d2dad2977fed1dc1e530f4047face3a50a
Yahia Abdella <yahia.abdella@tuhh.de>
2026-03-21 12:26:38 +0100

    drivers/qmi8658: make macro naming more unique

--- drivers/qmi8658/qmi8658.c
+++ drivers/qmi8658/qmi8658.c
@@ -27,8 +27,8 @@
 #define ENABLE_DEBUG 0
 #include "debug.h"
 
-#define BUS         (dev->params.i2c)
-#define ADDR        (dev->params.addr)
+#define QMI8658_BUS         (dev->params.i2c)
+#define QMI8658_ADDR        (dev->params.addr)
 
 /*
  * order in array [0, 1, 2, 3] is
@@ -70,33 +70,33 @@ int qmi8658_init(qmi8658_t *dev, const qmi8658_params_t *params)
 
     DEBUG("[LOG] qmi8658_init: Using I2C device %i\n", params->i2c);
 
-    i2c_acquire(BUS);
+    i2c_acquire(QMI8658_BUS);
 
     /* Soft reset */
-    res = i2c_write_reg(BUS, ADDR, QMI8658_REG_RESET, QMI8658_RESET_VALUE, 0);
+    res = i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_RESET, QMI8658_RESET_VALUE, 0);
     /* Reset process needs 15 ms */
     ztimer_sleep(ZTIMER_MSEC, 15);
     /* Reset success is represented by reg 0x4D */
-    res += i2c_read_reg(BUS, ADDR, 0x4D, &tmp, 0);
+    res += i2c_read_reg(QMI8658_BUS, QMI8658_ADDR, 0x4D, &tmp, 0);
     /* Check if reset was successful */
     if (res < 0 || tmp != 0x80) {
-        i2c_release(BUS);
+        i2c_release(QMI8658_BUS);
         DEBUG("[ERROR] qmi8658_init: Failed to reset device\n");
         return -EIO;
     }
 
     /* Check WHO_AM_I value */
-    res = i2c_read_reg(BUS, ADDR, QMI8658_REG_WHO_AM_I, &tmp, 0);
+    res = i2c_read_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_WHO_AM_I, &tmp, 0);
     if (res < 0 || tmp != QMI8658_WHO_AM_I_VALUE) {
-        i2c_release(BUS);
+        i2c_release(QMI8658_BUS);
         DEBUG("[ERROR] qmi8658_init: WHO_AM_I\n");
         return -EIO;
     }
 
     /* Enable address auto increment */
-    res = i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_ADDR_AI_MASK, 0);
+    res = i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_ADDR_AI_MASK, 0);
 
-    i2c_release(BUS);
+    i2c_release(QMI8658_BUS);
 
     if (res < 0) {
         DEBUG("[ERROR] qmi8658_init: Config\n");
@@ -126,11 +126,11 @@ int qmi8658_set_mode(const qmi8658_t *dev, qmi8658_mode_t mode)
         return -EIO;
     }
 
-    i2c_acquire(BUS);
+    i2c_acquire(QMI8658_BUS);
 
     switch (mode) {
     case QMI8658_POWER_DOWN:
-        res = i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_SENSOR_DISABLE_MASK, 0);
+        res = i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_SENSOR_DISABLE_MASK, 0);
         break;
 
     case QMI8658_NORMAL_ACC:
@@ -175,13 +175,13 @@ int qmi8658_set_mode(const qmi8658_t *dev, qmi8658_mode_t mode)
 
     if (mode != QMI8658_POWER_DOWN) {
         /* Clear sensor disable */
-        res += i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_ADDR_AI_MASK, 0);
+        res += i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL1, QMI8658_CTRL1_ADDR_AI_MASK, 0);
     }
     if (reg_ctrl2_value != 0) {
-        res += i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL2, reg_ctrl2_value, 0);
+        res += i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL2, reg_ctrl2_value, 0);
     }
     if (reg_ctrl3_value != 0) {
-        res += i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL3, reg_ctrl3_value, 0);
+        res += i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL3, reg_ctrl3_value, 0);
     }
 
     /* Filters need 3/ODR seconds to settle (Datasheet section 7.3) */
@@ -194,7 +194,7 @@ int qmi8658_set_mode(const qmi8658_t *dev, qmi8658_mode_t mode)
         ztimer_sleep(ZTIMER_MSEC, 1000);
     }
 
-    i2c_release(BUS);
+    i2c_release(QMI8658_BUS);
 
     if (res < 0) {
         DEBUG("[ERROR] qmi8658_set_mode\n");
@@ -251,12 +251,12 @@ int qmi8658_read_temp(const qmi8658_t *dev, int16_t *data)
     int res;
     uint8_t tmp[2];
 
-    i2c_acquire(BUS);
+    i2c_acquire(QMI8658_BUS);
 
     /* Read the 2 temperature bytes */
-    res = i2c_read_regs(BUS, ADDR, QMI8658_REG_TEMP_L, tmp, 2, 0);
+    res = i2c_read_regs(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_TEMP_L, tmp, 2, 0);
 
-    i2c_release(BUS);
+    i2c_release(QMI8658_BUS);
 
     if (res < 0) {
         DEBUG("[ERROR] qmi8658_read_temp\n");
@@ -292,16 +292,16 @@ static int _qmi8658_read_sensor(const qmi8658_t *dev, qmi8658_3d_data_t *data,
         break;
     }
 
-    i2c_acquire(BUS);
+    i2c_acquire(QMI8658_BUS);
 
     /* Check if data is available */
-    i2c_read_reg(BUS, ADDR, QMI8658_REG_STATUS0, &tmp, 0);
+    i2c_read_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_STATUS0, &tmp, 0);
     DEBUG("[LOG] qmi8658_read_sensor: status: %x\n", tmp);
 
     /* 6 byte burst read, register address is incremented automatically */
-    res = i2c_read_regs(BUS, ADDR, data_reg, tmp_arr, 6, 0);
+    res = i2c_read_regs(QMI8658_BUS, QMI8658_ADDR, data_reg, tmp_arr, 6, 0);
 
-    i2c_release(BUS);
+    i2c_release(QMI8658_BUS);
 
     if (res < 0) {
         DEBUG("[ERROR] qmi8658_read_sensor\n");
@@ -322,11 +322,11 @@ static int _qmi8658_set_sensors(const qmi8658_t *dev, qmi8658_enable_flag_t sens
 
     DEBUG("[LOG] Set sensor enable flags %x\n", sensor_enable_flags);
 
-    i2c_acquire(BUS);
+    i2c_acquire(QMI8658_BUS);
 
-    res = i2c_write_reg(BUS, ADDR, QMI8658_REG_CTRL7, sensor_enable_flags & 0x03, 0);
+    res = i2c_write_reg(QMI8658_BUS, QMI8658_ADDR, QMI8658_REG_CTRL7, sensor_enable_flags & 0x03, 0);
 
-    i2c_release(BUS);
+    i2c_release(QMI8658_BUS);
 
     if (res < 0) {
         DEBUG("[ERROR] qmi8658_enable_sensors\n");
