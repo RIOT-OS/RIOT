5ca333225216607b82e59937af3c3ac7bfada6e2
Adarsh Nair Mullachery <itsadarshnair@gamil.com>
2026-04-06 20:29:14 +0200

    fix remaining review issues

--- boards/nucleo-u385rg-q/include/periph_conf.h
+++ boards/nucleo-u385rg-q/include/periph_conf.h
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: 2026 Technische Universität Hamburg
-# SPDX-License-Identifier: LGPL-2.1-only
+/*
+ * SPDX-FileCopyrightText: 2026 Technische Universität Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 
 #pragma once
 
