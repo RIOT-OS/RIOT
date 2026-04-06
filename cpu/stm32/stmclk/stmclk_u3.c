f68d2cf145a5cb6331da40ede1daf50bd13649e0
Adarsh Nair Mullachery <itsadarshnair@gamil.com>
2026-04-06 20:29:14 +0200

    fix remaining review issues

--- cpu/stm32/stmclk/stmclk_u3.c
+++ cpu/stm32/stmclk/stmclk_u3.c
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: 2026 Technische Universität Hamburg
-# SPDX-License-Identifier: LGPL-2.1-only
+/*
+ * SPDX-FileCopyrightText: 2026 Technische Universität Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 
 /**
  * @ingroup     cpu_stm32
