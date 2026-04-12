956528189f456e9866c8d2384311fb5faa052c90
Adarsh Nair Mullachery <itsadarshnair@gamil.com>
2026-04-12 17:24:30 +0200

    Re-addressed and fixed the review comments

--- cpu/stm32/stmclk/stmclk_u3.c
+++ cpu/stm32/stmclk/stmclk_u3.c
@@ -10,7 +10,7 @@
  * @file
  * @brief       STM32U3 clock initialization
  *
- * @author      Adarsh Nair Mullachery 
+ * @author      Adarsh Nair Mullachery
  */
 
 #include "cpu.h"
@@ -18,7 +18,7 @@
 #include "periph_conf.h"
 
 #if !defined(CPU_FAM_STM32U3)
-#error "stmclk_u3.c compiled for non-STM32U3 CPU"
+#  error "stmclk_u3.c compiled for non-STM32U3 CPU"
 #endif
 
 #define RCC_CFGR1_SW_MSI            (0x00000000U)
@@ -29,103 +29,97 @@
 #define RCC_CFGR1_SWS_HSI           (RCC_CFGR1_SWS_0)
 #define RCC_CFGR1_SWS_HSE           (RCC_CFGR1_SWS_1)
 
-
-
-
 /* Define MSI range bitfields for STM32U3 */
 /* We assume MSISDIV (bits 29:30) and MSIRANGE (bits 16:18) */
-
 #if CONFIG_CLOCK_MSI == MHZ(48)
-#define CLOCK_MSIRANGE              (0) /* MSISRANGE 0, MSISDIV 0 */
+#  define CLOCK_MSIRANGE              (0) /* MSISRANGE 0, MSISDIV 0 */
 #elif CONFIG_CLOCK_MSI == MHZ(24)
-#define CLOCK_MSIRANGE              (RCC_ICSCR1_MSISDIV_0) /* Range 0, Div 2 */
+#  define CLOCK_MSIRANGE              (RCC_ICSCR1_MSISDIV_0) /* Range 0, Div 2 */
 #elif CONFIG_CLOCK_MSI == MHZ(16)
-/* Note: You need to check if your header defines RCC_ICSCR1_MSISRANGE_0 */
-#define CLOCK_MSIRANGE              (0x1UL << 16) 
+#  define CLOCK_MSIRANGE              (0x1UL << 16)
 #elif CONFIG_CLOCK_MSI == MHZ(12)
-#define CLOCK_MSIRANGE              (RCC_ICSCR1_MSISDIV_1) /* Range 0, Div 4 */
+#  define CLOCK_MSIRANGE              (RCC_ICSCR1_MSISDIV_1) /* Range 0, Div 4 */
 #elif CONFIG_CLOCK_MSI == MHZ(4)
-#define CLOCK_MSIRANGE              (0x2UL << 16) /* Range 2, Div 1 */
+#  define CLOCK_MSIRANGE              (0x2UL << 16) /* Range 2, Div 1 */
 #else
-#error "Invalid MSI clock for STM32U3 - check manual for supported LDO frequencies"
+#  error "Invalid MSI clock for STM32U3 - check manual for supported LDO frequencies"
 #endif
 
 /* Configure the AHB and APB buses prescalers */
 #define CLOCK_AHB_DIV               (0)
 
 #if CONFIG_CLOCK_APB1_DIV == 1
-#define CLOCK_APB1_DIV              (0)
+#  define CLOCK_APB1_DIV              (0)
 #elif CONFIG_CLOCK_APB1_DIV == 2
-#define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2)
+#  define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2)
 #elif CONFIG_CLOCK_APB1_DIV == 4
-#define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_0)
+#  define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_0)
 #elif CONFIG_CLOCK_APB1_DIV == 8
-#define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_1)
+#  define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_1)
 #elif CONFIG_CLOCK_APB1_DIV == 16
-#define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_1 | RCC_CFGR2_PPRE1_0)
+#  define CLOCK_APB1_DIV              (RCC_CFGR2_PPRE1_2 | RCC_CFGR2_PPRE1_1 | RCC_CFGR2_PPRE1_0)
 #endif
 
 #if CONFIG_CLOCK_APB2_DIV == 1
-#define CLOCK_APB2_DIV              (0)
+#  define CLOCK_APB2_DIV              (0)
 #elif CONFIG_CLOCK_APB2_DIV == 2
-#define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2)
+#  define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2)
 #elif CONFIG_CLOCK_APB2_DIV == 4
-#define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_0)
+#  define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_0)
 #elif CONFIG_CLOCK_APB2_DIV == 8
-#define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_1)
+#  define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_1)
 #elif CONFIG_CLOCK_APB2_DIV == 16
-#define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_1 | RCC_CFGR2_PPRE2_0)
+#  define CLOCK_APB2_DIV              (RCC_CFGR2_PPRE2_2 | RCC_CFGR2_PPRE2_1 | RCC_CFGR2_PPRE2_0)
 #endif
 
 /* Only periph_hwrng and periph_usbdev require HSI RC with 48MHz for the moment */
 #if IS_USED(MODULE_PERIPH_HWRNG) || IS_USED(MODULE_PERIPH_USBDEV_CLK)
-#define CLOCK_ENABLE_HSI48          1
+#  define CLOCK_ENABLE_HSI48          1
 #else
-#define CLOCK_ENABLE_HSI48          0
+#  define CLOCK_ENABLE_HSI48          0
 #endif
 
 /* Check if PLL is required
-  - When PLLQ is used as 48MHz clock source
-*/
-
+ * - When PLLQ is used as 48MHz clock source
+ */
 
 /* Check if HSE is required:
-  - When used as system clock
-  - When used as PLL input clock
-*/
+ * - When used as system clock
+ * - When used as PLL input clock
+ */
 #if IS_ACTIVE(CONFIG_USE_CLOCK_HSE) || \
     (IS_ACTIVE(CLOCK_ENABLE_PLL) && IS_ACTIVE(CONFIG_CLOCK_PLL_SRC_HSE))
-#define CLOCK_ENABLE_HSE            1
+#  define CLOCK_ENABLE_HSE            1
 #else
-#define CLOCK_ENABLE_HSE            0
+#  define CLOCK_ENABLE_HSE            0
 #endif
 
 /* HSE cannot be enabled if not provided by the board */
 #if IS_ACTIVE(CLOCK_ENABLE_HSE) && !IS_ACTIVE(CONFIG_BOARD_HAS_HSE)
-#error "HSE is required by the clock configuration but is not provided by the board."
+#  error "HSE is required by the clock configuration but is not provided by the board."
 #endif
 
 /* Check if HSI RC with 16 MHz is required:
-  - When used as system clock
-  - When used as PLL input clock
-*/
+ * - When used as system clock
+ * - When used as PLL input clock
+ */
 #if IS_ACTIVE(CONFIG_USE_CLOCK_HSI) || \
     (IS_ACTIVE(CLOCK_ENABLE_PLL) && IS_ACTIVE(CONFIG_CLOCK_PLL_SRC_HSI))
-#define CLOCK_ENABLE_HSI            1
+#  define CLOCK_ENABLE_HSI            1
 #else
-#define CLOCK_ENABLE_HSI            0
+#  define CLOCK_ENABLE_HSI            0
 #endif
 
 /* Check if MSI is required
-  - When used as system clock
-  - When used as PLL input clock
-*/
+ * - When used as system clock
+ * - When used as PLL input clock
+ */
 #if IS_ACTIVE(CONFIG_USE_CLOCK_MSI) || \
     (IS_ACTIVE(CLOCK_ENABLE_PLL) && IS_ACTIVE(CONFIG_CLOCK_PLL_SRC_MSI)) || \
     (IS_ACTIVE(CLOCK_ENABLE_48MHZ) && IS_ACTIVE(CLOCK48MHZ_USE_MSI))
-#define CLOCK_ENABLE_MSI            1
+#  define CLOCK_ENABLE_MSI            1
 #else
-#define CLOCK_ENABLE_MSI            0
+#  define CLOCK_ENABLE_MSI            0
 #endif
 
 /* Deduct the needed flash wait states from the core clock frequency */
@@ -180,3 +174,5 @@ void stmclk_init_sysclk(void)
 
     irq_restore(is);
 }
+
+/** @} */
