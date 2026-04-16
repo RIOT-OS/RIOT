82dcd5b449aba3133a8d7fa9c2929f03baeeb589
Fabian Hüßler <fabian.huessler@ml-pa.com>
2026-04-16 10:28:20 +0200

    fixup! fixup! boards: expose default EEPROM MTD

--- drivers/include/mtd_default.h
+++ drivers/include/mtd_default.h
@@ -27,15 +27,15 @@ extern "C" {
 #endif
 
 #if defined(MODULE_MTD_SDCARD_DEFAULT)
-#include "mtd_sdcard.h"
+#  include "mtd_sdcard.h"
 #endif
 
 #if defined(MODULE_MTD_EMULATED)
-#include "mtd_emulated.h"
+#  include "mtd_emulated.h"
 #endif
 
 #if defined(MODULE_MTD_EEPROM)
-#include "mtd_eeprom.h"
+#  include "mtd_eeprom.h"
 #endif
 
 #if defined(MODULE_MTD_SDCARD_DEFAULT)
