28c6a6bb01f4c9bec2a559f1fb11337cb797847f
Carl Seifert <115627588+carl-tud@users.noreply.github.com>
2026-05-10 16:38:22 +0100

    fix CON response ACK (new state notif sys)

--- examples/networking/coap/unicoap_client/main.c
+++ examples/networking/coap/unicoap_client/main.c
@@ -52,14 +52,16 @@ static int _print_usage(char** argv) {
     printf("usage: %s <get|post|put|delete|fetch|path|ipatch> [-r] <URI> [<UTF-8 payload>]\n", argv[0]);
     printf("Options: (default: unreliable)\n");
     printf("    -r   send reliably (send CON instead of NON over RFC7252 over UDP/DTLS)\n");
+    printf("    -t   print response as UTF-8 if Content-Format is absent\n");
     return 1;
 }
+
+#define _SHELL_FLAG_TEXT (1)
 
 static int _on_response(
     const unicoap_message_t* response,
     const unicoap_aux_t* aux, int error, void* arg
 ) {
-    puts("in app\n");
     (void)aux;
     (void)arg;
     if (error) {
@@ -70,21 +72,22 @@ static int _on_response(
     char status[4] = {0};
     unicoap_print_code(response->code, status);
     printf("response %s (%" PRIuSIZE " bytes)\n", status, response->payload_size);
-
     unicoap_content_format_t format;
-    if ((unicoap_options_get_content_format(response->options, &format) >= 0
-         && format == UNICOAP_FORMAT_TEXT)) {
-        printf("text respose: '%.*s'\n", (int)response->payload_size, (char*)response->payload);
-    }
-    else if (response->payload_size > 0) {
+    if (((error = unicoap_options_get_content_format(response->options, &format)) >= 0
+         && format == UNICOAP_FORMAT_TEXT)
+        || (error == -ENOENT && (((uint8_t)(uintptr_t)arg) & _SHELL_FLAG_TEXT))) {
+        printf("text response: '%.*s'\n", (int)response->payload_size, (char*)response->payload);
+    } else if (response->payload_size > 0) {
         od_hex_dump(response->payload, response->payload_size, 16);
     }
     return 0;
 }
 
-static int _set_flags(char* opt, unicoap_client_flags_t* flags) {
+static int _set_flags(char* opt, unicoap_client_flags_t* flags, uint8_t* shell_flags) {
     if (strcmp(opt, "-r") == 0) {
         *flags |= UNICOAP_CLIENT_FLAG_RELIABLE;
+    } else if (strcmp(opt, "-t") == 0) {
+        *shell_flags |= _SHELL_FLAG_TEXT;
     } else {
         printf("error: invalid option '%s'\n", opt);
         return -1;
@@ -99,6 +102,7 @@ static int _cli(int argc, char** argv) {
     };
 
     unicoap_client_flags_t flags = 0;
+    uint8_t shell_flags = 0;
     int res = 0;
 
     if (argc == 1) {
@@ -123,7 +127,7 @@ static int _cli(int argc, char** argv) {
     /* parse options */
     int k = 2;
     while (k < argc && strncmp(argv[k], "-", 1) == 0) {
-        if (_set_flags(argv[k], &flags) < 0) {
+        if (_set_flags(argv[k], &flags, &shell_flags) < 0) {
             goto help;
         }
         k += 1;
@@ -135,6 +139,7 @@ static int _cli(int argc, char** argv) {
 
         char* payload = NULL;
         UNICOAP_OPTIONS_ALLOC_STATIC(options, 60);
+        unicoap_options_clear(&options);
 
         if (k < argc) {
             payload = argv[k];
@@ -157,7 +162,7 @@ static int _cli(int argc, char** argv) {
         unicoap_request_init_string_with_options(&request, (unicoap_method_t)method, payload, &options);
         unicoap_destination_t destination = unicoap_destination_uri_string(uri);
 
-        if ((res = unicoap_send_request_async(&request, &destination, _on_response, NULL, flags)) < 0) {
+        if ((res = unicoap_send_request_async(&request, &destination, _on_response, (void*)(uintptr_t)shell_flags, flags)) < 0) {
             printf("sending request failed (error %i, %s)\n", res, strerror(-res));
             return res;
         }
