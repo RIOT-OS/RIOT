d7ccbdc3c9d6487a65967aca59abd23ef681c256
Bas Stottelaar <basstottelaar@gmail.com>
2026-05-19 00:36:05 +0200

    features: add lpc1768_eth to features

--- makefiles/features_modules.inc.mk
+++ makefiles/features_modules.inc.mk
@@ -119,6 +119,9 @@ endif
 # use efm32_coretemp if the feature is used
 USEMODULE += $(filter efm32_coretemp, $(FEATURES_USED))
 
+# use lpc1768_eth if the feature is used
+USEMODULE += $(filter lpc1768_eth, $(FEATURES_USED))
+
 # if LC filter(s) is attached to the CPUs voltage regulator, use it
 USEMODULE += $(filter vdd_lc_filter_%,$(FEATURES_USED))
 
