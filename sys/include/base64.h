186d47ae1d054da0f51a1c14d986857b7d9b4f17
Marian Buschsieweke <marian.buschsieweke@ml-pa.com>
2026-06-10 19:15:57 +0200

    sys/base64: add ACCESS() attribute
    - annotate functions using the `ACCESS()` macro with the `access`
      attribute so that GCC can emit proper `-Wstringop-overflow` warnings
    - use SPDX license IDs
    
    Co-authored-by: crasbe <crasbe@gmail.com>

--- sys/include/base64.h
+++ sys/include/base64.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2014 Hochschule für Angewandte Wissenschaften Hamburg (HAW)
- * Copyright (C) 2014 Martin Landsmann <Martin.Landsmann@HAW-Hamburg.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2014 HAW Hamburg
+ * SPDX-FileCopyrightText: 2014 Martin Landsmann <Martin.Landsmann@HAW-Hamburg.de>
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
@@ -15,11 +12,13 @@
  * @brief       base64 encoder and decoder
  * @{
  *
- * @brief       encoding and decoding functions for base64
+ * @brief       Encoding and decoding functions for base64
  * @author      Martin Landsmann <Martin.Landsmann@HAW-Hamburg.de>
  */
 
 #include <stddef.h> /* for size_t */
+
+#include "compiler_hints.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -61,17 +60,17 @@ static inline size_t base64_estimate_encode_size(size_t data_in_size)
  * @param[in]       data_in_size      the size of `data_in`
  * @param[out]      base64_out        pointer to store the encoded base64 string
  * @param[in,out]   base64_out_size   pointer to the variable containing the size of `base64_out.`
-                                      This value is overwritten with the estimated size used for
-                                      the encoded base64 string on BASE64_ERROR_BUFFER_OUT_SIZE.
-                                      This value is overwritten with the actual used size for the
-                                      encoded base64 string on BASE64_SUCCESS.
-
- * @returns BASE64_SUCCESS on success,
-            BASE64_ERROR_BUFFER_OUT_SIZE on insufficient size for encoding to `base64_out`,
-            BASE64_ERROR_BUFFER_OUT if `base64_out` equals NULL
-                                    but the `base64_out_size` is sufficient,
-            BASE64_ERROR_DATA_IN if `data_in` equals NULL.
+ *                                    This value is overwritten with the estimated size used for
+ *                                    the encoded base64 string on BASE64_ERROR_BUFFER_OUT_SIZE.
+ *                                    This value is overwritten with the actual used size for the
+ *                                    encoded base64 string on BASE64_SUCCESS.
+ *
+ * @retval  BASE64_SUCCESS                  success
+ * @retval  BASE64_ERROR_BUFFER_OUT_SIZE    insufficient size for encoding to `base64_out`
+ * @retval  BASE64_ERROR_BUFFER_OUT         `base64_out` equals `NULL`
+ * @retval  BASE64_ERROR_DATA_IN            `data_in` equals `NULL`
  */
+ACCESS(read_only, 1, 2)
 int base64_encode(const void *data_in, size_t data_in_size,
                   void *base64_out, size_t *base64_out_size);
 
@@ -87,17 +86,17 @@ int base64_encode(const void *data_in, size_t data_in_size,
  * @param[in]       data_in_size      the size of `data_in`
  * @param[out]      base64_out        pointer to store the encoded base64 string
  * @param[in,out]   base64_out_size   pointer to the variable containing the size of `base64_out.`
-                                      This value is overwritten with the estimated size used for
-                                      the encoded base64 string on BASE64_ERROR_BUFFER_OUT_SIZE.
-                                      This value is overwritten with the actual used size for the
-                                      encoded base64 string on BASE64_SUCCESS.
-
- * @returns BASE64_SUCCESS on success,
-            BASE64_ERROR_BUFFER_OUT_SIZE on insufficient size for encoding to `base64_out`,
-            BASE64_ERROR_BUFFER_OUT if `base64_out` equals NULL
-                                    but the `base64_out_size` is sufficient,
-            BASE64_ERROR_DATA_IN if `data_in` equals NULL.
+ *                                    This value is overwritten with the estimated size used for
+ *                                    the encoded base64 string on BASE64_ERROR_BUFFER_OUT_SIZE.
+ *                                    This value is overwritten with the actual used size for the
+ *                                    encoded base64 string on BASE64_SUCCESS.
+ *
+ * @retval  BASE64_SUCCESS                  success
+ * @retval  BASE64_ERROR_BUFFER_OUT_SIZE    insufficient size for encoding to `base64_out`
+ * @retval  BASE64_ERROR_BUFFER_OUT         `base64_out` equals `NULL`
+ * @retval  BASE64_ERROR_DATA_IN            `data_in` equals `NULL`
  */
+ACCESS(read_only, 1, 2)
 int base64url_encode(const void *data_in, size_t data_in_size,
                      void *base64_out, size_t *base64_out_size);
 
@@ -106,19 +105,19 @@ int base64url_encode(const void *data_in, size_t data_in_size,
  * @param[out]      base64_in        pointer to store the encoded base64 string
  * @param[in]       base64_in_size   pointer to the variable containing the size of `base64_out.`
  * @param[in]       data_out         pointer to the datum to encode
- * @param[in, out]  data_out_size    the size of `data_out`.
+ * @param[in,out]   data_out_size    the size of `data_out`.
                                      This value is overwritten with the estimated size used for the
                                      decoded string on BASE64_ERROR_BUFFER_OUT_SIZE.
                                      This value is overwritten with the actual used size for the
                                      decoded string on BASE64_SUCCESS.
 
- * @returns BASE64_SUCCESS on success,
-            BASE64_ERROR_BUFFER_OUT_SIZE on insufficient size for decoding to `data_out`,
-            BASE64_ERROR_BUFFER_OUT if `data_out` equals NULL
-                                    but the size for `data_out_size` is sufficient,
-            BASE64_ERROR_DATA_IN if `base64_in` equals NULL,
-            BASE64_ERROR_DATA_IN_SIZE if `base64_in_size` is between 1 and 4.
+ * @retval  BASE64_SUCCESS                  success
+ * @retval  BASE64_ERROR_BUFFER_OUT_SIZE    insufficient size for encoding to `base64_out`
+ * @retval  BASE64_ERROR_BUFFER_OUT         `base64_out` equals `NULL`
+ * @retval  BASE64_ERROR_DATA_IN            `data_in` equals `NULL`
+ * @retval  BASE64_ERROR_DATA_IN_SIZE       `base64_in_size` is between 1 and 4.
  */
+ACCESS(read_only, 1, 2)
 int base64_decode(const void *base64_in, size_t base64_in_size,
                   void *data_out, size_t *data_out_size);
 
