a691eb494c4f5d50ba996f596bd921ce94dc5719
MastenaM <116292884+MastenaM@users.noreply.github.com>
2026-06-22 11:20:32 +0300

    Update drivers/lis331dlh/include/lis331dlh_params.h
    Co-authored-by: Marian Buschsieweke <maribu@users.noreply.github.com>

--- drivers/lis331dlh/include/lis331dlh_params.h
+++ drivers/lis331dlh/include/lis331dlh_params.h
@@ -22,27 +22,27 @@ extern "C" {
 #endif
 
 #ifndef LIS331DLH_PARAM_I2C
-#define LIS331DLH_PARAM_I2C      I2C_DEV(0)
+#  define LIS331DLH_PARAM_I2C    I2C_DEV(0)
 #endif
 #ifndef LIS331DLH_PARAM_ADDR
-#define LIS331DLH_PARAM_ADDR     LIS331DLH_ADDR_DEFAULT
+#  define LIS331DLH_PARAM_ADDR   LIS331DLH_ADDR_DEFAULT
 #endif
 #ifndef LIS331DLH_PARAM_SCALE
-#define LIS331DLH_PARAM_SCALE    LIS331DLH_SCALE_2G
+#  define LIS331DLH_PARAM_SCALE  LIS331DLH_SCALE_2G
 #endif
 #ifndef LIS331DLH_PARAM_RATE
-#define LIS331DLH_PARAM_RATE     LIS331DLH_RATE_50HZ
+#  define LIS331DLH_PARAM_RATE   LIS331DLH_RATE_50HZ
 #endif
 
 #ifndef LIS331DLH_PARAMS
-#define LIS331DLH_PARAMS         { .i2c   = LIS331DLH_PARAM_I2C,   \
+#  define LIS331DLH_PARAMS       { .i2c   = LIS331DLH_PARAM_I2C,   \
                                    .addr  = LIS331DLH_PARAM_ADDR,  \
                                    .scale = LIS331DLH_PARAM_SCALE, \
                                    .rate  = LIS331DLH_PARAM_RATE }
 #endif
 
 #ifndef LIS331DLH_SAUL_INFO
-#define LIS331DLH_SAUL_INFO      { .name = "lis331dlh" }
+#  define LIS331DLH_SAUL_INFO    { .name = "lis331dlh" }
 #endif
 
 static const lis331dlh_params_t lis331dlh_params[] =
