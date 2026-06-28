660ae0974b2ea8bce9fd2ba695cf48686ad9322b
Bas Stottelaar <basstottelaar@gmail.com>
2026-06-28 23:23:12 +0200

    tests/sys/stdio_notify: add test application
    This application demonstrates the main/intended use of the stdio_notify
    module: one consumer that has multiple reasons to be unblocked while
    reading from stdio.

---
+++ tests/sys/stdio_notify/tests/01-run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2026 Bas Stottelaar <basstottelaar@gmail.com>
+# SPDX-License-Identifier: LGPL-2.1-only
+
+import sys
+from testrunner import run
+
+
+def testfunc(child):
+    child.expect_exact(
+        "Type characters to echo them, or wait for the periodic timer.")
+
+    # sending input should wake the thread through the notify callback and
+    # echo the bytes back via stdio_read
+    child.sendline("hi 123")
+    child.expect("hi 123")
+
+    # without any input, the periodic timer should still wake the thread
+    child.expect_exact("*** Woken up! ***", timeout=10)
+
+
+if __name__ == "__main__":
+    sys.exit(run(testfunc))
