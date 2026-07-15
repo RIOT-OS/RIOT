6b3b3650e51f6317b6a706393b3acff2085badf1
Karl Fessel <karl.fessel@ml-pa.com>
2026-07-15 12:25:44 +0200

    sys/include: use SPDX license tags[o-z]

--- sys/include/time_units.h
+++ sys/include/time_units.h
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2021 Kaspar Schleiser <kaspar@schleiser.de>
- * Copyright (C) 2014 Oliver Hahm <oliver.hahm@inria.fr>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2021 Kaspar Schleiser <kaspar@schleiser.de>
+ * SPDX-FileCopyrightText: 2014 Oliver Hahm <oliver.hahm@inria.fr>
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #pragma once
