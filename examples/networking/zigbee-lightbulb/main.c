1e61cbe72d774fc051a4147d7b5935d40e196c8b
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-07-31 16:28:02 +0200

    fixup! examples/networking: add two zigbee examples

--- examples/networking/zigbee-lightbulb/main.c
+++ examples/networking/zigbee-lightbulb/main.c
@@ -36,16 +36,24 @@
 #include "zb_zcl_basic.h"
 #include "zb_osif.h"
 
+#ifdef WS281X_PARAM_NUMOF
+#include "ws281x_params.h"
+#endif
+
 #define ENABLE_DEBUG (0)
 #include "debug.h"
 
 #define APPLICATION_ENDPOINT 11
 
+#define INPUT_CL_COUNT 4
+
+#define OUTPUT_CL_COUNT 0
+
 #define MAIN_QUEUE_SIZE (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 ws281x_t led_dev;
-color_rgb_t led_white = {.r = 255, .g = 255, .b = 255};
-color_rgb_t led_coff = {.r = 0, .g = 0, .b = 0};
+static const color_rgb_t led_white = {.r = 255, .g = 255, .b = 255};
+static const color_rgb_t led_coff = {.r = 0, .g = 0, .b = 0};
 
 /* these commands are provided by the zboss stack */
 int zb_input_packet(int argc, char **argv);
@@ -63,7 +71,6 @@ void set_led(zb_uint8_t param)
 {
 
 #ifdef WS281X_PARAM_NUMOF
-    ws281x_prepare_transmission(&led_dev);
     if (param) {
         ws281x_set(&led_dev, 0, led_white);
     }
@@ -71,7 +78,6 @@ void set_led(zb_uint8_t param)
         ws281x_set(&led_dev, 0, led_coff);
     }
     ws281x_write(&led_dev);
-    ws281x_end_transmission(&led_dev);
 #elif defined LED0_ON
     if (param) {
         LED0_ON;
@@ -92,77 +98,71 @@ static zb_zcl_on_off_srv_attr_t on_off_attrs = {
     .off_wait_time = 0x0000,
     .set_state = set_led,
 };
-static zb_zcl_basic_srv_attr_t basic_attrs;
+
+static zb_zcl_basic_srv_attr_t basic_attrs = {
+    .app_version = 1,
+    .power_source = ZB_ZCL_BASIC_POWER_SRC_MAINS,
+    .phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE,
+    .generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_LED_BULB,
+    .manufacturer_name = "RIOT OS",
+    .manufacturer_name_len = sizeof("RIOT OS"),
+    .model_id = RIOT_BOARD,
+    .model_id_len = sizeof(RIOT_BOARD),
+    .date_code = "19700101",
+    .date_code_len = sizeof("19700101"),
+    .location_desc = "Lab",
+    .location_desc_len = sizeof("Lab"),
+    .product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN,
+    .product_code = "1234567891011",
+    .product_code_len = sizeof("1234567891011"),
+    .software_build_id = RIOT_VERSION,
+    .software_build_id_len = sizeof(RIOT_VERSION),
+};
+
+/* This should equal (INPUT_CL_COUNT, OUTPUT_CL_COUNT) */
+ZB_DECLARE_SIMPLE_DESC(4, 0);
+static zb_af_simple_desc_4_0_t simple_desc_lightbulb = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    ZB_HA_ON_OFF_LIGHT_DEV_ID,
+    1,/* app version */
+    0,/* reserved */
+    INPUT_CL_COUNT,
+    OUTPUT_CL_COUNT,
+    {
+        // first input(srv), then output(cli)
+        ZB_BASIC_CLUSTER_ID,
+        ZB_ZLL_CLUSTER_ID,
+        ZB_GROUPS_CLUSTER_ID,
+        ZB_ON_OFF_CLUSTER_ID
+    }
+};
+
+static zb_af_ep_desc_t lightbulb_ep = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    (INPUT_CL_COUNT + OUTPUT_CL_COUNT),
+    (zb_af_simple_desc_1_1_t *)&simple_desc_lightbulb,
+    {
+        ZB_ZCL_CLUSTER_DESC(&basic_attrs, ZB_BASIC_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+        ZB_ZCL_CLUSTER_DESC(NULL, ZB_ZLL_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+        ZB_ZCL_CLUSTER_DESC(&group_attrs, ZB_GROUPS_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+        ZB_ZCL_CLUSTER_DESC(&on_off_attrs, ZB_ON_OFF_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+    }  
+};
 
 int main(void)
 {
     /* setup ws281x led */
 #ifdef WS281X_PARAM_NUMOF
-    ws281x_params_t led_params;
-    led_params.numof = WS281X_PARAM_NUMOF;
-    led_params.pin = WS281X_PARAM_PIN;
-    ws281x_init(&led_dev, &led_params);
+    ws281x_init(&led_dev, &ws281x_params[0]);
 #endif
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    puts("RIOT Zigbee ZLL example application");
+    puts("RIOT Zigbee ZLL Lightbulb example application");
 
-    /* set device descriptor */
-    ZB_DECLARE_SIMPLE_DESC(3/* in clusters. Groups, basic, on-off */, 1/* out clusters. on-off */);
-    static zb_af_simple_desc_3_1_t desc;
-    zb_set_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc,
-                              APPLICATION_ENDPOINT, /* endpoint */
-                              ZB_HA_PROFILE_ID,
-                              ZB_HA_ON_OFF_LIGHT_DEV_ID,
-                              1, /* app version */
-                              3, /* input_cl count*/
-                              0 /* output_cl count*/
-                            );
-    /* input clusters are typically servers -> call *_srv_setup*/
-    zb_set_input_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 0, ZB_BASIC_CLUSTER_ID);
-    zb_set_input_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 1, ZB_GROUPS_CLUSTER_ID);
-    zb_set_input_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 2, ZB_ON_OFF_CLUSTER_ID);
-
-    zb_add_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc);
-
-    /* set cluster attributes */
-    zb_zcl_basic_srv_set_defaults(&basic_attrs);
-
-    basic_attrs.app_version = 1;
-    basic_attrs.power_source = ZB_ZCL_BASIC_POWER_SRC_MAINS;
-    basic_attrs.phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE;
-    basic_attrs.generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_LED_BULB;
-
-    char manufacturer[] = "RIOT OS";
-    basic_attrs.manufacturer_name_len = sizeof(manufacturer);
-    ZB_MEMCPY(basic_attrs.manufacturer_name, manufacturer, sizeof(manufacturer));
-
-    char model_id[] = RIOT_BOARD;
-    basic_attrs.model_id_len = sizeof(model_id);
-    ZB_MEMCPY(basic_attrs.model_id, model_id, sizeof(model_id));
-
-    char date[] = "19700101";
-    basic_attrs.date_code_len = sizeof(date);
-    ZB_MEMCPY(basic_attrs.date_code, date, sizeof(date));
-
-    char location[] = "Lab";
-    basic_attrs.location_desc_len = sizeof(location);
-    ZB_MEMCPY(basic_attrs.location_desc, location, sizeof(location));
-
-    basic_attrs.product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN;
-    char ean[] = "1234567891011";
-    basic_attrs.product_code_len = sizeof(ean);
-    ZB_MEMCPY(basic_attrs.product_code, ean, sizeof(ean));
-
-    basic_attrs.software_build_id_len = sizeof(RIOT_VERSION);
-    ZB_MEMCPY(basic_attrs.software_build_id, RIOT_VERSION, sizeof(RIOT_VERSION));
-
-    zb_zcl_on_off_srv_setup(APPLICATION_ENDPOINT, &on_off_attrs);
-    zb_zcl_groups_srv_setup(APPLICATION_ENDPOINT, &group_attrs);
-    zb_zcl_basic_srv_setup(APPLICATION_ENDPOINT, &basic_attrs);
-
-    zb_zcl_zll_target_setup();
+    zb_zcl_init_ep(&lightbulb_ep);
 
     /* start shell */
     puts("All up, running the shell now");
--- examples/networking/zigbee-remote/main.c
+++ examples/networking/zigbee-remote/main.c
@@ -21,10 +21,7 @@
 
 #include "shell.h"
 #include "msg.h"
-#include "led.h"
 #include "board.h"
-#include "periph/gpio.h"
-#include <ws281x.h>
 
 #include "zb_common.h"
 #include "zb_aps.h"
@@ -41,11 +38,12 @@
 
 #define APPLICATION_ENDPOINT 11
 
+#define INPUT_CL_COUNT 1
+
+#define OUTPUT_CL_COUNT 2
+
 #define MAIN_QUEUE_SIZE (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-ws281x_t led_dev;
-color_rgb_t led_white = {.r = 255, .g = 255, .b = 255};
-color_rgb_t led_coff = {.r = 0, .g = 0, .b = 0};
 
 /* these commands are provided by the zboss stack */
 int zb_input_packet(int argc, char **argv);
@@ -107,7 +105,55 @@ int tl_scan(int argc, char **argv)
 }
 SHELL_COMMAND(tl_scan, "manual touchlink scan", tl_scan);
 
-static zb_zcl_basic_srv_attr_t basic_attrs;
+static zb_zcl_basic_srv_attr_t basic_attrs = {
+    .app_version = 1,
+    .power_source = ZB_ZCL_BASIC_POWER_SRC_BATTERY,
+    .phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE,
+    .generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_REMOTE_CONTROLLER,
+    .manufacturer_name = "RIOT OS",
+    .manufacturer_name_len = sizeof("RIOT OS"),
+    .model_id = RIOT_BOARD,
+    .model_id_len = sizeof(RIOT_BOARD),
+    .date_code = "19700101",
+    .date_code_len = sizeof("19700101"),
+    .location_desc = "Lab",
+    .location_desc_len = sizeof("Lab"),
+    .product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN,
+    .product_code = "1234567891011",
+    .product_code_len = sizeof("1234567891011"),
+    .software_build_id = RIOT_VERSION,
+    .software_build_id_len = sizeof(RIOT_VERSION),
+};
+
+/* This should equal (INPUT_CL_COUNT, OUTPUT_CL_COUNT) */
+ZB_DECLARE_SIMPLE_DESC(1, 2);
+static zb_af_simple_desc_1_2_t simple_desc_remote = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    ZB_HA_ON_OFF_L_SW_DEV_ID,
+    1,/* app version */
+    0,/* reserved */
+    INPUT_CL_COUNT,
+    OUTPUT_CL_COUNT,
+    {
+        // first input(srv), then output(cli)
+        ZB_BASIC_CLUSTER_ID,
+        ZB_ZLL_CLUSTER_ID,
+        ZB_ON_OFF_CLUSTER_ID
+    }
+};
+
+static zb_af_ep_desc_t remote_ep = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    (INPUT_CL_COUNT + OUTPUT_CL_COUNT),
+    (zb_af_simple_desc_1_1_t *)&simple_desc_remote,
+    {
+        ZB_ZCL_CLUSTER_DESC(&basic_attrs, ZB_BASIC_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+        ZB_ZCL_CLUSTER_DESC(NULL, ZB_ZLL_CLUSTER_ID, ZB_ZCL_CLIENT_ROLE),
+        ZB_ZCL_CLUSTER_DESC(NULL, ZB_ON_OFF_CLUSTER_ID, ZB_ZCL_CLIENT_ROLE),
+    }  
+};
 
 int main(void)
 {
@@ -116,61 +162,7 @@ int main(void)
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT Zigbee ZLL example application");
 
-    /* set device descriptor */
-    /* zb_af_simple_desc_1_1_t is already declared by default,
-     * uncomment and change numbers as needed */
-    //ZB_DECLARE_SIMPLE_DESC(1/* in clusters. on-off */, 1/* out clusters. on-off */);
-    static zb_af_simple_desc_1_1_t desc;
-    zb_set_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc,
-                             APPLICATION_ENDPOINT, /* endpoint */
-                             ZB_HA_PROFILE_ID,
-                             ZB_HA_ON_OFF_L_SW_DEV_ID,
-                             1, /* app version */
-                             1, /* input_cl count*/
-                             1  /* output_cl count*/
-                            );
-    /* input clusters are typically servers -> call *_srv_setup*/
-    zb_set_input_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 0, ZB_BASIC_CLUSTER_ID);
-
-    zb_set_output_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 0, ZB_ON_OFF_CLUSTER_ID);
-
-    zb_add_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc);
-
-    /* set basic cluster attributes */
-    zb_zcl_basic_srv_set_defaults(&basic_attrs);
-
-    basic_attrs.app_version = 1;
-    basic_attrs.power_source = ZB_ZCL_BASIC_POWER_SRC_MAINS;
-    basic_attrs.phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE;
-    basic_attrs.generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_LED_BULB;
-
-    char manufacturer[] = "RIOT OS";
-    basic_attrs.manufacturer_name_len = sizeof(manufacturer);
-    ZB_MEMCPY(basic_attrs.manufacturer_name, manufacturer, sizeof(manufacturer));
-
-    char model_id[] = RIOT_BOARD;
-    basic_attrs.model_id_len = sizeof(model_id);
-    ZB_MEMCPY(basic_attrs.model_id, model_id, sizeof(model_id));
-
-    char date[] = "19700101";
-    basic_attrs.date_code_len = sizeof(date);
-    ZB_MEMCPY(basic_attrs.date_code, date, sizeof(date));
-
-    char location[] = "Lab";
-    basic_attrs.location_desc_len = sizeof(location);
-    ZB_MEMCPY(basic_attrs.location_desc, location, sizeof(location));
-
-    basic_attrs.product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN;
-    char ean[] = "1234567891011";
-    basic_attrs.product_code_len = sizeof(ean);
-    ZB_MEMCPY(basic_attrs.product_code, ean, sizeof(ean));
-
-    basic_attrs.software_build_id_len = sizeof(RIOT_VERSION);
-    ZB_MEMCPY(basic_attrs.software_build_id, RIOT_VERSION, sizeof(RIOT_VERSION));
-
-    zb_zcl_basic_srv_setup(APPLICATION_ENDPOINT, &basic_attrs);
-
-    zb_zcl_zll_initiator_setup();
+    zb_zcl_init_ep(&remote_ep);
 
     /* start shell */
     puts("All up, running the shell now");
