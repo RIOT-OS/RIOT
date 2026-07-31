23cbeb91121e8f088ba1504b504b259268f2ea49
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2026-07-31 16:35:22 +0200

    fixup! tests/net: add zb touchlink target

--- tests/net/zigbee-tl-target/main.c
+++ tests/net/zigbee-tl-target/main.c
@@ -40,10 +40,59 @@
 
 #define APPLICATION_ENDPOINT 11
 
+#define INPUT_CL_COUNT 2
+
+#define OUTPUT_CL_COUNT 0
+
 #define MAIN_QUEUE_SIZE (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
-static zb_zcl_basic_srv_attr_t basic_attrs;
+static zb_zcl_basic_srv_attr_t basic_attrs = {
+    .app_version = 1,
+    .power_source = ZB_ZCL_BASIC_POWER_SRC_MAINS,
+    .phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE,
+    .generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_LED_BULB,
+    .manufacturer_name = "RIOT OS",
+    .manufacturer_name_len = sizeof("RIOT OS"),
+    .model_id = RIOT_BOARD,
+    .model_id_len = sizeof(RIOT_BOARD),
+    .date_code = "19700101",
+    .date_code_len = sizeof("19700101"),
+    .location_desc = "Lab",
+    .location_desc_len = sizeof("Lab"),
+    .product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN,
+    .product_code = "1234567891011",
+    .product_code_len = sizeof("1234567891011"),
+    .software_build_id = RIOT_VERSION,
+    .software_build_id_len = sizeof(RIOT_VERSION),
+};
+
+/* This should equal (INPUT_CL_COUNT, OUTPUT_CL_COUNT) */
+ZB_DECLARE_SIMPLE_DESC(2, 0);
+static zb_af_simple_desc_2_0_t simple_desc_target = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    ZB_HA_ON_OFF_LIGHT_DEV_ID,
+    1,/* app version */
+    0,/* reserved */
+    INPUT_CL_COUNT,
+    OUTPUT_CL_COUNT,
+    {
+        ZB_BASIC_CLUSTER_ID,
+        ZB_ZLL_CLUSTER_ID
+    }
+};
+
+static zb_af_ep_desc_t target_ep = {
+    APPLICATION_ENDPOINT,
+    ZB_HA_PROFILE_ID,
+    (INPUT_CL_COUNT + OUTPUT_CL_COUNT),
+    (zb_af_simple_desc_1_1_t *)&simple_desc_target,
+    {
+        ZB_ZCL_CLUSTER_DESC(&basic_attrs, ZB_BASIC_CLUSTER_ID, ZB_ZCL_SERVER_ROLE),
+        ZB_ZCL_CLUSTER_DESC(NULL, ZB_ZLL_CLUSTER_ID, ZB_ZCL_SERVER_ROLE)
+    }
+};
 
 int main(void)
 {
@@ -51,57 +100,8 @@ int main(void)
      * receive potentially fast incoming networking packets */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
-    /* set device descriptor */
-    //ZB_DECLARE_SIMPLE_DESC(1/* in clusters. basic */, 1/* out clusters. on-off */);
-    static zb_af_simple_desc_1_1_t desc;
-    zb_set_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc,
-                              APPLICATION_ENDPOINT, /* endpoint */
-                              ZB_HA_PROFILE_ID,
-                              ZB_HA_ON_OFF_LIGHT_DEV_ID,
-                              1, /* app version */
-                              1, /* input_cl count*/
-                              0 /* output_cl count*/
-                            );
-    /* input clusters are typically servers -> call *_srv_setup*/
-    zb_set_input_cluster_id((zb_af_simple_desc_1_1_t *)&desc, 0, ZB_BASIC_CLUSTER_ID);
+    zb_zcl_init_ep(&target_ep);
 
-    zb_add_simple_descriptor((zb_af_simple_desc_1_1_t *)&desc);
-
-    /* set cluster attributes */
-    zb_zcl_basic_srv_set_defaults(&basic_attrs);
-
-    basic_attrs.app_version = 1;
-    basic_attrs.power_source = ZB_ZCL_BASIC_POWER_SRC_MAINS;
-    basic_attrs.phys_environment = ZB_ZCL_BASIC_PHYS_ENV_OFFICE;
-    basic_attrs.generic_device_type = ZB_ZCL_BASIC_GEN_DEV_TYPE_LED_BULB;
-
-    char manufacturer[] = "RIOT OS";
-    basic_attrs.manufacturer_name_len = sizeof(manufacturer);
-    ZB_MEMCPY(basic_attrs.manufacturer_name, manufacturer, sizeof(manufacturer));
-
-    char model_id[] = RIOT_BOARD;
-    basic_attrs.model_id_len = sizeof(model_id);
-    ZB_MEMCPY(basic_attrs.model_id, model_id, sizeof(model_id));
-
-    char date[] = "19700101";
-    basic_attrs.date_code_len = sizeof(date);
-    ZB_MEMCPY(basic_attrs.date_code, date, sizeof(date));
-
-    char location[] = "Lab";
-    basic_attrs.location_desc_len = sizeof(location);
-    ZB_MEMCPY(basic_attrs.location_desc, location, sizeof(location));
-
-    basic_attrs.product_code_id = ZB_ZCL_BASIC_PRODUCT_CODE_EAN;
-    char ean[] = "1234567891011";
-    basic_attrs.product_code_len = sizeof(ean);
-    ZB_MEMCPY(basic_attrs.product_code, ean, sizeof(ean));
-
-    basic_attrs.software_build_id_len = sizeof(RIOT_VERSION);
-    ZB_MEMCPY(basic_attrs.software_build_id, RIOT_VERSION, sizeof(RIOT_VERSION));
-
-    zb_zcl_basic_srv_setup(APPLICATION_ENDPOINT, &basic_attrs);
-
-    zb_zcl_zll_target_setup();
     /* start shell */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
 
