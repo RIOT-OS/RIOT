35e180b68fb3994115d0a84673755088fa4adcac
Bas Stottelaar <basstottelaar@gmail.com>
2026-08-05 20:14:50 +0200

    tests/unittests: add header tests for sys/net/ipv4

---
+++ tests/unittests/tests-ipv4_hdr/tests-ipv4_hdr.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Bas Stottelaar <basstottelaar@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#pragma once
+
+/**
+ * @addtogroup  unittests
+ * @{
+ *
+ * @file
+ * @brief       Unittests for the ``ipv4_hdr`` module
+ *
+ * @author      Bas Stottelaar <basstottelaar@gmail.com>
+ */
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   The entry point of this test suite.
+ */
+void tests_ipv4_hdr(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
