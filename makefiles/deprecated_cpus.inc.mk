1d8ed26ed98b4bcb3cc25ae63b9d921331279596
crasbe <crasbe@gmail.com>
2026-08-05 13:12:14 +0200

    fixup! fixup! treewide: migrate to echoinfo, echowarn, echocolor, ...

--- makefiles/deprecated_cpus.inc.mk
+++ makefiles/deprecated_cpus.inc.mk
@@ -2,7 +2,6 @@
 # Keep this list ALPHABETICALLY SORTED!!!!
 ifeq ($(MAKELEVEL),0)
   ifneq (,$(filter $(DEPRECATED_CPUS),$(CPU)))
-    $(shell $(COLOR_ECHO) "$(COLOR_RED)Deprecated cpu: $(COLOR_RESET)"\
-                          "$(CPU)" 1>&2)
+    $(call echoinfo,($(call c_red,"Deprecated CPU: ")"\"$(CPU)\""))
   endif
 endif
