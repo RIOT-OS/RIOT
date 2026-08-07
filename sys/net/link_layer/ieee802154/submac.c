5e0b828501f4aab610abb843cf85f18bba26a9f8
Lukas-Luger <73549862+Lukas-Luger@users.noreply.github.com>
2026-08-07 10:20:59 +0200

    Update sys/net/link_layer/ieee802154/submac.c
    Co-authored-by: Stepan Konoplev <127890941+Stopkaa@users.noreply.github.com>

--- sys/net/link_layer/ieee802154/submac.c
+++ sys/net/link_layer/ieee802154/submac.c
@@ -250,7 +250,8 @@ static ieee802154_fsm_state_t _fsm_state_rx(ieee802154_submac_t *submac, ieee802
                 if ((submac->rx_buf[0] & IEEE802154_FCF_ACK_REQ) &&
                     (ieee802154_radio_get_frame_filter_mode(dev, &mode) < 0 ||
                     mode == IEEE802154_FILTER_ACCEPT)) {
-                    if ((res = _handle_fsm_ev_tx_ack(submac, ieee802154_get_seq(submac->rx_buf))) < 0) {
+                    if ((res = _handle_fsm_ev_tx_ack(submac, 
+                                                                          ieee802154_get_seq(submac->rx_buf))) < 0) {
                         DEBUG("IEEE802154 submac: Sending ACK failed with status: %d\n", res);
                     }
                     else {
