022eadbf643a4c1e59753733d7ef1d7fc5bc12e2
crasbe <crasbe@gmail.com>
2026-08-21 11:38:04 +0200

    makefiles/docker: bump riotdocker version

--- makefiles/docker.inc.mk
+++ makefiles/docker.inc.mk
@@ -5,7 +5,7 @@
 # When the docker image is updated, checks at
 # dist/tools/buildsystem_sanity_check/check.sh start complaining in CI, and
 # provide the latest values to verify and fill in.
-DOCKER_TESTED_IMAGE_REPO_DIGEST := 61c1aa5371bd0e3a8764121671555c8b54907ad8e1eddc520f4e601bbcbcd702
+DOCKER_TESTED_IMAGE_REPO_DIGEST := ed4b463606fe0653432f7c516f6bd94876ac13cc8949e4ffff39106d7c82ed0e
 
 DOCKER_PULL_IDENTIFIER := docker.io/riot/riotbuild@sha256:$(DOCKER_TESTED_IMAGE_REPO_DIGEST)
 export DOCKER_IMAGE ?= $(DOCKER_PULL_IDENTIFIER)
