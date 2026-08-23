6bb9680f3a126db97134e4ea382cea9eea937bac
N11cc00 <nico@nico-behrens.de>
2026-08-23 15:49:35 +0200

    sys/[i-n]*: replace license headers with SPDX

--- sys/net/gnrc/routing/rpl/gnrc_rpl.c
+++ sys/net/gnrc/routing/rpl/gnrc_rpl.c
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2018       HAW Hamburg
- * Copyright (C) 2015–2017  Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2018 HAW Hamburg
+ * SPDX-FileCopyrightText: 2015-2017 Cenk Gündoğan <cenk.guendogan@haw-hamburg.de>
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 /**
--- sys/net/gnrc/routing/rpl/gnrc_rpl_of_manager.c
+++ sys/net/gnrc/routing/rpl/gnrc_rpl_of_manager.c
@@ -1,12 +1,7 @@
 /*
-* RPL dodag implementation
-*
-* Copyright (C) 2014 Freie Universität Berlin
-*
-* This file is subject to the terms and conditions of the GNU Lesser
-* General Public License v2.1. See the file LICENSE in the top level
-* directory for more details.
-*/
+ * SPDX-FileCopyrightText: 2014 Freie Universität Berlin
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 
 /**
  *
@@ -14,6 +9,9 @@
  * @{
  * @file
  * @brief   RPL Objective functions manager
+ *
+ * RPL dodag implementation
+ *
  * @author  Fabian Brandt <fabianbr@zedat.fu-berlin.de>
  * @}
  */
