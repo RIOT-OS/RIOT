3809f138621268d4c8526de335ada6568c31331a
Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
2026-08-25 20:00:51 +0200

    core/debug.h: refactor to allow prefixing
    backwards-compatible by not changing any behavior as long as DEBUG_PREFIX is unset or empty
    
    set a prefix per file by defining DEBUG_PREFIX
    
    enable prefixing every call to debug functions with the function name and/or the thread name via configuration options CONFIG_DEBUG_SHOW_FUNC and CONFIG_DEBUG_SHOW_FUNC

--- core/lib/include/debug.h
+++ core/lib/include/debug.h
@@ -1,5 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2014 Freie Universität Berlin
+ * SPDX-FileCopyrightText: 2026 TU Dresden
  * SPDX-License-Identifier: LGPL-2.1-only
  */
 
@@ -18,41 +19,19 @@
  *              all calls to ::DEBUG will be ignored.
  *
  * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
  */
 
 #include <stdio.h>
+#include <string.h>
+
+#include "ansi_style.h"
+#include "irq.h"
 #include "sched.h"
 #include "thread.h"
 
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-/**
- * @def DEBUG_PRINT
- *
- * @brief Print debug information if the calling thread stack is large enough
- *
- * Use this macro the same as `printf`. When `DEVELHELP` is defined inside an
- * implementation file, all usages of ::DEBUG_PRINT will print the given
- * information to stdout after verifying the stack is big enough. If `DEVELHELP`
- * is not set, this check is not performed. (CPU exception may occur)
- */
-#ifdef DEVELHELP
-#include "cpu_conf.h"
-#define DEBUG_PRINT(...) \
-    do { \
-        if ((thread_get_active() == NULL) || \
-            (thread_get_active()->stack_size >= \
-             THREAD_EXTRA_STACKSIZE_PRINTF)) { \
-            printf(__VA_ARGS__); \
-        } \
-        else { \
-            puts("Cannot debug, stack too small. Consider using DEBUG_PUTS()."); \
-        } \
-    } while (0)
-#else
-#define DEBUG_PRINT(...) printf(__VA_ARGS__)
 #endif
 
 /**
@@ -70,10 +49,10 @@ extern "C" {
  * @param val   Breakpoint context for debugger, usually ignored.
  */
 #ifdef DEVELHELP
-#include "architecture.h"
-#define DEBUG_BREAKPOINT(val)   ARCHITECTURE_BREAKPOINT(val)
+#  include "architecture.h"
+#  define DEBUG_BREAKPOINT(val) ARCHITECTURE_BREAKPOINT(val)
 #else
-#define DEBUG_BREAKPOINT(val)   (void)0
+#  define DEBUG_BREAKPOINT(val) (void)0
 #endif
 
 /**
@@ -84,8 +63,16 @@ extern "C" {
  * @brief   This macro can be defined as 0 or other on a file-based level.
  *          @ref DEBUG() will generate output only if ENABLE_DEBUG is non-zero.
  */
-#if !defined(ENABLE_DEBUG) || defined(DOXYGEN)
-#define ENABLE_DEBUG 0
+#ifndef ENABLE_DEBUG
+#  define ENABLE_DEBUG 0
+#endif
+
+/**
+ * @brief   Common prefix for all debug messages, defaulting to an empty string.
+ *          Expected to be set on a file-based level.
+ */
+#ifndef DEBUG_PREFIX
+#  define DEBUG_PREFIX ""
 #endif
 
 /**
@@ -94,20 +81,153 @@ extern "C" {
  * @brief   Contains the function name if given compiler supports it.
  *          Otherwise it is an empty string.
  */
-# if defined(__cplusplus) && defined(__GNUC__)
+#if defined(__cplusplus) && defined(__GNUC__)
 #  define DEBUG_FUNC __PRETTY_FUNCTION__
-# elif __STDC_VERSION__ >= 199901L
+#elif __STDC_VERSION__ >= 199901L
 #  define DEBUG_FUNC __func__
-# elif __GNUC__ >= 2
+#elif __GNUC__ >= 2
 #  define DEBUG_FUNC __FUNCTION__
-# else
+#else
 #  define DEBUG_FUNC ""
-# endif
+#endif
+
+/**
+ * @brief   Whether calls to @ref DEBUG and @ref DEBUG_PUTS automatically include
+ *          the calling thread name.
+ *
+ * @warning Only applies to files where @ref DEBUG_PREFIX is non-empty.
+ *
+ * **Default**: disabled
+ */
+#ifndef CONFIG_DEBUG_SHOW_THREAD
+#  define CONFIG_DEBUG_SHOW_THREAD 0
+#endif
+
+#if IS_ACTIVE(CONFIG_DEBUG_SHOW_THREAD) && !defined(CONFIG_THREAD_NAMES)
+#  error "CONFIG_DEBUG_SHOW_THREAD can only be set if CONFIG_THREAD_NAMES is set"
+#endif
+
+/**
+ * @brief   Whether calls to @ref DEBUG and @ref DEBUG_PUTS automatically include
+ *          the current function name.
+ *
+ * @warning Only applies to files where @ref DEBUG_PREFIX is non-empty.
+ *
+ * **Default**: disabled
+ */
+#if !defined(CONFIG_DEBUG_SHOW_FUNC) || defined(DOXYGEN)
+#  define CONFIG_DEBUG_SHOW_FUNC 0
+#endif
+
+/**
+ * @brief Color for the debug prefix.
+ *
+ * @internal
+ */
+#define _DEBUG_DISPLAY_FOR_PREFIX      ANSI_STYLE(FG_BRIGHT(CYAN), BOLD)
+#define _DEBUG_DISPLAY_FOR_THREAD_FUNC ANSI_STYLE(FG(WHITE), DIM)
+
+/**
+ * @brief Check whether the stack of the current thread (or ISR) is big enough in total
+ *        for printf formatting when `DEVELHELP` is enabled. Otherwise, it returns true.
+ *
+ * @warning This only checks for the whole stack size, not for the currently free part of it.
+ *
+ * @internal
+ *
+ * @param    print              Whether to print a warning message
+ * @retval   true               Stack is sufficiently big, or `DEVELHELP` is disabled
+ * @retval   false              Stack is too small
+ */
+static inline bool __debug_sufficient_stack(bool print)
+{
+#if IS_ACTIVE(DEVELHELP)
+    const thread_t *thread = thread_get_active();
+    if (((thread != NULL) && (thread->stack_size < THREAD_EXTRA_STACKSIZE_PRINTF)) ||
+#  ifdef ISR_STACKSIZE
+        (irq_is_in() && (ISR_STACKSIZE < THREAD_EXTRA_STACKSIZE_PRINTF))) {
+#  else
+        false) {
+#  endif
+        if (print) {
+            fputs("Cannot debug, stack too small. Consider using DEBUG_PUTS().\n", stdout);
+        }
+        return false;
+    }
+#endif /* IS_ACTIVE(DEVELHELP) */
+    (void)print;
+    return true;
+}
+
+/**
+ * @brief Get thread name of the currently running thread, or "<isr>"
+ *
+ * @internal
+ *
+ * @return   const char*    the thread name, or "(isr)"
+ */
+static inline const char *__debug_thread_name_or_isr(void)
+{
+    const thread_t *thread = thread_get_active();
+    return (irq_is_in() || thread == NULL) ? "<isr>" : thread_get_name(thread);
+}
+
+/**
+ * @def DEBUG_
+ *
+ * @brief Print debug information to stdout with a custom prefix
+ *
+ * @internal
+ *
+ * Use this internal macro if you want to have something more fine-grained than
+ * the file-wide @ref DEBUG_PREFIX, to define your own debug function like
+ *
+ * @code {.c}
+ * #define CUSTOM_DEBUG(...) DEBUG_("custom-prefix", __VA_ARGS__)
+ * @endcode
+ *
+ * Otherwise, just use @ref DEBUG
+ */
+#define DEBUG_(prefix, ...)                                                                     \
+    do {                                                                                        \
+        if (ENABLE_DEBUG && __debug_sufficient_stack(true)) {                                   \
+            if (strlen(prefix) > 0) {                                                           \
+                if (IS_ACTIVE(CONFIG_DEBUG_SHOW_FUNC) && IS_ACTIVE(CONFIG_DEBUG_SHOW_THREAD)) { \
+                    printf(_DEBUG_DISPLAY_FOR_PREFIX prefix _DEBUG_DISPLAY_FOR_THREAD_FUNC      \
+                           " (%s@%s): " ANSI_STYLE_RESET,                                       \
+                           DEBUG_FUNC, __debug_thread_name_or_isr());                           \
+                }                                                                               \
+                else if (IS_ACTIVE(CONFIG_DEBUG_SHOW_FUNC)) {                                   \
+                    printf(_DEBUG_DISPLAY_FOR_PREFIX prefix _DEBUG_DISPLAY_FOR_THREAD_FUNC      \
+                           " (%s): " ANSI_STYLE_RESET,                                          \
+                           DEBUG_FUNC);                                                         \
+                }                                                                               \
+                else if (IS_ACTIVE(CONFIG_DEBUG_SHOW_THREAD)) {                                 \
+                    printf(_DEBUG_DISPLAY_FOR_PREFIX prefix _DEBUG_DISPLAY_FOR_THREAD_FUNC      \
+                           " (@%s): " ANSI_STYLE_RESET,                                         \
+                           __debug_thread_name_or_isr());                                       \
+                }                                                                               \
+                else {                                                                          \
+                    printf(_DEBUG_DISPLAY_FOR_PREFIX prefix _DEBUG_DISPLAY_FOR_THREAD_FUNC      \
+                           ": " ANSI_STYLE_RESET);                                              \
+                }                                                                               \
+            }                                                                                   \
+            printf(__VA_ARGS__);                                                                \
+        }                                                                                       \
+    } while (0)
 
 /**
  * @def DEBUG
  *
  * @brief Print debug information to stdout
+ *
+ * Use this macro similarly to `printf` when starting a new line.
+ * Remember to end the line with an explicit newline character `\n`.
+ * If you instead want to continue writing to the same line afterwards,
+ * use @ref DEBUG_CONT for the subsequent calls (and end the line there).
+ *
+ * DEBUG macros will perform a crude check whether the current stack may be
+ * big enough for a call to `printf` when `DEVELHELP` is defined.
  *
  * @note    This looks similar to the @ref LOG_DEBUG() function. However, it is
  *          enabled on a per-file basis. Prefer @ref DEBUG for debug output
@@ -115,29 +235,109 @@ extern "C" {
  *          debug output relevant for application developers using your module
  *          (e.g. to hint potentially incorrect / inefficient use of your
  *          library).
- * @details If a variable is only accessed by `DEBUG()`, the compiler will
+ * @warning If a variable is only accessed by `DEBUG()`, the compiler will
  *          warn about unused variables when `ENABLE_DEBUG` is set to `0`.
+ *
+ * Make use of @ref DEBUG_ if you need to use a custom prefix.
  */
-#define DEBUG(...) do { if (ENABLE_DEBUG) { DEBUG_PRINT(__VA_ARGS__); } } while (0)
+#define DEBUG(...) DEBUG_(DEBUG_PREFIX, __VA_ARGS__)
+
+/**
+ * @def DEBUG_CONT
+ *
+ * @brief Continue printing debug information to stdout, without repeating the prefix
+ *
+ * Use this macro the same way as `printf` if you want to continue printing to the
+ * same line that has been started with @ref DEBUG previously.
+ */
+#define DEBUG_CONT(...)                                        \
+    do {                                                       \
+        if (ENABLE_DEBUG && __debug_sufficient_stack(false)) { \
+            printf(__VA_ARGS__);                               \
+        }                                                      \
+    } while (0)
+
+/**
+ * @def DEBUG_PUTS
+ *
+ * @brief Print debug information to stdout using puts() witha custom prefix.
+ *
+ * @internal
+ *
+ * Use this internal macro if you want to have something more fine-grained than
+ * the file-wide @ref DEBUG_PREFIX, to define your own debug function like
+ *
+ * @code {.c}
+ * #define CUSTOM_DEBUG_PUTS(str) DEBUG_PUTS_("custom-prefix", str)
+ * @endcode
+ *
+ * Otherwise, just use @ref DEBUG_PUTS
+ */
+#define DEBUG_PUTS_(prefix, str)                                                                \
+    do {                                                                                        \
+        if (ENABLE_DEBUG) {                                                                     \
+            if (strlen(prefix) > 0) {                                                           \
+                if (IS_ACTIVE(CONFIG_DEBUG_SHOW_FUNC) && IS_ACTIVE(CONFIG_DEBUG_SHOW_THREAD)) { \
+                    fputs(_DEBUG_DISPLAY_FOR_PREFIX prefix, stdout);                            \
+                    fputs(_DEBUG_DISPLAY_FOR_THREAD_FUNC " (", stdout);                         \
+                    fputs(DEBUG_FUNC, stdout);                                                  \
+                    fputs("@", stdout);                                                         \
+                    fputs(__debug_thread_name_or_isr(), stdout);                                \
+                    fputs("): " ANSI_STYLE_RESET, stdout);                                      \
+                }                                                                               \
+                else if (IS_ACTIVE(CONFIG_DEBUG_SHOW_FUNC)) {                                   \
+                    fputs(_DEBUG_DISPLAY_FOR_PREFIX prefix, stdout);                            \
+                    fputs(_DEBUG_DISPLAY_FOR_THREAD_FUNC " (", stdout);                         \
+                    fputs(DEBUG_FUNC, stdout);                                                  \
+                    fputs("): " ANSI_STYLE_RESET, stdout);                                      \
+                }                                                                               \
+                else if (IS_ACTIVE(CONFIG_DEBUG_SHOW_THREAD)) {                                 \
+                    fputs(_DEBUG_DISPLAY_FOR_PREFIX prefix, stdout);                            \
+                    fputs(_DEBUG_DISPLAY_FOR_THREAD_FUNC " (@", stdout);                        \
+                    fputs(__debug_thread_name_or_isr(), stdout);                                \
+                    fputs("): " ANSI_STYLE_RESET, stdout);                                      \
+                }                                                                               \
+                else if (strlen(DEBUG_PREFIX) > 0) {                                            \
+                    fputs(_DEBUG_DISPLAY_FOR_PREFIX prefix _DEBUG_DISPLAY_FOR_THREAD_FUNC       \
+                          ": " ANSI_STYLE_RESET,                                                \
+                          stdout);                                                              \
+                }                                                                               \
+            }                                                                                   \
+            puts(str);                                                                          \
+        }                                                                                       \
+    } while (0)
 
 /**
  * @def DEBUG_PUTS
  *
  * @brief Print debug information to stdout using puts(), so no stack size
  *        restrictions do apply.
+ *
+ * Make use of @ref DEBUG_PUTS_ if you need to use a custom prefix.
  */
-#define DEBUG_PUTS(str) do { if (ENABLE_DEBUG) { puts(str); } } while (0)
+#define DEBUG_PUTS(str)  DEBUG_PUTS_(DEBUG_PREFIX, str)
 /** @} */
+
+/**
+ * @def DEBUG_PRINT
+ *
+ * @deprecated use @ref DEBUG instead. Will be removed after release 2027.04.
+ */
+#define DEBUG_PRINT(...) DEBUG(__VA_ARGS__)
 
 /**
  * @def DEBUG_EXTRA_STACKSIZE
  *
  * @brief Extra stacksize needed when ENABLE_DEBUG==1
+ *
+ * @deprecated This macro definition does not work anyway as ENABLE_DEBUG
+ *             is only set on file-level. Just remove its usages.
+ *             Will be removed after release 2027.04.
  */
 #if ENABLE_DEBUG
-#define DEBUG_EXTRA_STACKSIZE THREAD_EXTRA_STACKSIZE_PRINTF
+#  define DEBUG_EXTRA_STACKSIZE THREAD_EXTRA_STACKSIZE_PRINTF
 #else
-#define DEBUG_EXTRA_STACKSIZE (0)
+#  define DEBUG_EXTRA_STACKSIZE (0)
 #endif
 
 #ifdef __cplusplus
