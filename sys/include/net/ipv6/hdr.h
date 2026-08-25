57022d61e52e9bab54415a65d839a7675c568b04
Mikolai Gütschow <mikolai.guetschow@tu-dresden.de>
2026-08-25 20:59:35 +0200

    byteorder.h: remove unused union members
    this makes the union much more readible, e.g. in the debugger

--- sys/include/net/ipv6/hdr.h
+++ sys/include/net/ipv6/hdr.h
@@ -244,7 +244,8 @@ static inline void ipv6_hdr_set_fl(ipv6_hdr_t *hdr, uint32_t fl)
 {
     hdr->v_tc_fl.u8[1] &= 0xf0;
     hdr->v_tc_fl.u8[1] |= (0x0f & (byteorder_htonl(fl).u8[1]));
-    hdr->v_tc_fl.u16[1] = byteorder_htonl(fl).u16[1];
+    hdr->v_tc_fl.u8[2] = byteorder_htonl(fl).u8[2];
+    hdr->v_tc_fl.u8[3] = byteorder_htonl(fl).u8[3];
 }
 
 /**
