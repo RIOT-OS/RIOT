da2ba8fdb7e0f359fe43049e22aa5f251b3b643c
crasbe <crasbe@gmail.com>
2026-09-03 14:21:53 +0200

    dist/tools: fix shellcheck errors

--- dist/tools/cppcheck/check.sh
+++ dist/tools/cppcheck/check.sh
@@ -59,7 +59,7 @@ github_annotate_setup
 # TODO: switch back to 8 jobs when/if cppcheck issue is resolved
 cppcheck --std=c99 --enable=style --force --error-exitcode=2 --quiet -j 1 \
          --template "{file}:{line}: {severity} ({id}): {message}"         \
-         --inline-suppr ${DEFAULT_SUPPRESSIONS} ${CPPCHECK_OPTIONS} ${@}  \
+         --inline-suppr ${DEFAULT_SUPPRESSIONS} ${CPPCHECK_OPTIONS} "${@}"  \
          ${FILES} 2>&1 | ${LOG} 1>&2
 
 RESULT=${PIPESTATUS[0]}
