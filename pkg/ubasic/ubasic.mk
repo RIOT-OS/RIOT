356bc9be7f3d6994811eac4ffd18166262b18599
crasbe <crasbe@gmail.com>
2026-09-04 10:13:10 +0200

    pkg/ubasic: disable qualifier warning for LLVM

--- pkg/ubasic/ubasic.mk
+++ pkg/ubasic/ubasic.mk
@@ -1,8 +1,9 @@
 MODULE = ubasic
 
 # some toolchains complain about the usage of isdigit with a pointer in the
-# tokenizer
+# tokenizer as well as setting a non-const pointer with strchr
 CFLAGS += -Wno-char-subscripts
+CFLAGS += -Wno-discarded-qualifiers
 
 SRC := tokenizer.c ubasic.c
 NO_AUTO_SRC := 1
