6f8a747c698861cd90e1e8ce46d1d4c7de64666a
krzysztof-cabaj <kcabaj@gmail.com>
2026-09-08 16:47:41 +0200

    Update pkg/lvgl/include/lv_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -44,7 +44,8 @@ extern "C" {
 #endif
 
 /** Swap the 2 bytes of RGB565 color.
- *  Useful if the display has a 8 bit interface (e.g. SPI). */
+ *
+ * Useful if the display has an 8 bit interface (e.g. SPI). */
 #ifndef LV_COLOR_16_SWAP
 #  if IS_USED(MODULE_LCD)
 #    define LV_COLOR_16_SWAP   1
