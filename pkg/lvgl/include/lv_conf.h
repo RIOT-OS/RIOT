635527c983d96814d425ec21072968ff0413a361
krzysztof-cabaj <kcabaj@gmail.com>
2026-09-08 12:31:33 +0200

    Update pkg/lvgl/include/lv_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -55,8 +55,7 @@ extern "C" {
 /* Enable more complex drawing routines to manage screens transparency.
  * Can be used if the UI is above another layer, e.g. an OSD menu or video player.
  * Requires `LV_COLOR_DEPTH = 32` colors and the screen's `bg_opa` should be set
- * to non LV_OPA_COVER value
- */
+ * to a non LV_OPA_COVER value. */
 #define LV_COLOR_SCREEN_TRANSP    0
 
 /**Images pixels with this color will not be drawn if they are chroma keyed)*/
