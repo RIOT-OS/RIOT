97fd4a44d15a290bddceab0cdbddd89189add536
krzysztof-cabaj <kcabaj@gmail.com>
2026-09-08 12:32:46 +0200

    Update pkg/lvgl/include/lv_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -116,8 +116,7 @@ extern "C" {
 #define LV_INDEV_DEF_READ_PERIOD    30      /*[ms]*/
 
 /** Default Dot Per Inch. Used to initialize default sizes such as widgets sized,
- * style paddings. (Not so important, you can adjust it to modify default sizes and spaces)
-*/
+ * style paddings. (Not very important, you can adjust it to modify default sizes and spaces.) */
 #define LV_DPI_DEF                  130     /*[px/inch]*/
 
 /*=======================
