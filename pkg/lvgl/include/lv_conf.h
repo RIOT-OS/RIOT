a0f65043e541ac5489cc1fb14ee8cef13cd4b5b5
krzysztof-cabaj <kcabaj@gmail.com>
2026-09-08 17:11:21 +0200

    Update pkg/lvgl/include/lv_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -77,7 +77,11 @@ extern "C" {
    MEMORY SETTINGS
  *=========================*/
 
-/** 1: use custom malloc/free, 0: use the built-in `lv_mem_alloc()` and `lv_mem_free()`*/
+/** Memory allocation functions
+ *
+ * - 1: use custom malloc/free
+ * - 0: use the built-in `lv_mem_alloc()` and `lv_mem_free()`
+ */
 #define LV_MEM_CUSTOM      0
 #if LV_MEM_CUSTOM == 0
 /* Size of the memory available for `lv_mem_alloc()` in bytes (>= 2kB) */
