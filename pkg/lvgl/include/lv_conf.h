61bf6e7bf04bd73f39ea5251813e84959b2c5caf
krzysztof-cabaj <kcabaj@gmail.com>
2026-09-08 20:08:36 +0200

    Update pkg/lvgl/include/lv_conf.h
    Co-authored-by: crasbe <crasbe@gmail.com>

--- pkg/lvgl/include/lv_conf.h
+++ pkg/lvgl/include/lv_conf.h
@@ -328,7 +328,7 @@ extern "C" {
 /** Define a custom attribute to the `lv_tick_inc()` function */
 #define LV_ATTRIBUTE_TICK_INC
 
-/** Define a custom attribute to `lv_timer_handler` function */
+/** Define a custom attribute to the `lv_timer_handler()` function */
 #define LV_ATTRIBUTE_TIMER_HANDLER
 
 /** Define a custom attribute to `lv_disp_flush_ready` function */
