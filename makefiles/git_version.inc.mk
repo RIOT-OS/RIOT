4132827b068c519a0e68e660d92e8d8bbf7baaef
Bas Stottelaar <basstottelaar@gmail.com>
2026-09-15 14:36:58 +0200

    makefiles: evaluate RIOT_VERSION_GIT lazily
    This variable is recursively expanded on purpose, because it is only expanded
    when $(RIOT_VERSION) has not been set already. This is the case for CI
    builds. Outside of CI, $(GIT_VERSION) is memoized and will not be
    re-evaluated on every expansion of $(RIOT_VERSION_GIT).

--- makefiles/git_version.inc.mk
+++ makefiles/git_version.inc.mk
@@ -9,3 +9,10 @@ GIT_BRANCH = $(call memoized,GIT_BRANCH,$(shell git --git-dir="$(RIOTBASE)/.git"
 _GIT_VERSION = $(GIT_DESCRIBE)$(addprefix -,$(GIT_BRANCH:master=))
 
 GIT_VERSION ?= $(if $(GIT_DESCRIBE),$(_GIT_VERSION))
+
+# This variable is recursively expanded on purpose. It is only expanded when
+# $(RIOT_VERSION) has not been set already. For CI builds, Makefile.include
+# sets the version to 'buildtest' before this is used, so $(GIT_VERSION) is
+# never expanded and two shell calls are saved. Outside of CI, $(GIT_VERSION)
+# is memoized and will not be re-evaluated on every expansion.
+RIOT_VERSION_GIT = $(or $(GIT_VERSION),'UNKNOWN (builddir: $(RIOTBASE))')
