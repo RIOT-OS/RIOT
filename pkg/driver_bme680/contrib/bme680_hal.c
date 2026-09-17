39e6d07d2b7cf26a051275feb96f4523ee2f4d6a
Krzysztof Cabaj <kcabaj@gmail.com>
2026-09-17 18:51:21 +0200

    pkg: move licenses to SPDX format

--- pkg/driver_bme680/contrib/bme680_hal.c
+++ pkg/driver_bme680/contrib/bme680_hal.c
@@ -1,10 +1,7 @@
 /*
- * Copyright (C) 2019 Mesotic SAS
- *               2020 Gunar Schorcht
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * SPDX-FileCopyrightText: 2019 Mesotic SAS
+ * SPDX-FileCopyrightText: 2020 Gunar Schorcht
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 /**
--- pkg/etl/config/etl_profile.h
+++ pkg/etl/config/etl_profile.h
@@ -1,10 +1,7 @@
 /*
-* Copyright (C) 2022 Jens Wetterich <jens@wetterich-net.de>
-*
-* This file is subject to the terms and conditions of the GNU Lesser
-* General Public License v2.1. See the file LICENSE in the top level
-* directory for more details.
-*/
+ * SPDX-FileCopyrightText: 2022 Jens Wetterich <jens@wetterich-net.de>
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 
 #pragma once
 
