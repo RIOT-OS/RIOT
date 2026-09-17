ffbbca0850cff982cc4428cbf5c9b133a4388441
Jan Thies <jan.thies@haw-hamburg.de>
2026-09-17 12:16:24 +0200

    pkg/cryptoservice: add the package and PSA wrappers

---
+++ pkg/cryptoservice/contrib/os_mutex.c
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2024 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#include "mutex.h"
+#include "CYS/os_mutex.h"
+
+mutex_t cryptoservice_mutex;
+
+void os_init_mutex(void)
+{
+    mutex_init(&cryptoservice_mutex);
+}
+
+CYS_error_t os_get_mutex(void)
+{
+    return mutex_trylock(&cryptoservice_mutex) ? CYS_SUCCESS : CYS_ERROR_BAD_STATE;
+}
+
+void os_release_mutex(void)
+{
+    mutex_unlock(&cryptoservice_mutex);
+}
---
+++ pkg/cryptoservice/psa_cryptoservice/random.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2024 HAW Hamburg
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
+
+#include "psa/crypto.h"
+#include "CYS/unprotected.h"
+
+psa_status_t psa_custom_generate_random(uint8_t *output, size_t output_size)
+{
+    if (!output) {
+        return PSA_ERROR_INVALID_ARGUMENT;
+    }
+
+    return CYS_random_generate(output, output_size);
+}
