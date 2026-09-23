0c4d25a52e54169f939ef2dbe1de57c0b5a8a86b
Jose Alamos <jose@alamos.cc>
2026-09-23 15:57:06 +0200

    net/gnrc/netif: implement gnrc_netif_get_by_type with netif_register

--- sys/net/gnrc/netif/gnrc_netif.c
+++ sys/net/gnrc/netif/gnrc_netif.c
@@ -141,17 +141,17 @@ gnrc_netif_t *gnrc_netif_iter(const gnrc_netif_t *prev)
     return (result) ? container_of(result, gnrc_netif_t, netif) : NULL;
 }
 
-gnrc_netif_t *gnrc_netif_get_by_type(netdev_type_t type, uint8_t index)
+gnrc_netif_t *gnrc_netif_get_by_type(netif_type_t type, uint8_t index)
 {
     gnrc_netif_t *netif = NULL;
     while ((netif = gnrc_netif_iter(netif))) {
 
 #ifdef MODULE_NETDEV_REGISTER
-        if (netif->dev->type != type && type != NETDEV_ANY) {
+        if (netif->netif.type != type && type != NETIF_ANY) {
             continue;
         }
 
-        if (netif->dev->index != index && index != NETDEV_INDEX_ANY) {
+        if (netif->netif.index != index && index != NETIF_INDEX_ANY) {
             continue;
         }
 #else
