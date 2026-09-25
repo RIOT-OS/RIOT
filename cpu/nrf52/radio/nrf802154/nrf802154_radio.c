cb6ed275d33c1ec72ff441abcbc3fc5f133ef2e5
Jose Alamos <jose@alamos.cc>
2026-09-25 12:28:04 +0200

    drivers/ieee802154/nrf802154: declare frame retention capability

--- cpu/nrf52/radio/nrf802154/nrf802154_radio.c
+++ cpu/nrf52/radio/nrf802154/nrf802154_radio.c
@@ -779,7 +779,8 @@ static const ieee802154_radio_ops_t nrf802154_ops = {
           | IEEE802154_CAP_IRQ_TX_START
           | IEEE802154_CAP_IRQ_TX_DONE
           | IEEE802154_CAP_IRQ_CCA_DONE
-          | IEEE802154_CAP_PHY_OQPSK,
+          | IEEE802154_CAP_PHY_OQPSK
+          | IEEE802154_CAP_FRAME_RETENTION,
 
     .write = _write,
     .read = _read,
