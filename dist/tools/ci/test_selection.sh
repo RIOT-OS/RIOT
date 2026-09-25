0c0206d2892c830afba49fcac0002c914de8a93d
AnnsAnn <git@annsann.eu>
2026-09-25 13:40:06 +0200

    fix: multiline

--- dist/tools/ci/test_selection.sh
+++ dist/tools/ci/test_selection.sh
@@ -24,11 +24,11 @@ TEST_PATHS[any_build_files]="features.yaml ${TEST_PATHS[any_make_files]} boards/
 # Test specific path patterns, e.g. if multiple checks are needed.
 # Tests matching a single group use the group name directly.
 TEST_PATHS[features]="features.yaml makefiles/features_existing.inc.mk"
-TEST_PATHS[doccheck]="${TEST_PATHS[any_doc_files]} ${TEST_PATHS[any_c_files]}"\
-                     "makefiles/pseudomodules.inc.mk"
+TEST_PATHS[doccheck]="${TEST_PATHS[any_doc_files]} ${TEST_PATHS[any_c_files]} "\
+"makefiles/pseudomodules.inc.mk"
 TEST_PATHS[board_doc]="boards/*"
-TEST_PATHS[codespell]="${TEST_PATHS[any_c_files]} ${TEST_PATHS[any_shell_files]}"\
-                      "${TEST_PATHS[any_python_files]} ${TEST_PATHS[any_text_files]}"
+TEST_PATHS[codespell]="${TEST_PATHS[any_c_files]} ${TEST_PATHS[any_shell_files]} "\
+"${TEST_PATHS[any_python_files]} ${TEST_PATHS[any_text_files]}"
 TEST_PATHS[code_in_guides]="doc/guides/* ${TEST_PATHS[any_example_files]}"
 
 # true if any given path changed vs BASE_BRANCH; true if base unknown
